e99a884e5ffb6e65e73d5b5df9275e706c039598
Emmanuel Briot <briot@adacore.com>
2000-05-15 10:42:26 +0000

    (Clear_Value, Clone): New subprograms Add support for Modulo, Range and Union types.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@66630 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/generic_values.adb
+++ gvd/gvd/generic_values.adb
@@ -4,20 +4,20 @@ package body Generic_Values is
 
    package body Array_Indexes is
 
-      ---------------------
-      -- Compute_Indexes --
-      ---------------------
+      -------------------
+      -- Compute_Index --
+      -------------------
 
-      function Compute_Indexes (I : Indexes) return Integer is
-         Index : Integer := I (I'First);
+      function Compute_Index (I : Indexes) return Long_Integer is
+         Index : Long_Integer := I (I'First) - For_Array.Dimensions (1).First;
       begin
          for J in 2 .. For_Array.Num_Dimensions loop
             Index := Index * (For_Array.Dimensions (J).Last
                               - For_Array.Dimensions (J).First + 1)
-              + I (I'First + J - 1);
+              + I (I'First + J - 1) - For_Array.Dimensions (J).First;
          end loop;
          return Index;
-      end Compute_Indexes;
+      end Compute_Index;
 
    end Array_Indexes;
 
@@ -28,10 +28,36 @@ package body Generic_Values is
    procedure Print (Value : Simple_Type) is
    begin
       if Value.Value = null then
-         Put_Line ("Simple: <null>");
+         Put ("{Simple: <null>}");
       else
-         Put_Line ("Simple: " & Value.Value.all);
+         Put ("{Simple: " & Value.Value.all & "}");
       end if;
+   end Print;
+
+   -----------
+   -- Print --
+   -----------
+
+   procedure Print (Value : Range_Type) is
+   begin
+      Put ("{Range" & Value.Min'Img & " .." & Value.Max'Img & " = ");
+      if Value.Value /= null then
+         Put (Value.Value.all);
+      end if;
+      Put ("}");
+   end Print;
+
+   -----------
+   -- Print --
+   -----------
+
+   procedure Print (Value : Mod_Type) is
+   begin
+      Put ("{Modulo " & Value.Max'Img & " = ");
+      if Value.Value /= null then
+         Put (Value.Value.all);
+      end if;
+      Put ("}");
    end Print;
 
    -----------
@@ -40,11 +66,11 @@ package body Generic_Values is
 
    procedure Print (Value : Access_Type) is
    begin
-      Put ("Access");
+      Put ("{Access ");
       if Value.Value = null then
-         Put_Line ("<null>");
+         Put ("<null>)");
       else
-         Put_Line (Value.Value.all);
+         Put (Value.Value.all & "}");
       end if;
    end Print;
 
@@ -54,7 +80,7 @@ package body Generic_Values is
 
    procedure Print (Value : Array_Type) is
    begin
-      Put ("Array (");
+      Put ("{Array (");
       for J in 1 .. Value.Num_Dimensions loop
          Put (Value.Dimensions (J).First'Img & " .. "
               & Value.Dimensions (J).Last'Img);
@@ -76,7 +102,7 @@ package body Generic_Values is
             end if;
          end loop;
       end if;
-      Put_Line (")");
+      Put (")}");
    end Print;
 
    -----------
@@ -85,13 +111,13 @@ package body Generic_Values is
 
    procedure Print (Value : Repeat_Type) is
    begin
-      Put ("<repeat " & Value.Repeat_Num'Img & " times> : ");
+      Put ("{<" & Value.Repeat_Num'Img & " times> : ");
       if Value.Value /= null then
          Print (Value.Value.all);
+         Put ("}");
       else
-         Put ("<null>");
+         Put ("<null>}");
       end if;
-      New_Line;
    end Print;
 
    -----------
@@ -100,20 +126,23 @@ package body Generic_Values is
 
    procedure Print (Value : Record_Type) is
    begin
-      Put ("Record: (");
+      Put ("{Record: ");
+      if Value.Fields'Length = 0 then
+         Put ("null record");
+      end if;
       for J in Value.Fields'Range loop
-         Put (Value.Fields (J).Name.all & " => ");
-
          if Value.Fields (J).Variant_Part /= null then
-            Put ("<" & Value.Fields (J).Variant_Part'Length'Img
-                 & " variant_part>:");
+            Put ("<variant_part on "
+                 & Value.Fields (J).Name.all
+                 & "> => ");
             for P in Value.Fields (J).Variant_Part'Range loop
-               Put ("(");
+               Put ("{");
                Print (Value.Fields (J).Variant_Part (P).all);
-               Put_Line (")");
+               Put ("}");
             end loop;
 
          else
+            Put (Value.Fields (J).Name.all & " => ");
             if Value.Fields (J).Value /= null then
                Print (Value.Fields (J).Value.all);
             end if;
@@ -122,7 +151,26 @@ package body Generic_Values is
             Put (", ");
          end if;
       end loop;
-      Put_Line (")");
+      Put ("}");
+   end Print;
+
+   -----------
+   -- Print --
+   -----------
+
+   procedure Print (Value : Union_Type) is
+   begin
+      Put ("{Union: ");
+      for J in Value.Fields'Range loop
+         Put (Value.Fields (J).Name.all & " => ");
+         if Value.Fields (J).Value /= null then
+            Print (Value.Fields (J).Value.all);
+         end if;
+         if J /= Value.Fields'Last then
+            Put (", ");
+         end if;
+      end loop;
+      Put ("}");
    end Print;
 
    -----------
@@ -131,12 +179,196 @@ package body Generic_Values is
 
    procedure Print (Value : Enum_Type) is
    begin
-      Put ("Enumeration = ");
+      Put ("{Enumeration = ");
       if Value.Value = null then
-         Put_Line ("<Unknown>");
+         Put ("<Unknown>}");
       else
-         Put_Line (Value.Value.all);
+         Put (Value.Value.all & "}");
       end if;
    end Print;
+
+   -----------------
+   -- Clear_Value --
+   -----------------
+
+   procedure Clear_Value (Value : in out Simple_Type) is
+   begin
+      Free (Value.Value);
+   end Clear_Value;
+
+   -----------------
+   -- Clear_Value --
+   -----------------
+
+   procedure Clear_Value (Value : in out Array_Type) is
+   begin
+      if Value.Values /= null then
+         for J in Value.Values'Range loop
+            Clear_Value (Value.Values (J).Value.all);
+         end loop;
+         Free (Value.Values);
+      end if;
+   end Clear_Value;
+
+   -----------------
+   -- Clear_Value --
+   -----------------
+
+   procedure Clear_Value (Value : in out Repeat_Type) is
+   begin
+      if Value.Value /= null then
+         Clear_Value (Value.Value.all);
+      end if;
+   end Clear_Value;
+
+   -----------------
+   -- Clear_Value --
+   -----------------
+
+   procedure Clear_Value (Value : in out Record_Type) is
+   begin
+      for J in Value.Fields'Range loop
+         if Value.Fields (J).Value /= null then
+            Clear_Value (Value.Fields (J).Value.all);
+            if Value.Fields (J).Variant_Part /= null then
+               for V in Value.Fields (J).Variant_Part'Range loop
+                  Clear_Value (Value.Fields (J).Variant_Part (V).all);
+               end loop;
+            end if;
+         end if;
+      end loop;
+   end Clear_Value;
+
+   -----------
+   -- Clone --
+   -----------
+
+   function Clone (Value : Simple_Type)
+                  return Generic_Type_Access
+   is
+      R : Generic_Type_Access := new Simple_Type'(Value);
+   begin
+      Simple_Type_Access (R).Value := null;
+      return R;
+   end Clone;
+
+   -----------
+   -- Clone --
+   -----------
+
+   function Clone (Value : Range_Type)
+                  return Generic_Type_Access
+   is
+      R : Generic_Type_Access := new Range_Type'(Value);
+   begin
+      Simple_Type_Access (R).Value := null;
+      return R;
+   end Clone;
+
+   -----------
+   -- Clone --
+   -----------
+
+   function Clone (Value : Mod_Type)
+                  return Generic_Type_Access
+   is
+      R : Generic_Type_Access := new Mod_Type'(Value);
+   begin
+      Simple_Type_Access (R).Value := null;
+      return R;
+   end Clone;
+
+   -----------
+   -- Clone --
+   -----------
+
+   function Clone (Value : Access_Type)
+                  return Generic_Type_Access
+   is
+      R : Generic_Type_Access := new Access_Type'(Value);
+   begin
+      Simple_Type_Access (R).Value := null;
+      return R;
+   end Clone;
+
+   -----------
+   -- Clone --
+   -----------
+
+   function Clone (Value : Enum_Type)
+                  return Generic_Type_Access
+   is
+      R : Generic_Type_Access := new Enum_Type'(Value);
+   begin
+      Simple_Type_Access (R).Value := null;
+      return R;
+   end Clone;
+
+   -----------
+   -- Clone --
+   -----------
+
+   function Clone (Value : Array_Type)
+                  return Generic_Type_Access
+   is
+      R : Array_Type_Access := new Array_Type'(Value);
+   begin
+      R.Values := null;
+      R.Item_Type := Clone (Value.Item_Type.all);
+      return Generic_Type_Access (R);
+   end Clone;
+
+   -----------
+   -- Clone --
+   -----------
+
+   function Clone (Value : Repeat_Type)
+                  return Generic_Type_Access
+   is
+      R : Repeat_Type_Access := new Repeat_Type'(Value);
+   begin
+      R.Value := Clone (Value.Value.all);
+      return Generic_Type_Access (R);
+   end Clone;
+
+   -----------
+   -- Clone --
+   -----------
+
+   function Clone (Value : Record_Type)
+                  return Generic_Type_Access
+   is
+      R : Record_Type_Access := new Record_Type'(Value);
+   begin
+      for J in R.Fields'Range loop
+         R.Fields (J).Name := new String'(R.Fields (J).Name.all);
+         R.Fields (J).Value := Clone (R.Fields (J).Value.all);
+         if R.Fields (J).Variant_Part /= null then
+            R.Fields (J).Variant_Part
+              := new Record_Type_Array'(R.Fields (J).Variant_Part.all);
+            for V in R.Fields (J).Variant_Part'Range loop
+               R.Fields (J).Variant_Part (V) := Record_Type_Access
+                 (Clone (R.Fields (J).Variant_Part (V).all));
+            end loop;
+         end if;
+      end loop;
+      return Generic_Type_Access (R);
+   end Clone;
+
+   -----------
+   -- Clone --
+   -----------
+
+   function Clone (Value : Union_Type)
+                  return Generic_Type_Access
+   is
+      R : Union_Type_Access := new Union_Type'(Value);
+   begin
+      for J in R.Fields'Range loop
+         R.Fields (J).Name := new String'(R.Fields (J).Name.all);
+         R.Fields (J).Value := Clone (R.Fields (J).Value.all);
+      end loop;
+      return Generic_Type_Access (R);
+   end Clone;
 
 end Generic_Values;
--- gvd/gvd/generic_values.ads
+++ gvd/gvd/generic_values.ads
@@ -1,4 +1,5 @@
 with System;
+with Unchecked_Deallocation;
 
 package Generic_Values is
 
@@ -9,6 +10,7 @@ package Generic_Values is
 
 
    type String_Access is access all String;
+   procedure Free is new Unchecked_Deallocation (String, String_Access);
 
    ------------------
    -- Generic_Type --
@@ -22,6 +24,19 @@ package Generic_Values is
    procedure Print (Value : Generic_Type) is abstract;
    --  Print Value on Standard_Output.
 
+   procedure Clear_Value (Value : in out Generic_Type) is abstract;
+   --  Clear the value, and free the memory that was allocated for it.
+   --  This also clears all the children or components of Value.
+
+   function Clone (Value : Generic_Type) return Generic_Type_Access
+      is abstract;
+   --  Clone Value, and return a new allocated record.
+   --  Only the type-related fields are cloned, the value fields are reset to
+   --  Null.
+
+   procedure Free is new Unchecked_Deallocation
+     (Generic_Type'Class, Generic_Type_Access);
+
    -----------------
    -- Simple_Type --
    -----------------
@@ -29,11 +44,44 @@ package Generic_Values is
    --  the string as displayed by the debugger.
 
    type Simple_Type is new Generic_Type with record
-      Value : String_Access; --  The value, as displayed by the debugger
+      Value : String_Access := null;
+      --  The value, as displayed by the debugger
    end record;
+   type Simple_Type_Access is access all Simple_Type'Class;
 
    procedure Print (Value : Simple_Type);
    --  Print Value on Standard_Output.
+
+   procedure Clear_Value (Value : in out Simple_Type);
+
+   function Clone (Value : Simple_Type) return Generic_Type_Access;
+
+   -----------------
+   -- Range Types --
+   -----------------
+   --  A range value, as in Ada 'range 0 .. 10', ie a number between the min
+   --  and max value.
+
+   type Range_Type is new Simple_Type with record
+      Min, Max : Long_Integer;
+   end record;
+   type Range_Type_Access is access all Range_Type'Class;
+
+   procedure Print (Value : Range_Type);
+   function Clone (Value : Range_Type) return Generic_Type_Access;
+
+   ---------------
+   -- Mod Types --
+   ---------------
+   --  These are modular types, between 0 and a maximal value.
+
+   type Mod_Type is new Simple_Type with record
+      Max : Long_Integer;
+   end record;
+   type Mod_Type_Access is access all Mod_Type'Class;
+
+   procedure Print (Value : Mod_Type);
+   function Clone (Value : Mod_Type) return Generic_Type_Access;
 
    ------------------
    -- Access Types --
@@ -43,22 +91,18 @@ package Generic_Values is
    --  from the Address field in Generic_Type which is the address of the
    --  variable itself.
 
-   type Access_Type is new Generic_Type with record
-      Value : String_Access;
-   end record;
-
+   type Access_Type is new Simple_Type with null record;
    procedure Print (Value : Access_Type);
-   --  Print Value on Standard_Output.
+   function Clone (Value : Access_Type) return Generic_Type_Access;
 
    ----------------
    -- Enum Types --
    ----------------
 
-   type Enum_Type is new Generic_Type with record
-      Value : String_Access;
-   end record;
+   type Enum_Type is new Simple_Type with null record;
 
    procedure Print (Value : Enum_Type);
+   function Clone (Value : Enum_Type) return Generic_Type_Access;
 
    ----------------
    -- Array_Type --
@@ -73,37 +117,36 @@ package Generic_Values is
    --  a single item, a string.
 
    type Dimension is record
-      First, Last : Integer;
+      First, Last : Long_Integer;
    end record;
    type Dimension_Array is array (Positive range <>) of Dimension;
 
    type Integer_Array is array (Positive range <>) of Integer;
 
    type Array_Item is record
-      Index : Integer;
+      Index : Long_Integer;
       --  Calculated with Compute_Index.
 
-      Value : Generic_Type_Access;
+      Value : Generic_Type_Access := null;
    end record;
    type Array_Item_Array is array (Positive range <>) of Array_Item;
 
    type Array_Item_Array_Access is access Array_Item_Array;
+   procedure Free is new Unchecked_Deallocation
+     (Array_Item_Array, Array_Item_Array_Access);
 
    type Array_Orientation is (Vertical, Horizontal);
 
    type Array_Type (Num_Dimensions : Positive) Is new Generic_Type with record
       Dimensions  : Dimension_Array (1 .. Num_Dimensions);
-      Values      : Array_Item_Array_Access;
-      Item_Type   : Generic_Type_Access;
-
-      Orientation : Array_Orientation := Horizontal;
-      --  Set by default by the language (FORTRAN vs C), but can be
-      --  overriden by the user.
+      Values      : Array_Item_Array_Access := null;
+      Item_Type   : Generic_Type_Access := null;
    end record;
    type Array_Type_Access is access all Array_Type'Class;
 
    procedure Print (Value : Array_Type);
-   --  Print Value on Standard_Output.
+   procedure Clear_Value (Value : in out Array_Type);
+   function Clone (Value : Array_Type) return Generic_Type_Access;
 
    -----------------------
    -- Computing indexes --
@@ -112,8 +155,8 @@ package Generic_Values is
    generic
       For_Array : Array_Type;
    package Array_Indexes is
-      type Indexes is array (1 .. For_Array.Num_Dimensions) of Integer;
-      function Compute_Indexes (I : Indexes) return Integer;
+      type Indexes is array (1 .. For_Array.Num_Dimensions) of Long_Integer;
+      function Compute_Index (I : Indexes) return Long_Integer;
       --  Return the index corresponding to a given position in the array.
       --  This result can be used in the record Array_Item
    end Array_Indexes;
@@ -125,11 +168,13 @@ package Generic_Values is
 
    type Repeat_Type is new Generic_Type with record
       Repeat_Num : Integer;
-      Value      : Generic_Type_Access;
+      Value      : Generic_Type_Access := null;
    end record;
+   type Repeat_Type_Access is access all Repeat_Type'Class;
 
    procedure Print (Value : Repeat_Type);
-   --  Print Value on Standard_Output.
+   procedure Clear_Value (Value : in out Repeat_Type);
+   function Clone (Value : Repeat_Type) return Generic_Type_Access;
 
    -------------
    -- Records --
@@ -139,8 +184,8 @@ package Generic_Values is
    type Record_Type_Array_Access is access Record_Type_Array;
 
    type Record_Field is record
-      Name  : String_Access;
-      Value : Generic_Type_Access;
+      Name  : String_Access := null;
+      Value : Generic_Type_Access := null;
       Variant_Part : Record_Type_Array_Access := null;
    end record;
    type Record_Field_Array is array (Natural range <>) of Record_Field;
@@ -162,8 +207,21 @@ package Generic_Values is
    type Record_Type_Array is array (Positive range <>) of Record_Type_Access;
 
    procedure Print (Value : Record_Type);
-   --  Print Value on Standard_Output.
+   procedure Clear_Value (Value : in out Record_Type);
+   function Clone (Value : Record_Type) return Generic_Type_Access;
 
+   ------------
+   -- Unions --
+   ------------
+
+   type Union_Type (Num_Fields : Natural) is new Record_Type (Num_Fields) with
+      record
+         null;
+      end record;
+   type Union_Type_Access is access all Union_Type'Class;
+
+   procedure Print (Value : Union_Type);
+   function Clone (Value : Union_Type) return Generic_Type_Access;
 
 --  enum DispValueType {
 --    UnknownType = 0,    // Unknown type
