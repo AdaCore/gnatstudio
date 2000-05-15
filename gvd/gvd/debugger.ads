7ae54fa04673f3402d0d9d0c54298a55cf3d22c5
Emmanuel Briot <briot@adacore.com>
2000-05-15 10:39:50 +0000

    (Break_Exception, Finish): New subprograms.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@66627 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/debugger.ads
+++ gvd/gvd/debugger.ads
@@ -13,20 +13,42 @@ package Debugger is
                         Entity   : String)
                        return Generic_Values.Generic_Type_Access
       is abstract;
-   --  Parse the type definition for Entity, and return a 
+   --  Parse the type definition for Entity, and return a
    --  tree as explained in Generic_Values.
+
+   procedure Parse_Value
+     (Debugger  : Debugger_Root;
+      Entity    : String;
+      Value     : in out Generic_Values.Generic_Type_Access)
+      is abstract;
+   --  Parse the value of Entity.
+   --  Value should contain the result of Parse_Type when this procedure is
+   --  called, and it is completed to reflect the new value.
 
    procedure Wait_Prompt (Debugger : Debugger_Root) is abstract;
    --  Wait for the prompt.
-   
+
    function Type_Of (Debugger : Debugger_Root;
-                     Entity : String)
+                     Entity   : String)
                     return String
       is abstract;
    --  Return the type of the entity.
    --  An empty string is returned if the entity is not defined in the
    --  current context.
    --  GDB_COMMAND: "ptype"
+
+   type Value_Format is (Decimal,
+                         Binary,
+                         Hexadecimal,
+                         Octal);
+
+   function Value_Of (Debugger : Debugger_Root;
+                      Entity   : String;
+                      Format   : Value_Format := Decimal)
+                     return String
+      is abstract;
+   --  Return the value of the entity.
+   --  GDB_COMMAND: "print"
 
    procedure Set_Executable (Debugger : Debugger_Root;
                              Executable : String)
@@ -39,13 +61,13 @@ package Debugger is
    procedure Run (Debugger : Debugger_Root) is abstract;
    --  Start the execution of the executable.
    --  The arguments must have been set by a call to Set_Arguments
-   --  Note that this command does not wait for the prompt, and returns 
+   --  Note that this command does not wait for the prompt, and returns
    --  immediately.
    --  GDB_COMMAND: "run"
-   
+
    procedure Break_Exception (Debugger  : Debugger_Root;
-			      Name      : String  := "";
-			      Unhandled : Boolean := False)
+                              Name      : String  := "";
+                              Unhandled : Boolean := False)
       is abstract;
    --  Break on an exception, if the debugger and the language recognize that
    --  feature.
@@ -56,21 +78,31 @@ package Debugger is
    --  Note all combinations are possible (for instance, Gdb in Ada mode can
    --  not break on a specific exception only when it is unhandled).
    --  GDB_COMMAND: "break exception"
-   
+
+   procedure Break_Subprogram (Debugger : Debugger_Root;
+                               Name     : String)
+      is abstract;
+   --  Break at the beginning of a specific subprogram.
+   --  GDB_COMMAND: "break"
+
+   procedure Finish (Debugger : Debugger_Root) is abstract;
+   --  Finish executing the current frame.
+   --  GDB_COMMAND: "finish"
+
    function Backtrace (Debugger : Debugger_Root) return String is abstract;
    --  Return the current backtrace.
-   
+
    Unknown_Command : exception;
    --  Raised when a command is not recognized either by the debugger, or for
    --  the currently activate language.
-   
+
 private
-   
+
    type Pipes_Id_Access is access all GNAT.Expect.Pipes_Id;
-   procedure Free is new Unchecked_Deallocation 
+   procedure Free is new Unchecked_Deallocation
      (GNAT.Expect.Pipes_Id, Pipes_Id_Access);
-   
+
    type Debugger_Root is abstract tagged record
-      Process : Pipes_Id_Access := null;
+      Process      : Pipes_Id_Access := null;
    end record;
 end Debugger;
