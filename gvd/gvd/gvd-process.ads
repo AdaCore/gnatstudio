ab37b697c8c981bcc2bbd50cb7399e4942fd459f
Arnaud Charlet <charlet@adacore.com>
2000-05-16 22:24:51 +0000

    Minor reformatting. Remove target dependent code that is now handled by GNAT.Expect.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@66687 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/gvd-process.ads
+++ gvd/gvd/gvd-process.ads
@@ -18,52 +18,24 @@
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
-with GNAT.OS_Lib; use GNAT.OS_Lib;
-with Gtk.Window; use Gtk.Window;
 with System;
 
-package Odd_Tools.Process is
+with GNAT.OS_Lib; use GNAT.OS_Lib;
+with GNAT.Expect; use GNAT.Expect;
 
-   type File_Descriptor is new Integer;
+with Gtk.Window; use Gtk.Window;
+
+with Debugger.Gdb; use Debugger.Gdb;
+with Debugger.Gdb.Ada; use Debugger.Gdb.Ada;
+
+package Odd.Process is
 
    type Debugger_Descriptor is record
-      Name           : String_Access;
-      --  Name of the created debugger (gdb, jdb, ...)
-
-      Input_Channel  : File_Descriptor;
-      --  Input channel to get data from the debugger
-
-      Output_Channel : File_Descriptor;
-      --  Output channel to send data from the debugger
-
-      Process        : Integer := 0;
-      --  Process Id of the created debugger, 0 if none.
-
-      Window         : Gtk_Window;
+      Debugger : aliased Gdb_Debugger;
+      Language : aliased Gdb_Ada_Language;
+      Window   : Gtk_Window;
    end record;
    --  This type holds all the informations related to a given debugger.
-
-   function Read
-     (Fd   : File_Descriptor;
-      A    : System.Address;
-      N    : Integer)
-      return Integer;
-   pragma Import (C, Read, "read");
-   --  Read N bytes to address A from file referenced by FD. Returned value
-   --  is count of bytes actually read, which can be less than N at EOF.
-
-   function Write
-     (Fd   : File_Descriptor;
-      A    : System.Address;
-      N    : Integer)
-      return Integer;
-   pragma Import (C, Write, "write");
-   --  Read N bytes to address A from file referenced by FD. Returned value
-   --  is count of bytes actually read, which can be less than N at EOF.
-
-   procedure Close (Fd : File_Descriptor);
-   pragma Import (C, Close);
-   --  Close a file given its file descriptor.
 
    procedure Create_Debugger
      (Window : access Gtk_Window_Record'Class;
@@ -87,4 +59,4 @@ package Odd_Tools.Process is
    --  Return the answer from the debugger in Result.
    --  If Pool, poll for UI events while waiting.
 
-end Odd_Tools.Process;
+end Odd.Process;
