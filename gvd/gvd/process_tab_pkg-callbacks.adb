20dc87f9157c3ad4f9eab1816733bc541f99c243
Emmanuel Briot <briot@adacore.com>
2000-06-02 09:00:01 +0000

    when the command was empty (just LF), display the previous one in the  debugger window. It is now clearer which command was repeated (something  that gdb'command line doesn't have ! :-)
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@66801 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/process_tab_pkg-callbacks.adb
+++ gvd/gvd/process_tab_pkg-callbacks.adb
@@ -68,14 +68,10 @@ package body Process_Tab_Pkg.Callbacks is
          Emit_Stop_By_Name (Top.Debugger_Text, "insert_text");
       else
          if Arg1 (Arg1'First) = ASCII.LF then
-            --  ??? Would be nice to change the cursor
             declare
                S : String :=
                  Get_Chars (Top.Debugger_Text, Gint (Top.Edit_Pos));
             begin
-               --  Insert the newline character after the user's command.
-               Text_Output_Handler (Top, "" & ASCII.LF);
-
                --  If the command is empty, then we simply reexecute the last
                --  user command. Note that, with gdb, we can't simply send
                --  LF, since some internal commands might have been executed
@@ -84,6 +80,9 @@ package body Process_Tab_Pkg.Callbacks is
                if S'Length = 0 then
                   begin
                      Move_To_Previous (Command_History);
+                     Text_Output_Handler
+                       (Top, Get_Current (Command_History) & ASCII.LF,
+                        Is_Command => True);
                      Process_User_Command (Top, Get_Current (Command_History));
                      Append (Command_History, Get_Current (Command_History));
                   exception
@@ -93,6 +92,9 @@ package body Process_Tab_Pkg.Callbacks is
                   end;
 
                else
+                  --  Insert the newline character after the user's command.
+                  Text_Output_Handler (Top, "" & ASCII.LF);
+
                   --  Process the command, and save it into the history.
                   Process_User_Command (Top, S);
                   Append (Command_History, S);
@@ -153,7 +155,8 @@ package body Process_Tab_Pkg.Callbacks is
                Move_To_Next (Command_History);
             end if;
             Set_Point (Top.Debugger_Text, Top.Edit_Pos);
-            Text_Output_Handler (Top, Get_Current (Command_History));
+            Text_Output_Handler (Top, Get_Current (Command_History),
+                                 Is_Command => True);
             Set_Position (Top.Debugger_Text,
                           Gint (Get_Length (Top.Debugger_Text)));
          exception
