4292c84b4aa1d393f3201eac5b145be77fced55b
Arnaud Charlet <charlet@adacore.com>
2000-06-07 17:43:41 +0000

    (On_Task_List_Select_Row): Full task switching implementation.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@66925 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/gvd-dialogs-callbacks.adb
+++ gvd/gvd/gvd-dialogs-callbacks.adb
@@ -5,11 +5,11 @@ with Gtk.Frame; use Gtk.Frame;
 with Gtk.Notebook; use Gtk.Notebook;
 with Gtk.Widget; use Gtk.Widget;
 with Gtk.Main; use Gtk.Main;
-with Gtkada.Code_Editors; use Gtkada.Code_Editors;
 with Odd_Intl; use Odd_Intl;
 with Odd.Pixmaps; use Odd.Pixmaps;
 with Odd.Process; use Odd.Process;
 with Debugger; use Debugger;
+with Gtkada.Code_Editors; use Gtkada.Code_Editors;
 
 package body Task_Dialog_Pkg.Callbacks is
 
@@ -25,38 +25,56 @@ package body Task_Dialog_Pkg.Callbacks is
       Params : Gtk.Arguments.Gtk_Args)
    is
       --  Since lists start at 0, increment the value.
-      Thread   : Gint := To_Gint (Params, 1) + 1;
-      Frame    : Gtk_Frame;
-      Editor   : Code_Editor;
+      Thread        : Gint := To_Gint (Params, 1) + 1;
+      Frame         : Gtk_Frame;
 
       --  Get the process notebook from the main window which is associated
       --  with the task dialog (toplevel (object)).
 
-      Notebook : Gtk_Notebook := Task_Dialog_Access
+      Notebook      : Gtk_Notebook := Task_Dialog_Access
         (Get_Toplevel (Object)).Main_Window.Process_Notebook;
 
       --  Get the current page in the process notebook.
 
-      Process  : Debugger_Process_Tab :=
+      Process       : Debugger_Process_Tab :=
         Process_User_Data.Get (Get_Nth_Page
           (Notebook, Get_Current_Page (Notebook)));
-      Label    : Gtk_Label;
+      Label         : Gtk_Label;
+      Child         : Page_List.Glist;
+      Page          : Gint;
+      Notebook_Page : Gtk_Notebook_Page;
+
+      use Page_List;
 
    begin
+      Child := Get_Children (Process.Thread_Notebook);
+      Page := 0;
+
+      while Child /= Null_List loop
+         Notebook_Page := Get_Data (Child);
+
+         if Get (Gtk_Label (Get_Tab_Label (Notebook_Page))) =
+           -"Thread" & Gint'Image (Thread)
+         then
+            --  The desired thread has already a page associated with it.
+
+            Reparent (Process.Editor_Text, Get_Child (Notebook_Page));
+            Set_Page (Process.Thread_Notebook, Page);
+            Thread_Switch (Process.Debugger, Natural (Thread));
+
+            return;
+         end if;
+
+         Child := Next (Child);
+         Page := Page + 1;
+      end loop;
+
       Gtk_New (Frame);
       Gtk_New (Label, -"Thread" & Gint'Image (Thread));
       Append_Page (Process.Thread_Notebook, Frame, Label);
-      Gtk_New_Hbox (Editor, False, 0);
-      Add (Frame, Editor);
-      Show_All (Frame);
+      Reparent (Process.Editor_Text, Frame);
+      Show (Frame);
       Set_Page (Process.Thread_Notebook, -1);
-      Configure
-        (Editor, Editor_Font, Editor_Font_Size,
-         dot_xpm, arrow_xpm,
-         Comments_Color    => Comments_Color,
-         Strings_Color     => Strings_Color,
-         Keywords_Color    => Keywords_Color,
-         Show_Line_Numbers => Editor_Show_Line_Nums);
       Thread_Switch (Process.Debugger, Natural (Thread));
    end On_Task_List_Select_Row;
 
