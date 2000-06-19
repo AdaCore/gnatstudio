0e15e26777ec8fbbe1fd215cdeb37edc9d41406b
Arnaud Charlet <charlet@adacore.com>
2000-06-19 08:00:49 +0000

    Add header. Remove unused packages.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@67069 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/open_program_pkg.adb
+++ gvd/gvd/open_program_pkg.adb
@@ -1,9 +1,29 @@
-with Glib; use Glib;
+-----------------------------------------------------------------------
+--                 Odd - The Other Display Debugger                  --
+--                                                                   --
+--                         Copyright (C) 2000                        --
+--                 Emmanuel Briot and Arnaud Charlet                 --
+--                                                                   --
+-- Odd is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
 with Gtk; use Gtk;
 with Gtk.Widget;      use Gtk.Widget;
 with Gtk.Enums;       use Gtk.Enums;
+with Gtk.Menu;        use Gtk.Menu;
+with Gtk.Menu_Item;   use Gtk.Menu_Item;
 with Gtk.Main;
-with Gtkada.Handlers; use Gtkada.Handlers;
 with Callbacks_Odd; use Callbacks_Odd;
 with Odd_Intl; use Odd_Intl;
 with Open_Program_Pkg.Callbacks; use Open_Program_Pkg.Callbacks;
--- gvd/gvd/open_program_pkg.ads
+++ gvd/gvd/open_program_pkg.ads
@@ -1,3 +1,23 @@
+-----------------------------------------------------------------------
+--                 Odd - The Other Display Debugger                  --
+--                                                                   --
+--                         Copyright (C) 2000                        --
+--                 Emmanuel Briot and Arnaud Charlet                 --
+--                                                                   --
+-- Odd is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
 with Gtk.Window; use Gtk.Window;
 with Gtk.Box; use Gtk.Box;
 with Gtk.Frame; use Gtk.Frame;
@@ -8,10 +28,7 @@ with Gtk.GEntry; use Gtk.GEntry;
 with Gtk.Button; use Gtk.Button;
 with Gtk.Label; use Gtk.Label;
 with Gtk.Option_Menu; use Gtk.Option_Menu;
-with Gtk.Menu; use Gtk.Menu;
-with Gtk.Menu_Item; use Gtk.Menu_Item;
 with Gtk.Hbutton_Box; use Gtk.Hbutton_Box;
-with Gtk.Object; use Gtk.Object;
 with GNAT.OS_Lib; use GNAT.OS_Lib;
 with Debugger; use Debugger;
 
