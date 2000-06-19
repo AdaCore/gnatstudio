49d2abeafcc36de4baee1b148098c37d7486d41a
Arnaud Charlet <charlet@adacore.com>
2000-06-19 07:58:10 +0000

    Add header. Remove unused packages. (On_Backtrace_Process_Stopped): New callback needed to auto update the  backtrace window. Remove debug printing.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@67063 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/gvd-dialogs-callbacks.adb
+++ gvd/gvd/gvd-dialogs-callbacks.adb
@@ -1,3 +1,23 @@
+-----------------------------------------------------------------------
+--                 Odd - The Other Display Debugger                  --
+--                                                                   --
+--                         Copyright (C) 2000                        --
+--                 Emmanuel Briot and Arnaud Charlet                 --
+--                                                                   --
+-- Odd is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
 with System; use System;
 with Glib; use Glib;
 with Gtk.Label; use Gtk.Label;
@@ -9,8 +29,7 @@ with Odd.Process; use Odd.Process;
 with Debugger; use Debugger;
 with Gtkada.Code_Editors; use Gtkada.Code_Editors;
 with Main_Debug_Window_Pkg; use Main_Debug_Window_Pkg;
-
-with Ada.Text_IO; use Ada.Text_IO;
+with Process_Proxies; use Process_Proxies;
 
 package body Odd.Dialogs.Callbacks is
 
@@ -44,6 +63,33 @@ package body Odd.Dialogs.Callbacks is
    begin
       Stack_Frame (Process.Debugger, Positive (Frame), True);
    end On_Backtrace_List_Select_Row;
+
+   ----------------------------------
+   -- On_Backtrace_Process_Stopped --
+   ----------------------------------
+
+   procedure On_Backtrace_Process_Stopped
+     (Object : access Gtk_Widget_Record'Class;
+      Params : Gtk.Arguments.Gtk_Args)
+   is
+      Bt       : Backtrace_Array (1 .. Max_Frame);
+      Len      : Natural;
+      Dialog   : Backtrace_Dialog_Access :=
+        Debugger_Process_Tab (Object).Window.Backtrace_Dialog;
+      Process  : Process_Proxy_Access;
+      Internal : Boolean;
+
+   begin
+      if Visible_Is_Set (Dialog) then
+         Process := Get_Process (Debugger_Process_Tab (Object).Debugger);
+         Internal := Is_Internal_Command (Process);
+         Set_Internal_Command (Process, True);
+         Backtrace (Debugger_Process_Tab (Object).Debugger, Bt, Len);
+         Set_Internal_Command (Process, Internal);
+         Update (Dialog, Bt (1 .. Len));
+         Free  (Bt (1 .. Len));
+      end if;
+   end On_Backtrace_Process_Stopped;
 
    -----------------------------
    -- On_Task_List_Select_Row --
@@ -130,8 +176,6 @@ package body Odd.Dialogs.Callbacks is
       Dialog      : Question_Dialog_Access :=
         Question_Dialog_Access (Get_Toplevel (Object));
    begin
-      Put_Line ("Selected row was " & Row'Img);
-      Put_Line ("Contents is " & Get_Text (Dialog.List, Row, 0));
       Send (Dialog.Debugger,
             Get_Text (Dialog.List, Row, 0),
             Display => True,
