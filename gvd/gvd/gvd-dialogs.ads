d814d13a0d8ab41d6beb5a36e62263f97a4b6193
Arnaud Charlet <charlet@adacore.com>
2000-06-19 07:58:54 +0000

    Add header.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@67065 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/gvd-dialogs.ads
+++ gvd/gvd/gvd-dialogs.ads
@@ -1,3 +1,23 @@
+-----------------------------------------------------------------------
+--                 Odd - The Other Display Debugger                  --
+--                                                                   --
+--                         Copyright (C) 2000                        --
+--                 Emmanuel Briot and Arnaud Charlet                 --
+--                                                                   --
+-- Odd is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
 with Gtk.Dialog; use Gtk.Dialog;
 with Gtk.Box; use Gtk.Box;
 with Gtk.Scrolled_Window; use Gtk.Scrolled_Window;
