4d30405c9b571a61021d9b1248c78847f26729f2
Emmanuel Briot <briot@adacore.com>
2000-06-21 13:21:26 +0000

    (Looking_At): Fix highlighting of constant characters. The string  "Array ('A', 'B')" was incorrectly highlighted for the first  character constant
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@67133 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/common/language-ada.adb
+++ gvd/common/language-ada.adb
@@ -195,6 +195,7 @@ package body Language.Debugger.Ada is
            and then Buffer (Next_Char) /= ASCII.HT
            and then Buffer (Next_Char) /= '"'
            and then Buffer (Next_Char) /= '-'   --  for comments
+           and then Buffer (Next_Char) /= '''   --  constant character
          loop
             Next_Char := Next_Char + 1;
          end loop;
