4368ee9fcd305fed92b0d492e18302e1ba55bb83
Emmanuel Briot <briot@adacore.com>
2000-06-22 09:21:32 +0000

    (Initialize): Wait for an extra prompt after setting the right format,  since otherwise some output is displayed in the command window when it  shouldn't
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@67151 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/debugger-gdb.adb
+++ gvd/gvd/debugger-gdb.adb
@@ -384,6 +384,10 @@ package body Debugger.Gdb is
       Send (Debugger, "set height 0");
       Send (Debugger, "set annotate 1");
 
+      --  We are missing one prompt, because we did not wait for hit after
+      --  calling "set prompt"
+      Wait_Prompt (Debugger);
+
       --  Connect to the remote target if needed.
 
       Pop_Internal_Command_Status (Get_Process (Debugger));
@@ -411,6 +415,8 @@ package body Debugger.Gdb is
 
          Send (Debugger, "list");
          Send (Debugger, "info line");
+
+         --  Make sure everything is hidden
          Pop_Internal_Command_Status (Get_Process (Debugger));
       end if;
    end Initialize;
