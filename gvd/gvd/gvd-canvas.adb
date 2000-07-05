5fecc7b987ab7f1861c0276cd2f5cbb505a4f6f6
Emmanuel Briot <briot@adacore.com>
2000-07-05 08:11:36 +0000

    New package
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@67263 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ gvd/gvd/gvd-canvas.adb
@@ -0,0 +1,61 @@
+-----------------------------------------------------------------------
+--                 Odd - The Other Display Debugger                  --
+--                                                                   --
+--                         Copyright (C) 2000                        --
+--                 Emmanuel Briot and Arnaud Charlet                 --
+--                                                                   --
+-- Odd is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Gtkada.Canvas;  use Gtkada.Canvas;
+
+package body Odd.Canvas is
+
+   ------------------------
+   -- Get_Detect_Aliases --
+   ------------------------
+
+   function Get_Detect_Aliases (Canvas : access Odd_Canvas_Record'Class)
+                               return Boolean
+   is
+   begin
+      return Canvas.Detect_Aliases;
+   end Get_Detect_Aliases;
+
+   ------------------------
+   -- Set_Detect_Aliases --
+   ------------------------
+
+   procedure Set_Detect_Aliases
+     (Canvas   : access Odd_Canvas_Record'Class;
+      Activate : Boolean)
+   is
+   begin
+      --  ??? We should modify the items displayed so as to remove currently
+      --  detected aliases. This is part of the whole aliases detection
+      --  implementation.
+      Canvas.Detect_Aliases := Activate;
+   end Set_Detect_Aliases;
+
+   -------------
+   -- Gtk_New --
+   -------------
+
+   procedure Gtk_New (Canvas : out Odd_Canvas) is
+   begin
+      Canvas := new Odd_Canvas_Record;
+      Initialize (Canvas);
+   end Gtk_New;
+
+end Odd.Canvas;
---
+++ gvd/gvd/gvd-canvas.ads
@@ -0,0 +1,60 @@
+-----------------------------------------------------------------------
+--                 Odd - The Other Display Debugger                  --
+--                                                                   --
+--                         Copyright (C) 2000                        --
+--                 Emmanuel Briot and Arnaud Charlet                 --
+--                                                                   --
+-- Odd is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Gtkada.Canvas;
+
+package Odd.Canvas is
+
+   ---------------
+   -- Constants --
+   ---------------
+   --  ??? These will eventually be moved to a preferences package.
+
+   Default_Detect_Aliases : constant Boolean := True;
+   --  If True, do not create new items when a matching item is already
+   --  present in the canvas.
+
+   ----------------
+   -- Odd_Canvas --
+   ----------------
+
+   type Odd_Canvas_Record is new Gtkada.Canvas.Interactive_Canvas_Record
+     with private;
+   type Odd_Canvas is access all Odd_Canvas_Record'Class;
+
+   procedure Gtk_New (Canvas : out Odd_Canvas);
+   --  Create a new canvas.
+
+   function Get_Detect_Aliases (Canvas : access Odd_Canvas_Record'Class)
+                               return Boolean;
+   --  Return True if aliases detection has been activated.
+
+   procedure Set_Detect_Aliases
+     (Canvas   : access Odd_Canvas_Record'Class;
+      Activate : Boolean);
+   --  Change the status of aliases detection in the canvas
+
+private
+
+   type Odd_Canvas_Record is new Gtkada.Canvas.Interactive_Canvas_Record with
+      record
+         Detect_Aliases : Boolean := Default_Detect_Aliases;
+      end record;
+end Odd.Canvas;
