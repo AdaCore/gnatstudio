02c54e8fe36d22d02f3166bf0d87ef8efa0ffd04
Arnaud Charlet <charlet@adacore.com>
2000-07-25 10:09:37 +0000

    First usable version of this package. The log are written in  Main_Debug_Window.Log_File.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@67430 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/gvd-trace.adb
+++ gvd/gvd/gvd-trace.adb
@@ -18,30 +18,46 @@
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
-with GNAT.IO; use GNAT.IO;
+with GNAT.OS_Lib; use GNAT.OS_Lib;
+with Main_Debug_Window_Pkg; use Main_Debug_Window_Pkg;
+with System;
+with Unchecked_Conversion;
 
 package body Odd.Trace is
 
-   procedure Output_String (Str : String);
+   Input_String  : aliased constant String := "-> """;
+   Output_String : aliased constant String := "<- """;
+   Quote_EOL     : aliased constant String := '"' & ASCII.LF;
+   Quote_SOL     : aliased constant String := "   """;
+   Verbose_EOL   : aliased constant String := "\n";
+   Verbose_HT    : aliased constant String := "\t";
 
-   procedure Output_String (Str : String) is
+   function To_Main_Window is new
+     Unchecked_Conversion (System.Address, Main_Debug_Window_Access);
+
+   procedure Output_Message (File : File_Descriptor; Str : String);
+   --  Write on File the string Str, by replacing ASCII.LF by "\n" and
+   --  ASCII.HT by "\t", and putting lines in quotes.
+
+   procedure Output_Message (File : File_Descriptor; Str : String) is
+      N : Integer;
    begin
       for J in Str'Range loop
          case Str (J) is
             when ASCII.LF =>
-               Put ("\n");
+               N := Write (File, Verbose_EOL'Address, Verbose_EOL'Length);
 
                if J < Str'Last then
-                  Put_Line ("""");
-                  Put ("   """);
+                  N := Write (File, Quote_EOL'Address, Quote_EOL'Length);
+                  N := Write (File, Quote_SOL'Address, Quote_SOL'Length);
                end if;
             when ASCII.HT =>
-               Put ("\t");
+               N := Write (File, Verbose_HT'Address, Verbose_HT'Length);
             when others =>
-               Put (Str (J));
+               N := Write (File, Str (J)'Address, 1);
          end case;
       end loop;
-   end Output_String;
+   end Output_Message;
 
    ------------------
    -- Input_Filter --
@@ -50,11 +66,14 @@ package body Odd.Trace is
    procedure Input_Filter
      (Descriptor : Process_Descriptor;
       Str        : String;
-      User_Data  : System.Address := System.Null_Address) is
+      User_Data  : System.Address := System.Null_Address)
+   is
+      Window : constant Main_Debug_Window_Access := To_Main_Window (User_Data);
+      N      : Integer;
    begin
-      Put ("-> """);
-      Output_String (Str);
-      Put_Line ("""");
+      N := Write (Window.Log_File, Input_String'Address, Input_String'Length);
+      Output_Message (Window.Log_File, Str);
+      N := Write (Window.Log_File, Quote_EOL'Address, Quote_EOL'Length);
    end Input_Filter;
 
    -------------------
@@ -64,11 +83,15 @@ package body Odd.Trace is
    procedure Output_Filter
      (Descriptor : Process_Descriptor;
       Str        : String;
-      User_Data  : System.Address := System.Null_Address) is
+      User_Data  : System.Address := System.Null_Address)
+   is
+      Window : constant Main_Debug_Window_Access := To_Main_Window (User_Data);
+      N      : Integer;
    begin
-      Put ("<- """);
-      Output_String (Str);
-      Put_Line ("""");
+      N := Write
+        (Window.Log_File, Output_String'Address, Output_String'Length);
+      Output_Message (Window.Log_File, Str);
+      N := Write (Window.Log_File, Quote_EOL'Address, Quote_EOL'Length);
    end Output_Filter;
 
 end Odd.Trace;
