8db30e851d4c0e38c698e281d70495d0fec5a1b0
Emmanuel Briot <briot@adacore.com>
2000-08-30 12:28:50 +0000

    (List_Callback): New package
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@67587 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/callbacks_odd.ads
+++ gvd/gvd/callbacks_odd.ads
@@ -23,6 +23,7 @@ with Gtk.Menu_Item; use Gtk.Menu_Item;
 with Gtk.Check_Menu_Item; use Gtk.Check_Menu_Item;
 with Gtk.Button; use Gtk.Button;
 with Gtk.Radio_Button; use Gtk.Radio_Button;
+with Gtk.List; use Gtk.List;
 
 package Callbacks_Odd is
 
@@ -37,5 +38,8 @@ package Callbacks_Odd is
 
    package Radio_Button_Callback is new
      Gtk.Handlers.Callback (Gtk_Radio_Button_Record);
+
+   package List_Callback is new
+     Gtk.Handlers.Callback (Gtk_List_Record);
 
 end Callbacks_Odd;
