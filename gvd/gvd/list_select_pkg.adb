8ff3af21b3adaf1a2663ed27b4c9f02a17c3cb5e
Arnaud Charlet <charlet@adacore.com>
2000-09-08 10:57:45 +0000

    Add GVD header.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@67722 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/list_select_pkg.adb
+++ gvd/gvd/list_select_pkg.adb
@@ -1,3 +1,23 @@
+-----------------------------------------------------------------------
+--                 Odd - The Other Display Debugger                  --
+--                                                                   --
+--                         Copyright (C) 2000                        --
+--                 Emmanuel Briot and Arnaud Charlet                 --
+--                                                                   --
+-- Odd is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
 with Gtk;             use Gtk;
 with Gtk.List;        use Gtk.List;
 with Gtk.List_Item;   use Gtk.List_Item;
