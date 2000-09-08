72752a49ac8a9eb568ab38d7ed2c2e4d9d537d40
Emmanuel Briot <briot@adacore.com>
2000-09-08 08:26:57 +0000

    Add a callback for "delete_event" on the breakpoint dialog. Part of the fix  for a bug reported by Nicolas (closing external dialogs with the title  bar button and opening them again generates a SE)
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@67705 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/.gate/odd/breakpoints_pkg-callbacks.adb
+++ gvd/gvd/.gate/odd/breakpoints_pkg-callbacks.adb
@@ -12,6 +12,19 @@ package body Breakpoints_Pkg.Callbacks is
 
    use Gtk.Arguments;
 
+   ---------------------------------
+   -- On_Breakpoints_Delete_Event --
+   ---------------------------------
+
+   function On_Breakpoints_Delete_Event
+     (Object : access Gtk_Widget_Record'Class;
+      Params : Gtk.Arguments.Gtk_Args) return Boolean
+   is
+      Arg1 : Gdk_Event := To_Event (Params, 1);
+   begin
+      return False;
+   end On_Breakpoints_Delete_Event;
+
    ------------------------------
    -- On_Notebook1_Switch_Page --
    ------------------------------
--- gvd/gvd/.gate/odd/breakpoints_pkg-callbacks.ads
+++ gvd/gvd/.gate/odd/breakpoints_pkg-callbacks.ads
@@ -2,6 +2,10 @@ with Gtk.Arguments;
 with Gtk.Widget; use Gtk.Widget;
 
 package Breakpoints_Pkg.Callbacks is
+   function On_Breakpoints_Delete_Event
+     (Object : access Gtk_Widget_Record'Class;
+      Params : Gtk.Arguments.Gtk_Args) return Boolean;
+
    procedure On_Notebook1_Switch_Page
      (Object : access Gtk_Widget_Record'Class;
       Params : Gtk.Arguments.Gtk_Args);
