e9dcf3c0acb7914e80d9f69c376def6b8c2df8e5
Emmanuel Briot <briot@adacore.com>
2000-09-08 15:42:42 +0000

    New package that implements tooltips
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@67727 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ gvd/gvd/gvd-tooltips.adb
@@ -0,0 +1,245 @@
+with Glib;            use Glib;
+with Gtk.Widget;      use Gtk.Widget;
+with Gdk.Pixmap;      use Gdk.Pixmap;
+with Gdk.Window;      use Gdk.Window;
+with Gtk.Main;        use Gtk.Main;
+with Gtk.Handlers;    use Gtk.Handlers;
+with Gdk.Types;       use Gdk.Types;
+with Gdk.Event;       use Gdk.Event;
+with Gdk.Visual;      use Gdk.Visual;
+with Gdk.Window_Attr; use Gdk.Window_Attr;
+with Unchecked_Deallocation;
+
+package body Odd.Tooltips is
+
+   --------------------
+   -- Local packages --
+   --------------------
+
+   package Odd_Tooltips_Timeout is new Timeout (Tooltips);
+
+   ---------------------
+   -- Local functions --
+   ---------------------
+
+   function Display_Tooltip (Tooltip : in Tooltips) return Boolean;
+   --  Call the drawing function, then create a window which contains
+   --  the pixmap, and display it.
+
+   procedure Set_Tooltip (Tooltip : Tooltips);
+   --  Begin timeout countdown.
+
+   procedure Remove_Tooltip (Tooltip : Tooltips);
+   --  Cancel timeout countdown.
+
+   procedure Free_User_Type is new Unchecked_Deallocation
+     (User_Type, User_Type_Access);
+   --  Free memory used by user data.
+
+   procedure Free_Tooltips is new Unchecked_Deallocation
+     (Tooltips_Record, Tooltips);
+   --  Free memory used by tooltip.
+
+   --------------------
+   -- Mouse_Moved_Cb --
+   --------------------
+
+   procedure Mouse_Moved_Cb (Widget  : access Widget_Type'Class;
+                             Event   : Gdk.Event.Gdk_Event;
+                             Tooltip : Tooltips)
+   is
+   begin
+      --  Handler_Block (Widget, Data.Handler_Id);
+      --  ??? Would this be useful ?
+
+      Set_Tooltip (Tooltip);
+
+      --  Handler_Unblock (Widget, Data.Handler_Id);
+      --  ??? Is this useful ?
+   end Mouse_Moved_Cb;
+
+   --------------------
+   -- Mouse_Enter_Cb --
+   --------------------
+
+   procedure Mouse_Enter_Cb (Widget  : access Widget_Type'Class;
+                             Event   : Gdk.Event.Gdk_Event;
+                             Tooltip : Tooltips)
+   is
+   begin
+      Set_Tooltip (Tooltip);
+   end Mouse_Enter_Cb;
+
+   --------------------
+   -- Mouse_Leave_Cb --
+   --------------------
+
+   procedure Mouse_Leave_Cb (Widget  : access Widget_Type'Class;
+                             Event   : Gdk.Event.Gdk_Event;
+                             Tooltip : Tooltips)
+   is
+   begin
+      Remove_Tooltip (Tooltip);
+   end Mouse_Leave_Cb;
+
+   -----------------
+   -- Set_Timeout --
+   -----------------
+
+   procedure Set_Timeout (Tooltip : in out Tooltips;
+                          T       : in Glib.Guint32) is
+   begin
+      Tooltip.Timeout := T;
+      --  Reset the tooltip
+      Set_Tooltip (Tooltip);
+   end Set_Timeout;
+
+   -----------------
+   -- New_Tooltip --
+   -----------------
+
+   procedure New_Tooltip (Widget  : access Widget_Type'Class;
+                          Data    : in User_Type;
+                          Tooltip : out Tooltips)
+   is
+      use Gdk;
+   begin
+      Add_Events (Widget, Pointer_Motion_Mask or Enter_Notify_Mask);
+      Tooltip := new Tooltips_Record'
+        (Timeout        => Default_Timeout,
+         Data           => new User_Type' (Data),
+         Display_Window => Null_Window,
+         Parent_Window  => Get_Window (Widget),
+         Handler_Id     => 0,
+         Active         => False,
+         Widget         => Widget_Type_Access (Widget));
+      Tooltip_Handler.Connect
+        (Widget, "motion_notify_event",
+         Tooltip_Handler.To_Marshaller (Mouse_Moved_Cb'Access),
+         User_Data => Tooltip);
+      Tooltip_Handler.Connect
+        (Widget, "enter_notify_event",
+         Tooltip_Handler.To_Marshaller (Mouse_Enter_Cb'Access),
+         User_Data => Tooltip);
+      Tooltip_Handler.Connect
+        (Widget, "leave_notify_event",
+         Tooltip_Handler.To_Marshaller (Mouse_Leave_Cb'Access),
+         User_Data => Tooltip);
+   end New_Tooltip;
+
+   ---------------------
+   -- Display_Tooltip --
+   ---------------------
+
+   function Display_Tooltip (Tooltip : in Tooltips) return Boolean
+   is
+      Pixmap      : Gdk_Pixmap;
+      Visual      : Gdk_Visual;
+      Window_Attr : Gdk_Window_Attr;
+      Mask        : Gdk_Modifier_Type;
+      X, Y        : Gint;
+      Window      : Gdk_Window;
+      Width, Height : Gint;
+      use Gdk;
+   begin
+      if not Tooltip.Active then
+         return False;
+      end if;
+
+      Draw_Tooltip (Tooltip.Widget,
+                    Tooltip.Data.all,
+                    Pixmap,
+                    Width,
+                    Height);
+
+      if Width /= 0 and then Height /= 0 then
+
+         Get_Best (Visual);
+
+         Gdk_New
+           (Window_Attr,
+            Height => Height,
+            Width => Width,
+            Title => "",
+            Window_Type => Window_Temp,
+            Visual => Visual);
+
+         Gdk.Window.Gdk_New
+           (Tooltip.Display_Window,
+            Tooltip.all.Parent_Window,
+            Window_Attr,
+            Wa_Cursor and Wa_Wmclass
+            and Wa_Colormap and Wa_Visual);
+
+         Set_Back_Pixmap (Tooltip.Display_Window, Pixmap, 0);
+         Get_Pointer (Tooltip.Parent_Window, X, Y, Mask, Window);
+         Move (Tooltip.Display_Window, X + 10, Y + 10);
+         Gdk.Pixmap.Unref (Pixmap);
+         Show (Tooltip.Display_Window);
+      end if;
+      return False;
+   end Display_Tooltip;
+
+   -----------------
+   -- Set_Tooltip --
+   -----------------
+
+   procedure Set_Tooltip (Tooltip : Tooltips) is
+   begin
+      if Tooltip.Active = True then
+         Remove_Tooltip (Tooltip);
+      end if;
+      Tooltip.Handler_Id := Odd_Tooltips_Timeout.Add
+        (Tooltip.Timeout,
+         Display_Tooltip'Access,
+         Tooltip);
+      Tooltip.Active := True;
+   end Set_Tooltip;
+
+   --------------------
+   -- Remove_Tooltip --
+   --------------------
+
+   procedure Remove_Tooltip (Tooltip : Tooltips) is
+      use Gdk;
+   begin
+      if Tooltip.Active = True then
+         Timeout_Remove (Tooltip.Handler_Id);
+         if Tooltip.Display_Window /= Null_Window then
+            Destroy (Tooltip.Display_Window);
+         end if;
+         Tooltip.Active := False;
+      end if;
+   end Remove_Tooltip;
+
+   ---------------------
+   -- Destroy_Tooltip --
+   ---------------------
+
+   procedure Destroy_Tooltip (Tooltip : in out Tooltips) is
+   begin
+      Remove_Tooltip (Tooltip);
+      Free_User_Type (Tooltip.Data);
+      Free_Tooltips (Tooltip);
+   end Destroy_Tooltip;
+
+   --------------
+   -- Set_Data --
+   --------------
+
+   procedure Set_Data (Tooltip : in out Tooltips; Data : in User_Type) is
+   begin
+      Free_User_Type (Tooltip.Data);
+      Tooltip.Data := new User_Type' (Data);
+   end Set_Data;
+
+   --------------
+   -- Get_Data --
+   --------------
+
+   function Get_Data (Tooltip : in Tooltips) return User_Type is
+   begin
+      return Tooltip.Data.all;
+   end Get_Data;
+
+end Odd.Tooltips;
---
+++ gvd/gvd/gvd-tooltips.ads
@@ -0,0 +1,136 @@
+-----------------------------------------------------------------------
+--                 Odd - The Other Display Debugger                  --
+--                                                                   --
+--                         Copyright (C) 2000                        --
+--                 Emmanuel Briot and Arnaud Charlet                 --
+--                                                                   --
+-- Odd is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Glib;
+with Gtk.Widget; use Gtk.Widget;
+with Gdk.Pixmap;
+with Gdk.Window;
+with Gtk.Main;
+with Gtk.Handlers;
+with Gdk.Event;
+with Gdk.Window;
+
+--  This package provides tooltips-like functionality. It differs from
+--  the original Gtk.Tooltips package in that the drawing function is
+--  left to the end user.
+--  The drawing function is called after the timeout period, which means
+--  that dynamic tooltips can be implemented with this package.
+--
+--  The generic part enables the user to carry a user-defined object
+--  to the drawing function.
+--
+--  ??? Right now, both the user data and the widget on which the tooltips
+--  are set are carried to the drawind function.
+--  This was done because the widget is needed by the actual drawing
+--  routines in most cases, although it is not entirely useful, since a
+--  user could insert a pointer to that widget within his data type.
+--
+--  (For an example showing the use of that package, please see
+--  Odd.Source_Editors)
+
+
+generic
+   type User_Type (<>) is private;
+   type Widget_Type is new Gtk.Widget.Gtk_Widget_Record with private;
+   with procedure Draw_Tooltip (Widget : access Widget_Type'Class;
+                                Data   : in User_Type;
+                                Pixmap : out Gdk.Pixmap.Gdk_Pixmap;
+                                Width, Height : out Glib.Gint) is <>;
+package Odd.Tooltips is
+
+   Default_Timeout : constant Glib.Guint32 := 600;
+   --  The delay before a tooltip is displayed, in milliseconds)
+
+   type Tooltips_Record is private;
+
+   type Tooltips is access Tooltips_Record;
+
+   procedure Set_Timeout
+     (Tooltip : in out Tooltips;
+      T       : in Glib.Guint32);
+   --  Set a new delay for the tooltips.
+
+   procedure New_Tooltip
+     (Widget  : access Widget_Type'Class;
+      Data    : in User_Type;
+      Tooltip : out Tooltips);
+   --  Create tooltips information for the widget.
+   --  The widget must have a window.
+
+   procedure Set_Data
+     (Tooltip : in out Tooltips;
+      Data    : in User_Type);
+   --  Set the data associated with the tooltip.
+
+   function Get_Data (Tooltip : in Tooltips) return User_Type;
+   --  Return the data associated with the tooltip.
+
+   procedure Destroy_Tooltip (Tooltip : in out Tooltips);
+      --  Free memory allocated to the tooltip.
+
+private
+
+   type Widget_Type_Access is access all Widget_Type'Class;
+
+   type User_Type_Access is access User_Type;
+   package Tooltip_Handler is new Gtk.Handlers.User_Callback
+     (Widget_Type, Tooltips);
+
+   type Tooltips_Record is record
+      Timeout : Glib.Guint32 := Default_Timeout;
+      --  The delay before draw function is called.
+
+      Active : Boolean := False;
+      --  States whether tooltips should be displayed when drawing
+      --  is complete.
+
+      Parent_Window : Gdk.Window.Gdk_Window;
+      --  The window which contains the tooltip.
+
+      Display_Window : Gdk.Window.Gdk_Window;
+      --  The window in which the tooltip will be displayed.
+
+      Handler_Id : Gtk.Main.Timeout_Handler_Id;
+      --  Reference in case handler should be blocked.
+
+      Data : User_Type_Access;
+      --  User data.
+
+      Widget : Widget_Type_Access;
+      --  The widget on which the tooltip is set.
+   end record;
+
+
+   procedure Mouse_Moved_Cb
+     (Widget  : access Widget_Type'Class;
+      Event   : Gdk.Event.Gdk_Event;
+      Tooltip :  Tooltips);
+
+   procedure Mouse_Enter_Cb
+     (Widget  : access Widget_Type'Class;
+      Event   : Gdk.Event.Gdk_Event;
+      Tooltip :  Tooltips);
+
+   procedure Mouse_Leave_Cb
+     (Widget  : access Widget_Type'Class;
+      Event   : Gdk.Event.Gdk_Event;
+      Tooltip :  Tooltips);
+
+end Odd.Tooltips;
