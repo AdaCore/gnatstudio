6b8451b423e2e13d3195d6320e702da7bef4482a
Arnaud Charlet <charlet@adacore.com>
2000-09-18 13:28:39 +0000

    Update to version 0.6.1
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@67865 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/odd.ads
+++ gvd/gvd/odd.ads
@@ -24,7 +24,7 @@ package Odd is
    -- Constants --
    ---------------
 
-   Version : constant String := "0.6";
+   Version : constant String := "0.6.1";
    --  Version number of this software.
 
    Max_Frame : constant := 500;
