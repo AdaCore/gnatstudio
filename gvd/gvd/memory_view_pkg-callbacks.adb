8310dffceaf39eda0122db3f5d1b22b99e3ca465
Arnaud Charlet <charlet@adacore.com>
2000-09-26 09:12:14 +0000

    First implementation of the memory view. Not integrated yet. Checked in for Nicolas with minor fixes from Arno.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@67921 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ gvd/gvd/memory_view_pkg-callbacks.adb
@@ -0,0 +1,308 @@
+-----------------------------------------------------------------------
+--                 Odd - The Other Display Debugger                  --
+--                                                                   --
+--                         Copyright (C) 2000                        --
+--                 Emmanuel Briot and Arnaud Charlet                 --
+--                                                                   --
+-- Odd is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with System;      use System;
+with Glib;        use Glib;
+with Gdk.Event;   use Gdk.Event;
+
+with Gdk.Types;         use Gdk.Types;
+with Gdk.Types.Keysyms; use Gdk.Types.Keysyms;
+with Gtk.Arguments;     use Gtk.Arguments;
+with Gtk.Widget;        use Gtk.Widget;
+with Gtk.GEntry;        use Gtk.GEntry;
+with Gtk.Handlers;      use Gtk.Handlers;
+
+with Odd.Memory_View;   use Odd.Memory_View;
+with Odd.Types;         use Odd.Types;
+
+package body Memory_View_Pkg.Callbacks is
+
+   use Gtk.Arguments;
+
+   ----------------------------------
+   -- On_Memory_View_Size_Allocate --
+   ----------------------------------
+
+   procedure On_Memory_View_Size_Allocate
+     (Object : access Gtk_Window_Record'Class;
+      Params : Gtk.Arguments.Gtk_Args)
+   is
+      View : Odd_Memory_View := Odd_Memory_View (Get_Toplevel (Object));
+   begin
+      Update_Display (View);
+   end On_Memory_View_Size_Allocate;
+
+   -------------------------------
+   -- On_Vbox20_Key_Press_Event --
+   -------------------------------
+
+   function On_Vbox20_Key_Press_Event
+     (Object : access Gtk_Widget_Record'Class;
+      Params : Gtk.Arguments.Gtk_Args) return Boolean
+   is
+   begin
+      return False;
+   end On_Vbox20_Key_Press_Event;
+
+   -------------------------------
+   -- On_Address_Entry_Activate --
+   -------------------------------
+
+   procedure On_Address_Entry_Activate
+     (Object : access Gtk_Entry_Record'Class)
+   is
+      View : Odd_Memory_View := Odd_Memory_View (Get_Toplevel (Object));
+   begin
+      Display_Memory (View, Get_Text (View.Address_Entry));
+   end On_Address_Entry_Activate;
+
+   --------------------------
+   -- On_Value_Insert_Text --
+   --------------------------
+
+   procedure On_Value_Insert_Text
+     (Object : access Gtk_Entry_Record'Class;
+      Params : Gtk.Arguments.Gtk_Args)
+   is
+      Arg1 : String := To_String (Params, 1);
+      View : Odd_Memory_View := Odd_Memory_View (Get_Toplevel (Object));
+   begin
+      Insert (View, Arg1);
+   end On_Value_Insert_Text;
+
+   -----------------------------
+   -- On_Address_View_Clicked --
+   -----------------------------
+
+   procedure On_Address_View_Clicked
+     (Object : access Gtk_Button_Record'Class)
+   is
+      View : constant Odd_Memory_View :=
+        Odd_Memory_View (Get_Toplevel (Object));
+   begin
+      Display_Memory (View, Get_Text (View.Address_Entry));
+   end On_Address_View_Clicked;
+
+   -----------------------------
+   -- On_Value_Update_Clicked --
+   -----------------------------
+
+   procedure On_Value_Update_Clicked
+     (Object : access Gtk_Button_Record'Class)
+   is
+      View : constant Odd_Memory_View :=
+        Odd_Memory_View (Get_Toplevel (Object));
+   begin
+      Display_Memory (View, Get_Text (View.Address_Entry));
+   end On_Value_Update_Clicked;
+
+   ---------------------
+   -- On_Pgup_Clicked --
+   ---------------------
+
+   procedure On_Pgup_Clicked
+     (Object : access Gtk_Button_Record'Class)
+   is
+      View : constant Odd_Memory_View :=
+        Odd_Memory_View (Get_Toplevel (Object));
+   begin
+      Page_Up (View);
+   end On_Pgup_Clicked;
+
+   ---------------------
+   -- On_Pgdn_Clicked --
+   ---------------------
+
+   procedure On_Pgdn_Clicked
+     (Object : access Gtk_Button_Record'Class)
+   is
+      View : constant Odd_Memory_View :=
+        Odd_Memory_View (Get_Toplevel (Object));
+   begin
+      Page_Down (View);
+   end On_Pgdn_Clicked;
+
+   ---------------------------------
+   -- On_Page_Size_Entry_Activate --
+   ---------------------------------
+
+   procedure On_Page_Size_Entry_Activate
+     (Object : access Gtk_Entry_Record'Class)
+   is
+      View : Odd_Memory_View := Odd_Memory_View (Get_Toplevel (Object));
+   begin
+      View.Number_Of_Bytes := Integer'Value (Get_Text (Object));
+      Display_Memory (View, View.Starting_Address);
+   end On_Page_Size_Entry_Activate;
+
+   ---------------------------
+   -- On_Size_Entry_Changed --
+   ---------------------------
+
+   procedure On_Size_Entry_Changed
+     (Object : access Gtk_Entry_Record'Class)
+   is
+      View : Odd_Memory_View := Odd_Memory_View (Get_Toplevel (Object));
+   begin
+      Update_Display (View);
+   end On_Size_Entry_Changed;
+
+   ---------------------------
+   -- On_Data_Entry_Changed --
+   ---------------------------
+
+   procedure On_Data_Entry_Changed
+     (Object : access Gtk_Entry_Record'Class)
+   is
+      View : Odd_Memory_View := Odd_Memory_View (Get_Toplevel (Object));
+   begin
+      Update_Display (View);
+   end On_Data_Entry_Changed;
+
+   -----------------------------
+   -- On_View_Key_Press_Event --
+   -----------------------------
+
+   function On_View_Key_Press_Event
+     (Object : access Gtk_Widget_Record'Class;
+      Params : Gtk.Arguments.Gtk_Args) return Boolean
+   is
+      View : Odd_Memory_View := Odd_Memory_View (Get_Toplevel (Object));
+      Arg1 : Gdk_Event := To_Event (Params, 1);
+   begin
+      case Get_Key_Val (Arg1) is
+         when GDK_Right =>
+            Move_Cursor (View, Right);
+         when GDK_Left =>
+            Move_Cursor (View, Left);
+         when GDK_Up | GDK_Down =>
+            Move_Cursor (View, Up);
+         when others =>
+            null;
+      end case;
+
+      return False;
+   end On_View_Key_Press_Event;
+
+   -------------------------
+   -- On_View_Move_Cursor --
+   -------------------------
+
+   procedure On_View_Move_Cursor
+     (Object : access Gtk_Text_Record'Class;
+      Params : Gtk.Arguments.Gtk_Args)
+   is
+   begin
+      null;
+   end On_View_Move_Cursor;
+
+   ----------------------------------
+   -- On_View_Button_Release_Event --
+   ----------------------------------
+
+   function On_View_Button_Release_Event
+     (Object : access Gtk_Widget_Record'Class;
+      Params : Gtk.Arguments.Gtk_Args) return Boolean
+   is
+      View : Odd_Memory_View := Odd_Memory_View (Get_Toplevel (Object));
+   begin
+      View.Cursor_Index :=
+        Position_To_Index (View, Get_Position (View.View));
+      Update_Display (View);
+      Set_Position (View.View, View.Cursor_Position);
+      return False;
+   end On_View_Button_Release_Event;
+
+   --------------------------------
+   -- On_View_Button_Press_Event --
+   --------------------------------
+
+   function On_View_Button_Press_Event
+     (Object : access Gtk_Widget_Record'Class;
+      Params : Gtk.Arguments.Gtk_Args) return Boolean
+   is
+   begin
+      return False;
+   end On_View_Button_Press_Event;
+
+   -------------------------
+   -- On_View_Insert_Text --
+   -------------------------
+
+   procedure On_View_Insert_Text
+     (Object : access Gtk_Text_Record'Class;
+      Params : Gtk.Arguments.Gtk_Args)
+   is
+      Arg1 : String := To_String (Params, 1);
+      View : Odd_Memory_View := Odd_Memory_View (Get_Toplevel (Object));
+   begin
+      Insert (View, Arg1 (1 .. 1));
+      Emit_Stop_By_Name (View.View, "insert_text");
+   end On_View_Insert_Text;
+
+   ----------------------
+   -- On_Reset_Clicked --
+   ----------------------
+
+   procedure On_Reset_Clicked
+     (Object : access Gtk_Button_Record'Class)
+   is
+      View : Odd_Memory_View := Odd_Memory_View (Get_Toplevel (Object));
+   begin
+      Free (View.Flags);
+      View.Flags := new String' (View.Values.all);
+      Update_Display (View);
+   end On_Reset_Clicked;
+
+   -----------------------
+   -- On_Submit_Clicked --
+   -----------------------
+
+   procedure On_Submit_Clicked
+     (Object : access Gtk_Button_Record'Class)
+   is
+      View : Odd_Memory_View := Odd_Memory_View (Get_Toplevel (Object));
+   begin
+      Apply_Changes (View);
+   end On_Submit_Clicked;
+
+   -----------------------
+   -- On_Cancel_Clicked --
+   -----------------------
+
+   procedure On_Cancel_Clicked
+     (Object : access Gtk_Button_Record'Class)
+   is
+   begin
+      Hide (Get_Toplevel (Object));
+   end On_Cancel_Clicked;
+
+   ---------------------
+   -- On_Help_Clicked --
+   ---------------------
+
+   procedure On_Help_Clicked
+     (Object : access Gtk_Button_Record'Class)
+   is
+   begin
+      null;
+   end On_Help_Clicked;
+
+end Memory_View_Pkg.Callbacks;
---
+++ gvd/gvd/memory_view_pkg.adb
@@ -0,0 +1,305 @@
+-----------------------------------------------------------------------
+--                 Odd - The Other Display Debugger                  --
+--                                                                   --
+--                         Copyright (C) 2000                        --
+--                 Emmanuel Briot and Arnaud Charlet                 --
+--                                                                   --
+-- Odd is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Gtk; use Gtk;
+with Gtk.Widget;      use Gtk.Widget;
+with Gtk.Enums;       use Gtk.Enums;
+with Gtkada.Handlers; use Gtkada.Handlers;
+with Callbacks_Odd; use Callbacks_Odd;
+with Odd_Intl; use Odd_Intl;
+with Memory_View_Pkg.Callbacks; use Memory_View_Pkg.Callbacks;
+
+package body Memory_View_Pkg is
+
+procedure Gtk_New (Memory_View : out Memory_View_Access) is
+begin
+   Memory_View := new Memory_View_Record;
+   Memory_View_Pkg.Initialize (Memory_View);
+end Gtk_New;
+
+procedure Initialize (Memory_View : access Memory_View_Record'Class) is
+   Size_Items : String_List.Glist;
+   Format_Items : String_List.Glist;
+
+begin
+   Gtk.Window.Initialize (Memory_View, Window_Toplevel);
+   Window_Callback.Connect
+     (Memory_View, "size_allocate", On_Memory_View_Size_Allocate'Access);
+   Set_Title (Memory_View, -"Memory View");
+   Set_Policy (Memory_View, False, True, False);
+   Set_Position (Memory_View, Win_Pos_None);
+   Set_Modal (Memory_View, False);
+
+   Gtk_New_Vbox (Memory_View.Vbox20, False, 0);
+   Return_Callback.Connect
+     (Memory_View.Vbox20, "key_press_event", On_Vbox20_Key_Press_Event'Access);
+   Add (Memory_View, Memory_View.Vbox20);
+
+   Gtk_New (Memory_View.Frame, -"(no executable)");
+   Pack_Start (Memory_View.Vbox20, Memory_View.Frame, False, False, 0);
+   Set_Shadow_Type (Memory_View.Frame, Shadow_Etched_In);
+
+   Gtk_New_Hbox (Memory_View.Hbox8, False, 0);
+   Add (Memory_View.Frame, Memory_View.Hbox8);
+
+   Gtk_New_Hbox (Memory_View.Hbox11, False, 0);
+   Pack_Start (Memory_View.Hbox8, Memory_View.Hbox11, True, True, 0);
+
+   Gtk_New_Vbox (Memory_View.Vbox23, True, 0);
+   Pack_Start (Memory_View.Hbox11, Memory_View.Vbox23, False, False, 7);
+
+   Gtk_New (Memory_View.Label95, -("Address"));
+   Pack_Start (Memory_View.Vbox23, Memory_View.Label95, False, False, 0);
+   Set_Alignment (Memory_View.Label95, 0.5, 0.5);
+   Set_Padding (Memory_View.Label95, 0, 0);
+   Set_Justify (Memory_View.Label95, Justify_Center);
+   Set_Line_Wrap (Memory_View.Label95, False);
+
+   Gtk_New (Memory_View.Label96, -("Value"));
+   Set_Sensitive (Memory_View.Label96, False);
+   Pack_Start (Memory_View.Vbox23, Memory_View.Label96, False, False, 0);
+   Set_Alignment (Memory_View.Label96, 0.5, 0.5);
+   Set_Padding (Memory_View.Label96, 0, 0);
+   Set_Justify (Memory_View.Label96, Justify_Center);
+   Set_Line_Wrap (Memory_View.Label96, False);
+
+   Gtk_New_Vbox (Memory_View.Vbox24, True, 0);
+   Pack_Start (Memory_View.Hbox11, Memory_View.Vbox24, True, True, 0);
+
+   Gtk_New (Memory_View.Address_Entry);
+   Pack_Start (Memory_View.Vbox24, Memory_View.Address_Entry, False, False, 0);
+   Entry_Callback.Connect
+     (Memory_View.Address_Entry, "activate",
+      Entry_Callback.To_Marshaller (On_Address_Entry_Activate'Access));
+   Set_Editable (Memory_View.Address_Entry, True);
+   Set_Max_Length (Memory_View.Address_Entry, 0);
+   Set_Text (Memory_View.Address_Entry, -"");
+   Set_Visibility (Memory_View.Address_Entry, True);
+
+   Gtk_New (Memory_View.Value_Entry);
+   Set_Sensitive (Memory_View.Value_Entry, False);
+   Pack_Start (Memory_View.Vbox24, Memory_View.Value_Entry, False, False, 0);
+   Entry_Callback.Connect
+     (Memory_View.Value_Entry, "insert_text", On_Value_Insert_Text'Access);
+   Set_Editable (Memory_View.Value_Entry, True);
+   Set_Max_Length (Memory_View.Value_Entry, 0);
+   Set_Text (Memory_View.Value_Entry, -"");
+   Set_Visibility (Memory_View.Value_Entry, True);
+
+   Gtk_New (Memory_View.Vbuttonbox6);
+   Pack_Start (Memory_View.Hbox8, Memory_View.Vbuttonbox6, False, False, 0);
+   Set_Spacing (Memory_View.Vbuttonbox6, 10);
+   Set_Layout (Memory_View.Vbuttonbox6, Buttonbox_Spread);
+   Set_Child_Size (Memory_View.Vbuttonbox6, 85, 27);
+   Set_Child_Ipadding (Memory_View.Vbuttonbox6, 7, 0);
+
+   Gtk_New (Memory_View.Address_View, -"View");
+   Set_Flags (Memory_View.Address_View, Can_Default);
+   Button_Callback.Connect
+     (Memory_View.Address_View, "clicked",
+      Button_Callback.To_Marshaller (On_Address_View_Clicked'Access));
+   Add (Memory_View.Vbuttonbox6, Memory_View.Address_View);
+
+   Gtk_New (Memory_View.Value_Update, -"Update");
+   Set_Sensitive (Memory_View.Value_Update, False);
+   Set_Flags (Memory_View.Value_Update, Can_Default);
+   Button_Callback.Connect
+     (Memory_View.Value_Update, "clicked",
+      Button_Callback.To_Marshaller (On_Value_Update_Clicked'Access));
+   Add (Memory_View.Vbuttonbox6, Memory_View.Value_Update);
+
+   Gtk_New_Hbox (Memory_View.Hbox12, False, 0);
+   Pack_Start (Memory_View.Vbox20, Memory_View.Hbox12, False, False, 4);
+
+   Gtk_New (Memory_View.Vbuttonbox5);
+   Pack_Start (Memory_View.Hbox12, Memory_View.Vbuttonbox5, False, False, 0);
+   Set_Spacing (Memory_View.Vbuttonbox5, 10);
+   Set_Layout (Memory_View.Vbuttonbox5, Buttonbox_Spread);
+   Set_Child_Size (Memory_View.Vbuttonbox5, 85, 27);
+   Set_Child_Ipadding (Memory_View.Vbuttonbox5, 7, 0);
+
+   Gtk_New (Memory_View.Pgup);
+   Set_Flags (Memory_View.Pgup, Can_Default);
+   Pack_Start (Memory_View.Hbox12, Memory_View.Pgup, False, False, 0);
+   Button_Callback.Connect
+     (Memory_View.Pgup, "clicked",
+      Button_Callback.To_Marshaller (On_Pgup_Clicked'Access));
+
+   Memory_View.Pixmap1 := Create_Pixmap ("up_xpm", Memory_View);
+   Add (Memory_View.Pgup, Memory_View.Pixmap1);
+   Set_Alignment (Memory_View.Pixmap1, 0.5, 0.5);
+   Set_Padding (Memory_View.Pixmap1, 0, 0);
+
+   Gtk_New (Memory_View.Pgdn);
+   Set_Flags (Memory_View.Pgdn, Can_Default);
+   Pack_Start (Memory_View.Hbox12, Memory_View.Pgdn, False, False, 0);
+   Button_Callback.Connect
+     (Memory_View.Pgdn, "clicked",
+      Button_Callback.To_Marshaller (On_Pgdn_Clicked'Access));
+
+   Memory_View.Pixmap2 := Create_Pixmap ("down_xpm", Memory_View);
+   Add (Memory_View.Pgdn, Memory_View.Pixmap2);
+   Set_Alignment (Memory_View.Pixmap2, 0.5, 0.5);
+   Set_Padding (Memory_View.Pixmap2, 0, 0);
+
+   Gtk_New_Vseparator (Memory_View.Vseparator5);
+   Pack_Start (Memory_View.Hbox12, Memory_View.Vseparator5, True, True, 7);
+
+   Gtk_New (Memory_View.Label99, -("Page size :"));
+   Pack_Start (Memory_View.Hbox12, Memory_View.Label99, True, True, 1);
+   Set_Alignment (Memory_View.Label99, 0.5, 0.5);
+   Set_Padding (Memory_View.Label99, 0, 0);
+   Set_Justify (Memory_View.Label99, Justify_Center);
+   Set_Line_Wrap (Memory_View.Label99, False);
+
+   Gtk_New (Memory_View.Page_Size_Entry);
+   Pack_Start (Memory_View.Hbox12, Memory_View.Page_Size_Entry, False, False, 0);
+   Set_Usize (Memory_View.Page_Size_Entry, 50, -1);
+
+   Entry_Callback.Connect
+     (Memory_View.Page_Size_Entry, "activate",
+      Entry_Callback.To_Marshaller (On_Page_Size_Entry_Activate'Access));
+   Set_Editable (Memory_View.Page_Size_Entry, True);
+   Set_Max_Length (Memory_View.Page_Size_Entry, 0);
+   Set_Text (Memory_View.Page_Size_Entry, -"256");
+   Set_Visibility (Memory_View.Page_Size_Entry, True);
+
+   Gtk_New (Memory_View.Label100, -("bytes"));
+   Pack_Start (Memory_View.Hbox12, Memory_View.Label100, False, True, 0);
+   Set_Alignment (Memory_View.Label100, 0.5, 0.5);
+   Set_Padding (Memory_View.Label100, 0, 0);
+   Set_Justify (Memory_View.Label100, Justify_Center);
+   Set_Line_Wrap (Memory_View.Label100, False);
+
+   Gtk_New_Vseparator (Memory_View.Vseparator6);
+   Pack_Start (Memory_View.Hbox12, Memory_View.Vseparator6, True, True, 7);
+
+   Gtk_New (Memory_View.Label98, -("Unit size : "));
+   Pack_Start (Memory_View.Hbox12, Memory_View.Label98, True, True, 0);
+   Set_Alignment (Memory_View.Label98, 0.5, 0.5);
+   Set_Padding (Memory_View.Label98, 0, 0);
+   Set_Justify (Memory_View.Label98, Justify_Center);
+   Set_Line_Wrap (Memory_View.Label98, False);
+
+   Gtk_New (Memory_View.Size);
+   Pack_Start (Memory_View.Hbox12, Memory_View.Size, False, True, 0);
+   Set_Case_Sensitive (Memory_View.Size, False);
+   Set_Use_Arrows (Memory_View.Size, True);
+   Set_Use_Arrows_Always (Memory_View.Size, False);
+   String_List.Append (Size_Items, -"Byte");
+   String_List.Append (Size_Items, -"Halfword");
+   String_List.Append (Size_Items, -"Word");
+   Combo.Set_Popdown_Strings (Memory_View.Size, Size_Items);
+   Free_String_List (Size_Items);
+
+   Memory_View.Size_Entry := Get_Entry (Memory_View.Size);
+   Entry_Callback.Connect
+     (Memory_View.Size_Entry, "changed",
+      Entry_Callback.To_Marshaller (On_Size_Entry_Changed'Access));
+   Set_Editable (Memory_View.Size_Entry, False);
+   Set_Max_Length (Memory_View.Size_Entry, 0);
+   Set_Text (Memory_View.Size_Entry, -"Byte");
+   Set_Visibility (Memory_View.Size_Entry, True);
+
+   Gtk_New (Memory_View.Label97, -("Format : "));
+   Pack_Start (Memory_View.Hbox12, Memory_View.Label97, True, True, 4);
+   Set_Alignment (Memory_View.Label97, 0.5, 0.5);
+   Set_Padding (Memory_View.Label97, 0, 0);
+   Set_Justify (Memory_View.Label97, Justify_Center);
+   Set_Line_Wrap (Memory_View.Label97, False);
+
+   Gtk_New (Memory_View.Format);
+   Pack_Start (Memory_View.Hbox12, Memory_View.Format, False, True, 0);
+   Set_Case_Sensitive (Memory_View.Format, False);
+   Set_Use_Arrows (Memory_View.Format, True);
+   Set_Use_Arrows_Always (Memory_View.Format, False);
+   String_List.Append (Format_Items, -"Hex");
+   String_List.Append (Format_Items, -"Decimal");
+   String_List.Append (Format_Items, -"Octal");
+   String_List.Append (Format_Items, -"ASCII");
+   Combo.Set_Popdown_Strings (Memory_View.Format, Format_Items);
+   Free_String_List (Format_Items);
+
+   Memory_View.Data_Entry := Get_Entry (Memory_View.Format);
+   Entry_Callback.Connect
+     (Memory_View.Data_Entry, "changed",
+      Entry_Callback.To_Marshaller (On_Data_Entry_Changed'Access));
+   Set_Editable (Memory_View.Data_Entry, False);
+   Set_Max_Length (Memory_View.Data_Entry, 0);
+   Set_Text (Memory_View.Data_Entry, -"Hex");
+   Set_Visibility (Memory_View.Data_Entry, True);
+
+   Gtk_New (Memory_View.Scrolledwindow);
+   Pack_Start (Memory_View.Vbox20, Memory_View.Scrolledwindow, True, True, 0);
+   Set_Policy (Memory_View.Scrolledwindow, Policy_Automatic, Policy_Automatic);
+
+   Gtk_New (Memory_View.View);
+   Set_USize (Memory_View.View, 686, 320);
+   Return_Callback.Connect
+     (Memory_View.View, "key_press_event", On_View_Key_Press_Event'Access);
+   Text_Callback.Connect
+     (Memory_View.View, "move_cursor", On_View_Move_Cursor'Access);
+   Return_Callback.Connect
+     (Memory_View.View, "button_release_event", On_View_Button_Release_Event'Access);
+   Return_Callback.Connect
+     (Memory_View.View, "button_press_event", On_View_Button_Press_Event'Access);
+   Text_Callback.Connect
+     (Memory_View.View, "insert_text", On_View_Insert_Text'Access);
+   Add (Memory_View.Scrolledwindow, Memory_View.View);
+   Set_Editable (Memory_View.View, True);
+
+   Gtk_New (Memory_View.Hbuttonbox11);
+   Pack_Start (Memory_View.Vbox20, Memory_View.Hbuttonbox11, False, False, 0);
+   Set_Spacing (Memory_View.Hbuttonbox11, 30);
+   Set_Layout (Memory_View.Hbuttonbox11, Buttonbox_Spread);
+   Set_Child_Size (Memory_View.Hbuttonbox11, 85, 27);
+   Set_Child_Ipadding (Memory_View.Hbuttonbox11, 7, 0);
+
+   Gtk_New (Memory_View.Reset, -"Reset");
+   Set_Flags (Memory_View.Reset, Can_Default);
+   Button_Callback.Connect
+     (Memory_View.Reset, "clicked",
+      Button_Callback.To_Marshaller (On_Reset_Clicked'Access));
+   Add (Memory_View.Hbuttonbox11, Memory_View.Reset);
+
+   Gtk_New (Memory_View.Submit, -"Submit changes");
+   Set_Flags (Memory_View.Submit, Can_Default);
+   Button_Callback.Connect
+     (Memory_View.Submit, "clicked",
+      Button_Callback.To_Marshaller (On_Submit_Clicked'Access));
+   Add (Memory_View.Hbuttonbox11, Memory_View.Submit);
+
+   Gtk_New (Memory_View.Cancel, -"Cancel");
+   Set_Flags (Memory_View.Cancel, Can_Default);
+   Button_Callback.Connect
+     (Memory_View.Cancel, "clicked",
+      Button_Callback.To_Marshaller (On_Cancel_Clicked'Access));
+   Add (Memory_View.Hbuttonbox11, Memory_View.Cancel);
+
+   Gtk_New (Memory_View.Help, -"Help");
+   Set_Flags (Memory_View.Help, Can_Default);
+   Button_Callback.Connect
+     (Memory_View.Help, "clicked",
+      Button_Callback.To_Marshaller (On_Help_Clicked'Access));
+   Add (Memory_View.Hbuttonbox11, Memory_View.Help);
+
+end Initialize;
+
+end Memory_View_Pkg;
