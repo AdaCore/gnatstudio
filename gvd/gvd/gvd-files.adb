c0503af276f32f3a58ee086a341f16aec524f5f0
Emmanuel Briot <briot@adacore.com>
2000-10-17 16:46:24 +0000

    New package that handles access to remote files when we are using a  remote debugger
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@68075 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ gvd/gvd/gvd-files.adb
@@ -0,0 +1,171 @@
+-----------------------------------------------------------------------
+--                   GVD - The GNU Visual Debugger                   --
+--                                                                   --
+--                         Copyright (C) 2000                        --
+--                              ACT-Europe                           --
+--                                                                   --
+-- GVD is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Odd.Types;             use Odd.Types;
+with Main_Debug_Window_Pkg; use Main_Debug_Window_Pkg;
+with GNAT.OS_Lib;           use GNAT.OS_Lib;
+with Odd.Strings;           use Odd.Strings;
+with Ada.Text_IO;           use Ada.Text_IO;
+with Gtkada.Intl;           use Gtkada.Intl;
+with GNAT.Expect;           use GNAT.Expect;
+
+package body GVD.Files is
+
+   Remote_Copy : constant String := "rcp";
+   --  Program used to copy a file from a remote host.
+
+   -------------------
+   -- Find_In_Cache --
+   -------------------
+
+   function Find_In_Cache
+     (Window    : access Main_Debug_Window_Record'Class;
+      File_Name : String) return File_Cache_List
+   is
+      Tmp : File_Cache_List := Window.File_Caches;
+   begin
+      while Tmp /= null loop
+         if Tmp.File_Name /= null
+           and then Tmp.File_Name.all = File_Name
+         then
+            return Tmp;
+         end if;
+
+         Tmp := Tmp.Next;
+      end loop;
+
+      Tmp := Window.File_Caches;
+      Window.File_Caches := new File_Cache;
+      Window.File_Caches.File_Name := new String' (File_Name);
+      Window.File_Caches.Next := Tmp;
+      return Window.File_Caches;
+   end Find_In_Cache;
+
+   ---------------
+   -- Load_File --
+   ---------------
+
+   procedure Load_File
+     (Contents    : out Odd.Types.String_Access;
+      Error_Msg   : out Odd.Types.String_Access;
+      Cache       : Odd.Types.File_Cache_List;
+      Remote_Host : Odd.Types.String_Access := null)
+   is
+      F      : File_Descriptor;
+      Length : Positive;
+      Name   : aliased constant String := Cache.File_Name.all & ASCII.NUL;
+
+      Tmp_File : GNAT.OS_Lib.Temp_File_Name;
+      Args     : Argument_List (1 .. 2);
+      Result   : Expect_Match;
+      Descriptor : GNAT.Expect.Process_Descriptor;
+      Should_Delete : Boolean := False;
+
+   begin
+
+      Error_Msg := null;
+      Contents  := null;
+
+      --  Do we already know the file contents ?
+
+      if Cache.File_Contents /= null then
+         Contents := new String' (Cache.File_Contents.all);
+         return;
+      end if;
+
+      --  Read the size of the file, we first try locally.
+      F := Open_Read (Name'Address, Binary);
+
+      --  If not found, and we are in remote mode, try to download the file
+      if F = Invalid_FD and then Remote_Host /= null then
+
+         --  Create a temporary file and get its name
+         Create_Temp_File (F, Tmp_File);
+         Close (F);
+         Should_Delete := True;
+
+         --  Download the file
+
+         Args (1) := new String'
+           (Remote_Host.all & ":" & Cache.File_Name.all);
+         Args (2) := new String'
+           (Tmp_File (Tmp_File'First .. Tmp_File'Last - 1));
+
+         GNAT.Expect.Non_Blocking_Spawn
+           (Descriptor,
+            Command     => Remote_Copy,
+            Args        => Args,
+            Buffer_Size => 0,
+            Err_To_Out  => True);
+
+         begin
+            Expect (Descriptor, Result, -".+", Timeout => -1);
+
+            if Result /= Expect_Timeout then
+               --  File was not found
+               Error_Msg := new String'(Expect_Out (Descriptor));
+               Delete_File (Tmp_File'Address, Should_Delete);
+               return;
+            end if;
+
+         exception
+            when Process_Died =>
+               null;
+         end;
+
+         Free (Args (1));
+         Free (Args (2));
+         F := Open_Read (Tmp_File'Address, Binary);
+      end if;
+
+
+      if F /= Invalid_FD then
+         Length := Positive (File_Length (F));
+
+         --  Allocate the buffer
+         --  and strip the ^Ms from the string
+         declare
+            S : String (1 .. Length);
+         begin
+            Length := Read (F, S'Address, Length);
+            Contents := new String' (Strip_Control_M (S));
+
+            --  Only save the contents in the cache for remote files
+            if Remote_Host /= null then
+               Cache.File_Contents := new String' (Contents.all);
+            end if;
+         end;
+
+         if Should_Delete then
+            Delete_File (Tmp_File'Address, Should_Delete);
+         end if;
+
+         Close (F);
+         return;
+      end if;
+
+      if Should_Delete then
+         Delete_File (Tmp_File'Address, Should_Delete);
+      end if;
+
+      Error_Msg := new String'(-"File not found: " & Cache.File_Name.all);
+   end Load_File;
+
+end GVD.Files;
---
+++ gvd/gvd/gvd-files.ads
@@ -0,0 +1,51 @@
+-----------------------------------------------------------------------
+--                   GVD - The GNU Visual Debugger                   --
+--                                                                   --
+--                         Copyright (C) 2000                        --
+--                              ACT-Europe                           --
+--                                                                   --
+-- GVD is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+--  This package groups all the functions related to file manipulation
+--  in GVD, including remote file access and caching for information
+--  that is displayed in the source window.
+
+with Odd.Types;
+with Main_Debug_Window_Pkg;
+
+package GVD.Files is
+
+   function Find_In_Cache
+     (Window    : access Main_Debug_Window_Pkg.Main_Debug_Window_Record'Class;
+      File_Name : String) return Odd.Types.File_Cache_List;
+   --  Return the cached data for a given file.
+   --  If no data was previously cached for that file, then a new File_Cache
+   --  is returned.
+   
+   procedure Load_File
+     (Contents    : out Odd.Types.String_Access;
+      Error_Msg   : out Odd.Types.String_Access;
+      Cache       : Odd.Types.File_Cache_List;
+      Remote_Host : Odd.Types.String_Access := null);
+   --  Load the contents of the file associated with Cache, and return a 
+   --  pointer to it in Contents.
+   --  It is your responsability to free Contents and Error_Msg
+   --  Control-M characters have already been stripped.
+   --
+   --  If the file could not be found, Contents is set to null and Error_Msg
+   --  contains the exact text of the error message.
+   --  Error_Msg is left to null if the file could be found.
+
+end GVD.Files;
