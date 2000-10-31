badf3a1c72813f2b0d9e825b57aa748ce8ddff53
Arnaud Charlet <charlet@adacore.com>
2000-10-31 11:06:27 +0000

    Fix bad casing.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@68156 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/debugger-gdb.adb
+++ gvd/gvd/debugger-gdb.adb
@@ -48,7 +48,7 @@ with Items.Simples;     use Items.Simples;
 with Items.Arrays;      use Items.Arrays;
 with Items.Records;     use Items.Records;
 with Items.Classes;     use Items.Classes;
-with GtkAda.Intl;       use GtkAda.Intl;
+with Gtkada.Intl;       use Gtkada.Intl;
 
 with Unchecked_Conversion;
 
