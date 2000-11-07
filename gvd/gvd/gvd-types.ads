561e99a101c8fa55bb608c25cbc5a6b5eeb4494b
Arnaud Charlet <charlet@adacore.com>
2000-11-07 17:08:30 +0000

    Remove unused package.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@68248 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/gvd-types.ads
+++ gvd/gvd/gvd-types.ads
@@ -18,7 +18,6 @@
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
-with Glib;
 with Gtkada.Types;
 with Unchecked_Deallocation;
 
