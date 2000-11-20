9a27068d9e99a8d61cd3af6a93ec5068fcaa522d
Arnaud Charlet <charlet@adacore.com>
2000-11-20 10:19:31 +0000

    Minor fix.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@68284 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/po/build_skeleton.sh
+++ gvd/po/build_skeleton.sh
@@ -1,7 +1,7 @@
 grep -- '-".*"' ../gvd/*.ad[sb] > skeleton.tmp
 grep -- '-(".*"' ../gvd/*.ad[sb] >> skeleton.tmp
 sed -e 's/.*\(".*"\).*/msgid  \1/g' < skeleton.tmp | sort | uniq | \
-grep -v 'msgid  ""$' | sed -e 's/\(".*"\)/\1\
+grep -v 'msgid  " *"$' | sed -e 's/\(".*"\)/\1\
 msgstr \1\
 /g' > skeleton.po.new
 rm skeleton.tmp
