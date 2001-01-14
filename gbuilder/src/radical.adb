8c415c4c30d420232720b08ace85cfe8eecde4a8
Arnaud Charlet <charlet@adacore.com>
2001-01-14 20:04:26 +0000

    Add calls to RAD.Editor to get some visual tests.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@68514 936e1b1b-40f2-da11-902a-00137254ae57

--- gbuilder/src/radical.adb
+++ gbuilder/src/radical.adb
@@ -1,13 +1,22 @@
 with Gtk; use Gtk;
 with Gtk.Main;
 with Gtk.Widget; use Gtk.Widget;
+with Gtk.Window; use Gtk.Window;
 with Gui_Builder_Pkg; use Gui_Builder_Pkg;
+with RAD.Editor; use RAD.Editor;
 
 procedure Radical is
+   Window : Gtk_Window;
 begin
    Gtk.Main.Set_Locale;
    Gtk.Main.Init;
    Gtk_New (Gui_Builder);
    Show_All (Gui_Builder);
+   Gtk_New (Window);
+   Set_Name (Window, "window_test");
+   Add_Mouse_Signals (Window);
+   Add_Draw_Signals (Window);
+   Add_Key_Signals (Window);
+   Show_All (Window);
    Gtk.Main.Main;
 end Radical;
