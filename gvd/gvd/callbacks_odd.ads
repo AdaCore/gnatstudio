360f77f624602c8d63aefb8ee51fd67ab9568bfd
Nicolas Setton <setton@adacore.com>
2001-01-30 18:32:02 +0000

    Add check button callback for memory view.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@68645 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/callbacks_odd.ads
+++ gvd/gvd/callbacks_odd.ads
@@ -20,12 +20,13 @@
 
 with Gtk.Handlers;
 with Gtk.Menu_Item; use Gtk.Menu_Item;
-with Gtk.Button; use Gtk.Button;
 with Gtk.Clist; use Gtk.Clist;
+with Gtk.Button; use Gtk.Button;
 with Gtk.Radio_Button; use Gtk.Radio_Button;
 with Gtk.List; use Gtk.List;
 with Gtk.Window; use Gtk.Window;
 with Gtk.GEntry; use Gtk.GEntry;
+with Gtk.Check_Button; use Gtk.Check_Button;
 with Gtk.Text; use Gtk.Text;
 
 package Callbacks_Odd is
@@ -33,11 +34,11 @@ package Callbacks_Odd is
    package Menu_Item_Callback is new
      Gtk.Handlers.Callback (Gtk_Menu_Item_Record);
 
-   package Button_Callback is new
-     Gtk.Handlers.Callback (Gtk_Button_Record);
-
    package C_List_Callback is new
      Gtk.Handlers.Callback (Gtk_Clist_Record);
+
+   package Button_Callback is new
+     Gtk.Handlers.Callback (Gtk_Button_Record);
 
    package Radio_Button_Callback is new
      Gtk.Handlers.Callback (Gtk_Radio_Button_Record);
@@ -50,6 +51,9 @@ package Callbacks_Odd is
 
    package Entry_Callback is new
      Gtk.Handlers.Callback (Gtk_Entry_Record);
+
+   package Check_Button_Callback is new
+     Gtk.Handlers.Callback (Gtk_Check_Button_Record);
 
    package Text_Callback is new
      Gtk.Handlers.Callback (Gtk_Text_Record);
