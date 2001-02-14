947341ca3aec4016849285ab02bff04be1673c31
Arnaud Charlet <charlet@adacore.com>
2001-02-14 09:19:50 +0000

    Add sh header and warning about requirements to run this script.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@68771 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/po/build_skeleton.sh
+++ gvd/po/build_skeleton.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+# Warning: this scripts must be run using gnu textutils (sort, uniq)
+
 grep -- '-".*"' ../gvd/*.ad[sb] > skeleton.tmp
 grep -- '-(".*"' ../gvd/*.ad[sb] >> skeleton.tmp
 sed -e 's/.*\(".*"\).*/msgid  \1/g' < skeleton.tmp | sort | uniq | \
