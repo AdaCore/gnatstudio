fa3d60df4438cceb0a86fbeea92ad33436b13914
Arnaud Charlet <charlet@adacore.com>
2001-02-14 14:45:56 +0000

    (Bug_Dialog): Add more info (email to report bugs and target name).
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@68779 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/gvd_main.adb
+++ gvd/gvd/gvd_main.adb
@@ -224,10 +224,12 @@ procedure GVD_Main is
       if GVD.Can_Output then
          Put_Line (Standard_Error, -"Bug detected in GVD");
          Put_Line (Standard_Error,
-                   (-"Please report with the contents of the file ") &
+                   -"Please report to gvd-devel@lists.act-europe.fr");
+         Put_Line (Standard_Error, -("with the contents of the file ") &
                    Dir.all & Directory_Separator & "log");
          Put_Line (Standard_Error, -"the following information:");
          Put_Line (Standard_Error, (-"Version: ") & GVD.Version);
+         Put_Line (Standard_Error, (-"Target: ") & GVD.Target);
          Put_Line (Standard_Error, Exception_Information (E));
          Put_Line (Standard_Error,
            -("and a description as complete as possible " &
@@ -235,10 +237,12 @@ procedure GVD_Main is
       end if;
 
       Button := Message_Dialog
-        ((-"Please report with the contents of the file ") &
+        (-"Please report to gvd-devel@lists.act-europe.fr" & ASCII.LF &
+         (-"with the contents of the file ") &
          Dir.all & Directory_Separator & "log" & ASCII.LF &
          (-"the following information:") & ASCII.LF &
          (-"Version: ") & GVD.Version & ASCII.LF &
+         (-"Target: ") & GVD.Target & ASCII.LF &
          Format (Exception_Information (E), Columns => 80) & ASCII.LF &
          (-("and a description as complete as possible " &
            "(including sources) to reproduce the bug")),
