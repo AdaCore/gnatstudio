94596b8a1257cd15a74c3c7af016489c72111a01
Nicolas Setton <setton@adacore.com>
2001-02-25 17:00:03 +0000

    Place Process_Tab in a Hbox.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@68939 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/.gate/odd/process_tab_pkg.ads
+++ gvd/gvd/.gate/odd/process_tab_pkg.ads
@@ -1,16 +1,17 @@
 with Gtk.Window; use Gtk.Window;
+with Gtk.Box; use Gtk.Box;
 with Gtk.Paned; use Gtk.Paned;
 with Gtk.Scrolled_Window; use Gtk.Scrolled_Window;
 with Gtk.Clist; use Gtk.Clist;
 with Gtk.Label; use Gtk.Label;
 with Gtk.Viewport; use Gtk.Viewport;
-with Gtk.Box; use Gtk.Box;
 with Gtk.Separator; use Gtk.Separator;
 with Gtk.Text; use Gtk.Text;
 with Gtk.Button; use Gtk.Button;
 package Process_Tab_Pkg is
 
    type Process_Tab_Record is new Gtk_Window_Record with record
+      Process_Hbox : Gtk_Hbox;
       Process_Paned : Gtk_Vpaned;
       Data_Editor_Paned : Gtk_Vpaned;
       Data_Paned : Gtk_Hpaned;
