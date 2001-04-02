61ca19e74ba4878516f245aeac75ce47a6dc9271
Emmanuel Briot <briot@adacore.com>
2001-04-02 14:13:59 +0000

    Change the default size of the creation wizard
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@69205 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/src/wizard_window_pkg.adb
+++ prj_editor/src/wizard_window_pkg.adb
@@ -20,10 +20,10 @@ procedure Initialize (Wizard_Window : access Wizard_Window_Record'Class) is
 begin
    Gtk.Window.Initialize (Wizard_Window, Window_Toplevel);
    Set_Title (Wizard_Window, -"");
-   Set_Policy (Wizard_Window, False, True, False);
+   Set_Policy (Wizard_Window, True, True, True);
    Set_Position (Wizard_Window, Win_Pos_None);
    Set_Modal (Wizard_Window, False);
-   Set_Default_Size (Wizard_Window, 250, 160);
+   Set_Default_Size (Wizard_Window, 640, 400);
 
    Gtk_New_Vbox (Wizard_Window.Vbox1, False, 0);
    Add (Wizard_Window, Wizard_Window.Vbox1);
