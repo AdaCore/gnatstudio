8331bdc0f5428fd8d0bcded9646196677a053319
Sylvain Hitier <hitier@adacore.com>
2001-05-15 13:23:35 +0000

    Handle pending events.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@69430 936e1b1b-40f2-da11-902a-00137254ae57

--- vsearch/src/search_callback.adb
+++ vsearch/src/search_callback.adb
@@ -1,6 +1,8 @@
 with Ada.Text_IO;       use Ada.Text_IO;
 with Ada.Strings.Fixed;
 
+with Gtk.Main;
+
 package body Search_Callback is
 
    Cancelled : Boolean;
@@ -17,12 +19,18 @@ package body Search_Callback is
       Line_Text   : String   := "") return Boolean
    is
       use Ada.Strings.Fixed;
+
+      Dummy : Boolean;
    begin
       if Match_Found then
          Put_Line (File
                    & ':' & Trim (Positive'Image (Line_Nr), Ada.Strings.Left)
                    & ':' & Line_Text);
       end if;
+
+      while Gtk.Main.Events_Pending loop
+          Dummy := Gtk.Main.Main_Iteration;
+      end loop;
 
       if not Cancelled then
          return True;
