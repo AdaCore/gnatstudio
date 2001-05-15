7d91975bf9ca8d2a5ecc8b0abab759a605d850eb
Sylvain Hitier <hitier@adacore.com>
2001-05-15 13:23:07 +0000

    Add files extensions in order to get context.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@69429 936e1b1b-40f2-da11-902a-00137254ae57

--- vsearch/src/hypergrep.adb
+++ vsearch/src/hypergrep.adb
@@ -3,10 +3,23 @@ with Gtk.Main;
 with Gtk.Widget; use Gtk.Widget;
 with Hyper_Grep_Window_Pkg; use Hyper_Grep_Window_Pkg;
 
+with Language;      use Language;
+With Language.Ada;  use Language.Ada;
+With Language.C;    use Language.C;
+With Language.Cpp;  use Language.Cpp;
+With Language.Java; use Language.Java;
+
 procedure Hypergrep is
 begin
    Gtk.Main.Set_Locale;
    Gtk.Main.Init;
+
+   -- ??? remove final ';' when Add_Files_Extensions is no more buggy...
+   Add_File_Extensions (Ada_Lang,  ".ads;.adb;");
+   Add_File_Extensions (C_Lang,    ".c;.h;");
+   Add_File_Extensions (Cpp_Lang,  ".cxx;.cpp;.h;");
+   Add_File_Extensions (Java_Lang, ".java;");
+
    Gtk_New (Hyper_Grep_Window);
    Show_All (Hyper_Grep_Window);
    Gtk.Main.Main;
