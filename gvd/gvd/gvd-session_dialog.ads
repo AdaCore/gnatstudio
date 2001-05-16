31218cb4ac78ecc2d4fc84d81773ab790aaedc71
Arnaud Charlet <charlet@adacore.com>
2001-05-16 17:18:48 +0000

    Derived from Open_Session_Pkg.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@69495 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ gvd/gvd/gvd-session_dialog.ads
@@ -0,0 +1,106 @@
+-----------------------------------------------------------------------
+--                   GVD - The GNU Visual Debugger                   --
+--                                                                   --
+--                      Copyright (C) 2000-2001                      --
+--                              ACT-Europe                           --
+--                                                                   --
+-- GVD is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Gtk.Window; use Gtk.Window;
+with Gtk.Box; use Gtk.Box;
+with Gtk.Label; use Gtk.Label;
+with Gtk.Scrolled_Window; use Gtk.Scrolled_Window;
+with Gtk.Viewport; use Gtk.Viewport;
+with Gtk.List; use Gtk.List;
+with Gtk.GEntry; use Gtk.GEntry;
+with Gtk.Separator; use Gtk.Separator;
+with Gtk.Hbutton_Box; use Gtk.Hbutton_Box;
+with Gtk.Button; use Gtk.Button;
+with Gtk.Widget; use Gtk.Widget;
+with Gtk.Check_Button; use Gtk.Check_Button;
+with Basic_Types; use Basic_Types;
+with Open_Session_Pkg; use Open_Session_Pkg;
+
+package GVD.Session_Dialog is
+
+   type Button_Node is private;
+   type Button_Link is access Button_Node;
+
+   type GVD_Session_Dialog_Record is new Open_Session_Record with record
+      Sessions_Dir : String_Access;
+      First_Button : Button_Link := null;
+      Lock_Buttons : Boolean := False;
+   end record;
+   type GVD_Session_Dialog is access all GVD_Session_Dialog_Record'Class;
+
+   procedure Create_Buttons
+     (Open      : access GVD_Session_Dialog_Record'Class;
+      File_Name : String);
+   --  Add all the check_buttons corresponding to the session information.
+
+   procedure Remove_All_Buttons
+     (Open : access GVD_Session_Dialog_Record'Class);
+   --  Clear all the check_buttons.
+
+   ----------------------
+   -- Session Handling --
+   ----------------------
+               
+   --  The format for session files is as follows:
+   --
+   --  [Session_File Header]
+   --  <number_of_processes>
+   --  ---------------------
+   --      <program_file_name_1>
+   --      <debugger_type_1>
+   --      <remote_host_1>
+   --      <remote_target_1>
+   --      <protocol_1>
+   --      <debugger_name_1>
+   --  ---------------------
+   --      <program_file_name_2>
+   --      <debugger_type_2>
+   --      <remote_host_2>
+   --      <remote_target_2>
+   --      <protocol_2>
+   --      <debugger_name_2>
+   --  (etc)
+   --  [History]
+   --    <debugger_number> < H | V | U > <command>
+   --    <debugger_number> < H | V | U > <command>
+   --  (etc)
+   --  ---------------------
+
+   procedure Open_Session
+     (Window : access Gtk_Widget_Record'Class;
+      Open   : in out GVD_Session_Dialog;
+      Dir    : String);
+   --  Load a session into Gvd. Window is the main debug window.
+
+   procedure Save_Session
+     (Window : access Gtk_Widget_Record'Class;
+      Open   : in out GVD_Session_Dialog;
+      Dir    : String);
+   --  Save a session. Window is the main debug window.
+
+private
+
+   type Button_Node is record
+      Next   : Button_Link;
+      Button : Gtk_Check_Button;
+      Label  : String_Access;
+   end record;
+
+end GVD.Session_Dialog;
