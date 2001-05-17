32403899c750dd3301a39e7849bdda90cb2edd18
Sylvain Hitier <hitier@adacore.com>
2001-05-17 15:03:33 +0000

    Add specifications. Split Abort_Search in Abort_Search and Reset_Search.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@69516 936e1b1b-40f2-da11-902a-00137254ae57

--- vsearch/src/search_callback.adb
+++ vsearch/src/search_callback.adb
@@ -1,3 +1,31 @@
+-----------------------------------------------------------------------
+--                                                                   --
+--                     Copyright (C) 2001                            --
+--                          ACT-Europe                               --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
 with Ada.Text_IO;       use Ada.Text_IO;
 with Ada.Strings.Fixed;
 
@@ -5,12 +33,20 @@ with Gtk.Main;
 
 package body Search_Callback is
 
-   Cancelled : Boolean;
+   Continue : Boolean;
 
-   procedure Abort_Search (State : Boolean := True) is
+   ------------------
+   -- Abort_Search --
+   ------------------
+
+   procedure Abort_Search is
    begin
-      Cancelled := State;
+      Continue := False;
    end Abort_Search;
+
+   --------------
+   -- Callback --
+   --------------
 
    function Callback
      (Match_Found : Boolean;
@@ -29,15 +65,24 @@ package body Search_Callback is
       end if;
 
       while Gtk.Main.Events_Pending loop
-          Dummy := Gtk.Main.Main_Iteration;
+         Dummy := Gtk.Main.Main_Iteration;
       end loop;
 
-      if not Cancelled then
+      if Continue then
          return True;
       else
          Put_Line ("--- ABORTING !!!");
          return False;
       end if;
    end Callback;
+
+   ------------------
+   -- Reset_Search --
+   ------------------
+
+   procedure Reset_Search is
+   begin
+      Continue := True;
+   end Reset_Search;
 
 end Search_Callback;
--- vsearch/src/search_callback.ads
+++ vsearch/src/search_callback.ads
@@ -1,15 +1,49 @@
+-----------------------------------------------------------------------
+--                                                                   --
+--                     Copyright (C) 2001                            --
+--                          ACT-Europe                               --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
 package Search_Callback is
 
-   procedure Abort_Search (State : Boolean := True);
-   --  Called with True, make the search aborting. Call it with False before
-   --  searching.
+   --  NOTE: This callback is not task safe ! It can't be used to handle two
+   --        concurrent searches !
+
+   procedure Reset_Search;
+   --  Call it before every search using this callback.
+
+   procedure Abort_Search;
+   --  Call it to abort the search. The next time the callback is called, it
+   --  will abort the search.
 
    function Callback
      (Match_Found : Boolean;
       File        : String;
       Line_Nr     : Positive := 1;
       Line_Text   : String   := "") return Boolean;
-   --  Print every match 'file:line:text'.
-   --  Ignore file calls.
+   --  Print every match 'file:line:text'; ignore file calls.
+   --  Handle Gtk pending events.
 
 end Search_Callback;
