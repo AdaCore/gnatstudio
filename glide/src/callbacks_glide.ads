1fb49542efefd12eae557b6df576df608a868d33
Arnaud Charlet <charlet@adacore.com>
2001-06-25 08:21:53 +0000

    Regenerated from glide.glade:1.1
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@69757 936e1b1b-40f2-da11-902a-00137254ae57

--- glide/src/callbacks_glide.ads
+++ glide/src/callbacks_glide.ads
@@ -1,10 +1,4 @@
-with Gtk.Handlers;
-pragma Elaborate_All (Gtk.Handlers);
-with Gtk.Button; use Gtk.Button;
 
 package Callbacks_Glide is
-
-   package Button_Callback is new
-     Gtk.Handlers.Callback (Gtk_Button_Record);
 
 end Callbacks_Glide;
