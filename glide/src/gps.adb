557ff057a304d8abc5d7f9f55b61433e75f9a37f
Arnaud Charlet <charlet@adacore.com>
2001-06-25 08:22:20 +0000

    New main program that integrates GVD inside glide.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@69758 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ glide/src/gps.adb
@@ -0,0 +1,25 @@
+with Gtk; use Gtk;
+with Gtk.Main;
+with Glide_Page;
+with Glide_Menu;
+with GVD.Main_Window;
+
+procedure Glide2 is
+   use GVD.Main_Window;
+
+   Glide : GVD_Main_Window;
+   Page  : Glide_Page.Glide_Page;
+
+begin
+   Gtk.Main.Set_Locale;
+   Gtk.Main.Init;
+
+   Gtk_New (Glide, "<glide>", Glide_Menu.Glide_Menu_Items.all);
+   Set_Title (Glide, "Glide - New Generation");
+   Glide.Gvd_Home_Dir := new String' ("");
+   Glide.Prefix_Directory := new String' ("");
+
+   Glide_Page.Gtk_New (Page, Glide);
+   Show_All (Glide);
+   Gtk.Main.Main;
+end Glide2;
