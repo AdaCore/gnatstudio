fe1369d4e5f6d4ad488a0c936965297732a93067
Arnaud Charlet <charlet@adacore.com>
2001-06-26 15:38:59 +0000

    Get maximum size for initialize window.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@69798 936e1b1b-40f2-da11-902a-00137254ae57

--- glide/src/gps.adb
+++ glide/src/gps.adb
@@ -1,3 +1,5 @@
+with Glib; use Glib;
+with Gdk.Window; use Gdk.Window;
 with Gtk; use Gtk;
 with Gtk.Main;
 with Glide_Page;
@@ -7,14 +9,20 @@ with GVD.Main_Window;
 procedure Glide2 is
    use GVD.Main_Window;
 
-   Glide : GVD_Main_Window;
-   Page  : Glide_Page.Glide_Page;
+   Border        : constant := 32;
+   Glide         : GVD_Main_Window;
+   Page          : Glide_Page.Glide_Page;
+   X, Y          : Gint;
+   Width, Height : Gint;
+   Depth         : Gint;
 
 begin
    Gtk.Main.Set_Locale;
    Gtk.Main.Init;
 
+   Get_Geometry (null, X, Y, Width, Height, Depth);
    Gtk_New (Glide, "<glide>", Glide_Menu.Glide_Menu_Items.all);
+   Set_Default_Size (Glide, Width, Height - Border);
    Set_Title (Glide, "Glide - New Generation");
    Glide.Gvd_Home_Dir := new String' ("");
    Glide.Prefix_Directory := new String' ("");
