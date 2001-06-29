574e4b504e2e74c288ce4a0a2696e3019978d678
Joel Brobecker <brobecke@adacore.com>
2001-06-29 08:39:45 +0000

    (Src): New use Src_Menu.Create_Main_Window to create the main window. Removed unused withs.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@69907 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/src.adb
+++ src_editor/src/src.adb
@@ -1,43 +1,32 @@
-with Gtk.Box;          use Gtk.Box;
-with Gtk.Enums;        use Gtk.Enums;
-with Gtk.Item_Factory; use Gtk.Item_Factory;
 with Gtk.Main;
 with Gtk.Window;       use Gtk.Window;
-with Src_Cb;           use Src_Cb;
 with Src_Editor_Box;   use Src_Editor_Box;
 with Src_Menu;         use Src_Menu;
 
+with Language;         use Language;
 with Language.Ada;     use Language.Ada;
+with Language.C;       use Language.C;
+with Language.Cpp;     use Language.Cpp;
 
 procedure Src is
+   My_Box      : Source_Editor_Box;
    Main_Window : Gtk_Window;
-   V_Box : Gtk.Box.Gtk_Box;
-   My_Box : aliased Source_Editor_Box;
-   Menu : Gtk_Item_Factory;
-   Ignored : Boolean;
+   Ignored     : Boolean;
 begin
+
+   Add_File_Extensions (Ada_Lang, ".ads;.adb;.ada");
+   Add_File_Extensions (C_Lang, ".h;.c");
+   Add_File_Extensions (Cpp_Lang, ".hh;.cc;.cpp");
+
    Gtk.Main.Set_Locale;
    Gtk.Main.Init;
 
-   Gtk_New (Main_Window, Window_Toplevel);
-   Set_Title (Main_Window, "The GLIDE Source Editor");
-   Set_Default_Size (Main_Window, 350, 400);
-   Window_Cb.Connect
-     (Main_Window, "destroy",
-      Window_Cb.To_Marshaller (Exit_Main'Access));
-
-   Gtk_New_Vbox (V_Box);
-   Add (Main_Window, V_Box);
-
-   Gtk_New (My_Box, Lang => Ada_Lang);
-
-   Create_Menu (Menu, Main_Window, My_Box);
-   Pack_Start (V_Box, Get_Widget (Menu, Root), False, False, 0);
-
-   Attach (My_Box, V_Box);
-
+   Gtk_New (My_Box);
+   Create_Main_Window (Main_Window, My_Box);
    Show_All (Main_Window);
-   --  Load_File (My_Box, "/home/brobecke/act/gnat/sem_ch3.adb", Ignored);
+   Load_File (My_Box, "/home/brobecke/act/gnat/sem_ch3.adb",
+              Success => Ignored);
+
    Gtk.Main.Main;
 end Src;
 
