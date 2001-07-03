027ce0204919af353027f67a75d3af0686611696
Arnaud Charlet <charlet@adacore.com>
2001-07-03 10:44:11 +0000

    Define the main window of Glide.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@69920 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ kernel/src/glide_main_window.adb
@@ -0,0 +1,20 @@
+package body Glide_Main_Window is
+
+   procedure Gtk_New
+     (Main_Window : out Glide_Window;
+      Key         : String;
+      Menu_Items  : Gtk_Item_Factory_Entry_Array) is
+   begin
+      Main_Window := new Glide_Window_Record;
+      Initialize (Main_Window, Key, Menu_Items);
+   end Gtk_New;
+
+   procedure Initialize
+     (Main_Window : access Glide_Window_Record'Class;
+      Key         : String;
+      Menu_Items  : Gtk_Item_Factory_Entry_Array) is
+   begin
+      GVD.Main_Window.Initialize (Main_Window, Key, Menu_Items);
+   end Initialize;
+
+end Glide_Main_Window;
---
+++ kernel/src/glide_main_window.ads
@@ -0,0 +1,27 @@
+with GVD.Main_Window;
+with Glide_Kernel;
+with Gtk.Item_Factory; use Gtk.Item_Factory;
+
+package Glide_Main_Window is
+
+   type Glide_Window_Record is new GVD.Main_Window.GVD_Main_Window_Record with
+   record
+      Kernel : Glide_Kernel.Kernel_Handle;
+   end record;
+   type Glide_Window is access all Glide_Window_Record'Class;
+
+   procedure Gtk_New
+     (Main_Window : out Glide_Window;
+      Key         : String;
+      Menu_Items  : Gtk_Item_Factory_Entry_Array);
+   --  Create a new main window.
+   --  Key is a unique string identifying main_window.
+   --  Menu_Items is used to create the menu bar.
+
+   procedure Initialize
+     (Main_Window : access Glide_Window_Record'Class;
+      Key         : String;
+      Menu_Items  : Gtk_Item_Factory_Entry_Array);
+   --  Internal initialization function.
+
+end Glide_Main_Window;
