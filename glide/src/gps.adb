176e72154541b7a10771f2225cc9f004d1be155a
Arnaud Charlet <charlet@adacore.com>
2001-07-09 10:35:11 +0000

    Use Maximize instead of computing the maximal size by hand.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@69961 936e1b1b-40f2-da11-902a-00137254ae57

--- glide/src/gps.adb
+++ glide/src/gps.adb
@@ -1,5 +1,3 @@
-with Glib; use Glib;
-with Gdk.Window; use Gdk.Window;
 with Gtk; use Gtk;
 with Gtk.Main;
 with Glide_Page;
@@ -9,22 +7,16 @@ with Glide_Main_Window;
 procedure Glide2 is
    use Glide_Main_Window;
 
-   X_Border      : constant := 9;
-   Y_Border      : constant := 86;
-   Glide         : Glide_Window;
-   Page          : Glide_Page.Glide_Page;
-   X, Y          : Gint;
-   Width, Height : Gint;
-   Depth         : Gint;
+   Glide : Glide_Window;
+   Page  : Glide_Page.Glide_Page;
 
 begin
    Gtk.Main.Set_Locale;
    Gtk.Main.Init;
 
-   Get_Geometry (null, X, Y, Width, Height, Depth);
    Gtk_New (Glide, "<glide>", Glide_Menu.Glide_Menu_Items.all);
-   Set_Default_Size (Glide, Width - X_Border, Height - Y_Border);
    Set_Title (Glide, "Glide - New Generation");
+   Maximize (Glide);
    Glide.Gvd_Home_Dir := new String' ("");
    Glide.Prefix_Directory := new String' ("");
 
