7b2f0293e7cc9258f5e9ecd0eebf124e88c50dc7
Joel Brobecker <brobecke@adacore.com>
2001-07-09 16:28:56 +0000

    (Src): Initialize the project parser code. Migrate the creation of the  Source_Box by passing the Glide_Kernel handle. Add some commented-out  code used during testing sessions.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@69979 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/src.adb
+++ src_editor/src/src.adb
@@ -1,16 +1,25 @@
 with Gtk.Main;
-with Gtk.Window;       use Gtk.Window;
-with Src_Editor_Box;   use Src_Editor_Box;
-with Src_Menu;         use Src_Menu;
+with Gtk.Window;           use Gtk.Window;
+with Src_Editor_Box;       use Src_Editor_Box;
+with Src_Menu;             use Src_Menu;
 
-with Language;         use Language;
-with Language.Ada;     use Language.Ada;
-with Language.C;       use Language.C;
-with Language.Cpp;     use Language.Cpp;
+with Glide_Kernel.Project; use Glide_Kernel.Project;
+with Language;             use Language;
+with Language.Ada;         use Language.Ada;
+with Language.C;           use Language.C;
+with Language.Cpp;         use Language.Cpp;
+
+with Csets;
+with Namet;
+with Prj;
+with Prj.Tree;
+with Snames;
 
 procedure Src is
    My_Box      : Source_Editor_Box;
    Main_Window : Gtk_Window;
+   Kernel      : Glide_Kernel.Kernel_Handle;
+   --  Ignored     : Boolean;
 begin
 
    Add_File_Extensions (Ada_Lang, ".ads;.adb;.ada");
@@ -20,9 +29,20 @@ begin
    Gtk.Main.Set_Locale;
    Gtk.Main.Init;
 
-   Gtk_New (My_Box);
+   --  Initialize the Project parser code before loading a project.
+   --  This is because we need a project to create a Src_Editor_Box.
+   Namet.Initialize;
+   Csets.Initialize;
+   Snames.Initialize;
+   Prj.Initialize;
+   Prj.Tree.Initialize;
+   Load_Project (Kernel, "src.gpr");
+
+   Gtk_New (My_Box, Kernel);
    Create_Main_Window (Main_Window, My_Box);
    Show_All (Main_Window);
+   --  Load_File
+   --    (My_Box, "src_editor_box.adb", Success => Ignored);
 
    Gtk.Main.Main;
 end Src;
