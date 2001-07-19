b11704b1fe52f28d2cb3c158f3c7042afa1a1d73
Emmanuel Briot <briot@adacore.com>
2001-07-19 09:32:35 +0000

    (Refresh_Tree): removed All the tools that make up the project editor (explorer, viewer, ...) now  use a Project manager, instead of directly accessing the lower level  project tree. They also automatically react to changes to the project,  so we don't need to connect the "changed" signal in this package anymore.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@70126 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/src/prj_editor.adb
+++ prj_editor/src/prj_editor.adb
@@ -1,13 +1,36 @@
+-----------------------------------------------------------------------
+--                                                                   --
+--                     Copyright (C) 2001                            --
+--                          ACT-Europe                               --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
 
 with Prj.Tree;     use Prj.Tree;
 with Prj;          use Prj;
-with Prj.PP;       use Prj.PP;
 with Prj.Part;     use Prj.Part;
-with Prj.Proc;     use Prj.Proc;
-with Prj.Com;      use Prj.Com;
-with Errout;       use Errout;
 
-with Gtk.Main;     use Gtk.Main;
+with Gtk.Main;        use Gtk.Main;
 with Gtk.Window;      use Gtk.Window;
 with Gtk.Enums;       use Gtk.Enums;
 with Gtk.Scrolled_Window; use Gtk.Scrolled_Window;
@@ -17,14 +40,13 @@ with Gtk.Widget;      use Gtk.Widget;
 with Project_Trees;   use Project_Trees;
 with Project_Viewers; use Project_Viewers;
 with Scenario_Views;  use Scenario_Views;
+with Prj_Manager;     use Prj_Manager;
 
 with Gtkada.MDI;      use Gtkada.MDI;
 
 procedure Prj_Editor is
 
    Project : Project_Node_Id;
-   Project_View : Project_Id;
-
    Win : Gtk_Window;
    Tree : Project_Tree;
    Viewer : Project_Viewer;
@@ -32,12 +54,10 @@ procedure Prj_Editor is
    MDI : MDI_Window;
    Child : MDI_Child;
    Scrolled : Gtk_Scrolled_Window;
+   Manager : Project_Manager;
 
    procedure Selection_Changed (Tree : access Gtk_Widget_Record'Class);
    --  Called every time the selection has changed in the tree
-
-   procedure Refresh_Tree (Tree : access Gtk_Widget_Record'Class);
-   --  Recompute the tree in the current scenario
 
    -----------------------
    -- Selection_Changed --
@@ -45,51 +65,33 @@ procedure Prj_Editor is
 
    procedure Selection_Changed (Tree : access Gtk_Widget_Record'Class) is
       T : Project_Tree := Project_Tree (Tree);
-      Project : Project_Id := Get_Selected_Project (T);
+      Project_View : Project_Id := Get_Selected_Project (T);
    begin
       Clear (Viewer);  --  ??? Should delete selectively
-      if Project /= No_Project then
+      if Project_View /= No_Project then
          declare
-            Dirs : Name_Id_Array := Get_Selected_Directories (T, Project);
+            Dirs : Name_Id_Array :=
+              Get_Selected_Directories (T, Project_View);
          begin
             if Dirs'Length = 0 then
-               Show_Project (Viewer, Project);
+               Show_Project (Viewer, Project_View);
             else
-               Show_Project (Viewer, Project, Dirs (Dirs'First));
+               Show_Project (Viewer, Project_View, Dirs (Dirs'First));
             end if;
          end;
       end if;
    end Selection_Changed;
 
-   ------------------
-   -- Refresh_Tree --
-   ------------------
-
-   procedure Refresh_Tree (Tree : access Gtk_Widget_Record'Class) is
-      T : Project_Tree := Project_Tree (Tree);
-   begin
-      --  ??? Need to free as much memory as possible first.
-      --  ??? Maybe a call to Prj.Reset is enough
-      Errout.Initialize;
-      pragma Assert (Project /= Empty_Node);
-      Prj.Com.Units.Set_Last (No_Unit);
-      Prj.Com.Units_Htable.Reset;
-
-      Pretty_Print (Project);
-      Process (Project => Project_View, From_Project_Node => Project);
-      Errout.Finalize;
-      pragma Assert (Project_View /= No_Project);
-
-      Load (T, Project, Project_View);
-      Refresh (Scenar, Project_View);
-   end Refresh_Tree;
-
 begin
+   Gtk.Main.Init;
+
    Prj.Part.Parse (Project, "root.gpr", Always_Errout_Finalize => True);
    pragma Assert (Project /= Empty_Node);
 
-   Gtk.Main.Init;
+   --  Project manager
+   Gtk_New (Manager, Project);
 
+   --  Main window
    Gtk_New (Win, Window_Toplevel);
    Set_Default_Size (Win, 500, 500);
 
@@ -97,11 +99,10 @@ begin
    Add (Win, MDI);
    Set_Priorities (MDI, (Top => 1, Left => 2, others => 3));
 
-
    --  Explorer
    Gtk_New (Scrolled);
    Set_Policy (Scrolled, Policy_Automatic, Policy_Automatic);
-   Gtk_New (Tree, Columns => 1);
+   Gtk_New (Tree, Manager, Columns => 1);
    Add (Scrolled, Tree);
    Child := Put (MDI, Scrolled);
    Set_Title (Child, "Explorer");
@@ -117,28 +118,23 @@ begin
 
    --  Scenario editor
 
-   Gtk_New (Scenar, Project);
+   Gtk_New (Scenar, Manager);
    Child := Put (MDI, Scenar);
    Set_Title (Child, "Current Scenario");
    Set_Dock_Side (Child, Top);
    Dock_Child (Child);
 
-   Widget_Callback.Object_Connect
-     (Scenar, "changed",
-      Widget_Callback.To_Marshaller (Refresh_Tree'Unrestricted_Access), Tree);
-
    --  Project Viewer
 
-   Gtk_New (Viewer);
+   Gtk_New (Viewer, Manager);
    Child := Put (MDI, Viewer);
    Set_Title (Child, "Project Contents");
 
-
-   Refresh_Tree (Tree);
+   --  Compute the current view, and initialize the explorer, scenario, ...
+   Recompute_View (Manager);
 
    Maximize_Children (MDI);
 
    Show_All (Win);
    Main;
-   Pretty_Print (Project);
 end Prj_Editor;
