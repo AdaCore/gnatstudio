279e2f16d0a377d1731093f632d4fd4f8b6984eb
Emmanuel Briot <briot@adacore.com>
2001-07-20 15:53:07 +0000

    Add new test for Append_Declaration
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@70176 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/tests/test_scenario.adb
+++ prj_editor/tests/test_scenario.adb
@@ -5,6 +5,7 @@ with Types;          use Types;
 with Namet;          use Namet;
 with Stringt;        use Stringt;
 with Prj.Tree;       use Prj.Tree;
+with Prj.PP;         use Prj.PP;
 
 with Text_IO; use Text_IO;
 
@@ -43,6 +44,7 @@ procedure Test_Scenario is
 
    Val1, Val2, Val3, Val4, Val5, Val6 : String_Id;
    Project, Typ, Var1, Var2, Var3     : Project_Node_Id;
+   Output_Project                     : Project_Node_Id;
 
 begin
    Project := Create_Project ("Foo", "");
@@ -51,16 +53,19 @@ begin
    Val1 := Add_Possible_Value (Typ, "Val1");
    Val4 := Add_Possible_Value (Typ, "Val4");
    Var1 := Get_Or_Create_Typed_Variable (Project, "Var1", Typ);
+   Set_Value_As_External (Var1, "EXT_1");
 
    Typ := Get_Or_Create_Type (Project, "Typ2");
    Val2 := Add_Possible_Value (Typ, "Val2");
    Val3 := Add_Possible_Value (Typ, "Val3");
    Var2 := Get_Or_Create_Typed_Variable (Project, "Var2", Typ);
+   Set_Value_As_External (Var2, "EXT_2");
 
    Typ := Get_Or_Create_Type (Project, "Typ3");
    Val5 := Add_Possible_Value (Typ, "Val5");
    Val6 := Add_Possible_Value (Typ, "Val6");
    Var3 := Get_Or_Create_Typed_Variable (Project, "Var3", Typ);
+   Set_Value_As_External (Var3, "EXT_3");
 
    Initialize (Mgr, (Var1, Var2, Var3));
 
@@ -70,11 +75,15 @@ begin
    Set_Scenario_Name (Mgr, (Val4, Val3, Val5), "SCENAR_4_3_5");
    Set_Scenario_Name (Mgr, (Val1, Val2, Val6), "SCENAR_1_2_6");
    Set_Scenario_Name (Mgr, (Val1, Val3, Val6), "SCENAR_1_3_6");
-   Set_Scenario_Name (Mgr, (Val4, Val2, Val6), "SCENAR_4_2_6");
+--   Set_Scenario_Name (Mgr, (Val4, Val2, Val6), "SCENAR_4_2_6");
    Set_Scenario_Name (Mgr, (Val4, Val3, Val6), "SCENAR_4_3_6");
 
    Output ((Val1, Val2, Val5));
    Output ((No_String, Val2, Val5));
    Output ((Val1, No_String, Val5));
    Output ((Val1, No_String, No_String));
+
+   Output_Project := Create_Project ("Foo.Internal", "");
+   Append_Declaration (Mgr, Output_Project);
+   Pretty_Print (Output_Project);
 end Test_Scenario;
