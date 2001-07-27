89c60a04e77d16c29db2019d38291979daa30984
Arnaud Charlet <charlet@adacore.com>
2001-07-27 11:12:40 +0000

    Updated to use Ada_Analyze.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@70303 936e1b1b-40f2-da11-902a-00137254ae57

--- syntax/src/gnatparse.adb
+++ syntax/src/gnatparse.adb
@@ -1,26 +1,40 @@
-with Source_Analyzer; use Source_Analyzer;
+with Ada_Analyzer;     use Ada_Analyzer;
 with Ada.Command_Line; use Ada.Command_Line;
 with GNAT.OS_Lib;  use GNAT.OS_Lib;
 with Ada.Text_IO;  use Ada.Text_IO;
 with String_Utils; use String_Utils;
+with Basic_Types;      use Basic_Types;
+with Language;         use Language;
 
 procedure Gnatparse is
+   subtype String_Access is Basic_Types.String_Access;
+
    F           : File_Descriptor;
    Name        : constant String := Argument (1) & ASCII.NUL;
    Buffer      : String_Access;
    Length      : Integer;
-   Constructs  : Construct_List;
    Info        : Construct_Access;
    Indent      : Natural;
    Next_Indent : Natural;
+   New_Buffer  : Extended_Line_Buffer;
+   Constructs  : aliased Construct_List;
 
 begin
    F      := Open_Read (Name, Binary);
    Buffer := new String (1 .. Integer (File_Length (F)));
    Length := Read (F, Buffer.all'Address, Buffer'Length);
    Close (F);
-   Parse_Ada_Constructs
-     (Buffer (1 .. Length), Constructs, Indent, Next_Indent);
+
+   Analyze_Ada_Source
+     (To_Unchecked_String (Buffer.all'Address), Length,
+      New_Buffer, Default_Indent_Parameters,
+      Reserved_Casing  => Unchanged,
+      Ident_Casing     => Unchanged,
+      Format_Operators => False,
+      Indent           => False,
+      Constructs       => Constructs'Unchecked_Access,
+      Current_Indent   => Next_Indent,
+      Prev_Indent      => Indent);
    Free (Buffer);
    Put_Line ("Indent = " & Image (Indent));
    Put_Line ("Next Indent = " & Image (Next_Indent));
--- syntax/src/gnatpp.adb
+++ syntax/src/gnatpp.adb
@@ -1,17 +1,32 @@
-with Source_Analyzer;
+with Ada_Analyzer;     use Ada_Analyzer;
 with Ada.Command_Line; use Ada.Command_Line;
-with GNAT.OS_Lib; use GNAT.OS_Lib;
+with GNAT.OS_Lib;      use GNAT.OS_Lib;
+with Basic_Types;      use Basic_Types;
+with Language;         use Language;
 
 procedure Gnatpp is
-   F      : File_Descriptor;
-   Name   : constant String := Argument (1) & ASCII.NUL;
-   Buffer : String_Access;
-   Length : Integer;
+   subtype String_Access is Basic_Types.String_Access;
+
+   F          : File_Descriptor;
+   Name       : constant String := Argument (1) & ASCII.NUL;
+   Buffer     : String_Access;
+   Length     : Integer;
+   New_Buffer : Extended_Line_Buffer;
+   Ignore     : Natural;
 
 begin
    F := Open_Read (Name, Binary);
    Buffer := new String (1 .. Integer (File_Length (F)));
    Length := Read (F, Buffer.all'Address, Buffer'Length);
    Close (F);
-   Source_Analyzer.Format_Ada (Buffer.all);
+   New_Buffer := To_Line_Buffer (Buffer.all);
+   Analyze_Ada_Source
+     (To_Unchecked_String (Buffer.all'Address), Buffer'Length,
+      New_Buffer,
+      Indent_Params  => Default_Indent_Parameters,
+      Current_Indent => Ignore,
+      Prev_Indent    => Ignore);
+   Print (New_Buffer);
+   Free (New_Buffer);
+   Free (Buffer);
 end Gnatpp;
