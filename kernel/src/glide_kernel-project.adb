0e9dc939597186f11c5071c8d84373f684fb1467
Emmanuel Briot <briot@adacore.com>
2001-07-31 15:09:16 +0000

    Add headers (Recompute_View, Get_Project, Get_Project_View): New subprograms (Load_Project, Get_Project_Name): Parameters change since the kernel is  now a GObject
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@70353 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/glide_kernel-project.adb
+++ kernel/src/glide_kernel-project.adb
@@ -1,7 +1,38 @@
+-----------------------------------------------------------------------
+--                                                                   --
+--                     Copyright (C) 2001                            --
+--                          ACT-Europe                               --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
 
 with Prj;         use Prj;
-with Prj.Pars;    use Prj.Pars;
+with Prj.Com;     use Prj.Com;
+with Prj.Part;    use Prj.Part;
+with Prj.Proc;    use Prj.Proc;
 with Prj.Env;     use Prj.Env;
+with Prj.Tree;    use Prj.Tree;
+with Errout;      use Errout;
 with Namet;       use Namet;
 with GNAT.OS_Lib; use GNAT.OS_Lib;
 
@@ -12,14 +43,14 @@ package body Glide_Kernel.Project is
    ----------------------
 
    function Find_Source_File
-     (Kernel : Kernel_Handle; Short_File_Name : String)
+     (Kernel : access Kernel_Handle_Record'Class; Short_File_Name : String)
       return String
    is
       Path : String_Access;
    begin
       Path := Locate_Regular_File
         (Short_File_Name,
-         Ada_Include_Path (Kernel.Project).all);
+         Ada_Include_Path (Kernel.Project_View).all);
 
       if Path = null then
          return "";
@@ -38,19 +69,65 @@ package body Glide_Kernel.Project is
    -- Get_Project_File_Name --
    ---------------------------
 
-   function Get_Project_File_Name (Kernel : Kernel_Handle) return String is
+   function Get_Project_File_Name
+     (Kernel : access Kernel_Handle_Record'Class) return String is
    begin
-      return Get_Name_String (Projects.Table (Kernel.Project).Name);
+      return Get_Name_String (Name_Of (Kernel.Project));
    end Get_Project_File_Name;
 
    ------------------
    -- Load_Project --
    ------------------
 
-   procedure Load_Project (Kernel : in out Kernel_Handle; Project : String) is
+   procedure Load_Project
+     (Kernel : access Kernel_Handle_Record'class; Project : String) is
    begin
-      Prj.Pars.Parse (Kernel.Project, Project);
+      Prj.Part.Parse (Kernel.Project, Project, True);
+      Prj.Proc.Process (Kernel.Project_View, Kernel.Project);
+      Project_Changed (Kernel);
+      Project_View_Changed (Kernel);
    end Load_Project;
+
+   -----------------
+   -- Get_Project --
+   -----------------
+
+   function Get_Project (Handle : access Kernel_Handle_Record'Class)
+      return Prj.Tree.Project_Node_Id is
+   begin
+      return Handle.Project;
+   end Get_Project;
+
+   ----------------------
+   -- Get_Project_View --
+   ----------------------
+
+   function Get_Project_View
+     (Handle : access Kernel_Handle_Record'Class) return Prj.Project_Id is
+   begin
+      return Handle.Project_View;
+   end Get_Project_View;
+
+   --------------------
+   -- Recompute_View --
+   --------------------
+
+   procedure Recompute_View
+     (Handle  : access Kernel_Handle_Record'Class) is
+   begin
+      --  ??? Need to free as much memory as possible first.
+      --  ??? Maybe a call to Prj.Reset is enough
+      Errout.Initialize;
+      Prj.Com.Units.Set_Last (No_Unit);
+      Prj.Com.Units_Htable.Reset;
+
+      Prj.Proc.Process (Handle.Project_View, Handle.Project);
+
+      Errout.Finalize;
+      pragma Assert (Handle.Project_View /= No_Project);
+
+      Project_View_Changed (Handle);
+   end Recompute_View;
 
 end Glide_Kernel.Project;
 
--- kernel/src/glide_kernel-project.ads
+++ kernel/src/glide_kernel-project.ads
@@ -1,17 +1,93 @@
+-----------------------------------------------------------------------
+--                                                                   --
+--                     Copyright (C) 2001                            --
+--                          ACT-Europe                               --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
 --  This package provides access to the project characteristics such as
 --  object and source path, options.
+--
+--  Several concepts are attached to projects, and this interface tries to
+--  stay consistent with the following naming:
+--
+--  A --project-- is a tree representation of a project and its imported
+--  projects.
+--
+--  A --scenario variable-- is any variable in a project that is typed (ie has
+--  a defined set of possible values) and references external variables. In the
+--  context of the GUI, we do not actually reference environment variables, but
+--  these are set to the appropriate values before calling project-aware tools
+--  like gnatmake, gnatbind,...
+--
+--  A --scenario-- is a named set of values for all the environment variables
+--  in a project.
+--
+--  A --project view-- is the result of processing the tree in a given
+--  scenario. Such a view needs to be re-eavaluted every time the scenario
+--  changes. This is what is actually displayed in the project explorer and the
+--  project viewer.
+--  A fundamental assumption in the implementation is that the project
+--  hierarchy (imported projects, modified projects,...) can not change when
+--  the view changes.
+
+with Prj;
+with Prj.Tree;
 
 package Glide_Kernel.Project is
 
-   function Get_Project_File_Name (Kernel : Kernel_Handle) return String;
+   function Get_Project_File_Name
+     (Kernel : access Kernel_Handle_Record'Class) return String;
+   --  Return the name of the current project, as should be used on the command
+   --  line for all the tools that support projects
 
    function Find_Source_File
-     (Kernel : Kernel_Handle; Short_File_Name : String)
+     (Kernel : access Kernel_Handle_Record'Class; Short_File_Name : String)
       return String;
    --  Search in the project source path Short_File_Name and return its
    --  complete path, or an empty string in case of failure.
 
-   procedure Load_Project (Kernel : in out Kernel_Handle; Project : String);
+   procedure Load_Project
+     (Kernel : access Kernel_Handle_Record'Class; Project : String);
    --  Load project Project as the current project.
+   --  This emits the "project_changed" and "project_view_changed" signals.
+
+   function Get_Project (Handle : access Kernel_Handle_Record'Class)
+      return Prj.Tree.Project_Node_Id;
+   --  Return the current project tree. This tree can be fully manipulated, and
+   --  extended. However, you should reevaluate the view after you have
+   --  finished your changes, so as to report the changes to all the other
+   --  tools.
+
+   function Get_Project_View
+     (Handle : access Kernel_Handle_Record'Class) return Prj.Project_Id;
+   --  Return the current project view
+
+   procedure Recompute_View
+     (Handle  : access Kernel_Handle_Record'Class);
+   --  Recompute the view of the project, based on the current value of all
+   --  scenario variables.
+   --  This emits the "project_view_changed" signal.
 
 end Glide_Kernel.Project;
