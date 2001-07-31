ab41079798c179333a4b6b646058d1e87f0b7116
Emmanuel Briot <briot@adacore.com>
2001-07-31 15:08:08 +0000

    Add headers (Gtk_New, Project_Changed, Project_View_Changed): new signals The kernel is now a GObject, that emits signals when the project changes
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@70352 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ kernel/src/glide_kernel.adb
@@ -0,0 +1,83 @@
+-----------------------------------------------------------------------
+--                                                                   --
+--                     Copyright (C) 2001                            --
+--                          ACT-Europe                               --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
+with Glib;                 use Glib;
+with Glib.Object;          use Glib.Object;
+with Gtk.Handlers;         use Gtk.Handlers;
+with Interfaces.C;         use Interfaces.C;
+with Interfaces.C.Strings; use Interfaces.C.Strings;
+
+package body Glide_Kernel is
+
+   Project_Changed_Signal      : constant String := "project_changed";
+   Project_View_Changed_Signal : constant String := "project_view_changed";
+
+   Signals : constant chars_ptr_array :=
+     (1 => New_String (Project_Changed_Signal),
+      2 => New_String (Project_View_Changed_Signal));
+   --  The list of signals defined for this object
+
+   Kernel_Class : GObject_Class := Uninitialized_Class;
+   --  The class structure for this object
+
+   package Object_Callback is new Gtk.Handlers.Callback
+     (Glib.Object.GObject_Record);
+
+   -------------
+   -- Gtk_New --
+   -------------
+
+   procedure Gtk_New (Handle : out Kernel_Handle) is
+      Signal_Parameters : constant Signal_Parameter_Types :=
+        (1 => (1 => GType_None), 2 => (1 => GType_None));
+   begin
+      Handle := new Kernel_Handle_Record;
+      Glib.Object.Initialize (Handle);
+      Initialize_Class_Record
+        (Handle, Signals, Kernel_Class, "GlideKernel", Signal_Parameters);
+   end Gtk_New;
+
+   ---------------------
+   -- Project_Changed --
+   ---------------------
+
+   procedure Project_Changed (Handle : access Kernel_Handle_Record) is
+   begin
+      Object_Callback.Emit_By_Name (Handle, Project_Changed_Signal);
+   end Project_Changed;
+
+   --------------------------
+   -- Project_View_Changed --
+   --------------------------
+
+   procedure Project_View_Changed (Handle : access Kernel_Handle_Record) is
+   begin
+      Object_Callback.Emit_By_Name (Handle, Project_View_Changed_Signal);
+   end Project_View_Changed;
+
+end Glide_Kernel;
--- kernel/src/glide_kernel.ads
+++ kernel/src/glide_kernel.ads
@@ -1,18 +1,91 @@
+-----------------------------------------------------------------------
+--                                                                   --
+--                     Copyright (C) 2001                            --
+--                          ACT-Europe                               --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
 --  This package is the root of the glide's kernel API.
 
 with Prj;
+with Prj.Tree;
+with Glib.Object;
 
 package Glide_Kernel is
 
-   type Kernel_Handle is private;
+   type Kernel_Handle_Record is new Glib.Object.GObject_Record with private;
+   type Kernel_Handle is access all Kernel_Handle_Record'Class;
    --  A kernel handle used to share information throughout Glide.
    --  ??? Consider whether using a tagged type would be useful.
 
+   procedure Gtk_New (Handle : out Kernel_Handle);
+   --  Create a new Glide kernel.
+   --  By default, it isn't associated with any project, nor any source editor.
+
+   ---------------------
+   -- Signal emission --
+   ---------------------
+
+   procedure Project_Changed (Handle : access Kernel_Handle_Record);
+   --  Emits the "project_changed" signal
+
+   procedure Project_View_Changed (Handle : access Kernel_Handle_Record);
+   --  Emits the "project_view_changed" signal
+
+   -------------
+   -- Signals --
+   -------------
+
+   --  <signals>
+   --  The following new signals are defined for this widget:
+   --
+   --  - "project_changed"
+   --    procedure Handler (Handle : access Kernel_Handle_Record'Class);
+   --
+   --    Emitted when the project has changed. This means that a new project
+   --    has been loaded in Glide, and that all the previous settings and
+   --    caches are now obsolete.
+   --
+   --  - "project_view_changed"
+   --    procedure Handler (Handle : access Kernel_Handle_Record'Class);
+   --
+   --    Emitted when the project view has been changed (for instance because
+   --    one of the environment variables has changed). This means that the
+   --    list of directories, files or switches might now be different).
+   --
+   --  </signals>
+
 private
 
-   type Kernel_Handle is record
-      Project : Prj.Project_Id;
-      --  The project currently loaded.
+   type Kernel_Handle_Record is new Glib.Object.GObject_Record with record
+      Project : Prj.Tree.Project_Node_Id := Prj.Tree.Empty_Node;
+      --  The project currently loaded. This is the tree form, independent of
+      --  the current value of the environment variables.
+
+      Project_View : Prj.Project_Id := Prj.No_Project;
+      --  The current project view. This is the same Project, after it has been
+      --  evaluated based on the current value of the environment variables.
    end record;
 
 end Glide_Kernel;
