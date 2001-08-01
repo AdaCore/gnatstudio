8e52bdecd8e442a3e486bfa4ca0e3ce3e1103e7c
Arnaud Charlet <charlet@adacore.com>
2001-08-01 17:45:38 +0000

    Add Prj.* files needed by glide.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@70422 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/kernel.ads
+++ kernel/src/kernel.ads
@@ -1,5 +1,11 @@
 --  Dummy package to provide a main file to compile that contains no
 --  dependencies
 
+pragma Warnings (Off);
+with Prj.Ext;
+with Prj.Part;
+with Prj.Proc;
+pragma Warnings (On);
+
 package Kernel is
 end Kernel;
