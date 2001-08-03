e002fa048fcd618ab036512a9c9196b1effd6d3e
Emmanuel Briot <briot@adacore.com>
2001-08-03 09:58:36 +0000

    We now need to create a glide kernel, since the wizard now requires this.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@70478 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/src/prj_wizard.adb
+++ prj_editor/src/prj_wizard.adb
@@ -1,13 +1,16 @@
 
 with Gtk.Main;     use Gtk.Main;
 with Creation_Wizard; use Creation_Wizard;
+with Glide_Kernel;  use Glide_Kernel;
 
 procedure Prj_Wizard is
    Wiz  : Creation_Wizard.Prj_Wizard;
+   Kernel : Kernel_Handle;
 begin
    Gtk.Main.Init;
+   Gtk_New (Kernel, null);
 
-   Gtk_New (Wiz);
+   Gtk_New (Wiz, Kernel);
    Set_Current_Page (Wiz, 1);
    Show_All (Wiz);
 
