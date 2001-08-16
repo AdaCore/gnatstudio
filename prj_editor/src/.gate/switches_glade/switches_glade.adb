d1e39513acbaa1bb8def3fa89558f55ce37831bb
Emmanuel Briot <briot@adacore.com>
2001-08-16 10:53:17 +0000

    Remove Variable_Editor, since this is no longer used (was merged with the  scenario editor)
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@70536 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/src/.gate/switches_glade/switches_glade.adb
+++ prj_editor/src/.gate/switches_glade/switches_glade.adb
@@ -3,13 +3,11 @@ with Gtk.Main;
 with Gtk.Widget; use Gtk.Widget;
 with Switches_Editor_Pkg; use Switches_Editor_Pkg;
 with Naming_Scheme_Editor_Pkg; use Naming_Scheme_Editor_Pkg;
-with Variable_Editor_Pkg; use Variable_Editor_Pkg;
 with New_Variable_Editor_Pkg; use New_Variable_Editor_Pkg;
 
 procedure Switches_Glade is
    Switches_Editor : Switches_Editor_Access;
    Naming_Scheme_Editor : Naming_Scheme_Editor_Access;
-   Variable_Editor : Variable_Editor_Access;
    New_Variable_Editor : New_Variable_Editor_Access;
 
 begin
@@ -19,8 +17,6 @@ begin
    Show_All (Switches_Editor);
    Gtk_New (Naming_Scheme_Editor);
    Show_All (Naming_Scheme_Editor);
-   Gtk_New (Variable_Editor);
-   Show_All (Variable_Editor);
    Gtk_New (New_Variable_Editor);
    Show_All (New_Variable_Editor);
    Gtk.Main.Main;
