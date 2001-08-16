5aaa55fdfbb13a402708f30da9b7b7098b4183dc
Joel Brobecker <brobecke@adacore.com>
2001-08-16 10:36:26 +0000

    (Find_Declaration_Or_Body): new subprogram. The implementation is still  incomplete because it only searches for the declaration at the moment.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@70531 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/src_info-queries.ads
+++ kernel/src_info/src_info-queries.ads
@@ -2,4 +2,16 @@ package Src_Info.Queries is
 
    --  provide services for all sorts of queries on the ALI files...
 
+   procedure Find_Declaration_Or_Body
+     (Lib_Info        : LI_File_Ptr;
+      File_Name       : String;
+      Entity_Name     : String;
+      Line            : Positive;
+      Column          : Positive;
+      File_Name_Found : out String_Access;
+      Start_Line      : out Positive;
+      Start_Column    : out Positive;
+      End_Line        : out Positive;
+      End_Column      : out Positive);
+
 end Src_Info.Queries;
