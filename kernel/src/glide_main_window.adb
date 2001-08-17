d7160d12c856860378be2f202d33be9c1385c0d2
Emmanuel Briot <briot@adacore.com>
2001-08-17 10:10:01 +0000

    (Set_Focus): New subprogram  This is used to give the focus to the MDI child that contains the widget  that just got the keyboard focus. Although it doesn't handle the case where  the user clicks anywhere inside a widget to give it the focus, it will handle  the source editor correctly, which is the most common case.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@70617 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/glide_main_window.adb
+++ kernel/src/glide_main_window.adb
@@ -1,7 +1,39 @@
 with Glide_Kernel; use Glide_Kernel;
 with Gtk.Window; use Gtk.Window;
 
+with Gtkada.Handlers; use Gtkada.Handlers;
+with Gtk.Widget; use Gtk.Widget;
+with Gtk.Arguments; use Gtk.Arguments;
+with Gtkada.MDI;    use Gtkada.MDI;
+with Glide_Page;    use Glide_Page;
+with GVD.Process; use GVD.Process;
+
+
 package body Glide_Main_Window is
+
+   procedure Set_Focus (Win : access Gtk_Widget_Record'Class; Args : Gtk_Args);
+   --  Called when the widget that has the focus changes. This is used so that
+   --  we can automatically change the focus in the MDI in such cases.
+
+   ---------------
+   -- Set_Focus --
+   ---------------
+
+   procedure Set_Focus
+     (Win : access Gtk_Widget_Record'Class; Args : Gtk_Args)
+   is
+      pragma Warnings (Off, Win);
+      Top  : Glide_Window := Glide_Window (Win);
+      Widget : Gtk_Widget := Gtk_Widget (To_Object (Args, 1));
+      MDI    : constant MDI_Window :=
+        Glide_Page.Glide_Page (Get_Current_Process (Top)).Process_Mdi;
+   begin
+      Set_Focus_Child (MDI, Containing => Widget);
+   end Set_Focus;
+
+   -------------
+   -- Gtk_New --
+   -------------
 
    procedure Gtk_New
      (Main_Window : out Glide_Window;
@@ -9,8 +41,12 @@ package body Glide_Main_Window is
       Menu_Items  : Gtk_Item_Factory_Entry_Array) is
    begin
       Main_Window := new Glide_Window_Record;
-      Initialize (Main_Window, Key, Menu_Items);
+      Glide_Main_Window.Initialize (Main_Window, Key, Menu_Items);
    end Gtk_New;
+
+   ----------------
+   -- Initialize --
+   ----------------
 
    procedure Initialize
      (Main_Window : access Glide_Window_Record'Class;
@@ -19,6 +55,8 @@ package body Glide_Main_Window is
    begin
       GVD.Main_Window.Initialize (Main_Window, Key, Menu_Items);
       Gtk_New (Main_Window.Kernel, Gtk_Window (Main_Window));
+
+      Widget_Callback.Connect (Main_Window, "set_focus", Set_Focus'Access);
    end Initialize;
 
 end Glide_Main_Window;
