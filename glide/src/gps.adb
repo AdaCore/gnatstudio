fdfba8d17f0804e92ce5ba7b1ba5d9e7578aba64
Arnaud Charlet <charlet@adacore.com>
2001-08-17 12:33:06 +0000

    Implement simple command line handling: open a file for each argument,  except for .gpr files (open the project instead). If no project is opened, try to open the first .gpr file in the current  directory.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@70632 936e1b1b-40f2-da11-902a-00137254ae57

--- glide/src/gps.adb
+++ glide/src/gps.adb
@@ -3,12 +3,21 @@ with Gtk.Main;
 with Glide_Page;
 with Glide_Menu;
 with Glide_Main_Window;
+with String_Utils; use String_Utils;
+with GNAT.Directory_Operations; use GNAT.Directory_Operations;
+with Glide_Kernel.Project; use Glide_Kernel.Project;
+with Glide_Kernel.Editor; use Glide_Kernel.Editor;
+with Ada.Command_Line; use Ada.Command_Line;
 
 procedure Glide2 is
    use Glide_Main_Window;
 
-   Glide : Glide_Window;
-   Page  : Glide_Page.Glide_Page;
+   Glide          : Glide_Window;
+   Page           : Glide_Page.Glide_Page;
+   Dir            : Dir_Type;
+   Str            : String (1 .. 1024);
+   Last           : Natural;
+   Project_Loaded : Boolean := False;
 
 begin
    Gtk.Main.Set_Locale;
@@ -19,8 +28,35 @@ begin
    Maximize (Glide);
    Glide.Gvd_Home_Dir := new String' ("");
    Glide.Prefix_Directory := new String' ("");
-
    Glide_Page.Gtk_New (Page, Glide);
+
+   for J in 1 .. Argument_Count loop
+      if File_Extension (Argument (J)) = "gpr" then
+         Load_Project (Glide.Kernel, Argument (J));
+         Project_Loaded := True;
+      else
+         Open_File (Glide.Kernel, Argument (J));
+      end if;
+   end loop;
+
+   --  If no project has been specified on the command line, try to open
+   --  the first one in the current directory (if any).
+
+   if not Project_Loaded then
+      Open (Dir, Get_Current_Dir);
+
+      loop
+         Read (Dir, Str, Last);
+
+         exit when Last = 0;
+
+         if File_Extension (Str (1 .. Last)) = "gpr" then
+            Load_Project (Glide.Kernel, Str (1 .. Last));
+            exit;
+         end if;
+      end loop;
+   end if;
+
    Show_All (Glide);
    Gtk.Main.Main;
 end Glide2;
