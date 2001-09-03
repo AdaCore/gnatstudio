65c1f9d31fe6cf188240df40470767472ca1da6b
Joel Brobecker <brobecke@adacore.com>
2001-09-03 08:01:43 +0000

    (Query_Status): New enumerated type. (Find_Declaration_Or_Body): Add new out parameter to help printing more  meaningful messages to the user.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@70729 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/src_info-queries.ads
+++ kernel/src_info/src_info-queries.ads
@@ -2,6 +2,14 @@ package Src_Info.Queries is
 
    --  provide services for all sorts of queries on the ALI files...
 
+   type Query_Status is
+     (Entity_Not_Found,
+      Internal_Error,
+      No_Body_Entity_Found,
+      Success);
+   --  The status returned by all query routines. Only upon success should
+   --  return values returned be taken into consideration.
+
    procedure Find_Declaration_Or_Body
      (Lib_Info        : LI_File_Ptr;
       File_Name       : String;
@@ -12,6 +20,9 @@ package Src_Info.Queries is
       Start_Line      : out Positive;
       Start_Column    : out Positive;
       End_Line        : out Positive;
-      End_Column      : out Positive);
+      End_Column      : out Positive;
+      Status          : out Query_Status);
+   --  Implement the Goto Declaration<->Body algorithm using the given
+   --  Filename, Entity_Name, and Line/Column position.
 
 end Src_Info.Queries;
