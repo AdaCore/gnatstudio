dcb56a31109759252d6e811eedc368f1bdf20936
Arnaud Charlet <charlet@adacore.com>
2001-09-10 09:24:30 +0000

    Fix header.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@70796 936e1b1b-40f2-da11-902a-00137254ae57

--- widgets/src/gtkada-file_selector.ads
+++ widgets/src/gtkada-file_selector.ads
@@ -1,8 +1,7 @@
 -----------------------------------------------------------------------
 --               GtkAda - Ada95 binding for Gtk+/Gnome               --
 --                                                                   --
---   Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet   --
---                Copyright (C) 2000-2001 ACT-Europe                 --
+--                   Copyright (C) 2001 ACT-Europe                   --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
