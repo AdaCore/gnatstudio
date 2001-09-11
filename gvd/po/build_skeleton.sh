2f9ad8ec1a8e4fb725730b35d36cf90af3c9475f
Arnaud Charlet <charlet@adacore.com>
2001-09-11 09:24:49 +0000

    Take into account sources in common.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@70816 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/po/build_skeleton.sh
+++ gvd/po/build_skeleton.sh
@@ -2,8 +2,8 @@
 # Warning: this script must be run using the C locale, otherwise sort might
 # behave differently.
 
-grep -- '-".*"' ../gvd/*.ad[sb] > skeleton.tmp
-grep -- '-(".*"' ../gvd/*.ad[sb] >> skeleton.tmp
+grep -- '-".*"' ../gvd/*.ad[sb] ../common/*.ad[sb] > skeleton.tmp
+grep -- '-(".*"' ../gvd/*.ad[sb] ../common/*.ad[sb] >> skeleton.tmp
 sed -e 's/.*\(".*"\).*/msgid  \1/g' < skeleton.tmp | sort | uniq | \
 grep -v 'msgid  " *"$' | sed -e 's/\(".*"\)/\1\
 msgstr \1\
