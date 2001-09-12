b7c61fb4a5bf59e1f4e7249f1c582bc70c26a6e1
Emmanuel Briot <briot@adacore.com>
2001-09-12 14:24:15 +0000

    Add header. The console and the explorer are now separate widgets
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@70825 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/glide_page.ads
+++ kernel/src/glide_page.ads
@@ -1,18 +1,42 @@
+-----------------------------------------------------------------------
+--                                                                   --
+--                     Copyright (C) 2001                            --
+--                          ACT-Europe                               --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
 with GVD.Process;
-with Gtk.Scrolled_Window; use Gtk.Scrolled_Window;
-with Gtk.Text; use Gtk.Text;
+with Glide_Consoles;
 with Glide_Main_Window; use Glide_Main_Window;
-with Project_Trees;
-with Scenario_Views;
+with Project_Explorers;
 
 package Glide_Page is
 
    type Glide_Page_Record is new GVD.Process.Debugger_Process_Tab_Record with
    record
-      Console_Sw : Gtk_Scrolled_Window;
-      Console    : Gtk_Text;
-      Explorer   : Project_Trees.Project_Tree;
-      Scenario   : Scenario_Views.Scenario_View;
+      Console    : Glide_Consoles.Glide_Console;
+      Explorer   : Project_Explorers.Project_Explorer;
    end record;
    type Glide_Page is access all Glide_Page_Record'Class;
 
