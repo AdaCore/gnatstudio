a4014bf52e64e7c8faa11ee13d4fefdaab4a2f31
Emmanuel Briot <briot@adacore.com>
2001-10-08 19:07:38 +0000

    (Find_File, Filter, Is_System_File): New subprogram (Open_Browser): No longer open an item by default, which is much cleaner (Examine_Dependency): New subprogram  We now also complete the partial LI files if needed before getting the list  of dependencies  Create the links between the items before inserting the item in the canvas,  so that the layout algorithms have the full information when they are run.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@71002 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/glide_kernel-browsers.adb
+++ kernel/src/glide_kernel-browsers.adb
@@ -45,6 +45,24 @@ package body Glide_Kernel.Browsers is
 
    Default_Browser_Width  : constant := 400;
    Default_Browser_Height : constant := 400;
+   --  <preference> Default size for the browsers
+
+   function Find_File
+     (In_Browser : access Glide_Browser_Record'Class; Filename : String)
+      return Canvas_Item;
+   --  Return the child that shows Filename in the browser, or null if Filename
+   --  is not already displayed in the canvas.
+
+   function Filter (Dep : Dependency) return Boolean;
+   --  A filter function that decides whether Dep should be displayed in the
+   --  canvas. It should return false if Dep should not be displayed.
+   --
+   --  ??? This obviously needs to be modifiable from the browser itself.
+
+   function Is_System_File (Source : Internal_File) return Boolean;
+   --  Return True if Source is a system file (runtime file for Ada).
+   --  ??? This should be moved to a more general location, and perhaps be
+   --  ??? implemented with support from the project files.
 
    ------------------
    -- Open_Browser --
@@ -62,13 +80,6 @@ package body Glide_Kernel.Browsers is
       Mask    : Browser_Type_Mask;
       Child   : MDI_Child;
       Browser : Glide_Browser;
-
-      Item, Initial : File_Item;
-      Link     : Dependency_Link;
-      Dep      : Dependency_File_Info_List;
-      Lib_Info : LI_File_Ptr;
-      Status   : Dependencies_Query_Status;
-      Source   : Source_File;
 
    begin
       --  Check if there is already such a browser available
@@ -92,38 +103,186 @@ package body Glide_Kernel.Browsers is
         (Browser, Default_Browser_Width, Default_Browser_Height);
       Set_Title (Child, "<browser>");
 
-      --  Put a dummy item for now
-      Lib_Info := Locate_From_Source (Kernel, "glide_kernel.adb");
-      Source := Make_Source_File (Lib_Info, "glide_kernel.adb");
+      --  Examine_Dependencies (Kernel, Browser, "glide_kernel.adb");
+      return Child;
+   end Open_Browser;
 
-      Gtk_New (Initial, Get_Window (MDI), Kernel,  Source);
-      Put (Get_Canvas (Browser), Initial);
+   --------------------------
+   -- Examine_Dependencies --
+   --------------------------
 
-      Find_Dependencies (Lib_Info, Dep, Status);
+   procedure Examine_Dependencies
+     (Kernel       : access Kernel_Handle_Record'Class;
+      In_Browser   : access Glide_Browser_Record'Class;
+      File         : String)
+   is
+      Item, Initial : File_Item;
+      Link          : Dependency_Link;
+      Dep, List     : Dependency_List;
+      Lib_Info      : LI_File_Ptr;
+      Status        : Dependencies_Query_Status;
+      Intern        : Internal_File;
+      Parse_Success : Boolean;
+      New_Item      : Boolean;
+
+   begin
+      Lib_Info := Locate_From_Source (Kernel, File);
+      if Lib_Info = No_LI_File then
+         --  ??? Should be displayed in the status bar.
+
+         --  ??? Should we put an item in the browser anyway, even if nothing
+         --  ??? can be done with it anyway ? This might give more indication
+         --  ??? to the user that we don't know anything about that file.
+         return;
+      end if;
+
+      if Is_Incomplete (Lib_Info) then
+         declare
+            LI_Name : constant String :=
+              Find_Object_File (Kernel, Get_Li_Filename (Lib_Info));
+         begin
+            --  ??? Should we have another version of Parse_ALI_File that takes
+            --  ??? directly a LI_File_Ptr that needs to be completed.
+            Parse_ALI_File
+              (Handle       => Kernel,
+               ALI_Filename => LI_Name,
+               Unit         => Lib_Info,
+               Success      => Parse_Success);
+
+            if not Parse_Success then
+               return;
+            end if;
+         end;
+      end if;
+
+      Initial := File_Item (Find_File (In_Browser, File));
+      if Initial = null then
+         Gtk_New (Initial, Get_Window (In_Browser), Kernel,  File);
+         Put (Get_Canvas (In_Browser), Initial);
+
+         --  ??? Should check if the item was already expanded, so as to avoid
+         --  ??? useless work.
+      end if;
+
+      Find_Dependencies (Lib_Info, List, Status);
 
       if Status = Success then
-         while Dep /= No_Dependencies loop
-            --  Only show explicit dependencies, not implicit ones
-            if Get_Depends_From_Spec (Dependency_Information (Dep))
-              or else Get_Depends_From_Body (Dependency_Information (Dep))
-            then
-               Gtk_New (Item, Get_Window (MDI), Kernel,  File_Information (Dep));
-               Put (Get_Canvas (Browser), Item);
+         Dep := List;
+         while Dep /= null loop
+            if Filter (Dep.Value) then
+               Intern := File_Information (Dep.Value);
+               Item := File_Item
+                 (Find_File (In_Browser, Get_Source_Filename (Intern)));
+               New_Item := Item = null;
+               if New_Item then
+                  Gtk_New (Item, Get_Window (In_Browser), Kernel, Intern);
+               end if;
 
-               Gtk_New (Link, Dependency_Information (Dep));
-               Add_Link (Get_Canvas (Browser),
+               Gtk_New (Link, Dependency_Information (Dep.Value));
+               Add_Link (Get_Canvas (In_Browser),
                          Link => Link,
                          Src  => Initial,
                          Dest => Item);
+
+               if New_Item then
+                  Put (Get_Canvas (In_Browser), Item);
+               end if;
             end if;
 
-            Dep := Next (Dep);
+            Dep := Dep.Next;
          end loop;
 
-         Refresh_Canvas (Get_Canvas (Browser));
+         Destroy (List);
+         Refresh_Canvas (Get_Canvas (In_Browser));
       end if;
+   end Examine_Dependencies;
 
-      return Child;
-   end Open_Browser;
+   --------------------
+   -- Is_System_File --
+   --------------------
+
+   function Is_System_File (Source : Internal_File) return Boolean is
+      Name : constant String := Get_Source_Filename (Source);
+      Ada_Runtime_File, Gtk_System_File : Boolean;
+   begin
+      --  ??? The implementation here is two GNAT specific. However, getting
+      --  ??? the Unit_Name would be expensive.
+      Ada_Runtime_File := Name'Length > 2
+        and then (Name (Name'First .. Name'First + 1) = "a-"
+                  or else Name (Name'First .. Name'First + 1) = "s-"
+                  or else Name (Name'First .. Name'First + 1) = "i-"
+                  or else Name (Name'First .. Name'First + 1) = "g-");
+
+      Ada_Runtime_File := Ada_Runtime_File
+        or else Name = "ada.ads"
+        or else Name = "interfaces.ads"
+        or else Name = "system.ads"
+        or else Name = "gnat.ads";
+
+      Gtk_System_File := Name'Length > 4
+        and then (Name (Name'First .. Name'First + 2) = "gtk"
+                  or else Name (Name'First .. Name'First + 3) = "glib"
+                  or else Name (Name'First .. Name'First + 2) = "gdk");
+
+      return Ada_Runtime_File or else Gtk_System_File;
+   end Is_System_File;
+
+   ------------
+   -- Filter --
+   ------------
+
+   function Filter (Dep : Dependency) return Boolean is
+      Explicit_Dependency : Boolean;
+      Info : constant Dependency_Info := Dependency_Information (Dep);
+   begin
+      --  ??? This must be configurable at the GUI level.
+
+      --  Only show explicit dependencies, not implicit ones
+      Explicit_Dependency :=
+        Get_Depends_From_Spec (Info) or else Get_Depends_From_Body (Info);
+
+      --  Do not display dependencies on runtime files
+      return Explicit_Dependency
+        and then not Is_System_File (File_Information (Dep));
+   end Filter;
+
+   ---------------
+   -- Find_File --
+   ---------------
+
+   function Find_File
+     (In_Browser : access Glide_Browser_Record'Class; Filename : String)
+      return Canvas_Item
+   is
+      Found : Canvas_Item := null;
+
+      function Check_Item
+        (Canvas : access Interactive_Canvas_Record'Class;
+         Item   : access Canvas_Item_Record'Class) return Boolean;
+      --  Check whether Item contains File
+
+      ----------------
+      -- Check_Item --
+      ----------------
+
+      function Check_Item
+        (Canvas : access Interactive_Canvas_Record'Class;
+         Item   : access Canvas_Item_Record'Class) return Boolean is
+      begin
+         if Item.all in File_Item_Record'Class
+           and then Get_Source_Filename (Get_Source (File_Item (Item))) =
+           Filename
+         then
+            Found := Canvas_Item (Item);
+            return False;
+         end if;
+         return True;
+      end Check_Item;
+
+   begin
+      For_Each_Item (Get_Canvas (In_Browser), Check_Item'Unrestricted_Access);
+      return Found;
+   end Find_File;
+
 
 end Glide_Kernel.Browsers;
