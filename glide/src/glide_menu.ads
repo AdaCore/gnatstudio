6a20070caf15a3d1c7cafa2008620dc23a5017b3
Arnaud Charlet <charlet@adacore.com>
2001-10-23 13:10:12 +0000

    Add proper header.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@71088 936e1b1b-40f2-da11-902a-00137254ae57

--- glide/src/glide_menu.ads
+++ glide/src/glide_menu.ads
@@ -1,8 +1,8 @@
 -----------------------------------------------------------------------
---                   GVD - The GNU Visual Debugger                   --
+--                          G L I D E  I I                           --
 --                                                                   --
---                      Copyright (C) 2000-2001                      --
---                              ACT-Europe                           --
+--                        Copyright (C) 2001                         --
+--                            ACT-Europe                             --
 --                                                                   --
 -- GVD is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
