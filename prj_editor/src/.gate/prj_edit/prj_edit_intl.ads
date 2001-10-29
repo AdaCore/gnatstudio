9ce79eef7433831cec7398517a90acf50938842b
Arnaud Charlet <charlet@adacore.com>
2001-10-29 10:34:23 +0000

    Removed.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@71161 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/src/.gate/prj_edit/prj_edit_intl.ads
+++
@@ -1,6 +0,0 @@
-package Prj_Edit_Intl is
-
-   function "-" (Msg : String) return String;
-   --  Convenient shortcut to the Gettext function.
-
-end Prj_Edit_Intl;
--- prj_editor/src/.gate/switches_glade/switches_glade_intl.ads
+++
@@ -1,6 +0,0 @@
-package Switches_Glade_Intl is
-
-   function "-" (Msg : String) return String;
-   --  Convenient shortcut to the Gettext function.
-
-end Switches_Glade_Intl;
