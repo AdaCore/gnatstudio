602077bafeffa3441a776b67b6c97ee02d52c4f1
Nicolas Setton <setton@adacore.com>
2001-11-23 16:46:38 +0000

    Initial revision.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@71308 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ vcs/src/vcs_view_pixmaps.adb
@@ -0,0 +1,23 @@
+package body Vcs_View_Pixmaps is
+
+   procedure Init_Graphics is
+   begin
+      if Status_Unknown_Pixbuf = Null_Pixbuf then
+         Status_Unknown_Pixbuf
+           := Gdk_New_From_Xpm_Data (Unknown_Xpm);
+         Status_Up_To_Date_Pixbuf
+           := Gdk_New_From_Xpm_Data (Up_To_Date_Xpm);
+         Status_Modified_Pixbuf
+           := Gdk_New_From_Xpm_Data (Modified_Xpm);
+         Status_Needs_Merge_Pixbuf
+           := Gdk_New_From_Xpm_Data (Needs_Merge_Xpm);
+         Status_Needs_Update_Pixbuf
+           := Gdk_New_From_Xpm_Data (Needs_Update_Xpm);
+         Status_Removed_Pixbuf
+           := Gdk_New_From_Xpm_Data (Removed_Xpm);
+         Status_Not_Registered_Pixbuf
+           := Gdk_New_From_Xpm_Data (Not_Registered_Xpm);
+      end if;
+   end Init_Graphics;
+
+end Vcs_View_Pixmaps;
---
+++ vcs/src/vcs_view_pixmaps.ads
@@ -0,0 +1,41 @@
+with Glib; use Glib;
+with Gdk.Pixbuf;      use Gdk.Pixbuf;
+with Basic_Types; use Basic_Types;
+
+package Vcs_View_Pixmaps is
+
+   Status_Unknown_Pixbuf        : Gdk_Pixbuf := Null_Pixbuf;
+   Status_Not_Registered_Pixbuf : Gdk_Pixbuf := Null_Pixbuf;
+   Status_Up_To_Date_Pixbuf     : Gdk_Pixbuf := Null_Pixbuf;
+   Status_Removed_Pixbuf        : Gdk_Pixbuf := Null_Pixbuf;
+   Status_Modified_Pixbuf       : Gdk_Pixbuf := Null_Pixbuf;
+   Status_Needs_Merge_Pixbuf    : Gdk_Pixbuf := Null_Pixbuf;
+   Status_Needs_Update_Pixbuf   : Gdk_Pixbuf := Null_Pixbuf;
+
+   procedure Init_Graphics;
+   --  Initialize the pixbufs.
+
+private
+
+   Unknown_Xpm : aliased Pixmap_Array;
+   pragma Import (C, Unknown_Xpm, "unknown_xpm");
+
+   Removed_Xpm : aliased Pixmap_Array;
+   pragma Import (C, Removed_Xpm, "unknown_xpm");
+
+   Not_Registered_Xpm : aliased Pixmap_Array;
+   pragma Import (C, Not_Registered_Xpm, "unknown_xpm");
+
+   Modified_Xpm : aliased Pixmap_Array;
+   pragma Import (C, Modified_Xpm, "modified_xpm");
+
+   Up_To_Date_Xpm : aliased Pixmap_Array;
+   pragma Import (C, Up_To_Date_Xpm, "up_to_date_xpm");
+
+   Needs_Merge_Xpm : aliased Pixmap_Array;
+   pragma Import (C, Needs_Merge_Xpm, "needs_merge_xpm");
+
+   Needs_Update_Xpm : aliased Pixmap_Array;
+   pragma Import (C, Needs_Update_Xpm, "needs_update_xpm");
+
+end Vcs_View_Pixmaps;
