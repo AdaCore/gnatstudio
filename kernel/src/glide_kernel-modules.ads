7dc9060be1efecc5486fe3f667811d38b0f5d12f
Emmanuel Briot <briot@adacore.com>
2001-11-26 09:56:14 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@71310 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ kernel/src/glide_kernel-modules.ads
@@ -0,0 +1,253 @@
+-----------------------------------------------------------------------
+--                                                                   --
+--                     Copyright (C) 2001                            --
+--                          ACT-Europe                               --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
+--  General description of modules
+--  ==============================
+--
+--  This package contains all the subprograms needed to register new modules in
+--  Glide.
+--
+--  All the functionalities provided in Glide are organized into modules. Each
+--  of this module can do the following:
+--     - Add new menus to the standard Glide menu bar
+--     - Add new icons in the Glide toolbar
+--     - Add new entries in the contextual menus.
+--     - Associate any of the previous with specific callbacks, and insert new
+--       widgets in the Glide MDI
+--
+--  The modules should only interact with each other through the Glide kernel,
+--  never directly. This provides more flexibility, as well as room for future
+--  extensions like dynamic modules.
+--
+--  Yhe default modules provided in Glide (source editor, project editor,...)
+--  are more closely integrated into the kernel than other external
+--  modules. However, even these should ideally be fully replaceable with minor
+--  source modification (for instance if one wants to override the default
+--  source editor).
+--
+--  Each module is associated with a unique name. The names for the default
+--  Glide modules are provided as constants in this package, so that it is easy
+--  to check whether an action was initiated by one module or another.
+--
+--  Registering modules
+--  ===================
+--
+--  All the modules must be registered with the kernel before they can do
+--  anything. This is typically in the elaboration part of a package, by
+--  calling Register_Module.
+--
+--  Once the kernel has been created, it will call any initialization
+--  function you have provided. This function might for instance be used to
+--  register new menu for the menu bar, or new icons in the toolbar.
+--
+--  This mechanism allows the kernel to be completly independeny of the
+--  specific modules, since it doesn't need to do in advance the exact list of
+--  modules.
+--
+--  Contextual menus
+--  ================
+--
+--   Here is a description of the sequence of events used to display contextual
+--   menus in Glide:
+--      - Each object that should have a contextual menu should call
+--        Register_Contextual_Menu. The kernel will automatically setup
+--        appropriate gtk callbacks.
+--      - Whenever the user presses the right mouse button, the kernel will ask
+--        the object to report the context in which the event occured (name of
+--        selected file, selected project,...).
+--      - Each of the registered module then has the opportunity to add entries
+--        in the contextual menu, based on this context.
+--      - The menu is displayed, and the callback for the selected menu item
+--        will be called as usual.
+--      - The menu is automatically destroyed, and the context freed, when the
+--        action has finished executing.
+
+with Gdk.Event;
+with Glib.Object;
+with Gtk.Handlers;
+with Gtk.Menu_Item;
+with Gtk.Widget;
+with Prj;
+
+package Glide_Kernel.Modules is
+
+   Explorer_Module_Name       : constant String := "Explorer";
+   Project_Editor_Module_Name : constant String := "Project_Editor";
+   Aunit_Module_Name          : constant String := "Unit_Testing";
+   --  Names for the internal modules
+
+   -----------
+   -- Types --
+   -----------
+   --  See also the types defined in glide_kernel.ads
+
+   package Context_Callback is new Gtk.Handlers.User_Callback
+     (Gtk.Widget.Gtk_Widget_Record, Selection_Context_Access);
+
+   -------------------------
+   -- Module manipulation --
+   -------------------------
+
+   function Register_Module
+     (Module_Name             : String;
+      Priority                : Module_Priority := Default_Priority;
+      Initializer             : Module_Initializer  := null;
+      Contextual_Menu_Handler : Module_Menu_Handler := null)
+      return Module_ID;
+   --  Register a new module into Glide.
+   --  Module_Name can be used by other modules to check whether they want to
+   --  interact with this module.
+   --  See the general description for this package for explanation on
+   --  Initializer and Contextual_Menu_Handler.
+
+   function Module_Name (ID : Module_ID) return String;
+   --  Return the name of the module registered as ID.
+
+   ----------------------
+   -- COntextual menus --
+   ----------------------
+
+   type Context_Factory is access function
+     (Kernel       : access Kernel_Handle_Record'Class;
+      Event_Widget : access Gtk.Widget.Gtk_Widget_Record'Class;
+      Object       : access Glib.Object.GObject_Record'Class;
+      Event        : Gdk.Event.Gdk_Event;
+      Menu         : Gtk.Menu.Gtk_Menu) return Selection_Context_Access;
+   --  This function should return the context associated with the contextual
+   --  menu, when the mouse event Event happened on Widget.
+   --  The mouse event occured in Event_Widget, and the contextual menu was
+   --  registered for Object
+   --  The object should also add its default entries into the menu, so that
+   --  they always appear first in the menu.
+   --
+   --  If null is returned, no contextual menu will be displayed.
+   --
+   --  The kernel is automatically set in the context.
+
+   procedure Register_Contextual_Menu
+     (Kernel          : access Kernel_Handle_Record'Class;
+      Event_On_Widget : access Gtk.Widget.Gtk_Widget_Record'Class;
+      Object          : access Glib.Object.GObject_Record'Class;
+      ID              : Module_ID;
+      Context_Func    : Context_Factory);
+   --  Register that Widget should be associated with a contextual menu.
+   --  Whenever a right-button click happens inside Widget_On_Widget, then the
+   --  following will happen:
+   --     - the kernel detects the event, and creates an empty menu.
+   --     - it asks Widget, through Context_Func, the exact context for the
+   --       menu (selected file, ....)
+   --     - it then asks each of the registered modules whether it wants to
+   --       add new items to the menu, and let is do so (through the
+   --       Contextual_Menu_Handler provided in Register_Module)
+   --     - it then displays the menu
+   --     - it finally cleans up the memory when the menu is hidden
+
+   -----------
+   -- Menus --
+   -----------
+
+   procedure Register_Menu
+     (Kernel      : access Kernel_Handle_Record'Class;
+      Parent_Path : String;
+      Item        : Gtk.Menu_Item.Gtk_Menu_Item := null;
+      Ref_Item    : String := "";
+      Add_Before  : Boolean := True);
+   --  Add new menu items to the menu bar, as a child of Parent_Path.
+   --  Parent_Path should have a form like "/main_main/submenu".
+   --  Menus will be created if they don't exist.
+   --  This must be an absolute path, starting with '/'.
+   --
+   --  Item might be null, in which case only the parent menu items are
+   --  created, and Add_Before applies to the deepest one instead of Item.
+   --
+   --  The new item is inserted either:
+   --    - before Ref_Item if the latter is not the empty string and Add_Before
+   --      is true
+   --    - after Ref_Item if the latter is not the empty string and Add_Before
+   --      is false
+   --    - at the end of the menu
+
+   -------------------
+   -- File Contexts --
+   -------------------
+
+   type File_Selection_Context is new Selection_Context with private;
+   type File_Selection_Context_Access is access all File_Selection_Context;
+   --  This type contains all the information about the selection in any
+   --  module. Note that this is a tagged type, so that it can easily be
+   --  extended for modules external to Glide
+
+   procedure Set_File_Information
+     (Context : access File_Selection_Context;
+      Project_View : Prj.Project_Id;
+      Directory : String := "";
+      File_Name : String := "");
+   --  Set the information in Context.
+
+   function Has_Project_Information (Context : access File_Selection_Context)
+      return Boolean;
+   --  True if the context has information about a selected project.
+
+   function Project_Information (Context : access File_Selection_Context)
+      return Prj.Project_Id;
+   --  Return the id of the selected project. This can be No_Project is there
+   --  wasn't any information about a specific project
+
+   function Has_Directory_Information (Context : access File_Selection_Context)
+      return Boolean;
+   --  True if the context has information about a selected directory.
+
+   function Directory_Information (Context : access File_Selection_Context)
+      return String;
+   --  Return the information about the selected project. This is only relevant
+   --  if Has_Directory_Information is True.
+
+   function Has_File_Information (Context : access File_Selection_Context)
+      return Boolean;
+   --  True if the context has information about a selected file.
+
+   function File_Information (Context : access File_Selection_Context)
+      return String;
+   --  Return the information about the selected project. This is only relevant
+   --  if Has_File_Information is True.
+
+   procedure Destroy (Context : in out File_Selection_Context);
+   --  Free the memory associated with the context
+
+private
+
+   type File_Selection_Context is new Selection_Context with record
+      Project_View : Prj.Project_Id            := Prj.No_Project;
+      Directory    : GNAT.OS_Lib.String_Access := null;
+      File_Name    : GNAT.OS_Lib.String_Access := null;
+   end record;
+
+   pragma Inline (Has_Project_Information,
+                  Has_Directory_Information,
+                  Has_File_Information);
+end Glide_Kernel.Modules;
