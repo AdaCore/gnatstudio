e970e93ee97244406beb7f7446e0daec1edfb77a
Joel Brobecker <brobecke@adacore.com>
2001-11-28 14:01:11 +0000

    (Parse_ALI_Filename): add new parameter Extra_Object_Path. Rename  parameter Source_Path into Extra_Object_Path to make it clearer that  the Extra_Source/Object_Path is searched in addition to the paths  define by the project. Remove incorrect comment: We defined this procedure in child package ALI  because it is specific to ALI files.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@71437 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/src_info-ali.ads
+++ kernel/src_info/src_info-ali.ads
@@ -23,18 +23,16 @@ with Prj;
 package Src_Info.ALI is
 
    procedure Parse_ALI_File
-      (ALI_Filename : String;
-       Project      : Prj.Project_Id;
-       Source_Path  : String;
-       List         : in out LI_File_List;
-       Unit         : out LI_File_Ptr;
-       Success      : out Boolean);
+      (ALI_Filename      : String;
+       Project           : Prj.Project_Id;
+       Extra_Source_Path : String;
+       Extra_Object_Path : String;
+       List              : in out LI_File_List;
+       Unit              : out LI_File_Ptr;
+       Success           : out Boolean);
    --  Parse the given ALI file and update the Unit Info list. Also returns
    --  a handle to the Unit Info corresponding to this ALI file to avoid
    --  searching right-back for this handle for immediate queries.
-   --
-   --  Defined in a child package instead of the Src_Info package itself,
-   --  mainly because the implementation of such subprograms can be lengthy.
 
    function ALI_Filename_From_Source
      (Source_Filename : String;
