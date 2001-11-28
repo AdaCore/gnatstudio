1bc5ea9595593539281185aea2859eec0f31969f
Joel Brobecker <brobecke@adacore.com>
2001-11-28 15:50:56 +0000

    (Parse_ALI_File): Rename parameters Extra_Source/Object_Path into  Predefined_Source/Object_Path. (Locate_From_Source): Likewise. (ALI_Filename_From_Source): Rename parameter Source_Path into  Predefined_Source_Path. Update comments accordingly.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@71468 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/src_info-ali.ads
+++ kernel/src_info/src_info-ali.ads
@@ -23,26 +23,27 @@ with Prj;
 package Src_Info.ALI is
 
    procedure Parse_ALI_File
-      (ALI_Filename      : String;
-       Project           : Prj.Project_Id;
-       Extra_Source_Path : String;
-       Extra_Object_Path : String;
-       List              : in out LI_File_List;
-       Unit              : out LI_File_Ptr;
-       Success           : out Boolean);
+      (ALI_Filename           : String;
+       Project                : Prj.Project_Id;
+       Predefined_Source_Path : String;
+       Predefined_Object_Path : String;
+       List                   : in out LI_File_List;
+       Unit                   : out LI_File_Ptr;
+       Success                : out Boolean);
    --  Parse the given ALI file and update the Unit Info list. Also returns
    --  a handle to the Unit Info corresponding to this ALI file to avoid
    --  searching right-back for this handle for immediate queries.
 
    function ALI_Filename_From_Source
-     (Source_Filename : String;
-      Project         : Prj.Project_Id;
-      Source_Path     : String)
+     (Source_Filename        : String;
+      Project                : Prj.Project_Id;
+      Predefined_Source_Path : String)
       return String;
    --  Converts the given Source Filename into the corresponding ALI filename
-   --  using the Project and Source Path information. Return the empty string
-   --  when the given Source_Filename can not be found in the project exception
-   --  lists and when the extension does not follow the project naming scheme.
+   --  using the Project and Predefined Source Path information. Return the
+   --  empty string when the given Source_Filename can not be found in the
+   --  project exception lists and when the extension does not follow the
+   --  project naming scheme.
    --
    --  Side_Effects:
    --  This function destroys the contents of the Namet buffer. Save it before
@@ -61,12 +62,12 @@ package Src_Info.ALI is
    --  ??? to get the associated ALI filename.
 
    procedure Locate_From_Source
-     (List              : in out LI_File_List;
-      Source_Filename   : String;
-      Project           : Prj.Project_Id;
-      Extra_Source_Path : String;
-      Extra_Object_Path : String;
-      File              : out LI_File_Ptr);
+     (List                   : in out LI_File_List;
+      Source_Filename        : String;
+      Project                : Prj.Project_Id;
+      Predefined_Source_Path : String;
+      Predefined_Object_Path : String;
+      File                   : out LI_File_Ptr);
    --  Return a pointer to the LI_File which has a source file named
    --  Source_Filename. Return No_LI_File if not found.
    --  Source_Filename should be a basename (no directory).
