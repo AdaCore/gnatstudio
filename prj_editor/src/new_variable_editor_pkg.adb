dcf4df795a3e67115e4e8353fcfed10e95a4048c
Arnaud Charlet <charlet@adacore.com>
2001-12-10 13:00:58 +0000

    Use Glide_Intl. Fixes compilation failure.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@71829 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/src/new_variable_editor_pkg.adb
+++ prj_editor/src/new_variable_editor_pkg.adb
@@ -25,7 +25,7 @@ with Gtk.Widget;      use Gtk.Widget;
 with Gtk.Enums;       use Gtk.Enums;
 with Gtkada.Handlers; use Gtkada.Handlers;
 with Callbacks_Switches_Glade; use Callbacks_Switches_Glade;
-with Switches_Glade_Intl; use Switches_Glade_Intl;
+with Glide_Intl; use Glide_Intl;
 
 package body New_Variable_Editor_Pkg is
 
