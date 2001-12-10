37e9263eacc3948e9802ba647267e90b31532475
Arnaud Charlet <charlet@adacore.com>
2001-12-10 10:07:15 +0000

    Moved from the GVD repository. This version is not a dummy but a functional implementation.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@71740 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ kernel/src/gvd-text_box-source_editor-glide.adb
@@ -0,0 +1,188 @@
+-----------------------------------------------------------------------
+--                          G L I D E  I I                           --
+--                                                                   --
+--                        Copyright (C) 2001                         --
+--                            ACT-Europe                             --
+--                                                                   --
+-- GLIDE is free software; you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Gtk.Container;        use Gtk.Container;
+with Basic_Types;          use Basic_Types;
+with Glide_Kernel;         use Glide_Kernel;
+with Glide_Kernel.Console; use Glide_Kernel.Console;
+with Glide_Kernel.Editor;  use Glide_Kernel.Editor;
+with Glide_Main_Window;    use Glide_Main_Window;
+
+package body GVD.Text_Box.Source_Editor.Glide is
+
+   ------------
+   -- Attach --
+   ------------
+
+   procedure Attach
+     (Editor : access GEdit_Record;
+      Parent : access Gtk.Container.Gtk_Container_Record'Class) is
+   begin
+      --  Nothing needed within Glide
+      null;
+   end Attach;
+
+   ------------
+   -- Detach --
+   ------------
+
+   procedure Detach (Editor : access GEdit_Record) is
+   begin
+      --  Nothing needed within Glide
+      null;
+   end Detach;
+
+   --------------
+   -- Get_Line --
+   --------------
+
+   function Get_Line (Editor : access GEdit_Record) return Natural is
+   begin
+      return Editor.Line;
+   end Get_Line;
+
+   -------------
+   -- Gtk_New --
+   -------------
+
+   procedure Gtk_New
+     (Editor : out GEdit;
+      Window : access GVD.Main_Window.GVD_Main_Window_Record'Class) is
+   begin
+      Editor := new GEdit_Record;
+      Initialize (Editor, Window);
+   end Gtk_New;
+
+   ----------------------------
+   -- Highlight_Current_Line --
+   ----------------------------
+
+   procedure Highlight_Current_Line (Editor : access GEdit_Record) is
+      Success : Boolean;
+      Kernel  : constant Kernel_Handle := Glide_Window (Editor.Window).Kernel;
+
+   begin
+      pragma Assert (Editor.Current_File /= null);
+      Go_To (Kernel, Editor.Current_File.all,
+             Editor.Line, 1, Success => Success);
+   end Highlight_Current_Line;
+
+   --------------------
+   -- Highlight_Word --
+   --------------------
+
+   procedure Highlight_Word
+     (Editor   : access GEdit_Record;
+      Line     : Natural;
+      Column   : Natural;
+      Position : Position_Type) is
+   begin
+      --  Only needed by the GVD explorer, which is disabled within Glide
+      null;
+   end Highlight_Word;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize
+     (Editor : access GEdit_Record'Class;
+      Window : access GVD.Main_Window.GVD_Main_Window_Record'Class) is
+   begin
+      Editor.Window := GVD.Main_Window.GVD_Main_Window (Window);
+   end Initialize;
+
+   ---------------
+   -- Load_File --
+   ---------------
+
+   procedure Load_File
+     (Editor      : access GEdit_Record;
+      File_Name   : String;
+      Set_Current : Boolean := True;
+      Force       : Boolean := False)
+   is
+      Success : Boolean;
+      Kernel  : constant Kernel_Handle := Glide_Window (Editor.Window).Kernel;
+
+   begin
+      Free (Editor.Current_File);
+      Editor.Current_File := new String' (File_Name);
+      Open_File (Kernel, Editor.Current_File.all, Success);
+   end Load_File;
+
+   -------------------------
+   -- Preferences_Changed --
+   -------------------------
+
+   procedure Preferences_Changed (Editor : access GEdit_Record) is
+   begin
+      null;
+   end Preferences_Changed;
+
+   --------------
+   -- Set_Line --
+   --------------
+
+   procedure Set_Line
+     (Editor      : access GEdit_Record;
+      Line        : Natural;
+      Set_Current : Boolean := True)
+   is
+      Success : Boolean;
+      Kernel  : constant Kernel_Handle := Glide_Window (Editor.Window).Kernel;
+
+   begin
+      Editor.Line := Line;
+
+      pragma Assert (Editor.Current_File /= null);
+
+      Go_To (Kernel, Editor.Current_File.all,
+             Editor.Line, 1, Success => Success);
+   end Set_Line;
+
+   ------------------
+   -- Show_Message --
+   ------------------
+
+   procedure Show_Message
+     (Editor  : access GEdit_Record;
+      Message : String)
+   is
+      Kernel  : constant Kernel_Handle := Glide_Window (Editor.Window).Kernel;
+   begin
+      Free (Editor.Current_File);
+      Editor.Current_File := new String' ("");
+      Console.Insert (Kernel, Message);
+   end Show_Message;
+
+   ------------------------
+   -- Update_Breakpoints --
+   ------------------------
+
+   procedure Update_Breakpoints
+     (Editor : access GEdit_Record;
+      Br     : GVD.Types.Breakpoint_Array) is
+   begin
+      --  ???
+      null;
+   end Update_Breakpoints;
+
+end GVD.Text_Box.Source_Editor.Glide;
---
+++ kernel/src/gvd-text_box-source_editor-glide.ads
@@ -0,0 +1,99 @@
+-----------------------------------------------------------------------
+--                          G L I D E  I I                           --
+--                                                                   --
+--                        Copyright (C) 2001                         --
+--                            ACT-Europe                             --
+--                                                                   --
+-- GLIDE is free software; you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Gtk.Container;
+with Basic_Types;
+with GVD.Types;
+with GVD.Main_Window;
+
+--  This package provides an implementation of Source_Editor based on
+--  the Glide Editor.
+
+package GVD.Text_Box.Source_Editor.Glide is
+
+   type GEdit_Record is new Source_Editor_Record with private;
+   type GEdit is access all GEdit_Record'Class;
+
+   procedure Gtk_New
+     (Editor : out GEdit;
+      Window : access GVD.Main_Window.GVD_Main_Window_Record'Class);
+   --  Create a new source editor.
+
+   procedure Initialize
+     (Editor : access GEdit_Record'Class;
+      Window : access GVD.Main_Window.GVD_Main_Window_Record'Class);
+   --  Internal initialization procedure.
+
+   procedure Attach
+     (Editor : access GEdit_Record;
+      Parent : access Gtk.Container.Gtk_Container_Record'Class);
+   --  See GVD.Text_Box.Source_Editor for more information.
+
+   procedure Detach (Editor : access GEdit_Record);
+   --  See GVD.Text_Box.Source_Editor for more information.
+
+   procedure Highlight_Word
+     (Editor   : access GEdit_Record;
+      Line     : Natural;
+      Column   : Natural;
+      Position : Basic_Types.Position_Type);
+   --  See GVD.Text_Box.Source_Editor for more information.
+
+   procedure Update_Breakpoints
+     (Editor : access GEdit_Record;
+      Br     : GVD.Types.Breakpoint_Array);
+   --  See GVD.Text_Box.Source_Editor for more information.
+
+   procedure Show_Message
+     (Editor      : access GEdit_Record;
+      Message     : String);
+   --  Display a message in the editor.
+   --  This deletes the currently displayed file.
+
+   procedure Load_File
+     (Editor      : access GEdit_Record;
+      File_Name   : String;
+      Set_Current : Boolean := True;
+      Force       : Boolean := False);
+   --  See GVD.Text_Box.Source_Editor for more information.
+
+   procedure Highlight_Current_Line (Editor : access GEdit_Record);
+   --  See GVD.Text_Box.Source_Editor for more information.
+
+   procedure Preferences_Changed (Editor : access GEdit_Record);
+   --  See GVD.Text_Box.Source_Editor for more information.
+
+   procedure Set_Line
+     (Editor      : access GEdit_Record;
+      Line        : Natural;
+      Set_Current : Boolean := True);
+   --  See GVD.Text_Box.Source_Editor for more information.
+
+   function Get_Line (Editor : access GEdit_Record) return Natural;
+   --  See GVD.Text_Box.Source_Editor for more information.
+
+private
+
+   type GEdit_Record is new Source_Editor_Record with record
+      Window : GVD.Main_Window.GVD_Main_Window;
+      Line   : Natural := 0;
+   end record;
+
+end GVD.Text_Box.Source_Editor.Glide;
