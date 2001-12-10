7bcbf88a5259579b18c773695ef25de748e1f457
Emmanuel Briot <briot@adacore.com>
2001-12-10 10:52:23 +0000

    Initial revision.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@71767 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ widgets/src/gui_support.adb
@@ -0,0 +1,121 @@
+-----------------------------------------------------------------------
+--                          G L I D E  I I                           --
+--                                                                   --
+--                        Copyright (C) 2001                         --
+--                            ACT-Europe                             --
+--                                                                   --
+-- GLIDE is free software; you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Glib.Object;               use Glib.Object;
+with Glib.Values;               use Glib.Values;
+with Gtk.Handlers;
+with Gtk.Cell_Renderer_Toggle;  use Gtk.Cell_Renderer_Toggle;
+with Gtk.Tree_Store;            use Gtk.Tree_Store;
+with Gtk.Tree_Model;            use Gtk.Tree_Model;
+
+package body Gui_Support is
+
+   procedure Radio_Callback
+     (Model  : access GObject_Record'Class;
+      Params : Glib.Values.GValues;
+      Data   : Glib.Gint);
+   --  Callback for the toggle renderer
+
+   procedure Edited_Callback
+     (Model  : access GObject_Record'Class;
+      Params : Glib.Values.GValues;
+      Data   : Glib.Gint);
+   --  Callback for the editable renderer
+
+   ----------------------------
+   -- Set_Radio_And_Callback --
+   ----------------------------
+
+   procedure Set_Radio_And_Callback
+     (Model    : access Gtk.Tree_Store.Gtk_Tree_Store_Record'Class;
+      Renderer : access Gtk_Cell_Renderer_Toggle_Record'Class;
+      Column   : Glib.Gint) is
+   begin
+      Set_Radio (Renderer, True);
+
+      Tree_Model_Callback.Object_Connect
+        (Renderer, "toggled",
+         Radio_Callback'Access, Slot_Object => Model, User_Data => Column);
+   end Set_Radio_And_Callback;
+
+   --------------------
+   -- Radio_Callback --
+   --------------------
+
+   procedure Radio_Callback
+     (Model  : access GObject_Record'Class;
+      Params : Glib.Values.GValues;
+      Data   : Glib.Gint)
+   is
+      M           : Gtk_Tree_Store := Gtk_Tree_Store (Model);
+      Iter, Tmp   : Gtk_Tree_Iter;
+      Path_String : String := Get_String (Nth (Params, 1));
+
+   begin
+      Iter := Get_Iter_From_String (M, Path_String);
+      if Iter /= Null_Iter then
+         --  Can't click on an already active item, for a radio renderer, since
+         --  we want at least one selected item
+         if not Boolean_Get (M, Iter, Data) then
+            Tmp := Get_Iter_Root (M);
+            while Tmp /= Null_Iter loop
+               Set (M, Tmp, Data, False);
+               Next (M, Tmp);
+            end loop;
+
+            Set (M, Iter, Data, True);
+         end if;
+      end if;
+   end Radio_Callback;
+
+   -------------------------------
+   -- Set_Editable_And_Callback --
+   -------------------------------
+
+   procedure Set_Editable_And_Callback
+     (Model    : access Gtk.Tree_Store.Gtk_Tree_Store_Record'Class;
+      Renderer : access Gtk_Cell_Renderer_Text_Record'Class;
+      Column   : Glib.Gint) is
+   begin
+      Tree_Model_Callback.Object_Connect
+        (Renderer, "edited", Edited_Callback'Access,
+         Slot_Object => Model, User_Data => Column);
+   end Set_Editable_And_Callback;
+
+   ---------------------
+   -- Edited_Callback --
+   ---------------------
+
+   procedure Edited_Callback
+     (Model  : access GObject_Record'Class;
+      Params : Glib.Values.GValues;
+      Data   : Glib.Gint)
+   is
+      M             : Gtk_Tree_Store := Gtk_Tree_Store (Model);
+      Iter          : Gtk_Tree_Iter;
+      Path_String   : constant String := Get_String (Nth (Params, 1));
+      Text_Value    : GValue := Nth (Params, 2);
+   begin
+      Iter := Get_Iter_From_String (M, Path_String);
+      Set_Value (M, Iter, Data, Text_Value);
+   end Edited_Callback;
+
+
+end Gui_Support;
---
+++ widgets/src/gui_support.ads
@@ -0,0 +1,50 @@
+-----------------------------------------------------------------------
+--                          G L I D E  I I                           --
+--                                                                   --
+--                        Copyright (C) 2001                         --
+--                            ACT-Europe                             --
+--                                                                   --
+-- GLIDE is free software; you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Glib.Object;
+with Gtk.Handlers;
+with Gtk.Cell_Renderer_Toggle; use Gtk.Cell_Renderer_Toggle;
+with Gtk.Cell_Renderer_Text;   use Gtk.Cell_Renderer_Text;
+with Gtk.Tree_Model;
+with Gtk.Tree_Store;
+
+package Gui_Support is
+
+   function Boolean_Get is new Gtk.Tree_Model.Model_Data_Get (Boolean);
+   --  ??? Should be shared with vcs_view_pkg.adb
+
+   package Tree_Model_Callback is new Gtk.Handlers.User_Callback
+     (Glib.Object.GObject_Record, Glib.Gint);
+
+   procedure Set_Radio_And_Callback
+     (Model    : access Gtk.Tree_Store.Gtk_Tree_Store_Record'Class;
+      Renderer : access Gtk_Cell_Renderer_Toggle_Record'Class;
+      Column   : Glib.Gint);
+   --  Equivalent of Gtk.Cell_Renderer_Toggle, but this also sets up a callback
+   --  to make sure that only one line is active.
+
+   procedure Set_Editable_And_Callback
+     (Model    : access Gtk.Tree_Store.Gtk_Tree_Store_Record'Class;
+      Renderer : access Gtk_Cell_Renderer_Text_Record'Class;
+      Column   : Glib.Gint);
+   --  Set the renderer as editable, and make sure that its text is updated
+   --  when the user has finished editing it.
+
+end Gui_Support;
