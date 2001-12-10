5f91146d64d605d596125dd32c382211e7a2931b
Arnaud Charlet <charlet@adacore.com>
2001-12-10 10:14:43 +0000

    Generated from cschtml.list and gen-marshaller
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@71746 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ widgets/cschtml/cscmarshal.h
@@ -0,0 +1,20 @@
+
+#ifndef __cschtml_MARSHAL_H__
+#define __cschtml_MARSHAL_H__
+
+#include	<glib-object.h>
+
+G_BEGIN_DECLS
+
+/* VOID:STRING,POINTER (cschtml.list:1) */
+extern void cschtml_VOID__STRING_POINTER (GClosure     *closure,
+                                          GValue       *return_value,
+                                          guint         n_param_values,
+                                          const GValue *param_values,
+                                          gpointer      invocation_hint,
+                                          gpointer      marshal_data);
+
+G_END_DECLS
+
+#endif /* __cschtml_MARSHAL_H__ */
+
