dd665c1385a68987188eb186e403361e23ffef73
Arnaud Charlet <charlet@adacore.com>
2001-12-11 09:26:54 +0000

    (On_Open_File, On_Save_File): New callbacks. (Register_Default_Toolbar): Connect these callbacks to the corresponding  toolbar buttons.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@71851 936e1b1b-40f2-da11-902a-00137254ae57

--- glide/src/glide_toolbar.adb
+++ glide/src/glide_toolbar.adb
@@ -18,14 +18,15 @@
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
-with Glib.Object;         use Glib.Object;
-with Gtk.Button;          use Gtk.Button;
-with Gtk.Stock;           use Gtk.Stock;
-with Gtk.Toolbar;         use Gtk.Toolbar;
-with Glide_Intl;          use Glide_Intl;
-with Glide_Kernel.Editor; use Glide_Kernel.Editor;
-with Traces;              use Traces;
-with Ada.Exceptions;      use Ada.Exceptions;
+with Glib.Object;          use Glib.Object;
+with Gtk.Button;           use Gtk.Button;
+with Gtk.Stock;            use Gtk.Stock;
+with Gtk.Toolbar;          use Gtk.Toolbar;
+with Gtkada.File_Selector; use Gtkada.File_Selector;
+with Glide_Intl;           use Glide_Intl;
+with Glide_Kernel.Editor;  use Glide_Kernel.Editor;
+with Traces;               use Traces;
+with Ada.Exceptions;       use Ada.Exceptions;
 
 package body Glide_Toolbar is
 
@@ -35,6 +36,16 @@ package body Glide_Toolbar is
      (Object : access GObject_Record'Class;
       Kernel : Kernel_Handle);
    --  Callback for the 'New' Button
+
+   procedure On_Open_File
+     (Object : access GObject_Record'Class;
+      Kernel : Kernel_Handle);
+   --  Callback for the 'Open' Button
+
+   procedure On_Save_File
+     (Object : access GObject_Record'Class;
+      Kernel : Kernel_Handle);
+   --  Callback for the 'Save' Button
 
    -----------------
    -- On_New_File --
@@ -51,6 +62,50 @@ package body Glide_Toolbar is
          Trace (Me, "Unexpected exception: " & Exception_Information (E));
    end On_New_File;
 
+   ------------------
+   -- On_Open_File --
+   ------------------
+
+   procedure On_Open_File
+     (Object : access GObject_Record'Class;
+      Kernel : Kernel_Handle) is
+   begin
+      declare
+         Filename : constant String := Select_File (Title => -"Open File");
+      begin
+         if Filename = "" then
+            return;
+         end if;
+
+         Open_Or_Create (Kernel, Filename);
+      end;
+
+   exception
+      when E : others =>
+         Trace (Me, "Unexpected exception: " & Exception_Information (E));
+   end On_Open_File;
+
+   ------------------
+   -- On_Save_File --
+   ------------------
+
+   procedure On_Save_File
+     (Object : access GObject_Record'Class;
+      Kernel : Kernel_Handle)
+   is
+      Success : Boolean;
+   begin
+      Save_To_File (Kernel, Success => Success);
+
+   exception
+      when E : others =>
+         Trace (Me, "Unexpected exception: " & Exception_Information (E));
+   end On_Save_File;
+
+   ------------------------------
+   -- Register_Default_Toolbar --
+   ------------------------------
+
    procedure Register_Default_Toolbar
      (Kernel : access Kernel_Handle_Record'Class)
    is
@@ -66,7 +121,17 @@ package body Glide_Toolbar is
          Kernel_Handle (Kernel));
 
       Button := Insert_Stock (Toolbar, Stock_Open, -"Open a File");
+      Kernel_Callback.Connect
+        (Button, "clicked",
+         Kernel_Callback.To_Marshaller (On_Open_File'Access),
+         Kernel_Handle (Kernel));
+
       Button := Insert_Stock (Toolbar, Stock_Save, -"Save Current File");
+      Kernel_Callback.Connect
+        (Button, "clicked",
+         Kernel_Callback.To_Marshaller (On_Save_File'Access),
+         Kernel_Handle (Kernel));
+
       Append_Space (Toolbar);
       Button := Insert_Stock (Toolbar, Stock_Undo, -"Undo Previous Action");
       Button := Insert_Stock (Toolbar, Stock_Redo, -"Redo Previous Action");
