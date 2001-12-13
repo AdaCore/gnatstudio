a55f069b9cb636e100ab29ba76add91cd483f8f5
Nicolas Setton <setton@adacore.com>
2001-12-13 09:55:28 +0000

    Replace "OK" button with a "Commit" button.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@71936 936e1b1b-40f2-da11-902a-00137254ae57

--- vcs/src/log_editor_window_pkg.adb
+++ vcs/src/log_editor_window_pkg.adb
@@ -21,10 +21,11 @@
 with Glib;                 use Glib;
 with Gtk;                  use Gtk;
 with Gtk.Widget;           use Gtk.Widget;
-with Gtk.Stock;            use Gtk.Stock;
 with Gtk.Enums;            use Gtk.Enums;
 with Gtk.Scrolled_Window;  use Gtk.Scrolled_Window;
 with Gtk.Hbutton_Box;      use Gtk.Hbutton_Box;
+
+with Glide_Intl;           use Glide_Intl;
 
 package body Log_Editor_Window_Pkg is
 
@@ -38,12 +39,17 @@ package body Log_Editor_Window_Pkg is
    is
       Label : Gtk_Label;
    begin
+      if not Is_Empty (Log_Editor_Window.Files) then
+         Set_Text (Log_Editor_Window.Files_Label,
+                   -"Edit log for files :");
+      end if;
+
       Append (Log_Editor_Window.Files, File_Name);
 
       Gtk_New (Label, File_Name);
       Set_Alignment (Log_Editor_Window.Files_Label, 0.0, 0.5);
-      Set_Padding (Label, 0, 0);
-      Set_Justify (Label, Justify_Center);
+      Set_Padding (Label, 3, 3);
+      Set_Justify (Label, Justify_Left);
       Set_Line_Wrap (Label, False);
       Pack_Start (Log_Editor_Window.Labels_Vbox, Label, False, False, 0);
    end Add_File_Name;
@@ -92,7 +98,7 @@ package body Log_Editor_Window_Pkg is
      (Log_Editor_Window : access Log_Editor_Window_Record'Class)
    is
       Vbox1 : Gtk_Vbox;
-      Vbox3 : Gtk_Vbox;
+      Vbox2 : Gtk_Vbox;
       Hbox1 : Gtk_Hbox;
       Hbox2 : Gtk_Hbox;
 
@@ -100,7 +106,7 @@ package body Log_Editor_Window_Pkg is
       Scrolledwindow1 : Gtk_Scrolled_Window;
    begin
       Gtk.Window.Initialize (Log_Editor_Window, Window_Toplevel);
-      Set_Title (Log_Editor_Window, "Log Editor");
+      Set_Title (Log_Editor_Window, -"Log Editor");
       Set_Policy (Log_Editor_Window, False, True, False);
       Set_Position (Log_Editor_Window, Win_Pos_None);
       Set_Modal (Log_Editor_Window, False);
@@ -112,18 +118,18 @@ package body Log_Editor_Window_Pkg is
       Gtk_New_Hbox (Hbox2, False, 0);
       Pack_Start (Vbox1, Hbox2, False, False, 3);
 
-      Gtk_New_Vbox (Vbox3, False, 0);
-      Pack_Start (Hbox2, Vbox3, False, False, 3);
+      Gtk_New_Vbox (Vbox2, False, 0);
+      Pack_Start (Hbox2, Vbox2, False, False, 3);
 
-      Gtk_New (Log_Editor_Window.Files_Label, "Edit log for files:");
+      Gtk_New (Log_Editor_Window.Files_Label, -"Edit log for file :");
       Set_Alignment (Log_Editor_Window.Files_Label, 0.5, 0.5);
       Set_Padding (Log_Editor_Window.Files_Label, 3, 3);
       Set_Justify (Log_Editor_Window.Files_Label, Justify_Center);
       Set_Line_Wrap (Log_Editor_Window.Files_Label, False);
-      Pack_Start (Vbox3, Log_Editor_Window.Files_Label, False, False, 0);
+      Pack_Start (Vbox2, Log_Editor_Window.Files_Label, False, False, 0);
 
       Gtk_New_Vbox (Log_Editor_Window.Labels_Vbox, False, 0);
-      Pack_Start (Hbox2, Log_Editor_Window.Labels_Vbox, True, True, 0);
+      Pack_Start (Hbox2, Log_Editor_Window.Labels_Vbox, False, False, 0);
 
       Gtk_New_Hbox (Hbox1, False, 0);
       Pack_Start (Vbox1, Hbox1, True, True, 3);
@@ -143,7 +149,8 @@ package body Log_Editor_Window_Pkg is
       Set_Child_Ipadding (Hbuttonbox1, 7, 0);
       Pack_Start (Vbox1, Hbuttonbox1, False, False, 3);
 
-      Gtk_New_From_Stock (Log_Editor_Window.Ok_Button, Stock_Ok);
+      --  Gtk_New_From_Stock (Log_Editor_Window.Ok_Button, Stock_Ok);
+      Gtk_New (Log_Editor_Window.Ok_Button, -"Commit");
       Set_Relief (Log_Editor_Window.Ok_Button, Relief_Normal);
       Set_Flags (Log_Editor_Window.Ok_Button, Can_Default);
 
