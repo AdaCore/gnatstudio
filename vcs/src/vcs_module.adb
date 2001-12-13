adcf0f48043b08a852f9ea7160d070403b21fe80
Nicolas Setton <setton@adacore.com>
2001-12-13 09:56:48 +0000

    Two new submenus.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@71938 936e1b1b-40f2-da11-902a-00137254ae57

--- vcs/src/vcs_module.adb
+++ vcs/src/vcs_module.adb
@@ -18,26 +18,37 @@
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
+with Types;                   use Types;
+
 with Glib.Object;             use Glib.Object;
-with Glib.Values;             use Glib.Values;
 with Gtk.Widget;              use Gtk.Widget;
 with Gtk.Menu_Item;           use Gtk.Menu_Item;
+with Gtk.Arguments;           use Gtk.Arguments;
 
 with Glide_Kernel;            use Glide_Kernel;
 with Glide_Kernel.Modules;    use Glide_Kernel.Modules;
+with Glide_Kernel.Project;    use Glide_Kernel.Project;
 with Glide_Intl;              use Glide_Intl;
 
 with Traces;                  use Traces;
 
 with Gtkada.MDI;              use Gtkada.MDI;
-
+with Gtkada.Handlers;         use Gtkada.Handlers;
 with VCS_View_Pkg;            use VCS_View_Pkg;
+
+with Prj_API;                 use Prj_API;
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+with VCS;                     use VCS;
 
 package body VCS_Module is
 
+   use VCS.String_List;
+
    VCS_Module_ID : Module_ID;
 
-   Me : Debug_Handle := Create ("VCS_Module");
+   Me : Debug_Handle := Create ("Vcs_Module");
 
    procedure Initialize_Module
      (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class);
@@ -48,24 +59,116 @@ package body VCS_Module is
       Kernel : Kernel_Handle);
    --  ???
 
+   procedure On_Update_All
+     (Widget : access GObject_Record'Class;
+      Kernel : Kernel_Handle);
+
    procedure On_Context_Changed
-     (Widget  : access GObject_Record'Class;
-      Params  : GValues;
-      Kernel  : Kernel_Handle);
+     (Object  : access Gtk_Widget_Record'Class;
+      Args    : Gtk_Args);
    --  ???
+
+   procedure Update_Files_In_Current_Dir
+     (Widget  : access GObject_Record'Class;
+      Kernel  : Kernel_Handle);
+   --  Updates all the files in the current directory.
+
+   function Get_Current_Dir (Kernel : Kernel_Handle) return String;
+   --  Convenience function to get the current directory.
+
+   ---------------------
+   -- Get_Current_Dir --
+   ---------------------
+
+   function Get_Current_Dir (Kernel : Kernel_Handle) return String
+   is
+      Context : Selection_Context_Access
+        := Get_Current_Explorer_Context (Kernel);
+      File    : File_Selection_Context_Access := null;
+   begin
+      if Context = null then
+         return "";
+      end if;
+
+      if Context.all in File_Selection_Context'Class then
+         File := File_Selection_Context_Access (Context);
+         if Has_Directory_Information (File) then
+            return Directory_Information (File);
+         end if;
+      end if;
+
+      return "";
+   end Get_Current_Dir;
 
    ------------------------
    -- On_Context_Changed --
    ------------------------
 
    procedure On_Context_Changed
+     (Object  : access Gtk_Widget_Record'Class;
+      Args    : Gtk_Args)
+   is
+      Context      : Selection_Context_Access :=
+        To_Selection_Context_Access (To_Address (Args, 1));
+      File         : File_Selection_Context_Access;
+
+      Explorer     : VCS_View_Access := VCS_View_Access (Object);
+   begin
+      if Context = null then
+         return;
+      end if;
+
+      if Context.all in File_Selection_Context'Class then
+         File := File_Selection_Context_Access (Context);
+         if Has_Directory_Information (File) then
+            Show_Files (Explorer, Directory_Information (File));
+         end if;
+      end if;
+   end On_Context_Changed;
+
+   ---------------------------------
+   -- Update_Files_In_Current_Dir --
+   ---------------------------------
+
+   procedure Update_Files_In_Current_Dir
      (Widget  : access GObject_Record'Class;
-      Params  : GValues;
       Kernel  : Kernel_Handle)
    is
+      Files   : List;
+      Dirs    : List;
+      Status  : File_Status_List.List;
+      Ref     : VCS_Access;
+      Child   : MDI_Child;
+
+      Dir     : String := Get_Current_Dir (Kernel);
    begin
-      null;
-   end On_Context_Changed;
+      if Dir /= "" then
+         Ref := Get_Ref_From_Directory (Dir);
+         Append (Dirs, Dir);
+
+         Status := Local_Get_Status (Ref, Dirs);
+
+         while not  File_Status_List.Is_Empty (Status) loop
+            Append (Files,
+                    Head (File_Status_List.Head (Status).File_Name));
+            File_Status_List.Tail (Status);
+         end loop;
+
+         Child := Find_MDI_Child_By_Tag
+           (Get_MDI (Kernel), VCS_View_Record'Tag);
+
+         if Child = null then
+            Update_File_List (null, Kernel, Files, Ref);
+         else
+            Update_File_List
+              (VCS_View_Access (Get_Widget (Child)), Kernel, Files, Ref);
+         end if;
+
+         Free (Dirs);
+         Free (Ref);
+         Free (Files);
+      end if;
+   end Update_Files_In_Current_Dir;
 
    -----------------------
    -- On_Open_Interface --
@@ -84,21 +187,39 @@ package body VCS_Module is
 
       if Child = null then
          Gtk_New (Explorer, Kernel);
-         Set_Size_Request (Get_Child (Explorer), 400, 400);
+         Set_Size_Request (Explorer, 400, 400);
          Child := Put (MDI, Explorer);
          Set_Title (Child, "VCS Explorer");
-         Show_Files (Explorer, "");
+         Show_Files (Explorer, Get_Current_Dir (Kernel));
 
-         Kernel_Callback.Object_Connect
+         Widget_Callback.Object_Connect
            (Kernel,
-            "context_changed",
+            Context_Changed_Signal,
             On_Context_Changed'Access,
-            Explorer,
-            Kernel_Handle (Kernel));
+            Explorer);
       else
          Set_Focus_Child (Child);
       end if;
    end On_Open_Interface;
+
+   -------------------
+   -- On_Update_All --
+   -------------------
+
+   procedure On_Update_All
+     (Widget : access GObject_Record'Class;
+      Kernel : Kernel_Handle)
+   is
+      Dirs : String_Id_Array := Source_Dirs (Get_Project_View (Kernel));
+      --  ??? we need a project-recursive version of this function.
+   begin
+      for J in Dirs'Range loop
+         if Dirs (J) /= No_String then
+            --  String_To_Name_Buffer (Dirs (J));
+            Put_Line (Get_String (Dirs (J)));
+         end if;
+      end loop;
+   end On_Update_All;
 
    -----------------------
    -- Initialize_Module --
@@ -113,11 +234,26 @@ package body VCS_Module is
    begin
       Register_Menu
         (Kernel, "/_" & VCS, Ref_Item => -"Navigate", Add_Before => False);
+
       Gtk_New (Menu_Item, -"VCS Explorer");
       Register_Menu (Kernel, "/" & VCS, Menu_Item);
       Kernel_Callback.Connect
         (Menu_Item, "activate",
          Kernel_Callback.To_Marshaller (On_Open_Interface'Access),
+         Kernel_Handle (Kernel));
+
+      Gtk_New (Menu_Item, -"Update all files in project");
+      Register_Menu (Kernel, "/" & VCS, Menu_Item);
+      Kernel_Callback.Connect
+        (Menu_Item, "activate",
+         Kernel_Callback.To_Marshaller (On_Update_All'Access),
+         Kernel_Handle (Kernel));
+
+      Gtk_New (Menu_Item, -"Update current directory");
+      Register_Menu (Kernel, "/" & VCS, Menu_Item);
+      Kernel_Callback.Connect
+        (Menu_Item, "activate",
+         Kernel_Callback.To_Marshaller (Update_Files_In_Current_Dir'Access),
          Kernel_Handle (Kernel));
    end Initialize_Module;
 
