0a44bf9abecd53bb391b9d84d175e3a171529836
Emmanuel Briot <briot@adacore.com>
2001-12-14 16:44:52 +0000

    (Help_Browser_Record): New type.  We no longer store the current file or the html widget in the kernel itself,  since we were not reseting the pointer when the widget was destroyed anyway.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@72013 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/glide_kernel-help.adb
+++ kernel/src/glide_kernel-help.adb
@@ -23,11 +23,7 @@ with Csc_HTML_Widget;           use Csc_HTML_Widget;
 with GNAT.Directory_Operations; use GNAT.Directory_Operations;
 with GNAT.OS_Lib;               use GNAT.OS_Lib;
 with Gtkada.MDI;                use Gtkada.MDI;
-with Glide_Main_Window;         use Glide_Main_Window;
-with Glide_Page;                use Glide_Page;
-with GVD.Process;               use GVD.Process;
 with Gtk.Enums;                 use Gtk.Enums;
-with Gtk.Widget;                use Gtk.Widget;
 with Gtk.Scrolled_Window;       use Gtk.Scrolled_Window;
 with Glide_Intl;                use Glide_Intl;
 with Traces;                    use Traces;
@@ -42,9 +38,17 @@ package body Glide_Kernel.Help is
    Default_Width  : constant := 400;
    Default_Height : constant := 400;
 
+   type Help_Browser_Record is new Gtk_Scrolled_Window_Record with record
+      Current_Help_File : GNAT.OS_Lib.String_Access;
+      --  The current help file displayed. Used to find relative (hyper) links.
+
+      Csc               : Csc_HTML;
+   end record;
+   type Help_Browser is access all Help_Browser_Record'Class;
+
    function Load_File
      (Kernel : access Kernel_Handle_Record'Class;
-      HTML   : access Csc_HTML_Record'Class;
+      Html   : access Help_Browser_Record'Class;
       File   : String) return Boolean;
    --  Load File in HTML widget, and set File as the Current_Help_File for
    --  Kernel. Return True if the file could be successfully read.
@@ -69,9 +73,13 @@ package body Glide_Kernel.Help is
       Kernel : Kernel_Handle);
    --  Handler for the link_clicked signal
 
+   ---------------
+   -- Load_File --
+   ---------------
+
    function Load_File
      (Kernel : access Kernel_Handle_Record'Class;
-      HTML   : access Csc_HTML_Record'Class;
+      Html   : access Help_Browser_Record'Class;
       File   : String) return Boolean
    is
       Buffer   : String_Access;
@@ -82,11 +90,11 @@ package body Glide_Kernel.Help is
 
       if Buffer /= null then
          Trace (Me, "loading file: " & File);
-         Free (Kernel.Current_Help_File);
-         Kernel.Current_Help_File := new String' (File);
-         Stream := HTML_Begin (HTML);
-         HTML_Write (HTML, Stream, Buffer.all);
-         HTML_End (HTML, Stream, Stream_OK);
+         Free (Html.Current_Help_File);
+         Html.Current_Help_File := new String' (File);
+         Stream := HTML_Begin (Html.Csc);
+         HTML_Write (Html.Csc, Stream, Buffer.all);
+         HTML_End (Html.Csc, Stream, Stream_OK);
          Free (Buffer);
          return True;
 
@@ -105,10 +113,11 @@ package body Glide_Kernel.Help is
       Params : Glib.Values.GValues;
       Kernel : Kernel_Handle)
    is
+      Html     : Help_Browser := Help_Browser (Object);
       Url      : constant String := Get_String (Nth (Params, 1));
       Stream   : Csc_HTML_Stream :=
         Csc_HTML_Stream (Get_Proxy (Nth (Params, 2)));
-      Base_Dir : constant String := Dir_Name (Kernel.Current_Help_File.all);
+      Base_Dir : constant String := Dir_Name (Html.Current_Help_File.all);
       Buffer   : String_Access;
 
    begin
@@ -136,6 +145,7 @@ package body Glide_Kernel.Help is
       Params : Glib.Values.GValues;
       Kernel : Kernel_Handle)
    is
+      --  Html     : Help_Browser := Help_Browser (Object);
       Url : constant String := Get_String (Nth (Params, 1));
    begin
       Trace (Me, "on url: " & Url);
@@ -150,11 +160,12 @@ package body Glide_Kernel.Help is
       Params : Glib.Values.GValues;
       Kernel : Kernel_Handle)
    is
+      Html     : Help_Browser := Help_Browser (Object);
       Url      : constant String := Get_String (Nth (Params, 1));
       Result   : Boolean := True;
       Anchor   : Natural := Index (Url, "#");
-      Base_Dir : constant String := Dir_Name (Kernel.Current_Help_File.all);
-      Basename : constant String := Base_Name (Kernel.Current_Help_File.all);
+      Base_Dir : constant String := Dir_Name (Html.Current_Help_File.all);
+      Basename : constant String := Base_Name (Html.Current_Help_File.all);
 
    begin
       if Anchor = 0 then
@@ -162,21 +173,21 @@ package body Glide_Kernel.Help is
       end if;
 
       if Is_Absolute_Path (Url) then
-         if Url /= Kernel.Current_Help_File.all then
+         if Url /= Html.Current_Help_File.all then
             Result := Load_File
-              (Kernel, Csc_HTML (Object), Url (Url'First .. Anchor - 1));
+              (Kernel, Html, Url (Url'First .. Anchor - 1));
          end if;
 
       elsif Url /= Basename then
          Result := Load_File
-           (Kernel, Csc_HTML (Object),
+           (Kernel, Html,
             Base_Dir & Url (Url'First .. Anchor - 1));
       end if;
 
       if Result and then Anchor < Url'Last then
          Trace (Me, "jumping to anchor " & Url (Anchor .. Url'Last));
          Result := Jump_To_Anchor
-           (Csc_HTML (Object), Url (Anchor + 1 .. Url'Last));
+           (Html.Csc, Url (Anchor + 1 .. Url'Last));
       end if;
    end Link_Clicked;
 
@@ -188,43 +199,41 @@ package body Glide_Kernel.Help is
      (Kernel    : access Kernel_Handle_Record'Class;
       Help_File : String)
    is
-      HTML     : Csc_HTML;
-      Top      : constant Glide_Window := Glide_Window (Kernel.Main_Window);
-      MDI      : constant MDI_Window :=
-        Glide_Page.Glide_Page (Get_Current_Process (Top)).Process_Mdi;
-      Scrolled : Gtk_Scrolled_Window;
+      MDI      : constant MDI_Window := Get_MDI (Kernel);
+      Scrolled : Help_Browser;
       Child    : MDI_Child;
       Result   : Boolean;
-
-      use Gtk.Widget;
-
    begin
-      if Kernel.HTML_Widget = null then
-         Gtk_New (Scrolled);
+      Child := Find_MDI_Child_By_Tag (MDI, Help_Browser_Record'Tag);
+
+      if Child = null then
+         Scrolled := new Help_Browser_Record;
+         Gtk.Scrolled_Window.Initialize (Scrolled);
          Set_Policy (Scrolled, Policy_Automatic, Policy_Always);
-         Gtk_New (HTML);
-         Add (Scrolled, HTML);
-         Kernel.HTML_Widget := Gtk_Widget (HTML);
+         Gtk_New (Scrolled.Csc);
+         Add (Scrolled, Scrolled.Csc);
+         Set_Size_Request (Scrolled, Default_Width, Default_Height);
          Child := Put (MDI, Scrolled);
 
          Set_Title (Child, -"Help");
-         Set_Size_Request (Scrolled, Default_Width, Default_Height);
          Show_All (Scrolled);
-         Kernel_Callback.Connect
-           (HTML, "url_requested",
-            Url_Requested'Access, Kernel_Handle (Kernel));
-         Kernel_Callback.Connect
-           (HTML, "link_clicked",
-            Link_Clicked'Access, Kernel_Handle (Kernel));
-         Kernel_Callback.Connect
-           (HTML, "on_url",
-            On_Url'Access, Kernel_Handle (Kernel));
-
+         Kernel_Callback.Object_Connect
+           (Scrolled.Csc, "url_requested",
+            Url_Requested'Access, User_Data => Kernel_Handle (Kernel),
+            Slot_Object => Scrolled);
+         Kernel_Callback.Object_Connect
+           (Scrolled.Csc, "link_clicked",
+            Link_Clicked'Access, User_Data => Kernel_Handle (Kernel),
+            Slot_Object => Scrolled);
+         Kernel_Callback.Object_Connect
+           (Scrolled.Csc, "on_url",
+            On_Url'Access, User_Data => Kernel_Handle (Kernel),
+            Slot_Object => Scrolled);
       else
-         HTML := Csc_HTML (Kernel.HTML_Widget);
+         Scrolled := Help_Browser (Get_Widget (Child));
       end if;
 
-      Result := Load_File (Kernel, HTML, Help_File);
+      Result := Load_File (Kernel, Scrolled, Help_File);
    end Display_Help;
 
    ------------
@@ -236,14 +245,20 @@ package body Glide_Kernel.Help is
       Text           : String;
       Case_Sensitive : Boolean := True;
       Forward        : Boolean := True;
-      Regular        : Boolean := False) return Boolean is
+      Regular        : Boolean := False) return Boolean
+   is
+      MDI      : constant MDI_Window := Get_MDI (Kernel);
+      Child : MDI_Child := Find_MDI_Child_By_Tag
+        (MDI, Help_Browser_Record'Tag);
+      Scrolled : Help_Browser;
    begin
-      if Kernel.HTML_Widget = null then
+      if Scrolled = null then
          return False;
       end if;
 
+      Scrolled := Help_Browser (Get_Widget (Child));
       return Search
-        (Get_Engine (Csc_HTML (Kernel.HTML_Widget)), Text,
+        (Get_Engine (Scrolled.Csc), Text,
          Case_Sensitive, Forward, Regular);
    end Search;
 
@@ -252,13 +267,19 @@ package body Glide_Kernel.Help is
    -----------------
 
    function Search_Next
-     (Kernel : access Kernel_Handle_Record'Class) return Boolean is
+     (Kernel : access Kernel_Handle_Record'Class) return Boolean
+   is
+      MDI      : constant MDI_Window := Get_MDI (Kernel);
+      Child : MDI_Child := Find_MDI_Child_By_Tag
+        (MDI, Help_Browser_Record'Tag);
+      Scrolled : Help_Browser;
    begin
-      if Kernel.HTML_Widget = null then
+      if Scrolled = null then
          return False;
       end if;
 
-      return Search_Next (Get_Engine (Csc_HTML (Kernel.HTML_Widget)));
+      Scrolled := Help_Browser (Get_Widget (Child));
+      return Search_Next (Get_Engine (Scrolled.Csc));
    end Search_Next;
 
 end Glide_Kernel.Help;
