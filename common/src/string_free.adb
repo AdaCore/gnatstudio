cd9b9e1917eb52d25edfed279f993704accf488c
Arnaud Charlet <charlet@adacore.com>
2001-12-14 15:41:07 +0000

    Remove trailing spaces.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@72007 936e1b1b-40f2-da11-902a-00137254ae57

--- common/src/string_free.adb
+++ common/src/string_free.adb
@@ -17,7 +17,7 @@
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
-   
+
 procedure String_Free (S : in out String) is
 begin
    null;
--- common/src/string_free.ads
+++ common/src/string_free.ads
@@ -17,6 +17,6 @@
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
-   
+
 procedure String_Free (S : in out String);
 --  Dummy procedure used by String_List. Do nothing.
