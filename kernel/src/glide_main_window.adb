4607a48ffd9fca42dab1cec5c3dc0fbd985facb2
Arnaud Charlet <charlet@adacore.com>
2001-12-17 09:35:04 +0000

    Move from kernel/src to glide/src (Anim_Cb): New function used to animate images. (Gtk_New, Initialize): Now take extra params: Home_Dir, Prefix_Directory.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@72033 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/glide_main_window.adb
+++ kernel/src/glide_main_window.adb
@@ -21,21 +21,43 @@
 with Glide_Kernel; use Glide_Kernel;
 with Gtk.Box;      use Gtk.Box;
 with Gtk.Enums;    use Gtk.Enums;
+with Gtk.Image;    use Gtk.Image;
 with Gtk.Window;   use Gtk.Window;
+with GNAT.OS_Lib;  use GNAT.OS_Lib;
 
 package body Glide_Main_Window is
+
+   Animated_Image : constant String := "glide-animation.gif";
+   --  <preferences>
+
+   -------------
+   -- Anim_Cb --
+   -------------
+
+   function Anim_Cb (Kernel : Kernel_Handle) return Boolean is
+      Window : Glide_Window := Glide_Window (Get_Main_Window (Kernel));
+   begin
+      if Advance (Window.Animation_Iter) then
+         Set (Window.Animation_Image, Get_Pixbuf (Window.Animation_Iter));
+      end if;
+
+      return True;
+   end Anim_Cb;
 
    -------------
    -- Gtk_New --
    -------------
 
    procedure Gtk_New
-     (Main_Window : out Glide_Window;
-      Key         : String;
-      Menu_Items  : Gtk_Item_Factory_Entry_Array) is
+     (Main_Window      : out Glide_Window;
+      Key              : String;
+      Menu_Items       : Gtk_Item_Factory_Entry_Array;
+      Home_Dir         : String;
+      Prefix_Directory : String) is
    begin
       Main_Window := new Glide_Window_Record;
-      Glide_Main_Window.Initialize (Main_Window, Key, Menu_Items);
+      Glide_Main_Window.Initialize
+        (Main_Window, Key, Menu_Items, Home_Dir, Prefix_Directory);
    end Gtk_New;
 
    ----------------
@@ -43,16 +65,49 @@ package body Glide_Main_Window is
    ----------------
 
    procedure Initialize
-     (Main_Window : access Glide_Window_Record'Class;
-      Key         : String;
-      Menu_Items  : Gtk_Item_Factory_Entry_Array) is
+     (Main_Window      : access Glide_Window_Record'Class;
+      Key              : String;
+      Menu_Items       : Gtk_Item_Factory_Entry_Array;
+      Home_Dir         : String;
+      Prefix_Directory : String)
+   is
+      Box1   : Gtk_Hbox;
+      Error  : GError;
+      Pixbuf : Gdk_Pixbuf;
+
    begin
       GVD.Main_Window.Initialize (Main_Window, Key, Menu_Items);
-      Gtk_New (Main_Window.Kernel, Gtk_Window (Main_Window));
+      Main_Window.Home_Dir := new String' (Home_Dir);
+      Main_Window.Prefix_Directory := new String' (Prefix_Directory);
+      Main_Window.Standalone := False;
+      Gtk_New (Main_Window.Kernel, Gtk_Window (Main_Window), Home_Dir);
+
+      Gtk_New_Hbox (Box1);
+      Pack_Start (Main_Window.Toolbar_Box, Box1);
       Gtk_New (Main_Window.Toolbar, Orientation_Horizontal, Toolbar_Icons);
       Set_Tooltips (Main_Window.Toolbar, True);
-      Pack_Start (Main_Window.Toolbar_Box, Main_Window.Toolbar, False, False);
-      Main_Window.Standalone := False;
+      Pack_Start (Box1, Main_Window.Toolbar, True, True);
+
+      declare
+         File : constant String :=
+           Prefix_Directory & Directory_Separator & "share" &
+           Directory_Separator & "glide" & Directory_Separator &
+           Animated_Image;
+
+      begin
+         if Is_Regular_File (File) then
+            Gtk_New (Main_Window.Animation_Frame);
+            Set_Shadow_Type (Main_Window.Animation_Frame, Shadow_In);
+            Pack_End (Box1, Main_Window.Animation_Frame, False, False);
+
+            Gdk_New_From_File (Main_Window.Animation, File, Error);
+            Gtk_New (Main_Window.Animation_Image, Main_Window.Animation);
+            Main_Window.Animation_Iter := Get_Iter (Main_Window.Animation);
+            Pixbuf := Get_Pixbuf (Main_Window.Animation_Iter);
+            Set (Main_Window.Animation_Image, Pixbuf);
+            Add (Main_Window.Animation_Frame, Main_Window.Animation_Image);
+         end if;
+      end;
    end Initialize;
 
 end Glide_Main_Window;
--- kernel/src/glide_main_window.ads
+++ kernel/src/glide_main_window.ads
@@ -20,31 +20,51 @@
 
 with GVD.Main_Window;
 with Glide_Kernel;
+with Gdk.Pixbuf; use Gdk.Pixbuf;
+with Gtk.Frame; use Gtk.Frame;
+with Gtk.Image; use Gtk.Image;
 with Gtk.Item_Factory; use Gtk.Item_Factory;
 with Gtk.Toolbar; use Gtk.Toolbar;
+with Gtk.Main;
 
 package Glide_Main_Window is
 
    type Glide_Window_Record is new GVD.Main_Window.GVD_Main_Window_Record with
    record
-      Kernel      : Glide_Kernel.Kernel_Handle;
-      Toolbar     : Gtk_Toolbar;
-      Interrupted : Boolean := False;
+      Kernel          : Glide_Kernel.Kernel_Handle;
+      Toolbar         : Gtk_Toolbar;
+      Animation_Frame : Gtk_Frame;
+      Animation       : Gdk_Pixbuf_Animation;
+      Animation_Iter  : Gdk_Pixbuf_Animation_Iter;
+      Animation_Image : Gtk_Image;
+      Timeout_Id      : Gtk.Main.Timeout_Handler_Id;
+      Busy_Level      : Integer := 0;
+      Interrupted     : Boolean := False;
    end record;
    type Glide_Window is access all Glide_Window_Record'Class;
 
    procedure Gtk_New
-     (Main_Window : out Glide_Window;
-      Key         : String;
-      Menu_Items  : Gtk_Item_Factory_Entry_Array);
+     (Main_Window      : out Glide_Window;
+      Key              : String;
+      Menu_Items       : Gtk_Item_Factory_Entry_Array;
+      Home_Dir         : String;
+      Prefix_Directory : String);
    --  Create a new main window.
    --  Key is a unique string identifying main_window.
-   --  Menu_Items is used to create the menu bar.
+   --  Menu_Items is used to create the default menu bar.
+   --  Home_Dir is the home directory (e.g ~/.glide) under which configuration
+   --  files will be saved.
+   --  Prefix_Directory is the prefix where Glide is installed (e.g /opt/gnu).
 
    procedure Initialize
-     (Main_Window : access Glide_Window_Record'Class;
-      Key         : String;
-      Menu_Items  : Gtk_Item_Factory_Entry_Array);
+     (Main_Window      : access Glide_Window_Record'Class;
+      Key              : String;
+      Menu_Items       : Gtk_Item_Factory_Entry_Array;
+      Home_Dir         : String;
+      Prefix_Directory : String);
    --  Internal initialization function.
+
+   function Anim_Cb (Kernel : Glide_Kernel.Kernel_Handle) return Boolean;
+   --  Function called when the glide animation needs to be updated.
 
 end Glide_Main_Window;
