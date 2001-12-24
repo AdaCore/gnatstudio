c4558da95e214cb22eb13b8b034bd2cfcd9ae589
Emmanuel Briot <briot@adacore.com>
2001-12-24 09:46:23 +0000

    Removed, since no longer needed
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@72202 936e1b1b-40f2-da11-902a-00137254ae57

--- vsearch/src/.gate/vsearch/vsearch_pkg-callbacks.ads
+++
@@ -1,8 +0,0 @@
-with Gtk.Arguments;
-with Gtk.Widget; use Gtk.Widget;
-
-package Vsearch_Pkg.Callbacks is
-   procedure On_Browse_Button_Clicked
-     (Object : access Gtk_Widget_Record'Class);
-
-end Vsearch_Pkg.Callbacks;
