ff7787f5b162a921774fa19379d8b82d709c0227
Emmanuel Briot <briot@adacore.com>
2002-01-02 16:38:31 +0000

    minor style fix :-)
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@72282 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/common/language-java.adb
+++ gvd/common/language-java.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                   GVD - The GNU Visual Debugger                   --
 --                                                                   --
---                 Copyright (C) 2000-2002 ACT-Europe                -- 
+--                 Copyright (C) 2000-2002 ACT-Europe                --
 --                                                                   --
 -- GVD is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
