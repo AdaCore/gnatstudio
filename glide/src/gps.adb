eef58ea16f3b42be0a45487bafbe6072d8c1f26e
Arnaud Charlet <charlet@adacore.com>
2002-01-03 10:59:30 +0000

    Add 2002 copyright notice.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@72315 936e1b1b-40f2-da11-902a-00137254ae57

--- glide/src/gps.adb
+++ glide/src/gps.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                          G L I D E  I I                           --
 --                                                                   --
---                        Copyright (C) 2001                         --
+--                     Copyright (C) 2001-2002                       --
 --                            ACT-Europe                             --
 --                                                                   --
 -- GLIDE is free software; you can redistribute it and/or modify  it --
