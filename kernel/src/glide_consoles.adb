bb1fdbbe18b3023da3284697a3bb87fd436b114e
Emmanuel Briot <briot@adacore.com>
2002-01-03 14:41:05 +0000

    (On_Button_Release): Remove unused parameter Event
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@72399 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/glide_consoles.adb
+++ kernel/src/glide_consoles.adb
@@ -20,7 +20,6 @@
 
 with Glib;                 use Glib;
 with Gdk.Color;            use Gdk.Color;
-with Gdk.Event;            use Gdk.Event;
 with Gtk.Enums;            use Gtk.Enums;
 with Gtk.Scrolled_Window;  use Gtk.Scrolled_Window;
 with Gtk.Text;             use Gtk.Text;
@@ -44,8 +43,7 @@ package body Glide_Consoles is
    --  <preference>
 
    function On_Button_Release
-     (Widget : access Gtk_Widget_Record'Class;
-      Event  : Gdk_Event) return Boolean;
+     (Widget : access Gtk_Widget_Record'Class) return Boolean;
    --  Handler for "button_press_event" signal
 
    function Load_Desktop
@@ -179,8 +177,7 @@ package body Glide_Consoles is
    -----------------------
 
    function On_Button_Release
-     (Widget : access Gtk_Widget_Record'Class;
-      Event  : Gdk_Event) return Boolean
+     (Widget : access Gtk_Widget_Record'Class) return Boolean
    is
       Console     : Glide_Console := Glide_Console (Widget);
       Position    : constant Gint := Get_Position (Console.Text);
