594b34fe3c1112d24b814b167287ecdcce637655
Emmanuel Briot <briot@adacore.com>
2002-01-03 17:22:40 +0000

    Remove unneed pragma Warnings
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@72421 936e1b1b-40f2-da11-902a-00137254ae57

--- vcs/src/vcs-p4.adb
+++ vcs/src/vcs-p4.adb
@@ -18,8 +18,6 @@
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
-pragma Warnings (Off);
-
 package body VCS.P4 is
 
    ---------
