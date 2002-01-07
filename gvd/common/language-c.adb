ac4dbd1cf0b01bbd22813f9e9305adebe1b25e16
Arnaud Charlet <charlet@adacore.com>
2002-01-07 18:10:39 +0000

    (Get_Token): New function. (Analyze_C_Source): New procedure that provides the core functionalitiy  for Next_Indentation and Parse_Entities. (Next_Indentation, Parse_Entities): Now use Analyze_C_Source.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@72532 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/common/language-c.adb
+++ gvd/common/language-c.adb
@@ -22,6 +22,7 @@ with GNAT.Regpat;       use GNAT.Regpat;
 with Pixmaps_IDE;       use Pixmaps_IDE;
 with Ada.Strings.Fixed; use Ada.Strings.Fixed;
 with Basic_Types;       use Basic_Types;
+with String_Utils;      use String_Utils;
 
 package body Language.C is
 
@@ -54,6 +55,147 @@ package body Language.C is
             Position_Index => 4,
             Icon           => subprogram_xpm'Access,
             Make_Entry     => Make_Entry_Subprogram'Access));
+
+   -----------------
+   -- Local types --
+   -----------------
+
+   type Token_Type is
+     (Tok_Identifier,
+      Tok_Auto,
+      Tok_Case,
+      Tok_Const,
+      Tok_Do,
+      Tok_Else,
+      Tok_Extern,
+      Tok_For,
+      Tok_If,
+      Tok_Switch,
+      Tok_Register,
+      Tok_Static,
+      Tok_Struct,
+      Tok_Typedef,
+      Tok_Union,
+      Tok_Volatile,
+      Tok_While);
+   --  Reserved words for C
+
+   ----------------------
+   -- Local procedures --
+   ----------------------
+
+   function Get_Token (S : String) return Token_Type;
+   --  Return a Token_Type given a string.
+
+   procedure Analyze_C_Source
+     (Buffer        : String;
+      Indent        : out Integer;
+      Indent_Params : Indent_Parameters;
+      No_Contents   : out Boolean;
+      Callback      : Entity_Callback := null);
+   --  Analyze buffer.
+   --  Indent is set to the current indentation level after having parsed
+   --  Buffer.
+   --  No_Contents is set to True if the last line parsed contained no
+   --  contents other than closing curlys and parentheses.
+   --  If Callback is not null, it will be called for each entity.
+
+   ---------------
+   -- Get_Token --
+   ---------------
+
+   function Get_Token (S : String) return Token_Type is
+      pragma Suppress (All_Checks);
+      --  For efficiency
+
+      Second : Integer;
+   begin
+      if S'Length = 1 then
+         return Tok_Identifier;
+      end if;
+
+      Second := S'First + 1;
+
+      --  Use a case statement instead of a loop for efficiency
+
+      case S (S'First) is
+         when 'a' =>
+            if S (Second .. S'Last) = "uto" then
+               return Tok_Auto;
+            end if;
+
+         when 'c' =>
+            if S (Second .. S'Last) = "onst" then
+               return Tok_Const;
+            elsif S (Second .. S'Last) = "ase" then
+               return Tok_Case;
+            end if;
+
+         when 'd' =>
+            if S (Second .. S'Last) = "o" then
+               return Tok_Do;
+            end if;
+
+         when 'e' =>
+            if S (Second .. S'Last) = "lse" then
+               return Tok_Else;
+            elsif S (Second .. S'Last) = "xtern" then
+               return Tok_Extern;
+            end if;
+
+         when 'f' =>
+            if S (Second .. S'Last) = "or" then
+               return Tok_For;
+            end if;
+
+         when 'i' =>
+            if S (Second .. S'Last) = "f" then
+               return Tok_If;
+            end if;
+
+         when 'r' =>
+            if S (Second .. S'Last) = "egister" then
+               return Tok_Register;
+            end if;
+
+         when 's' =>
+            if S (Second) = 't' then
+               if S (Second + 1 .. S'Last) = "atic" then
+                  return Tok_Static;
+               elsif S (Second + 1 .. S'Last) = "ruct" then
+                  return Tok_Struct;
+               end if;
+
+            elsif S (Second .. S'Last) = "witch" then
+               return Tok_Switch;
+            end if;
+
+         when 't' =>
+            if S (Second .. S'Last) = "ypedef" then
+               return Tok_Typedef;
+            end if;
+
+         when 'u' =>
+            if S (Second .. S'Last) = "nion" then
+               return Tok_Union;
+            end if;
+
+         when 'v' =>
+            if S (Second .. S'Last) = "olatile" then
+               return Tok_Volatile;
+            end if;
+
+         when 'w' =>
+            if S (Second .. S'Last) = "hile" then
+               return Tok_While;
+            end if;
+
+         when others =>
+            return Tok_Identifier;
+      end case;
+
+      return Tok_Identifier;
+   end Get_Token;
 
    --------------------
    -- Is_Simple_Type --
@@ -192,6 +334,192 @@ package body Language.C is
    end Get_Language_Context;
 
    ----------------------
+   -- Analyze_C_Source --
+   ----------------------
+
+   procedure Analyze_C_Source
+     (Buffer        : String;
+      Indent        : out Integer;
+      Indent_Params : Indent_Parameters;
+      No_Contents   : out Boolean;
+      Callback      : Entity_Callback := null)
+   is
+      pragma Suppress (All_Checks);
+      --  For efficiency
+
+      First : Natural;
+      Index : Natural := Buffer'First;
+      Token : Token_Type;
+
+   begin
+      No_Contents := True;
+      Indent := 0;
+
+      while Index < Buffer'Last loop
+         case Buffer (Index) is
+            when ASCII.NUL .. ' ' =>
+               if Buffer (Index) = ASCII.LF then
+                  No_Contents := True;
+               end if;
+
+            when '{' | '(' =>
+               Indent := Indent + Indent_Params.Indent_Level;
+
+            when '}' | ')' =>
+               Indent := Indent - Indent_Params.Indent_Level;
+
+            when '"' =>
+               No_Contents := False;
+
+               --  Skip string
+
+               First := Index;
+               Index := Index + 1;
+
+               while Index < Buffer'Last
+                 and then (Buffer (Index) /= '"'
+                           or else Buffer (Index - 1) = '\')
+                 and then Buffer (Index) /= ASCII.LF
+               loop
+                  Index := Index + 1;
+               end loop;
+
+               if Callback /= null then
+                  exit when Callback
+                    (String_Text, (0, 0, First), (0, 0, Index), False);
+               end if;
+
+            when ''' =>
+               No_Contents := False;
+
+               --  Skip character
+
+               First := Index;
+               Index := Index + 1;
+
+               while Index < Buffer'Last
+                 and then (Buffer (Index) /= '''
+                           or else Buffer (Index - 1) = '\')
+                 and then Buffer (Index) /= ASCII.LF
+               loop
+                  Index := Index + 1;
+               end loop;
+
+               if Callback /= null then
+                  exit when Callback
+                    (Character_Text, (0, 0, First), (0, 0, Index), False);
+               end if;
+
+            when '/' =>
+               No_Contents := False;
+
+               --  Comment ?
+
+               First := Index;
+
+               if Buffer (Index + 1) = '/' then
+                  --  C++ style comment, skip whole line
+
+                  Index := Index + 2;
+
+                  while Index < Buffer'Last
+                    and then Buffer (Index) /= ASCII.LF
+                  loop
+                     Index := Index + 1;
+                  end loop;
+
+                  if Callback /= null then
+                     exit when Callback
+                       (Comment_Text,
+                        (0, 0, First), (0, 0, Index - 1), False);
+                  end if;
+
+               elsif Buffer (Index + 1) = '*' then
+                  --  Skip comment
+
+                  Index := Index + 3;
+
+                  while Index < Buffer'Last
+                    and then (Buffer (Index - 1) /= '*'
+                      or else Buffer (Index) /= '/')
+                  loop
+                     Index := Index + 1;
+                  end loop;
+
+                  if Callback /= null then
+                     exit when Callback
+                       (Comment_Text,
+                        (0, 0, First), (0, 0, Index),
+                        Buffer (Index) /= '/');
+                  end if;
+               end if;
+
+            when 'a' .. 'z' | 'A' .. 'Z' | '0' .. '9' | '_' =>
+               No_Contents := False;
+
+               --  Skip identifier or reserved word
+
+               First := Index;
+               Index := Index + 1;
+
+               while Index < Buffer'Last
+                 and then Is_Word_Char (Buffer (Index))
+               loop
+                  Index := Index + 1;
+               end loop;
+
+               Token := Get_Token (Buffer (First .. Index - 1));
+
+               if Callback /= null then
+                  if Token = Tok_Identifier then
+                     exit when Callback
+                       (Identifier_Text,
+                        (0, 0, First), (0, 0, Index - 1), False);
+                  else
+                     exit when Callback
+                       (Keyword_Text, (0, 0, First), (0, 0, Index - 1), False);
+                  end if;
+               end if;
+
+            when others =>
+               No_Contents := False;
+         end case;
+
+         Index := Index + 1;
+      end loop;
+
+   exception
+      when others =>
+         null;
+   end Analyze_C_Source;
+
+   --------------------
+   -- Parse_Entities --
+   --------------------
+
+   procedure Parse_Entities
+     (Lang          : access C_Language;
+      Buffer        : Interfaces.C.Strings.chars_ptr;
+      Buffer_Length : Natural;
+      Callback      : Entity_Callback)
+   is
+      pragma Unreferenced (Lang);
+      pragma Suppress (All_Checks);
+
+      S           : Unchecked_String_Access := To_Unchecked_String (Buffer);
+      Ignored     : Natural;
+      No_Contents : Boolean;
+
+   begin
+      Analyze_C_Source
+        (Buffer        => S (1 .. Buffer_Length),
+         Indent        => Ignored,
+         Indent_Params => Default_Indent_Parameters,
+         No_Contents   => No_Contents,
+         Callback      => Callback);
+   end Parse_Entities;
+
+   ----------------------
    -- Next_Indentation --
    ----------------------
 
@@ -227,81 +555,12 @@ package body Language.C is
       end loop;
 
       Indent := Index - First;
-
-      while Index < Buffer_Length loop
-         case S (Index) is
-            when ASCII.NUL .. ' ' =>
-               null;
-
-            when '{' | '(' =>
-               Offset := Offset + Indent_Params.Indent_Level;
-
-            when '}' | ')' =>
-               Offset := Offset - Indent_Params.Indent_Level;
-
-            when '"' =>
-               No_Contents := False;
-
-               --  Skip string
-
-               Index := Index + 1;
-
-               while Index < Buffer_Length
-                 and then (S (Index) /= '"' or else S (Index - 1) = '\')
-                 and then S (Index) /= ASCII.LF
-               loop
-                  Index := Index + 1;
-               end loop;
-
-            when ''' =>
-               No_Contents := False;
-
-               --  Skip character
-
-               Index := Index + 1;
-
-               while Index < Buffer_Length
-                 and then (S (Index) /= ''' or else S (Index - 1) = '\')
-                 and then S (Index) /= ASCII.LF
-               loop
-                  Index := Index + 1;
-               end loop;
-
-            when '/' =>
-               No_Contents := False;
-
-               --  Comment ?
-
-               if S (Index + 1) = '/' then
-                  --  C++ style comment, skip whole line
-
-                  Index := Index + 2;
-
-                  while Index < Buffer_Length
-                    and then S (Index) /= ASCII.LF
-                  loop
-                     Index := Index + 1;
-                  end loop;
-
-               elsif S (Index + 1) = '*' then
-                  --  Skip comment
-
-                  Index := Index + 3;
-
-                  while Index < Buffer_Length
-                    and then (S (Index - 1) /= '*'
-                      or else S (Index) /= '/')
-                  loop
-                     Index := Index + 1;
-                  end loop;
-               end if;
-
-            when others =>
-               No_Contents := False;
-         end case;
-
-         Index := Index + 1;
-      end loop;
+      Analyze_C_Source
+        (Buffer        => S (Index .. Buffer_Length),
+         Indent        => Offset,
+         Indent_Params => Indent_Params,
+         No_Contents   => No_Contents,
+         Callback      => null);
 
       if -Offset > Indent then
          Next_Indent := 0;
