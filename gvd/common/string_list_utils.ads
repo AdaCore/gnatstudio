98a7a4143627634f379a7f5158462e68b6ba2361
Nicolas Setton <setton@adacore.com>
2002-01-11 10:26:44 +0000

    Initial revision for this file.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@72607 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ gvd/common/string_list_utils.ads
@@ -0,0 +1,34 @@
+-----------------------------------------------------------------------
+--                          G L I D E  I I                           --
+--                                                                   --
+--                        Copyright (C) 2002                         --
+--                            ACT-Europe                             --
+--                                                                   --
+-- GLIDE is free software; you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Generic_List;
+
+package String_List_Utils is
+
+   procedure String_Free (S : in out String);
+   --  Free memory associated with S.
+   
+   package String_List is new Generic_List (String, Free => String_Free);
+   
+   function Copy_String_List
+     (S : in String_List.List) return String_List.List;
+   --  Return a deep copy of S.
+
+end String_List_Utils;
