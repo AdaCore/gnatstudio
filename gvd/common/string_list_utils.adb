a8ce5cfe1988226cebb13d851820d338524f080a
Nicolas Setton <setton@adacore.com>
2002-02-14 16:19:16 +0000

    (Split, Fuse, Sort): new subprograms.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73286 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/common/string_list_utils.adb
+++ gvd/common/string_list_utils.adb
@@ -20,6 +20,13 @@
 
 package body String_List_Utils is
 
+   use String_List;
+
+   procedure Split (L : in out String_List.List;
+                    L1, L2 : out String_List.List);
+   function Fuse (L_1, L_2 : String_List.List) return String_List.List;
+   --  Utilities for the Sort procedure.
+
    ----------------------
    -- Copy_String_List --
    ----------------------
@@ -27,8 +34,6 @@ package body String_List_Utils is
    function Copy_String_List
      (S : in String_List.List) return String_List.List
    is
-      use String_List;
-
       Result : String_List.List;
       Temp   : List_Node := First (S);
 
@@ -50,5 +55,79 @@ package body String_List_Utils is
    begin
       null;
    end String_Free;
+
+   ----------
+   -- Fuse --
+   ----------
+
+   function Fuse (L_1, L_2 : String_List.List) return String_List.List is
+      L  : List;
+      L1 : List := L_1;
+      L2 : List := L_2;
+
+   begin
+      while not Is_Empty (L1)
+        and then not Is_Empty (L2)
+      loop
+         if Head (L1) < Head (L2) then
+            Append (L, Head (L1));
+            Next (L1);
+         else
+            Append (L, Head (L2));
+            Next (L2);
+         end if;
+      end loop;
+
+      if Is_Empty (L1) then
+         Concat (L, L2);
+      else
+         Concat (L, L1);
+      end if;
+
+      return L;
+   end Fuse;
+
+   -----------
+   -- Split --
+   -----------
+
+   procedure Split (L : in out String_List.List;
+                    L1, L2 : out String_List.List)
+   is
+      Target : Natural := 1;
+   begin
+      while not Is_Empty (L) loop
+         if Target = 1 then
+            Append (L1, Head (L));
+            Target := 2;
+         else
+            Append (L2, Head (L));
+            Target := 1;
+         end if;
+         Next (L);
+      end loop;
+   end Split;
+
+   ----------
+   -- Sort --
+   ----------
+
+   procedure Sort (L : in out String_List.List)
+   is
+      L1, L2 : List;
+   begin
+      if Is_Empty (L) then
+         return;
+      end if;
+
+      if Length (L) = 1 then
+         return;
+      end if;
+
+      Split (L, L1, L2);
+      Sort (L1);
+      Sort (L2);
+      L := Fuse (L1, L2);
+   end Sort;
 
 end String_List_Utils;
