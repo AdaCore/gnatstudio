7097c2093ad4d304758cc595aff925767e8e4636
Arnaud Charlet <charlet@adacore.com>
2002-02-21 11:34:41 +0000

    Update header.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73450 936e1b1b-40f2-da11-902a-00137254ae57

--- builder/src/builder_module.ads
+++ builder/src/builder_module.ads
@@ -1,10 +1,10 @@
 -----------------------------------------------------------------------
---                          G L I D E  I I                           --
+--                               G P S                               --
 --                                                                   --
 --                     Copyright (C) 2001-2002                       --
 --                            ACT-Europe                             --
 --                                                                   --
--- GLIDE is free software; you can redistribute it and/or modify  it --
+-- GPS is free  software; you can  redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
 -- the Free Software Foundation; either version 2 of the License, or --
 -- (at your option) any later version.                               --
