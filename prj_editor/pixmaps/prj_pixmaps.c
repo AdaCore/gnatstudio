6766aaa6163f541994e4501546276169c75139d1
Arnaud Charlet <charlet@adacore.com>
2002-02-25 09:41:24 +0000

    Replace project_modified by project_ext and project_ext_closed.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73507 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/pixmaps/prj_pixmaps.c
+++ prj_editor/pixmaps/prj_pixmaps.c
@@ -1,6 +1,7 @@
 #include "logo_xpm"
 #include "project_closed_xpm"
 #include "project_xpm"
-#include "project_modified_xpm"
+#include "project_ext_closed_xpm"
+#include "project_ext_xpm"
 #include "mini-folder_object_xpm"
 #include "mini_page_xpm"
