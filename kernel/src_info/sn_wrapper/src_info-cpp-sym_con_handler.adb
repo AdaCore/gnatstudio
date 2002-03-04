908d6f7dbd3e79bbff766a5e072f9caf4ab5d1f2
Taras Galchenko (Elbrus) <galchenko@ot.ru>
2002-03-04 11:01:50 +0000

    Added CON handler - copy of GV handler
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73692 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/sn_wrapper/src_info-cpp-sym_con_handler.adb
+++ kernel/src_info/sn_wrapper/src_info-cpp-sym_con_handler.adb
@@ -1,10 +1,10 @@
 separate (Src_Info.CPP)
 
 --------------------
--- Sym_GV_Handler --
+-- Sym_CON_Handler --
 --------------------
 
-procedure Sym_GV_Handler (Sym : FIL_Table)
+procedure Sym_CON_Handler (Sym : FIL_Table)
 is
    Desc       : CType_Description;
    Var        : GV_Table;
@@ -13,17 +13,17 @@ is
    Attributes : SN_Attributes;
    Scope      : E_Scope := Global_Scope;
 begin
-   Info ("Sym_GV_Handler: """
+   Info ("Sym_CON_Handler: """
          & Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last)
          & """");
 
-   if not Is_Open (SN_Table (GV)) then
-      --  GV table does not exist, nothing to do ...
+   if not Is_Open (SN_Table (CON)) then
+      --  CON table does not exist, nothing to do ...
       return;
    end if;
 
    --  Lookup variable type
-   Var := Find (SN_Table (GV), Sym.Buffer
+   Var := Find (SN_Table (CON), Sym.Buffer
       (Sym.Identifier.First .. Sym.Identifier.Last));
    Type_Name_To_Kind (Var.Buffer
       (Var.Value_Type.First .. Var.Value_Type.Last), Desc, Success);
@@ -56,4 +56,5 @@ exception
    when  DB_Error |   -- non-existent table
          Not_Found => -- no such variable
       null;           -- ignore error
-end Sym_GV_Handler;
+end Sym_CON_Handler;
+
