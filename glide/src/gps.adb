49e5f590be4e6b40452f6b84a216626983e41568
Arnaud Charlet <charlet@adacore.com>
2002-03-04 18:33:08 +0000

    Replace Glide by GPS. Update header
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73703 936e1b1b-40f2-da11-902a-00137254ae57

--- glide/src/gps.adb
+++ glide/src/gps.adb
@@ -4,7 +4,7 @@
 --                     Copyright (C) 2001-2002                       --
 --                            ACT-Europe                             --
 --                                                                   --
--- GPS is free  software; you can  redistribute it and/or modify  it --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
 -- the Free Software Foundation; either version 2 of the License, or --
 -- (at your option) any later version.                               --
@@ -13,7 +13,7 @@
 -- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
 -- General Public License for more details. You should have received --
--- a copy of the GNU General Public License along with this library; --
+-- a copy of the GNU General Public License along with this program; --
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
@@ -92,7 +92,7 @@ procedure GPS is
    Handler        : Language_Handlers.Glide.Glide_Language_Handler;
 
    procedure Init_Settings;
-   --  Set up environment for Glide.
+   --  Set up environment for GPS.
 
    procedure Help;
    --  Display help on the standard output.
