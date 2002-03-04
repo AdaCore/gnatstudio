e0a07c93b4c14ab31f3e161ed0f14ef9aa4f2290
Arnaud Charlet <charlet@adacore.com>
2002-03-04 10:19:01 +0000

    (Register_Module, Load_Desktop, Save_Desktop): Moved from Glide_consoles. (On_Clear_Console): New callback, for new menu File->Console->Clear.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73673 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/glide_kernel-console.adb
+++ kernel/src/glide_kernel-console.adb
@@ -4,7 +4,7 @@
 --                     Copyright (C) 2001-2002                       --
 --                            ACT-Europe                             --
 --                                                                   --
--- GLIDE is free software; you can redistribute it and/or modify  it --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
 -- the Free Software Foundation; either version 2 of the License, or --
 -- (at your option) any later version.                               --
@@ -13,23 +13,30 @@
 -- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
 -- General Public License for more details. You should have received --
--- a copy of the GNU General Public License along with this library; --
+-- a copy of the GNU General Public License along with this program; --
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
-with Glide_Main_Window; use Glide_Main_Window;
-with Glide_Page;        use Glide_Page;
-with Glide_Consoles;    use Glide_Consoles;
-with GVD.Process;       use GVD.Process;
-with GNAT.IO;           use GNAT.IO;
-with Gtk.Widget;        use Gtk.Widget;
-with Gtkada.Handlers;   use Gtkada.Handlers;
+with Gint_Xml;             use Gint_Xml;
+with Glib.Object;          use Glib.Object;
+with Glide_Main_Window;    use Glide_Main_Window;
+with Glide_Page;           use Glide_Page;
+with Glide_Consoles;       use Glide_Consoles;
+with Glide_Intl;           use Glide_Intl;
+with Glide_Kernel.Modules; use Glide_Kernel.Modules;
+with GVD.Process;          use GVD.Process;
+with GNAT.IO;              use GNAT.IO;
+with Gtk.Widget;           use Gtk.Widget;
+with Gtkada.Handlers;      use Gtkada.Handlers;
 
 package body Glide_Kernel.Console is
 
-   function Get_Console (Kernel : access Kernel_Handle_Record'Class)
-      return Glide_Console;
+   Console_Module_Id   : Glide_Kernel.Module_ID;
+   Console_Module_Name : constant String := "Glide_Kernel.Console";
+
+   function Get_Console
+     (Kernel : access Kernel_Handle_Record'Class) return Glide_Console;
    --  Return the console associated with the kernel.
 
    procedure Console_Destroyed
@@ -41,11 +48,26 @@ package body Glide_Kernel.Console is
      (Console : access Gtk.Widget.Gtk_Widget_Record'Class) return Boolean;
    --  Prevent the destrution of the console in the MDI
 
+   procedure On_Clear_Console
+     (Widget : access GObject_Record'Class; Kernel : Kernel_Handle);
+   --  Callback for File->Console->Clear menu.
+
+   function Load_Desktop
+     (Node : Gint_Xml.Node_Ptr; User : Kernel_Handle)
+      return Gtk_Widget;
+   --  Save the status of the console to an XML tree
+
+   function Save_Desktop
+     (Widget : access Gtk.Widget.Gtk_Widget_Record'Class)
+      return Node_Ptr;
+   --  Restore the status of the console from a saved XML tree.
+
    -----------------
    -- Get_Console --
    -----------------
 
-   function Get_Console (Kernel : access Kernel_Handle_Record'Class)
+   function Get_Console
+     (Kernel : access Kernel_Handle_Record'Class)
       return Glide_Console
    is
       Top : constant Glide_Window := Glide_Window (Kernel.Main_Window);
@@ -55,6 +77,7 @@ package body Glide_Kernel.Console is
       then
          return Glide_Page.Glide_Page (Get_Current_Process (Top)).Console;
       end if;
+
       return null;
    end Get_Console;
 
@@ -120,6 +143,18 @@ package body Glide_Kernel.Console is
       return True;
    end Console_Delete_Event;
 
+   ----------------------
+   -- On_Clear_Console --
+   ----------------------
+
+   procedure On_Clear_Console
+     (Widget : access GObject_Record'Class; Kernel : Kernel_Handle)
+   is
+      pragma Unreferenced (Widget);
+   begin
+      Clear (Kernel);
+   end On_Clear_Console;
+
    ------------------------
    -- Initialize_Console --
    ------------------------
@@ -139,5 +174,71 @@ package body Glide_Kernel.Console is
             Return_Callback.To_Marshaller (Console_Delete_Event'Access));
       end if;
    end Initialize_Console;
+
+   ------------------
+   -- Load_Desktop --
+   ------------------
+
+   function Load_Desktop
+     (Node : Gint_Xml.Node_Ptr; User : Kernel_Handle)
+      return Gtk_Widget
+   is
+      Console : Glide_Console;
+   begin
+      if Node.Tag.all = "Console" then
+         Gtk_New (Console, User);
+         return Gtk_Widget (Console);
+      end if;
+
+      return null;
+   end Load_Desktop;
+
+   ------------------
+   -- Save_Desktop --
+   ------------------
+
+   function Save_Desktop
+     (Widget : access Gtk.Widget.Gtk_Widget_Record'Class)
+      return Node_Ptr
+   is
+      N : Node_Ptr;
+   begin
+      if Widget.all in Glide_Console_Record'Class then
+         N := new Node;
+         N.Tag := new String' ("Console");
+         return N;
+      end if;
+
+      return null;
+   end Save_Desktop;
+
+   ---------------------
+   -- Register_Module --
+   ---------------------
+
+   procedure Register_Module
+     (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class)
+   is
+      Console : constant String := '/' & (-"File") & '/' & (-"Console");
+   begin
+      Console_Module_Id := Register_Module
+        (Kernel       => Kernel,
+         Module_Name  => Console_Module_Name,
+         Priority     => Default_Priority);
+
+      Register_Menu
+        (Kernel, Console, Ref_Item => -"Close");
+      Register_Menu
+        (Kernel, Console, -"Clear", "", On_Clear_Console'Access);
+      Register_Menu
+        (Kernel, Console, -"Save As...", "", null, Sensitive => False);
+      --             On_Save_Console_As'Access);
+      Register_Menu
+        (Kernel, Console, -"Load Contents...", "", null, Sensitive => False);
+      --             On_Load_To_Console'Access);
+
+      Glide_Kernel.Kernel_Desktop.Register_Desktop_Functions
+        (Save_Desktop'Access, Load_Desktop'Access);
+   end Register_Module;
 
 end Glide_Kernel.Console;
--- kernel/src/glide_kernel-console.ads
+++ kernel/src/glide_kernel-console.ads
@@ -1,10 +1,10 @@
 -----------------------------------------------------------------------
---                          G L I D E  I I                           --
+--                               G P S                               --
 --                                                                   --
 --                     Copyright (C) 2001-2002                       --
 --                            ACT-Europe                             --
 --                                                                   --
--- GLIDE is free software; you can redistribute it and/or modify  it --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
 -- the Free Software Foundation; either version 2 of the License, or --
 -- (at your option) any later version.                               --
@@ -13,7 +13,7 @@
 -- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
 -- General Public License for more details. You should have received --
--- a copy of the GNU General Public License along with this library; --
+-- a copy of the GNU General Public License along with this program; --
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
@@ -25,6 +25,10 @@ package Glide_Kernel.Console is
    --   - Info for general information
    --   - Error for signaling errors
    --   - Verbose for detailed information
+
+   procedure Register_Module
+     (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class);
+   --  Register the console module into the list
 
    procedure Initialize_Console
      (Kernel         : access Kernel_Handle_Record'Class);
