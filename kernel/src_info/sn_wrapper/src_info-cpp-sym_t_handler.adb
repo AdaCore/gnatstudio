9af6feaeed7dab02b55db425325ed6a12b5d1318
Warren Paul <paul@adacore.com>
2002-03-05 17:26:12 +0000

    Added the simplest case: when the original type can be resolved.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73736 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/sn_wrapper/src_info-cpp-sym_t_handler.adb
+++ kernel/src_info/sn_wrapper/src_info-cpp-sym_t_handler.adb
@@ -6,7 +6,7 @@ separate (Src_Info.CPP)
 
 procedure Sym_T_Handler (Sym : FIL_Table)
 is
---   Decl_Info : E_Declaration_Info_List;
+   Decl_Info : E_Declaration_Info_List;
    Desc       : CType_Description;
    Success    : Boolean;
    Identifier : String :=
@@ -15,24 +15,50 @@ begin
 
    Info ("Sym_T_Hanlder: """ & Identifier & """");
 
-   --  find original type for typedef
-   Original_Type (Identifier, Desc, Success);
-
-   if not Success then
-      Warn ("Original type for typedef " & Identifier & " not found");
+   if not Is_Open (SN_Table (T)) then
+      --  .t table does not exist, nothing to do
       return;
    end if;
 
---   Insert_Declaration
---     (Handler           => LI_Handler (Global_CPP_Handler),
---      File              => Global_LI_File,
---      Symbol_Name       =>
---        Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last),
---      Source_Filename   =>
---        Sym.Buffer (Sym.File_Name.First .. Sym.File_Name.Last),
---      Location          => Sym.Start_Position,
---      Kind              => Enumeration_Literal,
---      Scope             => Global_Scope,
---      Declaration_Info  => Decl_Info);
+   --  find original type for given typedef
+   Original_Type (Identifier, Desc, Success);
+
+   if Success then
+      --  we know E_Kind for original type
+      --  Ancestor_Point and Ancestor_Filename has information about
+      --  parent type (do not mess with Parent_xxx in CType_Description)
+
+      if Desc.Builtin_Name /= null then
+         --  parent type is a builtin type: use Predefined_Point
+         --  FIXME Builtin_Name is not used anywhere. We should
+         --  use it (e.g. for a field like Predefined_Type_Name)
+         Desc.Ancestor_Point := Predefined_Point;
+      end if;
+
+      Insert_Declaration
+        (Handler           => LI_Handler (Global_CPP_Handler),
+         File              => Global_LI_File,
+         Symbol_Name       =>
+           Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last),
+         Source_Filename   =>
+           Sym.Buffer (Sym.File_Name.First .. Sym.File_Name.Last),
+         Location          => Sym.Start_Position,
+         Parent_Filename   => Desc.Ancestor_Filename.all,
+         Parent_Location   => Desc.Ancestor_Point,
+         Kind              => Desc.Kind,
+         Scope             => Global_Scope,
+         Declaration_Info  => Decl_Info);
+   else
+      --  could not get E_Kind for the original type
+
+      Warn ("Typedef " & Identifier & ": original type not found");
+
+      if Desc.Ancestor_Point /= Invalid_Point then
+         null;
+      end if;
+
+   end if;
+
+   Free (Desc);
 
 end Sym_T_Handler;
