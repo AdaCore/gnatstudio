72a2106e7d8c31aee5cec11db3e8e76c96b9b381
Alexey Zakharov (Elbrus) <zakharov@elbrus-tech.ru>
2002-03-05 16:08:52 +0000

    Sym_FD_Handler itself
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73733 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ kernel/src_info/sn_wrapper/src_info-cpp-sym_fd_handler.adb
@@ -0,0 +1,46 @@
+separate (Src_Info.CPP)
+
+--------------------
+-- Sym_FD_Handler --
+--------------------
+
+procedure Sym_FD_Handler (Sym : FIL_Table)
+is
+   Target_Kind : E_Kind;
+   tmp_ptr : E_Declaration_Info_List;
+begin
+   Info ("Sym_FD_Hanlder: """
+         & Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last)
+         & """");
+
+   declare
+      FD_Tab : FD_Table;
+   begin
+      FD_Tab := Find (SN_Table (FD),
+          Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last));
+      if eq (FD_Tab.Buffer (FD_Tab.Return_Type.First
+                                 .. FD_Tab.Return_Type.Last), "void") then
+         Target_Kind := Non_Generic_Procedure;
+      else
+         Target_Kind := Non_Generic_Function_Or_Operator;
+      end if;
+      Free (FD_Tab);
+   exception
+      when DB_Error | Not_Found =>
+         Fail ("unable to find function " &
+               Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last));
+   end;
+
+   Insert_Declaration
+     (Handler           => LI_Handler (Global_CPP_Handler),
+      File              => Global_LI_File,
+      Symbol_Name       =>
+        Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last),
+      Source_Filename   =>
+        Sym.Buffer (Sym.File_Name.First .. Sym.File_Name.Last),
+      Location          => Sym.Start_Position,
+      Kind              => Target_Kind,
+      Scope             => Global_Scope,
+      Declaration_Info  => tmp_ptr);
+
+end Sym_FD_Handler;
