b0b567597336bb6462a3cba0db9a24c2e37153e3
Taras Galchenko (Elbrus) <galchenko@ot.ru>
2002-03-06 12:06:12 +0000

    Replaced Insert_Dependency_Declaration with Insert_Dependency. Alex says this is the right function for includes
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73792 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/sn_wrapper/src_info-cpp-sym_iu_handler.adb
+++ kernel/src_info/sn_wrapper/src_info-cpp-sym_iu_handler.adb
@@ -4,28 +4,20 @@ separate (Src_Info.CPP)
 -- Sym_IU_Handler --
 --------------------
 
-procedure Sym_IU_Handler (Sym : FIL_Table)
-is
-   tmp_ptr    : E_Declaration_Info_List;
+procedure Sym_IU_Handler (Sym : FIL_Table) is
 begin
    Info ("Sym_IU_Handler: """
          & Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last)
          & """");
 
-   Insert_Dependency_Declaration
+   Insert_Dependency
      (Handler           => LI_Handler (Global_CPP_Handler),
       File              => Global_LI_File,
       List              => Global_LI_File_List,
-      Symbol_Name       =>
-        Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last),
       Source_Filename   =>
         Sym.Buffer (Sym.File_Name.First .. Sym.File_Name.Last),
       Referred_Filename =>
-        Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last),
-      Location          => Sym.Start_Position,
-      Kind              => Non_Generic_Package, -- ???
-      Scope             => Global_Scope,
-      Declaration_Info  => tmp_ptr);
+        Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last));
 
 end Sym_IU_Handler;
 
