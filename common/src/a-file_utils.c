c264ac978d7cf3a0b092967bf373f2e30efa51b2
Emmanuel Briot <briot@adacore.com>
2002-03-06 11:26:39 +0000

    (__gnat_subdirectories_count): stat doesn't work as expected on windows, so  we just pretend there is always a subdirectory. GPS can live with that, but  this needs to be fixed before integration into GNAT.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73781 936e1b1b-40f2-da11-902a-00137254ae57

--- common/src/a-file_utils.c
+++ common/src/a-file_utils.c
@@ -3,15 +3,24 @@
 int
 __gnat_subdirectories_count (name)
 {
-   struct stat statbuf;
-   int ret;
-
-   ret = __gnat_stat (name, &statbuf);
-
-   if (ret)
-      return -1;
-   else
-      return statbuf.st_nlink - 2;
+#ifdef _WIN32
+  /* On windows, stat(2) alwways return 1 for the number of links, and thus can
+     not be used reliably.
+     However, for GPS we only want to know if there is at least one
+     subdirectory, so we just pretend this is always true.
+  */
+  return 1;
+  
+#else
+  struct stat statbuf;
+  int ret;
+  
+  ret = __gnat_stat (name, &statbuf);
+  
+  if (ret)
+    return -1;
+  else
+    /* Do not count the subdirectories . and .. */
+    return statbuf.st_nlink - 2;
+#endif
 }
-
-
