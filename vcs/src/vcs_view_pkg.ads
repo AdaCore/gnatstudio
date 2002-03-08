916a42ec37f10e9c533dccc8d22b4e7e708a7733
Nicolas Setton <setton@adacore.com>
2002-03-08 05:07:12 +0000

    (VCS_View_Record): new field Hide_Not_Registered.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73889 936e1b1b-40f2-da11-902a-00137254ae57

--- vcs/src/vcs_view_pkg.ads
+++ vcs/src/vcs_view_pkg.ads
@@ -1,10 +1,10 @@
 -----------------------------------------------------------------------
---                          G L I D E  I I                           --
+--                               G P S                               --
 --                                                                   --
 --                     Copyright (C) 2001-2002                       --
 --                            ACT-Europe                             --
 --                                                                   --
--- GLIDE is free software; you can redistribute it and/or modify  it --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
 -- the Free Software Foundation; either version 2 of the License, or --
 -- (at your option) any later version.                               --
@@ -13,7 +13,7 @@
 -- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
 -- General Public License for more details. You should have received --
--- a copy of the GNU General Public License along with this library; --
+-- a copy of the GNU General Public License along with this program; --
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
@@ -44,6 +44,9 @@ package VCS_View_Pkg is
 
       Hide_Up_To_Date : Boolean := False;
       --  Whether up-to-date and unknown files should be hidden in the view.
+
+      Hide_Not_Registered : Boolean := False;
+      --  Whether files that are not registered should be hidden in the view.
 
       Stored_Status   : File_Status_List.List;
       Cached_Status   : File_Status_List.List;
