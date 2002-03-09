0a8258e3017282c7926042417661940dbdf4e38c
Taras Galchenko (Elbrus) <galchenko@ot.ru>
2002-03-09 10:02:37 +0000

    Implemented derived-to-base class referencing via Add_Parent
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73915 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/sn_wrapper/src_info-cpp-sym_cl_handler.adb
+++ kernel/src_info/sn_wrapper/src_info-cpp-sym_cl_handler.adb
@@ -6,12 +6,14 @@ separate (Src_Info.CPP)
 
 procedure Sym_CL_Handler (Sym : FIL_Table)
 is
-   Decl_Info : E_Declaration_Info_List;
-   Desc      : CType_Description;
-   Class_Def : CL_Table;
-   Success   : Boolean;
-   P         : Pair_Ptr;
-   Super     : IN_Table;
+   Decl_Info  : E_Declaration_Info_List;
+   Desc       : CType_Description;
+   Class_Def  : CL_Table;
+   Success    : Boolean;
+   P          : Pair_Ptr;
+   Super      : IN_Table;
+   Super_Def  : CL_Table;
+   Super_Desc : CType_Description;
 begin
 
    Info ("Sym_CL_Hanlder: """
@@ -27,6 +29,20 @@ begin
    if not Success then
       return;
    end if;
+
+   Insert_Declaration
+     (Handler               => LI_Handler (Global_CPP_Handler),
+      File                  => Global_LI_File,
+      List                  => Global_LI_File_List,
+      Symbol_Name           =>
+        Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last),
+      Source_Filename       =>
+        Sym.Buffer (Sym.File_Name.First .. Sym.File_Name.Last),
+      Location              => Sym.Start_Position,
+      Kind                  => Record_Type,
+      Scope                 => Global_Scope,
+      End_Of_Scope_Location => Class_Def.End_Position,
+      Declaration_Info      => Decl_Info);
 
    --  Find all the base classes for this one
    Set_Cursor
@@ -44,23 +60,25 @@ begin
       Super := Parse_Pair (P.all);
       Info ("Found base class: "
          & Super.Buffer (Super.Base_Class.First .. Super.Base_Class.Last));
+      --  Lookup base class definition to find its precise location
+      Find_Class
+       (Super.Buffer (Super.Base_Class.First .. Super.Base_Class.Last),
+        Super_Desc,
+        Super_Def,
+        Success);
+      if Success then -- if found, add it to parent list
+         Add_Parent
+           (Decl_Info,
+            Global_LI_File_List,
+            Super_Def.Buffer
+              (Super_Def.File_Name.First .. Super_Def.File_Name.Last),
+            Super_Def.Start_Position);
+         Free (Super_Desc);
+         Free (Super_Def);
+      end if;
       Free (Super);
       Free (P);
    end loop;
-
-   Insert_Declaration
-     (Handler               => LI_Handler (Global_CPP_Handler),
-      File                  => Global_LI_File,
-      List                  => Global_LI_File_List,
-      Symbol_Name           =>
-        Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last),
-      Source_Filename       =>
-        Sym.Buffer (Sym.File_Name.First .. Sym.File_Name.Last),
-      Location              => Sym.Start_Position,
-      Kind                  => Record_Type,
-      Scope                 => Global_Scope,
-      End_Of_Scope_Location => Class_Def.End_Position,
-      Declaration_Info      => Decl_Info);
 
    Insert_Reference
      (Declaration_Info      => Decl_Info,
