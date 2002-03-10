dd174f3c51355d78b25c4f2f3bb8005220e5619c
Taras Galchenko (Elbrus) <galchenko@ot.ru>
2002-03-10 14:37:14 +0000

    implemented fu-to-ec handler
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73934 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/sn_wrapper/src_info-cpp-fu_to_ec_handler.adb
+++ kernel/src_info/sn_wrapper/src_info-cpp-fu_to_ec_handler.adb
@@ -5,9 +5,76 @@ separate (Src_Info.CPP)
 ------------------------
 
 procedure Fu_To_Ec_Handler (Ref : TO_Table) is
-   Ref_Id : String := Ref.Buffer
-     (Ref.Referred_Symbol_Name.First .. Ref.Referred_Symbol_Name.Last);
+   Decl_Info    : E_Declaration_Info_List;
+   Enum_Const   : EC_Table;
 begin
-   Info ("Fu_To_EC_Handler: """ & Ref_Id & """");
+   Info ("Fu_To_EC_Handler: """
+         & Ref.Buffer (Ref.Referred_Symbol_Name.First ..
+               Ref.Referred_Symbol_Name.Last)
+         & """");
+
+   Enum_Const := Find (SN_Table (EC),
+      Ref.Buffer (Ref.Referred_Symbol_Name.First ..
+                  Ref.Referred_Symbol_Name.Last));
+
+   --  Find declaration
+   if Enum_Const.Buffer
+      (Enum_Const.File_Name.First .. Enum_Const.File_Name.Last)
+      = Get_LI_Filename (Global_LI_File) then
+      begin
+         Decl_Info := Find_Declaration
+           (File                    => Global_LI_File,
+            Symbol_Name             =>
+               Ref.Buffer (Ref.Referred_Symbol_Name.First ..
+                           Ref.Referred_Symbol_Name.Last),
+            Location                => Enum_Const.Start_Position);
+      exception
+         when Declaration_Not_Found =>
+            Fail ("Failed to lookup declaration for enumeration constant "
+               & Ref.Buffer (Ref.Referred_Symbol_Name.First ..
+                             Ref.Referred_Symbol_Name.Last));
+            Free (Enum_Const);
+            return;
+      end;
+   else -- another file
+      begin -- Find dependency declaration
+         Decl_Info := Find_Dependency_Declaration
+           (File                    => Global_LI_File,
+            Symbol_Name             =>
+               Ref.Buffer (Ref.Referred_Symbol_Name.First ..
+                           Ref.Referred_Symbol_Name.Last),
+            Filename                =>
+               Enum_Const.Buffer (Enum_Const.File_Name.First ..
+                                  Enum_Const.File_Name.Last),
+            Location                => Enum_Const.Start_Position);
+      exception
+         when Declaration_Not_Found => -- dep decl does not yet exist
+            Insert_Dependency_Declaration
+              (Handler           => LI_Handler (Global_CPP_Handler),
+               File              => Global_LI_File,
+               List              => Global_LI_File_List,
+               Symbol_Name       =>
+                  Enum_Const.Buffer (Enum_Const.Name.First ..
+                                     Enum_Const.Name.Last),
+               Source_Filename   =>
+                  Ref.Buffer (Ref.File_Name.First .. Ref.File_Name.Last),
+               Location          => Enum_Const.Start_Position,
+               Kind              => Enumeration_Literal,
+               Scope             => Global_Scope,
+               Referred_Filename =>
+                  Enum_Const.Buffer (Enum_Const.File_Name.First ..
+                                     Enum_Const.File_Name.Last),
+               Declaration_Info  => Decl_Info);
+      end;
+   end if;
+   Free (Enum_Const);
+
+   Insert_Reference
+     (Declaration_Info        => Decl_Info,
+      File                    => Global_LI_File,
+      Source_Filename         =>
+         Ref.Buffer (Ref.File_Name.First .. Ref.File_Name.Last),
+      Location                => Ref.Position,
+      Kind                    => Reference);
 end Fu_To_Ec_Handler;
 
