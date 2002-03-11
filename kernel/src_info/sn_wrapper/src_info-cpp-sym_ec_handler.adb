7856901cf2fd52a6f638018736a0441771935079
Warren Paul <paul@adacore.com>
2002-03-11 13:47:50 +0000

    Added setting parent for EC to its enum.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73994 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/sn_wrapper/src_info-cpp-sym_ec_handler.adb
+++ kernel/src_info/sn_wrapper/src_info-cpp-sym_ec_handler.adb
@@ -15,8 +15,9 @@ is
    Has_Enum  : Boolean := False;
 begin
 
-   Info ("Sym_EC_Hanlder: """ & Ec_Id & """");
+   Info ("Sym_EC_Hanlder: '" & Ec_Id & "'");
 
+   --  looking for enum, which contains given enum constant (EC)
    if Is_Open (SN_Table (EC)) and then Is_Open (SN_Table (E)) then
       declare
          EC_Def : EC_Table := Find (SN_Table (EC), Ec_Id, Sym.Start_Position);
@@ -25,7 +26,7 @@ begin
          Find_Enum
            (EC_Def.Buffer
               (EC_Def.Enumeration_Name.First ..
-               EC_Def.Enumeration_Name.First),
+               EC_Def.Enumeration_Name.Last),
             Desc, E_Def, Has_Enum);
          Free (E_Def);
          Free (EC_Def);
@@ -36,15 +37,12 @@ begin
       end;
    end if;
 
-   if Has_Enum
-     and then Desc.Parent_Point /= Invalid_Point
-   then -- corresponding enumeration found
+   if Has_Enum then -- corresponding enumeration found
       Insert_Declaration
         (Handler           => LI_Handler (Global_CPP_Handler),
          File              => Global_LI_File,
          List              => Global_LI_File_List,
-         Symbol_Name       =>
-           Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last),
+         Symbol_Name       => Ec_Id,
          Source_Filename   =>
            Sym.Buffer (Sym.File_Name.First .. Sym.File_Name.Last),
          Location          => Sym.Start_Position,
@@ -54,12 +52,12 @@ begin
          Scope             => Global_Scope,
          Declaration_Info  => Decl_Info);
    else
+      Warn ("Could not find enum for '" & Ec_Id & "'");
       Insert_Declaration
         (Handler           => LI_Handler (Global_CPP_Handler),
          File              => Global_LI_File,
          List              => Global_LI_File_List,
-         Symbol_Name       =>
-           Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last),
+         Symbol_Name       => Ec_Id,
          Source_Filename   =>
            Sym.Buffer (Sym.File_Name.First .. Sym.File_Name.Last),
          Location          => Sym.Start_Position,
