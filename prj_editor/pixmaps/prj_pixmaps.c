f7398aa0d268820e347fbcbde810bafb75dd6dbc
Emmanuel Briot <briot@adacore.com>
2002-03-11 08:59:27 +0000

    New icons for modified projects
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73946 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/pixmaps/prj_pixmaps.c
+++ prj_editor/pixmaps/prj_pixmaps.c
@@ -1,6 +1,8 @@
 #include "logo_xpm"
 #include "project_closed_xpm"
 #include "project_xpm"
+#include "project_modified_closed_xpm"
+#include "project_modified_xpm"
 #include "project_ext_closed_xpm"
 #include "project_ext_xpm"
 #include "mini-folder_object_xpm"
