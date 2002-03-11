b48a541302e1f3e1a555d2b8ed9629059dea3ca9
Taras Galchenko (Elbrus) <galchenko@ot.ru>
2002-03-11 11:49:18 +0000

    Implemented fwd decl search in fu handler
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73985 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/sn_wrapper/src_info-cpp-sym_fu_handler.adb
+++ kernel/src_info/sn_wrapper/src_info-cpp-sym_fu_handler.adb
@@ -13,12 +13,101 @@ is
    tmp_int        : Integer;
    P              : Pair_Ptr;
    FU_Tab         : FU_Table;
-   FD_Tab         : FD_Table;
    MI_Tab         : MI_Table;
    MD_Tab         : MD_Table;
    Start_Position : Point := Sym.Start_Position;
    Body_Position  : Point := Invalid_Point;
    End_Position   : Point;
+
+   function Find_First_Forward_Declaration
+     (Fn : FU_Table) return E_Declaration_Info_List;
+   --  Attempts to find the first forward declaration
+   --  for the function. Returns null if not found or
+   --  forward declaration is in another file which
+   --  has not been yet processed
+
+   function Find_First_Forward_Declaration
+     (Fn : FU_Table) return E_Declaration_Info_List is
+      P            : Pair_Ptr;
+      FD_Tab       : FD_Table;
+      FD_Tab_Tmp   : FD_Table;
+      First_FD_Pos : Point := Invalid_Point;
+   begin
+      --  First we have to find the first forward declaration
+      --  that corresponds to our function, that is prototypes
+      --  should be the same
+      --  ??? What should we do when forward declarations are
+      --  found in different files, which to choose?
+      --  So far we take the first one.
+      --  ??? What should we do when forward declaration located
+      --  in another file has not been processed yet?
+      Set_Cursor
+        (SN_Table (FD),
+         By_Key,
+         Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last) & Field_Sep,
+         False);
+
+      loop
+         P := Get_Pair (SN_Table (FD), Next_By_Key);
+         if P = null then -- no fwd decls at all
+            return null;
+         end if;
+         FD_Tab := Parse_Pair (P.all);
+         Free (P);
+         --  Update position of the first forward declaration
+         exit when Cmp_Prototypes
+              (FD_Tab.Buffer,
+               Fn.Buffer,
+               FD_Tab.Arg_Types,
+               Fn.Arg_Types,
+               FD_Tab.Return_Type,
+               Fn.Return_Type);
+         Free (FD_Tab);
+      end loop;
+
+      --  now find the first declaration in the file
+      Set_Cursor
+        (SN_Table (FD),
+         By_Key,
+         Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last) & Field_Sep,
+         False);
+
+      loop
+         P := Get_Pair (SN_Table (FD), Next_By_Key);
+         exit when P = null;
+         FD_Tab_Tmp := Parse_Pair (P.all);
+         Free (P);
+         --  Update position of the first forward declaration
+         if FD_Tab.Buffer (FD_Tab.File_Name.First .. FD_Tab.File_Name.Last)
+            = FD_Tab_Tmp.Buffer (FD_Tab_Tmp.File_Name.First ..
+                                 FD_Tab_Tmp.File_Name.Last)
+            and then Cmp_Prototypes
+              (FD_Tab.Buffer,
+               FD_Tab_Tmp.Buffer,
+               FD_Tab.Arg_Types,
+               FD_Tab_Tmp.Arg_Types,
+               FD_Tab.Return_Type,
+               FD_Tab_Tmp.Return_Type)
+            and then ((First_FD_Pos = Invalid_Point)
+            or else Less (FD_Tab_Tmp.Start_Position, First_FD_Pos)) then
+            First_FD_Pos := FD_Tab_Tmp.Start_Position;
+         end if;
+         Free (FD_Tab_Tmp);
+      end loop;
+
+      pragma Assert (First_FD_Pos /= Invalid_Point);
+      begin
+         return Find_Declaration
+           (Global_LI_File,
+            Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last),
+            "",
+            First_FD_Pos);
+      exception
+         when Declaration_Not_Found =>
+            return null;
+      end;
+   end Find_First_Forward_Declaration;
+
 begin
    Info ("Sym_FU_Hanlder: """
          & Sym.Buffer (Sym.Class.First .. Sym.Class.Last) & "."
@@ -37,7 +126,6 @@ begin
             Target_Kind := Non_Generic_Function_Or_Operator;
          end if;
          End_Position := MI_Tab.End_Position;
-         Free (MI_Tab);
       exception
          when DB_Error | Not_Found =>
             Fail ("unable to find method " &
@@ -55,7 +143,6 @@ begin
             Target_Kind := Non_Generic_Function_Or_Operator;
          end if;
          End_Position := FU_Tab.End_Position;
-         Free (FU_Tab);
       exception
          when DB_Error | Not_Found =>
             Fail ("unable to find function " &
@@ -72,6 +159,7 @@ begin
    --  We should also try to find GPS declaration created during
    --  FD processing and not create new declaration
    if Sym.Symbol = MI then
+      Free (MI_Tab);
       begin
          Set_Cursor
            (SN_Table (MD),
@@ -109,40 +197,12 @@ begin
             null;
       end;
    else
-      begin
-         Set_Cursor
-           (SN_Table (FD),
-            By_Key,
-            Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last)
-               & Field_Sep,
-            False);
-         P := Get_Pair (SN_Table (FD), Next_By_Key);
-         if P /= null then
-            FD_Tab := Parse_Pair (P.all);
-            Free (P);
-            P := Get_Pair (SN_Table (FD), Next_By_Key);
-            if P = null then -- not overloaded fwd declaration
-               Body_Position  := Sym.Start_Position;
-               Start_Position := FD_Tab.Start_Position;
-               begin -- locate forward declaration
-                  Decl_Info := Find_Declaration
-                    (Global_LI_File,
-                     Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last),
-                     "",
-                     FD_Tab.Start_Position);
-               exception
-                  when Declaration_Not_Found =>
-                     null;
-               end;
-            else -- overloaded or many forward declarations
-               Free (P);
-            end if;
-            Free (FD_Tab);
-         end if;
-      exception
-         when DB_Error =>
-            null;
-      end;
+      --  Try to find forward declaration
+      Decl_Info      := Find_First_Forward_Declaration (FU_Tab);
+      Body_Position  := Sym.Start_Position;
+      Start_Position.Line   := Decl_Info.Value.Declaration.Location.Line;
+      Start_Position.Column := Decl_Info.Value.Declaration.Location.Column;
+      Free (FU_Tab);
    end if;
 
    if Decl_Info = null then
@@ -160,6 +220,7 @@ begin
          End_Of_Scope_Location => End_Position,
          Declaration_Info      => Decl_Info);
    else
+      Decl_Info.Value.Declaration.End_Of_Scope.Kind := End_Of_Body;
       Decl_Info.Value.Declaration.End_Of_Scope.Location.Line
          := End_Position.Line;
       Decl_Info.Value.Declaration.End_Of_Scope.Location.Column
