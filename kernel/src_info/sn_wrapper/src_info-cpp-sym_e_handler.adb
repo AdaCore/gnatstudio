2356f4370885016c0d19497460c4767bba4b609e
Warren Paul <paul@adacore.com>
2002-03-11 13:35:30 +0000

    Polishing.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@73989 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/sn_wrapper/src_info-cpp-sym_e_handler.adb
+++ kernel/src_info/sn_wrapper/src_info-cpp-sym_e_handler.adb
@@ -7,18 +7,17 @@ separate (Src_Info.CPP)
 procedure Sym_E_Handler (Sym : FIL_Table)
 is
    Decl_Info : E_Declaration_Info_List;
+   E_Id      : String := Sym.Buffer
+     (Sym.Identifier.First .. Sym.Identifier.Last);
 begin
 
-   Info ("Sym_E_Hanlder: """
-         & Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last)
-         & """");
+   Info ("Sym_E_Hanlder: '" & E_Id & "'");
 
    Insert_Declaration
      (Handler           => LI_Handler (Global_CPP_Handler),
       File              => Global_LI_File,
       List              => Global_LI_File_List,
-      Symbol_Name       =>
-        Sym.Buffer (Sym.Identifier.First .. Sym.Identifier.Last),
+      Symbol_Name       => E_Id,
       Source_Filename   =>
         Sym.Buffer (Sym.File_Name.First .. Sym.File_Name.Last),
       Location          => Sym.Start_Position,
