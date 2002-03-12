fb8648c211e06b024186e1c0dea09735d58037e2
Alexey Zakharov (Elbrus) <zakharov@elbrus-tech.ru>
2002-03-12 13:22:46 +0000

    New Handler added
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@74030 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ kernel/src_info/sn_wrapper/src_info-cpp-fu_to_cl_handler.adb
@@ -0,0 +1,94 @@
+separate (Src_Info.CPP)
+
+
+------------------------
+--  Fu_To_Cl_Handler  --
+------------------------
+
+procedure Fu_To_Cl_Handler (Ref : TO_Table)
+is
+   Class_Desc : CType_Description;
+   Class_Def  : CL_Table;
+   Success    : Boolean;
+   Decl_Info  : E_Declaration_Info_List;
+begin
+   Info ("Fu_To_Cl_Handler: """
+         & Ref.Buffer (Ref.Referred_Symbol_Name.First ..
+               Ref.Referred_Symbol_Name.Last)
+         & """");
+   Find_Class
+     (Type_Name      => Ref.Buffer
+         (Ref.Referred_Symbol_Name.First .. Ref.Referred_Symbol_Name.Last),
+      Desc           => Class_Desc,
+      Class_Def      => Class_Def,
+      Success        => Success);
+   if not Success then
+      Warn ("Class " & Ref.Buffer (Ref.Referred_Symbol_Name.First ..
+               Ref.Referred_Symbol_Name.Last) & " is not found in SN DB");
+      return;
+   end if;
+   if Ref.Buffer (Ref.File_Name.First .. Ref.File_Name.Last) /=
+         Class_Def.Buffer (Class_Def.File_Name.First ..
+                                    Class_Def.File_Name.Last)
+   then
+      begin
+         Decl_Info :=
+           Find_Dependency_Declaration
+             (File        => Global_LI_File,
+              Symbol_Name => Class_Def.Buffer
+                (Class_Def.Name.First .. Class_Def.Name.Last),
+              Kind        => Record_Type,
+              Location    => Class_Def.Start_Position,
+              Filename    => Class_Def.Buffer
+                (Class_Def.File_Name.First .. Class_Def.File_Name.Last));
+      exception
+         when Declaration_Not_Found =>
+            Insert_Dependency_Declaration
+              (Handler            => LI_Handler (Global_CPP_Handler),
+               File               => Global_LI_File,
+               List               => Global_LI_File_List,
+               Symbol_Name        => Class_Def.Buffer
+                 (Class_Def.Name.First .. Class_Def.Name.Last),
+               Referred_Filename  => Class_Def.Buffer
+                 (Class_Def.File_Name.First .. Class_Def.File_Name.Last),
+               Source_Filename    => Class_Def.Buffer
+                 (Ref.File_Name.First .. Ref.File_Name.Last),
+               Location           => Class_Def.Start_Position,
+               Kind               => Record_Type,
+               Scope              => Global_Scope,
+               Declaration_Info   => Decl_Info);
+      end;
+   else
+      begin
+         Decl_Info :=
+           Find_Declaration
+             (File        => Global_LI_File,
+              Symbol_Name => Class_Def.Buffer
+                (Class_Def.Name.First .. Class_Def.Name.Last),
+              Kind        => Record_Type,
+              Location    => Class_Def.Start_Position);
+      exception
+         when Declaration_Not_Found =>
+            Insert_Declaration
+              (Handler            => LI_Handler (Global_CPP_Handler),
+               File               => Global_LI_File,
+               List               => Global_LI_File_List,
+               Symbol_Name        => Class_Def.Buffer
+                 (Class_Def.Name.First .. Class_Def.Name.Last),
+               Source_Filename    => Class_Def.Buffer
+                 (Ref.File_Name.First .. Ref.File_Name.Last),
+               Location           => Class_Def.Start_Position,
+               Kind               => Record_Type,
+               Scope              => Global_Scope,
+               Declaration_Info   => Decl_Info);
+      end;
+   end if;
+   Insert_Reference
+     (File              => Global_LI_File,
+      Declaration_Info  => Decl_Info,
+      Source_Filename   => Ref.Buffer
+        (Ref.File_Name.First .. Ref.File_Name.Last),
+      Location          => Ref.Position,
+      Kind              => Reference);
+   Free (Class_Def);
+end Fu_To_Cl_Handler;
