5fb33e73dc5dd26566ff15a7ad15781a48a13078
Taras Galchenko (Elbrus) <galchenko@ot.ru>
2002-03-14 12:16:47 +0000

    Implemented handling of pure virtual methods. Overloading is now considered by method declarations not implementations
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@74091 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/sn_wrapper/src_info-cpp-fu_to_mi_handler.adb
+++ kernel/src_info/sn_wrapper/src_info-cpp-fu_to_mi_handler.adb
@@ -8,13 +8,19 @@ separate (Src_Info.CPP)
 procedure Fu_To_Mi_Handler (Ref : TO_Table) is
    P            : Pair_Ptr;
    Fn           : MI_Table;
+   MDecl        : MD_Table;
+   MDecl_Tmp    : MD_Table;
    Decl_Info    : E_Declaration_Info_List;
    Overloaded   : Boolean := False;
    Init         : Boolean := True;
+   Pure_Virtual : Boolean := False;
    Kind         : E_Kind;
    IsTemplate   : Boolean := False;
    Ref_Id       : constant String := Ref.Buffer
      (Ref.Referred_Symbol_Name.First .. Ref.Referred_Symbol_Name.Last);
+   Ref_Class    : constant String := Ref.Buffer
+     (Ref.Referred_Class.First .. Ref.Referred_Class.Last);
+   Attributes   : SN_Attributes;
 
    function Find_Method (Fn : MI_Table) return E_Declaration_Info_List;
    --  searches for forward declaration. if no fwd decl found, searches for
@@ -51,47 +57,85 @@ begin
    Info ("Fu_To_Mi_Handler: '" & Ref_Id & "'");
 
    Set_Cursor
-     (SN_Table (MI),
+     (SN_Table (MD),
       By_Key,
-      Ref.Buffer (Ref.Referred_Class.First ..
-                  Ref.Referred_Class.Last)
-         & Field_Sep
-         & Ref_Id
-         & Field_Sep,
+      Ref_Class & Field_Sep & Ref_Id & Field_Sep,
       False);
 
    loop
-      P := Get_Pair (SN_Table (MI), Next_By_Key);
+      P := Get_Pair (SN_Table (MD), Next_By_Key);
       exit when P = null;
-      Overloaded := not Init;
-      if Init then
-         Fn   := Parse_Pair (P.all);
-         Init := False;
-      end if;
+      MDecl_Tmp := Parse_Pair (P.all);
       Free (P);
+      if Cmp_Arg_Types
+        (MDecl_Tmp.Buffer,
+         Ref.Buffer,
+         MDecl_Tmp.Arg_Types,
+         Ref.Caller_Argument_Types)
+      then
+         Overloaded := not Init;
+         if Init then
+            Init  := False;
+            MDecl := MDecl_Tmp;
+         else
+            Free (MDecl_Tmp);
+         end if;
+      else
+         Free (MDecl_Tmp);
+      end if;
       exit when Overloaded;
    end loop;
 
-   if Init then -- referred method not found
+   if Init then -- declaration for the referred method not found
       --  ??? We should handle this situation in a special way:
-      --  Abstract methods are only in .md table, but they are referenced
-      --  (in .to table) like MI symbols (see cpp_abstract test)
-      Warn ("Referred method " & Ref_Id & " not found");
+      Warn ("Referred method " & Ref_Class & "::" & Ref_Id & " not found");
       return;
    end if;
 
+   --  Once we have found the declaration(s) we may try to look up
+   --  implementation as well
    if not Overloaded then
+      Set_Cursor
+        (SN_Table (MI),
+         By_Key,
+         Ref_Class & Field_Sep & Ref_Id & Field_Sep,
+         False);
+
+      Init := True;
+      loop
+         P := Get_Pair (SN_Table (MI), Next_By_Key);
+         exit when P = null;
+         Fn := Parse_Pair (P.all);
+         Free (P);
+         Init := False;
+         exit when Cmp_Arg_Types
+           (MDecl_Tmp.Buffer,
+            Fn.Buffer,
+            MDecl_Tmp.Arg_Types,
+            Fn.Arg_Types);
+         Init := True;
+         Free (Fn);
+      end loop;
+
+      if Init then -- implementation for the referred method not found
+         --  this must be a pure virtual method
+         Attributes := SN_Attributes (MDecl.Attributes);
+         if (Attributes and SN_PUREVIRTUAL) /= SN_PUREVIRTUAL then
+            Fail ("Failed to locate method implementation, but it is not"
+               & " an abstract one: " & Ref_Class & "::" & Ref_Id);
+            Free (MDecl);
+            return;
+         end if;
+         Pure_Virtual := True;
+      end if;
+
       --  If method
       --    defined in the current file => add reference
       --    defined in another file => add dep decl and reference it
       declare
          Class_Def    : CL_Table;
       begin -- check if this class is template
-         Class_Def := Find
-           (SN_Table (CL),
-            Ref.Buffer (Ref.Referred_Class.First ..
-                        Ref.Referred_Class.Last));
-
+         Class_Def := Find (SN_Table (CL), Ref_Class);
          IsTemplate := Class_Def.Template_Parameters.First
             < Class_Def.Template_Parameters.Last;
          Free (Class_Def);
@@ -99,7 +143,8 @@ begin
          when DB_Error | Not_Found =>
             null;
       end;
-      if Fn.Buffer (Fn.Return_Type.First .. Fn.Return_Type.Last)
+
+      if MDecl.Buffer (MDecl.Return_Type.First .. MDecl.Return_Type.Last)
             = "void" then
          if IsTemplate then
             Kind := Generic_Function_Or_Operator;
@@ -113,13 +158,24 @@ begin
             Kind := Non_Generic_Procedure;
          end if;
       end if;
-      if Fn.Buffer (Fn.File_Name.First .. Fn.File_Name.Last)
+      if (Pure_Virtual
+         and then MDecl.Buffer (MDecl.File_Name.First .. MDecl.File_Name.Last)
+            = Get_LI_Filename (Global_LI_File))
+         or else Fn.Buffer (Fn.File_Name.First .. Fn.File_Name.Last)
             = Get_LI_Filename (Global_LI_File) then
          begin
             --  this is a method defined in the current file
             --  it may be either forward declared or implemented
             --  right away
-            Decl_Info := Find_Method (Fn);
+            if Pure_Virtual then
+               Decl_Info := Find_Declaration
+                 (File           => Global_LI_File,
+                  Symbol_Name    => Ref_Id,
+                  Class_Name     => Ref_Class,
+                  Location       => MDecl.Start_Position);
+            else
+               Decl_Info := Find_Method (Fn);
+            end if;
          exception
             when Declaration_Not_Found =>
                --  method is in the current file, but used before
@@ -128,8 +184,7 @@ begin
                  (Handler            => LI_Handler (Global_CPP_Handler),
                   File               => Global_LI_File,
                   List               => Global_LI_File_List,
-                  Symbol_Name        =>
-                     Fn.Buffer (Fn.Name.First .. Fn.Name.Last),
+                  Symbol_Name        => Ref_Id,
                   Source_Filename    =>
                      Ref.Buffer (Ref.File_Name.First .. Ref.File_Name.Last),
                   Location           => Ref.Position,
@@ -142,10 +197,8 @@ begin
             --  this method is defined somewhere else...
             Decl_Info := Find_Dependency_Declaration
               (File                 => Global_LI_File,
-               Symbol_Name          => Fn.Buffer
-                 (Fn.Name.First .. Fn.Name.Last),
-               Class_Name          => Fn.Buffer
-                 (Fn.Class.First .. Fn.Class.Last),
+               Symbol_Name          => Ref_Id,
+               Class_Name           => Ref_Class,
                Filename             => Fn.Buffer
                  (Fn.File_Name.First .. Fn.File_Name.Last),
                Location             => Fn.Start_Position);
@@ -155,8 +208,7 @@ begin
                  (Handler            => LI_Handler (Global_CPP_Handler),
                   File               => Global_LI_File,
                   List               => Global_LI_File_List,
-                  Symbol_Name        =>
-                     Fn.Buffer (Fn.Name.First .. Fn.Name.Last),
+                  Symbol_Name        => Ref_Id,
                   Source_Filename    =>
                      Ref.Buffer (Ref.File_Name.First .. Ref.File_Name.Last),
                   Location           => Fn.Start_Position,
@@ -168,11 +220,13 @@ begin
          end;
       end if;
    else -- overloaded entity
+      Free (MDecl);
       --  have we already declared it?
       begin
          Decl_Info := Find_Declaration
            (File        => Global_LI_File,
-            Symbol_Name => Fn.Buffer (Fn.Name.First .. Fn.Name.Last),
+            Symbol_Name => Ref_Id,
+            Class_Name  => Ref_Class,
             Kind        => Overloaded_Entity);
       exception
          when Declaration_Not_Found =>
