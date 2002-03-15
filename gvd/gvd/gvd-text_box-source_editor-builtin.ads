cb883bf48680840ae10c9dc1edd94b391a66edf4
Nicolas Setton <setton@adacore.com>
2002-03-15 15:31:05 +0000

    (Set_Line): new parameter Process. Add 2002 in header.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@74162 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/gvd-text_box-source_editor-builtin.ads
+++ gvd/gvd/gvd-text_box-source_editor-builtin.ads
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                   GVD - The GNU Visual Debugger                   --
 --                                                                   --
---                      Copyright (C) 2000-2001                      --
+--                      Copyright (C) 2000-2002                      --
 --                              ACT-Europe                           --
 --                                                                   --
 -- GVD is free  software;  you can redistribute it and/or modify  it --
@@ -143,7 +143,8 @@ package GVD.Text_Box.Source_Editor.Builtin is
    procedure Set_Line
      (Editor      : access Builtin_Record;
       Line        : Natural;
-      Set_Current : Boolean := True);
+      Set_Current : Boolean := True;
+      Process     : Gtk_Widget);
    --  See GVD.Text_Boxes.Source_Editor for more information.
 
    function Get_Line (Editor : access Builtin_Record) return Natural;
