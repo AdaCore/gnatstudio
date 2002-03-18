dfb6530ec641df0810019ea416fe69058e52e307
Arnaud Charlet <charlet@adacore.com>
2002-03-18 10:50:39 +0000

    Further fix to header (hoping that GPS will run ci_check this time)
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@74175 936e1b1b-40f2-da11-902a-00137254ae57

--- browsers/src/browsers-canvas.adb
+++ browsers/src/browsers-canvas.adb
@@ -2,7 +2,7 @@
 --                               G P S                               --
 --                                                                   --
 --                        Copyright (C) 2001-2002                    --
---                            ACT-Europe                             --
+--                             ACT-Europe                            --
 --                                                                   --
 -- GLIDE is free software; you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
