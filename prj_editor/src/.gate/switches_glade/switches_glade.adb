cbc56c68645defb23562265a4c280ca2c0434b27
Emmanuel Briot <briot@adacore.com>
2002-03-20 09:22:25 +0000

    Sync with switches_glade.adb:1.30
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@74296 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/src/.gate/switches_glade/switches_glade.adb
+++ prj_editor/src/.gate/switches_glade/switches_glade.adb
@@ -4,11 +4,13 @@ with Gtk.Widget; use Gtk.Widget;
 with Switches_Editor_Pkg; use Switches_Editor_Pkg;
 with Naming_Scheme_Editor_Pkg; use Naming_Scheme_Editor_Pkg;
 with New_Variable_Editor_Pkg; use New_Variable_Editor_Pkg;
+with Foreign_Naming_Scheme_Editor_Pkg; use Foreign_Naming_Scheme_Editor_Pkg;
 
 procedure Switches_Glade is
    Switches_Editor : Switches_Editor_Access;
    Naming_Scheme_Editor : Naming_Scheme_Editor_Access;
    New_Variable_Editor : New_Variable_Editor_Access;
+   Foreign_Naming_Scheme_Editor : Foreign_Naming_Scheme_Editor_Access;
 
 begin
    Gtk.Main.Set_Locale;
@@ -19,5 +21,7 @@ begin
    Show_All (Naming_Scheme_Editor);
    Gtk_New (New_Variable_Editor);
    Show_All (New_Variable_Editor);
+   Gtk_New (Foreign_Naming_Scheme_Editor);
+   Show_All (Foreign_Naming_Scheme_Editor);
    Gtk.Main.Main;
 end Switches_Glade;
