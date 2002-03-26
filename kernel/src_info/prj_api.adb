6039ef27193483ed600e05f9d774c154d540e93b
Emmanuel Briot <briot@adacore.com>
2002-03-26 13:13:13 +0000

    (Finalize): Redo 1.105 checkin, since gnatmem is only confused because the  memory was allocated with malloc() instead of __gnat_malloc.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@74577 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/prj_api.adb
+++ kernel/src_info/prj_api.adb
@@ -4092,6 +4092,7 @@ package body Prj_API is
    procedure Finalize is
    begin
       Prj.Reset;
+      Prj.Tree.Tree_Private_Part.Project_Nodes.Free;
       Prj.Tree.Tree_Private_Part.Projects_Htable.Reset;
       Namet.Finalize;
       Stringt.Initialize;
