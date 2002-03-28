23247d302581f9ed8f1fa14eab6b3b449cc7b558
Arnaud Charlet <charlet@adacore.com>
2002-03-28 10:53:49 +0000

    Update header. Remove unused packages.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@74673 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/src/new_variable_editor_pkg.ads
+++ prj_editor/src/new_variable_editor_pkg.ads
@@ -1,10 +1,10 @@
 -----------------------------------------------------------------------
---                          G L I D E  I I                           --
+--                               G P S                               --
 --                                                                   --
 --                     Copyright (C) 2001-2002                       --
 --                            ACT-Europe                             --
 --                                                                   --
--- GLIDE is free software; you can redistribute it and/or modify  it --
+-- GPS  is free software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
 -- the Free Software Foundation; either version 2 of the License, or --
 -- (at your option) any later version.                               --
@@ -13,7 +13,7 @@
 -- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
 -- General Public License for more details. You should have received --
--- a copy of the GNU General Public License along with this library; --
+-- a copy of the GNU General Public License along with this program; --
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
@@ -27,10 +27,8 @@ with Gtk.GEntry; use Gtk.GEntry;
 with Gtk.Scrolled_Window; use Gtk.Scrolled_Window;
 with Gtk.Viewport; use Gtk.Viewport;
 with Gtk.Box; use Gtk.Box;
---  with Gtk.Clist; use Gtk.Clist;
 with Gtk.Hbutton_Box; use Gtk.Hbutton_Box;
 with Gtk.Button; use Gtk.Button;
-with Gtk.Object; use Gtk.Object;
 with Gtk.Tree_View; use Gtk.Tree_View;
 
 package New_Variable_Editor_Pkg is
