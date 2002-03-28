43cdf462c665c3af536e46b63bc7c50fb0412292
Emmanuel Briot <briot@adacore.com>
2002-03-28 10:42:26 +0000

    Remove unneeded withs.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@74670 936e1b1b-40f2-da11-902a-00137254ae57

--- vsearch/src/vsearch_pkg.adb
+++ vsearch/src/vsearch_pkg.adb
@@ -18,14 +18,10 @@
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
-with Glib; use Glib;
 with Gtk; use Gtk;
-with Gdk.Types;       use Gdk.Types;
-with Gtk.Widget;      use Gtk.Widget;
 with Gtk.Enums;       use Gtk.Enums;
-with Gtkada.Handlers; use Gtkada.Handlers;
-with Callbacks_Vsearch; use Callbacks_Vsearch;
 with Glide_Intl; use Glide_Intl;
+with Gtk.Tooltips; use Gtk.Tooltips;
 
 package body Vsearch_Pkg is
 
