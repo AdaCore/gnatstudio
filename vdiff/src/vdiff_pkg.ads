b97b5222d6f44e5afeb984bf8856a5ceca85b890
Arnaud Charlet <charlet@adacore.com>
2002-03-28 09:52:18 +0000

    Update header. Remove unused package.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@74653 936e1b1b-40f2-da11-902a-00137254ae57

--- vdiff/src/vdiff_pkg.ads
+++ vdiff/src/vdiff_pkg.ads
@@ -1,10 +1,10 @@
 -----------------------------------------------------------------------
---                          G L I D E  I I                           --
+--                               G P S                               --
 --                                                                   --
---                        Copyright (C) 2001                         --
+--                     Copyright (C) 2001-2002                       --
 --                            ACT-Europe                             --
 --                                                                   --
--- GLIDE is free software; you can redistribute it and/or modify  it --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
 -- the Free Software Foundation; either version 2 of the License, or --
 -- (at your option) any later version.                               --
@@ -13,7 +13,7 @@
 -- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
 -- General Public License for more details. You should have received --
--- a copy of the GNU General Public License along with this library; --
+-- a copy of the GNU General Public License along with this program; --
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
@@ -25,7 +25,7 @@ with Gtk.Frame; use Gtk.Frame;
 with Gtk.Drawing_Area; use Gtk.Drawing_Area;
 with Gtk.Scrolled_Window; use Gtk.Scrolled_Window;
 with Gtk.Clist; use Gtk.Clist;
-with Gtk.Button; use Gtk.Button;
+
 package Vdiff_Pkg is
 
    type Vdiff_Record is new Gtk_Window_Record with record
