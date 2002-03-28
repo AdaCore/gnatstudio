961342db1f9ecbef492ad9a7345d2aba6bde4f53
Arnaud Charlet <charlet@adacore.com>
2002-03-28 11:30:35 +0000

    Add header. Remove unused packages.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@74680 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/src/foreign_naming_scheme_editor_pkg.adb
+++ prj_editor/src/foreign_naming_scheme_editor_pkg.adb
@@ -1,11 +1,26 @@
-with Glib; use Glib;
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2002                         --
+--                            ACT-Europe                             --
+--                                                                   --
+-- GPS is free  software; you can  redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
 with Gtk; use Gtk;
-with Gdk.Types;       use Gdk.Types;
-with Gtk.Widget;      use Gtk.Widget;
 with Gtk.Enums;       use Gtk.Enums;
 with Gtkada.Handlers; use Gtkada.Handlers;
-with Callbacks_Switches_Glade; use Callbacks_Switches_Glade;
---  with Switches_Glade_Intl; use Switches_Glade_Intl;
 with Glide_Intl;      use Glide_Intl;
 with Foreign_Naming_Scheme_Editor_Pkg.Callbacks; use Foreign_Naming_Scheme_Editor_Pkg.Callbacks;
 
