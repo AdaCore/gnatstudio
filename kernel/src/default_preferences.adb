388fa5758c9b6b923d6f9afb850ebaaaff0a0893
Emmanuel Briot <briot@adacore.com>
2002-04-02 10:30:56 +0000

    (Register_Property): New parameters, in preparation of the graphical preferences  editor. (Save_Default_Preferencees, Find_Default_Pref): New subprograms, so that the  visible dependency on Glib.XML can be hidden. (Get_Description, Get_GUI_Description, Get_Page, Get_Type, Is_Editable,  Get_All_Preferences): New subprograms
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@74742 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/default_preferences.adb
+++ kernel/src/default_preferences.adb
@@ -1,10 +1,10 @@
 -----------------------------------------------------------------------
---                          G L I D E  I I                           --
+--                               G P S                               --
 --                                                                   --
 --                     Copyright (C) 2001-2002                       --
 --                            ACT-Europe                             --
 --                                                                   --
--- GLIDE is free software; you can redistribute it and/or modify  it --
+-- GPS is free  software; you can  redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
 -- the Free Software Foundation; either version 2 of the License, or --
 -- (at your option) any later version.                               --
@@ -20,28 +20,46 @@
 
 with Glib;              use Glib;
 with Glib.Properties;   use Glib.Properties;
-with Glib.Xml_Int;      use Glib.Xml_Int;
+with Glib.XML;
+with Gdk.Color;         use Gdk.Color;
+with Gdk.Font;          use Gdk.Font;
+with Basic_Types;       use Basic_Types;
 
 package body Default_Preferences is
+
+   Internal_Color_Type : constant GType := GType'Last;
+   Internal_Font_Type  : constant GType := GType'Last - 1;
+   --  Internal types to represent the colors and fonts. We cannot use
+   --  Gdk.Color.Gdk_Color_Type or Gdk.Font.Get_Type, since gdk hasn't been
+   --  initialized when the preferences are registered.
+
+   type Property_Description is record
+      Description     : String_Access;
+      GUI_Description : String_Access;
+      Page            : String_Access;
+      Prop_Type       : Glib.GType;
+      Editable        : Boolean;
+   end record;
+
+   package Property_XML is new Glib.XML (Property_Description);
+   use Property_XML;
+   --  ??? We could register the description as attributes of the XML nodes.
 
    Default_Preferences : Node_Ptr;
    --  All the preferences and their default values.
    --  This is a global variable, but is only modified at elaboration time when
    --  the preferences are declared.
 
-   procedure Create_Default_Pref (Name : String; Default : String);
+   procedure Create_Default_Pref
+     (Name, Default, Description, GUI_Description, Page : String;
+      Prop_Type : Glib.GType;
+      Editable  : Boolean);
    --  Create a new node in the default_preferences tree for the preference
    --  Name.
    --  Default is the default value for that property
 
-   -----------------------------
-   -- Get_Default_Preferences --
-   -----------------------------
-
-   function Get_Default_Preferences return Node_Ptr is
-   begin
-      return Default_Preferences;
-   end Get_Default_Preferences;
+   function Find_Default_Pref (Name : String) return Node_Ptr;
+   --  Return the node for the preference Name
 
    -----------------------
    -- Find_Default_Pref --
@@ -55,11 +73,29 @@ package body Default_Preferences is
       return Node;
    end Find_Default_Pref;
 
+   -----------------------
+   -- Find_Default_Pref --
+   -----------------------
+
+   function Find_Default_Pref (Name : String) return String is
+      N : constant Node_Ptr := Find_Default_Pref (Name);
+   begin
+      if N /= null then
+         return N.Value.all;
+      else
+         return "";
+      end if;
+   end Find_Default_Pref;
+
    -------------------------
    -- Create_Default_Pref --
    -------------------------
 
-   procedure Create_Default_Pref (Name : String; Default : String) is
+   procedure Create_Default_Pref
+     (Name, Default, Description, GUI_Description, Page : String;
+      Prop_Type : Glib.GType;
+      Editable  : Boolean)
+   is
       N : Node_Ptr;
    begin
       if Default_Preferences = null then
@@ -74,10 +110,20 @@ package body Default_Preferences is
          N.Tag := new String' (Name);
          Add_Child (Default_Preferences, N);
       else
-         Glib.Xml_Int.Free (N.Value);
+         Property_XML.Free (N.Value);
       end if;
 
       N.Value := new String' (Default);
+
+      Free (N.Specific_Data.Description);
+      Free (N.Specific_Data.GUI_Description);
+      Free (N.Specific_Data.Page);
+      N.Specific_Data :=
+        (Description     => new String' (Description),
+         GUI_Description => new String' (GUI_Description),
+         Page            => new String' (Page),
+         Prop_Type       => Prop_Type,
+         Editable        => Editable);
    end Create_Default_Pref;
 
    -----------------------
@@ -85,49 +131,230 @@ package body Default_Preferences is
    -----------------------
 
    function Register_Property
-     (Name : String; Default : Glib.Gint)
+     (Name            : String;
+      Default         : Glib.Gint;
+      Description     : String;
+      GUI_Description : String;
+      Page            : String;
+      Editable        : Boolean := True)
       return Glib.Properties.Property_Int is
    begin
-      Create_Default_Pref (Name, Gint'Image (Default));
+      Create_Default_Pref
+        (Name, Gint'Image (Default),
+         Description, GUI_Description, Page, GType_Int, Editable);
       return Build (Name);
    end Register_Property;
 
+   -----------------------
+   -- Register_Property --
+   -----------------------
+
    function Register_Property
-     (Name : String; Default : Glib.Guint)
+     (Name            : String;
+      Default         : Glib.Guint;
+      Description     : String;
+      GUI_Description : String;
+      Page            : String;
+      Editable        : Boolean := True)
       return Glib.Properties.Property_Uint is
    begin
-      Create_Default_Pref (Name, Guint'Image (Default));
+      Create_Default_Pref
+        (Name, Guint'Image (Default),
+         Description, GUI_Description, Page, GType_Uint, Editable);
       return Build (Name);
    end Register_Property;
 
+   -----------------------
+   -- Register_Property --
+   -----------------------
+
    function Register_Property
-     (Name : String; Default : Boolean)
+     (Name            : String;
+      Default         : Boolean;
+      Description     : String;
+      GUI_Description : String;
+      Page            : String;
+      Editable        : Boolean := True)
       return Glib.Properties.Property_Boolean is
    begin
-      Create_Default_Pref (Name, Boolean'Image (Default));
+      Create_Default_Pref
+        (Name, Boolean'Image (Default),
+         Description, GUI_Description, Page, GType_Boolean, Editable);
       return Build (Name);
    end Register_Property;
 
+   -----------------------
+   -- Register_Property --
+   -----------------------
+
    function Register_Property
-     (Name : String; Default : String)
+     (Name            : String;
+      Default         : String;
+      Description     : String;
+      GUI_Description : String;
+      Page            : String;
+      Editable        : Boolean := True)
       return Glib.Properties.Property_String is
    begin
-      Create_Default_Pref (Name, Default);
+      Create_Default_Pref
+        (Name, Default,
+         Description, GUI_Description, Page, GType_String, Editable);
       return Build (Name);
    end Register_Property;
 
+   -----------------------
+   -- Register_Property --
+   -----------------------
+
    function Register_Property
-     (Name : String; Default : String) return Property_Color is
+     (Name            : String;
+      Default         : String;
+      Description     : String;
+      GUI_Description : String;
+      Page            : String;
+      Editable        : Boolean := True)
+      return Property_Color is
    begin
-      Create_Default_Pref (Name, Default);
+      Create_Default_Pref
+        (Name, Default,
+         Description, GUI_Description, Page, Internal_Color_Type, Editable);
       return Build (Name);
    end Register_Property;
 
+   -----------------------
+   -- Register_Property --
+   -----------------------
+
    function Register_Property
-     (Name : String; Default : String) return Property_Font is
+     (Name            : String;
+      Default         : String;
+      Description     : String;
+      GUI_Description : String;
+      Page            : String;
+      Editable        : Boolean := True)
+      return Property_Font is
    begin
-      Create_Default_Pref (Name, Default);
+      Create_Default_Pref
+        (Name, Default,
+         Description, GUI_Description, Page, Internal_Font_Type, Editable);
       return Build (Name);
    end Register_Property;
+
+   ------------------------------
+   -- Save_Default_Preferences --
+   ------------------------------
+
+   procedure Save_Default_Preferences (File_Name : String) is
+   begin
+      Print (Default_Preferences, File_Name => File_Name);
+   end Save_Default_Preferences;
+
+   ---------------------
+   -- Get_Description --
+   ---------------------
+
+   function Get_Description (Name : String) return String is
+      N : constant Node_Ptr := Find_Default_Pref (Name);
+   begin
+      if N /= null then
+         return N.Specific_Data.Description.all;
+      else
+         return "";
+      end if;
+   end Get_Description;
+
+   -------------------------
+   -- Get_GUI_Description --
+   -------------------------
+
+   function Get_GUI_Description (Name : String) return String is
+      N : constant Node_Ptr := Find_Default_Pref (Name);
+   begin
+      if N /= null then
+         return N.Specific_Data.GUI_Description.all;
+      else
+         return "";
+      end if;
+   end Get_GUI_Description;
+
+   --------------
+   -- Get_Page --
+   --------------
+
+   function Get_Page (Name : String) return String is
+      N : constant Node_Ptr := Find_Default_Pref (Name);
+   begin
+      if N /= null then
+         return N.Specific_Data.Page.all;
+      else
+         return "General";
+      end if;
+   end Get_Page;
+
+   --------------
+   -- Get_Type --
+   --------------
+
+   function Get_Type (Name : String) return Glib.GType is
+      N : constant Node_Ptr := Find_Default_Pref (Name);
+   begin
+      if N /= null then
+         case N.Specific_Data.Prop_Type is
+            when Internal_Color_Type =>
+               N.Specific_Data.Prop_Type := Gdk_Color_Type;
+            when Internal_Font_Type =>
+               N.Specific_Data.Prop_Type := Gdk.Font.Get_Type;
+            when others =>
+               null;
+         end case;
+
+         return N.Specific_Data.Prop_Type;
+      else
+         return GType_Int;
+      end if;
+   end Get_Type;
+
+   -----------------
+   -- Is_Editable --
+   -----------------
+
+   function Is_Editable (Name : String) return Boolean is
+      N : constant Node_Ptr := Find_Default_Pref (Name);
+   begin
+      if N /= null then
+         return N.Specific_Data.Editable;
+      else
+         return True;
+      end if;
+   end Is_Editable;
+
+   -------------------------
+   -- Get_All_Preferences --
+   -------------------------
+
+   function Get_All_Preferences return Basic_Types.String_Array is
+      Count : Natural := 0;
+      N : Node_Ptr := Default_Preferences.Child;
+   begin
+      while N /= null loop
+         Count := Count + 1;
+         N := N.Next;
+      end loop;
+
+      declare
+         Prefs : String_Array (1 .. Count);
+      begin
+         N := Default_Preferences.Child;
+         Count := 1;
+
+         while N /= null loop
+            Prefs (Count) := new String' (N.Tag.all);
+            Count := Count + 1;
+            N := N.Next;
+         end loop;
+
+         return Prefs;
+      end;
+   end Get_All_Preferences;
 
 end Default_Preferences;
--- kernel/src/default_preferences.ads
+++ kernel/src/default_preferences.ads
@@ -22,10 +22,10 @@
 --  Note that all the preferences must be registered through this package
 --  before being available to the user.
 --  This package is separated from Glide_Kernel.Preferences for elaboration
---  circularity reaons.
+--  circularity reasons.
 
 with Glib.Properties;
-with Glib.Xml_Int;
+with Basic_Types;
 
 package Default_Preferences is
 
@@ -33,30 +33,94 @@ package Default_Preferences is
    type Property_Font  is new Glib.Property;
 
    function Register_Property
-     (Name : String; Default : Glib.Gint)
-      return Glib.Properties.Property_Int;
-   function Register_Property
-     (Name : String; Default : Glib.Guint)
-      return Glib.Properties.Property_Uint;
-   function Register_Property
-     (Name : String; Default : Boolean)
-      return Glib.Properties.Property_Boolean;
-   function Register_Property
-     (Name : String; Default : String)
-      return Glib.Properties.Property_String;
-   function Register_Property
-     (Name : String; Default : String) return Property_Color;
-   function Register_Property
-     (Name : String; Default : String) return Property_Font;
+     (Name            : String;
+      Default         : Glib.Gint;
+      Description     : String;
+      GUI_Description : String;
+      Page            : String;
+      Editable        : Boolean := True) return Glib.Properties.Property_Int;
    --  Register a new property and its default value.
    --  The returned value should be used later on to retrieve the value of the
    --  property.
+   --  Description is the help string for this property. It is displayed in
+   --  tooltips in the preferences dialog.
+   --  GUI_Description is the label to use in the preferences dialog for this
+   --  property.
+   --  Page is the name of the preferences dialog page that should contain this
+   --  property. A new page will be created if no such page already exists. If
+   --  the name is of the form Page::subpage, then a notebook will be created
+   --  for Page, with a page called subpage.
+   --
+   --  If Editable is False, then this preference cannot be edited graphically
+   --  by the user, although it can be changed directly in the preferences
+   --  file.
 
-   function Find_Default_Pref
-     (Name : String) return Glib.Xml_Int.Node_Ptr;
-   --  Return the node in the default preferences that matches Name
+   function Register_Property
+     (Name            : String;
+      Default         : Glib.Guint;
+      Description     : String;
+      GUI_Description : String;
+      Page            : String;
+      Editable        : Boolean := True) return Glib.Properties.Property_Uint;
 
-   function Get_Default_Preferences return Glib.Xml_Int.Node_Ptr;
-   --  Return the default preferences node.
+   function Register_Property
+     (Name            : String;
+      Default         : Boolean;
+      Description     : String;
+      GUI_Description : String;
+      Page            : String;
+      Editable        : Boolean := True)
+      return Glib.Properties.Property_Boolean;
+
+   function Register_Property
+     (Name            : String;
+      Default         : String;
+      Description     : String;
+      GUI_Description : String;
+      Page            : String;
+      Editable        : Boolean := True)
+      return Glib.Properties.Property_String;
+
+   function Register_Property
+     (Name            : String;
+      Default         : String;
+      Description     : String;
+      GUI_Description : String;
+      Page            : String;
+      Editable        : Boolean := True) return Property_Color;
+
+   function Register_Property
+     (Name            : String;
+      Default         : String;
+      Description     : String;
+      GUI_Description : String;
+      Page            : String;
+      Editable        : Boolean := True) return Property_Font;
+
+   function Find_Default_Pref (Name : String) return String;
+   --  Return the value, as a string, in the default preferences that matches
+   --  Name
+
+   procedure Save_Default_Preferences (File_Name : String);
+   --  Save the default preferences to File_Name.
+
+   function Get_Description (Name : String) return String;
+   --  Return the full description of the Name preference.
+
+   function Get_GUI_Description (Name : String) return String;
+   --  Return the GUI description of the Name preference.
+
+   function Get_Page (Name : String) return String;
+   --  Return the name of the page for the Name preference.
+
+   function Get_Type (Name : String) return Glib.GType;
+   --  Return the type of the Name preference.
+
+   function Is_Editable (Name : String) return Boolean;
+   --  Return True if the preference can be edited graphically.
+
+   function Get_All_Preferences return Basic_Types.String_Array;
+   --  Return the name of all the registered preferences.
+   --  It is the responsability of the caller to free the returned array.
 
 end Default_Preferences;
