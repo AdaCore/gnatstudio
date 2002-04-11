a7b0c5f37cd01543ca7963c7f56d4c387bfa7248
Arnaud Charlet <charlet@adacore.com>
2002-04-11 15:55:50 +0000

    (Fill_Diff_Lists): Add the capability of displaying only chunks of diffs,  and not the unchanged parts, to ease finding actual changes. This is done  through a preference to accomodate all users.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@74948 936e1b1b-40f2-da11-902a-00137254ae57

--- vdiff/src/vdiff_utils.adb
+++ vdiff/src/vdiff_utils.adb
@@ -19,8 +19,6 @@
 -----------------------------------------------------------------------
 
 with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Strings; use Ada.Strings;
-with Ada.Strings.Fixed; use Ada.Strings.Fixed;
 with Interfaces.C.Strings;
 with Glib; use Glib;
 with Gdk.Color; use Gdk.Color;
@@ -31,6 +29,7 @@ with Glide_Kernel.Preferences; use Glide_Kernel.Preferences;
 with Gtk.Enums; use Gtk.Enums;
 with Gtk.Style; use Gtk.Style;
 with Vdiff_Pkg; use Vdiff_Pkg;
+with String_Utils; use String_Utils;
 
 package body Vdiff_Utils is
 
@@ -48,75 +47,98 @@ package body Vdiff_Utils is
       File2  : String;
       Diff   : Diff_Occurrence_Link)
    is
-      S             : String (1 .. 8192);
-      Last          : Natural;
-      Infile1       : Ada.Text_IO.File_Type;
-      Infile2       : Ada.Text_IO.File_Type;
-      Texts         : ICS.chars_ptr_array (0 .. 1);
-      Row           : Gint;
-      Line1         : Natural;
-      Line2         : Natural;
-      Offset1       : Natural;
-      Offset2       : Natural;
-      Link          : Diff_Occurrence_Link;
-      Default_Style : Gtk_Style;
-      Old_Style     : Gtk_Style;
-      Append_Style  : Gtk_Style;
-      Remove_Style  : Gtk_Style;
-      Change_Style  : Gtk_Style;
-      Color         : Gdk_Color;
-      Desc          : constant Pango_Font_Description :=
+      Context_Len     : Integer :=
+        Integer (Get_Pref (Kernel, Diff_Context_Length));
+      --  Number of lines displayed before and after each chunk of differences
+
+      Num_Line_Sep    : constant := 3;
+      --  Number of empty lines separating each chunk.
+
+      S               : String (1 .. 8192);
+      Last            : Natural;
+      Len             : Natural;
+      Infile1         : Ada.Text_IO.File_Type;
+      Infile2         : Ada.Text_IO.File_Type;
+      Texts           : ICS.chars_ptr_array (0 .. 1);
+      Row             : Gint;
+      Line1           : Natural;
+      Line2           : Natural;
+      Offset1         : Natural;
+      Offset2         : Natural;
+      Link            : Diff_Occurrence_Link;
+      Default_Style   : Gtk_Style;
+      Old_Style       : Gtk_Style;
+      Append_Style    : Gtk_Style;
+      Remove_Style    : Gtk_Style;
+      Change_Style    : Gtk_Style;
+      Color           : Gdk_Color;
+      Context_Changed : Boolean;
+      Desc            : constant Pango_Font_Description :=
         Get_Pref (Kernel, Default_Source_Editor_Font);
 
-      procedure Add_Blank_Line
+      procedure Add_Line
         (List  : access Gtk_Clist_Record'Class;
-         Style : Gtk_Style);
-      --  Add a blank line in List, using Style for font/color.
+         Style : Gtk_Style;
+         Line  : String := "");
+      --  Add a line constaining Line in List, using Style for font/color.
 
       procedure Read_Line
-        (Infile : File_Type;
-         List   : access Gtk_Clist_Record'Class;
-         Line   : Natural;
-         Style  : Gtk_Style);
+        (Infile  : File_Type;
+         List    : access Gtk_Clist_Record'Class;
+         Line    : Natural;
+         Style   : Gtk_Style;
+         Display : Boolean := True);
       --  Read the next line in Infile and add it in List, with the line
       --  number Line, using Style for font/color.
+      --  If Display is True, add the line in the list, otherwise only
+      --  read the line in Infile.
 
-      procedure Add_Blank_Line
+      procedure Add_Line
         (List  : access Gtk_Clist_Record'Class;
-         Style : Gtk_Style) is
+         Style : Gtk_Style;
+         Line  : String := "") is
       begin
          Texts (0) := ICS.Null_Ptr;
-         Texts (1) := ICS.Null_Ptr;
+         Texts (1) := ICS.New_String (Line);
          Row := Append (List, Texts);
          Set_Cell_Style (List, Row, 0, Style);
          Set_Cell_Style (List, Row, 1, Style);
-      end Add_Blank_Line;
+         ICS.Free (Texts (1));
+      end Add_Line;
 
       procedure Read_Line
-        (Infile : File_Type;
-         List   : access Gtk_Clist_Record'Class;
-         Line   : Natural;
-         Style  : Gtk_Style) is
+        (Infile  : File_Type;
+         List    : access Gtk_Clist_Record'Class;
+         Line    : Natural;
+         Style   : Gtk_Style;
+         Display : Boolean := True) is
       begin
          Get_Line (File => Infile, Item => S, Last => Last);
-         Texts (0) := ICS.New_String (Trim (Natural'Image (Line), Left));
-         Texts (1) := ICS.New_String (S (1 .. Last));
-         Row := Append (List, Texts);
-         Set_Cell_Style (List, Row, 0, Style);
-         Set_Cell_Style (List, Row, 1, Style);
-         ICS.Free (Texts (0));
-         ICS.Free (Texts (1));
+
+         if Display then
+            Texts (0) := ICS.New_String (Image (Line));
+            Texts (1) := ICS.New_String (S (1 .. Last));
+            Row := Append (List, Texts);
+            Set_Cell_Style (List, Row, 0, Style);
+            Set_Cell_Style (List, Row, 1, Style);
+            ICS.Free (Texts (0));
+            ICS.Free (Texts (1));
+         end if;
       end Read_Line;
 
    begin
+      if Context_Len = -1 then
+         Context_Len := Integer'Last;
+      end if;
+
       --  ??? When are these styles freed ?
       Default_Style := Copy (Get_Style (List1));
       Set_Font_Description (Default_Style, Desc);
 
-      Old_Style    := Copy (Default_Style);
-      Append_Style := Copy (Default_Style);
-      Remove_Style := Copy (Default_Style);
-      Change_Style := Copy (Default_Style);
+      Old_Style     := Copy (Default_Style);
+      Append_Style  := Copy (Default_Style);
+      Remove_Style  := Copy (Default_Style);
+      Change_Style  := Copy (Default_Style);
 
       --  <preferences>
       Set_Rgb (Color, 50000, 50000, 50000);
@@ -138,18 +160,50 @@ package body Vdiff_Utils is
       Link := Diff;
 
       while Link /= null loop
+         Context_Changed := False;
+
          for J in Line1 .. Link.Range1.First - 1 loop
-            Read_Line (Infile1, List1, J, Default_Style);
+            if J - Line1 < Context_Len
+              or else Link.Range1.First - J <= Context_Len
+            then
+               Read_Line (Infile1, List1, J, Default_Style);
+            else
+               Read_Line (Infile1, List1, J, Default_Style, False);
+
+               if not Context_Changed then
+                  for J in 1 .. Num_Line_Sep loop
+                     Add_Line (List1, Default_Style);
+                  end loop;
+
+                  Context_Changed := True;
+               end if;
+            end if;
          end loop;
 
+         Context_Changed := False;
+
          for J in Line2 .. Link.Range2.First - 1 loop
-            Read_Line (Infile2, List2, J, Default_Style);
+            if J - Line2 < Context_Len
+              or else Link.Range2.First - J <= Context_Len
+            then
+               Read_Line (Infile2, List2, J, Default_Style);
+            else
+               Read_Line (Infile2, List2, J, Default_Style, False);
+
+               if not Context_Changed then
+                  for J in 1 .. Num_Line_Sep loop
+                     Add_Line (List2, Default_Style);
+                  end loop;
+
+                  Context_Changed := True;
+               end if;
+            end if;
          end loop;
 
          case Link.Action is
             when Append =>
                for J in Link.Range2.First .. Link.Range2.Last - 1 loop
-                  Add_Blank_Line (List1, Old_Style);
+                  Add_Line (List1, Old_Style);
                end loop;
 
                for J in Link.Range2.First .. Link.Range2.Last - 1 loop
@@ -173,11 +227,11 @@ package body Vdiff_Utils is
 
                if Offset1 < Offset2 then
                   for J in Offset1 .. Offset2 - 1 loop
-                     Add_Blank_Line (List1, Old_Style);
+                     Add_Line (List1, Old_Style);
                   end loop;
                elsif Offset1 > Offset2 then
                   for J in Offset2 .. Offset1 - 1 loop
-                     Add_Blank_Line (List2, Change_Style);
+                     Add_Line (List2, Change_Style);
                   end loop;
                end if;
 
@@ -190,7 +244,7 @@ package body Vdiff_Utils is
                end loop;
 
                for J in Link.Range1.First .. Link.Range1.Last - 1 loop
-                  Add_Blank_Line (List2, Remove_Style);
+                  Add_Line (List2, Remove_Style);
                end loop;
 
                Line1 := Link.Range1.Last;
@@ -202,14 +256,18 @@ package body Vdiff_Utils is
 
       --  Complete files with the remaining lines.
 
-      while not End_Of_File (Infile1) loop
+      Len := 0;
+      while Len < Context_Len and then not End_Of_File (Infile1) loop
          Read_Line (Infile1, List1, Line1, Default_Style);
          Line1 := Line1 + 1;
+         Len   := Len + 1;
       end loop;
 
-      while not End_Of_File (Infile2) loop
+      Len := 0;
+      while Len < Context_Len and then not End_Of_File (Infile2) loop
          Read_Line (Infile2, List2, Line2, Default_Style);
          Line2 := Line2 + 1;
+         Len   := Len + 1;
       end loop;
 
       Thaw (List2);
