f11ebf97490196db7e2ddda244e09b9a906388ca
Arnaud Charlet <charlet@adacore.com>
2002-04-15 09:31:21 +0000

    Initial revision.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@75026 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ kernel/src/glide_kernel-timeout.adb
@@ -0,0 +1,158 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                     Copyright (C) 2001-2002                       --
+--                            ACT-Europe                             --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Glib;                 use Glib;
+with Gtk.Main;             use Gtk.Main;
+
+with GNAT.Expect;          use GNAT.Expect;
+pragma Warnings (Off);
+with GNAT.Expect.TTY;      use GNAT.Expect.TTY;
+pragma Warnings (On);
+
+with GNAT.OS_Lib;          use GNAT.OS_Lib;
+with Ada.Exceptions;       use Ada.Exceptions;
+
+with Traces;               use Traces;
+with Glide_Kernel.Console; use Glide_Kernel.Console;
+with Glide_Intl;           use Glide_Intl;
+
+package body Glide_Kernel.Timeout is
+
+   Me : constant Debug_Handle := Create ("Glide_Kernel.Timeout");
+
+   function Process_Cb (Data : Process_Data) return Boolean;
+   --  Generic callback for async spawn of processes.
+
+   ----------------
+   -- Process_Cb --
+   ----------------
+
+   function Process_Cb (Data : Process_Data) return Boolean is
+      Name   : String_Access := Data.Name;
+      Fd     : Process_Descriptor_Access := Data.Descriptor;
+      Result : Expect_Match;
+
+   begin
+      Expect (Fd.all, Result, "\n", Timeout => 1);
+
+      if Result /= Expect_Timeout then
+         Console.Insert (Data.Kernel, Expect_Out (Fd.all), Add_LF => False);
+      end if;
+
+      return True;
+
+   exception
+      when Process_Died =>
+         Console.Insert
+           (Data.Kernel, Expect_Out (Fd.all), Add_LF => False);
+         Close (Fd.all);
+         Free (Fd);
+         Data.Callback (Data);
+         Free (Name);
+         Pop_State (Data.Kernel);
+         return False;
+
+      when E : others =>
+         Close (Fd.all);
+         Free (Fd);
+         Free (Name);
+         Pop_State (Data.Kernel);
+         Trace (Me, "Unexpected exception: " & Exception_Information (E));
+         return False;
+   end Process_Cb;
+
+   --------------------
+   -- Launch_Process --
+   --------------------
+
+   procedure Launch_Process
+     (Kernel    : Kernel_Handle;
+      Command   : String;
+      Arguments : Argument_List;
+      Callback  : Process_Callback;
+      Name      : String;
+      Success   : out Boolean)
+   is
+      Timeout : constant Guint32 := 50;
+      Fd      : Process_Descriptor_Access;
+      Id      : Timeout_Handler_Id;
+
+      procedure Spawn
+        (Command   : String;
+         Arguments : Argument_List;
+         Success   : out Boolean);
+      --  Launch given command.
+
+      -----------
+      -- Spawn --
+      -----------
+
+      procedure Spawn
+        (Command   : String;
+         Arguments : Argument_List;
+         Success   : out Boolean)
+      is
+         Exec : String_Access := Locate_Exec_On_Path (Command);
+      begin
+         if Exec = null then
+            Success := False;
+            return;
+         end if;
+
+         Console.Insert (Kernel, Command, False, Add_LF => False);
+
+         for J in Arguments'Range loop
+            Console.Insert
+              (Kernel, ' ' & Arguments (J).all, False, Add_LF => False);
+         end loop;
+
+         --  Add end of line after last argument
+         Console.Insert (Kernel, "", False);
+
+         Fd := new TTY_Process_Descriptor;
+         Non_Blocking_Spawn (Fd.all, Exec.all, Arguments, Err_To_Out => True);
+         Success := True;
+         Free (Exec);
+
+      exception
+         when Invalid_Process =>
+            Success := False;
+            Console.Insert (Kernel, -"Invalid command.", False, Mode => Error);
+      end Spawn;
+
+   begin
+      Push_State (Kernel, Processing);
+      Spawn (Command, Arguments, Success);
+
+      if Success then
+         Id := Process_Timeout.Add
+           (Timeout, Process_Cb'Access,
+            (Kernel, Fd, new String' (Name), Callback));
+      else
+         Pop_State (Kernel);
+      end if;
+
+   exception
+      when E : others =>
+         Pop_State (Kernel);
+         Trace (Me, "Unexpected exception: " & Exception_Information (E));
+   end Launch_Process;
+
+end Glide_Kernel.Timeout;
