915bc9986c2c311e3dd42b10039b2261a1f08f39
Arnaud Charlet <charlet@adacore.com>
2002-04-19 16:56:12 +0000

    (Display_Button): Adjust colorized area to new pixmap.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@75178 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/gvd-color_combo.adb
+++ gvd/gvd/gvd-color_combo.adb
@@ -13,7 +13,7 @@
 -- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
 -- General Public License for more details. You should have received --
--- a copy of the GNU General Public License along with this library; --
+-- a copy of the GNU General Public License along with this program; --
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
@@ -245,7 +245,7 @@ package body GVD.Color_Combo is
       Gdk_New (Tmp_Gc, Get_Window (Combo));
       Set_Foreground (Tmp_Gc, Combo.Color);
       Get (Pixmap, Val, Mask);
-      Draw_Rectangle (Val, Tmp_Gc, True, 5, 20, 16, 4);
+      Draw_Rectangle (Val, Tmp_Gc, True, 21, 13, 16, 4);
       Draw (Pixmap, Full_Area);
       Unref (Tmp_Gc);
    end Display_Button;
