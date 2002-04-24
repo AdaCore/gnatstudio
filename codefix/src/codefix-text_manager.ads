492653f6c0b0c78ffe191f2cba3fae01d8335085
Quentin Ochem <ochem@adacore.com>
2002-04-24 08:55:25 +0000

    Major restructuration, type Text_Navigator_Abstr created in order to replace the old Text_Interface. Text_Interface now is just managing one file.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@75293 936e1b1b-40f2-da11-902a-00137254ae57

--- codefix/src/codefix-text_manager.ads
+++ codefix/src/codefix-text_manager.ads
@@ -1,7 +1,9 @@
-with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Unchecked_Deallocation;
 
+with Ada_Analyzer; use Ada_Analyzer;
 with Language; use Language;
+
+with Generic_List;
 
 package Codefix.Text_Manager is
 
@@ -32,11 +34,15 @@ package Codefix.Text_Manager is
    type Ptr_Text is access all Text_Interface'Class;
    procedure Free (This : in out Ptr_Text);
 
+   procedure Initialize
+     (This      : in out Text_Interface;
+      File_Name : String) is abstract;
+
    procedure Free (This : in out Text_Interface) is abstract;
 
    function Get
      (This   : Text_Interface;
-      Cursor : File_Cursor'Class;
+      Cursor : Text_Cursor'Class;
       Len    : Natural)
      return String is abstract;
    --  Get Len characters from the file and the position specified by the
@@ -44,14 +50,14 @@ package Codefix.Text_Manager is
 
    function Get_Line
      (This   : Text_Interface;
-      Cursor : File_Cursor'Class)
+      Cursor : Text_Cursor'Class)
      return String is abstract;
    --  Get all character from the file and the column specified by the cursor
    --  to the end of the line.
 
    procedure Replace
      (This      : in out Text_Interface;
-      Cursor    : File_Cursor'Class;
+      Cursor    : Text_Cursor'Class;
       Len       : Natural;
       New_Value : String) is abstract;
    --  Replace the Len characters, from the position designed by the cursor, by
@@ -59,36 +65,88 @@ package Codefix.Text_Manager is
 
    procedure Add_Line
      (This        : in out Text_Interface;
-      Cursor      : File_Cursor'Class;
+      Cursor      : Text_Cursor'Class;
       New_Line    : String) is abstract;
    --  Add a line at the cursor specified. To add a line at the
    --  begining of the text, set cursor line = 0.
 
    procedure Delete_Line
      (This : in out Text_Interface;
-      Cursor : File_Cursor'Class) is abstract;
+      Cursor : Text_Cursor'Class) is abstract;
    --  Delete the line where the cursor is.
 
    function Line_Length
      (This   : Text_Interface'Class;
-      Cursor : File_Cursor'Class)
+      Cursor : Text_Cursor'Class)
      return Natural;
    --  Returns le length of a line from the position of the cursor.
 
-   function Get_Declaration
-     (Current_Text : Text_Interface'Class
-      Cursor       : File_Cursor'Class
-      Category     : Language_Category)
-     return Construct_Information is abstract;
+   function Read_File
+     (This : Text_Interface)
+   return Dynamic_String is abstract;
 
-   procedure Set_Parse_Mode (This : in out Text_Interface) is abstract;
+   function Get_File_Name (This : Text_Interface) return String;
 
-   procedure Set_Update_Mode (This : in out Text_Interface) is abstract;
+   procedure Update (This : Text_Interface) is abstract;
 
-   procedure Get_Entity
-     (Current_Text : Text_Interface'Class
-      Cursor       : File_Cursor'Class
-      Category     : Language_Category);
+   ----------------------------------------------------------------------------
+   --  type Text_Navigator
+   ----------------------------------------------------------------------------
+
+   type Text_Navigator_Abstr is abstract tagged private;
+
+   procedure Free (This : in out Text_Navigator_Abstr);
+
+   function Get_Unit
+     (Current_Text : Text_Navigator_Abstr;
+      Cursor       : File_Cursor'Class)
+   return Construct_Information;
+
+   function Search_Body
+     (Current_Text : Text_Navigator_Abstr;
+      File_Name    : String;
+      Spec         : Construct_Information)
+     return Construct_Information;
+
+   function Get
+     (This   : Text_Navigator_Abstr;
+      Cursor : File_Cursor'Class;
+      Len    : Natural)
+     return String;
+
+   function Get_Line
+     (This   : Text_Navigator_Abstr;
+      Cursor : File_Cursor'Class)
+     return String;
+
+   function Read_File
+     (This      : Text_Navigator_Abstr;
+      File_Name : String) return Dynamic_String;
+
+   procedure Replace
+     (This      : in out Text_Navigator_Abstr;
+      Cursor    : File_Cursor'Class;
+      Len       : Natural;
+      New_Value : String);
+
+   procedure Add_Line
+     (This        : in out Text_Navigator_Abstr;
+      Cursor      : File_Cursor'Class;
+      New_Line    : String);
+
+   procedure Delete_Line
+     (This : in out Text_Navigator_Abstr;
+      Cursor : File_Cursor'Class);
+
+   function Line_Length
+     (This   : Text_Navigator_Abstr;
+      Cursor : File_Cursor'Class)
+     return Natural;
+
+   procedure Update (This : Text_Navigator_Abstr);
+
+   function New_Text_Interface (This : Text_Navigator_Abstr)
+      return Ptr_Text is abstract;
 
    ----------------------------------------------------------------------------
    --  type Extract_Line
@@ -101,9 +159,9 @@ package Codefix.Text_Manager is
    function Get_Cursor (This : Extract_Line) return File_Cursor'Class;
 
    --  PAS FINIE !!!
-   procedure Update (This        : Extract_Line;
-                     Current_Text : in out Text_Interface'Class;
-                     Offset_Line  : in out Natural);
+   procedure Update (This         : Extract_Line;
+                     Current_Text : in out Text_Navigator_Abstr'Class;
+                     Offset_Line  : in out Integer);
 
    procedure Free (This : in out Extract_Line);
 
@@ -123,13 +181,13 @@ package Codefix.Text_Manager is
    function Clone (This : Extract) return Extract;
 
    procedure Get
-     (This        : Text_Interface'Class;
+     (This        : Text_Navigator_Abstr'Class;
       Cursor      : File_Cursor'Class;
       Len         : Natural;
       Destination : in out Extract);
 
    procedure Get_Line
-     (This        : Text_Interface'Class;
+     (This        : Text_Navigator_Abstr'Class;
       Cursor      : File_Cursor'Class;
       Destination : in out Extract);
 
@@ -143,34 +201,34 @@ package Codefix.Text_Manager is
    --  PAS FINIE !!!
    procedure Update
      (This         : Extract;
-      Current_Text : in out Text_Interface'Class;
+      Current_Text : in out Text_Navigator_Abstr'Class;
       Offset_Line  : in out Natural);
 
    procedure Put_Line (This : Extract);
    procedure Put_Line_Original
-     (This : Extract;
-      Current_Text : Text_Interface'Class);
+     (This         : Extract;
+      Current_Text : Text_Navigator_Abstr'Class);
 
    procedure Replace_Word
      (This         : in out Extract;
-      Cursor       : Text_Cursor'Class;
+      Cursor       : File_Cursor'Class;
       New_String   : String;
       Format       : String := "(^[\w]*)");
 
    procedure Add_Word
      (This   : in out Extract;
-      Cursor : Text_Cursor'Class;
+      Cursor : File_Cursor'Class;
       Word   : String);
 
    function Get_Word_Length
      (This   : Extract;
-      Cursor : Text_Cursor'Class;
+      Cursor : File_Cursor'Class;
       Format : String)
      return Natural;
 
    procedure Free (This : in out Extract);
 
-   function Get_Line (This : Extract; Number : Natural)
+   function Get_Line (This : Extract; Position : File_Cursor)
      return Ptr_Extract_Line;
    --  Returns the line with the number specified in the original text.
 
@@ -179,27 +237,70 @@ package Codefix.Text_Manager is
    --  Returns the line recorded at the position Number in the extract.
 
    function Get_Number_Lines (This : Extract) return Natural;
+   --  Returns the number of the lines in the extract.
 
    procedure Add_Line
      (This   : in out Extract;
       Cursor : File_Cursor'Class;
       Text   : String);
+   --  Add a line AFTER the line specified by the cursor. Make a cursor with
+   --  0 for the line number to add a line at the begenning of the file.
 
    procedure Delete_Line
      (This   : in out Extract;
       Cursor : File_Cursor'Class);
+   --  Delete the line of the extract at the line number and in the file
+   --  specified by the cursor.
 
    procedure Delete_All_Lines (This : in out Extract);
+   --  Delete all the lines from the extract
 
    procedure Get_Entity
-     (Current_Text : Text_Interface'Class;
-      Cursor       : File_Cursor'Class;
-      This         : in out Extract;
-      Category     : Language_Category);
+     (This : in out Extract;
+      Current_Text : Text_Navigator_Abstr'Class;
+      Cursor : File_Cursor);
 
 private
 
-   type Text_Interface is abstract tagged null record;
+   ----------------------------------------------------------------------------
+   --  type Text_Navigator
+   ----------------------------------------------------------------------------
+
+   package Text_List is new Generic_List (Ptr_Text);
+   use Text_List;
+
+   type Ptr_List_Text is access Text_List.List;
+
+   type Text_Navigator_Abstr is abstract tagged record
+      Files : Ptr_List_Text := new Text_List.List;
+   end record;
+
+   function Get_File
+     (This : Text_Navigator_Abstr'Class;
+      Name : String)
+   return Ptr_Text;
+   --  Returns the existent file interface, or create a new one if it
+   --  doesn't exists.
+
+
+   ----------------------------------------------------------------------------
+   --  type Text_Interface
+   ----------------------------------------------------------------------------
+
+   type Text_Interface is abstract tagged record
+      Tokens_List : Construct_List_Access := new Construct_List;
+      File_Name   : Dynamic_String;
+   end record;
+
+   function Get_Unit
+     (Current_Text : Text_Interface;
+      Cursor       : Text_Cursor'Class)
+   return Construct_Information;
+
+   function Search_Body
+     (Current_Text : Text_Interface;
+      Spec         : Construct_Information)
+   return Construct_Information;
 
    type Line_Context is
      (Original_Line,
@@ -207,7 +308,8 @@ private
       Line_Created,
       Line_Deleted);
 
-   type Extract_Line (Context : Line_Context := Original_Line) is record
+   type Extract_Line is record
+      Context         : Line_Context := Original_Line;
       Cursor          : File_Cursor;
       Original_Length : Natural := 0;
       Content         : Dynamic_String;
@@ -226,10 +328,10 @@ private
       Len         : Natural;
       Destination : in out Extract_Line);
 
-   procedure Put_Line (This : Extract_Line);
+   procedure Put_Line (This : Extract_Line; Detail : Boolean := True);
    procedure Put_Line_Original
-     (This : Extract_Line;
-      Current_Text : Text_Interface'Class);
+     (This         : Extract_Line;
+      Current_Text : Text_Navigator_Abstr'Class);
 
    type Extract is record
       First : Ptr_Extract_Line;
