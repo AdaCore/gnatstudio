176e267e4cb1d27f0660f0841a449c3de68186f4
Quentin Ochem <ochem@adacore.com>
2002-04-24 08:57:32 +0000

    Move in the type Error_Messages and change the formal parameter Text_Interface'Class of corrections function to Text_Navigator_Abstr'Class.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@75295 936e1b1b-40f2-da11-902a-00137254ae57

--- codefix/src/codefix-formal_errors.ads
+++ codefix/src/codefix-formal_errors.ads
@@ -4,12 +4,46 @@ with Generic_List;
 with Language; use Language;
 
 with Codefix.Text_Manager; use Codefix.Text_Manager;
-with Codefix.Errors_Parser; use Codefix.Errors_Parser;
 
 package Codefix.Formal_Errors is
 
+   ----------------------------------------------------------------------------
+   --  type Error_Message
+   ----------------------------------------------------------------------------
+
+   type Error_Message is new File_Cursor with private;
+
+   procedure Initialize (This : in out Error_Message; Message : String);
+   --  Parse the message headed in order to get the col number and the
+   --  line number.
+
+   procedure Initialize (This : in out Error_Message; Line, Col : Positive);
+
+   function Get_Message (This : Error_Message) return String;
+   --  Returns the message with the header.
+
+   procedure Free (This : in out Error_Message);
+   --  Frees the memory used by the object.
+
+   package Extract_List is new Generic_List (Extract);
+   use Extract_List;
+
+   subtype Solution_List is Extract_List.List;
+   --  This is a list of solutions proposed to solve an error.
+
+   function Get_Extract
+     (This     : Solution_List;
+      Position : Positive)
+     return Extract;
+
+   procedure Free (This : in out Solution_List);
+
+   ----------------------------------------------------------------------------
+   --  functions of formal errors
+   ----------------------------------------------------------------------------
+
    function Should_Be
-     (Current_Text : Text_Interface'Class;
+     (Current_Text : Text_Navigator_Abstr'Class;
       Message      : Error_Message;
       Str_Expected : String;
       Str_Red      : String := "")
@@ -19,7 +53,7 @@ package Codefix.Formal_Errors is
    --  looks for the first word in the position.
 
    function Wrong_Order
-     (Current_Text                : Text_Interface'Class;
+     (Current_Text                : Text_Navigator_Abstr'Class;
       Message                     : Error_Message;
       First_String, Second_String : String)
       return Extract;
@@ -27,7 +61,7 @@ package Codefix.Formal_Errors is
    --  in the message to the beginning, and invert the two strings.
 
    function Expected
-     (Current_Text    : Text_Interface'Class;
+     (Current_Text    : Text_Navigator_Abstr'Class;
       Message         : Error_Message;
       String_Expected : String;
       Add_Spaces      : Boolean := True)
@@ -35,7 +69,7 @@ package Codefix.Formal_Errors is
    --  Add the missing keyword into the text.
 
    function Unexpected
-     (Current_Text      : Text_Interface'Class;
+     (Current_Text      : Text_Navigator_Abstr'Class;
       Message           : Error_Message;
       String_Unexpected : String;
       Mode              : String_Mode := Text_Ascii)
@@ -43,14 +77,14 @@ package Codefix.Formal_Errors is
    --  Delete the unexpected string
 
    function Wrong_Column
-     (Current_Text    : Text_Interface'Class;
+     (Current_Text    : Text_Navigator_Abstr'Class;
       Message         : Error_Message;
       Column_Expected : Natural := 0)
       return Extract;
    --  Try re-indent the line
 
    function With_Clause_Missing
-     (Current_Text   : Text_Interface'Class;
+     (Current_Text   : Text_Navigator_Abstr'Class;
       Cursor         : File_Cursor'Class;
       Missing_Clause : String)
       return Extract;
@@ -59,7 +93,7 @@ package Codefix.Formal_Errors is
    type Case_Type is (Lower, Upper, Mixed);
 
    function Bad_Casing
-     (Current_Text : Text_Interface'Class;
+     (Current_Text : Text_Navigator_Abstr'Class;
       Cursor       : File_Cursor'Class;
       Correct_Word : String := "";
       Word_Case    : Case_Type := Mixed)
@@ -67,12 +101,21 @@ package Codefix.Formal_Errors is
    --  Re-case the word
 
    function Not_Referenced
-     (Current_Text : Text_Interface'Class;
+     (Current_Text : Text_Navigator_Abstr'Class;
       Cursor       : File_Cursor'Class;
       Category     : Language_Category;
       Name         : String)
    return Solution_List;
    --  Propose to delete the unit unreferrenced or, in some cases, to add
    --  a pragma 'not referreced'
+
+private
+
+   type Error_Message is new File_Cursor with record
+      Message : Dynamic_String;
+   end record;
+
+   procedure Parse_Head (Message : String; This : out Error_Message);
+   function Clone (This : Error_Message) return Error_Message;
 
 end Codefix.Formal_Errors;
