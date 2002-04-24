c991d1dee6f6c2ac7970afabf4d296395e50034d
Quentin Ochem <ochem@adacore.com>
2002-04-24 08:58:00 +0000

    Implements the changes of formal-errors.ads.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@75296 936e1b1b-40f2-da11-902a-00137254ae57

--- codefix/src/codefix-formal_errors.adb
+++ codefix/src/codefix-formal_errors.adb
@@ -5,12 +5,121 @@ with GNAT.Regpat; use GNAT.Regpat;
 
 package body Codefix.Formal_Errors is
 
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize (This : in out Error_Message; Message : String) is
+   begin
+      Affect (This.Message, Message);
+      Parse_Head (Message, This);
+   end Initialize;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize (This : in out Error_Message; Line, Col : Positive) is
+   begin
+      Affect (This.Message, "");
+      This.Line := Line;
+      This.Col := Col;
+   end Initialize;
+
+   -----------------
+   -- Get_Message --
+   -----------------
+
+   function Get_Message (This : Error_Message) return String is
+   begin
+      return This.Message.all;
+   end Get_Message;
+
+   ----------
+   -- Free --
+   ----------
+
+   procedure Free (This : in out Error_Message) is
+   begin
+      Free (File_Cursor (This));
+      Free (This.Message);
+   end Free;
+
+   ----------------
+   -- Parse_Head --
+   ----------------
+
+   procedure Parse_Head (Message : String; This : out Error_Message) is
+      Matches : Match_Array (0 .. 3);
+      Matcher : constant Pattern_Matcher :=
+         Compile ("([^:]*):([0-9]*):([0-9]*)");
+
+   begin
+      Match (Matcher, Message, Matches);
+
+      begin
+         Affect (This.File_Name,
+                 Message (Matches (1).First .. Matches (1).Last));
+         This.Line := Positive'Value
+            (Message (Matches (2).First .. Matches (2).Last));
+         This.Col := Positive'Value
+            (Message (Matches (3).First .. Matches (3).Last));
+      exception
+         when Constraint_Error => -- et tester No_Match
+            null; -- Lever une exception due au 'Value
+      end;
+   end Parse_Head;
+
+   -----------
+   -- Clone --
+   -----------
+
+   function Clone (This : Error_Message) return Error_Message is
+      New_Message : Error_Message;
+   begin
+      New_Message := (Clone (File_Cursor (This)) with
+                         new String'(This.Message.all));
+      return New_Message;
+   end Clone;
+
+   -----------------
+   -- Get_Extract --
+   -----------------
+
+   function Get_Extract
+     (This     : Solution_List;
+      Position : Positive)
+     return Extract is
+
+      Current_Node : Extract_List.List_Node;
+
+   begin
+
+      Current_Node := First (This);
+
+      for J in 1 .. Position - 1 loop
+         Current_Node := Next (Current_Node);
+      end loop;
+
+      return Data (Current_Node);
+
+   end Get_Extract;
+
+   ----------
+   -- Free --
+   ----------
+
+   procedure Free (This : in out Solution_List) is
+   begin
+      Free (This, True);
+   end Free;
+
    ---------------
    -- Should_Be --
    ---------------
 
    function Should_Be
-     (Current_Text : Text_Interface'Class;
+     (Current_Text : Text_Navigator_Abstr'Class;
       Message      : Error_Message;
       Str_Expected : String;
       Str_Red      : String := "")
@@ -44,7 +153,7 @@ package body Codefix.Formal_Errors is
    -----------------
 
    function Wrong_Order
-     (Current_Text                 : Text_Interface'Class;
+     (Current_Text                 : Text_Navigator_Abstr'Class;
       Message                      : Error_Message;
       First_String, Second_String  : String)
       return Extract is
@@ -95,7 +204,7 @@ package body Codefix.Formal_Errors is
    -----------------
 
    function Expected
-     (Current_Text    : Text_Interface'Class;
+     (Current_Text    : Text_Navigator_Abstr'Class;
       Message         : Error_Message;
       String_Expected : String;
       Add_Spaces      : Boolean := True)
@@ -152,7 +261,7 @@ package body Codefix.Formal_Errors is
    ----------------
 
    function Unexpected
-     (Current_Text      : Text_Interface'Class;
+     (Current_Text      : Text_Navigator_Abstr'Class;
       Message           : Error_Message;
       String_Unexpected : String;
       Mode              : String_Mode := Text_Ascii)
@@ -196,7 +305,7 @@ package body Codefix.Formal_Errors is
    ------------------
 
    function Wrong_Column
-     (Current_Text    : Text_Interface'Class;
+     (Current_Text    : Text_Navigator_Abstr'Class;
       Message         : Error_Message;
       Column_Expected : Natural := 0)
       return Extract is
@@ -251,7 +360,7 @@ package body Codefix.Formal_Errors is
    -------------------------
 
    function With_Clause_Missing
-     (Current_Text   : Text_Interface'Class;
+     (Current_Text   : Text_Navigator_Abstr'Class;
       Cursor         : File_Cursor'Class;
       Missing_Clause : String)
      return Extract is
@@ -272,7 +381,7 @@ package body Codefix.Formal_Errors is
    ----------------
 
    function Bad_Casing
-     (Current_Text : Text_Interface'Class;
+     (Current_Text : Text_Navigator_Abstr'Class;
       Cursor       : File_Cursor'Class;
       Correct_Word : String := "";
       Word_Case    : Case_Type := Mixed)
@@ -346,20 +455,32 @@ package body Codefix.Formal_Errors is
    ---------------------
 
    function Not_Referenced
-     (Current_Text : Text_Interface'Class;
+     (Current_Text : Text_Navigator_Abstr'Class;
       Cursor       : File_Cursor'Class;
       Category     : Language_Category;
       Name         : String)
      return Solution_List is
 
+      function Delete_Var return Extract;
       function Delete_Entity return Extract;
       function Add_Pragma return Extract;
+
+
+      function Delete_Var return Extract is
+         New_Extract : Extract;
+         Line_Cursor : File_Cursor := File_Cursor (Cursor);
+      begin
+         Line_Cursor.Col := 1;
+         Get_Line (Current_Text, Line_Cursor, New_Extract);
+
+         return New_Extract;
+      end Delete_Var;
 
       function Delete_Entity return Extract is
          New_Extract : Extract;
       begin
-         Get_Entity (Current_Text, Cursor, New_Extract, Category);
-         Delete_All_Lines (Current_Text);
+         Get_Entity (New_Extract, Current_Text, Cursor);
+         Delete_All_Lines (New_Extract);
          return New_Extract;
       end Delete_Entity;
 
@@ -368,12 +489,12 @@ package body Codefix.Formal_Errors is
          New_Position : File_Cursor;
          Declaration  : Construct_Information;
       begin
-         Declaration := Get_Declaration (Current_Text, Cursor, Category);
+         Declaration := Get_Unit (Current_Text, Cursor);
          New_Position.Line := Declaration.Sloc_End.Line;
          New_Position.Col  := Declaration.Sloc_End.Column;
          Affect (New_Position.File_Name, Cursor.File_Name);
          Add_Line (New_Extract, New_Position, "pragma Unreferenced (" &
-                                              Name & ";");
+                                              Name & ");");
          return New_Extract;
       end Add_Pragma;
 
@@ -381,8 +502,8 @@ package body Codefix.Formal_Errors is
 
    begin
       case Category is
-         when Cat_Variable | Cat_With =>
-            Append (New_Solutions, Delete_Entity);
+         when Cat_Variable =>
+            Append (New_Solutions, Delete_Var);
          when Cat_Function | Cat_Procedure | Cat_Type =>
             Append (New_Solutions, Delete_Entity);
             Append (New_Solutions, Add_Pragma);
