56e8f5a902a1ce429381d48eac4efbcf61e24dbb
Arnaud Charlet <charlet@adacore.com>
2002-05-03 14:01:48 +0000

    Minor reformatting. Add ??? for missing spec.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@75540 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/commands-external.ads
+++ kernel/src/commands-external.ads
@@ -1,10 +1,10 @@
 -----------------------------------------------------------------------
---                          G L I D E  I I                           --
+--                              G P S                                --
 --                                                                   --
---                     Copyright (C) 2001 - 2002                     --
+--                     Copyright (C) 2001-2002                       --
 --                            ACT-Europe                             --
 --                                                                   --
--- GLIDE is free software; you can redistribute it and/or modify  it --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
 -- the Free Software Foundation; either version 2 of the License, or --
 -- (at your option) any later version.                               --
@@ -13,7 +13,7 @@
 -- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
 -- General Public License for more details. You should have received --
--- a copy of the GNU General Public License along with this library; --
+-- a copy of the GNU General Public License along with this program; --
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
@@ -42,6 +42,7 @@ package Commands.External is
       Args         : String_List.List;
       Head         : String_List.List;
       Handler      : String_List_Handler);
+   --  ???
 
    function Execute (Command : access External_Command) return Boolean;
 
@@ -58,7 +59,6 @@ private
       Args    : String_List.List;
       Head    : String_List.List;
       Handler : String_List_Handler;
-
       Output  : String_List.List;
    end record;
 
