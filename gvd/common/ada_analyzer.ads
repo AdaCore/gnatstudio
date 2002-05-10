9dc393eadb3c771363dbc92145983b2363803a3a
Arnaud Charlet <charlet@adacore.com>
2002-05-10 08:04:18 +0000

    (Token_Type): Reorganize items so that we can declare more subtypes suitable  with Analyze_Ada_Source. (Reserved_Token_Type): Now contains all the reserved word.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@75716 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/common/ada_analyzer.ads
+++ gvd/common/ada_analyzer.ads
@@ -25,11 +25,6 @@ with Language;             use Language;
 
 package Ada_Analyzer is
 
-   --  Note: the following type is taken from Scans (GNAT sources).
-   --  The following type is used to identify token types returned by Scan.
-   --  The class column in this table indicates the token classes which
-   --  apply to the token, as defined by subsequent subtype declarations.
-
    type Token_Type is (
 
       --  Token name          Token type   Class(es)
@@ -53,31 +48,13 @@ package Ada_Analyzer is
       Tok_Plus,            -- +            Binary_Addop, Unary_Addop
 
       Tok_Asterisk,        -- *            Mulop
-      Tok_Mod,             -- MOD          Mulop
-      Tok_Rem,             -- REM          Mulop
       Tok_Slash,           -- /            Mulop
-
-      Tok_New,             -- NEW
-
-      Tok_Abs,             -- ABS
-      Tok_Others,          -- OTHERS
-      Tok_Null,            -- NULL
 
       Tok_Dot,             -- .            Namext
       Tok_Apostrophe,      -- '            Namext
 
-      Tok_Left_Paren,      -- (            Namext, Consk
-
-      Tok_Delta,           -- DELTA        Atkwd, Sterm, Consk
-      Tok_Digits,          -- DIGITS       Atkwd, Sterm, Consk
-      Tok_Range,           -- RANGE        Atkwd, Sterm, Consk
-
       Tok_Right_Paren,     -- )            Sterm
       Tok_Comma,           -- ,            Sterm
-
-      Tok_And,             -- AND          Logop, Sterm
-      Tok_Or,              -- OR           Logop, Sterm
-      Tok_Xor,             -- XOR          Logop, Sterm
 
       Tok_Less,            -- <            Relop, Sterm
       Tok_Equal,           -- =            Relop, Sterm
@@ -86,12 +63,10 @@ package Ada_Analyzer is
       Tok_Greater_Equal,   -- >=           Relop, Sterm
       Tok_Less_Equal,      -- <=           Relop, Sterm
 
-      Tok_In,              -- IN           Relop, Sterm
-      Tok_Not,             -- NOT          Relop, Sterm
+      Tok_Less_Less,       -- <<           Eterm, Sterm, After_SM
 
       Tok_Box,             -- <>           Relop, Eterm, Sterm
       Tok_Colon_Equal,     -- :=           Eterm, Sterm
-      Tok_Colon,           -- :            Eterm, Sterm
       Tok_Greater_Greater, -- >>           Eterm, Sterm
 
       Tok_Abstract,        -- ABSTRACT     Eterm, Sterm
@@ -100,29 +75,42 @@ package Ada_Analyzer is
       Tok_All,             -- ALL          Eterm, Sterm
       Tok_Array,           -- ARRAY        Eterm, Sterm
       Tok_At,              -- AT           Eterm, Sterm
+
+      Tok_Mod,             -- MOD          Mulop
+      Tok_Rem,             -- REM          Mulop
+
+      Tok_And,             -- AND          Logop, Sterm
+
+      Tok_Delta,           -- DELTA        Atkwd, Sterm, Consk
+      Tok_Digits,          -- DIGITS       Atkwd, Sterm, Consk
+      Tok_Range,           -- RANGE        Atkwd, Sterm, Consk
+
+      Tok_Abs,             -- ABS
+      Tok_New,             -- NEW
+      Tok_Null,            -- NULL
+      Tok_Others,          -- OTHERS
+
+      Tok_In,              -- IN           Relop, Sterm
+      Tok_Not,             -- NOT          Relop, Sterm
+
+      Tok_Or,              -- OR           Logop, Sterm
+      Tok_Xor,             -- XOR          Logop, Sterm
+
       Tok_Body,            -- BODY         Eterm, Sterm
       Tok_Constant,        -- CONSTANT     Eterm, Sterm
-      Tok_Do,              -- DO           Eterm, Sterm
-      Tok_Is,              -- IS           Eterm, Sterm
       Tok_Limited,         -- LIMITED      Eterm, Sterm
       Tok_Of,              -- OF           Eterm, Sterm
       Tok_Out,             -- OUT          Eterm, Sterm
-      Tok_Record,          -- RECORD       Eterm, Sterm
       Tok_Renames,         -- RENAMES      Eterm, Sterm
       Tok_Reverse,         -- REVERSE      Eterm, Sterm
       Tok_Tagged,          -- TAGGED       Eterm, Sterm
-      Tok_Then,            -- THEN         Eterm, Sterm
 
-      Tok_Less_Less,       -- <<           Eterm, Sterm, After_SM
-
-      Tok_Abort,           -- ABORT        Eterm, Sterm, After_SM
-      Tok_Accept,          -- ACCEPT       Eterm, Sterm, After_SM
       Tok_Case,            -- CASE         Eterm, Sterm, After_SM
       Tok_Delay,           -- DELAY        Eterm, Sterm, After_SM
-      Tok_Else,            -- ELSE         Eterm, Sterm, After_SM
+
+      Tok_Accept,          -- ACCEPT       Eterm, Sterm, After_SM
       Tok_Elsif,           -- ELSIF        Eterm, Sterm, After_SM
       Tok_End,             -- END          Eterm, Sterm, After_SM
-      Tok_Exception,       -- EXCEPTION    Eterm, Sterm, After_SM
       Tok_Exit,            -- EXIT         Eterm, Sterm, After_SM
       Tok_Goto,            -- GOTO         Eterm, Sterm, After_SM
       Tok_If,              -- IF           Eterm, Sterm, After_SM
@@ -130,16 +118,14 @@ package Ada_Analyzer is
       Tok_Raise,           -- RAISE        Eterm, Sterm, After_SM
       Tok_Requeue,         -- REQUEUE      Eterm, Sterm, After_SM
       Tok_Return,          -- RETURN       Eterm, Sterm, After_SM
-      Tok_Select,          -- SELECT       Eterm, Sterm, After_SM
       Tok_Terminate,       -- TERMINATE    Eterm, Sterm, After_SM
       Tok_Until,           -- UNTIL        Eterm, Sterm, After_SM
       Tok_When,            -- WHEN         Eterm, Sterm, After_SM
 
-      Tok_Begin,           -- BEGIN        Eterm, Sterm, After_SM, Labeled_Stmt
-      Tok_Declare,         -- DECLARE      Eterm, Sterm, After_SM, Labeled_Stmt
       Tok_For,             -- FOR          Eterm, Sterm, After_SM, Labeled_Stmt
-      Tok_Loop,            -- LOOP         Eterm, Sterm, After_SM, Labeled_Stmt
       Tok_While,           -- WHILE        Eterm, Sterm, After_SM, Labeled_Stmt
+
+      Tok_Separate,        -- SEPARATE     Eterm, Sterm, Cunit, After_SM
 
       Tok_Entry,           -- ENTRY        Eterm, Sterm, Declk, Deckn, After_SM
       Tok_Protected,       -- PROTECTED    Eterm, Sterm, Declk, Deckn, After_SM
@@ -148,18 +134,35 @@ package Ada_Analyzer is
       Tok_Subtype,         -- SUBTYPE      Eterm, Sterm, Declk, Deckn, After_SM
       Tok_Use,             -- USE          Eterm, Sterm, Declk, Deckn, After_SM
 
-      Tok_Function,        -- FUNCTION     Eterm, Sterm, Cunit, Declk, After_SM
       Tok_Generic,         -- GENERIC      Eterm, Sterm, Cunit, Declk, After_SM
+
+      Tok_Function,        -- FUNCTION     Eterm, Sterm, Cunit, Declk, After_SM
       Tok_Package,         -- PACKAGE      Eterm, Sterm, Cunit, Declk, After_SM
       Tok_Procedure,       -- PROCEDURE    Eterm, Sterm, Cunit, Declk, After_SM
 
+      Tok_Do,              -- DO           Eterm, Sterm
+      Tok_Is,              -- IS           Eterm, Sterm
+      Tok_Record,          -- RECORD       Eterm, Sterm
+      Tok_Then,            -- THEN         Eterm, Sterm
+
+      Tok_Abort,           -- ABORT        Eterm, Sterm, After_SM
+      Tok_Else,            -- ELSE         Eterm, Sterm, After_SM
+      Tok_Exception,       -- EXCEPTION    Eterm, Sterm, After_SM
+
+      Tok_Select,          -- SELECT       Eterm, Sterm, After_SM
+
+      Tok_Begin,           -- BEGIN        Eterm, Sterm, After_SM, Labeled_Stmt
+      Tok_Declare,         -- DECLARE      Eterm, Sterm, After_SM, Labeled_Stmt
+      Tok_Loop,            -- LOOP         Eterm, Sterm, After_SM, Labeled_Stmt
+
       Tok_Private,         -- PRIVATE      Eterm, Sterm, Cunit, After_SM
       Tok_With,            -- WITH         Eterm, Sterm, Cunit, After_SM
-      Tok_Separate,        -- SEPARATE     Eterm, Sterm, Cunit, After_SM
-
-      Tok_EOF,             -- End of file  Eterm, Sterm, Cterm, After_SM
 
       Tok_Semicolon,       -- ;            Eterm, Sterm, Cterm
+
+      Tok_Left_Paren,      -- (            Namext, Consk
+
+      Tok_Colon,           -- :            Eterm, Sterm
 
       Tok_Arrow,           -- =>           Sterm, Cterm, Chtok
 
@@ -174,12 +177,13 @@ package Ada_Analyzer is
       Tok_Modifying,
       Tok_External,
 
+      Tok_EOF,             -- End of file  Eterm, Sterm, Cterm, After_SM
+
       No_Token);
       --  No_Token is used for initializing Token values to indicate that
       --  no value has been set yet.
 
-   subtype Reserved_Token_Type is
-     Token_Type range Tok_Abstract .. Tok_Separate;
+   subtype Reserved_Token_Type is Token_Type range Tok_Abstract .. Tok_With;
 
    -----------
    -- Types --
