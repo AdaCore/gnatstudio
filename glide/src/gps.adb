666fa901a9d147b8d4910cdd2ed65a0f31d45100
Arnaud Charlet <charlet@adacore.com>
2002-05-10 15:23:57 +0000

    Uncomment call to Destroy (GPS), since the problem when destroying the  HTML widget has been fixed. May fix memory leaks wrt X server resources  (e.g pixmaps).
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@75774 936e1b1b-40f2-da11-902a-00137254ae57

--- glide/src/gps.adb
+++ glide/src/gps.adb
@@ -548,11 +548,7 @@ begin
    Free_Modules (GPS.Kernel);
    Glide_Page.Destroy (Page);
 
-   --  ??? The following call sometimes causes a SEGV, e.g when destroying
-   --  the help widget. Until this is resolved, and since it is not critical
-   --  to release all the memory before exiting, this call is disabled for now.
-   --  Destroy (GPS);
-
+   Destroy (GPS);
    Destroy (GPS.Kernel);
    Prj_API.Finalize;
    Traces.Finalize;
