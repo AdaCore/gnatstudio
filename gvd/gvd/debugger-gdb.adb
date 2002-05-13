c05100dc260da5afc160d732dc6e5c0d31125f69
Arnaud Charlet <charlet@adacore.com>
2002-05-13 10:51:58 +0000

    Remove unused package.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@75838 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/debugger-gdb.adb
+++ gvd/gvd/debugger-gdb.adb
@@ -13,12 +13,11 @@
 -- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
 -- General Public License for more details. You should have received --
--- a copy of the GNU General Public License along with this library; --
+-- a copy of the GNU General Public License along with this program; --
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
-with System;            use System;
 with Ada.Strings;       use Ada.Strings;
 with Ada.Strings.Fixed; use Ada.Strings.Fixed;
 with Ada.Tags;          use Ada.Tags;
