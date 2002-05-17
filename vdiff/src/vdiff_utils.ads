86f156b4f8f2204cd2c49ecf2d5dd34ddc22389f
Quentin Ochem <ochem@adacore.com>
2002-05-17 08:00:41 +0000

    Modify the head of the file. Add type Text_Iterator. Add function Fill_Diff_List working with Text_Iterator. Move in the Num_Line_Sep constant.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@76052 936e1b1b-40f2-da11-902a-00137254ae57

--- vdiff/src/vdiff_utils.ads
+++ vdiff/src/vdiff_utils.ads
@@ -1,10 +1,10 @@
 -----------------------------------------------------------------------
---                          G L I D E  I I                           --
+--                               G P S                               --
 --                                                                   --
---                        Copyright (C) 2001                         --
+--                        Copyright (C) 2002                         --
 --                            ACT-Europe                             --
 --                                                                   --
--- GLIDE is free software; you can redistribute it and/or modify  it --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
 -- the Free Software Foundation; either version 2 of the License, or --
 -- (at your option) any later version.                               --
@@ -13,7 +13,7 @@
 -- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
 -- General Public License for more details. You should have received --
--- a copy of the GNU General Public License along with this library; --
+-- a copy of the GNU General Public License along with this program; --
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
@@ -24,6 +24,7 @@
 with Gtk.Widget;   use Gtk.Widget;
 with Gtk.Clist;    use Gtk.Clist;
 with Diff_Utils;   use Diff_Utils;
+with Basic_Types;  use Basic_Types;
 with Glide_Kernel;
 
 package Vdiff_Utils is
@@ -37,6 +38,32 @@ package Vdiff_Utils is
       Diff   : Diff_Occurrence_Link);
    --  Fill List1 and List2 with lines of File given a result of diff Diff.
 
+   type Text_Iterator;
+
+   type Text_Iterator_Access is access all Text_Iterator;
+
+   type Text_Iterator is record
+      New_Line, Old_Line : String_Access;
+      Original_Position  : Natural;
+      Action             : Diff_Action := Nothing;
+      Next               : Text_Iterator_Access;
+   end record;
+   --  In this structure, Original_Position is the number of the line in the
+   --  old text. If Action is Append, then New_Line should be null and
+   --  Original_Position must be the number of the previous line in the old
+   --  text. When  Action is Delete, New_Line should be null.
+
+   procedure Free (This : in out Text_Iterator_Access);
+   --  Free the memory associated to a Text_Iterator_Access, and all next
+   --  objects linked to this one.
+
+   procedure Fill_Diff_Lists
+     (Kernel     : access Glide_Kernel.Kernel_Handle_Record'Class;
+      List1      : access Gtk_Clist_Record'Class;
+      List2      : access Gtk_Clist_Record'Class;
+      First_Line : Text_Iterator_Access);
+   --  Fill List1 and List2 with lines given from Next_Line.
+
    procedure Value1_Changed (Object : access Gtk_Widget_Record'Class);
    --  Callback for the value_changed signal on the first
    --  scrolled window of a Vdiff_Access.
@@ -44,5 +71,11 @@ package Vdiff_Utils is
    procedure Value2_Changed (Object : access Gtk_Widget_Record'Class);
    --  Callback for the value_changed signal on the second
    --  scrolled window of a Vdiff_Access.
+
+private
+
+   Num_Line_Sep : constant := 3;
+   --  Number of empty lines separating each chunk, in Fill_Diff_List
+   --  functions.
 
 end Vdiff_Utils;
