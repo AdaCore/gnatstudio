9514d73c86c17a7491e494955cf643abb39ccdc5
Quentin Ochem <ochem@adacore.com>
2002-05-17 10:14:26 +0000

    Regenerated from Codefix_Interface.glade 1.2.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@76083 936e1b1b-40f2-da11-902a-00137254ae57

--- codefix/src/.gate/codefix_interface/codefix_interface.adb
+++ codefix/src/.gate/codefix_interface/codefix_interface.adb
@@ -2,12 +2,10 @@ with Gtk; use Gtk;
 with Gtk.Main;
 with Gtk.Widget; use Gtk.Widget;
 with Codefix_Window_Pkg; use Codefix_Window_Pkg;
-with Gen_Proposition_Pkg; use Gen_Proposition_Pkg;
 with Final_Window_Pkg; use Final_Window_Pkg;
 
 procedure Codefix_Interface is
    Codefix_Window : Codefix_Window_Access;
-   Gen_Proposition : Gen_Proposition_Access;
    Final_Window : Final_Window_Access;
 
 begin
@@ -15,8 +13,6 @@ begin
    Gtk.Main.Init;
    Gtk_New (Codefix_Window);
    Show_All (Codefix_Window);
-   Gtk_New (Gen_Proposition);
-   Show_All (Gen_Proposition);
    Gtk_New (Final_Window);
    Show_All (Final_Window);
    Gtk.Main.Main;
