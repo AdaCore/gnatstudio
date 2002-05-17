675756aa789847ec26f6eb8b1e4aaabff8e7081c
Arnaud Charlet <charlet@adacore.com>
2002-05-17 09:19:56 +0000

    Minor reformatting. Make variables constant when appropriate.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@76072 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/src/vsearch_ext.adb
+++ prj_editor/src/vsearch_ext.adb
@@ -597,9 +597,10 @@ package body Vsearch_Ext is
      (Vsearch : access Gtk_Widget_Record'Class)
    is
       use Widget_List;
-      Search : Vsearch_Extended := Vsearch_Extended (Vsearch);
+      Search : constant Vsearch_Extended := Vsearch_Extended (Vsearch);
       Data   : Search_User_Data_Record;
       Item   : Gtk_List_Item;
+
    begin
       if Get_Selection (Get_List (Search.Pattern_Combo)) /=
         Widget_List.Null_List
@@ -624,16 +625,17 @@ package body Vsearch_Ext is
    procedure New_Predefined_Regexp
      (Vsearch : access GObject_Record'Class; Kernel : Kernel_Handle)
    is
-      Search : Vsearch_Extended := Vsearch_Extended (Vsearch);
-      Item : Gtk_List_Item;
+      Search  : constant Vsearch_Extended := Vsearch_Extended (Vsearch);
+      Item    : Gtk_List_Item;
       Is_Regexp, Case_Sensitive : Boolean;
-      Data : Search_User_Data_Record;
-      Options : Search_Options :=
+      Data    : Search_User_Data_Record;
+      Options : constant Search_Options :=
         (Scope          => Search_Scope'Val
             (Get_Index_In_List (Search.Scope_Combo)),
          Case_Sensitive => Get_Active (Search.Case_Check),
          Whole_Word     => Get_Active (Search.Whole_Word_Check),
          Regexp         => Get_Active (Search.Regexp_Check));
+
    begin
       for S in 1 .. Search_Regexps_Count (Kernel) loop
          Item := Add_Unique_Combo_Entry
@@ -652,6 +654,7 @@ package body Vsearch_Ext is
 
       --  Restore the options as before (they might have changed depending on
       --  the last predefined regexp we inserted)
+
       Select_Item
         (Get_List (Search.Scope_Combo), Search_Scope'Pos (Options.Scope));
       Set_Active (Search.Case_Check, Options.Case_Sensitive);
