7463468c10d7f3a9b74889bd19fca5f8779a7686
Quentin Ochem <ochem@adacore.com>
2002-05-17 07:58:33 +0000

    Modifiy the head of the file. Add 'Nothing' to type Diff_Action.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@76051 936e1b1b-40f2-da11-902a-00137254ae57

--- vdiff/src/diff_utils.ads
+++ vdiff/src/diff_utils.ads
@@ -1,10 +1,10 @@
 -----------------------------------------------------------------------
---                          G L I D E  I I                           --
+--                               G P S                               --
 --                                                                   --
---                        Copyright (C) 2001                         --
+--                        Copyright (C) 2002                         --
 --                            ACT-Europe                             --
 --                                                                   --
--- GLIDE is free software; you can redistribute it and/or modify  it --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
 -- the Free Software Foundation; either version 2 of the License, or --
 -- (at your option) any later version.                               --
@@ -13,7 +13,7 @@
 -- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
 -- General Public License for more details. You should have received --
--- a copy of the GNU General Public License along with this library; --
+-- a copy of the GNU General Public License along with this program; --
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
@@ -30,7 +30,7 @@ package Diff_Utils is
       Last  : Natural;
    end record;
 
-   type Diff_Action is (Append, Change, Delete);
+   type Diff_Action is (Append, Change, Delete, Nothing);
 
    type Diff_Occurrence;
 
