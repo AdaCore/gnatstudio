05817cc6bed66c60f32091d76276f624b7ad0087
Arnaud Charlet <charlet@adacore.com>
2002-05-21 09:33:00 +0000

    Minor reformatting. Change default font sizes
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@76147 936e1b1b-40f2-da11-902a-00137254ae57

--- widgets/cschtml/htmlgdkfontmanager.c
+++ widgets/cschtml/htmlgdkfontmanager.c
@@ -1,4 +1,4 @@
-/* -*- Mode: C; tab-width: 3; indent-tabs-mode: nil; c-basic-offset: 3 -*- */
+/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 3 -*- */
 /* This file is part of the CscHTML library.
 
    Copyright (C) 2000 Helix Code, Inc.
@@ -28,132 +28,138 @@
 /* FIXME this should be dynamically done, and based on the base font name.  */
 
 static guint real_font_sizes[CSC_HTML_FONT_STYLE_SIZE_MAX] = {
-   6, 6, 7, 8, 10, 11, 12
+   6, 7, 8, 10, 12, 14, 16
 };
 
 static void unref_fonts (gpointer key, GdkFont *font, gpointer user_data);
-
 
 /*****************************
  ** html_gdk_font_manager_new
  *****************************/
 
-HTMLGdkFontManager *html_gdk_font_manager_new (void) {
-	HTMLGdkFontManager * new = g_new (HTMLGdkFontManager, 1);
-   new->font_hash = g_hash_table_new(g_str_hash, g_str_equal);
-   return new;
+HTMLGdkFontManager *
+html_gdk_font_manager_new (void)
+{
+  HTMLGdkFontManager * new = g_new (HTMLGdkFontManager, 1);
+  new->font_hash = g_hash_table_new(g_str_hash, g_str_equal);
+  return new;
 }
 
 /***************
  ** unref_fonts
  ***************/
 
-static void unref_fonts (gpointer key, GdkFont *font, gpointer user_data) {
-   gdk_font_unref (font);
-   g_free (key);
+static void
+unref_fonts (gpointer key, GdkFont *font, gpointer user_data)
+{
+  gdk_font_unref (font);
+  g_free (key);
 }
 
 /*********************************
  ** html_gdk_font_manager_destroy
  *********************************/
 
-void html_gdk_font_manager_destroy (HTMLGdkFontManager *manager) {
-	g_return_if_fail (manager != NULL);
-   g_hash_table_foreach (manager->font_hash, (GHFunc) unref_fonts, NULL);
-   g_hash_table_destroy (manager->font_hash);
-	g_free (manager);
+void
+html_gdk_font_manager_destroy (HTMLGdkFontManager *manager)
+{
+  g_return_if_fail (manager != NULL);
+  g_hash_table_foreach (manager->font_hash, (GHFunc) unref_fonts, NULL);
+  g_hash_table_destroy (manager->font_hash);
+  g_free (manager);
 }
 
 /**********************************
  ** html_gdk_font_manager_get_font
  **********************************/
 
-GdkFont *html_gdk_font_manager_get_font
-   (HTMLGdkFontManager *manager,
-    CscHTMLFontStyle style,
-    const gchar *face)
+GdkFont *
+html_gdk_font_manager_get_font
+  (HTMLGdkFontManager *manager,
+   CscHTMLFontStyle style,
+   const gchar *face)
 {
-   gint   size;
-   gchar* weight_string;
-   gchar* slant_string;
-   gchar **font_names;
-   gchar* font_name;
-   PangoFontDescription* descr;
-   GdkFont *font = NULL;
-   gint i;
-   gchar  key [256];
-   
-   g_return_val_if_fail (manager != NULL, NULL);
-   g_return_val_if_fail (style < CSC_HTML_FONT_STYLE_MAX, NULL);
-   
-   if (face) {
-      sprintf (key, "%s%d\n", face, style);
-   } else {
-      sprintf (key, "default%d\n", style);
-   }
-   font = (GdkFont *)g_hash_table_lookup((GHashTable *)manager->font_hash, key);
-   if (font != NULL) {
-      return gdk_font_ref (font);
-   }
+  gint   size;
+  gchar* weight_string;
+  gchar* slant_string;
+  gchar **font_names;
+  gchar* font_name;
+  PangoFontDescription* descr;
+  GdkFont *font = NULL;
+  gint i;
+  gchar  key [256];
+  
+  g_return_val_if_fail (manager != NULL, NULL);
+  g_return_val_if_fail (style < CSC_HTML_FONT_STYLE_MAX, NULL);
+  
+  if (face) {
+    sprintf (key, "%s%d\n", face, style);
+  } else {
+    sprintf (key, "default%d\n", style);
+  }
+  font = (GdkFont *)g_hash_table_lookup((GHashTable *)manager->font_hash, key);
+  if (font != NULL) {
+     return gdk_font_ref (font);
+  }
 
-	/* Compute the most appropriate size for the font */
-	
-	size = style & CSC_HTML_FONT_STYLE_SIZE_MASK;
-	if (size == 0)
-		size = 3;
+  /* Compute the most appropriate size for the font */
 
-	size = real_font_sizes [size];
+  size = style & CSC_HTML_FONT_STYLE_SIZE_MASK;
+  if (size == 0)
+    size = 3;
 
-	/* Compute the weight of the font */
+  size = real_font_sizes [size];
 
-	if (style & CSC_HTML_FONT_STYLE_BOLD)
-		weight_string = "bold ";
-	else
-		weight_string = "medium ";
+  /* Compute the weight of the font */
+
+  if (style & CSC_HTML_FONT_STYLE_BOLD)
+    weight_string = "bold ";
+  else
+    weight_string = "medium ";
 
    /* Compute the slant of the font */
-	
-	if (style & CSC_HTML_FONT_STYLE_ITALIC)
-		slant_string = "italic ";
-	else
-		slant_string = "";
 
-   /* Special handling for fixed fonts: make sure we use a valid one */
+  if (style & CSC_HTML_FONT_STYLE_ITALIC)
+    slant_string = "italic ";
+  else
+    slant_string = "";
 
-   if (style & CSC_HTML_FONT_STYLE_FIXED)
-      font_names = g_strsplit ("courier new,courier", ",", 10);
-   else if (face)
-      font_names = g_strsplit (face, ",", 10);
-   else {
-      /* g_warning ("null font passed to html_gdk_font_manager_get_font"); */
-      font_names = g_strsplit ("sans", ",", 10);
-   }
+  /* Special handling for fixed fonts: make sure we use a valid one */
 
-   /* Find the first font that matches style */
+  if (style & CSC_HTML_FONT_STYLE_FIXED)
+    font_names = g_strsplit ("courier new,courier", ",", 10);
+  else if (face)
+    font_names = g_strsplit (face, ",", 10);
+  else {
+    /* g_warning ("null font passed to html_gdk_font_manager_get_font"); */
+    font_names = g_strsplit ("sans", ",", 10);
+  }
 
-	for (i = 0; (font == NULL) && (font_names[i]); i++) {
-      font_name = g_strdup_printf
-         ("%s %s%s%d", font_names [i], weight_string, slant_string, size);
-      descr = pango_font_description_from_string (font_name);
-      font = gdk_font_from_description (descr);	
-      pango_font_description_free (descr);
-      g_free (font_name);
-   }
+  /* Find the first font that matches style */
 
-   /* Fallback on a default font, that should exist on all systems */
+  for (i = 0; (font == NULL) && (font_names[i]); i++) {
+     font_name = g_strdup_printf
+       ("%s %s%s%d", font_names [i], weight_string, slant_string, size);
+     descr = pango_font_description_from_string (font_name);
+     font = gdk_font_from_description (descr);	
+     pango_font_description_free (descr);
+     g_free (font_name);
+  }
 
-	if (font == NULL) {
-		g_warning ("font `%s' not found", face);
+  /* Fallback on a default font, that should exist on all systems */
 
-      descr = pango_font_description_from_string ("sans 10");
-      font = gdk_font_from_description (descr);	
-      pango_font_description_free (descr);
-   }
+  if (font == NULL) {
+    g_warning ("font `%s' not found", face);
 
-	g_strfreev (font_names);
+    descr = pango_font_description_from_string ("sans 10");
+    font = gdk_font_from_description (descr);	
+    pango_font_description_free (descr);
+  }
 
-   g_hash_table_insert
-      ((GHashTable *)manager->font_hash, g_strdup(key), font);
+  g_strfreev (font_names);
+
+  g_hash_table_insert
+    ((GHashTable *)manager->font_hash, g_strdup(key), font);
    
-   return gdk_font_ref (font);
+  return gdk_font_ref (font);
 }
