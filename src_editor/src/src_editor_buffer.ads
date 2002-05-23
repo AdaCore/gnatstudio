f31a040b4168bee12141f24ffd78ec842e06b46e
Arnaud Charlet <charlet@adacore.com>
2002-05-23 10:52:49 +0000

    Update header. Minor reformatting.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@76307 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/src_editor_buffer.ads
+++ src_editor/src/src_editor_buffer.ads
@@ -4,7 +4,7 @@
 --                     Copyright (C) 2001-2002                       --
 --                            ACT-Europe                             --
 --                                                                   --
--- GPS is free  software; you can  redistribute it and/or modify  it --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
 -- the Free Software Foundation; either version 2 of the License, or --
 -- (at your option) any later version.                               --
@@ -13,7 +13,7 @@
 -- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
 -- General Public License for more details. You should have received --
--- a copy of the GNU General Public License along with this library; --
+-- a copy of the GNU General Public License along with this program; --
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
@@ -25,7 +25,7 @@
 --  reference support, etc.
 --  </description>
 
-with Glib;             use Glib;
+with Glib; use Glib;
 with Gtk;
 with Gtk.Main;
 with Gtk.Text_Buffer;
@@ -37,9 +37,9 @@ with Gtkada.Types;
 with Language;
 with Src_Highlighting;
 
-with GNAT.OS_Lib;         use GNAT.OS_Lib;
+with GNAT.OS_Lib; use GNAT.OS_Lib;
 
-with Commands;         use Commands;
+with Commands;    use Commands;
 with Glide_Kernel;
 with Src_Info;
 
