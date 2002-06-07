0890a596c44281ec19d0079182e9b9bffe0c48c3
Quentin Ochem <ochem@adacore.com>
2002-06-07 10:05:44 +0000

    Fix name of test.out file.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@76829 936e1b1b-40f2-da11-902a-00137254ae57

--- codefix/src/regression.adb
+++ codefix/src/regression.adb
@@ -1,3 +1,23 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2002                         --
+--                            ACT-Europe                             --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
 with Ada.Text_IO; use Ada.Text_IO;
 
 with GNAT.Directory_Operations; use GNAT.Directory_Operations;
@@ -80,8 +100,8 @@ begin
                    Success);
 
             Difference := Compare
-              ("regressions/" & Path_Name & "/err.ora",
-               "regressions/" & Path_Name & "/err.out");
+              ("regressions/" & Path_Name & "/err.out",
+               "regressions/" & Path_Name & "/test.out");
 
             if Difference /= 0 then
                Put_Line ("failed at line " & Difference'Img);
