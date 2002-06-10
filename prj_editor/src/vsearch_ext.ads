123b3e7a22669473ff52302ef3a132ea090818f8
Arnaud Charlet <charlet@adacore.com>
2002-06-10 08:36:44 +0000

    Update header. (Vsearch_Extended_Record): New field Prev_Menu_Item.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@76870 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/src/vsearch_ext.ads
+++ prj_editor/src/vsearch_ext.ads
@@ -1,10 +1,10 @@
 -----------------------------------------------------------------------
---                          G L I D E  I I                           --
+--                               G P S                               --
 --                                                                   --
---                        Copyright (C) 2001                         --
+--                     Copyright (C) 2001-2002                       --
 --                            ACT-Europe                             --
 --                                                                   --
--- GLIDE is free software; you can redistribute it and/or modify  it --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
 -- the Free Software Foundation; either version 2 of the License, or --
 -- (at your option) any later version.                               --
@@ -13,7 +13,7 @@
 -- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
 -- General Public License for more details. You should have received --
--- a copy of the GNU General Public License along with this library; --
+-- a copy of the GNU General Public License along with this program; --
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
@@ -62,9 +62,8 @@ private
       Extra_Information      : Gtk.Widget.Gtk_Widget;
       Search_Idle_Handler    : Gtk.Main.Idle_Handler_Id := 0;
       Last_Search_Context    : Find_Utils.Search_Context_Access;
-
       Next_Menu_Item         : Gtk.Menu_Item.Gtk_Menu_Item;
-
+      Prev_Menu_Item         : Gtk.Menu_Item.Gtk_Menu_Item;
       Find_Next              : Boolean := False;
    end record;
 
