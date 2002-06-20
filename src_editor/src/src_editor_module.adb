ca15198b0c5b6fc1be90c63ccb33234fa569bb4d
Arnaud Charlet <charlet@adacore.com>
2002-06-20 08:31:27 +0000

    (On_Open_File): Fix error in previous check in.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@77068 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/src_editor_module.adb
+++ src_editor/src/src_editor_module.adb
@@ -1025,7 +1025,7 @@ package body Src_Editor_Module is
          end if;
 
          Open_File_Editor (Kernel, Filename);
-         Change_Directory (Dir_Name (Filename));
+         Change_Dir (Dir_Name (Filename));
       end;
 
    exception
