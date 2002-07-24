e5e2971b99c68f3ca9e451900e90c5af262c2661
Arnaud Charlet <charlet@adacore.com>
2002-07-24 13:29:18 +0000

    i18n of strings.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@77694 936e1b1b-40f2-da11-902a-00137254ae57

--- vdiff/src/vdiff_module.adb
+++ vdiff/src/vdiff_module.adb
@@ -111,7 +111,7 @@ package body Vdiff_Module is
            (Kernel, Vdiff.Clist1, Vdiff.Clist2, File1, File2, Result);
          Show_All (Vdiff);
          Child := Put (Get_MDI (Kernel), Vdiff);
-         Set_Title (Child, "Visual Comparison");
+         Set_Title (Child, -"Visual Comparison");
 
          Free (Result);
       end;
@@ -239,7 +239,7 @@ package body Vdiff_Module is
                Get_Pref (Kernel, Default_Widget_Height));
             Show_All (Vdiff);
             Child := Put (Get_MDI (Kernel), Vdiff);
-            Set_Title (Child, "Visual Comparison");
+            Set_Title (Child, -"Visual Comparison");
             return True;
          end;
       end if;
