ee5b67093afd2dfd8fffbec2192f0fab5e296832
Nicolas Setton <setton@adacore.com>
2002-07-25 13:10:28 +0000

    (Location_Callback): Take into account the TAB expansion when placing  the cursor.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@77764 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/src_editor_module.adb
+++ src_editor/src/src_editor_module.adb
@@ -1202,17 +1202,14 @@ package body Src_Editor_Module is
    begin
       Grab_Focus (D.Edit);
 
-      if Is_Valid_Location (D.Edit, D.Line, D.Column) then
-         Set_Cursor_Location (D.Edit, D.Line, D.Column);
+      if Is_Valid_Location (D.Edit, D.Line) then
+         Set_Screen_Location (D.Edit, D.Line, D.Column);
 
          if D.Column_End /= 0
            and then Is_Valid_Location (D.Edit, D.Line, D.Column_End)
          then
             Select_Region (D.Edit, D.Line, D.Column, D.Line, D.Column_End);
          end if;
-
-      elsif Is_Valid_Location (D.Edit, D.Line) then
-         Set_Cursor_Location (D.Edit, D.Line);
       end if;
 
       return False;
