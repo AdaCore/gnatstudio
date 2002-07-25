dacb23f92459b7a0a77967bb7e732c6f1fc30c00
Emmanuel Briot <briot@adacore.com>
2002-07-25 16:41:24 +0000

    (On_Process_Notebook_Switch_Page): Make sure the MDI Window menu's key  shortcuts can be changed dynamically
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@77793 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/main_debug_window_pkg-callbacks.adb
+++ gvd/gvd/main_debug_window_pkg-callbacks.adb
@@ -113,7 +113,9 @@ package body Main_Debug_Window_Pkg.Callbacks is
       Update_External_Dialogs (Main, Gtk_Widget (Process));
 
       Menu_Item := Gtk_Menu_Item (Get_Widget (Main.Factory, -"/Window"));
-      Set_Submenu (Menu_Item, Create_Menu (Process.Process_Mdi));
+      Set_Submenu
+        (Menu_Item,
+         Create_Menu (Process.Process_Mdi, Main.Main_Accel_Group));
 
       if Main.Breakpoints_Editor /= null then
          Set_Process
