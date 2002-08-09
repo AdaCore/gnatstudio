fab06e863303e7ae4a5571bc3dbe8a8aada948bf
Quentin Ochem <ochem@adacore.com>
2002-08-09 15:30:14 +0000

    Add subprogram Unchecked_Free.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@78064 936e1b1b-40f2-da11-902a-00137254ae57

--- codefix/src/codefix-text_manager-ada_extracts.adb
+++ codefix/src/codefix-text_manager-ada_extracts.adb
@@ -67,6 +67,17 @@ package body Codefix.Text_Manager.Ada_Extracts is
       Free (This.Stop);
    end Free;
 
+   --------------------
+   -- Unchecked_Free --
+   --------------------
+
+   procedure Unchecked_Free (This : in out Ada_Instruction) is
+   begin
+      Unchecked_Free (Extract (This));
+      This.Start := Null_File_Cursor;
+      This.Stop := Null_File_Cursor;
+   end Unchecked_Free;
+
    -----------
    -- Clone --
    -----------
@@ -219,6 +230,17 @@ package body Codefix.Text_Manager.Ada_Extracts is
       Free (This.Elements_List);
       Free (This.Back);
    end Free;
+
+   --------------------
+   -- Unchecked_Free --
+   --------------------
+
+   procedure Unchecked_Free (This : in out Ada_List) is
+   begin
+      Unchecked_Free (Ada_Instruction (This));
+      This.Elements_List := Tokens_List.Null_List;
+      This.Back := null;
+   end Unchecked_Free;
 
    -----------
    -- Clone --
@@ -636,7 +658,7 @@ package body Codefix.Text_Manager.Ada_Extracts is
    is
       Current_Node  : Tokens_List.List_Node;
       Current_Token : Token_Record;
-      Length        : Natural :=
+      Length        : constant Natural :=
         Token_Deleted.Last_Col - Token_Deleted.First_Col + 1;
    begin
       Current_Node := First (This.Elements_List);
