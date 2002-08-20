58fb641c007b46092b1eb01b435a99a047d732eb
Quentin Ochem <ochem@adacore.com>
2002-08-20 12:30:29 +0000

    Initial revision for this file.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@78106 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ codefix/src/codefix-merge_utils.adb
@@ -0,0 +1,585 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2002                         --
+--                            ACT-Europe                             --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+package body Codefix.Merge_Utils is
+
+   -------------------
+   -- Generic_Merge --
+   -------------------
+
+   procedure Generic_Merge
+     (Result              : out Merge_Type;
+      Object_1, Object_2  : Merge_Type;
+      Success             : out Boolean;
+      Chronologic_Changes : Boolean)
+   is
+
+      It_1, It_2 : Merge_Iterator;
+      New_Unit   : Merged_Unit;
+
+      procedure Merge_Original;
+      procedure Merge_Modified;
+      procedure Merge_Deleted;
+      procedure Merge_Created;
+
+      procedure Merge_Original is
+      begin
+         case Get_Merge_Info (Data (It_2)) is
+            when Original_Unit =>
+               Append (Result, Clone (Data (It_1)));
+               It_1 := Next (It_1);
+               It_2 := Next (It_2);
+            when Unit_Modified =>
+               Append (Result, Clone (Data (It_2)));
+               It_1 := Next (It_1);
+               It_2 := Next (It_2);
+            when Unit_Deleted =>
+               Append (Result, Clone (Data (It_2)));
+               It_1 := Next (It_1);
+               It_2 := Next (It_2);
+            when Unit_Created =>
+               Append (Result, Clone (Data (It_1)));
+               It_1 := Next (It_1);
+         end case;
+      end Merge_Original;
+
+      procedure Merge_Modified is
+      begin
+         case Get_Merge_Info (Data (It_2)) is
+            when Original_Unit =>
+               Append (Result, Clone (Data (It_1)));
+               It_1 := Next (It_1);
+               It_2 := Next (It_2);
+            when Unit_Modified =>
+               Merge_Units
+                 (New_Unit,
+                  Data (It_1),
+                  Data (It_2),
+                  Success,
+                  Chronologic_Changes);
+
+               if Success then
+                  Append (Result, New_Unit);
+               elsif Chronologic_Changes then
+                  Append (Result, Clone (Data (It_2)));
+                  Success := True;
+               end if;
+
+               It_1 := Next (It_1);
+               It_2 := Next (It_2);
+            when Unit_Deleted =>
+               Success := False;
+            when Unit_Created =>
+               Append (Result, Clone (Data (It_1)));
+               It_1 := Next (It_1);
+         end case;
+      end Merge_Modified;
+
+      procedure Merge_Deleted is
+      begin
+         case Get_Merge_Info (Data (It_2)) is
+            when Original_Unit =>
+               Append (Result, Clone (Data (It_1)));
+               It_1 := Next (It_1);
+               It_2 := Next (It_2);
+            when Unit_Modified =>
+               Success := False;
+            when Unit_Deleted =>
+               Append (Result, Clone (Data (It_1)));
+               It_1 := Next (It_1);
+               It_2 := Next (It_2);
+            when Unit_Created =>
+               Append (Result, Clone (Data (It_1)));
+               It_1 := Next (It_1);
+         end case;
+      end Merge_Deleted;
+
+      procedure Merge_Created is
+      begin
+         case Get_Merge_Info (Data (It_2)) is
+            when Original_Unit =>
+               Append (Result, Clone (Data (It_2)));
+               It_2 := Next (It_2);
+            when Unit_Modified =>
+               Append (Result, Clone (Data (It_2)));
+               It_2 := Next (It_2);
+            when Unit_Deleted =>
+               Append (Result, Clone (Data (It_2)));
+               It_2 := Next (It_2);
+            when Unit_Created =>
+               Append (Result, Clone (Data (It_1)));
+
+               if Data (It_1) /= Data (It_2) then
+                  It_1 := Next (It_1);
+               else
+                  It_1 := Next (It_1);
+                  It_2 := Next (It_2);
+               end if;
+               --  This block means that if the same thing is added at the same
+               --  position in both objects, then the result must have only one
+               --  occurence of it.
+         end case;
+      end Merge_Created;
+
+      --  begin of Merge
+
+   begin
+
+      It_1 := First (Object_1);
+      It_2 := First (Object_2);
+      Success := True;
+
+      while not Is_Null (It_1) and then not Is_Null (It_2) loop
+         if It_1 < It_2 then
+            Append (Result, Clone (Data (It_1)));
+            It_1 := Next (It_1);
+         elsif It_2 < It_1 then
+            Append (Result, Clone (Data (It_2)));
+            It_2 := Next (It_2);
+         else
+            case Get_Merge_Info (Data (It_1)) is
+               when Original_Unit =>
+                  Merge_Original;
+               when Unit_Modified =>
+                  Merge_Modified;
+               when Unit_Created =>
+                  Merge_Created;
+               when Unit_Deleted =>
+                  Merge_Deleted;
+            end case;
+         end if;
+
+         if not Success then
+            return;
+         end if;
+      end loop;
+
+
+      while not Is_Null (It_1) loop
+         Append (Result, Clone (Data (It_1)));
+         It_1 := Next (It_1);
+      end loop;
+
+      while not Is_Null (It_2) loop
+         Append (Result, Clone (Data (It_2)));
+         It_2 := Next (It_2);
+      end loop;
+
+   end Generic_Merge;
+
+   ------------
+   -- Delete --
+   ------------
+
+   procedure Delete (This : in out Mergable_String; Start, Len : Natural) is
+      Position   : constant Natural := Get_Array_Position (This, Start);
+      Offset     : Natural := 0;
+      Copy_Infos : constant Merge_Array := This.Infos.all;
+      J          : Natural;
+   begin
+      J := Position;
+
+      for K in 1 .. Len loop
+
+         while Copy_Infos (J) = Unit_Deleted loop
+            J := J + 1;
+         end loop;
+
+         if Copy_Infos (J) = Original_Unit
+           or else Copy_Infos (J) = Unit_Modified
+         then
+            This.Infos (J) := Unit_Deleted;
+         elsif Copy_Infos (J) = Unit_Created then
+            Delete_Char (This.Str, J - Offset);
+            Delete_Info (This.Infos, J - Offset);
+            Offset := Offset + 1;
+         end if;
+
+         J := J + 1;
+
+      end loop;
+   end Delete;
+
+   ------------
+   -- Insert --
+   ------------
+
+   procedure Insert
+     (This : in out Mergable_String; Start : Natural; Value : String)
+   is
+      Insertion_Array : constant Merge_Array (Value'Range) :=
+        (others => Unit_Created);
+      New_Str         : Dynamic_String;
+      New_Infos       : Ptr_Merge_Array;
+      Position        : constant Natural := Get_Array_Position (This, Start);
+   begin
+      if Position > This.Str'First then
+         New_Str := new String'
+           (This.Str (This.Str'First .. Position - 1)
+              & Value
+              & This.Str (Position .. This.Str'Last));
+         New_Infos := new Merge_Array'
+           (This.Infos (This.Infos'First .. Position - 1)
+              & Insertion_Array
+              & This.Infos (Position .. This.Str'Last));
+      else
+         New_Str := new String'(Value & This.Str.all);
+         New_Infos := new Merge_Array '(Insertion_Array & This.Infos.all);
+      end if;
+
+      Free (This.Str);
+      Free (This.Infos);
+      This.Str := New_Str;
+      This.Infos := New_Infos;
+
+   end Insert;
+
+   -------------
+   -- Replace --
+   -------------
+
+   procedure Replace
+     (This : in out Mergable_String; Start, Len : Natural; Value : String) is
+   begin
+      Delete (This, Start, Len);
+      Insert (This, Start, Value);
+   end Replace;
+
+   ------------------
+   -- Merge_String --
+   ------------------
+
+   procedure Merge_String
+     (Result              : out Mergable_String;
+      Object_1, Object_2  : Mergable_String;
+      Success             : out Boolean;
+      Chronologic_Changes : Boolean)
+   is
+
+      procedure Merge_Intern is new Generic_Merge
+        (Mergable_String,
+         String_Char,
+         String_Iterator,
+         Merge_Units => Merge_Null);
+
+   begin
+      Merge_Intern
+        (Result,
+         Object_1,
+         Object_2,
+         Success,
+         Chronologic_Changes);
+   end Merge_String;
+
+   ----------
+   -- Free --
+   ----------
+
+   procedure Free (This : in out Mergable_String) is
+   begin
+      Free (This.Str);
+      Free (This.Infos);
+   end Free;
+
+   ---------------
+   -- To_String --
+   ---------------
+
+   function To_String (This : Mergable_String) return String is
+
+      function Get_Block (Current_Char : Natural) return String;
+
+      function Get_Block (Current_Char : Natural) return String is
+      begin
+         if This.Infos (Current_Char) = Unit_Deleted then
+            for J in Current_Char .. This.Str'Last loop
+               if This.Infos (J) /= Unit_Deleted then
+                  return Get_Block (J);
+               end if;
+            end loop;
+
+            return "";
+         else
+            for J in Current_Char .. This.Str'Last loop
+               if This.Infos (J) = Unit_Deleted then
+                  return This.Str (Current_Char .. J - 1) & Get_Block (J);
+               end if;
+            end loop;
+         end if;
+
+         return This.Str (Current_Char .. This.Str'Last);
+      end Get_Block;
+
+   begin
+      if This.Str'Length > 0 then
+         return Get_Block (This.Str'First);
+      else
+         return "";
+      end if;
+   end To_String;
+
+   ------------
+   -- Assign --
+   ------------
+
+   procedure Assign
+     (This : in out Mergable_String; Value : Mergable_String) is
+   begin
+      Free (This.Str);
+      Free (This.Infos);
+      This.Str := Clone (Value.Str);
+      This.Infos := new Merge_Array'(Value.Infos.all);
+   end Assign;
+
+   -----------
+   -- Clone --
+   -----------
+
+   function Clone (This : Mergable_String) return Mergable_String is
+   begin
+      return (Clone (This.Str), new Merge_Array'(This.Infos.all));
+   end Clone;
+
+   ------------------------
+   -- To_Mergable_String --
+   ------------------------
+
+   function To_Mergable_String (This : String) return Mergable_String is
+      Result : Mergable_String;
+   begin
+      Result.Str := new String'(This);
+      Result.Infos := new Merge_Array (This'Range);
+      Result.Infos.all := (others => Original_Unit);
+      return Result;
+   end To_Mergable_String;
+
+   -----------
+   -- First --
+   -----------
+
+   function First (This : Mergable_String) return String_Iterator is
+   begin
+      return (This, This.Str'First);
+   end First;
+
+   ----------
+   -- Next --
+   ----------
+
+   function Next (This : String_Iterator) return String_Iterator is
+      Result : String_Iterator := This;
+   begin
+      Result.Position := Result.Position + 1;
+      return Result;
+   end Next;
+
+   ----------
+   -- Data --
+   ----------
+
+   function Data (This : String_Iterator) return String_Char is
+   begin
+      return
+        (This.Object.Str (This.Position),
+         This.Object.Infos (This.Position));
+   end Data;
+
+   -------------
+   -- Is_Null --
+   -------------
+
+   function Is_Null (This : String_Iterator) return Boolean is
+   begin
+      return This.Position > This.Object.Str'Last;
+   end Is_Null;
+
+   ---------
+   -- "<" --
+   ---------
+
+   function "<" (Left, Right : String_Iterator) return Boolean is
+   begin
+      return Left.Position < Right.Position;
+   end "<";
+
+   --------------------
+   -- Get_Merge_Info --
+   --------------------
+
+   function Get_Merge_Info (This : String_Char) return Merge_Info is
+   begin
+      return This.Info;
+   end Get_Merge_Info;
+
+   ------------
+   -- Append --
+   ------------
+
+   procedure Append (This : in out Mergable_String; Object : String_Char) is
+      Garbage_Str   : Dynamic_String := This.Str;
+      Garbage_Infos : Ptr_Merge_Array := This.Infos;
+   begin
+      This.Str := new String'(This.Str.all & Object.Char);
+      This.Infos := new Merge_Array'(This.Infos.all & Object.Info);
+
+      Free (Garbage_Str);
+      Free (Garbage_Infos);
+   end Append;
+
+   -----------
+   -- Clone --
+   -----------
+
+   function Clone (This : String_Char) return String_Char is
+   begin
+      return This;
+   end Clone;
+
+   ----------------
+   -- Merge_Null --
+   ----------------
+
+   procedure Merge_Null
+     (Result              : out String_Char;
+      Object_1, Object_2  : String_Char;
+      Success             : out Boolean;
+      Chronologic_Changes : Boolean)
+   is
+      pragma Unreferenced (Object_1, Object_2, Chronologic_Changes);
+   begin
+      Result := (Character'Val (0), Original_Unit);
+      Success := False;
+   end Merge_Null;
+
+   ---------
+   -- "=" --
+   ---------
+
+   function "=" (Left, Right : String_Char) return Boolean is
+   begin
+      return Left.Char = Right.Char;
+   end "=";
+
+   ------------------------
+   -- Get_Array_Position --
+   ------------------------
+
+   function Get_Array_Position (Str : Mergable_String; Position : Natural)
+     return Natural is
+      Existent_Chars : Natural := 0;
+   begin
+      for J in 1 .. Str.Infos'Last loop
+         if Str.Infos (J) /= Unit_Deleted then
+            Existent_Chars := Existent_Chars + 1;
+         end if;
+
+         if Existent_Chars = Position then
+            return J;
+         end if;
+      end loop;
+
+      return Str.Infos'Last;
+   end Get_Array_Position;
+
+   -----------------
+   -- Delete_Char --
+   -----------------
+
+   procedure Delete_Char (This : in out Dynamic_String; Position : Natural) is
+      Garbage : Dynamic_String := This;
+   begin
+      if Position < This'Last then
+         This := new String'(This (This'First .. Position - 1) &
+                               This (Position + 1 .. This'Last));
+      else
+         This := new String'(This (This'First .. Position - 1));
+      end if;
+
+      Free (Garbage);
+   end Delete_Char;
+
+   -----------------
+   -- Delete_Info --
+   -----------------
+
+   procedure Delete_Info (This : in out Ptr_Merge_Array; Position : Natural) is
+      Garbage : Ptr_Merge_Array := This;
+   begin
+      if Position < This'Last then
+         This := new Merge_Array'(This (This'First .. Position - 1) &
+                                    This (Position + 1 .. This'Last));
+      else
+         This := new Merge_Array'(This (This'First .. Position - 1));
+      end if;
+
+      Free (Garbage);
+   end Delete_Info;
+
+   -----------
+   -- Reset --
+   -----------
+
+   procedure Reset (This : in out Mask_Iterator) is
+   begin
+      This.Info_Index := 1;
+      This.Real_Index := 1;
+   end Reset;
+
+   -------------------
+   -- Get_Next_Area --
+   -------------------
+
+   procedure Get_Next_Area
+     (This       : Mergable_String;
+      It         : in out Mask_Iterator;
+      Start, Len : out Natural;
+      Info       : out Merge_Info) is
+   begin
+
+      Len := 0;
+
+      if It.Info_Index > This.Infos'Last then
+         return;
+      end if;
+
+      Start := It.Real_Index;
+      Info := This.Infos (It.Info_Index);
+
+      case Info is
+         when Unit_Modified | Unit_Created | Original_Unit =>
+            while It.Info_Index <= This.Infos'Last
+              and then This.Infos (It.Info_Index) = Info
+            loop
+               Len := Len + 1;
+               It.Real_Index := It.Real_Index + 1;
+               It.Info_Index := It.Info_Index + 1;
+            end loop;
+         when Unit_Deleted =>
+            while It.Info_Index <= This.Infos'Last
+              and then This.Infos (It.Info_Index) = Info
+            loop
+               Len := Len + 1;
+               It.Info_Index := It.Info_Index + 1;
+            end loop;
+      end case;
+
+   end Get_Next_Area;
+
+end Codefix.Merge_Utils;
---
+++ codefix/src/codefix-merge_utils.ads
@@ -0,0 +1,200 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2002                         --
+--                            ACT-Europe                             --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+--  Those functions implements the main merge function, and all merge tools
+--  used with strings.
+
+with Ada.Unchecked_Deallocation;
+
+package Codefix.Merge_Utils is
+
+   type Merge_Info is
+     (Original_Unit, Unit_Modified, Unit_Deleted, Unit_Created);
+
+   generic
+
+      type Merge_Type is private;
+      --  Type of objects that can be merged.
+      --  Note : units recorded in Merge_Type are not necessary adjacents.
+
+      type Merged_Unit is private;
+      --  Type of objects contained in Merge_Type, which are the basic elements
+      --  of the merge.
+
+      type Merge_Iterator is private;
+      --  Object by what it is possible to access objects in Merge_Type.
+
+      with function First (This : Merge_Type) return Merge_Iterator is <>;
+      --  Return an iterator positioned in the first object of This.
+
+      with function Next (This : Merge_Iterator) return Merge_Iterator is <>;
+      --  Return the iterator positioned after This.
+
+      with function Data (This : Merge_Iterator) return Merged_Unit is <>;
+      --  Return the object referenced by an iterator.
+
+      with function Is_Null (This : Merge_Iterator) return Boolean is <>;
+      --  Return True if the iterator doesn't references anything. Typycally,
+      --  when the loop has ended.
+
+      with function "<" (Left, Right : Merge_Iterator) return Boolean is <>;
+      --  Return True if Left is before Right.
+
+      with function Get_Merge_Info
+        (This : Merged_Unit) return Merge_Info is <>;
+      --  Return the Merge_Info of an unit.
+
+      with procedure Append
+        (This : in out Merge_Type; Object : Merged_Unit) is <>;
+      --  Add an unit in This.
+
+      with function Clone (This : Merged_Unit) return Merged_Unit is <>;
+      --  Clone all informations contained in This, specially the pointers.
+
+      with procedure Merge_Units
+        (Result              : out Merged_Unit;
+         Object_1, Object_2  : Merged_Unit;
+         Success             : out Boolean;
+         Chronologic_Changes : Boolean);
+      --  If the units contained in a Merge_Type are themselves mergable, then
+      --  this function should is the one that merge them. Otherwise, it
+      --  doesn't have to do anything.
+
+      with function "=" (Left, Right : Merged_Unit) return Boolean is <>;
+      --  Return True if Left contains same informations as Right.
+
+   procedure Generic_Merge
+     (Result              : out Merge_Type;
+      --  This is the result of the merge. Note that each unit in this
+      --  object is created using clone functions.
+
+      Object_1, Object_2  : Merge_Type;
+      --  These are the two objects that have to be merged into result.
+
+      Success             : out Boolean;
+      --  Success is false when the merge has non sens.
+
+      Chronologic_Changes : Boolean
+      --  If Chronologic_Changes is true, Object_2 should have been created
+      --  after Object_1. This information can help the function to solve
+      --  some ambiguities.
+
+      );
+
+
+   type Mergable_String is private;
+
+   procedure Delete (This : in out Mergable_String; Start, Len : Natural);
+   --  Delete len characters from Start.
+
+   procedure Insert
+     (This : in out Mergable_String; Start : Natural; Value : String);
+   --  Insert Value on (means 'before') the Start Position.
+
+   procedure Replace
+     (This : in out Mergable_String; Start, Len : Natural; Value : String);
+   --  Replace Len characters from Start column by value.
+
+   procedure Merge_String
+     (Result              : out Mergable_String;
+      Object_1, Object_2  : Mergable_String;
+      Success             : out Boolean;
+      Chronologic_Changes : Boolean);
+   --  Merge two strings into Result. For details see Generic_Merge above.
+
+   procedure Free (This : in out Mergable_String);
+   --  Free the memory associated to a Mergable_String.
+
+   function To_String (This : Mergable_String) return String;
+   --  Return the current value of the string contained in This.
+
+   procedure Assign (This : in out Mergable_String; Value : Mergable_String);
+   --  Assign values from 'Value' to 'This'.
+
+   function Clone (This : Mergable_String) return Mergable_String;
+   --  Clone each information contained in This, specially the pointers.
+
+   function To_Mergable_String (This : String) return Mergable_String;
+   --  Convert a simple String to a Mergeable_String,
+
+   type Mask_Iterator is private;
+
+   procedure Reset (This : in out Mask_Iterator);
+   --  Initialize all fields of This at the beginning of mergeables strings>
+
+   procedure Get_Next_Area
+     (This       : Mergable_String;
+      It         : in out Mask_Iterator;
+      Start, Len : out Natural;
+      Info       : out Merge_Info);
+   --  Return a text area composed by only one type of information. By using
+   --  those informations, it is possible to transform the original string to
+   --  the new one. When all areas are gotten, Len will be 0.
+
+private
+
+   type Merge_Array is array (Natural range <>) of Merge_Info;
+
+   type Ptr_Merge_Array is access all Merge_Array;
+
+   procedure Free is new
+     Ada.Unchecked_Deallocation (Merge_Array, Ptr_Merge_Array);
+
+   type Mergable_String is record
+      Str   : Dynamic_String;
+      Infos : Ptr_Merge_Array;
+   end record;
+
+   type String_Char is record
+      Char : Character;
+      Info : Merge_Info;
+   end record;
+
+   type String_Iterator is record
+      Object   : Mergable_String;
+      Position : Natural;
+   end record;
+
+   function First (This : Mergable_String) return String_Iterator;
+   function Next (This : String_Iterator) return String_Iterator;
+   function Data (This : String_Iterator) return String_Char;
+   function Is_Null (This : String_Iterator) return Boolean;
+   function "<" (Left, Right : String_Iterator) return Boolean;
+   function Get_Merge_Info (This : String_Char) return Merge_Info;
+   procedure Append (This : in out Mergable_String; Object : String_Char);
+   function Clone (This : String_Char) return String_Char;
+   procedure Merge_Null
+     (Result              : out String_Char;
+      Object_1, Object_2  : String_Char;
+      Success             : out Boolean;
+      Chronologic_Changes : Boolean);
+   function "=" (Left, Right : String_Char) return Boolean;
+
+   function Get_Array_Position (Str : Mergable_String; Position : Natural)
+     return Natural;
+
+   procedure Delete_Char (This : in out Dynamic_String; Position : Natural);
+   procedure Delete_Info (This : in out Ptr_Merge_Array; Position : Natural);
+
+   type Mask_Iterator is record
+      Info_Index, Real_Index : Natural;
+   end record;
+
+end Codefix.Merge_Utils;
