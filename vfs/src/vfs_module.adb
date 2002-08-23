cf76b36e5733c7c1fe7d45af42b84d1d15201f53
Arnaud Charlet <charlet@adacore.com>
2002-08-23 21:15:04 +0000

    The Virtual File System module
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@78272 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ vfs/src/vfs_module.adb
@@ -0,0 +1,393 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2002                         --
+--                            ACT-Europe                             --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Ada.Exceptions;            use Ada.Exceptions;
+with Basic_Types;               use Basic_Types;
+with File_Utils;                use File_Utils;
+with GNAT.Directory_Operations; use GNAT.Directory_Operations;
+with GNAT.OS_Lib;               use GNAT.OS_Lib;
+with GNAT.Regexp;               use GNAT.Regexp;
+with Glib;                      use Glib;
+with Glib.Object;               use Glib.Object;
+with Glide_Intl;                use Glide_Intl;
+with Glide_Kernel;              use Glide_Kernel;
+with Glide_Kernel.Console;      use Glide_Kernel.Console;
+with Glide_Kernel.Modules;      use Glide_Kernel.Modules;
+with Gtk.Menu;                  use Gtk.Menu;
+with Gtk.Menu_Item;             use Gtk.Menu_Item;
+with Gtk.Widget;                use Gtk.Widget;
+with Traces;                    use Traces;
+with String_List_Utils;         use String_List_Utils;
+with String_Utils;              use String_Utils;
+
+package body VFS_Module is
+
+   VFS_Module_Name : constant String := "VFS_Module";
+   Me              : constant Debug_Handle := Create (VFS_Module_Name);
+   VFS_Module_Id   : Module_ID;
+
+   -----------------
+   -- Subprograms --
+   -----------------
+
+   procedure VFS_Contextual
+     (Object  : access GObject_Record'Class;
+      Context : access Selection_Context'Class;
+      Menu    : access Gtk.Menu.Gtk_Menu_Record'Class);
+   --  Add entries to the conextual menu if necessary
+
+   function VFS_Command_Handler
+     (Kernel  : access Kernel_Handle_Record'Class;
+      Command : String;
+      Args    : String_List_Utils.String_List.List) return String;
+   --  Interactive command handler for the vfs module.
+
+   procedure On_Delete
+     (Widget  : access GObject_Record'Class;
+      Context : Selection_Context_Access);
+   --  Delete recursively the file or directory described in the context
+
+   --------------------------
+   -- VFS_Command_Handler --
+   --------------------------
+
+   function VFS_Command_Handler
+     (Kernel  : access Kernel_Handle_Record'Class;
+      Command : String;
+      Args    : String_List_Utils.String_List.List) return String
+   is
+      pragma Unreferenced (Kernel);
+
+      use String_List_Utils.String_List;
+
+      Node     : List_Node;
+      Filename : Basic_Types.String_Access;
+      Success  : Boolean;
+      Result   : GNAT.OS_Lib.String_Access;
+
+      procedure List_Files (Pattern : String);
+      --  List files following Pattern
+      --  Pattern may contain directory information and
+      --  regular expressions.
+
+      procedure Append_Result (S : String);
+      --  Appends S & ASCII.LF to Result.
+      --  Result must not be set to Null when calling this subprogram.
+
+      -------------------
+      -- Append_Result --
+      -------------------
+
+      procedure Append_Result (S : String) is
+         Tmp : GNAT.OS_Lib.String_Access := Result;
+      begin
+         Result := new String'(Result.all & S & ASCII.LF);
+         Free (Tmp);
+      end Append_Result;
+
+      ----------------
+      -- List_Files --
+      ----------------
+
+      procedure List_Files (Pattern : String) is
+         Directory_Name : constant String := Dir_Name (Pattern);
+         Base           : constant String := Base_Name (Pattern);
+         Is_Cur_Dir     : Boolean;
+         File_Regexp    : Regexp;
+         Directory      : GNAT.OS_Lib.String_Access;
+         Dir            : Dir_Type;
+         Buffer         : String (1 .. 4096);
+         Last           : Natural;
+
+      begin
+         if Is_Directory (Pattern) then
+            Directory := new String'(Name_As_Directory (Pattern));
+            File_Regexp :=
+              Compile ("*", Glob => True,
+                       Case_Sensitive => Filenames_Are_Case_Sensitive);
+         else
+            Directory := new String'(Directory_Name);
+
+            if Base = "" then
+               File_Regexp :=
+                 Compile ("*", Glob => True,
+                          Case_Sensitive => Filenames_Are_Case_Sensitive);
+            else
+               File_Regexp :=
+                 Compile (Base, Glob => True,
+                          Case_Sensitive => Filenames_Are_Case_Sensitive);
+            end if;
+         end if;
+
+         Is_Cur_Dir :=
+           Normalize_Pathname (Directory.all, Resolve_Links => False) =
+           Get_Current_Dir;
+         Open (Dir, Directory.all);
+
+         loop
+            Read (Dir, Buffer, Last);
+
+            exit when Last = 0;
+
+            if Match (Buffer (1 .. Last), File_Regexp) then
+               if Is_Cur_Dir then
+                  Append_Result (Buffer (1 .. Last));
+               else
+                  Append_Result (Directory.all & Buffer (1 .. Last));
+               end if;
+            end if;
+         end loop;
+
+         Close (Dir);
+         Free (Directory);
+
+      exception
+         when Error_In_Regexp =>
+            Free (Directory);
+            raise;
+      end List_Files;
+
+   begin
+      if Command = "pwd" then
+         return Get_Current_Dir;
+
+      elsif Command = "cd" or else Command = "delete" then
+         Node := First (Args);
+
+         while Node /= Null_Node loop
+            if Filename = null then
+               Filename := new String'(Data (Node));
+            else
+               Free (Filename);
+               return Command & ": " & (-"too many parameters");
+            end if;
+
+            Node := Next (Node);
+         end loop;
+
+         if Filename /= null then
+            declare
+               File : constant String := Filename.all;
+            begin
+               Free (Filename);
+
+               if Command = "cd" then
+                  begin
+                     Change_Dir (File);
+                  exception
+                     when Directory_Error =>
+                        return Command & ": " &
+                          (-"cannot change current directory");
+                  end;
+
+               elsif Command = "delete" then
+                  if Is_Directory (File) then
+                     begin
+                        Remove_Dir (File, True);
+                     exception
+                        when Directory_Error =>
+                           return Command & ": " & "cannot delete directory";
+                     end;
+
+                  else
+                     Delete_File (File, Success);
+
+                     if not Success then
+                        return Command & ": " & (-"cannot delete file");
+                     end if;
+                  end if;
+               end if;
+
+               return "";
+            end;
+
+         else
+            return Command & ": " & (-"missing name parameter");
+         end if;
+
+      elsif Command = "dir" or else Command = "ls" then
+         Result := new String'("");
+         Node := First (Args);
+
+         begin
+            if Node = Null_Node then
+               List_Files ("*");
+            else
+               while Node /= Null_Node loop
+                  List_Files (Data (Node));
+                  Node := Next (Node);
+
+                  exit when Node = Null_Node;
+               end loop;
+            end if;
+
+         exception
+            when Error_In_Regexp =>
+               Free (Result);
+               return (-"error in regexp: ") & Data (Node);
+         end;
+
+         declare
+            R : constant String := Result.all;
+         begin
+            Free (Result);
+            return R;
+         end;
+
+      else
+         return -"command not recognized: " & Command;
+      end if;
+   end VFS_Command_Handler;
+
+   ---------------
+   -- On_Delete --
+   ---------------
+
+   procedure On_Delete
+     (Widget  : access GObject_Record'Class;
+      Context : Selection_Context_Access)
+   is
+      pragma Unreferenced (Widget);
+
+      File    : constant File_Selection_Context_Access :=
+        File_Selection_Context_Access (Context);
+      Dir     : constant String := Directory_Information (File);
+      Success : Boolean;
+
+   begin
+      Push_State (Get_Kernel (File), Busy);
+      Trace (Me, "deleting " & Directory_Information (File) &
+             File_Information (File));
+
+      if Has_File_Information (File) then
+         Delete_File (Dir & File_Information (File), Success);
+      else
+         begin
+            Remove_Dir (Dir, True);
+         exception
+            when Directory_Error =>
+               Console.Insert
+                 (Get_Kernel (Context),
+                  (-"Cannot remove directory: ") & Dir,
+                  Mode => Error);
+         end;
+      end if;
+
+      --  ??? Need also to update project/file views
+      Pop_State (Get_Kernel (File));
+
+   exception
+      when E : others =>
+         Trace (Me, "Unexpected exception: " & Exception_Information (E));
+         Pop_State (Get_Kernel (File));
+   end On_Delete;
+
+   --------------------
+   -- VFS_Contextual --
+   --------------------
+
+   procedure VFS_Contextual
+     (Object  : access GObject_Record'Class;
+      Context : access Selection_Context'Class;
+      Menu    : access Gtk.Menu.Gtk_Menu_Record'Class)
+   is
+      pragma Unreferenced (Object);
+
+      File    : File_Selection_Context_Access;
+      Submenu : Gtk_Menu;
+      Mitem   : Gtk_Menu_Item;
+
+   begin
+      if Context.all in File_Selection_Context'Class then
+         File := File_Selection_Context_Access (Context);
+
+         if Has_Directory_Information (File) then
+            Gtk_New (Mitem, Label => -"File System");
+            Gtk_New (Submenu);
+            Set_Submenu (Mitem, Gtk_Widget (Submenu));
+            Append (Menu, Mitem);
+
+            if Has_File_Information (File) then
+               Gtk_New (Mitem, -"Delete file");
+            else
+               Gtk_New (Mitem, -"Delete directory recursively");
+            end if;
+
+            Append (Submenu, Mitem);
+            Context_Callback.Connect
+              (Mitem, "activate",
+               Context_Callback.To_Marshaller (On_Delete'Access),
+               Selection_Context_Access (Context));
+         end if;
+      end if;
+   end VFS_Contextual;
+
+   ---------------------
+   -- Register_Module --
+   ---------------------
+
+   procedure Register_Module
+     (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class) is
+   begin
+      VFS_Module_Id := new Module_ID_Record;
+
+      Register_Module
+        (Module                  => VFS_Module_Id,
+         Kernel                  => Kernel,
+         Module_Name             => VFS_Module_Name,
+         Priority                => Default_Priority,
+         Contextual_Menu_Handler => VFS_Contextual'Access);
+
+      Register_Command
+        (Kernel,
+         Command => "pwd",
+         Help    => -"Print name of current/working directory.",
+         Handler => VFS_Command_Handler'Access);
+      Register_Command
+        (Kernel,
+         Command => "cd",
+         Help    => -"Usage:" & ASCII.LF
+         & "  cd dir" & ASCII.LF
+         & (-"Change the current directory to dir."),
+         Handler => VFS_Command_Handler'Access);
+      Register_Command
+        (Kernel,
+         Command => "delete",
+         Help    => -"Usage:" & ASCII.LF
+         & "  delete name" & ASCII.LF
+         & (-"Delete file/directory name from the file system."),
+         Handler => VFS_Command_Handler'Access);
+      Register_Command
+        (Kernel,
+         Command => "dir",
+         Help    => -"Usage:" & ASCII.LF
+         & "  dir [pattern]" & ASCII.LF
+         & (-"list files following pattern (all files by default)."),
+         Handler => VFS_Command_Handler'Access);
+      Register_Command
+        (Kernel,
+         Command => "ls",
+         Help    => -"Usage:" & ASCII.LF
+         & "  ls [pattern]" & ASCII.LF
+         & (-"list files following pattern (all files by default)."),
+         Handler => VFS_Command_Handler'Access);
+   end Register_Module;
+
+end VFS_Module;
