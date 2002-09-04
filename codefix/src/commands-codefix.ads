2ba096d80434b15fed4c4e1349418e1b5b39bc89
Quentin Ochem <ochem@adacore.com>
2002-09-04 09:58:12 +0000

    Add file header.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@78560 936e1b1b-40f2-da11-902a-00137254ae57

--- codefix/src/commands-codefix.ads
+++ codefix/src/commands-codefix.ads
@@ -1,3 +1,23 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2002                         --
+--                            ACT-Europe                             --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
 with Glide_Kernel;           use Glide_Kernel;
 
 with Codefix.Errors_Manager; use Codefix.Errors_Manager;
@@ -6,7 +26,7 @@ with Codefix.Text_Manager;   use Codefix.Text_Manager;
 package Commands.Codefix is
 
    type Codefix_Command is new Root_Command with record
-      Error : Error_Id;
+      Error        : Error_Id;
       Current_Text : Ptr_Text_Navigator;
       Corrector    : Ptr_Correction_Manager;
       Kernel       : Kernel_Handle;
