45c7b34ff6287889acbbea5f76d54145ede1d6c0
Arnaud Charlet <charlet@adacore.com>
2002-09-12 13:56:44 +0000

    Reformatting, style fixes, comments updates. (Chars_Before): Remove, unused.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@78869 936e1b1b-40f2-da11-902a-00137254ae57

--- docgen/src/docgen-html_output.ads
+++ docgen/src/docgen-html_output.ads
@@ -29,14 +29,14 @@
 --  part of the documentation.
 
 
-with Ada.Text_IO;           use Ada.Text_IO;
-with GNAT.OS_Lib;           use GNAT.OS_Lib;
+with Ada.Text_IO; use Ada.Text_IO;
+with GNAT.OS_Lib; use GNAT.OS_Lib;
 
 package Docgen.Html_Output is
 
    procedure Doc_HTML_Create
-     (File        : in Ada.Text_IO.File_Type;
-      Info        : in out Doc_Info);
+     (File : Ada.Text_IO.File_Type;
+      Info : in out Doc_Info);
    --  This procedure is called every time the HTML files are concerned.
    --  What happens with the given information (which of the procedures
    --  below will be called) depands on the contents and the kind of the
@@ -45,116 +45,109 @@ package Docgen.Html_Output is
 private
 
    procedure Doc_HTML_Open
-     (File   : in Ada.Text_IO.File_Type;
+     (File   : Ada.Text_IO.File_Type;
       Info   : Doc_Info);
-   --  called each time a new file has been created
+   --  Called each time a new file has been created
 
    procedure Doc_HTML_Close
-     (File   : in Ada.Text_IO.File_Type;
+     (File   : Ada.Text_IO.File_Type;
       Info   : Doc_Info);
-   --  called each time the file should be closed
+   --  Called each time the file should be closed
 
    procedure Doc_HTML_Subtitle
-     (File   : in Ada.Text_IO.File_Type;
+     (File   : Ada.Text_IO.File_Type;
       Info   : Doc_Info);
-   --  add a subtitle for the entity type to the documentation
+   --  Add a subtitle for the entity type to the documentation
 
    procedure Doc_HTML_Subprogram
-     (File   : in Ada.Text_IO.File_Type;
+     (File   : Ada.Text_IO.File_Type;
       Info   : Doc_Info);
-   --  add a subprogram to the documentation
+   --  Add a subprogram to the documentation
 
    procedure Doc_HTML_Pack_Desc
-     (File    : in Ada.Text_IO.File_Type;
-      Info    : Doc_Info);
-   --  add the package description to the documentation
+     (File   : Ada.Text_IO.File_Type;
+      Info   : Doc_Info);
+   --  Add the package description to the documentation
 
    procedure Doc_HTML_Package
-     (File   : in Ada.Text_IO.File_Type;
+     (File   : Ada.Text_IO.File_Type;
       Info   : Doc_Info);
-   --  add the renamed and instantiated package to the documentation
+   --  Add the renamed and instantiated package to the documentation
 
    procedure Doc_HTML_With
-     (File   : in Ada.Text_IO.File_Type;
+     (File   : Ada.Text_IO.File_Type;
       Info   : Doc_Info);
-   --  add the dependencies to the documentation
+   --  Add the dependencies to the documentation
 
    procedure Doc_HTML_Var
-     (File    : in Ada.Text_IO.File_Type;
+     (File    : Ada.Text_IO.File_Type;
       Info    : Doc_Info);
-   --  add a constant or named number to the documentation
+   --  Add a constant or named number to the documentation
 
    procedure Doc_HTML_Exception
      (File   : in Ada.Text_IO.File_Type;
       Info   : Doc_Info);
-   --  add an exception to the documentation
+   --  Add an exception to the documentation
 
    procedure Doc_HTML_Type
      (File   : in Ada.Text_IO.File_Type;
       Info   : Doc_Info);
-   --  add a type to the documentation
+   --  Add a type to the documentation
 
    function Format_HTML
-     (Entity_List       : Type_Entity_List.List;
-      Text              : String;
-      File_Name         : String;
-      Entity_Name       : String;
-      Entity_Line       : Natural;
-      Is_Body           : Boolean;
-      Do_Checks         : Boolean) return GNAT.OS_Lib.String_Access;
-   --  returns the formatted Text as HTML code
+     (Entity_List : Type_Entity_List.List;
+      Text        : String;
+      File_Name   : String;
+      Entity_Name : String;
+      Entity_Line : Natural;
+      Is_Body     : Boolean;
+      Do_Checks   : Boolean) return GNAT.OS_Lib.String_Access;
+   --  Return the formatted Text as HTML code
 
    procedure Doc_HTML_Header
-     (File   : in Ada.Text_IO.File_Type;
+     (File   : Ada.Text_IO.File_Type;
       Info   : Doc_Info);
-   --  add the header of a package to the documentation
+   --  Add the header of a package to the documentation
 
    procedure Doc_HTML_Footer
-     (File   : in Ada.Text_IO.File_Type;
+     (File   : Ada.Text_IO.File_Type;
       Info   : Doc_Info);
-   --  add the footer of a package to the documentation
+   --  Add the footer of a package to the documentation
 
    procedure Doc_HTML_Unit_Index_Header
-     (File   : in Ada.Text_IO.File_Type;
+     (File   : Ada.Text_IO.File_Type;
       Info   : Doc_Info);
-   --  create the header of the index of all packages
+   --  Create the header of the index of all packages
    --  and also create the whole index.htm for the frames
 
    procedure Doc_HTML_Sub_Index_Header
-     (File   : in Ada.Text_IO.File_Type;
+     (File   : Ada.Text_IO.File_Type;
       Info   : Doc_Info);
-   --  create the header of the index of all subprograms
+   --  Create the header of the index of all subprograms
 
    procedure Doc_HTML_Type_Index_Header
-     (File   : in Ada.Text_IO.File_Type;
+     (File   : Ada.Text_IO.File_Type;
       Info   : Doc_Info);
-   --  create the header of the index of all types
+   --  Create the header of the index of all types
 
    procedure Doc_HTML_Index_Item
-     (File   : in Ada.Text_IO.File_Type;
+     (File   : Ada.Text_IO.File_Type;
       Info   : Doc_Info);
-   --  add an item to an index, used for all 3 index types
+   --  Add an item to an index, used for all 3 index types
 
    procedure Doc_HTML_Index_End
-   (File   : in Ada.Text_IO.File_Type;
-    Info   : Doc_Info);
-   --  add the footer to the index, used for all 3 indes files
+     (File   : Ada.Text_IO.File_Type;
+      Info   : Doc_Info);
+   --  Add the footer to the index, used for all 3 indes files
 
    procedure Doc_HTML_Body
-   (File   : in Ada.Text_IO.File_Type;
-    Info   : in out Doc_Info);
-   --  format the body by calling Format_HTML for the whole body file
+     (File   : Ada.Text_IO.File_Type;
+      Info   : in out Doc_Info);
+   --  Format the body by calling Format_HTML for the whole body file
    --  and write it to the doc file
 
-   function Chars_Before    --  still used ???
-     (Line    : String;
-      Line_Nr : Natural) return Natural;
-   --  returns the sum of the number of chars in the
-   --  lines until Line_Nr-1 in the Line string
-
-   function Get_Html_File_Name
-   (File : String) return String;
-   --  creates a .htm file name from the full path of the source file
+   function Get_Html_File_Name (File : String) return String;
+   --  Create a .htm file name from the full path of the source file
    --  for ex.: from util/src/docgen.adb the name docgen_adb.htm is created
 
 end Docgen.Html_Output;
