5853ef8572ad52e79607b75df3ef13dfe9a0f92d
Arnaud Charlet <charlet@adacore.com>
2002-10-01 16:08:55 +0000

    Remove Tmp_Dir preference.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@79216 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/glide_kernel-preferences.ads
+++ kernel/src/glide_kernel-preferences.ads
@@ -102,7 +102,6 @@ package Glide_Kernel.Preferences is
    Animated_Image        : Param_Spec_String;
    Splash_Screen         : Param_Spec_Boolean;
    Start_Maximized       : Param_Spec_Boolean;
-   Tmp_Dir               : Param_Spec_String;
    Toolbar_Show_Text     : Param_Spec_Boolean;
    Save_Desktop_On_Exit  : Param_Spec_Boolean;
    Display_Welcome       : Param_Spec_Boolean;
