4a42b6086cc8744e400cf99ef9bb60ed8ae1ba58
Nicolas Setton <setton@adacore.com>
2002-10-04 13:54:47 +0000

    (Undo_Redo_Information): Move this type from Src_Editor_Box.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@79319 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/src_editor_module.ads
+++ src_editor/src/src_editor_module.ads
@@ -1,10 +1,10 @@
 -----------------------------------------------------------------------
---                          G L I D E  I I                           --
+--                               G P S                               --
 --                                                                   --
 --                     Copyright (C) 2001-2002                       --
 --                            ACT-Europe                             --
 --                                                                   --
--- GLIDE is free software; you can redistribute it and/or modify  it --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
 -- the Free Software Foundation; either version 2 of the License, or --
 -- (at your option) any later version.                               --
@@ -13,10 +13,14 @@
 -- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
 -- General Public License for more details. You should have received --
--- a copy of the GNU General Public License along with this library; --
+-- a copy of the GNU General Public License along with this program; --
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
+
+with Glib.Object;
+with Gtk.Button;
+with Gtk.Menu_Item;
 
 with Src_Editor_Box;
 with Glide_Kernel;
@@ -26,6 +30,18 @@ package Src_Editor_Module is
 
    Src_Editor_Module_Id : Glide_Kernel.Module_ID;
    Src_Editor_Module_Name : constant String := "Source_Editor";
+
+   type Undo_Redo_Information is record
+      Undo_Button          : Gtk.Button.Gtk_Button;
+      Redo_Button          : Gtk.Button.Gtk_Button;
+
+      Undo_Menu_Item       : Gtk.Menu_Item.Gtk_Menu_Item;
+      Redo_Menu_Item       : Gtk.Menu_Item.Gtk_Menu_Item;
+   end record;
+
+   Undo_Redo_Id : constant String := "Source_Editor_Undo_Redo_Information";
+
+   package Undo_Redo_Data is new Glib.Object.User_Data (Undo_Redo_Information);
 
    procedure Register_Module
      (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class);
