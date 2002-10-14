ae1f7726eb58288d31007a8aeddfaabcccb5459c
Arnaud Charlet <charlet@adacore.com>
2002-10-14 22:14:04 +0000

    Regenerated from gvd.glade:1.119 Use stock buttons when possible, friendlier. Part of BA14-010.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@79430 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/breakpoints_pkg.adb
+++ gvd/gvd/breakpoints_pkg.adb
@@ -250,7 +250,7 @@ begin
       Widget_Callback.To_Marshaller (On_Add_Location_Clicked'Access), Breakpoints);
    Add (Breakpoints.Vbuttonbox2, Breakpoints.Add_Location);
 
-   Gtk_New (Breakpoints.Update_Location, -"Update");
+   Gtk_New_From_Stock (Breakpoints.Update_Location, Stock_Apply);
    Set_Relief (Breakpoints.Update_Location, Relief_Normal);
    Set_Sensitive (Breakpoints.Update_Location, False);
    Set_Flags (Breakpoints.Update_Location, Can_Default);
@@ -336,7 +336,7 @@ begin
       Widget_Callback.To_Marshaller (On_Add_Watchpoint_Clicked'Access), Breakpoints);
    Add (Breakpoints.Vbuttonbox3, Breakpoints.Add_Watchpoint);
 
-   Gtk_New (Breakpoints.Update_Watchpoint, -"Update");
+   Gtk_New_From_Stock (Breakpoints.Update_Watchpoint, Stock_Apply);
    Set_Relief (Breakpoints.Update_Watchpoint, Relief_Normal);
    Set_Sensitive (Breakpoints.Update_Watchpoint, False);
    Set_Flags (Breakpoints.Update_Watchpoint, Can_Default);
@@ -438,7 +438,7 @@ begin
       Widget_Callback.To_Marshaller (On_Add_Exception_Clicked'Access), Breakpoints);
    Add (Breakpoints.Vbuttonbox4, Breakpoints.Add_Exception);
 
-   Gtk_New (Breakpoints.Update_Exception, -"Update");
+   Gtk_New_From_Stock (Breakpoints.Update_Exception, Stock_Apply);
    Set_Relief (Breakpoints.Update_Exception, Relief_Normal);
    Set_Sensitive (Breakpoints.Update_Exception, False);
    Set_Flags (Breakpoints.Update_Exception, Can_Default);
