2cf9e72a8c49b06febf6fc3f238ab49ba3895ace
Nicolas Setton <setton@adacore.com>
2003-01-15 14:19:02 +0000

    Move source editor module information from the body to the spec private part.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@80771 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/src_editor_module.ads
+++ src_editor/src/src_editor_module.ads
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                     Copyright (C) 2001-2002                       --
+--                     Copyright (C) 2001-2003                       --
 --                            ACT-Europe                             --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
@@ -19,12 +19,26 @@
 -----------------------------------------------------------------------
 
 with Glib.Object;
+with Glib;          use Glib;
+with Gdk.GC;
 with Gtk.Button;
-with Gtk.Menu_Item;
+with Gtk.Box;       use Gtk.Box;
+with Gtk.Dialog;    use Gtk.Dialog;
+with Gtk.Handlers;  use Gtk.Handlers;
+with Gtk.Menu_Item; use Gtk.Menu_Item;
+with Gtk.Text_Mark; use Gtk.Text_Mark;
+
+with Gtkada.MDI;                use Gtkada.MDI;
+with Gtkada.Entry_Completion;   use Gtkada.Entry_Completion;
 
 with Src_Editor_Box;
-with Glide_Kernel;
-with Gtkada.MDI;
+
+with Glide_Kernel;      use Glide_Kernel;
+with Basic_Types;       use Basic_Types;
+with String_List_Utils; use String_List_Utils;
+
+with Ada.Unchecked_Deallocation;
+with Generic_List;
 
 package Src_Editor_Module is
 
@@ -70,5 +84,87 @@ package Src_Editor_Module is
       Editor : access Src_Editor_Box.Source_Editor_Box_Record'Class)
       return Gtkada.MDI.MDI_Child;
    --  Return the MDI child containing Editor
+
+private
+
+   type Source_Box_Record is new Gtk_Hbox_Record with record
+      Editor : Src_Editor_Box.Source_Editor_Box;
+   end record;
+   type Source_Box is access all Source_Box_Record'Class;
+
+   -----------
+   -- Marks --
+   -----------
+
+   type Mark_Identifier_Record is record
+      Id     : Natural;
+      Child  : MDI_Child;
+      File   : Basic_Types.String_Access;
+      Line   : Natural;
+      Column : Natural;
+      Mark   : Gtk_Text_Mark;
+      Length : Natural;
+   end record;
+
+   procedure Free (X : in out Mark_Identifier_Record);
+   --  Free memory associated to X.
+
+   package Mark_Identifier_List is new Generic_List (Mark_Identifier_Record);
+
+   -----------------------------
+   -- Highlighting categories --
+   -----------------------------
+
+   type Highlighting_Category_Record (L : Natural) is record
+      Id : String (1 .. L);
+      GC : Gdk.GC.Gdk_GC;
+   end record;
+
+   type Highlighting_Category is access Highlighting_Category_Record;
+
+   procedure Unchecked_Free is new Ada.Unchecked_Deallocation
+     (Highlighting_Category_Record, Highlighting_Category);
+
+   type Highlighting_Category_Array is array (Natural range <>) of
+     Highlighting_Category;
+
+   type Highlighting_Category_Array_Access is
+     access Highlighting_Category_Array;
+
+   procedure Unchecked_Free is new Ada.Unchecked_Deallocation
+      (Highlighting_Category_Array, Highlighting_Category_Array_Access);
+
+   --------------------------
+   -- Source_Editor_Module --
+   --------------------------
+
+   No_Handler : constant Handler_Id := (Null_Signal_Id, null);
+
+   type Source_Editor_Module_Record is new Module_ID_Record with record
+      Kernel                   : Kernel_Handle;
+
+      Recent_Menu_Item         : Gtk_Menu_Item;
+      Source_Lines_Revealed_Id : Handler_Id := No_Handler;
+      File_Edited_Id           : Handler_Id := No_Handler;
+      File_Closed_Id           : Handler_Id := No_Handler;
+      Display_Line_Numbers     : Boolean    := False;
+
+      Stored_Marks             : Mark_Identifier_List.List;
+      Next_Mark_Id             : Natural := 0;
+
+      Open_File_Entry          : Gtkada_Entry;
+      Open_File_Dialog         : Gtk_Dialog;
+
+      Unopened_Files           : String_List_Utils.String_List.List;
+      --  Contains a list of files for which marks have been created but
+      --  that are not open.
+
+      Categories               : Highlighting_Category_Array_Access;
+      --  Contains a list of registered categories.
+   end record;
+   type Source_Editor_Module is access all Source_Editor_Module_Record'Class;
+
+   procedure Destroy (Id : in out Source_Editor_Module_Record);
+   --  Free the memory used by the module.
 
 end Src_Editor_Module;
