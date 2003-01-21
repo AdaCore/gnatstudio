c793ae51595b8587802eb07b6f0273475cac913c
Nicolas Setton <setton@adacore.com>
2003-01-21 14:43:57 +0000

    Initial revision for this file.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@80859 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ src_editor/src/src_editor_buffer-blocks.adb
@@ -0,0 +1,80 @@
+-----------------------------------------------------------------------
+--                              G P S                                --
+--                                                                   --
+--                     Copyright (C) 2001-2003                       --
+--                            ACT-Europe                             --
+--                                                                   --
+-- GPS is free  software; you can  redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Language; use Language;
+
+package body Src_Editor_Buffer.Blocks is
+
+   --------------------
+   -- Compute_Blocks --
+   --------------------
+
+   procedure Compute_Blocks (Buffer : access Source_Buffer_Record'Class) is
+      Constructs : Construct_List;
+      Current    : Construct_Access;
+      Line_Start : Integer;
+      Line_End   : Integer;
+      Column     : Integer;
+   begin
+      if Buffer.Lang = null or else not Buffer.Parse_Blocks then
+         return;
+      end if;
+
+      Parse_Constructs (Buffer.Lang, Get_Slice (Buffer, 0, 0), Constructs);
+
+      for Line in Buffer.Line_Data'Range loop
+         Buffer.Line_Data (Line).Block := New_Block;
+      end loop;
+
+      Current := Constructs.First;
+
+      while Current /= null loop
+         if Current.Category in Construct_Category
+           or else Current.Category in Enclosing_Entity_Category
+         then
+            Line_Start := Current.Sloc_Start.Line;
+            Line_End   := Current.Sloc_End.Line;
+            Column     := Integer'Min
+              (Current.Sloc_Start.Column, Current.Sloc_End.Column);
+
+            Buffer.Line_Data (Line_Start).Block :=
+              (Indentation_Level =>
+                 Buffer.Line_Data (Line_Start).Block.Indentation_Level + 1,
+               Offset            => Column,
+               Other_Line        => Line_End,
+               Block_Type        => Current.Category,
+               GC                => null);
+
+            Buffer.Line_Data (Line_End).Block :=
+              (Indentation_Level =>
+                 Buffer.Line_Data (Line_End).Block.Indentation_Level - 1,
+               Offset            => Column,
+               Other_Line        => Line_Start,
+               Block_Type        => Current.Category,
+               GC                => null);
+         end if;
+
+         Current := Current.Next;
+      end loop;
+
+      Free (Constructs);
+   end Compute_Blocks;
+
+end Src_Editor_Buffer.Blocks;
