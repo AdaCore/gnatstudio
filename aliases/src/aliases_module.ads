0bea1ff9c31b269b91bed5dceae4a8463ebaa22a
Emmanuel Briot <briot@adacore.com>
2003-01-22 13:51:46 +0000

    (Register_Special_Alias_Entity): New subprogram
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@80905 936e1b1b-40f2-da11-902a-00137254ae57

--- aliases/src/aliases_module.ads
+++ aliases/src/aliases_module.ads
@@ -26,4 +26,33 @@ package Aliases_Module is
      (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class);
    --  Register the module into the list
 
+   Invalid_Expansion : constant String := "!@#$%";
+
+   type Alias_Expansion_Function is access
+     function (Data    : Glide_Kernel.Event_Data;
+               Special : Character) return String;
+   --  Function called to test alias expansion.
+   --  It is passed the current context in which the expansion takes module,
+   --  and modules are encouraged to do nothing if the context is not thr right
+   --  one. For instance, the "current file" special entity only makes sense in
+   --  the source editor, and the src_editor module should do nothing if it is
+   --  called but Get_Widget (Data) is not a source editor.
+   --  Special is the special entity seen in the alias expansion, without the
+   --  leading '%'.
+   --  This function should return the replacement text for Special, and should
+   --  return the Invalid_Expansion if it couldn't expand Special because the
+   --  context is incorrect.
+
+   procedure Register_Special_Alias_Entity
+     (Kernel      : access Glide_Kernel.Kernel_Handle_Record'Class;
+      Description : String;
+      Entity      : Character;
+      Func        : Alias_Expansion_Function);
+   --  Register a new special entity that can be used in aliases expansion.
+   --  Special is the text for that special entity, without the leading '%'.
+   --  Func is the function called to get the replacement text for Special.
+   --  Description is used in the contextual menu in the aliases editor to
+   --  describe this special character.
+
 end Aliases_Module;
+
