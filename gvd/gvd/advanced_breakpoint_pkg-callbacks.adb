50ade818b4432a406d37c3dde4480bbad4dfd220
Arnaud Charlet <charlet@adacore.com>
2003-04-11 12:26:27 +0000

    Add exception handlers, traces. Implement new advanced breakpoint interface (separated from the  breakpoint editor).
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@82204 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/advanced_breakpoint_pkg-callbacks.adb
+++ gvd/gvd/advanced_breakpoint_pkg-callbacks.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                   GVD - The GNU Visual Debugger                   --
 --                                                                   --
---                      Copyright (C) 2000-2002                      --
+--                      Copyright (C) 2000-2003                      --
 --                              ACT-Europe                           --
 --                                                                   --
 -- GVD is free  software;  you can redistribute it and/or modify  it --
@@ -18,7 +18,13 @@
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
+with Traces; use Traces;
+with Ada.Exceptions; use Ada.Exceptions;
+with Gtk.Main; use Gtk.Main;
+
 package body Advanced_Breakpoint_Pkg.Callbacks is
+
+   Me : constant Debug_Handle := Create ("Advanced_Breakpoint_Pkg.Callbacks");
 
    -----------------------------
    -- On_Start_Record_Clicked --
@@ -30,6 +36,10 @@ package body Advanced_Breakpoint_Pkg.Callbacks is
       pragma Unreferenced (Object);
    begin
       null;
+
+   exception
+      when E : others =>
+         Trace (Me, "Unexpected exception: " & Exception_Information (E));
    end On_Start_Record_Clicked;
 
    ----------------------------
@@ -42,6 +52,48 @@ package body Advanced_Breakpoint_Pkg.Callbacks is
       pragma Unreferenced (Object);
    begin
       null;
+
+   exception
+      when E : others =>
+         Trace (Me, "Unexpected exception: " & Exception_Information (E));
    end On_Stop_Record_Clicked;
+
+   ----------------------
+   -- On_Apply_Clicked --
+   ----------------------
+
+   procedure On_Apply_Clicked
+     (Object : access Gtk_Widget_Record'Class)
+   is
+      Editor : constant Advanced_Breakpoint_Access :=
+        Advanced_Breakpoint_Access (Object);
+   begin
+      Editor.Response_Action := Gtk_Response_Apply;
+      Hide (Object);
+      Main_Quit;
+
+   exception
+      when E : others =>
+         Trace (Me, "Unexpected exception: " & Exception_Information (E));
+   end On_Apply_Clicked;
+
+   ----------------------
+   -- On_Close_Clicked --
+   ----------------------
+
+   procedure On_Close_Clicked
+     (Object : access Gtk_Widget_Record'Class)
+   is
+      Editor : constant Advanced_Breakpoint_Access :=
+        Advanced_Breakpoint_Access (Object);
+   begin
+      Editor.Response_Action := Gtk_Response_Close;
+      Hide (Object);
+      Main_Quit;
+
+   exception
+      when E : others =>
+         Trace (Me, "Unexpected exception: " & Exception_Information (E));
+   end On_Close_Clicked;
 
 end Advanced_Breakpoint_Pkg.Callbacks;
