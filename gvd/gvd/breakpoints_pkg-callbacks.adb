d8065e08ad2b616a8d14e7f33872d9e9703139b6
Arnaud Charlet <charlet@adacore.com>
2003-04-11 12:28:31 +0000

    Regenerated from gvd.glade:1.121 Add support for separate breakpoint and advanced bp editor.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@82206 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/breakpoints_pkg-callbacks.adb
+++ gvd/gvd/breakpoints_pkg-callbacks.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                   GVD - The GNU Visual Debugger                   --
 --                                                                   --
---                      Copyright (C) 2000-2002                      --
+--                      Copyright (C) 2000-2003                      --
 --                              ACT-Europe                           --
 --                                                                   --
 -- GVD is free  software;  you can redistribute it and/or modify  it --
@@ -21,11 +21,9 @@
 with Glib;               use Glib;
 with Glib.Object;        use Glib.Object;
 with GVD.Types;          use GVD.Types;
-with Basic_Types;        use Basic_Types;
 with Gtk.Arguments;      use Gtk.Arguments;
 with Debugger;           use Debugger;
 with Odd_Intl;           use Odd_Intl;
-with Gtk.Enums;          use Gtk.Enums;
 with Gtk.Combo;          use Gtk.Combo;
 with GUI_Utils;          use GUI_Utils;
 with GVD.Code_Editors;   use GVD.Code_Editors;
@@ -33,8 +31,6 @@ with GVD.Process;        use GVD.Process;
 with Gdk.Event;          use Gdk.Event;
 with Gdk.Types.Keysyms;  use Gdk.Types.Keysyms;
 with Breakpoints_Editor; use Breakpoints_Editor;
-
-with Advanced_Breakpoint_Pkg; use Advanced_Breakpoint_Pkg;
 with Ada.Exceptions;          use Ada.Exceptions;
 with Traces;                  use Traces;
 
@@ -84,42 +80,6 @@ package body Breakpoints_Pkg.Callbacks is
          Trace (Me, "Unexpected exception: " & Exception_Information (E));
          return False;
    end On_Breakpoints_Key_Press_Event;
-
-   -------------------------
-   -- On_Breakpoints_Show --
-   -------------------------
-
-   procedure On_Breakpoints_Show
-     (Object : access Gtk_Widget_Record'Class)
-   is
-      Editor    : constant Breakpoint_Editor_Access :=
-        Breakpoint_Editor_Access (Object);
-
-   begin
-      if Editor.Advanced_Breakpoints_Location /= null then
-         declare
-            Debugger     : Debugger_Access;
-            WTX_Version  : Natural;
-         begin
-            Debugger := Editor.Process.Debugger;
-            Info_WTX (Debugger, WTX_Version);
-
-            if WTX_Version /= 3 then
-               Hide (Editor.Advanced_Breakpoints_Location.Scope_Box);
-               Hide (Editor.Advanced_Breakpoints_Watchpoints.Scope_Box);
-               Hide (Editor.Advanced_Breakpoints_Exceptions.Scope_Box);
-            else
-               Show (Editor.Advanced_Breakpoints_Location.Scope_Box);
-               Show (Editor.Advanced_Breakpoints_Watchpoints.Scope_Box);
-               Show (Editor.Advanced_Breakpoints_Exceptions.Scope_Box);
-            end if;
-         end;
-      end if;
-
-   exception
-      when E : others =>
-         Trace (Me, "Unexpected exception: " & Exception_Information (E));
-   end On_Breakpoints_Show;
 
    ----------------------------------
    -- On_Location_Selected_Toggled --
@@ -210,97 +170,13 @@ package body Breakpoints_Pkg.Callbacks is
    procedure On_Add_Location_Clicked
      (Object : access Gtk_Widget_Record'Class)
    is
-      B : Breakpoint_Identifier;
-      pragma Unreferenced (B);
    begin
-      B := Set_Location_Breakpoint (Breakpoint_Editor_Access (Object));
+      Set_Location_Breakpoint (Breakpoint_Editor_Access (Object));
 
    exception
       when E : others =>
          Trace (Me, "Unexpected exception: " & Exception_Information (E));
    end On_Add_Location_Clicked;
-
-   --------------------------------
-   -- On_Update_Location_Clicked --
-   --------------------------------
-
-   procedure On_Update_Location_Clicked
-     (Object : access Gtk_Widget_Record'Class)
-   is
-      Editor    : constant Breakpoint_Editor_Access :=
-        Breakpoint_Editor_Access (Object);
-      Selection : constant Integer := Get_Selection_Index (Editor);
-      Num       : Breakpoint_Identifier;
-
-      WTX_Version : Natural;
-
-   begin
-      --  Only update the breakpoint if its type matches the current page.
-      if Selection /= -1
-        and then Editor.Process.Breakpoints (Selection).Except = null
-      then
-         Num := Set_Location_Breakpoint (Editor, Selection);
-
-         --  Update scope and action for the selected breakpoint
-         Info_WTX (Editor.Process.Debugger, WTX_Version);
-
-         if WTX_Version = 3 then
-            declare
-               Scope_Action : constant Advanced_Breakpoint_Access :=
-                 Editor.Advanced_Breakpoints_Location;
-               Scope_Value  : GVD.Types.Scope_Type;
-               Action_Value : GVD.Types.Action_Type;
-            begin
-               if Get_Active (Scope_Action.Scope_Task) then
-                  Scope_Value := Current_Task;
-               elsif Get_Active (Scope_Action.Scope_Pd) then
-                  Scope_Value := Tasks_In_PD;
-               elsif Get_Active (Scope_Action.Scope_Any) then
-                  Scope_Value := Any_Task;
-               end if;
-
-               if Get_Active (Scope_Action.Action_Task) then
-                  Action_Value := Current_Task;
-               elsif Get_Active (Scope_Action.Action_Pd) then
-                  Action_Value := Tasks_In_PD;
-               elsif Get_Active (Scope_Action.Action_All) then
-                  Action_Value := All_Tasks;
-               end if;
-
-               Set_Scope_Action (Editor.Process.Debugger, Scope_Value,
-                                 Action_Value, Num);
-            end;
-         end if;
-
-         for B in Editor.Process.Breakpoints'Range loop
-            if Editor.Process.Breakpoints (B).Num = Num then
-               Select_Row (Editor.Breakpoint_List, Gint (B) - 1, -1);
-               exit;
-            end if;
-         end loop;
-      end if;
-
-   exception
-      when E : others =>
-         Trace (Me, "Unexpected exception: " & Exception_Information (E));
-   end On_Update_Location_Clicked;
-
-   ----------------------------------
-   -- On_Advanced_Location_Clicked --
-   ----------------------------------
-
-   procedure On_Advanced_Location_Clicked
-     (Object : access Gtk_Widget_Record'Class)
-   is
-      Editor : constant Breakpoint_Editor_Access :=
-        Breakpoint_Editor_Access (Object);
-   begin
-      Toggle_Advanced_Dialog (Editor);
-
-   exception
-      when E : others =>
-         Trace (Me, "Unexpected exception: " & Exception_Information (E));
-   end On_Advanced_Location_Clicked;
 
    -------------------------------
    -- On_Add_Watchpoint_Clicked --
@@ -317,39 +193,6 @@ package body Breakpoints_Pkg.Callbacks is
       when E : others =>
          Trace (Me, "Unexpected exception: " & Exception_Information (E));
    end On_Add_Watchpoint_Clicked;
-
-   ----------------------------------
-   -- On_Update_Watchpoint_Clicked --
-   ----------------------------------
-
-   procedure On_Update_Watchpoint_Clicked
-     (Object : access Gtk_Widget_Record'Class)
-   is
-      pragma Unreferenced (Object);
-   begin
-      null;
-
-   exception
-      when E : others =>
-         Trace (Me, "Unexpected exception: " & Exception_Information (E));
-   end On_Update_Watchpoint_Clicked;
-
-   ------------------------------------
-   -- On_Advanced_Watchpoint_Clicked --
-   ------------------------------------
-
-   procedure On_Advanced_Watchpoint_Clicked
-     (Object : access Gtk_Widget_Record'Class)
-   is
-      Editor : constant Breakpoint_Editor_Access :=
-        Breakpoint_Editor_Access (Object);
-   begin
-      Toggle_Advanced_Dialog (Editor);
-
-   exception
-      when E : others =>
-         Trace (Me, "Unexpected exception: " & Exception_Information (E));
-   end On_Advanced_Watchpoint_Clicked;
 
    ------------------------------------
    -- On_Load_Exception_List_Clicked --
@@ -399,67 +242,16 @@ package body Breakpoints_Pkg.Callbacks is
    procedure On_Add_Exception_Clicked
      (Object : access Gtk_Widget_Record'Class)
    is
-      B      : Breakpoint_Identifier;
-      pragma Unreferenced (B);
-
       Editor : constant Breakpoint_Editor_Access :=
         Breakpoint_Editor_Access (Object);
 
    begin
-      B := Set_Exception_Breakpoint (Editor);
+      Set_Exception_Breakpoint (Editor);
 
    exception
       when E : others =>
          Trace (Me, "Unexpected exception: " & Exception_Information (E));
    end On_Add_Exception_Clicked;
-
-   ---------------------------------
-   -- On_Update_Exception_Clicked --
-   ---------------------------------
-
-   procedure On_Update_Exception_Clicked
-     (Object : access Gtk_Widget_Record'Class)
-   is
-      Editor    : constant Breakpoint_Editor_Access :=
-        Breakpoint_Editor_Access (Object);
-      Selection : constant Integer := Get_Selection_Index (Editor);
-      Num : Breakpoint_Identifier;
-   begin
-      --  Only update the breakpoint if its type matches the current page.
-      if Selection /= -1
-        and then Editor.Process.Breakpoints (Selection).Except /= null
-      then
-         Num := Set_Exception_Breakpoint (Editor, Selection);
-
-         for B in Editor.Process.Breakpoints'Range loop
-            if Editor.Process.Breakpoints (B).Num = Num then
-               Select_Row (Editor.Breakpoint_List, Gint (B), -1);
-               exit;
-            end if;
-         end loop;
-      end if;
-
-   exception
-      when E : others =>
-         Trace (Me, "Unexpected exception: " & Exception_Information (E));
-   end On_Update_Exception_Clicked;
-
-   -----------------------------------
-   -- On_Advanced_Exception_Clicked --
-   -----------------------------------
-
-   procedure On_Advanced_Exception_Clicked
-     (Object : access Gtk_Widget_Record'Class)
-   is
-      Editor : constant Breakpoint_Editor_Access :=
-        Breakpoint_Editor_Access (Object);
-   begin
-      Toggle_Advanced_Dialog (Editor);
-
-   exception
-      when E : others =>
-         Trace (Me, "Unexpected exception: " & Exception_Information (E));
-   end On_Advanced_Exception_Clicked;
 
    -----------------------
    -- On_Remove_Clicked --
@@ -468,17 +260,15 @@ package body Breakpoints_Pkg.Callbacks is
    procedure On_Remove_Clicked
      (Object : access Gtk_Widget_Record'Class)
    is
-      use Gint_List;
       Editor    : constant Breakpoint_Editor_Access :=
         Breakpoint_Editor_Access (Object);
-      Selection : Gint;
+      Selection : constant Integer := Get_Selection_Index (Editor);
+
    begin
-      if Get_Selection (Editor.Breakpoint_List) /= Null_List then
-         Selection := Get_Data (Get_Selection (Editor.Breakpoint_List));
+      if Selection /= -1 then
          Remove_Breakpoint
            (Editor.Process.Debugger,
-            Breakpoint_Identifier'Value
-              (Get_Text (Editor.Breakpoint_List, Selection, 0)),
+            Editor.Process.Breakpoints (Selection).Num,
             Mode => GVD.Types.Visible);
       end if;
 
@@ -497,6 +287,7 @@ package body Breakpoints_Pkg.Callbacks is
       Editor    : constant Breakpoint_Editor_Access :=
         Breakpoint_Editor_Access (Object);
       Selection : constant Integer := Get_Selection_Index (Editor);
+
    begin
       if Selection /= -1 then
          Load_File
@@ -513,6 +304,27 @@ package body Breakpoints_Pkg.Callbacks is
          Trace (Me, "Unexpected exception: " & Exception_Information (E));
    end On_View_Clicked;
 
+   ----------------------------------
+   -- On_Advanced_Location_Clicked --
+   ----------------------------------
+
+   procedure On_Advanced_Location_Clicked
+     (Object : access Gtk_Widget_Record'Class)
+   is
+      Editor    : constant Breakpoint_Editor_Access :=
+        Breakpoint_Editor_Access (Object);
+      Selection : constant Integer := Get_Selection_Index (Editor);
+
+   begin
+      if Selection /= -1 then
+         Run_Advanced_Dialog (Editor, Selection);
+      end if;
+
+   exception
+      when E : others =>
+         Trace (Me, "Unexpected exception: " & Exception_Information (E));
+   end On_Advanced_Location_Clicked;
+
    ----------------------
    -- On_Ok_Bp_Clicked --
    ----------------------
@@ -520,49 +332,7 @@ package body Breakpoints_Pkg.Callbacks is
    procedure On_Ok_Bp_Clicked
      (Object : access Gtk_Widget_Record'Class)
    is
-      Editor       : constant Breakpoint_Editor_Access :=
-        Breakpoint_Editor_Access (Object);
-      Scope_Action : constant Advanced_Breakpoint_Access :=
-        Editor.Advanced_Breakpoints_Location;
-      Debugger     : Debugger_Access;
-      WTX_Version  : Natural;
-
    begin
-      Debugger := Editor.Process.Debugger;
-      Info_WTX (Debugger, WTX_Version);
-
-      --  If we are using AE and the user has activated the "Set as
-      --  default" checkbox for the scope and action values, send the
-      --  appropriate commands to the debugger
-
-      if WTX_Version = 3
-        and then Editor.Advanced_Breakpoints_Location /= null
-        and then Get_Active (Scope_Action.Set_Default)
-      then
-         declare
-            Scope_Value  : Scope_Type;
-            Action_Value : Action_Type;
-         begin
-            if Get_Active (Scope_Action.Scope_Task) then
-               Scope_Value := Current_Task;
-            elsif Get_Active (Scope_Action.Scope_Pd) then
-               Scope_Value := Tasks_In_PD;
-            elsif Get_Active (Scope_Action.Scope_Any) then
-               Scope_Value := Any_Task;
-            end if;
-
-            if Get_Active (Scope_Action.Action_Task) then
-               Action_Value := Current_Task;
-            elsif Get_Active (Scope_Action.Action_Pd) then
-               Action_Value := Tasks_In_PD;
-            elsif Get_Active (Scope_Action.Action_All) then
-               Action_Value := All_Tasks;
-            end if;
-
-            Set_Scope_Action (Debugger, Scope_Value, Action_Value);
-         end;
-      end if;
-
       Hide (Object);
 
    exception
