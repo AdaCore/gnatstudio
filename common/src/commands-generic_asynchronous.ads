869073aa137ce016824b2c12e88a727ec5436a12
Arnaud Charlet <charlet@adacore.com>
2003-04-11 10:59:10 +0000

    Add ??? comment for missing package description.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@82200 936e1b1b-40f2-da11-902a-00137254ae57

--- common/src/commands-generic_asynchronous.ads
+++ common/src/commands-generic_asynchronous.ads
@@ -18,6 +18,8 @@
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
+--  ??? Description of this package
+
 with Ada.Unchecked_Deallocation;
 
 generic
