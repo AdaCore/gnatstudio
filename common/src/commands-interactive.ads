70e4e998709b10655ecb2e13aeefc4efffcdd230
Emmanuel Briot <briot@adacore.com>
2003-04-16 16:49:24 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@82370 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ common/src/commands-interactive.ads
@@ -0,0 +1,44 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                     Copyright (C) 2003                            --
+--                            ACT-Europe                             --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+--  This package defines a new kind of command which can be called
+--  interactively by the user (bound to keys, menus or toolbars for
+--  instance)
+
+with Gdk.Event;
+
+package Commands.Interactive is
+
+   type Interactive_Command is abstract new Root_Command with null record;
+   type Interactive_Command_Access is access all Interactive_Command'Class;
+
+   function Execute
+     (Command : access Interactive_Command;
+      Event   : Gdk.Event.Gdk_Event) return Command_Return_Type is abstract;
+   --  Execute the command.
+   --  If Event is null, it should be executed non-interactively. Otherwise,
+   --  Event is set to the event that started the execution (a Gdk_Key_Event
+   --  if started from a key, a Gtk_Button_Event if started from a menu,...)
+
+   function Execute (Command : access Interactive_Command)
+      return Command_Return_Type;
+   --  Execute the command non-interactively
+
+end Commands.Interactive;
