642cdea3ccec33d93e20db803c6e39fc38584f49
Sebastien Nedjar <nedjar@adacore.com>
2003-04-18 15:33:32 +0000

    Add Diff_List_Head for gestion of all currents Diff Add Mark in record Diff_range for jump directly to the location of a differences. Instenciate generic liste And create all procedure for memory management
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@82401 936e1b1b-40f2-da11-902a-00137254ae57

--- vdiff/src/diff_utils.ads
+++ vdiff/src/diff_utils.ads
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                        Copyright (C) 2002                         --
+--                        Copyright (C) 2003                         --
 --                            ACT-Europe                             --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
@@ -20,14 +20,17 @@
 
 --  This package provides low-level utilities to handle differences between
 --  files.
-
+with Generic_List;
 with Glide_Kernel;
+with Ada.Unchecked_Deallocation;
+with GNAT.OS_Lib;              use GNAT.OS_Lib;
 
 package Diff_Utils is
 
    type Diff_Range is record
       First : Natural;
       Last  : Natural;
+      Mark  : String_Access := null;
    end record;
 
    type Diff_Action is (Append, Change, Delete, Nothing);
@@ -42,7 +45,17 @@ package Diff_Utils is
       Range2 : Diff_Range;
       Action : Diff_Action;
       Next   : Diff_Occurrence_Link;
+      Prev   : Diff_Occurrence_Link;
    end record;
+
+   type Diff_List_Head is record
+      List         : Diff_Occurrence_Link;
+      File1        : String_Access;
+      File2        : String_Access;
+      File3        : String_Access := null;
+      Current_Diff : Diff_Occurrence_Link := null;
+   end record;
+   type Diff_Head_Access is access all Diff_List_Head;
 
    function Diff
      (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class;
@@ -58,13 +71,26 @@ package Diff_Utils is
    --  Compute the differences from Diff_File.
    --  If Revert is False, create New_File from Orig_File and Diff_File.
    --  If Revert is True, create Orig_File from New_File and Diff_File.
-
    procedure Free (Link : in out Diff_Occurrence_Link);
    --  Free the memory associated with each node of the list Link.
+   procedure Free (Link : in out Diff_List_Head);
+   --  Free the memory of this Link. just for use generic_list
+
+   procedure Free is
+        new Ada.Unchecked_Deallocation (Diff_List_Head, Diff_Head_Access);
+   --  Free the memory associated with the head of the list Link.
+   package Diff_Occurrence_List is new Generic_List (Diff_List_Head, Free);
+
+   type Diff_Occurrence_List_Access is access all Diff_Occurrence_List.List;
+   procedure Free_List (List : in out Diff_Occurrence_List.List);
+   --  Free all content of node of the list. overide the standard Free
 
    function Fine_Diff (Line1, Line2 : String) return Diff_Occurrence_Link;
    --  Do a fine diff between two lines.
    --  The only fields set in the resulting list is Range1 and Next, other
    --  fields should be ignored.
+   function Diff3 (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class;
+                   File1, File2, File3 : String) return Diff_Occurrence_Link;
+   --  Execute diff on File1,File2 and File3 and return a list of differences.
 
 end Diff_Utils;
