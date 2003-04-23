097661f453b50c1e2b626a3adb5578df455d4054
Sebastien Nedjar <nedjar@adacore.com>
2003-04-23 17:22:28 +0000

    add command
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@82472 936e1b1b-40f2-da11-902a-00137254ae57

--- vdiff/src/vdiff2_command.ads
+++ vdiff/src/vdiff2_command.ads
@@ -1,3 +1,22 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2001-2003                    --
+--                            ACT-Europe                             --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
 with Glide_Kernel; use Glide_Kernel;
 with Commands;     use Commands;
 with Diff_Utils;   use Diff_Utils;
@@ -35,5 +54,11 @@ package Vdiff2_Command is
                               Diff   : in out Diff_Head_Access);
 
    procedure Last_Difference (Kernel : Kernel_Handle;
-                             Diff   : in out Diff_Head_Access);
+                              Diff   : in out Diff_Head_Access);
+   procedure Reload_Difference (Kernel : Kernel_Handle;
+                              Diff   : in out Diff_Head_Access);
+   procedure Close_Difference (Kernel : Kernel_Handle;
+                               Diff   : in out Diff_Head_Access);
+   procedure Unhighlight_Difference (Kernel : Kernel_Handle;
+                              Diff   : in out Diff_Head_Access);
 end Vdiff2_Command;
