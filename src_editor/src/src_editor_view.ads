96c83c1b293249c4e3f898820d41e399b0fdd635
Nicolas Setton <setton@adacore.com>
2003-04-24 17:05:10 +0000

    Add caches for the side column graphics.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@82499 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/src_editor_view.ads
+++ src_editor/src/src_editor_view.ads
@@ -26,6 +26,7 @@
 with Glib; use Glib;
 with Gdk.GC;
 with Gdk.Event;
+with Gdk.Pixmap;
 
 with Pango.Font;
 
@@ -114,6 +115,13 @@ private
 
       Top_Line            : Src_Editor_Buffer.Buffer_Line_Type := 1;
       Bottom_Line         : Src_Editor_Buffer.Buffer_Line_Type := 0;
+
+      Buffer_Top_Line     : Src_Editor_Buffer.Buffer_Line_Type := 1;
+      Buffer_Bottom_Line  : Src_Editor_Buffer.Buffer_Line_Type := 0;
+      Side_Column_Buffer  : Gdk.Pixmap.Gdk_Pixmap;
+      --  Cache for avoiding to redraw the side columns too often.
+
+      Side_Columns_Up_To_Date : Boolean := False;
 
       Connect_Expose_Id : Gtk.Main.Idle_Handler_Id;
       --  Handler ID for the Connect_Expose idle callback.
