32879ba1e6ef107980603f4d48baf5660301222d
Nicolas Setton <setton@adacore.com>
2003-05-12 13:33:10 +0000

    (Compute_Blocks): Get the string using Get_String. More correct when there is  block folding, added lines, etc (although there are still some problems, to  be fixed in later check-ins).  Add commands to fold the blocks for enclosing entities.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@82795 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/src_editor_buffer-blocks.adb
+++ src_editor/src/src_editor_buffer-blocks.adb
@@ -19,17 +19,18 @@
 -----------------------------------------------------------------------
 
 with Language;    use Language;
-with Basic_Types; use Basic_Types;
-with Interfaces.C;
-with Interfaces.C.Strings;
+
+with Src_Editor_Buffer.Line_Information;
+use Src_Editor_Buffer.Line_Information;
+
+with Src_Editor_Module; use Src_Editor_Module;
+
+with Commands.Editor; use Commands.Editor;
+with Gtk.Text_Iter;   use Gtk.Text_Iter;
 
 package body Src_Editor_Buffer.Blocks is
 
    use Block_List;
-
-   function Strlen
-     (Str : Gtkada.Types.Chars_Ptr) return Interfaces.C.size_t;
-   pragma Import (C, Strlen);
 
    --------------------
    -- Compute_Blocks --
@@ -38,16 +39,13 @@ package body Src_Editor_Buffer.Blocks is
    procedure Compute_Blocks (Buffer : access Source_Buffer_Record'Class) is
       Constructs    : Construct_List;
       Current       : Construct_Access;
-      Line_Start    : Buffer_Line_Type;
-      Line_End      : Buffer_Line_Type;
+      Line_Start    : Editable_Line_Type;
+      Line_End      : Editable_Line_Type;
       Column        : Integer;
-      C_Str         : Gtkada.Types.Chars_Ptr := Gtkada.Types.Null_Ptr;
-      Slice_Length  : Natural;
-      Slice         : Unchecked_String_Access;
+      Text          : GNAT.OS_Lib.String_Access;
       Block         : Block_Access;
-      pragma Suppress (Access_Check, Slice);
-
-      use type Interfaces.C.Strings.chars_ptr;
+      Buffer_Line   : Buffer_Line_Type;
+      Block_Folded  : Boolean;
 
    begin
       if not Buffer.Parse_Blocks then
@@ -68,24 +66,23 @@ package body Src_Editor_Buffer.Blocks is
          Buffer.Line_Data (Line).Block := null;
       end loop;
 
-      C_Str := Get_Slice (Buffer, 0, 0);
+      Text := Get_String (Source_Buffer (Buffer));
 
-      if C_Str = Gtkada.Types.Null_Ptr then
-         return;
-      end if;
+      --  ??? See if we could have a line-by-line version of Parse_Constructs.
 
-      Slice        := To_Unchecked_String (C_Str);
-      Slice_Length := Natural (Strlen (C_Str));
+      Parse_Constructs (Buffer.Lang, Text.all, Constructs);
+      GNAT.OS_Lib.Free (Text);
 
-      Parse_Constructs (Buffer.Lang, Slice (1 .. Slice_Length), Constructs);
       Current := Constructs.First;
 
       while Current /= null loop
+         Block_Folded := False;
+
          if Current.Category in Construct_Category
            or else Current.Category in Enclosing_Entity_Category
          then
-            Line_Start := Buffer_Line_Type (Current.Sloc_Start.Line);
-            Line_End   := Buffer_Line_Type (Current.Sloc_End.Line);
+            Line_Start := Editable_Line_Type (Current.Sloc_Start.Line);
+            Line_End   := Editable_Line_Type (Current.Sloc_End.Line);
             Column     := Integer'Min
               (Current.Sloc_Start.Column, Current.Sloc_End.Column);
 
@@ -97,15 +94,60 @@ package body Src_Editor_Buffer.Blocks is
                Block_Type        => Current.Category,
                GC                => null);
 
-            Buffer.Line_Data (Line_Start).Block := Block;
+            Buffer_Line := Get_Buffer_Line (Buffer, Line_Start);
 
-            Append (Buffer.Blocks, Block);
+            if Buffer_Line = 0 then
+               Block_Folded := True;
+            else
+               Buffer.Line_Data (Buffer_Line).Block := Block;
+               Append (Buffer.Blocks, Block);
 
-            for J in Line_Start + 1 .. Line_End loop
-               if Buffer.Line_Data (J).Block = null then
-                  Buffer.Line_Data (J).Block := Block;
+               for J in Line_Start + 1 .. Line_End loop
+
+                  Buffer_Line := Get_Buffer_Line (Buffer, J);
+
+                  if Buffer_Line = 0 then
+                     Block_Folded := True;
+                     exit;
+                  end if;
+
+                  if Buffer.Line_Data (Buffer_Line).Block = null then
+                     Buffer.Line_Data (Buffer_Line).Block := Block;
+                  end if;
+               end loop;
+            end if;
+         end if;
+
+         --  Fill the folding information.
+         --  ??? This needs to be optimized.
+
+         if (not Block_Folded)
+           and then Current.Category in Enclosing_Entity_Category
+           and then Current.Sloc_End.Line /= Current.Sloc_Start.Line
+         then
+            declare
+               Command : Hide_Editable_Lines_Command;
+               Iter    : Gtk_Text_Iter;
+               Buffer_Line : Buffer_Line_Type;
+            begin
+               Buffer_Line := Get_Buffer_Line
+                 (Buffer, Editable_Line_Type (Current.Sloc_Start.Line));
+
+               if Buffer_Line /= 0 then
+                  Command := new Hide_Editable_Lines_Type;
+                  Command.Buffer := Source_Buffer (Buffer);
+                  Get_Iter_At_Line (Buffer, Iter, Gint (Buffer_Line - 1) + 1);
+                  Command.Mark   := Create_Mark (Buffer, "", Iter);
+                  Command.Number := Editable_Line_Type
+                    (Current.Sloc_End.Line - Current.Sloc_Start.Line) - 1;
+
+                  Add_Block_Command
+                    (Buffer,
+                     Buffer_Line,
+                     Command_Access (Command),
+                     Hide_Block_Pixbuf);
                end if;
-            end loop;
+            end;
          end if;
 
          Current := Current.Next;
