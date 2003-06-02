22248a033d44d0a48163eee7881a37b7a509d956
Sebastien Nedjar <nedjar@adacore.com>
2003-06-02 12:08:44 +0000

    Initial revision for this file.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@83328 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ vdiff/src/vdiff2_module-utils-text.adb
@@ -0,0 +1,265 @@
+
+with Vdiff2_Module.Utils.Shell_Command; use Vdiff2_Module.Utils.Shell_Command;
+
+package body Vdiff2_Module.Utils.Text is
+
+   --------------
+   -- New_Line --
+   --------------
+
+   procedure New_Line
+     (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class;
+      File   : Virtual_File;
+      Line   : Natural)
+   is
+   begin
+      Insert_Line (Kernel, File, Line, "");
+   end New_Line;
+
+   -----------------
+   -- Line_Length --
+   -----------------
+
+   function Line_Length
+     (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class;
+      File   : Virtual_File;
+      Line   : Natural)
+      return Natural
+   is
+   begin
+      return Get_Line (Kernel, File, Line)'Length;
+   end Line_Length;
+
+   ------------
+   -- Delete --
+   ------------
+
+   procedure Delete
+     (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class;
+      File   : Virtual_File;
+      Line   : Natural;
+      Column : Natural;
+      Len    : Natural := 0)
+   is
+   begin
+      Replace_Text (Kernel, File, Line, Column, "", 0, Len);
+   end Delete;
+
+   -----------------
+   -- Delete_Line --
+   -----------------
+
+   procedure Delete_Line
+     (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class;
+      File   : Virtual_File;
+      Line   : Natural)
+   is
+   begin
+      Replace_Text (Kernel, File, Line, 1, "");
+   end Delete_Line;
+
+   ------------
+   -- Insert --
+   ------------
+
+   procedure Insert
+     (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class;
+      File   : Virtual_File;
+      Line   : Natural;
+      Column : Natural;
+      Text   : String)
+   is
+   begin
+      Replace_Text (Kernel, File, Line, Column, Text, 0, 0);
+   end Insert;
+
+   -----------------
+   -- Insert_Line --
+   -----------------
+
+   procedure Insert_Line
+     (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class;
+      File   : Virtual_File;
+      Line   : Natural;
+      Text   : String)
+   is
+   begin
+      Replace_Text (Kernel, File, Line, 1,
+                    Text & ASCII.LF, 0, 0);
+   end Insert_Line;
+
+   ---------
+   -- Get --
+   ---------
+
+   function Get
+     (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class;
+      File   : Virtual_File;
+      Line   : Natural;
+      Column : Natural;
+      Len    : Natural)
+      return String
+   is
+   begin
+      return Get_Chars (Kernel, File, Line, Column, 0, Len);
+   end Get;
+
+   --------------
+   -- Get_Line --
+   --------------
+
+   function Get_Line
+     (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class;
+      File   : Virtual_File;
+      Line   : Natural)
+      return String
+   is
+   begin
+      return Get_Chars (Kernel, File, Line, 1);
+   end Get_Line;
+
+   ----------------
+   -- Move_Block --
+   ----------------
+
+   procedure Move_Block
+     (Kernel       : Kernel_Handle;
+      Source_File  : Virtual_File;
+      Dest_File    : Virtual_File;
+      Source_Range : in out Diff_Range;
+      Dest_Range   : in out Diff_Range)
+   is
+      Offset_Dest       : constant Natural :=
+        Dest_Range.Last - Dest_Range.First;
+      Offset_Source     : constant Natural :=
+        Source_Range.Last - Source_Range.First;
+      Offset_Min        : Natural := Offset_Source;
+
+      First_Dest        : Natural;
+      First_Source      : Natural;
+
+      Current_Line      : String_Access;
+
+   begin
+      Remove_Blank_Lines (Kernel, Dest_Range.Blank_Lines);
+      Remove_Blank_Lines (Kernel, Source_Range.Blank_Lines);
+
+      First_Dest := Get_Line_Number (Kernel, Dest_Range.Mark.all);
+      First_Source := Get_Line_Number (Kernel, Source_Range.Mark.all);
+
+      Trace (Me, "VALEUR DE FIRST DEST : "& Integer'Image (First_Dest));
+      Trace (Me, "VALEUR DE FIRST SOURCE : "& Integer'Image (First_Source));
+
+      if Offset_Source > Offset_Dest then
+         Offset_Min := Offset_Dest;
+      end if;
+
+      if Offset_Source > 0 and Offset_Dest > 0 then
+
+         for J in 1 .. Offset_Min loop
+            Current_Line := new String'
+              (Get_Line (Kernel, Source_File, (First_Source + J - 1)));
+
+            Replace_Line (Kernel, Dest_File, (First_Dest + J - 1),
+                          Current_Line.all);
+         end loop;
+
+         if Offset_Source /= Offset_Min then
+
+            for J in Offset_Source .. Offset_Dest loop
+               Delete_Line (Kernel, Dest_File, First_Dest + J - 1);
+            end loop;
+         end if;
+
+         if Offset_Dest /= Offset_Min then
+
+            for J in Offset_Min .. Offset_Source loop
+               Current_Line := new String'
+                 (Get_Line
+                    (Kernel, Source_File,
+                     First_Source + J));
+               Insert_Line
+                 (Kernel, Dest_File,
+                  First_Dest + J,
+                  Current_Line.all);
+            end loop;
+         end if;
+
+      elsif Offset_Dest <= 0 then
+
+         for J in 1 .. Offset_Source loop
+            Current_Line := new String'
+              (Get_Line
+                 (Kernel, Source_File,
+                  First_Source + J - 1));
+            Insert
+              (Kernel, Dest_File,
+               First_Dest + J - 1, 1,
+               Current_Line.all);
+         end loop;
+      end if;
+
+   end Move_Block;
+
+   ------------------
+   -- Delete_Block --
+   ------------------
+
+   procedure Delete_Block
+     (Kernel       : Kernel_Handle;
+      Dest_File    : Virtual_File;
+      Source_Range : in out Diff_Range;
+      Dest_Range   : in out Diff_Range)
+   is
+      Offset_Dest       : constant Natural :=
+        Dest_Range.Last - Dest_Range.First;
+      First_Dest        : Natural;
+
+   begin
+      Remove_Blank_Lines (Kernel, Dest_Range.Blank_Lines);
+      Remove_Blank_Lines (Kernel, Source_Range.Blank_Lines);
+
+      First_Dest := Get_Line_Number (Kernel, Dest_Range.Mark.all);
+
+      Trace (Me, "VALEUR DE FIRST DEST : "& Integer'Image (First_Dest));
+
+      for J in 1 .. Offset_Dest loop
+         Delete_Line (Kernel, Dest_File, (First_Dest));
+      end loop;
+
+   end Delete_Block;
+
+   -------------
+   -- Replace --
+   -------------
+
+   procedure Replace
+     (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class;
+      File   : Virtual_File;
+      Line   : Natural;
+      Column : Natural;
+      Len    : Natural;
+      Text   : String)
+   is
+   begin
+      Replace_Text (Kernel, File, Line, Column,
+                    Text, 0, Len);
+   end Replace;
+
+   ------------------
+   -- Replace_Line --
+   ------------------
+
+   procedure Replace_Line
+     (Kernel : access Glide_Kernel.Kernel_Handle_Record'Class;
+      File   : Virtual_File;
+      Line   : Natural;
+      Text   : String)
+   is
+   begin
+      Replace_Text (Kernel, File, Line, 1,
+                    Text);
+   end Replace_Line;
+
+end Vdiff2_Module.Utils.Text;
+
