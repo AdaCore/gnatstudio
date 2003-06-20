179c18f572db4a302e26cf3133190d68111e2700
Emmanuel Briot <briot@adacore.com>
2003-06-20 09:47:55 +0000

    Add support for reading file extensions, for better compatibility with Glide Check that the specified file exists before converting
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@83528 936e1b1b-40f2-da11-902a-00137254ae57

--- tools/project_converter/convert_prj.adb
+++ tools/project_converter/convert_prj.adb
@@ -1,32 +1,86 @@
 with GNAT.IO;                   use GNAT.IO;
 with Ada.Command_Line;          use Ada.Command_Line;
+with GNAT.Command_Line;         use GNAT.Command_Line;
 with Convert.Adp;               use Convert.Adp;
 with Convert.Gpr;               use Convert.Gpr;
 with GNAT.OS_Lib;               use GNAT.OS_Lib;
 with GNAT.Directory_Operations; use GNAT.Directory_Operations;
 
 procedure Convert_Prj is
-   Project_Name : String_Access;
-begin
-   if Argument_Count /= 1 then
+
+   procedure Help;
+   --  Display help
+
+   procedure Help is
+   begin
       Put_Line ("Usage: " & Command_Name & " project_name.gpr");
       Put_Line ("  convert to .adp project file");
-      Put_Line ("Usage: " & Command_Name & " project_name.adp");
+      Put_Line
+        ("Usage: " & Command_Name & " [-b ext] [-s ext] project_name.adp");
       Put_Line ("  convert to .gpr project file");
+      Put_Line ("     -b ext:  Specifies the extension of body files"
+                & " (default is "".adb""");
+      Put_Line ("     -s ext:  Specifies the extension of spec files"
+                & " (default is "".ads""");
+   end Help;
+
+   Spec_Extension : String_Access := new String'(".ads");
+   Body_Extension : String_Access := new String'(".adb");
+   Project_Name : String_Access;
+
+begin
+   loop
+      case Getopt ("s: b:") is
+         when 's' =>
+            Free (Spec_Extension);
+            Spec_Extension := new String'(Parameter);
+         when 'b' =>
+            Free (Body_Extension);
+            Body_Extension := new String'(Parameter);
+         when others =>
+            exit;
+      end case;
+   end loop;
+
+   loop
+      declare
+         S : constant String := Get_Argument (Do_Expansion => True);
+      begin
+         exit when S'Length = 0;
+
+         if Project_Name /= null then
+            Help;
+            return;
+         end if;
+
+         Free (Project_Name);
+         Project_Name := new String'(S);
+      end;
+   end loop;
+
+   if Project_Name = null then
+      Help;
       return;
    end if;
 
-   Project_Name := new String'(Argument (1));
-
-   if File_Extension (Project_Name.all) = ".gpr" then
-      Convert_From_Gpr_To_Adp (Project_Name.all);
-
-   elsif File_Extension (Project_Name.all) = ".adp" then
-      Convert_From_Adp_To_Gpr (Project_Name.all);
-
+   if not Is_Regular_File (Project_Name.all) then
+      Put_Line ("File not found: " & Project_Name.all);
    else
-      Put_Line ("Unsupported file extension "
-                & File_Extension (Project_Name.all));
+      if File_Extension (Project_Name.all) = ".gpr" then
+         Convert_From_Gpr_To_Adp (Project_Name.all);
+
+      elsif File_Extension (Project_Name.all) = ".adp" then
+         Convert_From_Adp_To_Gpr
+           (Project_Name.all, Spec_Extension, Body_Extension);
+
+      else
+         Put_Line ("Unsupported file extension "
+                   & File_Extension (Project_Name.all));
+      end if;
    end if;
 
+
+exception
+   when Invalid_Switch | Invalid_Parameter =>
+      Help;
 end Convert_Prj;
