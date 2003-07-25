077431f640c4555174bbd6110b94d45b5b4de1ce
Pascal Obry <obry@adacore.com>
2003-07-25 18:16:00 +0000

    (Finish_Setup): Do not disable Navigate->Next/Previous Subprogram.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@84196 936e1b1b-40f2-da11-902a-00137254ae57

--- glide/src/gps.adb
+++ glide/src/gps.adb
@@ -931,11 +931,6 @@ procedure GPS is
          Set_Sensitive (Find_Menu_Item
            (GPS.Kernel, Navigate & (-"End Of Statement")), False);
          Set_Sensitive (Find_Menu_Item
-           (GPS.Kernel, Navigate & (-"Next Procedure")), False);
-         Set_Sensitive (Find_Menu_Item
-           (GPS.Kernel, Navigate & (-"Previous Procedure")), False);
-
-         Set_Sensitive (Find_Menu_Item
            (GPS.Kernel, Tools & (-"Profile")), False);
          Set_Sensitive (Find_Menu_Item
            (GPS.Kernel, Tools & (-"Memory Analyzer")), False);
