cb13ea5c9350bba5b6e0877162dd5cdf52fc2490
Arnaud Charlet <charlet@adacore.com>
2003-08-20 08:50:34 +0000

    Minor reformatting. Add support for language engines in shared libraries to provide formatters  and parsers via xml files. Add ability to derive from another language (known statically such as  Ada, C, C++, or created dynamically via xml files) (Get_Language_Context): Now return a Language_Context_Access.  More efficient, and also avoid a code generation problem.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@84655 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/common/language-custom.ads
+++ gvd/common/language-custom.ads
@@ -21,6 +21,8 @@
 --  This is a generic language that it customized from a XML file.
 --  See language.ads for a complete spec.
 
+with System;
+with Interfaces.C.Strings;
 with Glib.Xml_Int;
 
 package Language.Custom is
@@ -44,7 +46,7 @@ package Language.Custom is
      return GNAT.Regpat.Pattern_Matcher;
 
    function Get_Language_Context
-     (Lang : access Custom_Language) return Language_Context;
+     (Lang : access Custom_Language) return Language_Context_Access;
 
    --------------
    -- Explorer --
@@ -91,21 +93,228 @@ package Language.Custom is
    function Get_Body_Suffix (Lang : access Custom_Language) return String;
    --  Return the body suffix associated with Lang.
 
+   ----------------------
+   -- Source Analyzing --
+   ----------------------
+
+   --  The following primitives are either defaulting to the Language_Root
+   --  implementation, or redefined using a shared library specified in the
+   --  xml file.
+
+   function Comment_Line
+     (Lang : access Custom_Language;
+      Line : String) return String;
+
+   function Uncomment_Line
+     (Lang : access Custom_Language;
+      Line : String) return String;
+
+   procedure Parse_Constructs
+     (Lang   : access Custom_Language;
+      Buffer : String;
+      Result : out Construct_List);
+
+   procedure Format_Buffer
+     (Lang          : access Custom_Language;
+      Buffer        : String;
+      Replace       : Replace_Text_Callback;
+      From, To      : Natural := 0;
+      Indent_Params : Indent_Parameters := Default_Indent_Parameters);
+
+   procedure Parse_Entities
+     (Lang     : access Custom_Language;
+      Buffer   : String;
+      Callback : Entity_Callback);
+
 private
 
-   type Language_Context_Access is access Language_Context;
-   type Explorer_Categories_Access is access Explorer_Categories;
-   type Pattern_Matcher_Access is access GNAT.Regpat.Pattern_Matcher;
-   type Project_Field_Array_Access is access Project_Field_Array;
+   type Explorer_Categories_Access is access all Explorer_Categories;
+   type Pattern_Matcher_Access is access all GNAT.Regpat.Pattern_Matcher;
+   type Project_Field_Array_Access is access all Project_Field_Array;
+
+   -----------------------------------------------
+   -- C interface to source analyzing functions --
+   -----------------------------------------------
+
+   use Interfaces.C.Strings;
+
+   function New_Construct return Construct_Access;
+   --  Export allocator to C via callbacks
+   pragma Convention (C, New_Construct);
+
+   procedure Set_Construct
+     (Construct      : Construct_Access;
+      Category       : Language_Category;
+      Name           : chars_ptr;
+      Profile        : chars_ptr;
+      Sloc_Start     : Source_Location;
+      Sloc_Entity    : Source_Location;
+      Sloc_End       : Source_Location;
+      Is_Declaration : Boolean;
+      Prev, Next     : Construct_Access);
+   --  Export setter to C via callbacks
+   pragma Convention (C, Set_Construct);
+
+   type Comment_Line_Proc is access
+     function (Line : String; Length : Integer) return chars_ptr;
+   pragma Convention (C, Comment_Line_Proc);
+   --  C profile for Comment/Uncomment_Line:
+   --  typedef char * (*comment_line_proc) (char *line, int length);
+
+   type Parse_Constructs_Proc is access
+     procedure (Buffer        : String;
+                Result        : out Construct_List;
+                Length        : Integer;
+                New_Construct : System.Address;
+                Set_Construct : System.Address);
+   pragma Convention (C, Parse_Constructs_Proc);
+   --  C profile for Parse_Constructs:
+   --
+   --  typedef struct
+   --    {
+   --      int line;
+   --      int column;
+   --      int index;
+   --    } source_location;
+   --
+   --  typedef enum
+   --    {cat_unknown,
+   --
+   --     cat_package,
+   --     cat_namespace,
+   --     cat_task,
+   --     cat_procedure,
+   --     cat_function,
+   --     cat_method,
+   --     cat_constructor,
+   --     cat_destructor,
+   --     cat_protected,
+   --     cat_entry,
+   --
+   --     cat_class,
+   --     cat_structure,
+   --     cat_union,
+   --     cat_type,
+   --     cat_subtype,
+   --     cat_variable,
+   --     cat_local_variable,
+   --     cat_parameter,
+   --     cat_literal,
+   --     cat_representation_clause,
+   --
+   --     cat_with,
+   --     cat_use,
+   --     cat_include,
+   --
+   --     cat_loop_statement,
+   --     cat_if_statement,
+   --     cat_case_statement,
+   --     cat_select_statement,
+   --     cat_accept_statement,
+   --     cat_declare_block,
+   --     cat_simple_block,
+   --     cat_exception_handler} language_category;
+   --
+   --  typedef void *construct_access;
+   --
+   --  typedef construct_access (*new_construct_proc) (void);
+   --
+   --  typedef void (*set_construct_proc)
+   --    (construct_access construct,
+   --     language_category category,
+   --     char *name,
+   --     char *profile,
+   --     source_location *sloc_start,
+   --     source_location *sloc_entity,
+   --     source_location *sloc_end,
+   --     char is_declaration,
+   --     construct_access prev,
+   --     construct_access next);
+   --
+   --  typedef struct
+   --    {
+   --      construct_access first, current, last;
+   --    } construct_list;
+   --
+   --  typedef void (*parse_constructs_proc)
+   --    (char *buffer,
+   --     construct_list *result,
+   --     int length,
+   --     new_construct_proc new_construct,
+   --     set_construct_proc set_construct);
+
+   type Format_Buffer_Proc is access
+     procedure (Buffer        : String;
+                Replace       : System.Address;
+                From, To      : Integer;
+                Indent_Params : Indent_Parameters;
+                Length        : Integer);
+   pragma Convention (C, Format_Buffer_Proc);
+   --  C profile for Format_Buffer:
+   --
+   --  typedef void (*replace_text_cb)
+   --    (int line, int first, int last, char *replace);
+   --
+   --  typedef enum {unchanged, upper, lower, mixed, smart_mixed} casing_type;
+   --
+   --  typedef struct
+   --    {
+   --      int indent_level;
+   --      int indent_continue;
+   --      int indent_decl;
+   --      int tab_width;
+   --      char indent_case_extra;
+   --      casing_type reserved_casing;
+   --      casing_type ident_casing;
+   --      char format_operators;
+   --      char use_tabs;
+   --      char align_on_colons;
+   --      char align_on_arrows;
+   --    } indent_parameters;
+   --
+   --  typedef void (*format_buffer_proc)
+   --    (char *buffer, replace_text_cb replace,
+   --     int from, int to, indent_parameters *indent_params, int length);
+
+   type Parse_Entities_Proc is access
+     procedure (Buffer   : String;
+                Callback : System.Address;
+                Length   : Integer);
+   pragma Convention (C, Parse_Entities_Proc);
+   --  C profile for Parse_Entities:
+   --
+   --  typedef enum
+   --    {normal_text,
+   --     identifier_text,
+   --     keyword_text,
+   --     comment_text,
+   --     character_text,
+   --     string_text} language_entity;
+   --
+   --  typedef char (*entity_cb)
+   --    (language_entity entity,
+   --     source_location *sloc_start,
+   --     source_location *sloc_end,
+   --     char partial_entity);
+   --
+   --  typedef void (*parse_entities_proc)
+   --    (char *buffer, int length, entity_cb callback);
 
    type Custom_Language is new Language_Root with record
-      Categories  : Explorer_Categories_Access;
-      Keywords    : Pattern_Matcher_Access;
-      Context     : Language_Context_Access;
-      Name        : Glib.String_Ptr;
-      Spec_Suffix : Glib.String_Ptr;
-      Body_Suffix : Glib.String_Ptr;
-      Project_Fields : Project_Field_Array_Access;
+      Categories       : Explorer_Categories_Access;
+      Keywords         : Pattern_Matcher_Access;
+      Context          : Language_Context_Access;
+      Name             : Glib.String_Ptr;
+      Spec_Suffix      : Glib.String_Ptr;
+      Body_Suffix      : Glib.String_Ptr;
+      Project_Fields   : Project_Field_Array_Access;
+      Comment_Line     : Comment_Line_Proc;
+      Uncomment_Line   : Comment_Line_Proc;
+      Parse_Constructs : Parse_Constructs_Proc;
+      Format_Buffer    : Format_Buffer_Proc;
+      Parse_Entities   : Parse_Entities_Proc;
+      Parent           : Language_Access;
+      Next             : Custom_Language_Access;
    end record;
 
 end Language.Custom;
