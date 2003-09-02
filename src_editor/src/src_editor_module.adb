46db37015c5be8774086c44d1887068e2780402e
Arnaud Charlet <charlet@adacore.com>
2003-09-02 14:10:24 +0000

    (On_Pretty_Print): Take the project path, not the project name. Fixes C901-006.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@84813 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/src_editor_module.adb
+++ src_editor/src/src_editor_module.adb
@@ -2902,7 +2902,7 @@ package body Src_Editor_Module is
            File_Selection_Context_Access (Context);
          File         : constant Virtual_File :=
            File_Information (File_Context);
-         Project      : constant String := Project_Name
+         Project      : constant String := Project_Path
            (Get_Project_From_File (Get_Registry (Kernel), File));
          Success      : Boolean;
          Args, Vars   : Argument_List_Access;
