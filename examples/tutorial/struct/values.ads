20957518cf66b6cf94561ec7d0c7c1facc343ba0
Nicolas Setton <setton@adacore.com>
2003-09-18 14:13:04 +0000

    Values can now be either integers or matrices.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@85120 936e1b1b-40f2-da11-902a-00137254ae57

--- examples/tutorial/struct/values.ads
+++ examples/tutorial/struct/values.ads
@@ -1,6 +1,8 @@
 --  Values (such as integers, reals, complex numbers, strings, etc.)
 --  manipulated by SDC.
 
+with Matrix_Binding; use Matrix_Binding;
+
 package Values is
 
    type Value is private;
@@ -18,10 +20,18 @@ package Values is
 
 private
 
-   --  For now integer is the only data type available in SDC.
+   --  For now integer and matrices are the only data type available in SDC.
 
-   type Value_Info is record
-      E : Integer;
+   type Value_Kind is (Int, Matrix);
+
+   type Value_Info (Kind : Value_Kind) is record
+      case Kind is
+         when Int =>
+            E : Integer;
+
+         when Matrix =>
+            M : Matrix_Type;
+      end case;
    end record;
 
    type Value is access Value_Info;
