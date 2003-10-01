5929057c5a17eb9b323f4ec576179522ed58ca1d
Emmanuel Briot <briot@adacore.com>
2003-10-01 08:59:31 +0000

    Reindent everything through GPS, to make editing easier Change DB_Pair_struct, so that no dynamic memory allocation is needed anymore (csf_free): removed, no longer needed (csf_init): profile changed for adaptation to the new struct (ada_db_get_pair): no longer preparse the key and data, this is done when  reading only. Saves some memory allocation and reduces the size of the  structure
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@85269 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/sn/src/db_capi.c
+++ kernel/src_info/sn/src/db_capi.c
@@ -20,24 +20,24 @@
 #define POS_FIRST       1	/* first key/data pair */
 #define POS_LAST        2	/* last key/data pair  */
 #define POS_BY_KEY      3	/* first key/data pair matched to key pattern
-				 * (see db_api.ads, Set_Cursor) */
+* (see db_api.ads, Set_Cursor) */
 
 #define MOVE_PREV   4		/* move to the previous  pair */
 #define MOVE_NEXT   5		/* move to the next pair      */
 #define MOVE_BY_KEY 6		/* move to the next key/data pair matched to
-				   key pattern */
+key pattern */
 
 /* C presentation of Ada type DB_File */
 typedef struct DB_File_struct {
-    int dbc;                    /* number of database files - 1 */
-    DB **db;                    /* database handlers */
-    char **fname;    		/* file names (copies) */
-    int last_errno;		/* 0 or last error number */
-    char *key_p;		/* key pattern */
-    int exact_match;		/* 1/0, if exact key match needed/not needed */
-    int pos;			/* starting cursor position */
-    int saved_pos;		/* saved starting cursor position */
-    int dbi;			/* index of current database handler */
+  int dbc;                    /* number of database files - 1 */
+  DB **db;                    /* database handlers */
+  char **fname;    		/* file names (copies) */
+  int last_errno;		/* 0 or last error number */
+  char *key_p;		/* key pattern */
+  int exact_match;		/* 1/0, if exact key match needed/not needed */
+  int pos;			/* starting cursor position */
+  int saved_pos;		/* saved starting cursor position */
+  int dbi;			/* index of current database handler */
 } DB_File;
 
 #define CSF_MAX_FIELDS 15	/* maximum of fields in csf */
@@ -45,331 +45,300 @@ typedef struct DB_File_struct {
 /* C presentation of Ada type CSF,
  * CSF stands for "Character Separated Fields".
  * Fields are separated by DB_FLDSEP_CHR character, defined in sn.h
- */
+*/
 typedef struct CSF_struct {
-    int num_of_fields;
-    int length;
-    char *fields[CSF_MAX_FIELDS + 1];	/* num_of_fields pointers to char;
+  int num_of_fields;
+  char *fields[CSF_MAX_FIELDS + 1];	/* num_of_fields pointers to char;
 					 * +1 to store pointer after the last '\0'
-					 * to unify field length calculation */
-    /* string buffer goes here */
+  * to unify field length calculation */
 } CSF;
 
 typedef struct DB_Pair_struct {
-    CSF *key;
-    CSF *data;
-    int dbi;
+  char* key;
+  char* data;
+  int dbi;
 } DB_Pair;
 
-/* Release CSF from memory */
-void csf_free(CSF * csf)
-{
-    free(csf);
-}
 
 /* Constructs CSF from given string, makes a copy of given string */
-CSF *csf_init(char *v)
-{
-    CSF *csf;
-    char *p, *q;
-    int cf;
+void csf_init (char* v, CSF* csf) {
+  char *p;
+  int cf;
 
-    if (v == 0)
-	return 0;
+  if (v == 0)
+    return;
 
-    /* allocate memory for structure and for string buffer */
-    csf = (CSF *) malloc(sizeof(CSF) + strlen(v) + 1);
+  cf = 0;
+  csf->fields[cf++] = v;
+  for (p = v; *p; p++)
+    if (*p == DB_FLDSEP_CHR) {
+      if (cf < CSF_MAX_FIELDS)
+	csf->fields[cf++] = p + 1;
+    }
+  csf->fields[cf] = p + 1; /* note: this last pointer is not legal:
+  * it is used only for field length calculation */
 
-    csf->length = 0;
-
-    cf = 0;
-    csf->fields[cf++] = q = (char*) csf + sizeof(CSF);
-    for (p = v; *p; p++, q++)
-	if (*p == DB_FLDSEP_CHR) {
-	    *q = 0;
-	    if (cf < CSF_MAX_FIELDS)
-		csf->fields[cf++] = q + 1;
-	} else {
-	    *q = *p;
-	    csf->length++;
-	}
-    *q = 0;
-    csf->fields[cf] = q + 1; /* note: this last pointer is not legal:
-			      * it is used only for field length calculation */
-
-    csf->num_of_fields = cf; /* see note above */
-
-    return csf;
+  csf->num_of_fields = cf; /* see note above */
 }
 
 /* Returns number of fields. */
 int csf_get_field_count(CSF * csf)
 {
-    if (csf == 0)
-	return 0;
-    return csf->num_of_fields;
-}
-
-int csf_get_total_length(CSF *csf)
-{
-    int i, n=0;
-    if (csf == 0)
-	return 0;
-    return csf->length;
+  if (csf == 0)
+    return 0;
+  return csf->num_of_fields;
 }
 
 /* Return string at index, index is 1 .. get_field_count.
- * Returns NULL if index is out of range */
+* Returns NULL if index is out of range */
 char *csf_get_field(CSF * csf, int index)
 {
-    if (csf == 0)
-	return 0;
-    if (csf->num_of_fields < index || index < 1)
-	return 0;
-    return (csf->fields[index - 1]);
+  if (csf == 0)
+    return 0;
+  if (csf->num_of_fields < index || index < 1)
+    return 0;
+  return (csf->fields[index - 1]);
 }
 
 int csf_get_field_length(CSF * csf, int index)
 {
-    if (csf == 0)
-	return 0;
-    if (csf->num_of_fields < index || index < 1)
-	return 0;
-    return csf->fields[index] - csf->fields[index - 1] - 1;
+  if (csf == 0)
+    return 0;
+  if (csf->num_of_fields < index || index < 1)
+    return 0;
+  return csf->fields[index] - csf->fields[index - 1] - 1;
 }
 
 DB_File *ada_db_open(const int num_of_files, const char **file_names)
 {
-    DB_File *file;
-    int i,j = 0;
+  DB_File *file;
+  int i,j = 0;
 
-    file = (DB_File *) malloc(sizeof(DB_File));
-    file->fname = (char **) malloc(sizeof(char *) * num_of_files);
-    file->db = (DB **) malloc(sizeof(DB *) * num_of_files);
+  file = (DB_File *) malloc(sizeof(DB_File));
+  file->fname = (char **) malloc(sizeof(char *) * num_of_files);
+  file->db = (DB **) malloc(sizeof(DB *) * num_of_files);
 
-    file->dbi = -1;
-    file->pos = POS_FIRST;
+  file->dbi = -1;
+  file->pos = POS_FIRST;
 
-    for (i = 0; i < num_of_files; i++) {
-	file->db[i] = dbopen(file_names[i], O_RDONLY, 0644, DB_BTREE, 0);
-	if (file->db[i] == 0) {
-	    file->last_errno = errno;
-	    file->fname[i] = 0;
-	} else  {
-	    file->fname[i] = strdup(file_names[i]);
-	    j++;
-    	}
+  for (i = 0; i < num_of_files; i++) {
+    file->db[i] = dbopen(file_names[i], O_RDONLY, 0644, DB_BTREE, 0);
+    if (file->db[i] == 0) {
+      file->last_errno = errno;
+      file->fname[i] = 0;
+    } else  {
+      file->fname[i] = strdup(file_names[i]);
+      j++;
     }
+  }
 
-    if (j == 0) { /* no open files at all */
-	return file;
-    }
-
-    file->last_errno = 0; /* reset error flag if there is
-			   * at least one open file */
-    file->dbc = num_of_files - 1;
-    file->key_p = 0;
-
+  if (j == 0) { /* no open files at all */
     return file;
+  }
+
+  file->last_errno = 0; /* reset error flag if there is
+  * at least one open file */
+  file->dbc = num_of_files - 1;
+  file->key_p = 0;
+
+  return file;
 }
 
 DB_File *ada_db_dup(const DB_File * file)
 {
-    DB_File *new_file;
-    new_file = ada_db_open (file->dbc + 1, (const char **) file->fname);
-    return new_file;
+  DB_File *new_file;
+  new_file = ada_db_open (file->dbc + 1, (const char **) file->fname);
+  return new_file;
 }
 
 int ada_get_last_errno(const DB_File * file)
 {
-    return file->last_errno;
+  return file->last_errno;
 }
 
 char *ada_get_errstr(const DB_File * file)
 {
-    return strerror(file->last_errno);
+  return strerror(file->last_errno);
 }
 
 void ada_db_close(DB_File * file)
 {
-    int i;
-    
-    file->last_errno = 0;
-    if (file->key_p) {
-	free(file->key_p);
-    }
+  int i;
 
-    for (i = file->dbc; i >= 0; i--) {
-        if (file->fname[i]) {
-            free(file->fname[i]);
-        }
-        if (file->db[i]) {
-            if (file->db[i]->close(file->db[i]) != 0)
-                file->last_errno = errno;
-        }
-    }
+  file->last_errno = 0;
+  if (file->key_p) {
+    free(file->key_p);
+  }
 
-    free (file->fname);
-    free (file->db);
+  for (i = file->dbc; i >= 0; i--) {
+    if (file->fname[i]) {
+      free(file->fname[i]);
+    }
+    if (file->db[i]) {
+      if (file->db[i]->close(file->db[i]) != 0)
+	file->last_errno = errno;
+    }
+  }
+
+  free (file->fname);
+  free (file->db);
 }
 
 void ada_db_set_cursor(DB_File * file, int pos, char *key_p,
 		       int exact_match)
 {
-    file->last_errno = 0;
+  file->last_errno = 0;
 #ifdef RESTRICTED_CURSOR
-    if (file->dbi >= 0) {
-	file->last_errno = EINPROGRESS;
-    }
+  if (file->dbi >= 0) {
+    file->last_errno = EINPROGRESS;
+  }
 #endif
-    if (pos == POS_BY_KEY) {
-	if (file->key_p) { /* key pattern was already set, free it before */
+  if (pos == POS_BY_KEY) {
+    if (file->key_p) { /* key pattern was already set, free it before */
 #ifdef RESTRICTED_CURSOR
-            file->last_errno = EINPROGRESS;
+      file->last_errno = EINPROGRESS;
 #endif
-            free(file->key_p);
-	}
-	file->key_p = strdup(key_p);
-	file->exact_match = exact_match;
+      free(file->key_p);
     }
-    file->pos = pos;
-    file->dbi = 0;
+    file->key_p = strdup(key_p);
+    file->exact_match = exact_match;
+  }
+  file->pos = pos;
+  file->dbi = 0;
 }
 
 void ada_db_free_cursor(DB_File * file)
 {
-    if (file->key_p) {
-        free(file->key_p);
-        file->key_p = 0;
-    }
-    file->dbi = -1;
+  if (file->key_p) {
+    free(file->key_p);
+    file->key_p = 0;
+  }
+  file->dbi = -1;
 }
 
 void ada_db_get_pair (DB_File * file, int move, DB_Pair * output) {
-    DBT key, data;
-    int len, matched;
-    int flag;
-    int result;
-    int next_file;
+  DBT key, data;
+  int len, matched;
+  int flag;
+  int result;
+  int next_file;
 
-    file->last_errno = 0;
+  file->last_errno = 0;
 
-    if (move == MOVE_BY_KEY
-	&& !(file->pos == POS_BY_KEY || file->pos == MOVE_BY_KEY)) {
-	/* key matching requested, but key pattern was not set */
-	output->key = 0;
-	return;
+  if (move == MOVE_BY_KEY
+      && !(file->pos == POS_BY_KEY || file->pos == MOVE_BY_KEY)) {
+      /* key matching requested, but key pattern was not set */
+      output->dbi = -1;
+      return;
     }
 
-    switch (file->pos) {
+  switch (file->pos) {
     case POS_FIRST:
-	flag = R_FIRST;
-	file->saved_pos = R_FIRST;
-	break;
+      flag = R_FIRST;
+      file->saved_pos = R_FIRST;
+      break;
     case POS_LAST:
-	flag = R_LAST;
-	file->saved_pos = R_LAST;
-	break;
+      flag = R_LAST;
+      file->saved_pos = R_LAST;
+      break;
     case POS_BY_KEY:
-        if (!file->key_p) {
-	    file->last_errno = EINPROGRESS;
-	    output->key = 0;
-	    return;
-        }
-	flag = R_CURSOR;
-	file->saved_pos = R_CURSOR;
+      if (!file->key_p) {
+	file->last_errno = EINPROGRESS;
+	output->dbi = -1;
+	return;
+      }
+      flag = R_CURSOR;
+      file->saved_pos = R_CURSOR;
+      key.data = file->key_p;
+      key.size = strlen(file->key_p) + 1;
+      break;
+    case MOVE_PREV:
+      flag = R_PREV;
+      break;
+    case MOVE_NEXT:
+      flag = R_NEXT;
+      break;
+    case MOVE_BY_KEY:
+      flag = R_NEXT;
+      break;
+    default:
+      flag = R_FIRST;
+      file->saved_pos = R_FIRST;
+  }
+
+  file->pos = move;
+
+  next_file = 0;
+  do {
+    if (file->db[file->dbi] == 0) {
+      result = 1;
+      if (file->dbi < file->dbc) {
+	file->dbi++;
+	next_file = 1;
+      } else {
+	next_file = 0;
+      }
+      continue;
+    }
+    if (next_file) {
+      if (file->saved_pos == R_CURSOR) {
 	key.data = file->key_p;
 	key.size = strlen(file->key_p) + 1;
-	break;
-    case MOVE_PREV:
-	flag = R_PREV;
-	break;
-    case MOVE_NEXT:
-	flag = R_NEXT;
-	break;
-    case MOVE_BY_KEY:
-	flag = R_NEXT;
-	break;
-    default:
-	flag = R_FIRST;
-	file->saved_pos = R_FIRST;
+      }
+      result = file->db[file->dbi]->seq(file->db[file->dbi], &key, &data, file->saved_pos);
+    } else {
+      result = file->db[file->dbi]->seq(file->db[file->dbi], &key, &data, flag);
     }
-
-    file->pos = move;
-
     next_file = 0;
-    do {
-	if (file->db[file->dbi] == 0) {
-	    result = 1;
-	    if (file->dbi < file->dbc) {
-		file->dbi++;
-		next_file = 1;
-	    } else {
-		next_file = 0;
-	    }
-	    continue;
+    if (result == 1 || result == -1) {
+      if (file->dbi < file->dbc) {
+	file->dbi++;
+	next_file = 1;
+	continue;
+      }
+    } else if (move == MOVE_BY_KEY) {
+      /* check if retrieved key is matched */
+      len = strlen(file->key_p);
+      if (strncmp(key.data, file->key_p, len) != 0) {
+	/* not matched */
+	result = 1;
+	if (file->dbi < file->dbc) {
+	  file->dbi++;
+	  next_file = 1;
+	  continue;
 	}
-	if (next_file) {
-	    if (file->saved_pos == R_CURSOR) {
-		key.data = file->key_p;
-		key.size = strlen(file->key_p) + 1;
-	    }
-	    result = file->db[file->dbi]->seq(file->db[file->dbi], &key, &data, file->saved_pos);
-	} else {
-    	    result = file->db[file->dbi]->seq(file->db[file->dbi], &key, &data, flag);
+      }
+      if (file->exact_match != 0 && (key.size - 1) != len) {
+	/* not matched */
+	result = 1;
+	if (file->dbi < file->dbc) {
+	  file->dbi++;
+	  next_file = 1;
+	  continue;
 	}
-	next_file = 0;
-	if (result == 1 || result == -1) {
-	    if (file->dbi < file->dbc) {
-		file->dbi++;
-		next_file = 1;
-		continue;
-	    }
-	} else if (move == MOVE_BY_KEY) {
-		/* check if retrieved key is matched */
-		len = strlen(file->key_p);
-		if (strncmp(key.data, file->key_p, len) != 0) {
-		    /* not matched */
-		    result = 1;
-		    if (file->dbi < file->dbc) {
-			file->dbi++;
-			next_file = 1;
-			continue;
-		    }
-		}
-		if (file->exact_match != 0 && (key.size - 1) != len) {
-		    /* not matched */
-		    result = 1;
-		    if (file->dbi < file->dbc) {
-			file->dbi++;
-			next_file = 1;
-			continue;
-		    }
-		}
-		/* mached */
-	}
-
-    } while (next_file);
-    
-    if (result == 1) {		/* no more key/data pairs */
-	output->key = 0;
-	return;
-    } else if (result == -1) {	/* error, errno is set    */
-	file->last_errno = errno;
-	output->key = 0;
-	return;
+      }
+      /* mached */
     }
 
-    /* here result == 0 */
+  } while (next_file);
 
-    output->key = csf_init((char *) key.data);
-    output->data = csf_init((char *) data.data);
-    output->dbi = file->dbi;
+  if (result == 1) {		/* no more key/data pairs */
+    output->dbi = -1;
+    return;
+  } else if (result == -1) {	/* error, errno is set    */
+    file->last_errno = errno;
+    output->dbi = -1;
+    return;
+  }
 
-    if (output->key == 0 || output->data == 0) {
-	csf_free(output->key);
-	csf_free(output->data);
-	output->key = 0;
-    }
+  /* here result == 0 */
+
+  output->key  = key.data;
+  output->data = data.data;
+
+  output->dbi = file->dbi;
+
+  if (output->key == 0 || output->data == 0) {
+    output->dbi = -1;
+  }
 }
-
