7dfa6938d6143325eade4320c5640fbe97c48ef3
Emmanuel Briot <briot@adacore.com>
2003-11-12 13:52:51 +0000

    Definition of contexts is now in Glide_Kernel.Contexts
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@85743 936e1b1b-40f2-da11-902a-00137254ae57

--- browsers/src/browsers-projects.adb
+++ browsers/src/browsers-projects.adb
@@ -24,6 +24,7 @@ with Glib;                     use Glib;
 with Glib.Object;              use Glib.Object;
 with Glib.Xml_Int;             use Glib.Xml_Int;
 with Glide_Kernel;             use Glide_Kernel;
+with Glide_Kernel.Contexts;    use Glide_Kernel.Contexts;
 with Glide_Kernel.Modules;     use Glide_Kernel.Modules;
 with Glide_Kernel.Project;     use Glide_Kernel.Project;
 with Glide_Kernel.Preferences; use Glide_Kernel.Preferences;
