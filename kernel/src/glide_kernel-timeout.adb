e641623f35968b6f50e91ce718ddc0ff33a04502
Emmanuel Briot <briot@adacore.com>
2004-01-09 11:12:44 +0000

    (Process_Cb): Correctly display remaining output of the command in the console  when the command has finished executing. We were discarding some of the  output (for instance when executing an invalid gnatstub command, error msgs  were not visible)
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@86640 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/glide_kernel-timeout.adb
+++ kernel/src/glide_kernel-timeout.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                     Copyright (C) 2001-2003                       --
+--                     Copyright (C) 2001-2004                       --
 --                            ACT-Europe                             --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
@@ -152,6 +152,8 @@ package body Glide_Kernel.Timeout is
    exception
       when Process_Died =>
          if Data.Console /= null then
+            --  Capture all remaining output
+            Expect (Fd.all, Result, "[.\n]*$", Timeout => -1);
             Insert
               (Data.Console, Strip_CR (Expect_Out (Fd.all)), Add_LF => False);
             Highlight_Child
