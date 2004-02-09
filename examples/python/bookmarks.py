98687b12d5c518561c15a628004fa9de446a0bb8
Emmanuel Briot <briot@adacore.com>
2004-02-09 11:02:56 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@87047 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ examples/python/bookmarks.py
@@ -0,0 +1,33 @@
+# This file adds support for bookmarks in GPS
+# Missing in this very partial implementation:
+#   - saving bookmarks from one session to the next. This could be done if the
+#     locations window was saving its contents
+#   - Add known bookmarks to a menu dynamically, for easy jumps
+
+import GPS
+
+GPS.Editor.register_highlighting ("bookmarks", "lightblue")
+
+def add_bookmark ():
+   context=GPS.current_context()
+   if isinstance (context, GPS.FileContext):
+       name=GPS.MDI.input_dialog ("Name of bookmark", "Name")
+       if name != ():
+          GPS.Locations.add ("Bookmarks", context.file(),
+                             GPS.Editor.cursor_get_line (context.file().name()),
+                             GPS.Editor.cursor_get_column (context.file().name()),
+                             name[0])
+
+GPS.parse_xml ("""
+  <action name="add bookmark" >
+     <filter id="Source editor" />
+     <description>Add a bookmark for the current line</description>
+     <shell lang="python">bookmarks.add_bookmark()</shell>
+  </action>
+  <Submenu>
+     <Title>Navigate/Bookmarks</Title>
+     <menu action="add bookmark">
+        <Title>Add bookmark</Title>
+     </menu>
+  </Submenu>""")
+
