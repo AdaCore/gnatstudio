0230828b1895ad3af8d04571d7884cbb82ea19ce
Pascal Obry <obry@adacore.com>
2004-02-11 18:28:35 +0000

    (Set_Casing): New routine. (On_Set_Lower): New routine. (On_Set_Upper): New routine. (On_Set_Mixed): New routine. (On_Set_Smart_Mixed): New routine. (Casing_Contextual): Add support for case setting: lower, upper,  mixed and smart mixed. Minor reformatting.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@87085 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/casing_exceptions.adb
+++ src_editor/src/casing_exceptions.adb
@@ -18,15 +18,18 @@
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
-with Ada.Exceptions;        use Ada.Exceptions;
-with Glide_Intl;            use Glide_Intl;
-with Glide_Kernel.Modules;  use Glide_Kernel.Modules;
-with Glide_Kernel.Contexts; use Glide_Kernel.Contexts;
-with Gtk.Menu_Item;         use Gtk.Menu_Item;
-with Gtk.Widget;            use Gtk.Widget;
-with Traces;                use Traces;
-with String_Utils;          use String_Utils;
-with Case_Handling;         use Case_Handling;
+with Ada.Exceptions;          use Ada.Exceptions;
+with Ada.Characters.Handling; use Ada.Characters.Handling;
+with Glide_Intl;              use Glide_Intl;
+with Glide_Kernel.Modules;    use Glide_Kernel.Modules;
+with Glide_Kernel.Contexts;   use Glide_Kernel.Contexts;
+with Glide_Kernel.Scripts;    use Glide_Kernel.Scripts;
+with Gtk.Menu_Item;           use Gtk.Menu_Item;
+with Gtk.Widget;              use Gtk.Widget;
+with Traces;                  use Traces;
+with String_Utils;            use String_Utils;
+with Case_Handling;           use Case_Handling;
+with VFS;                     use VFS;
 
 package body  Casing_Exceptions is
 
@@ -45,6 +48,31 @@ package body  Casing_Exceptions is
      (Widget  : access GObject_Record'Class;
       Context : Selection_Context_Access);
    --  Callback for the "Remove case exception for" contextual menu
+
+   procedure Set_Casing
+     (Context  : Entity_Selection_Context_Access;
+      New_Name : String);
+   --  Function used by the following callbacks to set the casing
+
+   procedure On_Set_Lower
+     (Widget  : access GObject_Record'Class;
+      Context : Selection_Context_Access);
+   --  Callback for the "Lower" contextual menu
+
+   procedure On_Set_Upper
+     (Widget  : access GObject_Record'Class;
+      Context : Selection_Context_Access);
+   --  Callback for the "Upper" contextual menu
+
+   procedure On_Set_Mixed
+     (Widget  : access GObject_Record'Class;
+      Context : Selection_Context_Access);
+   --  Callback for the "Mixed" contextual menu
+
+   procedure On_Set_Smart_Mixed
+     (Widget  : access GObject_Record'Class;
+      Context : Selection_Context_Access);
+   --  Callback for the "Smart_Mixed" contextual menu
 
    -------------------
    -- Add_Exception --
@@ -111,6 +139,108 @@ package body  Casing_Exceptions is
       when E : others =>
          Trace (Me, "Unexpected exception: " & Exception_Information (E));
    end On_Remove_Case_Exception;
+
+   ----------------
+   -- Set_Casing --
+   ----------------
+
+   procedure Set_Casing
+     (Context  : Entity_Selection_Context_Access;
+      New_Name : String)
+   is
+      File   : constant Virtual_File := Contexts.File_Information (Context);
+      Line   : constant Integer      := Contexts.Line_Information (Context);
+      Column : constant Integer      := Entity_Column_Information (Context);
+   begin
+      Execute_GPS_Shell_Command
+        (Kernel  => Get_Kernel (Context),
+         Command => "Editor.replace_text " &
+           Full_Name (File).all &
+           Integer'Image (Line) &
+           Integer'Image (Column) &
+           ' ' & New_Name &
+           " 0 " & Integer'Image (New_Name'Length));
+   end Set_Casing;
+
+   ------------------
+   -- On_Set_Lower --
+   ------------------
+
+   procedure On_Set_Lower
+     (Widget  : access GObject_Record'Class;
+      Context : Selection_Context_Access)
+   is
+      pragma Unreferenced (Widget);
+      C      : constant Entity_Selection_Context_Access :=
+        Entity_Selection_Context_Access (Context);
+      Name   : constant String       := Entity_Name_Information (C);
+   begin
+      Set_Casing (C, To_Lower (Name));
+   exception
+      when E : others =>
+         Trace (Me, "Unexpected exception: " & Exception_Information (E));
+   end On_Set_Lower;
+
+   ------------------
+   -- On_Set_Upper --
+   ------------------
+
+   procedure On_Set_Upper
+     (Widget  : access GObject_Record'Class;
+      Context : Selection_Context_Access)
+   is
+      pragma Unreferenced (Widget);
+      C      : constant Entity_Selection_Context_Access :=
+        Entity_Selection_Context_Access (Context);
+      Name   : constant String       := Entity_Name_Information (C);
+   begin
+      Set_Casing (C, To_Upper (Name));
+   exception
+      when E : others =>
+         Trace (Me, "Unexpected exception: " & Exception_Information (E));
+   end On_Set_Upper;
+
+   ------------------
+   -- On_Set_Mixed --
+   ------------------
+
+   procedure On_Set_Mixed
+     (Widget  : access GObject_Record'Class;
+      Context : Selection_Context_Access)
+   is
+      pragma Unreferenced (Widget);
+      C        : constant Entity_Selection_Context_Access :=
+        Entity_Selection_Context_Access (Context);
+      Name     : constant String       := Entity_Name_Information (C);
+      New_Name : String (Name'Range)   := Name;
+   begin
+      Mixed_Case (New_Name);
+      Set_Casing (C, New_Name);
+   exception
+      when E : others =>
+         Trace (Me, "Unexpected exception: " & Exception_Information (E));
+   end On_Set_Mixed;
+
+   ------------------------
+   -- On_Set_Smart_Mixed --
+   ------------------------
+
+   procedure On_Set_Smart_Mixed
+     (Widget  : access GObject_Record'Class;
+      Context : Selection_Context_Access)
+   is
+      pragma Unreferenced (Widget);
+      C        : constant Entity_Selection_Context_Access :=
+        Entity_Selection_Context_Access (Context);
+      Name     : constant String       := Entity_Name_Information (C);
+      New_Name : String (Name'Range)   := Name;
+   begin
+      Smart_Mixed_Case (New_Name);
+      Set_Casing (C, New_Name);
+   exception
+      when E : others =>
+         Trace (Me, "Unexpected exception: " & Exception_Information (E));
+   end On_Set_Smart_Mixed;
 
    ----------------------
    -- Casing_Customize --
@@ -185,10 +315,45 @@ package body  Casing_Exceptions is
       Append (Menu, Menu_Item);
 
       declare
-         Name : constant String :=
+         Name     : constant String :=
            Krunch (Entity_Name_Information (Selection));
+         New_Name : String (Name'Range);
       begin
-         --  ??? Add menu entries to set the entity to Upper/Lower/Miwed case
+         Gtk_New (Menu_Item, -"Lower " & To_Lower (Name));
+         Add (Submenu, Menu_Item);
+         Context_Callback.Connect
+           (Menu_Item, "activate",
+            Context_Callback.To_Marshaller (On_Set_Lower'Access),
+            User_Data   => Selection_Context_Access (Context));
+
+         Gtk_New (Menu_Item, -"Upper " & To_Upper (Name));
+         Add (Submenu, Menu_Item);
+         Context_Callback.Connect
+           (Menu_Item, "activate",
+            Context_Callback.To_Marshaller (On_Set_Upper'Access),
+            User_Data   => Selection_Context_Access (Context));
+
+         New_Name := Name;
+         Mixed_Case (New_Name);
+         Gtk_New (Menu_Item, -"Mixed " & New_Name);
+         Add (Submenu, Menu_Item);
+         Context_Callback.Connect
+           (Menu_Item, "activate",
+            Context_Callback.To_Marshaller (On_Set_Mixed'Access),
+            User_Data   => Selection_Context_Access (Context));
+
+         New_Name := Name;
+         Smart_Mixed_Case (New_Name);
+         Gtk_New (Menu_Item, -"Smart Mixed " & New_Name);
+         Add (Submenu, Menu_Item);
+         Context_Callback.Connect
+           (Menu_Item, "activate",
+            Context_Callback.To_Marshaller (On_Set_Smart_Mixed'Access),
+            User_Data   => Selection_Context_Access (Context));
+
+         Gtk_New (Menu_Item);
+         Append (Submenu, Menu_Item);
+
          Gtk_New (Menu_Item, -"Add exception for " & Name);
          Add (Submenu, Menu_Item);
          Context_Callback.Connect
