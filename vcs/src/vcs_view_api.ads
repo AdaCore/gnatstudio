fbb82c29fdac2b723ee63e5487c793f05f73f255
Nicolas Setton <setton@adacore.com>
2004-02-18 12:55:12 +0000

    (On_Menu_Diff2): New subprogram.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@87218 936e1b1b-40f2-da11-902a-00137254ae57

--- vcs/src/vcs_view_api.ads
+++ vcs/src/vcs_view_api.ads
@@ -82,6 +82,10 @@ package VCS_View_API is
      (Widget  : access GObject_Record'Class;
       Context : Selection_Context_Access);
 
+   procedure On_Menu_Diff2
+     (Widget  : access GObject_Record'Class;
+      Context : Selection_Context_Access);
+
    procedure On_Menu_View_Log
      (Widget  : access GObject_Record'Class;
       Context : Selection_Context_Access);
