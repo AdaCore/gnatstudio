3ec71d8d69e0609a48a3529733b12db0d193c2cc
Nicolas Setton <setton@adacore.com>
2004-03-04 17:36:28 +0000

    (Codefix_Add_Command): New command type. (Execute, Free, Progress, Name): New subprograms.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@87429 936e1b1b-40f2-da11-902a-00137254ae57

--- codefix/src/commands-codefix.ads
+++ codefix/src/commands-codefix.ads
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                        Copyright (C) 2003                         --
+--                     Copyright (C) 2003-2004                       --
 --                            ACT-Europe                             --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
@@ -31,9 +31,25 @@ package Commands.Codefix is
       Kernel       : Kernel_Handle;
    end record;
 
+   type Codefix_Add_Command is new Root_Command with record
+      Kernel        : Kernel_Handle;
+      Session       : Codefix_Module.Codefix_Session;
+      Current_Error : Error_Id;
+      Errors_Num    : Natural;
+      Errors_Fixed  : Natural := 0;
+   end record;
+
    function Execute
      (Command : access Codefix_Command) return Command_Return_Type;
    --  Fix the error recorded in the Codefix_Command.
+
+   function Execute
+     (Command : access Codefix_Add_Command) return Command_Return_Type;
+   procedure Free (Command : in out Codefix_Add_Command);
+   function Progress
+     (Command : access Codefix_Add_Command) return Progress_Record;
+   function Name (Command : access Codefix_Add_Command) return String;
+   --  See Commands for a description of overloaded subprograms.
 
    function Undo (Command : access Codefix_Command) return Boolean;
    --  Unfix the error recored.
