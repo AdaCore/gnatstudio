cf63c3bd5b5dbf5177d0d34e3b5142ced78780b7
Emmanuel Briot <briot@adacore.com>
2004-04-27 13:44:53 +0000

    (Process_Entity_Ref): Only store the end-of-scope information if the current  LI is the one where the full info for the entity is visible, to avoid  duplicate information in the LI structures. No change in functionality,  since entities-queries.adb always parses the right ALI file first.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@88007 936e1b1b-40f2-da11-902a-00137254ae57

--- ada_module/src/ali_parser.adb
+++ ada_module/src/ali_parser.adb
@@ -749,7 +749,12 @@ package body ALI_Parser is
                       Column => Column_Type (Xref.Table (Current_Ref).Col));
 
          if Is_End_Reference (Kind) then
+            --  Only insert the end-of-scope is we are parsing the ALI file
+            --  for the file that contains this end-of-scope. Otherwise, we
+            --  get duplicate references.
+            if Get_LI (Location.File) = LI then
                Set_End_Of_Scope (Entity, Location, Kind);
+            end if;
 
          elsif Kind = Primitive_Operation
            or else Kind = Overriding_Primitive_Operation
