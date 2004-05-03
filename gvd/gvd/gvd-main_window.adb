f8be04b324d3d064039bc949e60dd7b5cccb0f9c
Arnaud Charlet <charlet@adacore.com>
2004-05-03 14:34:27 +0000

    Complete previous check-in.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/branches/gps-2_0-branch@88069 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/gvd-main_window.adb
+++ gvd/gvd/gvd-main_window.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                   GVD - The GNU Visual Debugger                   --
 --                                                                   --
---                      Copyright (C) 2000-2003                      --
+--                      Copyright (C) 2000-2004                      --
 --                              ACT-Europe                           --
 --                                                                   --
 -- GVD is free  software;  you can redistribute it and/or modify  it --
