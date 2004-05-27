9bbdb95d127324348c1ca172cc406dfbea6b01b1
Arnaud Charlet <charlet@adacore.com>
2004-05-27 07:52:21 +0000

    Fix style error and update copyright, as reported by gnat and ci_check.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@88212 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/external_editor_module.adb
+++ src_editor/src/external_editor_module.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                     Copyright (C) 2001-2003                       --
+--                     Copyright (C) 2001-2004                       --
 --                            ACT-Europe                             --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
@@ -341,7 +341,7 @@ package body External_Editor_Module is
                if Path /= null then
                   Free (Path);
                   Match := True;
-                  end if;
+               end if;
             end if;
 
             Free (Args.all);
