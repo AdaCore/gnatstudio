bb8c4564f76274579e0ceda25cac9bc347a56d22
Emmanuel Briot <briot@adacore.com>
2004-06-14 19:33:27 +0000

    Initial revision: Add support for using XML/Ada has a parser instead of Glib.XM
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@88323 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ gvd/common/xml_parsers.ads
@@ -0,0 +1,33 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                     Copyright (C) 2003-2004                       --
+--                            ACT-Europe                             --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Glib.Xml_Int;
+with GNAT.OS_Lib;
+
+package XML_Parsers is
+
+   procedure Parse
+     (File  : String;
+      Tree  : out Glib.Xml_Int.Node_Ptr;
+      Error : out GNAT.OS_Lib.String_Access);
+   --  Parse File, and return a pointer to the resulting Tree.
+   --  The resulting value must be freed by the user
+
+end XML_Parsers;
