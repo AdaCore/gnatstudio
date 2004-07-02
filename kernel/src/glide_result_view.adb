39a1ecb6e5ad9874d1c98a0ea657365d62852099
Nicolas Setton <setton@adacore.com>
2004-07-02 15:54:20 +0000

    (Add_Location): When scrolling automatically to the first item in a  category, do not specify an alignment, so that the item is fully displayed,  whatever the size of the locations view.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@88455 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/glide_result_view.adb
+++ kernel/src/glide_result_view.adb
@@ -1051,7 +1051,7 @@ package body Glide_Result_View is
 
             Path := Get_Path (Model, Iter);
             Select_Path (Get_Selection (View.Tree), Path);
-            Scroll_To_Cell (View.Tree, Path, null, True, 0.1, 0.1);
+            Scroll_To_Cell (View.Tree, Path, null, False, 0.1, 0.1);
             Path_Free (Path);
 
             if not Quiet
