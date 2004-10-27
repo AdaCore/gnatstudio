868fd8083b54d26f1cc8917f0730ab803d8f8c89
Arnaud Charlet <charlet@adacore.com>
2004-10-27 15:44:36 +0000

    Add ??? about commented out code that needs clarification.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@89421 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/src_editor_view.adb
+++ src_editor/src/src_editor_view.adb
@@ -150,6 +150,8 @@ package body Src_Editor_View is
    function Key_Press_Event_Cb
      (Widget : access Gtk_Widget_Record'Class;
       Event  : Gdk_Event) return Boolean;
+
+--  ??? Why is the following code and related code in this unit commented out
 --     function Key_Release_Event_Cb
 --       (Widget : access Gtk_Widget_Record'Class;
 --        Event  : Gdk_Event) return Boolean;
