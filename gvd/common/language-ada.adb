a9ac5b7ae7c1f4e1aab14d9e5422acbcd30412da
Arnaud Charlet <charlet@adacore.com>
2005-02-04 13:53:16 +0000

    Update copyirght notice.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@90278 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/common/language-ada.adb
+++ gvd/common/language-ada.adb
@@ -1,8 +1,8 @@
 -----------------------------------------------------------------------
 --                   GVD - The GNU Visual Debugger                   --
 --                                                                   --
---                      Copyright (C) 2000-2004                      --
---                              ACT-Europe                           --
+--                      Copyright (C) 2000-2005                      --
+--                              AdaCore                              --
 --                                                                   --
 -- GVD is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
