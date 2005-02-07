189016377cf27fec10828633ca7b92460d5fd6e7
Arnaud Charlet <charlet@adacore.com>
2005-02-07 14:38:38 +0000

    (Bg_Color): Change to white, since the new AdaCore logo is using a white  background.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@90301 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/src/logo_boxes.adb
+++ prj_editor/src/logo_boxes.adb
@@ -1,8 +1,8 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                     Copyright (C) 2001-2002                       --
---                            ACT-Europe                             --
+--                     Copyright (C) 2001-2005                       --
+--                              AdaCore                              --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -34,7 +34,7 @@ with Pixmaps_Prj;   use Pixmaps_Prj;
 
 package body Logo_Boxes is
 
-   Bg_Color : constant String := "#0e79bd";
+   Bg_Color : constant String := "#ffffff";
 
    -------------
    -- Gtk_New --
