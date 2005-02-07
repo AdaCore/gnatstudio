3eb426a2a2a8834306f45646428639a51b12895f
Nicolas Setton <setton@adacore.com>
2005-02-07 17:53:28 +0000

    Initial revision. For E207-013.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@90309 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ kernel/src/welcome_page.adb
@@ -0,0 +1,204 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                         Copyright (C) 2005                        --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Glib.Object; use Glib.Object;
+with Gdk.Color;   use Gdk.Color;
+with Gtk.Button;  use Gtk.Button;
+with Gtk.Enums;   use Gtk.Enums;
+with Gtk.Image;   use Gtk.Image;
+with Gtk.Label;   use Gtk.Label;
+with Gtk.Widget;  use Gtk.Widget;
+
+with VFS;         use VFS;
+with Traces;      use Traces;
+with Glide_Intl;  use Glide_Intl;
+with Glide_Kernel.Standard_Hooks; use Glide_Kernel.Standard_Hooks;
+
+with GNAT.OS_Lib;    use GNAT.OS_Lib;
+with Ada.Exceptions; use Ada.Exceptions;
+
+package body Welcome_Page is
+
+   procedure On_Overview
+     (Widget : access GObject_Record'Class; Kernel : Kernel_Handle);
+   --  Display the overview page.
+
+   procedure On_Tutorial
+     (Widget : access GObject_Record'Class; Kernel : Kernel_Handle);
+   --  Display the overview page.
+
+   procedure On_UG
+     (Widget : access GObject_Record'Class; Kernel : Kernel_Handle);
+   --  Display the overview page.
+
+   -----------------
+   -- On_Overview --
+   -----------------
+
+   procedure On_Overview
+     (Widget : access GObject_Record'Class; Kernel : Kernel_Handle)
+   is
+      pragma Unreferenced (Widget);
+   begin
+      Open_Html (Kernel, Create_From_Base ("gps-welcome.html"));
+
+   exception
+      when E : others =>
+         Trace (Exception_Handle,
+                "Unexpected exception: " & Exception_Information (E));
+   end On_Overview;
+
+   -----------------
+   -- On_Tutorial --
+   -----------------
+
+   procedure On_Tutorial
+     (Widget : access GObject_Record'Class; Kernel : Kernel_Handle)
+   is
+      pragma Unreferenced (Widget);
+   begin
+      Open_Html (Kernel, Create_From_Base ("gps-tutorial.html"));
+
+   exception
+      when E : others =>
+         Trace (Exception_Handle,
+                "Unexpected exception: " & Exception_Information (E));
+   end On_Tutorial;
+
+   -----------
+   -- On_UG --
+   -----------
+
+   procedure On_UG
+     (Widget : access GObject_Record'Class; Kernel : Kernel_Handle)
+   is
+      pragma Unreferenced (Widget);
+   begin
+      Open_Html (Kernel, Create_From_Base ("gps.html"));
+
+   exception
+      when E : others =>
+         Trace (Exception_Handle,
+                "Unexpected exception: " & Exception_Information (E));
+   end On_UG;
+
+   --------------------------
+   -- Display_Welcome_Page --
+   --------------------------
+
+   procedure Display_Welcome_Page (Kernel : Kernel_Handle) is
+      Child : MDI_Child;
+   begin
+      Child := Find_MDI_Child_By_Tag
+        (Get_MDI (Kernel), Welcome_Page_Record'Tag);
+
+      if Child = null then
+         Child := Create_Welcome_Page (Kernel);
+      end if;
+
+      Raise_Child (Child);
+   end Display_Welcome_Page;
+
+   -------------------------
+   -- Create_Welcome_Page --
+   -------------------------
+
+   function Create_Welcome_Page (Kernel : Kernel_Handle) return MDI_Child is
+      Main_Box : Welcome_Page_Access;
+      Hbox     : Gtk_Hbox;
+      Vbox     : Gtk_Vbox;
+      Button   : Gtk_Button;
+      Label    : Gtk_Label;
+      Child    : MDI_Child;
+      Image    : Gtk_Image;
+
+      Pics_Dir : constant String := Get_System_Dir (Kernel)
+        & "doc" & Directory_Separator & "gps" & Directory_Separator
+        & "html" & Directory_Separator;
+   begin
+      Main_Box := new Welcome_Page_Record;
+      Initialize_Vbox (Main_Box);
+
+      Gtk_New (Image, Pics_Dir & "gps_title.jpg");
+      Pack_Start (Main_Box, Image);
+
+      Gtk_New_Hbox (Hbox, Homogeneous => True, Spacing => 3);
+      Pack_Start (Main_Box, Hbox);
+
+      Gtk_New
+        (Label,
+         -("GPS is a complete integrated development environment that gives"
+           & " access to a wide range of tools and integrates them smoothly,"
+           & " unleashing the full power of GNAT Pro."));
+      Set_Line_Wrap (Label, True);
+      Pack_Start (Hbox, Label);
+
+      Gtk_New_Hbox (Hbox, Homogeneous => True, Spacing => 3);
+      Pack_Start (Main_Box, Hbox);
+
+      Gtk_New_Vbox (Vbox);
+      Pack_Start (Hbox, Vbox, False, False, 3);
+
+      Gtk_New (Button, "");
+      Gtk_New (Image, Pics_Dir & "overview.png");
+      Add (Button, Image);
+      Set_Relief (Button, Relief_None);
+      Pack_Start (Vbox, Button, False, False);
+      Kernel_Callback.Connect (Button, "clicked", On_Overview'Access, Kernel);
+      Gtk_New (Label, -"Overview");
+      Pack_Start (Vbox, Label, False, False);
+
+      Gtk_New_Vbox (Vbox);
+      Pack_Start (Hbox, Vbox, False, False, 3);
+
+      Gtk_New (Button, "");
+      Gtk_New (Image, Pics_Dir & "tutorial.png");
+      Add (Button, Image);
+      Set_Relief (Button, Relief_None);
+      Pack_Start (Vbox, Button, False, False);
+      Kernel_Callback.Connect (Button, "clicked", On_Tutorial'Access, Kernel);
+      Gtk_New (Label, -"Tutorial");
+      Pack_Start (Vbox, Label, False, False);
+
+      Gtk_New_Vbox (Vbox);
+      Pack_Start (Hbox, Vbox, False, False, 3);
+
+      Gtk_New (Button, "");
+      Gtk_New (Image, Pics_Dir & "user_guide.png");
+      Add (Button, Image);
+      Set_Relief (Button, Relief_None);
+      Pack_Start (Vbox, Button, False, False);
+      Kernel_Callback.Connect (Button, "clicked", On_UG'Access, Kernel);
+      Gtk_New (Label, -"User's Guide");
+      Pack_Start (Vbox, Label, False, False);
+
+      Show_All (Main_Box);
+
+      Child := Put (Get_MDI (Kernel), Main_Box);
+
+      Modify_Bg (Get_Parent (Main_Box),
+                 State_Normal, White (Get_Default_Colormap));
+
+      Set_Title (Child, -"Welcome to GPS");
+
+      return Child;
+   end Create_Welcome_Page;
+
+end Welcome_Page;
---
+++ kernel/src/welcome_page.ads
@@ -0,0 +1,37 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                         Copyright (C) 2005                        --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Glide_Kernel; use Glide_Kernel;
+with Gtk.Box;      use Gtk.Box;
+with Gtkada.MDI;   use Gtkada.MDI;
+
+package Welcome_Page is
+
+   type Welcome_Page_Record is new Gtk_Vbox_Record with null record;
+   type Welcome_Page_Access is access all Welcome_Page_Record'Class;
+
+   procedure Display_Welcome_Page (Kernel : Kernel_Handle);
+   --  Display the welcome page in the central GPS dialog.
+
+   function Create_Welcome_Page (Kernel : Kernel_Handle) return MDI_Child;
+   --  Internal function to create the welcome page.
+
+end Welcome_Page;
+
