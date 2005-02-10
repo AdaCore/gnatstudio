1aecad06a9ee5dc90f0dfb73650510cd1fe1d3a3
Arnaud Charlet <charlet@adacore.com>
2005-02-10 16:13:45 +0000

    Improve implementation by trying a bunch of known browsers in case the  preference is set to "" (now the default). Part of E207-013.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@90402 936e1b1b-40f2-da11-902a-00137254ae57

--- help/src/help_module-display_help_unix.adb
+++ help/src/help_module-display_help_unix.adb
@@ -25,12 +25,50 @@ procedure Display_Help
   (Kernel    : access Kernel_Handle_Record'Class;
    Help_File : VFS.Virtual_File)
 is
-   Args    : Argument_List_Access;
-   File    : GNAT.OS_Lib.String_Access;
-   Cmd     : GNAT.OS_Lib.String_Access;
-   Process : Process_Id;
-   Browser : constant String :=
+   HTML_Browser : constant String :=
      Get_Pref (Kernel, GVD.Preferences.Html_Browser);
+   Args         : Argument_List_Access;
+
+   type Cst_String_Access is access constant String;
+   type Browser_List is array (Natural range <>) of Cst_String_Access;
+
+   Browsers : constant Browser_List :=
+     (new String'("firefox"),
+      new String'("mozilla"),
+      new String'("galeon"),
+      new String'("netscape"),
+      new String'("opera"),
+      new String'("nautilus"),
+      new String'("konqueror"));
+
+   function Launch_Browser
+     (Browser : String;
+      Args    : Argument_List := (1 .. 0 => null)) return Boolean;
+   --  Launch the given browser.
+   --  Return True in case of success, false otherwise.
+
+   function Launch_Browser
+     (Browser : String;
+      Args    : Argument_List := (1 .. 0 => null)) return Boolean
+   is
+      Cmd     : GNAT.OS_Lib.String_Access;
+      File    : GNAT.OS_Lib.String_Access;
+      Process : Process_Id;
+
+   begin
+      Cmd := Locate_Exec_On_Path (Browser);
+
+      if Cmd = null then
+         return False;
+      else
+         File := new String'(Full_Name (Help_File, True).all);
+         Process := Non_Blocking_Spawn (Cmd.all, Args & (1 => File));
+         Free (Cmd);
+         Free (File);
+
+         return Process /= Invalid_Pid;
+      end if;
+   end Launch_Browser;
 
 begin
    if not Is_Regular_File (Help_File) then
@@ -39,40 +77,27 @@ begin
               Mode => Error);
    end if;
 
-   if Browser = "" then
-      --  ??? look for the following:
-      --  firefox, mozilla, galeon, netscape, opera, nautilus, konqueror
+   if HTML_Browser = "" then
+      for J in Browsers'Range loop
+         if Launch_Browser (Browsers (J).all) then
+            return;
+         end if;
+      end loop;
 
       Insert (Kernel, -"No HTML browser specified", Mode => Error);
       return;
    end if;
 
-   Args := Argument_String_To_List (Browser);
-   Cmd := Locate_Exec_On_Path
-            (Unprotect (Protect (Args (Args'First).all, False)));
+   Args := Argument_String_To_List (HTML_Browser);
 
-   if Cmd = null then
+   if not Launch_Browser
+     (Unprotect (Protect (Args (Args'First).all, False)),
+      Args (Args'First + 1 .. Args'Last))
+   then
       Insert
         (Kernel,
-         -"Could not locate HTML browser on path: "
-         & Args (Args'First).all,
+         -"Could not launch HTML browser: " & Args (Args'First).all,
          Mode => Error);
-
-   else
-      File := new String'(Full_Name (Help_File, True).all);
-      Process :=
-        Non_Blocking_Spawn
-          (Cmd.all, Args (Args'First + 1 .. Args'Last) & (1 => File));
-
-      if Process = Invalid_Pid then
-         Insert
-           (Kernel,
-            -"Could not launch HTML browser: " & Args (Args'First).all,
-            Mode => Error);
-      end if;
-
-      Free (Cmd);
-      Free (File);
    end if;
 
    Free (Args);
