4c79c983b96902368ec86b177573da350e48dce9
Emmanuel Briot <briot@adacore.com>
2005-02-14 17:19:26 +0000

    Minor trace update
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@90470 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/language_handlers-glide.adb
+++ kernel/src_info/language_handlers-glide.adb
@@ -1,8 +1,8 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                      Copyright (C) 2002-2004                      --
---                            ACT-Europe                             --
+--                      Copyright (C) 2002-2005                      --
+--                            AdaCore                                --
 --                                                                   --
 -- GPS is free  software; you can  redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -363,7 +363,7 @@ package body Language_Handlers.Glide is
          if Index /= 0 then
             Trace (Me, "No LI_Handler for language "
                    & Full_Name (Source_Filename).all
-                   & " Index=" & Index'Img);
+                   & " Index=" & Index'Img & " lang=" & Lang);
          end if;
 
          return null;
