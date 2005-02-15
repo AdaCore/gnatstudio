bd25de4e0b1883be8be9545a8e426e93ef51cdb9
Emmanuel Briot <briot@adacore.com>
2005-02-15 17:05:53 +0000

    Fix copyright notice in previous check in
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@90529 936e1b1b-40f2-da11-902a-00137254ae57

--- codefix/src/commands-codefix.adb
+++ codefix/src/commands-codefix.adb
@@ -1,8 +1,8 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                     Copyright (C) 2001-2004                       --
---                            ACT-Europe                             --
+--                     Copyright (C) 2001-2005                       --
+--                            AdaCore                                --
 --                                                                   --
 -- GPS is free  software; you can  redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
