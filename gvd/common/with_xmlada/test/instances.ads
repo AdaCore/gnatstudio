0e0be9951592e3c695d62abec11f091f394569bf
Arnaud Charlet <charlet@adacore.com>
2005-02-24 14:49:58 +0000

    Merge gvd_common and common
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@90760 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/common/with_xmlada/test/instances.ads
+++
@@ -1,7 +0,0 @@
-with XML_Gtk.Readers;
-with Glib.XML;
-
-package Instances is
-   package XML_Int is new Glib.XML (Integer);
-   package Gtk_Readers is new XML_Gtk.Readers (Integer, 0, XML_Int);
-end Instances;
