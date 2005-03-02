f7c609c705c271efe7953f84057923b1d91a900f
Arnaud Charlet <charlet@adacore.com>
2005-03-02 13:08:23 +0000

    Update comments (Odd -> GVD)
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@90921 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/items.ads
+++ gvd/gvd/items.ads
@@ -1,10 +1,10 @@
 -----------------------------------------------------------------------
---                   GVD - The GNU Visual Debugger                   --
+--                               G P S                               --
 --                                                                   --
---                      Copyright (C) 2000-2003                      --
---                              ACT-Europe                           --
+--                      Copyright (C) 2000-2005                      --
+--                              AdaCore                              --
 --                                                                   --
--- GVD is free  software;  you can redistribute it and/or modify  it --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
 -- the Free Software Foundation; either version 2 of the License, or --
 -- (at your option) any later version.                               --
@@ -32,7 +32,7 @@ with Basic_Types;
 
 package Items is
 
-   --  Description of the types and values that are parsed by Odd.
+   --  Description of the types and values that are parsed by GVD.
    --
    --  When a user wants to display an item in the canvas, its type is
    --  first parsed, and then the value itself is parsed.
