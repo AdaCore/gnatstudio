b570bb087de4aa812396e02ca77d849c3e649c1e
Pascal Obry <obry@adacore.com>
2005-03-02 14:46:10 +0000

    (Entities_Kind): Moved here from docgen backend. (Templates_Set): New type. (Output_Description): Add field Entities_Templates used to record the template  filename to use for each entity. Add some comments.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@90923 936e1b1b-40f2-da11-902a-00137254ae57

--- docgen/src/docgen_registry.ads
+++ docgen/src/docgen_registry.ads
@@ -18,17 +18,47 @@
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
+--  This unit records the docgen format descriptions. It is used by the docgen
+--  backend.
+
 package Docgen_Registry is
 
    type Output_Type is (Text, Binary);
+   --  Text   : the output format is text-based, the rendering is done through
+   --           a set of template files.
+   --  Binary : a binary output format, the rendering is done internally.
+
+   type Entities_Kind is
+     (File_Header_Kind, File_Footer_Kind,
+      Comment_Kind, Keyword_Kind, String_Kind, Char_Kind,
+      Subtitle_Kind, Package_Desc_Kind, Package_Kind, With_Kind,
+      Variable_Kind, Exception_Kind, Type_Kind, Tagged_Type_Kind,
+      Calls_References_Kind, Caller_References_Kind,
+      Entity_Kind, Subprogram_Kind, Header_Kind, Footer_Kind,
+      Header_Private_Kind, Main_Frame_Kind,
+      Unit_Index_Kind, Subprogram_Index_Kind, Index_Tagged_Type_Kind,
+      Type_Index_Kind, Tagged_Type_Index_Kind, Index_Item_Kind,
+      Private_Index_Kind, Public_Index_Kind, End_Index_Kind, Block_Kind,
+      Link_Kind, Description_Kind);
+   --  The entities supported by the textual docgen backend, for each one a
+   --  separate template file will be used for the final rendering.
 
    type String_Access is access String;
 
+   type Templates_Set is array (Entities_Kind) of String_Access;
+
    type Output_Description is record
-      Format      : Output_Type;
-      Description : String_Access;
-      Extension   : String_Access;
+      Format             : Output_Type;
+      Name               : String_Access;
+      --  The name of the backend used dialogs
+      Description        : String_Access;
+      --  A description for this backend
+      Extension          : String_Access;
+      --  The extension to be used for the generated files
+      Entities_Templates : Templates_Set;
+      --  The templates files for each entity
    end record;
+   --  Contains all the output information
    type Output_Description_Access is access Output_Description;
 
    procedure Insert (O : Output_Description);
