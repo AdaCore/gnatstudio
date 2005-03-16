86252480a6627f289cfb44a2f95550da24977bba
Emmanuel Briot <briot@adacore.com>
2005-03-16 10:10:37 +0000

    Update copyright date (should have been done earlier)
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@91120 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/src/creation_wizard.adb
+++ prj_editor/src/creation_wizard.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                     Copyright (C) 2004                            --
+--                     Copyright (C) 2004-2005                       --
 --                            AdaCore                                --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
