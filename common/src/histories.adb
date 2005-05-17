052cd666e4a2d6c72542290b29618b38202795a2
Arnaud Charlet <charlet@adacore.com>
2005-05-17 13:57:12 +0000

    (On_Changed): Replace Shorten by Krunch, which is more appropriate after  all. Motivated by D401-008.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@92174 936e1b1b-40f2-da11-902a-00137254ae57

--- common/src/histories.adb
+++ common/src/histories.adb
@@ -37,7 +37,7 @@ with Gtk.Widget;    use Gtk.Widget;
 with Gtk.Toggle_Button; use Gtk.Toggle_Button;
 with Traces;        use Traces;
 with GUI_Utils;     use GUI_Utils;
-with File_Utils;    use File_Utils;
+with String_Utils;  use String_Utils;
 
 package body Histories is
 
@@ -705,7 +705,7 @@ package body Histories is
                Path  : constant String := Value (V).all;
                Mitem : Full_Path_Menu_Item;
             begin
-               Gtk_New (Mitem, Shorten (Path), Path);
+               Gtk_New (Mitem, Krunch (Path, 60), Path);
                Append (Menu, Mitem);
 
                if Notifier.Callback /= null then
