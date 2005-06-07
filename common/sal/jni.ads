f2e93b9d6cf0cf9558f82a86080fbb2cbd6a5d21
Quentin Ochem <ochem@adacore.com>
2005-06-07 22:11:01 +0000

    Added wrappers to allow calls of java functions.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@92307 936e1b1b-40f2-da11-902a-00137254ae57

--- common/sal/jni.ads
+++ common/sal/jni.ads
@@ -8,6 +8,8 @@ package JNI is
    type Jstring is new System.Address;
    type Jobject is new System.Address;
    type JNIEnv is new System.Address;
+   type Jclass is new System.Address;
+   type JmethodID is new System.Address;
    type Jbyte is new Interfaces.C.unsigned_char;
 
    function GetStringUTFChars (Env : JNIEnv; String : Jstring) return
@@ -24,6 +26,23 @@ package JNI is
      (Env : JNIEnv;
       Str : Interfaces.C.Strings.chars_ptr) return Jstring;
    pragma Import (C, NewStringUTF, "W_NewStringUTF");
+
+   function GetMethodID
+     (Env     : JNIEnv;
+      Class   : Jclass;
+      Name    : Interfaces.C.Strings.chars_ptr;
+      Profile : Interfaces.C.Strings.chars_ptr) return JmethodID;
+   pragma Import (C, GetMethodID, "W_GetMethodID");
+
+   procedure CallVoidMethodIIIS
+    (Env    : JNIEnv;
+     Object : Jobject;
+     Id     : JmethodID;
+     P1     : Jint;
+     P2     : Jint;
+     P3     : Jint;
+     P4     : Jstring);
+   pragma Import (C, CallVoidMethodIIIS, "W_CallVoidMethodIIIS");
 
 end JNI;
 
