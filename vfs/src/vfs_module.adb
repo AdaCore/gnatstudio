67b898ca43361ca9c76c505f62d934a836a54fb9
Arnaud Charlet <charlet@adacore.com>
2005-06-30 07:47:46 +0000

    (Register_Module): Disable delete contextual menus, activated unintentionally. Found by E630-002.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/branches/gps-3_0-branch@92500 936e1b1b-40f2-da11-902a-00137254ae57

--- vfs/src/vfs_module.adb
+++ vfs/src/vfs_module.adb
@@ -256,7 +256,7 @@ package body VFS_Module is
    procedure Register_Module
      (Kernel : access GPS.Kernel.Kernel_Handle_Record'Class)
    is
-      Command : Interactive_Command_Access;
+      --  Command : Interactive_Command_Access;
    begin
       VFS_Module_Id := new Module_ID_Record;
 
@@ -266,15 +266,15 @@ package body VFS_Module is
          Module_Name => VFS_Module_Name,
          Priority    => Default_Priority);
 
-      Command := new Delete_Command;
-      Register_Contextual_Menu
-        (Kernel, "Delete file",
-         Action => Command,
-         Filter => Lookup_Filter (Kernel, "Explorer_File_Node"));
-      Register_Contextual_Menu
-        (Kernel, "Delete directory recursively",
-         Action => Command,
-         Filter => Lookup_Filter (Kernel, "Explorer_Directory_Node"));
+      --  Command := new Delete_Command;
+      --  Register_Contextual_Menu
+      --    (Kernel, "Delete file",
+      --     Action => Command,
+      --     Filter => Lookup_Filter (Kernel, "Explorer_File_Node"));
+      --  Register_Contextual_Menu
+      --    (Kernel, "Delete directory recursively",
+      --     Action => Command,
+      --     Filter => Lookup_Filter (Kernel, "Explorer_Directory_Node"));
 
       Register_Command
         (Kernel, "pwd",
