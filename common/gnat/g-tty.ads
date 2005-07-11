c0da88cc89afeaae184ebabb402d71a75132b590
Arnaud Charlet <charlet@adacore.com>
2005-07-11 12:14:36 +0000

    Remove GPL exception so that this unit can be compiled with GAP. Part of E711-001.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@92674 936e1b1b-40f2-da11-902a-00137254ae57

--- common/gnat/g-tty.ads
+++ common/gnat/g-tty.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---              Copyright (C) 2002 Ada Core Technologies, Inc.              --
+--                     Copyright (C) 2002-2005 AdaCore                      --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -19,14 +19,7 @@
 -- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
 -- MA 02111-1307, USA.                                                      --
 --                                                                          --
--- As a special exception,  if other files  instantiate  generics from this --
--- unit, or you link  this unit with other files  to produce an executable, --
--- this  unit  does not  by itself cause  the resulting  executable  to  be --
--- covered  by the  GNU  General  Public  License.  This exception does not --
--- however invalidate  any other reasons why  the executable file  might be --
--- covered by the  GNU Public License.                                      --
---                                                                          --
--- GNAT is maintained by Ada Core Technologies Inc (http://www.gnat.com).   --
+-- GNAT is maintained by AdaCore (http://www.adacore.com).                  --
 --                                                                          --
 ------------------------------------------------------------------------------
 
