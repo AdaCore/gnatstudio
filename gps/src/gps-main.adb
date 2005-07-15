9356355640791609e5121d302b83c45c4c640d87
Nicolas Setton <setton@adacore.com>
2005-07-15 15:48:00 +0000

    Remove blank lines.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@92758 936e1b1b-40f2-da11-902a-00137254ae57

--- gps/src/gps-main.adb
+++ gps/src/gps-main.adb
@@ -18,8 +18,6 @@
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
-
-
 with Glib.Error;                use Glib.Error;
 with Glib.Messages;             use Glib.Messages;
 with Glib.Object;               use Glib.Object;
