417ff2ece5b5c68ea29dd45c2fc8b5af93978617
Florian Villoing <villoing@adacore.com>
2005-07-19 09:11:47 +0000

    New file that implements a new object based on Gtk.Text_buffer which aims at gathering the source editor functionalities independent from the GPS kernel.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@92779 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ widgets/src/gtkada-text_buffer.adb
@@ -0,0 +1,67 @@
+-----------------------------------------------------------------------
+--              GtkAda - Ada95 binding for Gtk+/Gnome                --
+--                                                                   --
+--                   Copyright (C) 2005 AdaCore                      --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
+with Glib;          use Glib;
+with Gtk.Text_Iter; use Gtk.Text_Iter;
+
+package body Gtkada.Text_Buffer is
+
+   -------------
+   -- Gtk_New --
+   -------------
+
+   procedure Gtk_New (Buffer : out Text_Buffer) is
+   begin
+      Buffer := new Text_Buffer_Record;
+      Initialize (Buffer);
+   end Gtk_New;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize (Buffer : access Text_Buffer_Record'Class) is
+   begin
+      Gtk.Text_Buffer.Initialize (Buffer);
+   end Initialize;
+
+   -----------
+   -- Clear --
+   -----------
+
+   procedure Clear (Buffer : access Text_Buffer_Record'Class) is
+      Start_Iter : Gtk_Text_Iter;
+      End_Iter   : Gtk_Text_Iter;
+   begin
+      if Get_Char_Count (Buffer) > 0 then
+         Get_Bounds (Buffer, Start_Iter, End_Iter);
+         Delete (Buffer, Start_Iter, End_Iter);
+      end if;
+   end Clear;
+
+end Gtkada.Text_Buffer;
