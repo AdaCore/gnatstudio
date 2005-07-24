d1a910ab00af096d68d6f331667f991b18b859af
Pascal Obry <obry@adacore.com>
2005-07-24 19:39:01 +0000

    Update copyright notice.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@92831 936e1b1b-40f2-da11-902a-00137254ae57

--- common/src/basic_mapper.ads
+++ common/src/basic_mapper.ads
@@ -1,8 +1,8 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                        Copyright (C) 2003                         --
---                            ACT-Europe                             --
+--                        Copyright (C) 2003-2005                    --
+--                              AdaCore                              --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
