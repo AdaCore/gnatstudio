2e6b5eab46f92f8fd8e8401b0d6ce6da3c61cd69
Florian Villoing <villoing@adacore.com>
2005-08-01 09:31:15 +0000

    Reformate copyright notice.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@92975 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/src_editor_buffer-line_information.ads
+++ src_editor/src/src_editor_buffer-line_information.ads
@@ -2,7 +2,7 @@
 --                              G P S                                --
 --                                                                   --
 --                    Copyright (C) 2003 - 2005                      --
---                             AdaCore                               --
+--                            AdaCore                                --
 --                                                                   --
 -- GPS is free  software; you can  redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
