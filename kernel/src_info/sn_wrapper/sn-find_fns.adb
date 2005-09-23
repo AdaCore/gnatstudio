f1fc7c2c945798360294f13f5e45ffb0871078ff
Emmanuel Briot <briot@adacore.com>
2005-09-23 12:56:50 +0000

    (Set_Cursor_At): Fix the case where no filename was specified because we want  to get them all. Fixes the behavior of Parse_All_LI_Information for C/C++  files (found in the context of E503-024)
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@93399 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/sn_wrapper/sn-find_fns.adb
+++ kernel/src_info/sn_wrapper/sn-find_fns.adb
@@ -1,8 +1,8 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                     Copyright (C) 2002-2004                       --
---                            ACT-Europe                             --
+--                     Copyright (C) 2002-2005                       --
+--                            AdaCore                                --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -73,7 +73,11 @@ package body SN.Find_Fns is
      (DB             : DB_File;
       Filename       : String) is
    begin
-      Set_Cursor (DB, By_Key, Filename, Exact_Match => True);
+      if Filename = "" then
+         Set_Cursor (DB, By_Key, Filename, Exact_Match => False);
+      else
+         Set_Cursor (DB, By_Key, Filename, Exact_Match => True);
+      end if;
    end Set_Cursor_At;
 
    -------------------
