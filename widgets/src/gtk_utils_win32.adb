214c5f693e48092d95559c031b71febd3789951e
Arnaud Charlet <charlet@adacore.com>
2005-09-27 13:33:33 +0000

    New unit, used to handle target dependencies wrt Gtk+ utilities.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@93437 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ widgets/src/gtk_utils_win32.adb
@@ -0,0 +1,61 @@
+-----------------------------------------------------------------------
+--                                 G P S                             --
+--                                                                   --
+--                        Copyright (C) 2005                         --
+--                                AdaCore                            --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+--  Windows specific implementation
+--  ??? This version should be the only one, assuming DLL convention is
+--  mapped to C under non Windows systems, which is no longer the case
+--  in recent GNAT versions.
+
+package body Gtk_Utils is
+
+   -----------------------
+   -- Gtk_Major_Version --
+   -----------------------
+
+   function Gtk_Major_Version return Integer is
+      Version : Integer;
+      pragma Import (Dll, Version, "gtk_major_version");
+   begin
+      return Version;
+   end Gtk_Major_Version;
+
+   -----------------------
+   -- Gtk_Minor_Version --
+   -----------------------
+
+   function Gtk_Minor_Version return Integer is
+      Version : Integer;
+      pragma Import (Dll, Version, "gtk_minor_version");
+   begin
+      return Version;
+   end Gtk_Minor_Version;
+
+   -----------------------
+   -- Gtk_Micro_Version --
+   -----------------------
+
+   function Gtk_Micro_Version return Integer is
+      Version : Integer;
+      pragma Import (Dll, Version, "gtk_micro_version");
+   begin
+      return Version;
+   end Gtk_Micro_Version;
+
+end Gtk_Utils;
