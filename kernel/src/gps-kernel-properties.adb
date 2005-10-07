38db7012d348a4c77d06c3a6ff95de70b417ff92
Emmanuel Briot <briot@adacore.com>
2005-10-07 15:46:09 +0000

    Initial implementation, discussed in E713-013
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@93548 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ kernel/src/gps-kernel-properties.adb
@@ -0,0 +1,545 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                     Copyright (C) 2005                            --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is free  software; you  can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Ada.Streams;                use Ada.Streams;
+with Ada.Strings.Unbounded;      use Ada.Strings.Unbounded;
+with Ada.Unchecked_Deallocation;
+with File_Utils;                 use File_Utils;
+with GNAT.OS_Lib;                use GNAT.OS_Lib;
+with GPS.Kernel.Project;         use GPS.Kernel.Project;
+with Glib.Xml_Int;               use Glib.Xml_Int;
+with Projects;                   use Projects;
+with String_Hash;
+with Traces;                     use Traces;
+with VFS;                        use VFS;
+
+package body GPS.Kernel.Properties is
+   Me : constant Debug_Handle := Create ("Properties");
+
+   type Property_Description is record
+      Value      : Property_Access;
+      --  The actual value of the property. This is still null if the property
+      --  hasn't been parsed yet (and then Unparsed it not null and contains
+      --  the value read from the XML file). This parsing is implemented
+      --  lazily so that we do not have to register the property types in
+      --  advance, and they are only needed when actually reading a property
+
+      Unparsed   : Node_Ptr;
+      Persistent : Boolean;
+   end record;
+   No_Description : constant Property_Description := (null, null, False);
+   --  The description of a property
+
+   procedure Free (Description : in out Property_Description);
+
+   package Properties_Description_Hash is new String_Hash
+     (Data_Type      => Property_Description,
+      Free_Data      => Free,
+      Null_Ptr       => No_Description,
+      Case_Sensitive => True);
+   use Properties_Description_Hash.String_Hash_Table;
+
+   type Properties_Description_HTable is
+     access all Properties_Description_Hash.String_Hash_Table.HTable;
+
+   procedure Free (Hash : in out Properties_Description_HTable);
+
+   package Properties_Hash is new String_Hash
+     (Data_Type      => Properties_Description_HTable,
+      Free_Data      => Free,
+      Null_Ptr       => null,
+      Case_Sensitive => Filenames_Are_Case_Sensitive);
+   use Properties_Hash.String_Hash_Table;
+
+   All_Properties : Properties_Hash.String_Hash_Table.HTable;
+   --  Global variable storing all the current properties for the current
+   --  project.
+   --  ??? It would be nicer to store this in the kernel but:
+   --    - it really doesn't provide anything in addition (no more task safe
+   --      in any case)
+   --    - It would require an extra Kernel parameter to Set_File_Property
+   --      and Get_File_Property, thus making the API harder to use.
+   --  For now, we'll leave with this global variable.
+
+   function Get_Properties_Filename
+     (Kernel : access Kernel_Handle_Record'Class) return String;
+   --  Return the filename to use when saving the persistent properties for the
+   --  current project
+
+   procedure Set_Resource_Property
+     (Resource_Key  : String;
+      Resource_Kind : String;
+      Name          : String;
+      Property      : Property_Description);
+   --  Set property for any kind of resource. This is the internal
+   --  implementation of Set_*_Property
+
+   --------------------
+   --  String stream --
+   --------------------
+
+   type String_Stream_Type is new Root_Stream_Type with record
+      Str        : Unbounded_String;
+      Read_Index : Natural := 1;
+   end record;
+   --  A stream that reads and writes to a string
+
+   procedure Read
+     (Stream : in out String_Stream_Type;
+      Item   : out Stream_Element_Array;
+      Last   : out Stream_Element_Offset);
+   procedure Write
+     (Stream : in out String_Stream_Type;
+      Item   : in Stream_Element_Array);
+   --  See inherited documentation
+
+   procedure Open (Stream : in out String_Stream_Type'Class; Str : String);
+   --  Open a new string
+
+   ----------
+   -- Open --
+   ----------
+
+   procedure Open (Stream : in out String_Stream_Type'Class; Str : String) is
+   begin
+      Stream.Str        := To_Unbounded_String (Str);
+      Stream.Read_Index := 1;
+   end Open;
+
+   ----------
+   -- Read --
+   ----------
+
+   procedure Read
+     (Stream : in out String_Stream_Type;
+      Item   : out Stream_Element_Array;
+      Last   : out Stream_Element_Offset)
+   is
+      Str : constant String := Slice
+        (Stream.Str, Stream.Read_Index, Stream.Read_Index + Item'Length - 1);
+      J   : Stream_Element_Offset := Item'First;
+   begin
+      for S in Str'Range loop
+         Item (J) := Stream_Element (Character'Pos (Str (S)));
+         J := J + 1;
+      end loop;
+      Last := Item'First + Str'Length - 1;
+   end Read;
+
+   -----------
+   -- Write --
+   -----------
+
+   procedure Write
+     (Stream : in out String_Stream_Type;
+      Item   : in Stream_Element_Array)
+   is
+      Str : String (1 .. Integer (Item'Length));
+      S   : Integer := Str'First;
+   begin
+      Trace (Me, "MANU Writing to string stream " & Item'Length'Img);
+      for J in Item'Range loop
+         Str (S) := Character'Val (Item (J));
+         S := S + 1;
+      end loop;
+
+      Append (Stream.Str, Str);
+   end Write;
+
+   ----------
+   -- Free --
+   ----------
+
+   procedure Free (Description : in out Property_Description) is
+      procedure Unchecked_Free is new Ada.Unchecked_Deallocation
+        (Property_Record'Class, Property_Access);
+   begin
+      if Description.Value /= null then
+         Destroy (Description.Value.all);
+         Unchecked_Free (Description.Value);
+      end if;
+   end Free;
+
+   ----------
+   -- Free --
+   ----------
+
+   procedure Free (Hash : in out Properties_Description_HTable) is
+      procedure Unchecked_Free is new Ada.Unchecked_Deallocation
+        (Properties_Description_Hash.String_Hash_Table.HTable,
+         Properties_Description_HTable);
+   begin
+      if Hash /= null then
+         Reset (Hash.all);
+         Unchecked_Free (Hash);
+      end if;
+   end Free;
+
+   ----------
+   -- Save --
+   ----------
+
+   function Save
+     (Property : access Property_Record) return Glib.Xml_Int.Node_Ptr
+   is
+      Str : aliased String_Stream_Type;
+   begin
+      Property_Record'Class'Write
+        (Str'Access, Property_Record'Class (Property.all));
+
+      return new Node'
+        (Tag        => new String'("stream"),
+         Attributes => null,
+         --  ??? Would be better to use a <![CDATA[...]]> section, but special
+         --  characters get quoted by the glib-xml. Perhaps when we are using
+         --  XML/Ada to write XML streams ?
+         Value      => new String'(To_String (Str.Str)),
+         Parent     => null,
+         Child      => null,
+         Next       => null,
+         Specific_Data => 1);
+   end Save;
+
+   ----------
+   -- Load --
+   ----------
+
+   procedure Load
+     (Property : in out Property_Record; From : Glib.Xml_Int.Node_Ptr)
+   is
+      Str : aliased String_Stream_Type;
+   begin
+      Open (Str, From.Value.all);
+      Property_Record'Class'Read
+        (Str'Access, Property_Record'Class (Property));
+   end Load;
+
+   -------------
+   -- Destroy --
+   -------------
+
+   procedure Destroy (Property : in out Property_Record) is
+      pragma Unreferenced (Property);
+   begin
+      null;
+   end Destroy;
+
+   ----------
+   -- Save --
+   ----------
+
+   function Save
+     (Property : access String_Property) return Glib.Xml_Int.Node_Ptr is
+   begin
+      if Property.Value /= null then
+         return new Node'
+           (Tag        => new String'("value"),
+            Attributes => null,
+            Value      => new String'(Property.Value.all),
+            Parent     => null,
+            Child      => null,
+            Next       => null,
+            Specific_Data => 1);
+      else
+         return null;
+      end if;
+   end Save;
+
+   ----------
+   -- Save --
+   ----------
+
+   function Save
+     (Property : access Integer_Property) return Glib.Xml_Int.Node_Ptr is
+   begin
+      return new Node'
+        (Tag        => new String'("value"),
+         Attributes => null,
+         Value      => new String'(Integer'Image (Property.Value)),
+         Parent     => null,
+         Child      => null,
+         Next       => null,
+         Specific_Data => 1);
+   end Save;
+
+   ----------
+   -- Save --
+   ----------
+
+   function Save
+     (Property : access Boolean_Property) return Glib.Xml_Int.Node_Ptr is
+   begin
+      return new Node'
+        (Tag        => new String'("value"),
+         Attributes => null,
+         Value      => new String'(Boolean'Image (Property.Value)),
+         Parent     => null,
+         Child      => null,
+         Next       => null,
+         Specific_Data => 1);
+   end Save;
+
+   ----------
+   -- Load --
+   ----------
+
+   procedure Load
+     (Property : in out String_Property; From : Glib.Xml_Int.Node_Ptr) is
+   begin
+      if From.Value /= null then
+         Property.Value := new String'(From.Value.all);
+      else
+         Property.Value := null;
+      end if;
+   end Load;
+
+   ----------
+   -- Load --
+   ----------
+
+   procedure Load
+     (Property : in out Integer_Property; From : Glib.Xml_Int.Node_Ptr) is
+   begin
+      Property.Value := Integer'Value (From.Value.all);
+   exception
+      when Constraint_Error =>
+         Property.Value := 0;
+   end Load;
+
+   ----------
+   -- Load --
+   ----------
+
+   procedure Load
+     (Property : in out Boolean_Property; From : Glib.Xml_Int.Node_Ptr) is
+   begin
+      Property.Value := Boolean'Value (From.Value.all);
+   exception
+      when Constraint_Error =>
+         Property.Value := True;
+   end Load;
+
+   -------------
+   -- Destroy --
+   -------------
+
+   procedure Destroy (Property : in out String_Property) is
+   begin
+      Free (Property.Value);
+   end Destroy;
+
+   ---------------------------
+   -- Set_Resource_Property --
+   ---------------------------
+
+   procedure Set_Resource_Property
+     (Resource_Key  : String;
+      Resource_Kind : String;
+      Name          : String;
+      Property      : Property_Description)
+   is
+      pragma Unreferenced (Resource_Kind);
+      --  Not used yet, the idea is to have various attributes in the XML file
+      --  depending on the resource type
+
+      Hash : Properties_Description_HTable;
+   begin
+      Hash := Get (All_Properties, Resource_Key);
+      if Hash = null then
+         Hash := new Properties_Description_Hash.String_Hash_Table.HTable;
+         Set (All_Properties, Resource_Key, Hash);
+      end if;
+
+      Set (Hash.all, Name, Property);
+   end Set_Resource_Property;
+
+   -----------------------
+   -- Set_File_Property --
+   -----------------------
+
+   procedure Set_File_Property
+     (File       : VFS.Virtual_File;
+      Name       : String;
+      Property   : access Property_Record'Class;
+      Persistent : Boolean := False) is
+   begin
+      Set_Resource_Property
+        (Full_Name (File).all, "file", Name,
+         Property_Description'(Value      => Property_Access (Property),
+                               Unparsed   => null,
+                               Persistent => Persistent));
+   end Set_File_Property;
+
+   -----------------------
+   -- Get_File_Property --
+   -----------------------
+
+   procedure Get_File_Property
+     (Property : out Property_Record'Class;
+      File     : VFS.Virtual_File;
+      Name     : String;
+      Found    : out Boolean)
+   is
+      Hash : Properties_Description_HTable;
+      Descr : Property_Description;
+   begin
+      Found := False;
+      Hash := Get (All_Properties, Full_Name (File).all);
+      if Hash /= null then
+         Descr := Get (Hash.all, Name);
+         if Descr /= No_Description then
+            if Descr.Value = null then
+               Load (Property, Descr.Unparsed);
+               Descr.Value := new Property_Record'Class'(Property);
+            end if;
+
+            Property := Descr.Value.all;
+            Found := True;
+         end if;
+      end if;
+
+   exception
+      when others =>
+         Found := False;
+   end Get_File_Property;
+
+   -----------------------------
+   -- Get_Properties_Filename --
+   -----------------------------
+
+   function Get_Properties_Filename
+     (Kernel : access Kernel_Handle_Record'Class) return String is
+   begin
+      --  We could use the .gps directory, but that would mean we have to keep
+      --  in memory information for files that do not belong to the current
+      --  project.
+      --  return Get_Hom_Dir (Kernel) & "properties.xml";
+
+      return Object_Path (Get_Project (Kernel), Recursive => False)
+        & Directory_Separator & ".gps_properties.xml";
+   end Get_Properties_Filename;
+
+   --------------------------------
+   -- Save_Persistent_Properties --
+   --------------------------------
+
+   procedure Save_Persistent_Properties
+     (Kernel : access Kernel_Handle_Record'Class)
+   is
+      Filename : constant String := Get_Properties_Filename (Kernel);
+      Iter  : Properties_Hash.String_Hash_Table.Iterator;
+      Iter2 : Properties_Description_Hash.String_Hash_Table.Iterator;
+      Hash  : Properties_Description_HTable;
+      Root, File, Prop : Node_Ptr;
+      Descr : Property_Description;
+   begin
+      Trace (Me, "Saving " & Filename);
+      Root := new Node'
+        (Tag        => new String'("persistent_properties"),
+         Attributes => null,
+         Value      => null,
+         Parent     => null,
+         Child      => null,
+         Next       => null,
+         Specific_Data => 1);
+
+      Get_First (All_Properties, Iter);
+      loop
+         Hash := Get_Element (Iter);
+         exit when Hash = null;
+
+         File := new Node'
+           (Tag        => new String'("properties"),
+            Attributes => new String'("file='" & Get_Key (Iter) & "'"),
+            Value      => null,
+            Parent     => null,
+            Child      => null,
+            Next       => null,
+            Specific_Data => 1);
+         Add_Child (Root, File);
+
+         Get_First (Hash.all, Iter2);
+         loop
+            Descr := Get_Element (Iter2);
+            exit when Descr = No_Description;
+
+            if Descr.Persistent then
+               Prop := new Node'
+                 (Tag        => new String'("property"),
+                  Attributes => new String'("name='" & Get_Key (Iter2) & "'"),
+                  Value      => null,
+                  Parent     => null,
+                  Child      => null,
+                  Next       => null,
+                  Specific_Data => 1);
+               Add_Child (File, Prop);
+
+               if Descr.Value = null then
+                  Add_Child (Prop, Descr.Unparsed);
+               else
+                  Add_Child (Prop, Save (Descr.Value));
+               end if;
+            end if;
+
+            Get_Next (Hash.all, Iter2);
+         end loop;
+
+         Get_Next (All_Properties, Iter);
+      end loop;
+
+      Print (Root, Filename);
+      Reset (All_Properties);
+   end Save_Persistent_Properties;
+
+   -----------------------------------
+   -- Restore_Persistent_Properties --
+   -----------------------------------
+
+   procedure Restore_Persistent_Properties
+     (Kernel : access Kernel_Handle_Record'Class)
+   is
+      Filename : constant String := Get_Properties_Filename (Kernel);
+      Root, File, Prop : Node_Ptr;
+   begin
+      Trace (Me, "Loading " & Filename);
+      if Is_Readable_File (Filename) then
+         Root := Parse (Filename);
+         File := Root.Child;
+         while File /= null loop
+            Prop := File.Child;
+            while Prop /= null loop
+               Set_Resource_Property
+                 (Resource_Key  => Get_Attribute (File, "file"),
+                  Resource_Kind => "file",
+                  Name          => Get_Attribute (Prop, "name"),
+                  Property      => (Value    => null,
+                                    Unparsed => Prop.Child,
+                                    Persistent    => True));
+               Prop.Child := null; --  Since we are deleting the tree afterward
+               Prop := Prop.Next;
+            end loop;
+
+            File := File.Next;
+         end loop;
+         Free (Root);
+      end if;
+   end Restore_Persistent_Properties;
+
+end GPS.Kernel.Properties;
---
+++ kernel/src/gps-kernel-properties.ads
@@ -0,0 +1,142 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                     Copyright (C) 2005                            --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is free  software; you  can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+--  This package provides file-specific properties, optionaly persistent
+
+with Glib.Xml_Int;
+
+package GPS.Kernel.Properties is
+
+   ----------------
+   -- Properties --
+   ----------------
+
+   type Property_Record is tagged null record;
+   type Property_Access is access all Property_Record'Class;
+   --  A general property that can be associated with a file.
+   --  Such properties can be marked as persistent, that is they will exist
+   --  from one session of GPS to the next, transparently.
+   --  By default, it uses streams to dump the property
+
+   function Save
+     (Property : access Property_Record) return Glib.Xml_Int.Node_Ptr;
+   --  Save the property to an XML node. This is the child node of the node
+   --  associated with the property.
+   --  Null should be returned if the property cannot be saved.
+   --  In the end, the XML file will contain something like:
+   --     <properties file="...">
+   --        <property name="...">save1</property>
+   --        <property name="...">save2</property>
+   --     </properties>
+   --  where "save1" and "save2" are results of Save.
+
+   procedure Load
+     (Property : in out Property_Record; From : Glib.Xml_Int.Node_Ptr);
+   --  Load a property from an XML node.
+   --  From has been found automatically by GPS based on the property node. If
+   --  it doesn't match the type expected by Property, it is likely because two
+   --  properties have the same name. In this case, an error message should be
+   --  written in the console.
+
+   procedure Destroy (Property : in out Property_Record);
+   --  Free the memory occupied by the property. You should always call the
+   --  parent's Destroy handler.
+
+   ---------------------------------------
+   -- Associating properties with files --
+   ---------------------------------------
+
+   procedure Set_File_Property
+     (File       : VFS.Virtual_File;
+      Name       : String;
+      Property   : access Property_Record'Class;
+      Persistent : Boolean := False);
+   --  Associate a given property with File, so that it can be queries later
+   --  through Get_File_Property.
+   --  If Persistent is True, the property will be preserved from one session
+   --  of GPS to the next.
+   --  Property names are case sensitive.
+
+   procedure Get_File_Property
+     (Property : out Property_Record'Class;
+      File     : VFS.Virtual_File;
+      Name     : String;
+      Found    : out Boolean);
+   --  Return the given named property associated with File.
+   --  Found is set to False if there is no such property.
+   --  Property names are case sensitive.
+
+   -----------------------------
+   -- Specific property types --
+   -----------------------------
+   --  These are provided for convenience
+
+   type Integer_Property is new Property_Record with record
+      Value : Integer;
+   end record;
+   type Integer_Property_Access is access all Integer_Property'Class;
+
+   type String_Property is new Property_Record with record
+      Value : GNAT.OS_Lib.String_Access;
+   end record;
+   type String_Property_Access is access all String_Property'Class;
+
+   type Boolean_Property is new Property_Record with record
+      Value : Boolean;
+   end record;
+   type Boolean_Property_Access is access all Boolean_Property'Class;
+
+   -----------------------------------------
+   -- Saving and restoring all properties --
+   -----------------------------------------
+
+   procedure Save_Persistent_Properties
+     (Kernel : access Kernel_Handle_Record'Class);
+   --  Save all persistent properties for all files in the current project.
+   --  This clears the current cache, so that no property will be available
+   --  after this call, not even non-persistent ones.
+   --  This subprogram should only be called by the kernel itself.
+
+   procedure Restore_Persistent_Properties
+     (Kernel : access Kernel_Handle_Record'Class);
+   --  Restore persistent properties for the files in the current project.
+   --  This subprogram should only be called by the kernel itself.
+
+private
+   procedure Destroy (Property : in out String_Property);
+   function Save
+     (Property : access String_Property) return Glib.Xml_Int.Node_Ptr;
+   procedure Load
+     (Property : in out String_Property; From : Glib.Xml_Int.Node_Ptr);
+   --  See inherited documentation
+
+   function Save
+     (Property : access Integer_Property) return Glib.Xml_Int.Node_Ptr;
+   procedure Load
+     (Property : in out Integer_Property; From : Glib.Xml_Int.Node_Ptr);
+   --  See inherited documentation
+
+   function Save
+     (Property : access Boolean_Property) return Glib.Xml_Int.Node_Ptr;
+   procedure Load
+     (Property : in out Boolean_Property; From : Glib.Xml_Int.Node_Ptr);
+   --  See inherited documentation
+
+end GPS.Kernel.Properties;
