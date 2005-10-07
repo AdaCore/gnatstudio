d08be80858206eb8569e7f6fe0ac2dd2eb2768dd
Arnaud Charlet <charlet@adacore.com>
2005-10-07 11:47:29 +0000

    Add missing header. Minor reformatting.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@93535 936e1b1b-40f2-da11-902a-00137254ae57

--- common/src/rbtrees.ads
+++ common/src/rbtrees.ads
@@ -1,3 +1,22 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                      Copyright (C) 2003-2005                      --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
 
 --  This package implements red-black trees, which can be used to provide
 --  priority queues, or associative containers (sets, htables,...) as is done
@@ -65,7 +84,6 @@ package Rbtrees is
    --  over and over again
 
 private
-
    type Node;
    type Rbtree_Iterator is access all Node;
    Null_Iterator : constant Rbtree_Iterator := null;
@@ -74,4 +92,3 @@ private
       Root : Rbtree_Iterator;
    end record;
 end Rbtrees;
-
