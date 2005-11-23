c8fbac855f7404be0e5360e21863c46d0724b6b4
Nicolas Setton <setton@adacore.com>
2005-11-23 18:21:59 +0000

    (Generic_Command): For VCS that does not support absolute names we pass "."  as argument. In fact in this case we cd into the working directory and the  command must be applied to the current directory. This fixes problem with  empty arguments on Windows. Work for EB09-025.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/branches/gps-3_1-branch@94421 936e1b1b-40f2-da11-902a-00137254ae57

--- vcs/src/vcs-generic_vcs.adb
+++ vcs/src/vcs-generic_vcs.adb
@@ -466,7 +466,9 @@ package body VCS.Generic_VCS is
 
       while Node /= Null_Node loop
          declare
-            Args : GNAT.OS_Lib.String_List_Access;
+            Args    : GNAT.OS_Lib.String_List_Access;
+            --  Set to True when the current working directory already part of
+            --  the command line.
          begin
             --  Args, Dir and Dir_Args are freed when the command proxy is
             --  destroyed.
@@ -499,6 +501,7 @@ package body VCS.Generic_VCS is
                end if;
 
                if Ref.Absolute_Names then
+                  --  The VCS works on absolute names
                   declare
                      Filename : constant String := Data (Node);
                      Pref_Len : Natural := Dir.all'Length;
@@ -520,7 +523,20 @@ package body VCS.Generic_VCS is
                   end;
 
                else
-                  Args (Index) := new String'(Base_Name (Data (Node)));
+                  --  The VCS works on the local directory
+                  declare
+                     Base : constant String := Base_Name (Data  (Node));
+                  begin
+                     if Base = "" then
+                        --  The data is a directory. In this case, since we
+                        --  are launching the command from the local directory
+                        --  transform the directory to ".".
+
+                        Args (Index) := new String'(".");
+                     else
+                        Args (Index) := new String'(Base);
+                     end if;
+                  end;
                end if;
 
                Index := Index + 1;
