cfa687decce9d8293a926e036a4ca18b7c63eda7
Pascal Obry <obry@adacore.com>
2005-11-24 18:18:45 +0000

    (Glib.Object): Removed with clause as not used anymore. Fix build failure.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@94446 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/display_items.ads
+++ gvd/gvd/display_items.ads
@@ -1,8 +1,8 @@
 -----------------------------------------------------------------------
 --                   GVD - The GNU Visual Debugger                   --
 --                                                                   --
---                      Copyright (C) 2000-2002                      --
---                              ACT-Europe                           --
+--                      Copyright (C) 2000-2005                      --
+--                              AdaCore                              --
 --                                                                   --
 -- GVD is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -19,7 +19,6 @@
 -----------------------------------------------------------------------
 
 with Glib;
-with Glib.Object;
 with Items;
 with GVD.Process;
 with Gdk.Event;
