9e557f25ce39f9f8422c15c695a1d63750a0194b
Florian Villoing <villoing@adacore.com>
2005-12-02 17:11:10 +0000

    Merge 1.36 -> 1.38. (Get_Label.Substitution): Make sure to work on booleans that have the expected  meaning and not the opposite. Fixes EC02-003. (Get_Label): Has_Error no longer needs to be marked aliased.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/branches/gps-3_1-branch@94600 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/gps-kernel-modules.adb
+++ kernel/src/gps-kernel-modules.adb
@@ -483,7 +483,7 @@ package body GPS.Kernel.Modules is
      (Creator : access Contextual_Label_Parameters;
       Context : access Selection_Context'Class) return String
    is
-      Has_Error : aliased Boolean := False;
+      Has_Error : Boolean := False;
 
       function Substitution (Param : String; Quoted : Boolean) return String;
       --  Substitute %P, %f,... as appropriate
@@ -509,10 +509,12 @@ package body GPS.Kernel.Modules is
             end if;
          else
             declare
-               Tmp : constant String := GPS.Kernel.Macros.Substitute
+               Done : aliased Boolean := False;
+               Tmp  : constant String := GPS.Kernel.Macros.Substitute
                  (Param, Selection_Context_Access (Context),
-                  Quoted, Has_Error'Access);
+                  Quoted, Done'Access);
             begin
+               Has_Error := not Done;
                return Tmp;
             end;
          end if;
