e39faee9709c630990ff6cc1e95fb4de6acae8db
Quentin Ochem <ochem@adacore.com>
2005-12-05 17:02:13 +0000

    (Scheduled_Command): new type, encapsulating any command stored in the task  manager. (Scheduled_Command_Access): new subprogram (Name): new subprogram (Progress): new subprogram (Set_Progress): new subprogram (Undo): new subprogram (Interrupt): new subprogram (Free): new subprogram (Execute): new subprogram (Get_Instance): new subprogram
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@94618 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/gps-kernel-task_manager.ads
+++ kernel/src/gps-kernel-task_manager.ads
@@ -21,12 +21,39 @@
 --  This package initializes the GPS Task Manager, and provides menus
 --  to display the Task Manager Interface.
 
-with Commands;     use Commands;
-with Task_Manager; use Task_Manager;
+with Commands;           use Commands;
+with Task_Manager;       use Task_Manager;
+with Generic_List;
+with GPS.Kernel.Scripts; use GPS.Kernel.Scripts;
 
 package GPS.Kernel.Task_Manager is
 
    use Commands.Command_Queues;
+
+   type Scheduled_Command is new Root_Command with private;
+   --  This command encloses any command scheduled or running in the task
+   --  manager
+
+   type Scheduled_Command_Access is access all Scheduled_Command'Class;
+
+   function Name (Command : access Scheduled_Command) return String;
+   procedure Interrupt (Command : in out Scheduled_Command);
+   procedure Set_Progress (Command : access Scheduled_Command;
+                           Progress : Progress_Record);
+   function Progress (Command : access Scheduled_Command)
+                      return Progress_Record;
+   function Execute
+     (Command : access Scheduled_Command) return Command_Return_Type;
+   function Undo (This : access Scheduled_Command) return Boolean;
+   procedure Free (Command : in out Scheduled_Command);
+   --  See inhertited documentation
+
+   function Get_Instance
+     (Command  : access Scheduled_Command'Class;
+      Language : access Scripting_Language_Record'Class)
+      return Class_Instance;
+   --  Returns the class instance associated to this command for the given
+   --  scripting language. Create one when needed.
 
    procedure Launch_Background_Command
      (Kernel          : access Kernel_Handle_Record'Class;
@@ -64,5 +91,29 @@ package GPS.Kernel.Task_Manager is
    function Get_Task_Manager
      (Kernel : access Kernel_Handle_Record'Class) return Task_Manager_Access;
    --  Return the GPS task manager.
+
+private
+
+   type Instance_Item is record
+      Script   : GPS.Kernel.Scripts.Scripting_Language;
+      Instance : GPS.Kernel.Scripts.Class_Instance;
+   end record;
+   --  This type is used to store the association between a instance item and
+   --  a script, for a given command.
+
+   procedure Free (Item : in out Instance_Item);
+   --  Free the memory of the class instance in this class item.
+
+   package Instance_List is new Generic_List (Instance_Item);
+   use Instance_List;
+
+   type Scheduled_Command is new Root_Command with record
+      Command         : Command_Access;
+      Destroy_On_Exit : Boolean;
+
+      Instances       : Instance_List.List;
+      --  List of instances for the given command, created only when needed,
+      --  one per script language.
+   end record;
 
 end GPS.Kernel.Task_Manager;
