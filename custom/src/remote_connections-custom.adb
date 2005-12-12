dc6878d33d139167b5b3d428e70508394d1a7c6b
Jerome Lambourg <lambourg@adacore.com>
2005-12-12 16:32:25 +0000

    (Execute_Action_Recursive): Fill a buffer from output line by line to prevent   the Process's buffer to overflow. Part of the fix for EC09-006
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@94709 936e1b1b-40f2-da11-902a-00137254ae57

--- custom/src/remote_connections-custom.adb
+++ custom/src/remote_connections-custom.adb
@@ -66,6 +66,10 @@ package body Remote_Connections.Custom is
    Ls_Year_Parens  : constant := 10;
    --  Regexp to match the output of ls, and extract the required fields
 
+   Line_Regexp : constant Pattern_Matcher_Access :=
+     new Pattern_Matcher'(Compile ("^.+$", Multiple_Lines));
+   --  Regexp matching any new line
+
    -----------------------
    -- Local Subprograms --
    -----------------------
@@ -333,11 +337,12 @@ package body Remote_Connections.Custom is
       Ret_Value       : out Return_Enum)
    is
       Regexps    : constant Compiled_Regexp_Array :=
-        Get_Regexp_Array (Action);
+        Get_Regexp_Array (Action) & Line_Regexp;
       Args       : Argument_List_Access;
       L_Action   : Action_Access;
       Expect_Ptr : Expect_Access;
       Tmp        : String_Access;
+      Outstr     : String_Access := null;
       Matched    : Match_Array (0 .. 0);
       Exp_Result : Expect_Match;
       Success    : Boolean;
@@ -565,7 +570,7 @@ package body Remote_Connections.Custom is
 
       --  Once the action executed, wait for Expects (if any)
 
-      if Regexps'Length > 0 then
+      if Regexps'Length > 1 then
          while Ret_Value = No_Statement loop
             Trace (Full_Me, "Execute_Action: expecting" &
                    Natural'Image (Regexps'Length) & " Expect(s)");
@@ -578,6 +583,7 @@ package body Remote_Connections.Custom is
                    Expect_Match'Image (Exp_Result));
 
             if Exp_Result = Expect_Timeout then
+
                Trace (Full_Me, "Timeout !");
                --  If no action is defined for timeout
 
@@ -599,6 +605,17 @@ package body Remote_Connections.Custom is
                      L_Action := L_Action.Next;
                   end loop;
                end if;
+
+            elsif Integer (Exp_Result) = Regexps'Last then
+
+               if Outstr = null then
+                  Outstr := new String'(Expect_Out (L_Pd.all));
+               else
+                  Tmp := Outstr;
+                  Outstr := new String'(Tmp.all & Expect_Out (L_Pd.all));
+                  Free (Tmp);
+               end if;
+
             else
 
                Expect_Ptr := Action.Expects;
@@ -619,19 +636,35 @@ package body Remote_Connections.Custom is
                   declare
                      Output : constant String := Expect_Out (L_Pd.all);
                   begin
+
                      Trace (Full_Me, "execute Expect's actions");
                      L_Action := Expect_Ptr.Actions;
 
                      while Ret_Value = No_Statement and L_Action /= null loop
-                        Execute_Action_Recursive
-                          (Connection,
-                           L_Action,
-                           Local_Full_Name,
-                           WriteTmpFile,
-                           ReadTmpBase,
-                           Output (Output'First .. Matched (0).First - 2),
-                           L_Pd,
-                           Ret_Value);
+                        if Outstr = null then
+                           Execute_Action_Recursive
+                             (Connection,
+                              L_Action,
+                              Local_Full_Name,
+                              WriteTmpFile,
+                              ReadTmpBase,
+                              Output (Output'First .. Matched (0).First - 2),
+                              L_Pd,
+                              Ret_Value);
+                        else
+                           Execute_Action_Recursive
+                             (Connection,
+                              L_Action,
+                              Local_Full_Name,
+                              WriteTmpFile,
+                              ReadTmpBase,
+                              Outstr.all &
+                               Output (Output'First .. Matched (0).First - 2),
+                              L_Pd,
+                              Ret_Value);
+                           Free (Outstr);
+                        end if;
+
                         L_Action := L_Action.Next;
                      end loop;
                   end;
