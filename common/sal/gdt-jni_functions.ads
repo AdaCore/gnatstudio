9c7dc3b55932314923e2b83bbe637ac3f36c487f
Quentin Ochem <ochem@adacore.com>
2006-01-16 15:18:11 +0000

    Fixed name of exported procedures, now in gnatbench.core package instead  of gdt.study.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/branches/gps-3_1-branch@94906 936e1b1b-40f2-da11-902a-00137254ae57

--- common/sal/gdt-jni_functions.ads
+++ common/sal/gdt-jni_functions.ads
@@ -30,7 +30,7 @@ package GDT.JNI_Functions is
    pragma Export
      (C,
       Java_GpsJni_analyzeAdaSourceInt,
-      "Java_com_adacore_gdt_study_gpsjni_GPSJni_analyseAdaSourceInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_GPSJni_analyseAdaSourceInt");
 
    procedure Java_GPSJni_indentAdaBufferInt
      (Env               : JNIEnv;
@@ -44,104 +44,108 @@ package GDT.JNI_Functions is
    pragma Export
      (C,
       Java_GpsJni_indentAdaBufferInt,
-      "Java_com_adacore_gdt_study_gpsjni_GPSJni_indentAdaBufferInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_GPSJni_indentAdaBufferInt");
 
    function Java_ConstructAccess_getSlocStartInt
      (Env : JNIEnv; This : Jobject; Addr : Jint) return Jint;
    pragma Export
      (C, Java_ConstructAccess_getSlocStartInt,
-      "Java_com_adacore_gdt_study_gpsjni_ConstructAccess_getSlocStartInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_"
+      & "ConstructAccess_getSlocStartInt");
 
    function Java_ConstructAccess_getSlocEntityInt
      (Env : JNIEnv; This : Jobject; Addr : Jint) return Jint;
    pragma Export
      (C,
       Java_ConstructAccess_getSlocEntityInt,
-      "Java_com_adacore_gdt_study_gpsjni_ConstructAccess_getSlocEntityInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_"
+      & "ConstructAccess_getSlocEntityInt");
 
    function Java_ConstructAccess_getSlocEndInt
      (Env : JNIEnv; This : Jobject; Addr : Jint) return Jint;
    pragma Export
      (C,
       Java_ConstructAccess_getSlocEndInt,
-      "Java_com_adacore_gdt_study_gpsjni_ConstructAccess_getSlocEndInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_ConstructAccess_getSlocEndInt");
 
    function Java_ConstructAccess_getPrevInt
      (Env : JNIEnv; This : Jobject; Addr : Jint) return Jint;
    pragma Export
      (C,
       Java_ConstructAccess_getPrevInt,
-      "Java_com_adacore_gdt_study_gpsjni_ConstructAccess_getPrevInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_ConstructAccess_getPrevInt");
 
    function Java_ConstructAccess_getNextInt
      (Env : JNIEnv; This : Jobject; Addr : Jint) return Jint;
    pragma Export
      (C,
       Java_ConstructAccess_getNextInt,
-      "Java_com_adacore_gdt_study_gpsjni_ConstructAccess_getNextInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_ConstructAccess_getNextInt");
 
    function Java_ConstructAccess_getCategoryInt
      (Env : JNIEnv; This : Jobject; Addr : Jint) return Jint;
    pragma Export
      (C,
       Java_ConstructAccess_getCategoryInt,
-      "Java_com_adacore_gdt_study_gpsjni_ConstructAccess_getCategoryInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_ConstructAccess_getCategoryInt");
 
    function Java_ConstructAccess_getNameInt
      (Env : JNIEnv; This : Jobject; Addr : Jint) return Jstring;
    pragma Export
      (C,
       Java_ConstructAccess_getNameInt,
-      "Java_com_adacore_gdt_study_gpsjni_ConstructAccess_getNameInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_ConstructAccess_getNameInt");
 
    function Java_ConstructAccess_getVisibilityInt
      (Env : JNIEnv; This : Jobject; Addr : Jint) return Jint;
    pragma Export
      (C,
       Java_ConstructAccess_getVisibilityInt,
-      "Java_com_adacore_gdt_study_gpsjni_ConstructAccess_getVisibilityInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_" &
+      "ConstructAccess_getVisibilityInt");
 
    function Java_ConstructAccess_getProfileInt
      (Env : JNIEnv; This : Jobject; Addr : Jint) return Jstring;
    pragma Export
      (C,
       Java_ConstructAccess_getProfileInt,
-      "Java_com_adacore_gdt_study_gpsjni_ConstructAccess_getProfileInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_ConstructAccess_getProfileInt");
 
    function Java_ConstructAccess_getIsDeclarationInt
      (Env : JNIEnv; This : Jobject; Addr : Jint) return Jint;
    pragma Export
      (C,
       Java_ConstructAccess_getIsDeclarationInt,
-      "Java_com_adacore_gdt_study_gpsjni_ConstructAccess_getIsDeclarationInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_"
+      & "ConstructAccess_getIsDeclarationInt");
 
    function Java_ConstructList_getFirstInt
      (Env : JNIEnv; This : Jobject; Addr : Jint) return Jint;
    pragma Export
      (C,
       Java_ConstructList_getFirstInt,
-      "Java_com_adacore_gdt_study_gpsjni_ConstructList_getFirstInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_ConstructList_getFirstInt");
 
    function Java_ConstructList_getCurrentInt
      (Env : JNIEnv; This : Jobject; Addr : Jint) return Jint;
    pragma Export
      (C,
       Java_ConstructList_getCurrentInt,
-      "Java_com_adacore_gdt_study_gpsjni_ConstructList_getCurrentInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_ConstructList_getCurrentInt");
 
    function Java_ConstructList_getLastInt
      (Env : JNIEnv; This : Jobject; Addr : Jint) return Jint;
    pragma Export
      (C,
       Java_ConstructList_getLastInt,
-      "Java_com_adacore_gdt_study_gpsjni_ConstructList_getLastInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_ConstructList_getLastInt");
 
    procedure Java_ConstructList_freeInt
      (Env : JNIEnv; This : Jobject; Addr : Jint);
    pragma Export
      (C,
       Java_ConstructList_freeInt,
-      "Java_com_adacore_gdt_study_gpsjni_ConstructList_freeInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_ConstructList_freeInt");
 
 
    function Java_SourceLocation_getLineInt
@@ -149,20 +153,20 @@ package GDT.JNI_Functions is
    pragma Export
      (C,
       Java_SourceLocation_getLineInt,
-      "Java_com_adacore_gdt_study_gpsjni_SourceLocation_getLineInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_SourceLocation_getLineInt");
 
    function Java_SourceLocation_getColumnInt
      (Env : JNIEnv; This : Jobject; Addr : Jint) return Jint;
    pragma Export
      (C,
       Java_SourceLocation_getColumnInt,
-      "Java_com_adacore_gdt_study_gpsjni_SourceLocation_getColumnInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_SourceLocation_getColumnInt");
 
    function Java_SourceLocation_getIndexInt
      (Env : JNIEnv; This : Jobject; Addr : Jint) return Jint;
    pragma Export
      (C,
       Java_SourceLocation_getIndexInt,
-      "Java_com_adacore_gdt_study_gpsjni_SourceLocation_getIndexInt");
+      "Java_com_adacore_gnatbench_core_gpsjni_SourceLocation_getIndexInt");
 
 end GDT.JNI_Functions;
