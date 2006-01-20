c6f4ec065a7020341290d4458cb0e41e050065ed
Florian Villoing <villoing@adacore.com>
2006-01-20 11:02:36 +0000

    (Is_In_Comment): Relax assert condition so that there is no restriction on  the language case sensitivity. For EB16-002.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@94970 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/src_editor_buffer.adb
+++ src_editor/src/src_editor_buffer.adb
@@ -2291,12 +2291,10 @@ package body Src_Editor_Buffer is
       --  have a proper support in every context.
       Traces.Assert
         (Exception_Handle,
-         not Lang_Context.Case_Sensitive
-         and then
-           (Lang_Context.Comment_Start_Length = 0
-            or else Lang_Context.Comment_Start_Length = 0
-            or else Lang_Context.New_Line_Comment_Start /= null
-            or else Lang_Context.New_Line_Comment_Start_Regexp /= null),
+         Lang_Context.Comment_Start_Length = 0
+           or else Lang_Context.Comment_Start_Length = 0
+           or else Lang_Context.New_Line_Comment_Start /= null
+           or else Lang_Context.New_Line_Comment_Start_Regexp /= null,
          "Is_In_Comment not supported for multi-line comments");
 
       Get_Iter_Position (Buffer, Iter, Line, Column);
