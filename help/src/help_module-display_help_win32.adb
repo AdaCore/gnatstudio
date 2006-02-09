b8d263a66461c3b1aa5e70f4efcec7ef28d8e703
Pascal Obry <obry@adacore.com>
2006-02-09 12:35:33 +0000

    (System): Add with clause for this separate unit as it was removed from the  main unit. Fix build failure on Windows.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@95225 936e1b1b-40f2-da11-902a-00137254ae57

--- help/src/help_module-display_help_win32.adb
+++ help/src/help_module-display_help_win32.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                     Copyright (C) 2001-2005                       --
+--                      Copyright (C) 2001-2006                      --
 --                              AdaCore                              --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
@@ -17,6 +17,8 @@
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
+
+with System;
 
 separate (Help_Module)
 procedure Display_Help
