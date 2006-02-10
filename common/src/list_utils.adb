407efef4fa23f1fe9e70b30423ab7430de02db49
Florian Villoing <villoing@adacore.com>
2006-02-10 10:36:45 +0000

    Remove redundant with clause.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@95245 936e1b1b-40f2-da11-902a-00137254ae57

--- common/src/list_utils.adb
+++ common/src/list_utils.adb
@@ -18,8 +18,6 @@
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
-with Generic_List;
-
 package body List_Utils is
 
    ----------
