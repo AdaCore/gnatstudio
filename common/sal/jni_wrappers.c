406bd4215f0a4d7928b8daf6210107b243b38961
Quentin Ochem <ochem@adacore.com>
2006-02-10 17:18:20 +0000

    (Java_com_adacore_gnatbench_core_gpsjni_GPSJni_libInitInt ): fixed profile. Added wrappers to set/get/free SEGV handlers, for both windows and non  windows cases. For F210-021
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@95266 936e1b1b-40f2-da11-902a-00137254ae57

--- common/sal/jni_wrappers.c
+++ common/sal/jni_wrappers.c
@@ -1,5 +1,4 @@
 #include <jni.h>
-
 // TODO: See if there are existing C profiles, in order to avoid the
 // use of those wrappers.
 
@@ -45,13 +44,59 @@ void W_CallVoidMethodIIIS
    (*Env)->CallVoidMethod (Env, Object, Id, P1, P2, P3, P4);
 }
 
+#ifdef __MANAGE_SEGV_HANDLER__
+
+/* This case has to be used on solaris or linux, by setting 
+ * -D__MANAGE_SEGV_HANDLERS__ on the compilation line of this file */
+
+#include <signal.h>
+
+jint Java_com_adacore_gnatbench_core_SystemHandler_getSEGVHandlerInt (JNIEnv * Env, jclass * this)
+{
+   struct sigaction * handler = malloc (sizeof (sigaction));
+   
+   sigaction (SIGSEGV, NULL, handler);
+   
+   return jint (handler);
+}
+
+void Java_com_adacore_gnatbench_core_gpsjni_SystemHandler_setSEGVHandlerInt (JNIEnv * Env, jclass * this, jint addr)
+{
+   struct sigaction * handler = (struct sigaction *) addr;
+   
+   sigaction (SIGSEGV, handler, NULL);
+}
+
+void java_com_adacore_gnatbench_core_gpsjni_SystemHandler_freeSEGVHandlerInt (JNIEnv * Env, jclass * this, jint addr)
+{
+   struct sigaction * handler = (struct sigaction *) addr;
+   free (handler);
+}
+
+#else
+
+jint Java_com_adacore_gnatbench_core_gpsjni_SystemHandler_getSEGVHandlerInt (JNIEnv * Env, jclass * this)
+{
+   return 0;
+}
+
+void Java_com_adacore_gnatbench_core_gpsjni_SystemHandler_setSEGVHandlerInt (JNIEnv * Env, jclass * this, jint addr)
+{
+}
+
+void Java_com_adacore_gnatbench_core_gpsjni_SystemHandler_freeSEGVHandlerInt (JNIEnv * Env, jclass * this, jint addr)
+{
+}
+
+#endif
+
 void gps_utilsinit (void);
 
 /**
  * This function is used to manually initialize the ada library (since there
  * is no way to do it automatically under win32).
  */
-void Java_com_adacore_gdt_study_gpsjni_GPSJni_libInitInt 
+void Java_com_adacore_gnatbench_core_gpsjni_GPSJni_libInitInt 
    (JNIEnv * Env, jclass * this) 
 {
    gps_utilsinit ();
