e6bb31acbe3ea1fccefa8f633e68559d4baa6b5a
Pascal Obry <obry@adacore.com>
2006-02-16 21:14:18 +0000

    Initial revision. Implements a DB cache for the file to ignore in the VCS Explorer. -- Add support for files to ignore. The default CVS implementation is to use the .cvsignore file. Work for D319-023.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@95385 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ vcs/src/ignore_db.adb
@@ -0,0 +1,123 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2006                         --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Ada.Strings.Fixed;          use Ada.Strings.Fixed; use Ada.Strings;
+with Ada.Text_IO;                use Ada.Text_IO;
+with Ada.Unchecked_Deallocation;
+
+with GNAT.OS_Lib;                use GNAT;
+
+with Basic_Types;                use Basic_Types;
+with String_Hash;
+
+package body Ignore_Db is
+
+   --  Table of files
+
+   Not_Null : constant String_Access := new String'("");
+
+   procedure No_Free (Str : in out String_Access);
+
+   package Files_Table is new String_Hash (String_Access, No_Free, null);
+   use Files_Table;
+
+   type File_Table_Access is access String_Hash_Table.HTable;
+
+   --  Table of directory containing ignore file
+
+   procedure Free is new Ada.Unchecked_Deallocation
+     (String_Hash_Table.HTable, File_Table_Access);
+
+   package Dir_Table is new String_Hash (File_Table_Access, Free, null);
+
+   DB : Dir_Table.String_Hash_Table.HTable;
+
+   function Load (Ignore_File : String) return File_Table_Access;
+   --  Load Ignore_File and store the values into DB. Returns the new table
+   --  created.
+
+   -----------------
+   -- Ignore_File --
+   -----------------
+
+   function Ignore_File
+     (VCS  : VCS_Access;
+      File : Virtual_File)
+      return Boolean
+   is
+      Ign_File : constant String :=
+                   Dir_Name (File).all & Ignore_Filename (VCS);
+      Files    : File_Table_Access :=
+                   Dir_Table.String_Hash_Table.Get (DB, Ign_File);
+   begin
+      if Files = null
+        and then OS_Lib.Is_Regular_File (Ign_File)
+      then
+         Files := Load (Ign_File);
+      end if;
+
+      if Files /= null then
+         return String_Hash_Table.Get
+           (Files.all, Base_Name (File)) /= null;
+      else
+         return False;
+      end if;
+   end Ignore_File;
+
+   ----------
+   -- Load --
+   ----------
+
+   function Load (Ignore_File : String) return File_Table_Access is
+      Files  : constant File_Table_Access := new String_Hash_Table.HTable;
+      File   : File_Type;
+      Buffer : String (1 .. 256);
+      Last   : Natural;
+   begin
+      --  Add all files read from Ignore_File
+
+      Open (File, In_File, Ignore_File);
+
+      while not End_Of_File (File) loop
+         Get_Line (File, Buffer, Last);
+         String_Hash_Table.Set
+           (Files.all, Trim (Buffer (1 .. Last), Both), Not_Null);
+      end loop;
+
+      Close (File);
+
+      --  Register this table
+
+      Dir_Table.String_Hash_Table.Set (DB, Ignore_File, Files);
+
+      return Files;
+   end Load;
+
+   -------------
+   -- No_Free --
+   -------------
+
+   procedure No_Free (Str : in out String_Access) is
+      pragma Unreferenced (Str);
+   begin
+      null;
+   end No_Free;
+
+end Ignore_Db;
---
+++ vcs/src/ignore_db.ads
@@ -0,0 +1,37 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2006                         --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with VCS; use VCS;
+with VFS; use VFS;
+
+with GPS.Kernel;
+
+package Ignore_Db is
+
+   function Ignore_File
+     (VCS  : VCS_Access;
+      File : Virtual_File) return Boolean;
+   --  Returns True if the given file is to be ignored. This depends on the
+   --  VCS. If a specific ignore file is set it is used to check wether a file
+   --  must be ignored or not. On CVS this is typically what is done with the
+   --  file .cvsignore. This implementation can used any filename and supports
+   --  any VCS. An ingored file will not be displyed on the VCS Explorer.
+
+end Ignore_Db;
