fe26d897364ad567b727c6f6d9465c91b9ef243f
Pascal Obry <obry@adacore.com>
2006-03-15 21:48:17 +0000

    (On_Menu_View_Log_Text): New routine. For the new view history as text menu  entry. -- Implementation of the new Revision Browser. For this 5 new commands have been implemented. Three on the new Revision class :  add_log, add_link and add_revision and two on the VCS class : log_parse and revision_parse. A revision browser is used to display the revision tree for a single file. Work for F315-016.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@95771 936e1b1b-40f2-da11-902a-00137254ae57

--- vcs/src/vcs_view_api.ads
+++ vcs/src/vcs_view_api.ads
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                     Copyright (C) 2001-2006                       --
+--                      Copyright (C) 2001-2006                      --
 --                              AdaCore                              --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
@@ -121,6 +121,11 @@ package VCS_View_API is
    --  ???
 
    procedure On_Menu_View_Log
+     (Widget  : access GObject_Record'Class;
+      Context : Selection_Context);
+   --  ???
+
+   procedure On_Menu_View_Log_Text
      (Widget  : access GObject_Record'Class;
       Context : Selection_Context);
    --  ???
