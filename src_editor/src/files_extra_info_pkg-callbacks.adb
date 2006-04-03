d6f2f3fda5ee99dc4ec06b7b66fb4a658edc4cf7
Arnaud Charlet <charlet@adacore.com>
2006-04-03 10:46:06 +0000

    Update copyright notice
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@96215 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/files_extra_info_pkg-callbacks.adb
+++ src_editor/src/files_extra_info_pkg-callbacks.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                      Copyright (C) 2001-2005                      --
+--                      Copyright (C) 2001-2006                      --
 --                               AdaCore                             --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
