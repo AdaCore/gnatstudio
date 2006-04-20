6c1c24f6b7214d674e5edf03df6433226fef98b1
Nicolas Setton <setton@adacore.com>
2006-04-20 12:43:18 +0000

    Initial revision.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@96439 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ widgets/src/completion_window.adb
@@ -0,0 +1,424 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2006                         --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Glib;                   use Glib;
+with Glib.Values;            use Glib.Values;
+
+with Gdk.Event; use Gdk.Event;
+with Gdk.Types; use Gdk.Types;
+with Gdk.Types.Keysyms; use Gdk.Types.Keysyms;
+with Gdk.Rectangle; use Gdk.Rectangle;
+with Gdk.Window;             use Gdk.Window;
+
+with Gtk.Handlers; use Gtk.Handlers;
+with Gtk.Frame; use Gtk.Frame;
+with Gtk.Enums;              use Gtk.Enums;
+with Gtk.Tree_Model;         use Gtk.Tree_Model;
+with Gtk.Tree_Selection;     use Gtk.Tree_Selection;
+with Gtk.Tree_View_Column;   use Gtk.Tree_View_Column;
+with Gtk.Cell_Renderer_Text; use Gtk.Cell_Renderer_Text;
+with Gtk.Style;              use Gtk.Style;
+
+with Ada.Exceptions;            use Ada.Exceptions;
+with Ada.Unchecked_Deallocation;
+
+with Traces;                    use Traces;
+
+package body Completion_Window is
+
+   Markup_Column : constant := 0;
+   Index_Column  : constant := 1;
+
+   procedure Unchecked_Free is new Ada.Unchecked_Deallocation
+     (String, String_Access);
+
+   procedure Unchecked_Free is new Ada.Unchecked_Deallocation
+     (Information_Array, Information_Array_Access);
+
+   package Return_Cb is new Gtk.Handlers.Return_Callback
+     (Completion_Window_Record, Boolean);
+   use Return_Cb;
+
+   package Cb is new Gtk.Handlers.Callback (Completion_Window_Record);
+   use Cb;
+
+   Column_Types : constant GType_Array :=
+     (Markup_Column => GType_String,
+      Index_Column  => GType_Int);
+
+   function On_Focus_Out
+     (Window : access Completion_Window_Record'Class;
+      Event  : Gdk_Event) return Boolean;
+   --  Callback for a focus_out_event on the tree view.
+
+   function On_Key_Press
+     (Window : access Completion_Window_Record'Class;
+      Event  : Gdk_Event) return Boolean;
+   --  Callback for a key_press_event on the text view
+
+   procedure Insert_Text_Handler
+     (Window : access Completion_Window_Record'Class;
+      Params : Glib.Values.GValues);
+   --  Callback after a text insertion.
+
+   procedure Delete (Window : access Completion_Window_Record'Class);
+   --  Destroy the window and its contents.
+
+   procedure Free (X : in out Information_Record);
+   --  Free memory associated to X.
+
+   ----------
+   -- Free --
+   ----------
+
+   procedure Free (X : in out Information_Record) is
+   begin
+      Unchecked_Free (X.Markup);
+      Unchecked_Free (X.Text);
+      Unchecked_Free (X.Notes);
+   end Free;
+
+   ------------
+   -- Delete --
+   ------------
+
+   procedure Delete (Window : access Completion_Window_Record'Class) is
+   begin
+      for J in 1 .. Window.Index - 1 loop
+         Free (Window.Info (Window.Index));
+      end loop;
+
+      Unchecked_Free (Window.Info);
+
+      Destroy (Window);
+   end Delete;
+
+   -------------------------
+   -- Insert_Text_Handler --
+   -------------------------
+
+   procedure Insert_Text_Handler
+     (Window : access Completion_Window_Record'Class;
+      Params : Glib.Values.GValues)
+   is
+      Iter   : Gtk_Text_Iter;
+      Beg    : Gtk_Text_Iter;
+
+      Prev   : Gtk_Tree_Iter;
+      Curr   : Gtk_Tree_Iter;
+
+      Selection : Gtk_Tree_Selection;
+      Model     : Gtk_Tree_Model;
+
+      Previously_Selected : Natural := 0;
+   begin
+      Get_Text_Iter (Nth (Params, 1), Iter);
+      Get_Iter_At_Mark (Window.Buffer, Beg, Window.Mark);
+
+      declare
+         UTF8 : constant UTF8_String := Get_Text (Window.Buffer, Beg, Iter);
+      begin
+         Trace (Exception_Handle,
+                "inserted #" & UTF8 & "#");
+
+         Selection := Get_Selection (Window.View);
+
+         --  Find the entry currently being selected
+         Get_Selected (Selection, Model, Curr);
+
+         if Curr /= Null_Iter then
+            Previously_Selected :=
+              Natural (Get_Int (Window.Model, Curr, Index_Column));
+         end if;
+
+         --  Clear the model
+
+         Clear (Window.Model);
+
+         --  Browse through the completion possibilities and filter out the
+         --  lines that don't match.
+
+         Prev := Null_Iter;
+
+         for J in 1 .. Window.Index - 1 loop
+            if Window.Info (J).Text'Length >= UTF8'Length
+              and then Window.Info (J).Text
+              (Window.Info (J).Text'First
+               .. Window.Info (J).Text'First - 1 + UTF8'Length) = UTF8
+            then
+               Append (Window.Model, Curr);
+
+               Set (Window.Model, Curr, Markup_Column,
+                    Window.Info (J).Markup.all);
+               Set (Window.Model, Curr, Index_Column, Gint (J));
+
+               if J = Previously_Selected then
+                  Iter_Copy (Curr, Prev);
+               end if;
+            end if;
+         end loop;
+
+         --  Re-select the item previously selected, or, if there was none,
+         --  select the first iter
+
+         if Prev /= Null_Iter then
+            Select_Iter (Selection, Prev);
+         else
+            Prev := Get_Iter_First (Window.Model);
+
+            if Prev = Null_Iter then
+               --  If there is no entry in the tree, delete the window.
+               Delete (Window);
+               return;
+            else
+               Select_Iter (Selection, Prev);
+            end if;
+         end if;
+      end;
+
+   exception
+      when E : others =>
+         Trace (Exception_Handle,
+                "Unexpected exception: " & Exception_Information (E));
+   end Insert_Text_Handler;
+
+   ------------------
+   -- On_Focus_Out --
+   ------------------
+
+   function On_Focus_Out
+     (Window : access Completion_Window_Record'Class;
+      Event  : Gdk_Event) return Boolean
+   is
+      pragma Unreferenced (Event);
+   begin
+      Delete (Window);
+      return False;
+   exception
+      when E : others =>
+         Trace (Exception_Handle,
+                "Unexpected exception: " & Exception_Information (E));
+         return False;
+   end On_Focus_Out;
+
+   ------------------
+   -- On_Key_Press --
+   ------------------
+
+   function On_Key_Press
+     (Window : access Completion_Window_Record'Class;
+      Event  : Gdk_Event) return Boolean
+   is
+      Key   : Gdk_Key_Type;
+      Sel   : Gtk_Tree_Selection;
+      Iter  : Gtk_Tree_Iter;
+      Model : Gtk_Tree_Model;
+      Path  : Gtk_Tree_Path;
+   begin
+      Key := Get_Key_Val (Event);
+
+      --  Case by case basis
+
+      case Key is
+         when GDK_Escape =>
+            Delete (Window);
+            return True;
+
+         when GDK_Return =>
+            --  ??? Insert the text
+            Destroy (Window);
+            return True;
+
+         when GDK_Down | GDK_KP_Down =>
+            Sel := Get_Selection (Window.View);
+            Get_Selected (Sel, Model, Iter);
+            Next (Window.Model, Iter);
+
+            if Iter = Null_Iter then
+               Iter := Get_Iter_First (Window.Model);
+            end if;
+
+            Select_Iter (Sel, Iter);
+            return True;
+
+         when GDK_Up | GDK_KP_Up =>
+            Sel := Get_Selection (Window.View);
+            Get_Selected (Sel, Model, Iter);
+            Path := Get_Path (Window.Model, Iter);
+
+            if Prev (Path) then
+               Iter := Get_Iter (Window.Model, Path);
+            else
+               Iter := Nth_Child
+                 (Window.Model, Null_Iter, N_Children (Window.Model) - 1);
+            end if;
+
+            Path_Free (Path);
+
+            Select_Iter (Sel, Iter);
+            return True;
+
+         when others =>
+            null;
+      end case;
+
+      return False;
+
+   exception
+      when E : others =>
+         Trace (Exception_Handle,
+                "Unexpected exception: " & Exception_Information (E));
+         return False;
+   end On_Key_Press;
+
+   -------------
+   -- Gtk_New --
+   -------------
+
+   procedure Gtk_New (Window : out Completion_Window_Access) is
+   begin
+      Window := new Completion_Window_Record;
+      Completion_Window.Initialize (Window);
+   end Gtk_New;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize (Window : access Completion_Window_Record'Class) is
+      Col   : Gtk_Tree_View_Column;
+      Text  : Gtk_Cell_Renderer_Text;
+      Dummy : Gint;
+      Frame : Gtk_Frame;
+      pragma Unreferenced (Dummy);
+   begin
+      Gtk.Window.Initialize (Window, Window_Popup);
+      Set_Decorated (Window, False);
+
+      Gtk_New (Window.Model, Column_Types);
+      Gtk_New (Window.View, Window.Model);
+      Set_Headers_Visible (Window.View, False);
+
+      Gtk_New (Col);
+      Dummy := Append_Column (Window.View, Col);
+
+      Gtk_New (Text);
+      Pack_Start (Col, Text, True);
+      Add_Attribute (Col, Text, "markup", Markup_Column);
+
+      Gtk_New (Frame);
+      Add (Frame, Window.View);
+      Add (Window, Frame);
+
+      Window.Info := new Information_Array (1 .. 1024);
+      Window.Index := 1;
+   end Initialize;
+
+   ------------------
+   -- Add_Contents --
+   ------------------
+
+   procedure Add_Contents
+     (Window     : Completion_Window_Access;
+      Markup     : String;
+      Completion : String;
+      Notes      : String)
+   is
+      Iter : Gtk_Tree_Iter;
+      Info : Information_Record;
+   begin
+      Info :=
+        (new String'(Markup), new String'(Completion), new String'(Notes),
+         True);
+
+      Window.Info (Window.Index) := Info;
+      Window.Index := Window.Index + 1;
+
+      Append (Window.Model, Iter);
+
+      Set (Window.Model, Iter, Markup_Column, Markup);
+      Set (Window.Model, Iter, Index_Column, Gint (Window.Index - 1));
+   end Add_Contents;
+
+   ----------
+   -- Show --
+   ----------
+
+   procedure Show
+     (Window : Completion_Window_Access;
+      View   : Gtk_Text_View;
+      Buffer : Gtk_Text_Buffer;
+      Iter   : Gtk_Text_Iter)
+   is
+      Iter_Coords : Gdk_Rectangle;
+      Window_X, Window_Y : Gint;
+      Gdk_X, Gdk_Y       : Gint;
+      Dummy : Boolean;
+   begin
+      --  ??? Set size according to screen space
+
+      Window.Text := View;
+      Window.Buffer := Buffer;
+      Window.Mark := Create_Mark (Buffer, "", Iter);
+
+      --  Set the position.
+
+      Get_Iter_Location (View, Iter, Iter_Coords);
+
+      Buffer_To_Window_Coords
+        (View, Text_Window_Text,
+         Iter_Coords.X,
+         Iter_Coords.Y + Iter_Coords.Height + 1,
+         Window_X, Window_Y);
+
+      Get_Desk_Relative_Origin
+        (Get_Window (View, Text_Window_Text), Gdk_X, Gdk_Y, Dummy);
+      --  ??? should we check for the result ?
+
+      --  ??? Uposition should take into account the current desktop.
+
+      Set_UPosition (Window, Gdk_X + Window_X, Gdk_Y + Window_Y);
+
+      Modify_Font (Window.View, Get_Font_Description (Get_Style (View)));
+
+      Show_All (Window);
+
+      Object_Connect
+        (Window.Text, "focus_out_event",
+            To_Marshaller (On_Focus_Out'Access), Window, After => False);
+
+      Object_Connect
+        (Window.Text, "button_press_event",
+         To_Marshaller (On_Focus_Out'Access), Window, After => False);
+
+      Object_Connect
+        (Window.Text, "key_press_event",
+         To_Marshaller (On_Key_Press'Access), Window, After => False);
+
+      Object_Connect
+        (Buffer, "insert_text", Insert_Text_Handler'Access, Window,
+         After => True);
+
+      Object_Connect
+        (Buffer, "delete_range", Insert_Text_Handler'Access, Window,
+         After => True);
+   end Show;
+
+end Completion_Window;
