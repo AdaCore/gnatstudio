0bdf178dd1d69c4533da24bdd60167ff18ef6727
Nicolas Setton <setton@adacore.com>
2006-05-09 16:03:37 +0000

    Remove unneeded pixmaps. For F419-008.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@96779 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/pixmaps/pixmaps.c
+++ gvd/pixmaps/pixmaps.c
@@ -1,3 +1,23 @@
+/*---------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                      Copyright (C) 2001-2006                      --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+---------------------------------------------------------------------*/
+
 #include "arrow_xpm"
 #include "box_xpm"
 #include "break_xpm"
@@ -9,9 +29,6 @@
 #include "grey_dot_xpm"
 #include "i_page_xpm"
 #include "lock_xpm"
-#include "mini-folder_xpm"
-#include "mini-ofolder_xpm"
-#include "mini_page_xpm"
 #include "package_xpm"
 #include "paint_xpm"
 #include "stop_xpm"
