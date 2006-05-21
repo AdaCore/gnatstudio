5900367065a1c8cb3f4ea181aafe7c7146625c9c
Pascal Obry <obry@adacore.com>
2006-05-21 08:03:05 +0000

    (Initialize): Kill warning on Old_Handler (useless assignment). Related to F518-012.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@96877 936e1b1b-40f2-da11-902a-00137254ae57

--- python/src/python-gui.adb
+++ python/src/python-gui.adb
@@ -1,8 +1,8 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                     Copyright (C) 2003-2005                       --
---                            AdaCore                                --
+--                      Copyright (C) 2003-2006                      --
+--                              AdaCore                              --
 --                                                                   --
 -- GPS is free  software; you can  redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -161,6 +161,7 @@ package body Python.GUI is
       Main_Module    : PyObject;
       Sigint         : constant Integer := 2;
       Old_Handler    : System.Address;
+      pragma Warnings (Off, Old_Handler);
       Prompt         : PyObject;
       Ignored        : Integer;
       pragma Unreferenced (Ignored);
