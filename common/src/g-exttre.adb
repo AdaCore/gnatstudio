5320caa2c8719329c520e45038f55deca1caa620
Jerome Lambourg <lambourg@adacore.com>
2006-06-13 15:19:22 +0000

    (Get_Or_Init_Session): Correct strange case on windows happening on first line  received from ssh session... Part of C328-004
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@97287 936e1b1b-40f2-da11-902a-00137254ae57

--- common/src/g-exttre.adb
+++ common/src/g-exttre.adb
@@ -780,6 +780,9 @@ package body GNAT.Expect.TTY.Remote is
          --  Wait for connection confirmation
 
          Wait_For_Prompt (True);
+         Send (Descriptor.Machine.Sessions (Session_Nb).Pd, "");
+         --  Send just a LF to force Windows console to scroll, and thus
+         --  correctly init the expect interface... strange...
 
          --  Determine if the machine echoes commands
 
