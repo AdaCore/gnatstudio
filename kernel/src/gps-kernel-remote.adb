7d31229c835bf217a0d9687e3736ff1da99e04e1
Jerome Lambourg <lambourg@adacore.com>
2006-07-04 07:52:11 +0000

    (Spawn): Catch exception raised by gexpect and replace the message by a more  explicit one. Part of F703-011
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/branches/gps-4_0-branch@97813 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/gps-kernel-remote.adb
+++ kernel/src/gps-kernel-remote.adb
@@ -4015,25 +4015,31 @@ package body GPS.Kernel.Remote is
 
          --  Set buffer_size to 0 for dynamically allocated buffer (prevents
          --  possible overflow)
+         begin
+            if L_Args /= null then
+               Non_Blocking_Spawn
+                 (Pd.all,
+                  L_Args (L_Args'First).all,
+                  L_Args (L_Args'First + 1 .. L_Args'Last) &
+                  Args.all,
+                  Buffer_Size => 0,
+                  Err_To_Out  => True);
+               Free (L_Args);
 
-         if L_Args /= null then
-            Non_Blocking_Spawn
-              (Pd.all,
-               L_Args (L_Args'First).all,
-               L_Args (L_Args'First + 1 .. L_Args'Last) &
-               Args.all,
-               Buffer_Size => 0,
-               Err_To_Out  => True);
-            Free (L_Args);
+            else
+               Non_Blocking_Spawn
+                 (Pd.all,
+                  Args (Args'First).all,
+                  Args (Args'First + 1 .. Args'Last),
+                  Buffer_Size => 0,
+                  Err_To_Out  => True);
+            end if;
 
-         else
-            Non_Blocking_Spawn
-              (Pd.all,
-               Args (Args'First).all,
-               Args (Args'First + 1 .. Args'Last),
-               Buffer_Size => 0,
-               Err_To_Out  => True);
-         end if;
+         exception
+            when Invalid_Process =>
+               Raise_Exception
+                 (Invalid_Process'Identity, "not an executable");
+         end;
 
          if Directory /= "" then
             Change_Dir (Old_Dir.all);
