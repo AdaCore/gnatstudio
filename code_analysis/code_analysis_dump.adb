1ca150e385ac870e8dc4d1a14ea9b0ece1dd1a96
Simon Descarpentries <descarpe@adacore.com>
2006-07-17 18:50:32 +0000

    Intial revision:  - code_analysis package contains tree structure to store code analysis information  - code_analysis_dump package contains subprograms that realize a text dumping of the code_analysis tree structure to the standard output  - code_coverage package will contain the code coverage user API (but now only a tiny dumping subprogram for the coverage record of every tree node)  - integer_hash package contain a hash subprogram design to be used by hash tables or Hashed_Maps.
     - the main subprogram just build a basic code_analysis tree structure, and dump it to the standard output.
    
    
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@97997 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ code_analysis/code_analysis_dump.adb
@@ -0,0 +1,133 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2006                         --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is Free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Ada.Text_IO; use Ada.Text_IO;
+with Code_Coverage; use Code_Coverage;
+
+package body Code_Analysis_Dump is
+
+   ---------------
+   -- Dump_Text --
+   ---------------
+
+   procedure Dump_Text is
+   begin
+      Projects.Iterate (Dump_Project'Access);
+   end Dump_Text;
+
+   ------------------
+   -- Dump_Project --
+   ------------------
+
+   procedure Dump_Project (C : Project_Maps.Cursor) is
+      P_A : constant Project_Access := Project_Maps.Element (C);
+   begin
+      Put (" O¦ Project " & Get_Id (P_A));
+      if Get_Analysis (P_A) /= null then
+         Dump_Coverage (Get_Analysis (P_A).Coverage_Data);
+      end if;
+      Put (" : ");
+      Get_Map (P_A).Iterate (Dump_Children_Names'Access);
+      Put (ASCII.lf);
+      Get_Map (P_A).Iterate (Dump_File'Access);
+   end Dump_Project;
+
+   ---------------
+   -- Dump_File --
+   ---------------
+
+   procedure Dump_File (C : File_Maps.Cursor) is
+      F_A : constant Code_Analysis.File_Access := File_Maps.Element (C);
+   begin
+      Put ("   o¦ File " & Get_Id (F_A));
+      if Get_Analysis (F_A) /= null then
+         Dump_Coverage (Get_Analysis (F_A).Coverage_Data);
+      end if;
+      Put (" : ");
+      Get_Map (F_A).Iterate (Dump_Children_Names'Access);
+      Put (ASCII.lf);
+      Get_Map (F_A).Iterate (Dump_Subprogram'Access);
+   end Dump_File;
+
+   ---------------------
+   -- Dump_Subprogram --
+   ---------------------
+
+   procedure Dump_Subprogram (C : Subprogram_Maps.Cursor) is
+      S_A : constant Subprogram_Access := Subprogram_Maps.Element (C);
+   begin
+      Put ("     °¦ Subprogram " & Get_Id (S_A));
+      if Get_Analysis (S_A) /= null then
+         Dump_Coverage (Get_Analysis (S_A).Coverage_Data);
+      end if;
+      Put (" : ");
+      Get_Map (S_A).Iterate (Dump_Children_Names'Access);
+      Put (ASCII.lf);
+      Get_Map (S_A).Iterate (Dump_Line'Access);
+   end Dump_Subprogram;
+
+   ---------------
+   -- Dump_Line --
+   ---------------
+
+   procedure Dump_Line (C : Line_Maps.Cursor) is
+      L_A : constant Line_Access := Line_Maps.Element (C);
+   begin
+      Put ("       ·¦ Line " & Integer'Image (Get_Id (L_A)));
+      if Get_Analysis (L_A) /= null then
+         Dump_Coverage (Get_Analysis (L_A).Coverage_Data);
+      end if;
+      Put (" : ");
+      Put (ASCII.lf);
+   end Dump_Line;
+
+   ------------------------
+   -- Dump_Children_Name --
+   ------------------------
+
+   procedure Dump_Children_Names (C : File_Maps.Cursor) is
+      F_A : constant Code_Analysis.File_Access := File_Maps.Element (C);
+   begin
+      Put (Get_Id (F_A) & "; ");
+   end Dump_Children_Names;
+
+   ------------------------
+   -- Dump_Children_Name --
+   ------------------------
+
+   procedure Dump_Children_Names (C : Subprogram_Maps.Cursor) is
+      S_A : constant Code_Analysis.Subprogram_Access
+        := Subprogram_Maps.Element (C);
+   begin
+      Put (Get_Id (S_A) & "; ");
+   end Dump_Children_Names;
+
+   ------------------------
+   -- Dump_Children_Name --
+   ------------------------
+
+   procedure Dump_Children_Names (C : Line_Maps.Cursor) is
+      L_A : constant Code_Analysis.Line_Access
+        := Line_Maps.Element (C);
+   begin
+      Put (Integer'Image (Get_Id (L_A)) & "; ");
+   end Dump_Children_Names;
+
+end Code_Analysis_Dump;
---
+++ code_analysis/main.adb
@@ -0,0 +1,68 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2006                         --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is Free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Code_Analysis; use Code_Analysis;
+with Ada.Text_IO; use Ada.Text_IO;
+with Code_Analysis_Dump; use Code_Analysis_Dump;
+
+procedure Main is
+   P : constant Project_Access := Get_Or_Create_Project ("trucages.txt");
+
+   F1 : Code_Analysis.File_Access;
+   F2 : Code_Analysis.File_Access;
+
+   S1 : Subprogram_Access;
+   S2 : Subprogram_Access;
+
+   L1 : Line_Access;
+   L2 : Line_Access;
+
+   A2 : Analysis_Access;
+begin
+
+   F1 := Get_Or_Create_File (P, "fichier1.txt");
+   F2 := Get_Or_Create_File (P, "fichier2.txt");
+   Put_Line (" * Created File :" & Get_Id (F1));
+   Put_Line (" * Created File :" & Get_Id (F2));
+
+   S1 := Get_Or_Create_Subprogram (F1, "Subprog1.txt");
+   S2 := Get_Or_Create_Subprogram (F2, "Subprog2.txt");
+   Put_Line (" * Created Subprogram :" & Get_Id (S1));
+   Put_Line (" * Created Subprogram :" & Get_Id (S2));
+
+   A2 := new Analysis;
+   A2.Coverage_Data := new Coverage;
+   A2.Coverage_Data.Total_Child_Count := 42;
+   Set_Analysis (S2, A2);
+   Put_Line (" · Subprogram " & Get_Id (S2) & " coverage data" & Integer'Image
+             (A2.Coverage_Data.Total_Child_Count));
+
+   L1 := Get_Or_Create_Line (S1, 1);
+   L2 := Get_Or_Create_Line (S1, 2);
+
+   Put_Line (" * Created Line :" & Integer'Image (Get_Id (L1)));
+   Put_Line (" * Created Line :" & Integer'Image (Get_Id (L2)));
+
+   Put_Line (" * ******");
+
+   Dump_Text;
+
+   Free_Project (P);
+end Main;
