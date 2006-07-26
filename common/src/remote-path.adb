6203044be5a9ad86741bafd1444c4b8b9d185210
Jerome Lambourg <lambourg@adacore.com>
2006-07-26 11:49:44 +0000

    New package.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@98259 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ common/src/remote-path.adb
@@ -0,0 +1,218 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2006                         --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+package body Remote.Path is
+
+   ----------
+   -- Init --
+   ----------
+
+   procedure Init
+     (Mirror          : in out Mirror_Path'Class;
+      Local_Path      : String;
+      Remote_Path     : String;
+      Synchronisation : Synchronisation_Type)
+   is
+   begin
+      Mirror.Actual :=
+        (Local_Path  => new String'(Local_Path),
+         Remote_Path => new String'(Remote_Path),
+         Sync        => Synchronisation);
+      Mirror.Tentative :=
+        (Local_Path  => null,
+         Remote_Path => null,
+         Sync        => Never);
+      Mirror.Tentative_Sync_Set := False;
+   end Init;
+
+   --------------------
+   -- Get_Local_Path --
+   --------------------
+
+   function Get_Local_Path
+     (Mirror    : Mirror_Path;
+      Tentative : Boolean := False)
+      return String is
+   begin
+      if not Tentative or else Mirror.Tentative.Local_Path = null then
+         if Mirror.Actual.Local_Path = null then
+            return "";
+         else
+            return Mirror.Actual.Local_Path.all;
+         end if;
+
+      else
+         return Mirror.Tentative.Local_Path.all;
+      end if;
+   end Get_Local_Path;
+
+   ---------------------
+   -- Get_Remote_Path --
+   ---------------------
+
+   function Get_Remote_Path
+     (Mirror    : Mirror_Path;
+      Tentative : Boolean := False)
+      return String is
+   begin
+      if not Tentative or else Mirror.Tentative.Remote_Path = null then
+         if Mirror.Actual.Remote_Path = null then
+            return "";
+         else
+            return Mirror.Actual.Remote_Path.all;
+         end if;
+
+      else
+         return Mirror.Tentative.Remote_Path.all;
+      end if;
+   end Get_Remote_Path;
+
+   -------------------------
+   -- Get_Synchronisation --
+   -------------------------
+
+   function Get_Synchronisation
+     (Mirror : Mirror_Path;
+      Tentative : Boolean := False)
+      return Synchronisation_Type is
+   begin
+      if not Tentative or else not Mirror.Tentative_Sync_Set then
+         return Mirror.Actual.Sync;
+      else
+         return Mirror.Tentative.Sync;
+      end if;
+   end Get_Synchronisation;
+
+   ------------------------------
+   -- Set_Tentative_Local_Path --
+   ------------------------------
+
+   procedure Set_Tentative_Local_Path
+     (Mirror : in out Mirror_Path;
+      Path   : String) is
+   begin
+      if Mirror.Tentative.Local_Path /= null then
+         Free (Mirror.Tentative.Local_Path);
+      end if;
+
+      Mirror.Tentative.Local_Path := new String'(Path);
+   end Set_Tentative_Local_Path;
+
+   -------------------------------
+   -- Set_Tentative_Remote_Path --
+   -------------------------------
+
+   procedure Set_Tentative_Remote_Path
+     (Mirror : in out Mirror_Path;
+      Path   : String) is
+   begin
+      if Mirror.Tentative.Remote_Path /= null then
+         Free (Mirror.Tentative.Remote_Path);
+      end if;
+
+      Mirror.Tentative.Remote_Path := new String'(Path);
+   end Set_Tentative_Remote_Path;
+
+   -----------------------------------
+   -- Set_Tentative_Synchronisation --
+   -----------------------------------
+
+   procedure Set_Tentative_Synchronisation
+     (Mirror          : in out Mirror_Path;
+      Synchronisation : Synchronisation_Type)
+   is
+   begin
+      Mirror.Tentative_Sync_Set := True;
+      Mirror.Tentative.Sync := Synchronisation;
+   end Set_Tentative_Synchronisation;
+
+   -----------
+   -- Apply --
+   -----------
+
+   procedure Apply (Mirror : in out Mirror_Path) is
+   begin
+      if Mirror.Tentative.Local_Path /= null then
+         if Mirror.Actual.Local_Path /= null then
+            Free (Mirror.Actual.Local_Path);
+         end if;
+
+         Mirror.Actual.Local_Path := Mirror.Tentative.Local_Path;
+         Mirror.Tentative.Local_Path := null;
+      end if;
+
+      if Mirror.Tentative.Remote_Path /= null then
+         if Mirror.Actual.Remote_Path /= null then
+            Free (Mirror.Actual.Remote_Path);
+         end if;
+
+         Mirror.Actual.Remote_Path := Mirror.Tentative.Remote_Path;
+         Mirror.Tentative.Remote_Path := null;
+      end if;
+
+      if Mirror.Tentative_Sync_Set then
+         Mirror.Tentative_Sync_Set := False;
+         Mirror.Actual.Sync := Mirror.Tentative.Sync;
+      end if;
+   end Apply;
+
+   ------------
+   -- Cancel --
+   ------------
+
+   procedure Cancel (Mirror : in out Mirror_Path) is
+   begin
+      if Mirror.Tentative.Local_Path /= null then
+         Free (Mirror.Tentative.Local_Path);
+      end if;
+
+      if Mirror.Tentative.Remote_Path /= null then
+         Free (Mirror.Tentative.Remote_Path);
+      end if;
+
+      Mirror.Tentative_Sync_Set := False;
+   end Cancel;
+
+   ---------
+   -- "=" --
+   ---------
+
+   function "=" (M1, M2 : Mirror_Path) return Boolean is
+      function Same (P1, P2 : Glib.String_Ptr) return Boolean;
+
+      function Same (P1, P2 : Glib.String_Ptr) return Boolean is
+      begin
+         if P1 = null then
+            return P2 = null;
+         end if;
+
+         if P2 = null then
+            return False;
+         end if;
+
+         return P1.all = P2.all;
+      end Same;
+   begin
+      return Same (M1.Actual.Local_Path, M2.Actual.Local_Path)
+        and then Same (M1.Actual.Remote_Path, M2.Actual.Remote_Path)
+        and then M1.Actual.Sync = M2.Actual.Sync;
+   end "=";
+
+end Remote.Path;
---
+++ common/src/remote-path.ads
@@ -0,0 +1,94 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2006                         --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+--  This package defines the remote path element needed for path translation
+--  between remote and local host.
+
+with Glib; use Glib;
+
+package Remote.Path is
+
+   type Synchronisation_Type is (Never,
+                                 Once_To_Local,
+                                 Once_To_Remote,
+                                 Always);
+   --  Synchronisation mechanism used for a given remote path.
+
+   type Mirror_Path is tagged private;
+
+   Null_Path : constant Mirror_Path;
+
+   procedure Init (Mirror          : in out Mirror_Path'Class;
+                   Local_Path      : String;
+                   Remote_Path     : String;
+                   Synchronisation : Synchronisation_Type);
+   function Get_Local_Path (Mirror    : Mirror_Path;
+                            Tentative : Boolean := False) return String;
+   function Get_Remote_Path (Mirror    : Mirror_Path;
+                             Tentative : Boolean := False) return String;
+   function Get_Synchronisation
+     (Mirror : Mirror_Path;
+      Tentative : Boolean := False) return Synchronisation_Type;
+
+   procedure Set_Tentative_Local_Path
+     (Mirror : in out Mirror_Path;
+      Path   : String);
+   --  Tentatively set the local path. Call Apply to effectively set this path.
+
+   procedure Set_Tentative_Remote_Path
+     (Mirror : in out Mirror_Path;
+      Path   : String);
+   --  Same as above for the remote path
+
+   procedure Set_Tentative_Synchronisation
+     (Mirror : in out Mirror_Path;
+      Synchronisation : Synchronisation_Type);
+   --  Same as above for sync
+
+   procedure Apply (Mirror : in out Mirror_Path);
+
+   procedure Cancel (Mirror : in out Mirror_Path);
+
+   function "=" (M1, M2 : Mirror_Path) return Boolean;
+
+private
+
+   type Mirror_Path_Record is record
+      Local_Path  : Glib.String_Ptr      := null;
+      Remote_Path : Glib.String_Ptr      := null;
+      Sync        : Synchronisation_Type := Never;
+   end record;
+
+   --  ??? should be a controlled type
+   type Mirror_Path is tagged record
+      Actual             : Mirror_Path_Record;
+      Tentative          : Mirror_Path_Record;
+      Tentative_Sync_Set : Boolean := False;
+   end record;
+
+   Null_Path : constant Mirror_Path :=
+                 (Actual             => (Local_Path  => null,
+                                         Remote_Path => null,
+                                         Sync        => Never),
+                  Tentative          => (Local_Path  => null,
+                                         Remote_Path => null,
+                                         Sync        => Never),
+                  Tentative_Sync_Set => False);
+
+end Remote.Path;
