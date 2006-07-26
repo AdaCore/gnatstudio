c0c1f2bd50e18abdb721c7bdad02fb5f89450bed
Simon Descarpentries <descarpe@adacore.com>
2006-07-26 18:19:20 +0000

    (Read_File): replaced by the use of VFS.Read_File F704-020
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@98287 936e1b1b-40f2-da11-902a-00137254ae57

--- code_analysis/src/main.adb
+++ code_analysis/src/main.adb
@@ -18,65 +18,36 @@
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
-with Code_Analysis; use Code_Analysis;
+with Code_Analysis;      use Code_Analysis;
 with Code_Analysis_Dump; use Code_Analysis_Dump;
-with Code_Coverage; use Code_Coverage;
+with Code_Coverage;      use Code_Coverage;
 
-with Ada.Text_IO; use Ada.Text_IO;
-with GNAT.OS_Lib; use GNAT.OS_Lib;
+with GNAT.OS_Lib;        use GNAT.OS_Lib;
+with VFS;                use VFS;
 
 procedure Main is
-
-   function Read_File (File : String) return String_Access;
-   --  Read a file from the given long possible name and return its content via
-   --  a string access
-
-   ---------------
-   -- Read_File --
-   ---------------
-
-   function Read_File (File : String) return String_Access is
-      FD           : File_Descriptor := Invalid_FD;
-      Buffer       : String_Access;
-      Length       : Integer;
-      Dummy_Length : Integer;
-      pragma Unreferenced (Dummy_Length);
-
-   begin
-      FD := Open_Read (File, Fmode => Binary);
-
-      if FD = Invalid_FD then
-         Put_Line ("Couldn't open " & File);
-         return null;
-      end if;
-
-      Length := Integer (File_Length (FD));
-      Buffer := new String (1 .. Length);
-      Dummy_Length := Read (FD, Buffer.all'Address, Length);
-      Close (FD);
-      return Buffer;
-   end Read_File;
-
-   Source_File_Name : constant File_Id := new String'("main.adb");
-   File_Contents : String_Access;
-   Project_Name  : Project_Id;
+   VFS_File_Name : constant VFS.Virtual_File := VFS.Create ("dummy_main.adb");
+   Cov_File_Name : constant VFS.Virtual_File := VFS.Create
+     ("/bonn.a/descarpe/src/gps/code_analysis/main.adb.gcov");
+   File_Contents : GNAT.OS_Lib.String_Access;
+   Project_Name  : VFS.Virtual_File;
    Project_Node  : Project_Access;
    File_Node     : Code_Analysis.File_Access;
 begin
-
-   Project_Name  := Get_Project_From_File (Source_File_Name);
+   Project_Name  := Get_Project_From_File (VFS_File_Name);
    Project_Node  := Get_Or_Create (Project_Name);
-   File_Contents := Read_File (Source_File_Name.all & ".gcov");
+   File_Contents := Read_File (Cov_File_Name);
+
    declare
       Dummy  : Code_Analysis.File_Access;
       pragma Unreferenced (Dummy);
    begin
-      Dummy  := Get_Or_Create (Project_Node, Source_File_Name);
+      Dummy  := Get_Or_Create (Project_Node, VFS_File_Name);
    end;
-   File_Node := Get_Or_Create (Get_Or_Create (Project_Name), Source_File_Name);
+
+   File_Node := Get_Or_Create (Get_Or_Create (Project_Name), VFS_File_Name);
    Add_Subprograms (File_Node, File_Contents);
-   --   Add_Lines (File_Node, File_Contents);
-   --  Will return to working state in the next version
+   Add_Lines (File_Node, File_Contents);
    Dump_Text;
    Free (File_Contents);
    Free_Project (Project_Node);
