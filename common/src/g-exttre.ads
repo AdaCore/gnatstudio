a165490253e9f3b4092a36a6b13e729db0e9d31a
Jerome Lambourg <lambourg@adacore.com>
2006-07-26 11:55:00 +0000

    (Remote_Spawn): New callback when a connection occurs. Part of F608-001
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@98268 936e1b1b-40f2-da11-902a-00137254ae57

--- common/src/g-exttre.ads
+++ common/src/g-exttre.ads
@@ -296,7 +296,8 @@ package GNAT.Expect.TTY.Remote is
       Args                : GNAT.OS_Lib.Argument_List;
       Execution_Directory : String := "";
       Err_To_Out          : Boolean := False;
-      Main_Window         : Gtk.Window.Gtk_Window := null);
+      Main_Window         : Gtk.Window.Gtk_Window := null;
+      On_New_Connection   : access procedure (Target_Name : String) := null);
    --  Spawns a process on a remote machine
    --  Target_Name designs the machine on which the process is spawned
    --  Target_Identifier is used to retrieve the descriptors
