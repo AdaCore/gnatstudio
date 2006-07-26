fe20e2778a845be3c16792a9552821e9f6f0c852
Jerome Lambourg <lambourg@adacore.com>
2006-07-26 11:49:29 +0000

    Renamed Remote_Servers package.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@98258 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ common/src/remote.adb
@@ -0,0 +1,94 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                       Copyright (C) 2006                          --
+--                             AdaCore                               --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Glib; use Glib;
+
+package body Remote is
+
+   type Server_Config is record
+      Is_Local : Boolean := True;
+      --  Is_Local Tells if the server is the local machine or not
+      Nickname : String_Ptr;
+      --  Identifier of the server
+   end record;
+
+   type Servers_Config is array (Server_Type) of Server_Config;
+
+   Servers : Servers_Config :=
+               (others => (Is_Local => True, Nickname => new String'("")));
+   --  Servers currently used. Default is the localhost.
+   --  ??? Global variable, should get rid of it
+
+   ------------
+   -- Assign --
+   ------------
+
+   procedure Assign
+     (Server   : Server_Type;
+      Nickname : String)
+   is
+   begin
+      Glib.Free (Servers (Server).Nickname);
+      if Nickname /= "" and Nickname /= Local_Nickname then
+         Servers (Server) := (Is_Local => False,
+                              Nickname => new String'(Nickname));
+      else
+         Servers (Server) := (Is_Local => True,
+                              Nickname => new String'(""));
+      end if;
+   end Assign;
+
+   --------------
+   -- Is_Local --
+   --------------
+
+   function Is_Local (Server : Server_Type) return Boolean is
+   begin
+      return Servers (Server).Is_Local;
+   end Is_Local;
+
+   ------------------
+   -- Get_Nickname --
+   ------------------
+
+   function Get_Nickname (Server : Server_Type) return String is
+   begin
+      if Is_Local (Server) then
+         return "";
+      else
+         return Servers (Server).Nickname.all;
+      end if;
+   end Get_Nickname;
+
+   ----------------------------
+   -- Get_Printable_Nickname --
+   ----------------------------
+
+   function Get_Printable_Nickname (Server : Server_Type) return String
+   is
+   begin
+      if Is_Local (Server) then
+         return Local_Nickname;
+      else
+         return Servers (Server).Nickname.all;
+      end if;
+   end Get_Printable_Nickname;
+
+end Remote;
---
+++ common/src/remote.ads
@@ -0,0 +1,53 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                       Copyright (C) 2006                          --
+--                             AdaCore                               --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+--  This package handles the remote servers assignment.
+
+package Remote is
+
+   type Server_Type is
+     (GPS_Server,
+      Build_Server,
+      Execution_Server,
+      Debug_Server);
+
+   Local_Nickname : constant String;
+
+   subtype Distant_Server_Type is Server_Type
+     range Build_Server .. Debug_Server;
+
+   procedure Assign (Server   : Server_Type;
+                     Nickname : String);
+
+   function Is_Local (Server : Server_Type) return Boolean;
+   --  Tell is the server is the localhost
+
+   function Get_Nickname (Server : Server_Type) return String;
+   --  Get the nickname of a server
+
+   function Get_Printable_Nickname (Server : Server_Type) return String;
+   --  Get the nickname of a server. If server is local, Local_Nickname is
+   --  returned
+
+private
+
+   Local_Nickname : constant String := "(local)";
+
+end Remote;
