051d305e2d436cc688d70d06d635dee10c81b5db
Simon Descarpentries <descarpe@adacore.com>
2006-08-08 15:15:20 +0000

    New package that handles Gtk_Tree_Model construction as Code_Analasysis_Dump handles the text dumping.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@98622 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ code_analysis/src/code_analysis_tree_model.adb
@@ -0,0 +1,139 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2006                         --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is Free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+--  <description>
+--  This package provides the subprograms needed by the construction of a
+--  Gtk_Tree_View for Code_Analysis structure
+--  </description>
+
+with Code_Analysis_Tree_Model; use Code_Analysis_Tree_Model;
+with Code_Coverage;            use Code_Coverage;
+with VFS;
+
+package body Code_Analysis_Tree_Model is
+
+   ----------------
+   -- Fill_Store --
+   ----------------
+
+   procedure Fill_Store
+     (Tree_Store  : in out Gtk_Tree_Store;
+      Iter        : in out Gtk_Tree_Iter;
+      Analysis_Id : Analysis) is
+   begin
+      if Analysis_Id.Coverage_Data /= null then
+         Fill_Store (Tree_Store, Iter, Analysis_Id.Coverage_Data);
+      end if;
+   end Fill_Store;
+
+   ----------------
+   -- Fill_Store --
+   ----------------
+
+   procedure Fill_Store
+     (Tree_Store : in out Gtk_Tree_Store;
+      Iter       : in out Gtk_Tree_Iter;
+      Parent     : Gtk_Tree_Iter;
+      Sub_Node   : Subprogram_Access) is
+   begin
+      Append (Tree_Store, Iter, Parent);
+      Set (Tree_Store, Iter, Node_Col, String (Sub_Node.Name.all));
+      Fill_Store (Tree_Store, Iter, Sub_Node.Analysis_Data);
+   end Fill_Store;
+
+   ----------------
+   -- Fill_Store --
+   ----------------
+
+   procedure Fill_Store
+     (Tree_Store : in out Gtk_Tree_Store;
+      Iter       : in out Gtk_Tree_Iter;
+      Parent     : Gtk_Tree_Iter;
+      File_Node  : File_Access)
+   is
+      use Subprogram_Maps;
+      Self_Iter : Gtk_Tree_Iter;
+      Cur       : Cursor;
+      Sub_Node  : Subprogram_Access;
+   begin
+      Append (Tree_Store, Iter, Parent);
+      Self_Iter := Iter;
+      Set (Tree_Store, Iter, Node_Col, VFS.Base_Name (File_Node.Name));
+      Fill_Store (Tree_Store, Iter, File_Node.Analysis_Data);
+      Cur := File_Node.Subprograms.First;
+
+      loop
+         exit when Cur = No_Element;
+         Sub_Node := Element (Cur);
+         Fill_Store (Tree_Store, Iter, Self_Iter, Sub_Node);
+         Next (Cur);
+      end loop;
+   end Fill_Store;
+
+   ----------------
+   -- Fill_Store --
+   ----------------
+
+   procedure Fill_Store
+     (Tree_Store   : in out Gtk_Tree_Store;
+      Iter         : in out Gtk_Tree_Iter;
+      Project_Node : Project_Access)
+   is
+      use File_Maps;
+      Self_Iter    : Gtk_Tree_Iter;
+      Cur          : Cursor;
+      File_Node    : File_Access;
+   begin
+      Append (Tree_Store, Iter, Null_Iter);
+      Self_Iter := Iter;
+      Set (Tree_Store, Iter, Node_Col, VFS.Base_Name (Project_Node.Name));
+      Fill_Store (Tree_Store, Iter, Project_Node.Analysis_Data);
+      Cur := Project_Node.Files.First;
+
+      loop
+         exit when Cur = No_Element;
+         File_Node := Element (Cur);
+         Fill_Store (Tree_Store, Iter, Self_Iter, File_Node);
+         Next (Cur);
+      end loop;
+   end Fill_Store;
+
+   ----------------
+   -- Fill_Store --
+   ----------------
+
+   procedure Fill_Store
+     (Tree_Store : in out Gtk_Tree_Store;
+      Iter       : in out Gtk_Tree_Iter)
+   is
+      use Project_Maps;
+      Cur          : Cursor;
+      Project_Node : Project_Access;
+   begin
+      Cur := Get_Tree.First;
+
+      loop
+         exit when Cur = No_Element;
+         Project_Node := Element (Cur);
+         Fill_Store (Tree_Store, Iter, Project_Node);
+         Next (Cur);
+      end loop;
+   end Fill_Store;
+end Code_Analysis_Tree_Model;
---
+++ code_analysis/src/code_analysis_tree_model.ads
@@ -0,0 +1,67 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2006                         --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is Free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+--  <description>
+--  This package provides the subprograms needed by the construction of a
+--  Gtk_Tree_View for Code_Analysis structure
+--  </description>
+
+with Code_Analysis;   use Code_Analysis;
+with Gtk.Tree_Store;  use Gtk.Tree_Store;
+with Gtk.Tree_Model;  use Gtk.Tree_Model;
+
+package Code_Analysis_Tree_Model is
+
+   ------------
+   -- Filler --
+   ------------
+
+   procedure Fill_Store
+     (Tree_Store   : in out Gtk_Tree_Store;
+      Iter         : in out Gtk_Tree_Iter);
+   --  Fill the Gtk_Tree_Store
+
+   procedure Fill_Store
+     (Tree_Store : in out Gtk_Tree_Store;
+      Iter       : in out Gtk_Tree_Iter;
+      Parent     : Gtk_Tree_Iter;
+      Sub_Node   : Subprogram_Access);
+   --  Fill the Gtk_Tree_Store with the given Subprogram node
+
+   procedure Fill_Store
+     (Tree_Store : in out Gtk_Tree_Store;
+      Iter       : in out Gtk_Tree_Iter;
+      Parent     : Gtk_Tree_Iter;
+      File_Node  : File_Access);
+   --  Fill the Gtk_Tree_Store with the given File node
+
+   procedure Fill_Store
+     (Tree_Store   : in out Gtk_Tree_Store;
+      Iter         : in out Gtk_Tree_Iter;
+      Project_Node : Project_Access);
+   --  Fill the Gtk_Tree_Store with the given Project node
+
+   procedure Fill_Store
+     (Tree_Store  : in out Gtk_Tree_Store;
+      Iter        : in out Gtk_Tree_Iter;
+      Analysis_Id : Analysis);
+   --  Fill the Gtk_Tree_Store with the given Analysis record
+
+end Code_Analysis_Tree_Model;
