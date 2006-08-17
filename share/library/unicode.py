b68ac596d8c9c96e454a29e4dc100f15fafb4565
Emmanuel Briot <briot@adacore.com>
2006-08-17 13:34:37 +0000

    Added completion for unicode character names New GPS action to describe the unicode character under the cursor.  In the context of F817-004
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@98836 936e1b1b-40f2-da11-902a-00137254ae57

--- share/library/unicode.py
+++ share/library/unicode.py
@@ -12,10 +12,29 @@
       greek capital letter pi
    or (in hexadecimal):
       x3A0
+
+   Another GPS action is provided that describes the unicode character
+   currently under the cursor, including its numeric value and its name.
+   This action is called "Describe unicode char". It can be bound to a key,
+   assigned to a menu, or more easily executed through the startup script
+   execute_extended.py
 """
 
 background_color = "yellow"
 ## Background color to use for the command window
+
+prefixes = ["greek capital letter ",
+            "greek small letter ",
+            "latin capital letter ",
+            "latin small letter "]
+words    = ["acute", "grave", "circumflex", "diaresis", "title",
+            "with"]
+## Possible completions: prefixes is checked from the beginning of the
+## command line, whereas words is checked only for the previous word.
+## You can add to these in the initialization strings by using a python
+## command similar to
+##    prefix = prefix + ["vertical", "opening"]
+## Only lower case letters should be used
 
 
 ############################################################################
@@ -27,23 +46,50 @@ import unicodedata
 
 def on_gps_started (hook_name):
    parse_xml ("""
-   <action name="Enter unicode" output="none">
+   <action name="Insert unicode" output="none" category="Editor">
+      <description>Insert any unicode character given its decimal or hexadecimal value, or its name</description>
       <filter id="Source editor"/>
       <shell lang="python">unicode.Unicode()</shell>
    </action>
-   <menu action="Enter unicode" before="Insert File...">
+   <action name="Describe unicode char" output="none" category="Editor">
+      <description>Describe the unicode character under the cursor (name, value,...)</description>
+      <filter id="Source editor" />
+      <shell lang="python">unicode.describe_char()</shell>
+   </action>
+   <menu action="Insert unicode" before="Insert File...">
      <title>/Edit/Insert Unicode</title>
    </menu>""")
+
+def findcommonstart(strlist):
+  return strlist[0][:([min([x[0]==elem for elem in x]) \
+                 for x in zip(*strlist)]+[0]).index(0)]
 
 class Unicode (CommandWindow):
    def __init__ (self):
       CommandWindow.__init__ (self, prompt="Character code:",
+                              on_key      = self.on_key,
                               on_activate = self.on_activate)
       self.set_background (background_color)
+
+   def on_key (self, input, key, cursor_pos):
+      if key.lower() == "tab":
+         input = " ".join (input.split()) # Only one space between words
+         input = input.lower()
+         compl = [p for p in prefixes if p.startswith (input)]
+         if compl:
+            self.write (findcommonstart(compl))
+         else:
+            split = input.split()
+            compl = [p for p in words if p.startswith (split[-1])]
+            if compl:
+               self.write \
+                 (" ".join (split[:-1]) + " " + findcommonstart (compl) + " ")
+         return True
 
    def on_activate (self, input):
       if input != "":
          buffer = EditorBuffer.get()
+         input = input.strip()
          if input[0] in "0123456789":
             chr = unichr (int (input))
          elif input[0] == 'x' and input[1] in "0123456789":
@@ -51,5 +97,16 @@ class Unicode (CommandWindow):
          else:
             chr = unicodedata.lookup (input)
          buffer.insert (buffer.current_view().cursor(), chr.encode('utf-8'))
+
+def describe_char (char = None):
+   """Describe the unicode character under the cursor"""
+   if not char:
+      char = EditorBuffer.get().current_view().cursor().get_char()
+   uni = char.decode ("utf-8")
+   Console().write ("Character:  " + char + "\n")
+   Console().write ("      Name: " + unicodedata.name (uni) + "\n")
+   Console().write ("   Unicode: " + `ord (uni)` \
+                    + " (U+" + hex (ord (uni))[2:] + ")\n")
+   Console().write ("  Category: " + unicodedata.category (uni) + "\n")
                           
 Hook ("gps_started").add (on_gps_started)
