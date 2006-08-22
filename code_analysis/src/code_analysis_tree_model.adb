41aaa94b49cf3950220db6815cfd2d2733cb0664
Simon Descarpentries <descarpe@adacore.com>
2006-08-22 16:25:16 +0000

    Add support for icons in the tree. Minor rewording to match what already exists in GPS.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@98937 936e1b1b-40f2-da11-902a-00137254ae57

--- code_analysis/src/code_analysis_tree_model.adb
+++ code_analysis/src/code_analysis_tree_model.adb
@@ -26,108 +26,113 @@
 with Glib;                     use Glib;
 with Projects;                 use Projects;
 with Code_Analysis_Tree_Model; use Code_Analysis_Tree_Model;
+with Code_Analysis_Module;     use Code_Analysis_Module;
 with Code_Coverage;            use Code_Coverage;
 with VFS;
 
 package body Code_Analysis_Tree_Model is
 
-   ----------------
-   -- Fill_Store --
-   ----------------
+   ---------------
+   -- Fill_Iter --
+   ---------------
 
-   procedure Fill_Store
-     (Tree_Store  : in out Gtk_Tree_Store;
+   procedure Fill_Iter
+     (Model       : in out Gtk_Tree_Store;
       Iter        : in out Gtk_Tree_Iter;
       Analysis_Id : Analysis) is
    begin
       if Analysis_Id.Coverage_Data /= null then
-         Fill_Store (Tree_Store, Iter, Analysis_Id.Coverage_Data);
+         Fill_Iter (Model, Iter, Analysis_Id.Coverage_Data);
       end if;
-   end Fill_Store;
+   end Fill_Iter;
 
-   ----------------
-   -- Fill_Store --
-   ----------------
+   ---------------
+   -- Fill_Iter --
+   ---------------
 
-   procedure Fill_Store
-     (Tree_Store : in out Gtk_Tree_Store;
-      Iter       : in out Gtk_Tree_Iter;
-      Parent     : Gtk_Tree_Iter;
-      Sub_Node   : Subprogram_Access) is
+   procedure Fill_Iter
+     (Model    : in out Gtk_Tree_Store;
+      Iter     : in out Gtk_Tree_Iter;
+      Parent   : Gtk_Tree_Iter;
+      Sub_Node : Subprogram_Access) is
    begin
-      Append (Tree_Store, Iter, Parent);
-      Set (Tree_Store, Iter, Node_Col, String (Sub_Node.Name.all));
-      Fill_Store (Tree_Store, Iter, Sub_Node.Analysis_Data);
-   end Fill_Store;
+      Append (Model, Iter, Parent);
+      Set (Model, Iter, Pix_Col, C_Proxy
+           (Code_Analysis_Module_ID.Subp_Pixbuf));
+      Set (Model, Iter, Node_Col, String (Sub_Node.Name.all));
+      Fill_Iter (Model, Iter, Sub_Node.Analysis_Data);
+   end Fill_Iter;
 
-   ----------------
-   -- Fill_Store --
-   ----------------
+   ---------------
+   -- Fill_Iter --
+   ---------------
 
-   procedure Fill_Store
-     (Tree_Store : in out Gtk_Tree_Store;
-      Iter       : in out Gtk_Tree_Iter;
-      Parent     : Gtk_Tree_Iter;
-      File_Node  : File_Access)
+   procedure Fill_Iter
+     (Model     : in out Gtk_Tree_Store;
+      Iter      : in out Gtk_Tree_Iter;
+      Parent    : Gtk_Tree_Iter;
+      File_Node : File_Access)
    is
       use Subprogram_Maps;
       Self_Iter : Gtk_Tree_Iter;
       Cur       : Cursor;
       Sub_Node  : Subprogram_Access;
    begin
-      Append (Tree_Store, Iter, Parent);
+      Append (Model, Iter, Parent);
       Self_Iter := Iter;
-      Set (Tree_Store, Iter, Node_Col, VFS.Base_Name (File_Node.Name));
-      Fill_Store (Tree_Store, Iter, File_Node.Analysis_Data);
+      Set (Model, Iter, Pix_Col, C_Proxy
+           (Code_Analysis_Module_ID.File_Pixbuf));
+      Set (Model, Iter, Node_Col, VFS.Base_Name (File_Node.Name));
+      Fill_Iter (Model, Iter, File_Node.Analysis_Data);
       Cur := File_Node.Subprograms.First;
 
       loop
          exit when Cur = No_Element;
          Sub_Node := Element (Cur);
-         Fill_Store (Tree_Store, Iter, Self_Iter, Sub_Node);
+         Fill_Iter (Model, Iter, Self_Iter, Sub_Node);
          Next (Cur);
       end loop;
-   end Fill_Store;
+   end Fill_Iter;
 
-   ----------------
-   -- Fill_Store --
-   ----------------
+   ---------------
+   -- Fill_Iter --
+   ---------------
 
-   procedure Fill_Store
-     (Tree_Store   : in out Gtk_Tree_Store;
+   procedure Fill_Iter
+     (Model        : in out Gtk_Tree_Store;
       Iter         : in out Gtk_Tree_Iter;
       Project_Node : Project_Access)
    is
       use File_Maps;
-      Self_Iter    : Gtk_Tree_Iter;
-      Cur          : Cursor;
-      File_Node    : File_Access;
+      Self_Iter : Gtk_Tree_Iter;
+      Cur       : Cursor;
+      File_Node : File_Access;
    begin
-      Append (Tree_Store, Iter, Null_Iter);
+      Append (Model, Iter, Null_Iter);
       Self_Iter := Iter;
-      Set (Tree_Store,
-        Iter,
-        Node_Col,
-        UTF8_String (String'(Project_Name (Project_Node.Name))));
-      Fill_Store (Tree_Store, Iter, Project_Node.Analysis_Data);
+      Set (Model, Iter, Pix_Col, C_Proxy
+           (Code_Analysis_Module_ID.Project_Pixbuf));
+      Set (Model, Iter, Node_Col,
+           UTF8_String (String'(Project_Name (Project_Node.Name))));
+      Fill_Iter (Model, Iter, Project_Node.Analysis_Data);
       Cur := Project_Node.Files.First;
 
       loop
          exit when Cur = No_Element;
          File_Node := Element (Cur);
-         Fill_Store (Tree_Store, Iter, Self_Iter, File_Node);
+         Fill_Iter (Model, Iter, Self_Iter, File_Node);
          Next (Cur);
       end loop;
-   end Fill_Store;
+   end Fill_Iter;
 
-   ----------------
-   -- Fill_Store --
-   ----------------
+   ---------------
+   -- Fill_Iter --
+   ---------------
 
-   procedure Fill_Store
-     (Tree_Store : in out Gtk_Tree_Store;
-      Iter       : in out Gtk_Tree_Iter;
-      Cur        : Project_Maps.Cursor)
+   procedure Fill_Iter
+     (Model : in out Gtk_Tree_Store;
+      Iter  : in out Gtk_Tree_Iter;
+      Cur   : Project_Maps.Cursor)
    is
       use Project_Maps;
       Variable_Cur : Cursor := Cur;
@@ -136,8 +141,8 @@ package body Code_Analysis_Tree_Model is
       loop
          exit when Variable_Cur = No_Element;
          Project_Node := Element (Variable_Cur);
-         Fill_Store (Tree_Store, Iter, Project_Node);
+         Fill_Iter (Model, Iter, Project_Node);
          Next (Variable_Cur);
       end loop;
-   end Fill_Store;
+   end Fill_Iter;
 end Code_Analysis_Tree_Model;
