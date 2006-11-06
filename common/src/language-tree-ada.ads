7788cb8a1b22c647c6a8691e6b7ede22bf2b517c
Quentin Ochem <ochem@adacore.com>
2006-11-06 12:02:55 +0000

    (Generate_Ada_Construct_Tree): Buffer is now a String_Access. (Generate_Ada_Construct_Trees): New subprogram. (Get_Spec): Now returns a Construct_Cell_Access. (Get_First_Body): Now returns a Construct_Cell_Access. (Get_Second_Body): Now returns a Construct_Cell_Access. (Is_Most_Complete_View): New subprogram. (Is_Body_Of): New subprogram. (Ada_Construct_Tree_Information): Now references a pointer to an Ada_Relation,  instead of having directly and systematically the fields recorded.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@99687 936e1b1b-40f2-da11-902a-00137254ae57

--- common/src/language-tree-ada.ads
+++ common/src/language-tree-ada.ads
@@ -26,33 +26,50 @@ package Language.Tree.Ada is
    --  the information, and has to be used with the construct tree from which
    --  it has been created.
 
-   procedure Free (Ada_Tree : in out Ada_Construct_Tree);
-   --  Free the data associated to an ada construct tree access.
+   procedure Free
+     (Tree : Construct_Tree; Ada_Tree : in out Ada_Construct_Tree);
+   --  Free the data associated to an ada construct tree access. The
+   --  Construct_Tree given in parameter will not be freed, but is needed in
+   --  order to identify clearly the contents referenced by the Ada_Tree.
 
    function Generate_Ada_Construct_Tree
      (Tree     : Construct_Tree;
-      Language : Language_Access;
-      Buffer   : String) return Ada_Construct_Tree;
+      Buffer   : String_Access) return Ada_Construct_Tree;
    --  Return the ada construct tree corresponding to this construct tree.
+
+   procedure Generate_Ada_Construct_Trees
+     (Spec_Tree, Body_Tree         : Construct_Tree;
+      Spec_Buffer, Body_Buffer     : String_Access;
+      Spec_Ada_Tree, Body_Ada_Tree : out Ada_Construct_Tree);
+   --  Generates the ada constructs trees for the two trees given in parameter,
+   --  assuming that Spec_Tree is the specification of Body_Tree. References
+   --  will be made between the two trees when appropriate.
 
    function Get_Spec
      (Tree     : Construct_Tree;
       Ada_Tree : Ada_Construct_Tree;
-      Iter     : Construct_Tree_Iterator) return Construct_Tree_Iterator;
+      Iter     : Construct_Tree_Iterator) return Construct_Cell_Access;
    --  Return the specification of the given iterator if any, or the iterator
    --  itself otherwise.
 
    function Get_First_Body
      (Tree     : Construct_Tree;
       Ada_Tree : Ada_Construct_Tree;
-      Iter     : Construct_Tree_Iterator) return Construct_Tree_Iterator;
+      Iter     : Construct_Tree_Iterator) return Construct_Cell_Access;
    --  Return the first body found in the tree
 
    function Get_Second_Body
      (Tree     : Construct_Tree;
       Ada_Tree : Ada_Construct_Tree;
-      Iter     : Construct_Tree_Iterator) return Construct_Tree_Iterator;
+      Iter     : Construct_Tree_Iterator) return Construct_Cell_Access;
    --  Return the second body found in the tree
+
+   function Is_Most_Complete_View
+     (Tree     : Construct_Tree;
+      Ada_Tree : Ada_Construct_Tree;
+      Iter     : Construct_Tree_Iterator) return Boolean;
+   --  Return false if we have build a most complete view of the entity pointed
+   --  by the iterator, false otherwise.
 
    function Get_Visible_Constructs
      (Tree       : Construct_Tree;
@@ -133,19 +150,28 @@ package Language.Tree.Ada is
 
    Ada_Tree_Lang : constant Tree_Language_Access;
 
+   function Is_Body_Of (Body_Tree, Spec_Tree : Construct_Tree) return Boolean;
+   --  Return True if Body_Tree is indeed the body of Spec_Tree
+
 private
 
-   type Ada_Construct_Tree_Information is record
-      Spec_Index             : Natural := 0;
+   type Ada_Construct_Relation is record
+      Spec : Construct_Cell_Access;
 
-      First_Body_Index       : Natural := 0;
+      First_Body : Construct_Cell_Access;
       --  If the entity is a subprogram, this stores the position of its body.
       --  If it's a type, it stores the position of the declaration (the public
       --  declaration in case there is a full one in the private one as well).
 
-      Second_Body_Index      : Natural := 0;
+      Second_Body : Construct_Cell_Access;
       --  Only used in the construct is a type. Stores the position of the
       --  full declaration in the private part.
+   end record;
+
+   type Relation_Ptr is access all Ada_Construct_Relation;
+
+   type Ada_Construct_Tree_Information is record
+      Ada_Relation : Relation_Ptr;
    end record;
 
    type Ada_Construct_Tree_Array is array
