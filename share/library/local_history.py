f8a8103b35bbb2251a1918a26e8e8d3d4e370c4d
Emmanuel Briot <briot@adacore.com>
2006-11-14 17:29:13 +0000

    Add support for keeping only revisions for the last n days (sometimes more  logical than the last n revisions) Add contextual menu to revert to a previous revision
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@99861 936e1b1b-40f2-da11-902a-00137254ae57

--- share/library/local_history.py
+++ share/library/local_history.py
@@ -16,8 +16,11 @@ local_rcs_dir = ".gpsrcs"
 ## Name of the local directory created to store history. Several such
 ## directories might be created, one per project
 
-max_revisions = 30
-## Keep at most that many revision histories for each file
+max_days = 2
+## Keep revisions for that many days at most. See also max_revisions
+
+max_revisions = 200
+## Maximal number of completions to keep. See also max_days
 
 
 ###########################################################################
@@ -25,11 +28,13 @@ max_revisions = 30
 ############################################################################
 
 from GPS import *
-import os.path, shutil
+import os.path, shutil, datetime, traceback, time, stat
 
-def create_RCS_dir (for_file):
+def create_RCS_dir (for_file, allow_create=True):
    """Create the RCS directory for the given GPS.File instance.
-      The name of the directory is returned"""
+      Return a tuple (dir, full rcs file path).
+      If allow_create is False, and the directory doesn't exists, None
+      is returned."""
    project = for_file.project (default_to_root = False)
    if project:
       dir = project.object_dirs (recursive = False)[0]
@@ -38,38 +43,66 @@ def create_RCS_dir (for_file):
 
    dir = os.path.join (dir, local_rcs_dir)
 
-   try:
-     os.makedirs (dir)
-   except:
-     pass
+   if not allow_create and not os.path.isdir (dir):
+      return None
 
-   Logger ("LocalHist").log ("creating directory " + `dir`)
-   return dir
+   if allow_create:
+     try:
+       os.makedirs (dir)
+       Logger ("LocalHist").log ("creating directory " + `dir`)
+     except: pass
+
+   return (dir, os.path.join (dir, os.path.basename (for_file.name())) + ",v")
 
 def add_to_history (file, dir):
    """Expand the local history for file, to include the current version"""
    pwd = os.getcwd()
    os.chdir (dir)
    shutil.copy2 (file.name(), dir)
-   proc = Process ("ci -l " + os.path.basename (file.name()))
-   proc.send (".\n")
-   proc.wait ()
-   os.unlink (os.path.join (dir, os.path.basename (file.name())))
+   proc = Process ("rcs -l " + os.path.basename (file.name()))
+   if proc.wait() == 0:
+      proc = Process ("ci " + os.path.basename (file.name()))
+      proc.send (".\n")
+      proc.wait ()
    os.chdir (pwd)
 
-def cleanup_history (file_name, dir):
-   """Keep at most max_revisions histories for file"""
-   f = file (os.path.join (dir, os.path.basename (file_name.name()) + ",v"))
-   version = f.readline().split()[1]
-   f.close()
+def get_revisions (rcs_file):
+   """Given a ,v file, extract all revisions and associated dates.
+      Result is a list of tuples: (revision_number, date), where
+      revision is the RCS revision number less the "1." prefix.
+      First in the list is the most recent revision."""
+   f = file (rcs_file)
+   result = []
 
-   version = version[2:-1]  # Omit "1." at start, and ";" at end
-   version = int (version) - max_revisions
-   if version > 0:
+   for line in f.readlines():
+     if line.startswith ("log"): break
+     if line.startswith ("date\t"):
+       date = line.split()[1]
+       result.append ((int (previous[2:]), date[:-1]))
+     previous = line
+
+   f.close ()
+   return result
+
+def cleanup_history (dir, rcs_file):
+   """Keep at most max_revisions histories for file"""
+   older = datetime.datetime.now() - datetime.timedelta (days = max_days)
+   older = older.strftime ("%Y.%m.%d.%H.%M.%S")
+
+   revisions = get_revisions (rcs_file)
+
+   version = max (0, revisions[0][0] - max_revisions)
+   for r in revisions:
+     if r[1] < older:
+        version = max (version, r[0])
+        break
+
+   if version >= 1:
+      Logger ("LocalHist").log \
+        ("Truncating file " + rcs_file + " to revision " + `version`)
       pwd = os.getcwd()
       os.chdir (dir)
-      proc = Process ("rcs -o:1." + `version` + " " + \
-                      os.path.basename (file_name.name())) 
+      proc = Process ("rcs -o:1." + version + " " + os.path.basename (rcs_file))
       proc.wait ()
       os.chdir (pwd)
 
@@ -83,14 +116,63 @@ def has_RCS_on_path():
 
 def on_file_saved (hook, file):
    """Called when a file has been saved"""
-   dir = create_RCS_dir (file)
+   (dir, rcs_file) = create_RCS_dir (file)
    add_to_history (file, dir)
-   cleanup_history (file, dir)
+   cleanup_history (dir, rcs_file)
+
+def has_local_history (file):
+   """Whether there is local history information for file"""
+   rcs = create_RCS_dir (file, allow_create = False)
+   return rcs and os.path.isfile (rcs[1])
+
+def contextual_filter (context):
+   try:    return has_local_history (context.file())
+   except: 
+     Logger ("LocalHist").log ("Unexpected exception " + traceback.format_exc())
+     return False
+
+def revert_file (file, revision):
+   """Revert file to a local history revision"""
+   Logger ("LocalHist").log ("revert " + file.name() + " to " + revision)
+   (dir, rcs_file) = create_RCS_dir (file, allow_create = False)
+   pwd = os.getcwd()
+   os.chdir (dir)
+
+   try: os.unlink (os.path.basename (file.name()))
+   except: pass
+
+   proc = Process ("co -r" + revision + " " + os.path.basename (rcs_file))
+   if proc.wait() == 0:
+      shutil.copymode (file.name(), os.path.basename (file.name()))
+      shutil.move (os.path.basename (file.name()), file.name())
+      EditorBuffer.get (file, force = True)
+   os.chdir (pwd)
+
+def contextual_factory (context):
+   (dir, rcs_file) = create_RCS_dir (context.file())
+   revisions = get_revisions (rcs_file)
+   context.revisions = ["1." + `a[0]` for a in revisions]
+
+   result = []
+   for a in revisions:
+     date = datetime.datetime (*(time.strptime (a[1], "%Y.%m.%d.%H.%M.%S")[0:6]))
+     result.append (date.strftime ("%Y/%m/%d %H:%M:%S"))
+   return result
+
+def on_revert (context, choice, choice_index):
+   file = context.file ()
+   revert_to = context.revisions [choice_index]
+   revert_file (file, revert_to)
 
 def register_module (hook):
    """Activate this local history module if RCS is found on the path"""
 
    if has_RCS_on_path():
      Hook ("file_saved").add (on_file_saved, last = True)     
+     Contextual ("Local History Revert to").create_dynamic \
+       (factory     = contextual_factory,
+        on_activate = on_revert,
+        label       = "Local History/Revert To",
+        filter      = contextual_filter)
    
 Hook ("gps_started").add (register_module)
