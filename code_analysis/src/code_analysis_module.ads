45ea2f2130b12d472ca982922592b0bc7fc00e98
Simon Descarpentries <descarpe@adacore.com>
2006-11-19 21:36:09 +0000

    (Filter_Matches_Primitive): Associated with Has_Coverage_Filter, determine if  the "Coverage" entry of the global contextual menu has to be built. (Append_To_Menu): Actually fill the "Coverage" contextual submenu (Context_And_Code_Analysis): Artificial type that contains what the  User_Callbacks used by "Coverage" contextual submenu entries need.
    (Shell_Add_Gcov_Info):
    (Shell_List_Lines_Not_Covered):
    (Shell_Show_Tree_View):
    (Add_Coverage_Annotations_From_Report):
    (List_Lines_Not_Covered_In_File_From_Report):
    (List_Lines_Not_Covered_In_Project):
    (List_Lines_Not_Covered_In_File): Refactored names in order to introduce a
     clear name schema that allow new subprogram to come, and to improve the
     readability in the outline view
    
    (Add_Coverage_Annotations_From_Report): Old Add_Coverage_Annotations that have
     been refactored. See reason above.
    (Add_Coverage_Annotations_From_Context): New callback, for the global
     contextual "Coverage" submenu.
    (Add_Coverage_Annotations): Factored part of the old Add_Coverage_Annotations,
     between ..._From_Report and ..._From_Context
    (Remove_Coverage_Annotations_From_Report):
    (Remove_Coverage_Annotations_From_Context):
    (Remove_Coverage_Annotations): idem as above
    
    (Load_Coverage_Information): Not yet implemented, currently just output some
     traces.
    
    
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@99940 936e1b1b-40f2-da11-902a-00137254ae57

--- code_analysis/src/code_analysis_module.ads
+++ code_analysis/src/code_analysis_module.ads
@@ -48,6 +48,7 @@ with Gtk.Tree_Model;       use Gtk.Tree_Model;
 with Gtk.Tree_View_Column; use Gtk.Tree_View_Column;
 with Gtk.Widget;           use Gtk.Widget;
 with Gtk.Menu;             use Gtk.Menu;
+with Gtk.Handlers;         use Gtk.Handlers;
 
 package Code_Analysis_Module is
 
@@ -70,10 +71,6 @@ package Code_Analysis_Module is
    --  Register the module into the list
 
 private
-   type Code_Analysis_Class_Record;
-
-   type Code_Analysis_Class is access all Code_Analysis_Class_Record;
-
    type Code_Analysis_View_Record is new Gtk_Hbox_Record with record
       Tree        : Gtk_Tree_View;
       Model       : Gtk_Tree_Store;
@@ -95,8 +92,41 @@ private
       Child    : GPS_MDI_Child;
    end record;
 
+   type Code_Analysis_Class is access all Code_Analysis_Class_Record;
+
    procedure Unchecked_Free is new Ada.Unchecked_Deallocation
      (Code_Analysis_Class_Record, Code_Analysis_Class);
+
+   type Has_Coverage_Filter is new Action_Filter_Record with null record;
+   function Filter_Matches_Primitive
+     (Filter  : access Has_Coverage_Filter;
+      Context : Selection_Context) return Boolean;
+   --  True when the current context is associated with project, file or
+   --  subprogram information
+
+   type Coverage_Contextual_Menu is new Submenu_Factory_Record
+   with record
+      Code_Analysis : Code_Analysis_Class;
+   end record;
+
+   type Coverage_Contextual_Menu_Access is access all Coverage_Contextual_Menu;
+
+   procedure Append_To_Menu
+     (Factory : access Coverage_Contextual_Menu;
+      Object  : access Glib.Object.GObject_Record'Class;
+      Context : Selection_Context;
+      Menu    : access Gtk.Menu.Gtk_Menu_Record'Class);
+   --  Fill Menu with the contextual menu for the Coverage module in ,
+   --  if Context is appropriate.
+
+   type Context_And_Code_Analysis is record
+      Context       : Selection_Context;
+      Code_Analysis : Code_Analysis_Class;
+   end record;
+
+   package Context_And_Code_Analysis_CB is new User_Callback
+     (Glib.Object.GObject_Record, Context_And_Code_Analysis);
+   --  Used to connect handlers on the global Coverage contextual menu
 
    Code_Analysis_Cst_Str : constant String := "code_analysis";
    Coverage_Category     : constant Glib.UTF8_String := -"Not covered lines";
@@ -107,18 +137,18 @@ private
       Command : String);
    --  Create a new instance of Code_Analysis data structure
 
-   procedure Add_Gcov_Info
+   procedure Shell_Add_Gcov_Info
      (Data    : in out Callback_Data'Class;
       Command : String);
    --  Add node and coverage info provided by a gcov file parsing
 
-   procedure List_Not_Covered_Lines
+   procedure Shell_List_Lines_Not_Covered
      (Data    : in out Callback_Data'Class;
       Command : String);
    --  Add in the location view every not covered lines of any projects loaded
    --  in the Code_Analysis structure of the current Instance.
 
-   procedure Show_Tree_View
+   procedure Shell_Show_Tree_View
      (Data    : in out Callback_Data'Class;
       Command : String);
    --  Creata an display a Code_Analysis tree view within a MDI Child
@@ -146,36 +176,66 @@ private
    --  Determine the content of the contextual menu displays for the Coverage
    --  Report MDI child nodes
 
-   procedure Add_Coverage_Annotations
+   procedure Add_Coverage_Annotations_From_Report
      (Object : access Gtk_Widget_Record'Class);
    --  Callback attached to the "View with coverage annotations" contextual
    --  menu entry of a File or Subprogram nodes in a Coverage Report
 
-   procedure Remove_Coverage_Annotations
+   procedure Add_Coverage_Annotations_From_Context
+     (Widget : access Glib.Object.GObject_Record'Class;
+      C      : Context_And_Code_Analysis);
+   --  Add coverage annotations to source file editor
+   --  Callback to the global contextual menu entry
+   --  "View with coverage annotations"
+
+   procedure Add_Coverage_Annotations
+     (File_Node : Code_Analysis.File_Access);
+   --  Actually add the annotation columns
+
+   procedure Remove_Coverage_Annotations_From_Report
      (Object : access Gtk_Widget_Record'Class);
    --  Callback attached to the "Remove coverage annotations" contextual
    --  menu entry of a File or Subprogram nodes in a Coverage Report
 
-   procedure Menu_List_Not_Covered_Lines_In_Project
+   procedure Remove_Coverage_Annotations_From_Context
+     (Widget : access Glib.Object.GObject_Record'Class;
+      C      : Context_And_Code_Analysis);
+   --  Callback attached to the "Remove coverage annotations" contextual
+   --  menu entry of every objects that have File or Subprogram information in
+   --  its context
+
+   procedure Remove_Coverage_Annotations
+     (File_Node : Code_Analysis.File_Access);
+   --  Actually removes coverage annotations of src_editors of file represented
+   --  by File_Node
+
+   procedure List_Lines_Not_Covered_In_Project_From_Report
      (Object : access Gtk_Widget_Record'Class);
    --  Callback of contextual menu for Project rows in a Coverage Report.
    --  Add to the location view the unexecuted lines of all files of the
    --  selected Project.
 
-   procedure Menu_List_Not_Covered_Lines_In_File
+   procedure List_Lines_Not_Covered_In_File_From_Report
      (Object : access Gtk_Widget_Record'Class);
    --  Callback of contextual menu for File rows in a Coverage Report. Opens
    --  the select File in a source editor and call
-   --  List_Not_Covered_Lines_In_File.
+   --  List_Lines_Not_Covered_In_File.
 
-   procedure List_Not_Covered_Lines_In_Project
+   procedure List_Lines_Not_Covered_In_Project
      (Project_Node : Project_Access);
    --  Add to the location view the unexecuted lines of the given Project of a
    --  Coverage Report.
 
-   procedure List_Not_Covered_Lines_In_File
+   procedure List_Lines_Not_Covered_In_File
      (File_Node : Code_Analysis.File_Access);
    --  Add to the location view the unexecuted lines of the given File of a
    --  Coverage Report.
 
+   procedure Load_Coverage_Information
+     (Widget : access Glib.Object.GObject_Record'Class;
+      C      : Context_And_Code_Analysis);
+   --  Not yet implemented
+   --  Will try to load every .gcov files of the project in the
+   --  Selection_Context of the Context_And_Code_Analysis in its Code_Analysis
+   --  structure
 end Code_Analysis_Module;
