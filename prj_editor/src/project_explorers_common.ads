b2a7e3bd0b579d630de4e18443085afc34223f91
Nicolas Setton <setton@adacore.com>
2006-11-22 17:15:53 +0000

    (Entity_Icons): New profile, take into account new possibilities for icons. For FA31-013.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@99982 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/src/project_explorers_common.ads
+++ prj_editor/src/project_explorers_common.ads
@@ -100,11 +100,14 @@ package Project_Explorers_Common is
    Open_Pixbufs  : Pixbuf_Array;
    Close_Pixbufs : Pixbuf_Array;
 
+   type Cat_Array is array (Language_Category) of Gdk.Pixbuf.Gdk_Pixbuf;
+
    type Pixbuf_Entity_Array is array
-     (Language_Category) of Gdk.Pixbuf.Gdk_Pixbuf;
+     (Boolean,              --  True for entities that are declarations
+      Construct_Visibility) --  The visibility of entities
+   of Cat_Array;
 
    Entity_Icons      : Pixbuf_Entity_Array;
-   Entity_Spec_Icons : Pixbuf_Entity_Array;
 
    procedure Init_Graphics (Widget : Gtk_Widget);
    --  Initialize the pixbufs.
