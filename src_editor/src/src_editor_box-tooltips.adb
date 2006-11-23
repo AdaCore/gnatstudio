fda18b002ae8bf05ddb8fa22d5886f2d653cf559
Nicolas Setton <setton@adacore.com>
2006-11-23 14:53:01 +0000

    Extract code to draw the tooltips, move it to Entity.Tooltips, for general use. For FB23-014.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@100028 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/src_editor_box-tooltips.adb
+++ src_editor/src/src_editor_box-tooltips.adb
@@ -36,21 +36,18 @@ with Gtk.Enums;                 use Gtk.Enums;
 with Gtk.Widget;                use Gtk.Widget;
 with Pango.Font;                use Pango.Font;
 with Pango.Layout;              use Pango.Layout;
-with Pango.Tabs;                use Pango.Tabs;
 
 with Tooltips;                  use Tooltips;
-with Doc_Utils;                 use Doc_Utils;
 with GPS.Kernel.Contexts;       use GPS.Kernel, GPS.Kernel.Contexts;
 with GPS.Kernel.Preferences;    use GPS.Kernel.Preferences;
-with GPS.Intl;                  use GPS.Intl;
 with Entities;                  use Entities;
 with Entities.Queries;          use Entities.Queries;
+with Entities.Tooltips;         use Entities.Tooltips;
 with VFS;                       use VFS;
 with Traces;                    use Traces;
 with Src_Editor_Module;         use Src_Editor_Module;
 with Src_Editor_View;           use Src_Editor_View;
 with GUI_Utils;                 use GUI_Utils;
-with String_Utils;              use String_Utils;
 with Src_Editor_Buffer.Line_Information;
 use Src_Editor_Buffer.Line_Information;
 
@@ -80,21 +77,6 @@ package body Src_Editor_Box.Tooltips is
    --  Ref is the closest reference to the entity from Context. It might not be
    --  set if we haven't found this information
 
-   function Get_Documentation
-     (Kernel : access Kernel_Handle_Record'Class;
-      Entity : Entity_Information) return String;
-   --  Return the documentation for the entity (prefixed by a LF char if not
-   --  null)
-
-   function Get_Parameters
-     (Entity : Entity_Information;
-      Widget : access Gtk_Widget_Record'Class;
-      Font   : Pango_Font_Description) return Pango_Layout;
-   --  Return the list of parameters for the entity
-
-   function Get_Instance (Entity_Ref : Entity_Reference) return String;
-   --  Return the text describing from what instance the entity is
-
    ---------------------
    -- Create_Tooltips --
    ---------------------
@@ -106,142 +88,6 @@ package body Src_Editor_Box.Tooltips is
       return new Editor_Tooltips'
         (Pixmap_Tooltips with Box => Source_Editor_Box (Box));
    end Create_Tooltips;
-
-   --------------------
-   -- Get_Parameters --
-   --------------------
-
-   function Get_Parameters
-     (Entity : Entity_Information;
-      Widget : access Gtk_Widget_Record'Class;
-      Font   : Pango_Font_Description) return Pango_Layout
-   is
-      use Ada.Strings.Unbounded;
-      --  ??? Display should depend on the language
-      Iter                          : Subprogram_Iterator;
-      Param                         : Entity_Information;
-      Param_Type                    : Entity_Information;
-      Result                        : Unbounded_String;
-      Longuest_Param, Longuest_Type : Gint := 0;
-      Layout                        : Pango_Layout;
-      Tabs                          : Pango_Tab_Array;
-      Char_Width, Char_Height       : Gint;
-   begin
-      if Is_Subprogram (Entity) then
-         Layout := Create_Pango_Layout (Widget, "");
-         Set_Font_Description (Layout, Font);
-
-         Iter := Get_Subprogram_Parameters (Subprogram => Entity);
-
-         Get (Iter, Param);
-
-         if Param = null then
-            Result := To_Unbounded_String ("<b>(No parameter)</b>");
-
-         else
-            Result := To_Unbounded_String ("<b>Parameters:</b>");
-
-            while Param /= null loop
-               Set_Markup (Layout, "   <b>" & Get_Name (Param).all & "</b>");
-               Get_Pixel_Size (Layout, Char_Width, Char_Height);
-               Longuest_Param := Gint'Max (Longuest_Param, Char_Width);
-
-               Set_Text (Layout, ": " & Image (Get_Type (Iter)));
-               Get_Pixel_Size (Layout, Char_Width, Char_Height);
-               Longuest_Type := Gint'Max (Longuest_Type, Char_Width);
-
-               Next (Iter);
-               Get (Iter, Param);
-            end loop;
-         end if;
-
-         Pango_New (Tabs, Initial_Size => 2, Positions_In_Pixels => True);
-         Set_Tab (Tabs, 0, Location => Longuest_Param + 2);
-         Set_Tab (Tabs, 1, Location => Longuest_Param + 2 + Longuest_Type);
-         Set_Tabs (Layout, Tabs);
-
-         Iter := Get_Subprogram_Parameters (Subprogram => Entity);
-         loop
-            Get (Iter, Param);
-            exit when Param = null;
-
-            Result := Result & ASCII.LF
-              & "   <b>" & Get_Name (Param).all & "</b>" & ASCII.HT
-              & ": " & Image (Get_Type (Iter)) & ASCII.HT & " ";
-
-            if Get_Type (Iter) = Access_Parameter then
-               Param_Type := Pointed_Type (Param);
-            else
-               Param_Type := Get_Type_Of (Param);
-            end if;
-
-            if Param_Type /= null then
-               Result := Result & Get_Name (Param_Type).all;
-            end if;
-
-            Next (Iter);
-         end loop;
-
-         Param := Returned_Type (Entity);
-         if Param /= null then
-            Result := Result & ASCII.LF & "   return <b>"
-              & Get_Name (Param).all & "</b>";
-         end if;
-
-         Set_Markup (Layout, To_String (Result));
-
-         Free (Tabs);
-         return Layout;
-      else
-         return null;
-      end if;
-   end Get_Parameters;
-
-   ------------------
-   -- Get_Instance --
-   ------------------
-
-   function Get_Instance (Entity_Ref : Entity_Reference) return String is
-      use Ada.Strings.Unbounded;
-      Result  : Unbounded_String;
-      Inst    : Entity_Instantiation;
-      Inst_E  : Entity_Information;
-      Inst_Of : Entity_Information;
-
-   begin
-      if Entity_Ref /= No_Entity_Reference then
-         Inst := From_Instantiation_At (Entity_Ref);
-
-         while Inst /= No_Instantiation loop
-            Inst_E := Get_Entity (Inst);
-            Inst_Of := Is_Instantiation_Of (Inst_E);
-
-            if Inst_Of = null then
-               Result := Result
-                 & (-"from instance at ");
-            else
-               Result := Result
-                 & (-"from instance of ")
-                 & Get_Name (Inst_Of).all & ':'
-                 & Base_Name (Get_Filename
-                     (Get_File (Get_Declaration_Of (Inst_Of)))) & ':'
-                 & Image (Get_Line (Get_Declaration_Of (Inst_Of)))
-                 & ASCII.LF & "  at ";
-            end if;
-
-            Result := Result
-              & Get_Name (Inst_E).all
-              & ':'
-              &  Base_Name (Get_Filename
-                              (Get_File (Get_Declaration_Of (Inst_E)))) & ':'
-              & Image (Get_Line (Get_Declaration_Of (Inst_E)))
-              & ASCII.LF & ASCII.LF;
-            Inst := Generic_Parent (Inst);
-         end loop;
-      end if;
-
-      return To_String (Result);
-   end Get_Instance;
 
    --------------------------
    -- Get_Declaration_Info --
@@ -286,17 +132,6 @@ package body Src_Editor_Box.Tooltips is
                 "Unexpected exception: " & Exception_Information (E));
          Entity := null;
    end Get_Declaration_Info;
-
-   -----------------------
-   -- Get_Documentation --
-   -----------------------
-
-   function Get_Documentation
-     (Kernel : access Kernel_Handle_Record'Class;
-      Entity : Entity_Information) return String is
-   begin
-      return Get_Documentation (Get_Language_Handler (Kernel), Entity);
-   end Get_Documentation;
 
    ----------
    -- Draw --
@@ -504,88 +339,9 @@ package body Src_Editor_Box.Tooltips is
          end if;
 
          Ref (Entity);
+         --  ??? What is the purpose of the Ref/Unref call?
 
-         declare
-            Str   : constant String :=
-                      "<b>" & Attributes_To_String (Get_Attributes (Entity)) &
-                      ' ' &
-                      (-Kind_To_String (Get_Kind (Entity))) & ' ' &
-                      Get_Full_Name (Entity, ".") & "</b>" &
-                      ASCII.LF &
-                      (-"declared at ") &
-                      Base_Name (Get_Filename
-                                 (Get_File (Get_Declaration_Of (Entity)))) &
-                      ':' & Image (Get_Line (Get_Declaration_Of (Entity)));
-            Str2   : constant String :=
-                       Get_Instance (Entity_Ref)
-                       & Get_Documentation (Box.Kernel, Entity);
-            Font   : constant Pango_Font_Description :=
-                       Get_Pref (Default_Font);
-            Layout1, Layout2, Layout3 : Pango_Layout;
-            Width, Height, W1, H1, W2, H2, W3, H3 : Gint := 0;
-            GC     : Gdk.Gdk_GC;
-         begin
-            Layout1 := Create_Pango_Layout (Widget, "");
-
-            if Status = Overloaded_Entity_Found
-              or else Status = Fuzzy_Match
-            then
-               Set_Markup (Layout1,
-                           -("(Cross-references info not up-to-date, "
-                             & "this is a guess)")
-                           & ASCII.LF & Str);
-            else
-               Set_Markup (Layout1, Str);
-            end if;
-
-            Set_Font_Description (Layout1, Font);
-            Get_Pixel_Size (Layout1, W1, H1);
-
-            if Str2 /= "" then
-               Layout2 := Create_Pango_Layout (Widget, Str2);
-               Set_Font_Description (Layout2, Font);
-               Get_Pixel_Size (Layout2, W2, H2);
-            end if;
-
-            Layout3 := Get_Parameters (Entity, Widget, Font);
-            if Layout3 /= null then
-               Get_Pixel_Size (Layout3, W3, H3);
-               H3 := H3 + 5;
-            end if;
-
-            Width  := Gint'Max (W1, W2);
-            Height := 6 + H1 + H2;
-            Width  := 4 + Gint'Max (Width, W3);
-            Height := Height + H3;
-
-            Gdk_New (GC, Get_Window (Widget));
-            Set_Foreground (GC, Get_Pref (Tooltip_Color));
-
-            Gdk.Pixmap.Gdk_New (Pixmap, Get_Window (Widget), Width, Height);
-            Draw_Rectangle (Pixmap, GC, True, 0, 0, Width - 1, Height - 1);
-
-            Set_Foreground (GC, Black (Get_Default_Colormap));
-            Draw_Rectangle (Pixmap, GC, False, 0, 0, Width - 1, Height - 1);
-
-            Draw_Layout (Pixmap, GC, 2, 0, Layout1);
-            Unref (Layout1);
-
-            if Layout2 /= null or else Layout3 /= null then
-               Draw_Line (Pixmap, GC, 0, H1 + 1, Width - 1, H1 + 1);
-            end if;
-
-            if Layout2 /= null then
-               Draw_Layout (Pixmap, GC, 2, H1 + 3, Layout2);
-               Unref (Layout2);
-            end if;
-
-            if Layout3 /= null then
-               Draw_Layout (Pixmap, GC, 2, H1 + H2 + 8, Layout3);
-               Unref (Layout3);
-            end if;
-
-            Unref (GC);
-         end;
+         Pixmap := Draw_Tooltip (Box.Kernel, Entity, Entity_Ref, Status);
 
          Unref (Entity);
       end;
