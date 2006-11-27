99ba2ebc2569d355709426e303f052d9478580aa
Nicolas Setton <setton@adacore.com>
2006-11-27 16:31:13 +0000

    Minor edit.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@100104 936e1b1b-40f2-da11-902a-00137254ae57

--- common/src/language-documentation.ads
+++ common/src/language-documentation.ads
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                      Copyright (C) 2006                      --
+--                        Copyright (C) 2006                         --
 --                              AdaCore                              --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
