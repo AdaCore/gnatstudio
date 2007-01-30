06688b729df821ee6c2cf001dc8019d4b750c429
Nicolas Setton <setton@adacore.com>
2007-01-30 14:17:35 +0000

    New package, introduced to break the dependency between Language_Handlers and GtkAda. Part of G129-013.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@100546 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ kernel/src_info/language_handlers-gui.adb
@@ -0,0 +1,84 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2007                         --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Basic_Types;               use Basic_Types;
+with Case_Handling;             use Case_Handling;
+with GNAT.OS_Lib;               use GNAT.OS_Lib;
+with GPS.Intl;                  use GPS.Intl;
+with Gtk.Combo;                 use Gtk.Combo;
+with Gtk.GEntry;                use Gtk.GEntry;
+with Gtk.List;                  use Gtk.List;
+with Gtk.List_Item;             use Gtk.List_Item;
+with VFS;                       use VFS;
+
+package body Language_Handlers.GUI is
+
+   ---------------------------
+   -- Create_Language_Combo --
+   ---------------------------
+
+   function Create_Language_Combo
+     (Handler : access Language_Handler_Record'Class;
+      File    : VFS.Virtual_File;
+      Default : String := "") return Gtk_Combo
+   is
+      Combo     : Gtk_Combo;
+      Languages : Argument_List := Known_Languages (Handler, Sorted => True);
+      Project_Lang : String :=
+        Get_Language_From_File (Handler, File, From_Project_Only => True);
+      Item : Gtk_List_Item;
+   begin
+      Gtk_New (Combo);
+      Set_Editable (Get_Entry (Combo), False);
+
+      if Project_Lang = "" then
+         Gtk_New (Item, -"(From project) unknown");
+      else
+         Mixed_Case (Project_Lang);
+         Gtk_New (Item, -"(From project) " & Project_Lang);
+      end if;
+
+      Add (Get_List (Combo), Item);
+      Show_All (Item);
+
+      for L in Languages'Range loop
+         Gtk_New (Item, Languages (L).all);
+         Add (Get_List (Combo), Item);
+         Show_All (Item);
+      end loop;
+
+      Free (Languages);
+
+      if File = VFS.No_File and then Default /= "" then
+         Set_Text (Get_Entry (Combo), Default);
+      elsif File /= VFS.No_File
+        and then Language_Is_Overriden (Handler, File)
+      then
+         Set_Text (Get_Entry (Combo), Get_Language_From_File (Handler, File));
+      elsif Project_Lang = "" then
+         Set_Text (Get_Entry (Combo), -"(From project) unknown");
+      else
+         Set_Text (Get_Entry (Combo), -"(From project) " & Project_Lang);
+      end if;
+
+      return Combo;
+   end Create_Language_Combo;
+
+end Language_Handlers.GUI;
---
+++ kernel/src_info/language_handlers-gui.ads
@@ -0,0 +1,39 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2007                         --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+--  This package contains graphical utilities related to Language_Handlers.
+
+with Gtk.Combo;
+with VFS;
+
+package Language_Handlers.GUI is
+
+   function Create_Language_Combo
+     (Handler : access Language_Handler_Record'Class;
+      File    : VFS.Virtual_File;
+      Default : String := "") return Gtk.Combo.Gtk_Combo;
+   --  Create a combo box to select the language for File.
+   --  File is used to select the default value in the combo.
+   --  The first entry in the combo always indicates that the language from the
+   --  project should be used.
+   --  Default is used when File is VFS.No_File (and thus has no associated
+   --  language)
+
+end Language_Handlers.GUI;
