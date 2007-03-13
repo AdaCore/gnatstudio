92e7c0128aa7fbed612f4e5e89ad685caceef1dc
Quentin Ochem <ochem@adacore.com>
2007-03-13 09:07:16 +0000

    (File_Array_Access): Now access all, in order to generate the corresponding  java binding.
    For G222-019.
    
    
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@100952 936e1b1b-40f2-da11-902a-00137254ae57

--- common/src/vfs.ads
+++ common/src/vfs.ads
@@ -55,7 +55,7 @@ package VFS is
    type Virtual_File_Access is access constant Virtual_File;
 
    type File_Array is array (Natural range <>) of Virtual_File;
-   type File_Array_Access is access File_Array;
+   type File_Array_Access is access all File_Array;
    procedure Unchecked_Free (Arr : in out File_Array_Access);
 
    function Create (Full_Filename : UTF8_String) return Virtual_File;
