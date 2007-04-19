bb632c9a58dd1f963c82ea92d8bc349db67264e2
Jerome Lambourg <lambourg@adacore.com>
2007-04-19 10:49:43 +0000

    Major update. remove use of pygtk (gnatCheck): gnatcheck configuration is now done via xml (add_rule, get_supported_rules): correction of a previous incomplete  refactoring. (check_file): handle the long rules list command line here, previously  performed by the gtk dialog (contextualMenu.filter): remove unused self.project variable. Rerun the xml configuration upon project change, wait for gps startup  before initializing the module. Part of G413-006
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@101314 936e1b1b-40f2-da11-902a-00137254ae57

--- share/library/gnatcheck.py
+++ share/library/gnatcheck.py
@@ -2,11 +2,6 @@
 #  gnatcheck Python support for GPS integration
 #
 import GPS, os, os.path, re, string
-try:
-   import gtk, gobject
-except ImportError:
-   print ("Could not load gnatcheck module: PyGTK not available")
-   raise
 
 def locate_exec_on_path (prog):
     alldirs = string.split (os.getenv('PATH'), os.pathsep)
@@ -15,141 +10,29 @@ def locate_exec_on_path (prog):
             return file
     return ""
 
-# Dialog allowing selection of the different gnatcheck rules
-class rulesDialog (gtk.Dialog):
-   def __init__ (self):
-      self.rules_list = gnatcheckproc.get_supported_rules()
-      self.rules = self.get_project_rules()
-
-      # copy the initial set of rules
-      self.initial_rules = []
-      for r in self.rules:
-         self.initial_rules.append (r)
-
-      gtk.Dialog.__init__ (self)
-      self.set_default_size (400, 450)
-      self.vbox.pack_start (gtk.Label("Gnat check options"), expand=False, fill=False)
-
-      hbox = gtk.HBox(homogeneous=True, spacing=10)
-      self.vbox.pack_start (hbox, expand=False, fill=False)
-
-      self.allChecksButton = gtk.Button ("Activate all checks")
-      self.allChecksButton.connect ("clicked", self.on_all_checks_clicked)
-      hbox.pack_start (self.allChecksButton, expand=True, fill=True)
-
-      self.noChecksButton = gtk.Button ("Deactivate all checks")
-      self.noChecksButton.connect ("clicked", self.on_no_checks_clicked)
-      hbox.pack_start (self.noChecksButton, expand=True, fill=True)
-
-      scrolledWindow = gtk.ScrolledWindow()
-      scrolledWindow.set_policy (gtk.POLICY_NEVER, gtk.POLICY_ALWAYS)
-      self.vbox.pack_start (scrolledWindow, expand=True, fill=True)
-      vbox = gtk.VBox()
-      scrolledWindow.add_with_viewport (vbox)
-
-      self.rulesButton = []
-      none_active = True;
-      all_active = True;
-      for rule in self.rules_list:
-         button = gtk.CheckButton (rule[1])
-         # check if the rule is in activeRules
-         if self.rules.count (rule[0]) > 0:
-            none_active = False
-            button.set_active (True)
-         else:
-            all_active = False
-         # update the rules list upon toggle of the button
-         button.connect ("toggled", self.on_toggled, rule[0])
-         self.rulesButton.append(button)
-         vbox.pack_start (button, expand=False, fill=False)
-
-      # udpate sensitivity of all/no checks buttons
-      self.allChecksButton.set_sensitive (not all_active)
-      self.noChecksButton.set_sensitive (not none_active)
-
-      self.vbox.show_all()
-
-      self.add_buttons (gtk.STOCK_OK, gtk.RESPONSE_OK,
-                        gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
-      self.connect ("response", self.on_response)
-
-   def on_response (self, dialog, response_id):
-      if (response_id == gtk.RESPONSE_OK) and (self.initial_rules != self.rules):
-         self.set_project_rules()
-
-   def on_all_checks_clicked (self, button):
-      for button in self.rulesButton:
-         button.set_active (True)
-      self.allChecksButton.set_sensitive (False)
-      self.noChecksButton.set_sensitive (True)
-      return False
-
-   def on_no_checks_clicked (self, button):
-      for button in self.rulesButton:
-         button.set_active (False)
-      self.allChecksButton.set_sensitive (True)
-      self.noChecksButton.set_sensitive (False)
-      return False
-
-   def on_toggled (self, button, rule):
-      if button.get_active():
-         self.noChecksButton.set_sensitive (True)
-         self.rules.append (rule)
-      else:
-         self.allChecksButton.set_sensitive (True)
-         self.rules.remove (rule)
-      if len(self.rules) == 0:
-         self.noChecksButton.set_sensitive (False)
-      elif len(self.rules) == len(self.rules_list):
-         self.allChecksButton.set_sensitive (False)
-
-   def get_project_rules (self):
-      rules = []
-      opts = GPS.Project.root().get_tool_switches_as_string ("GnatCheck")
-      if re.search ("[+]ALL", opts) != None:
-         for j in self.rules_list:
-            rules.append (j[0])
-
-      defined = re.findall ("[+]R([^ ]+)", opts)
-      for j in defined:
-         if not rules.count (j):
-            rules.append (j)
-         else:
-            GPS.Console ("Messages").write ("Error: gnatcheck rule " + j + " defined twice")
-
-      undefined = re.findall ("[-]R([^ ]+)", opts)
-      for j in undefined:
-         if rules.count(j) > 0:
-            rules.remove (j)
-
-      return rules
-
-   def set_project_rules (self):
-      switches = []
-      GPS.Project.root().clear_attribute_values ("Default_Switches", "IDE", "gnatcheck")
-      if len (self.rules) == 0:
-         return
-      if len (self.rules) < (len (self.rules_list) / 2):
-         switches.append("-ALL");
-         for j in self.rules:
-            switches.append("+R"+j);
-      else:
-         switches.append("+ALL");
-         for j in self.rules_list:
-            if self.rules.count (j[0]) == 0:
-               switches.append("-R"+j[0]);
-      j = len (switches) - 1
-      while j >= 0:
-         GPS.Project.root().add_attribute_values ("Default_Switches", "IDE", "gnatcheck", switches[j])
-         j-=1
-
 # class controling the gnatcheck execution
 class gnatCheck:
    def __init__ (self):
-      self.project = None
       self.rules_list = []
+      self.initXml()
       self.locations_string = "gnatcheck rules violations"
       self.activeRules = []
+
+   def initXml (self):
+      self.get_supported_rules()
+      xml = """
+  <tool name="GnatCheck" package="Ide" index="gnatcheck" >
+     <language>Ada</language>
+     <switches lines="1" use_scrolled_window="true">
+        <title line="1" >General</title>
+      """
+      for j in self.rules_list:
+        xml += '<check label="' + j[1] + '" switch="+R' + j[0] + '" line="1"/>'
+      xml += """
+     </switches>
+   </tool>
+  """
+      GPS.parse_xml (xml)
 
    def init_gnatcheck_cmd (self):
       driver = GPS.Project.root().get_attribute_as_string("gnat", "ide")
@@ -174,7 +57,7 @@ class gnatCheck:
          res = re.split ("^ *([^ ]+) +[-] +(.+) *$", matched)
          if len(res) > 1:
             self.rules_list.append([res[1], res[2]])
-         elif len(self.rules) > 0:
+         elif len(self.rules_list) > 0:
             # Explanation was continuing on next line. Append it to previously
             # inserted rule
             self.rules_list [len (self.rules_list) - 1] [1] += ' ' + matched.strip()
@@ -182,7 +65,7 @@ class gnatCheck:
    def get_supported_rules (self):
       # Verify we have the correct gnatcheck executable
       self.init_gnatcheck_cmd ()
-      self.rules = []
+      self.rules_list = []
       self.rules_analysis_finished = False
       if self.gnatcheckCmd != "":
          process = GPS.Process (self.gnatcheckCmd + " -h", "^.+$",
@@ -208,11 +91,22 @@ class gnatCheck:
       # use progress, specify the file name to check
       cmd +=  " -dd " + file.name()
       # retrieve the rules to check, from project
-      opts = GPS.Project.root().get_tool_switches_as_string ("GnatCheck")
-      if opts == "":
+      opts = GPS.Project.root().get_tool_switches_as_list ("GnatCheck")
+      if opts == []:
          print "Gnat check: no rules to check"
          return
-      cmd += " -rules " + opts
+
+      if len(opts) > (len(self.rules_list)/2):
+         # opts is a long list: define by default all rules, and remove the
+         # ones not present in this list
+         cmd += " -rules +ALL"
+         for r in self.rules_list:
+            if opts.count("+R"+r[0]) == 0:
+               cmd += " -R"+r[0]
+      else:
+        cmd += " -rules -ALL"
+        for o in opts:
+           cmd += " "+o
 
       # clear the Checks category in the Locations view
       if GPS.Locations.list_categories().count (self.locations_string) > 0:
@@ -242,8 +136,7 @@ class contextualMenu (GPS.Contextual):
          return False
       if self.file.language().lower() != "ada":
          return False
-      self.project = GPS.Project.root()
-      srcs = self.project.sources (recursive = True)
+      srcs = GPS.Project.root().sources (recursive = True)
       found = False
       for f in srcs:
          if f.name().lower() == self.file.name().lower():
@@ -257,24 +150,20 @@ class contextualMenu (GPS.Contextual):
    def on_activate (self, context):
       gnatcheckproc.check_file (self.file)
 
-# runs the configuration dialog
-def rundialog():
-   d = rulesDialog()
-   d.run()
-   d.destroy()
-   return
 
 # create the gnatcheck and menus instances.
-gnatcheckproc = gnatCheck()
-gnatcheckmenu = contextualMenu()
-GPS.parse_xml ("""
-  <tool name="GnatCheck" package="Ide" index="GnatCheck" >
-     <language>Ada</language>
-  </tool>
-  <action name="gnatcheck_flags" show-command="false" category="Projects">
-     <shell lang="python" show-command="false" show-task-manager="false">gnatcheck.rundialog()</shell>
-  </action>
-  <menu action="gnatcheck_flags">
-     <title>/Project/Gnat Check rules</title>
-  </menu>
-  """)
+gnatcheckproc = None;
+def init(p):
+   global gnatcheckproc;
+   global gnatcheckmenu;
+   gnatcheckproc = gnatCheck()
+   gnatcheckmenu = contextualMenu()
+
+def on_project_change(p):
+   global gnatcheckproc;
+   if gnatcheckproc != None:
+      gnatcheckproc.initXml();
+
+GPS.Hook ("gps_started").add (init)
+GPS.Hook("project_changed").add (on_project_change);
+GPS.parse_xml ("""<tool name="GnatCheck" package="Ide" index="gnatcheck"/>""");
