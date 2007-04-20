6d9d30c08bb7c8860d5df3940f08c55b9d37d6c0
Arnaud Charlet <charlet@adacore.com>
2007-04-20 13:21:25 +0000

    Fix incorrect formatting done in previous change.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@101343 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/gps-location_view.adb
+++ kernel/src/gps-location_view.adb
@@ -1549,7 +1549,7 @@ package body GPS.Location_View is
       Set_Active (Check, Explorer.Sort_By_Category);
       Append (Menu, Check);
       Widget_Callback.Object_Connect
-         (Check, "activate", Toggle_Sort'Access, Explorer);
+        (Check, "activate", Toggle_Sort'Access, Explorer);
 
       Gtk_New (Mitem);
       Append (Menu, Mitem);
