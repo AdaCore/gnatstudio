0a14b7253f1409e87fcd2c371998f8a15a0fcf0f
Nicolas Setton <setton@adacore.com>
2007-05-22 10:15:07 +0000

    Make variable declarations constant where reported by the compiler. For G522-010.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@101522 936e1b1b-40f2-da11-902a-00137254ae57

--- codefix/src/codefix-text_manager.adb
+++ codefix/src/codefix-text_manager.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                      Copyright (C) 2003-2006                      --
+--                      Copyright (C) 2003-2007                      --
 --                              AdaCore                              --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
@@ -971,7 +971,7 @@ package body Codefix.Text_Manager is
          Str_Array,
          Escape_Manager,
          Step);
-      Real_Result : File_Cursor := Clone (File_Cursor (Result));
+      Real_Result : constant File_Cursor := Clone (File_Cursor (Result));
    begin
       Free (Str_Array);
       Free (Result);
@@ -1869,7 +1869,7 @@ package body Codefix.Text_Manager is
          Str_Array,
          Escape_Manager,
          Step);
-      Real_Result : File_Cursor := Clone (File_Cursor (Result));
+      Real_Result : constant File_Cursor := Clone (File_Cursor (Result));
    begin
       Free (Str_Array);
       Free (Result);
