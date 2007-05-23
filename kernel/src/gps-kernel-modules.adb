b9745eee065e0ca4ce2988d115680bca9d36ccc1
Simon Descarpentries <descarpe@adacore.com>
2007-05-23 21:40:53 +0000

    Revert previous change
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@101552 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/gps-kernel-modules.adb
+++ kernel/src/gps-kernel-modules.adb
@@ -18,6 +18,7 @@
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
+with Ada.Exceptions;            use Ada.Exceptions;
 with Ada.Unchecked_Conversion;
 with Ada.Unchecked_Deallocation;
 with GNAT.Directory_Operations; use GNAT.Directory_Operations;
