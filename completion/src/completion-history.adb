1c8b6e998f5539e2b489a2ef516986453c6b17c8
Quentin Ochem <ochem@adacore.com>
2007-05-23 09:46:49 +0000

    This new package implement storage of completion history. For FA12-012.
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@101535 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ completion/src/completion-history.adb
@@ -0,0 +1,126 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2007                         --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+package body Completion.History is
+
+   -----------------------
+   -- Get_Possibilities --
+   -----------------------
+
+   procedure Get_Possibilities
+     (Resolver   : access Completion_History;
+      Identifier : String;
+      Is_Partial : Boolean;
+      Context    : Completion_Context;
+      Offset     : Integer;
+      Filter     : Possibilities_Filter;
+      Result     : in out Completion_List)
+   is
+      It : Proposal_Stack.Cursor := First (Resolver.Stack);
+
+      List : Completion_List_Extensive_Pckg.Extensive_List_Pckg.List;
+   begin
+      It := First (Resolver.Stack);
+
+      while It /= Proposal_Stack.No_Element loop
+         declare
+            Proposal : Completion_Proposal_Access :=
+              From_Completion_Id (Resolver.Manager, Element (It));
+         begin
+            if Proposal /= null and then Match
+              (Proposal.all, Identifier, Is_Partial, Context, Offset, Filter)
+            then
+               Completion_List_Extensive_Pckg.Extensive_List_Pckg.Append
+                 (List, Proposal.all);
+            end if;
+
+            Free (Proposal);
+         end;
+
+         It := Next (It);
+      end loop;
+
+      Completion_List_Pckg.Append
+        (Result.List, Completion_List_Extensive_Pckg.To_Extensive_List (List));
+   end Get_Possibilities;
+
+   ------------------------
+   -- From_Completion_Id --
+   ------------------------
+
+   function From_Completion_Id
+     (Resolver : access Completion_History; Id : Completion_Id)
+      return Completion_Proposal_Access
+   is
+      pragma Unreferenced (Resolver, Id);
+   begin
+      return null;
+   end From_Completion_Id;
+
+   ----------
+   -- Free --
+   ----------
+
+   procedure Free (Resolver : in out Completion_History) is
+   begin
+      Clear (Resolver.Stack);
+   end Free;
+
+   ----------------------
+   -- Prepend_Proposal --
+   ----------------------
+
+   procedure Prepend_Proposal
+     (Resolver : access Completion_History;
+      Proposal : Completion_Proposal'Class)
+   is
+      It : Proposal_Stack.Cursor := First (Resolver.Stack);
+      Id : Completion_Id :=  To_Completion_Id (Proposal);
+   begin
+      while It /= Proposal_Stack.No_Element loop
+         if Element (It) = Id then
+            Delete (Resolver.Stack, It);
+
+            exit;
+         end if;
+
+         It := Next (It);
+      end loop;
+
+      Prepend (Resolver.Stack, Id);
+
+      if Natural (Length (Resolver.Stack)) > Resolver.Size then
+         Delete_Last
+           (Resolver.Stack,
+            Length (Resolver.Stack) - Count_Type (Resolver.Size));
+      end if;
+   end Prepend_Proposal;
+
+   ----------------------
+   -- Set_History_Size --
+   ----------------------
+
+   procedure Set_History_Size
+     (History : access Completion_History; Size : Natural) is
+   begin
+      History.Size := Size;
+   end Set_History_Size;
+
+end Completion.History;
---
+++ completion/src/completion-history.ads
@@ -0,0 +1,75 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                        Copyright (C) 2007                         --
+--                              AdaCore                              --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Ada.Containers.Indefinite_Doubly_Linked_Lists;
+use Ada.Containers;
+
+package Completion.History is
+
+   type Completion_History is new Completion_Resolver with private;
+   --  A completion history has to be populated by user chosen completion.
+   --  Later, the most recent proposals will be displayed in top of the list.
+   --  This resolver has to be the first one referenced in the manager.
+
+   type Completion_History_Access is access all Completion_History'Class;
+
+   procedure Get_Possibilities
+     (Resolver   : access Completion_History;
+      Identifier : String;
+      Is_Partial : Boolean;
+      Context    : Completion_Context;
+      Offset     : Integer;
+      Filter     : Possibilities_Filter;
+      Result     : in out Completion_List);
+   --  See inherited documentation
+
+   function From_Completion_Id
+     (Resolver : access Completion_History; Id : Completion_Id)
+      return Completion_Proposal_Access;
+   --  See inherited documentation
+
+   procedure Free (Resolver : in out Completion_History);
+   --  See inherited documentation
+
+   procedure Prepend_Proposal
+     (Resolver : access Completion_History;
+      Proposal : Completion_Proposal'Class);
+   --  This function has to be called each time a completion is applied by
+   --  the user. The proposal will be added in the front of the history list.
+   --  If the proposal given in parameter is already in the list, then it will
+   --  be moved to the front.
+
+   procedure Set_History_Size
+     (History : access Completion_History; Size : Natural);
+   --  Sets the history maximum size. By default, it has 50 elements.
+
+private
+
+   package Proposal_Stack is new
+     Indefinite_Doubly_Linked_Lists (Completion_Id);
+
+   use Proposal_Stack;
+
+   type Completion_History is new Completion_Resolver with record
+      Stack : Proposal_Stack.List;
+      Size  : Natural := 50;
+   end record;
+
+end Completion.History;
