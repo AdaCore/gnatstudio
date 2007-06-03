4b38b5046c9ab658efd07429c3f5b2c53e86bafb
Arnaud Charlet <charlet@adacore.com>
2007-06-03 08:33:03 +0000

    (Draw_Expander, Set_Draw_Expander): Removed, no longer used. Draw_Expander was wrongly called for some time already, and previous commit  apparently broke things even more than before, so remove this code  completely. Part of G523-012
    git-svn-id: svn+ssh://svn.eu/Dev/importfromcvs/trunk@101609 936e1b1b-40f2-da11-902a-00137254ae57

--- widgets/src/gtkada-tree_view.adb
+++ widgets/src/gtkada-tree_view.adb
@@ -26,43 +26,16 @@
 -- executable file  might be covered by the  GNU Public License.     --
 -----------------------------------------------------------------------
 
-with System;
-
-with Gdk.Color;       use Gdk.Color;
-with Gdk.Drawable;    use Gdk.Drawable;
-with Gdk.GC;          use Gdk.GC;
-with Gdk.Rectangle;   use Gdk.Rectangle;
-with Gdk;             use Gdk;
-
 with Glib.Object;     use Glib.Object;
 with Glib.Values;     use Glib.Values;
 with Glib;            use Glib;
 
 with Gtk.Enums;       use Gtk.Enums;
-with Gtk.Style;       use Gtk.Style;
 with Gtk.Widget;      use Gtk.Widget;
 
 with Gtkada.Handlers; use Gtkada.Handlers;
 
 package body Gtkada.Tree_View is
-
-   procedure Set_Draw_Expander
-     (Style : System.Address; Expander : System.Address);
-   pragma Import (C, Set_Draw_Expander, "ada_style_set_draw_expander");
-   --  Set the draw_expander function for the widget's class.
-
-   procedure Draw_Expander
-     (Style          : Gtk_Style;
-      Window         : Gdk_Window;
-      State_Type     : Gtk_State_Type;
-      Area           : Gdk_Rectangle_Access;
-      Widget         : System.Address;
-      Detail         : System.Address;
-      X              : Gint;
-      Y              : Gint;
-      Expander_Style : Gtk_Expander_Style);
-   pragma Convention (C, Draw_Expander);
-   --  Override default draw_expander function style.
 
    procedure Row_Expanded_Callback
      (Widget : access GObject_Record'Class;
@@ -78,104 +51,6 @@ package body Gtkada.Tree_View is
      (Widget : access GObject_Record'Class;
       Params : Glib.Values.GValues);
    --  Callback for the "row_inserted" signal.
-
-   procedure Draw_Expander
-     (Style          : Gtk_Style;
-      Window         : Gdk_Window;
-      State_Type     : Gtk_State_Type;
-      Area           : Gdk_Rectangle_Access;
-      Widget         : System.Address;
-      Detail         : System.Address;
-      X              : Gint;
-      Y              : Gint;
-      Expander_Style : Gtk_Expander_Style)
-   is
-      pragma Unreferenced (Detail);
-
-      Red   : constant := 120 * 256;
-      Green : constant := 152 * 256;
-      Blue  : constant := 181 * 256;
-      --  RGB values to emulate Windows XP style.
-
-      Expander_Size      : Gint;
-      Expander_Semi_Size : Gint;
-      Local_X            : Gint;
-      Local_Y            : Gint;
-      GC                 : Gdk.Gdk_GC;
-      Color              : Gdk_Color;
-      Color_Saved        : Gdk_Color;
-      Success            : Boolean;
-
-      procedure Style_Get
-        (Widget   : System.Address;
-         Property : String;
-         Size     : out Gint);
-      pragma Import (C, Style_Get, "ada_gtk_widget_style_get_int");
-
-   begin
-      Style_Get (Widget, "expander_size" & ASCII.NUL, Expander_Size);
-
-      if Expander_Size > 2 then
-         Expander_Size := Expander_Size - 2;
-      end if;
-
-      GC := Get_Fg_GC (Style, State_Type);
-
-      if Area /= null then
-         Set_Clip_Rectangle (GC, Area);
-      end if;
-
-      Expander_Semi_Size := Expander_Size / 2;
-      Local_X := X - Expander_Semi_Size;
-      Local_Y := Y - Expander_Semi_Size;
-
-      Color_Saved := Get_Foreground (Style, State_Type);
-      Set_Rgb (Color, Red, Green, Blue);
-      Alloc_Color (Get_Default_Colormap, Color, Success => Success);
-
-      if Success then
-         Set_Foreground (GC, Color);
-      end if;
-
-      Draw_Line
-        (Window, GC,
-         Local_X + 1, Local_Y, Local_X + Expander_Size - 1, Local_Y);
-      Draw_Line
-        (Window, GC,
-         Local_X + 1, Local_Y + Expander_Size,
-         Local_X + Expander_Size - 1, Local_Y + Expander_Size);
-      Draw_Line
-        (Window, GC,
-         Local_X, Local_Y + 1, Local_X, Local_Y + Expander_Size - 1);
-      Draw_Line
-        (Window, GC,
-         Local_X + Expander_Size, Local_Y + 1,
-         Local_X + Expander_Size, Local_Y + Expander_Size - 1);
-
-      if Success then
-         Set_Foreground (GC, Color_Saved);
-      end if;
-
-      Draw_Line
-        (Window, GC,
-         Local_X + 2, Local_Y + Expander_Semi_Size,
-         Local_X + Expander_Size - 2, Local_Y + Expander_Semi_Size);
-
-      case Expander_Style is
-         when Expander_Collapsed | Expander_Semi_Collapsed =>
-            Draw_Line
-              (Window, GC,
-               Local_X + Expander_Semi_Size, Local_Y + 2,
-               Local_X + Expander_Semi_Size, Local_Y + Expander_Size - 2);
-
-         when Expander_Semi_Expanded | Expander_Expanded =>
-            null;
-      end case;
-
-      if Area /= null then
-         Set_Clip_Rectangle (GC);
-      end if;
-   end Draw_Expander;
 
    ---------------------------
    -- Row_Expanded_Callback --
@@ -271,8 +146,6 @@ package body Gtkada.Tree_View is
 
       Gtk_New (Widget.Model, Real_Column_Types);
       Initialize (Gtk_Tree_View (Widget), Widget.Model);
-      Set_Draw_Expander
-        (Get_Object (Get_Style (Widget)), Draw_Expander'Address);
 
       Gtkada.Handlers.Object_Callback.Object_Connect
         (Widget,
