5651702dd36e0af1f61da8620f166d3dc5cfb107
Arnaud Charlet <charlet@adacore.com>
2007-06-08 13:14:19 +0000

    Update copyright.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@107795 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/debugger.adb
+++ gvd/gvd/debugger.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                      Copyright (C) 2000-2006                      --
+--                      Copyright (C) 2000-2007                      --
 --                              AdaCore                              --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
