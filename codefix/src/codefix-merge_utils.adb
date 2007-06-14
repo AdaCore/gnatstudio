5e21ca651d244cdd9844bbccc566e6a7ae5327c2
Nicolas Setton <setton@adacore.com>
2007-06-14 09:12:32 +0000

    (Get_Next_Area): Give values to "out" variables before returning.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@108147 936e1b1b-40f2-da11-902a-00137254ae57

--- codefix/src/codefix-merge_utils.adb
+++ codefix/src/codefix-merge_utils.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                      Copyright (C) 2002-2006                      --
+--                      Copyright (C) 2002-2007                      --
 --                              AdaCore                              --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
@@ -691,6 +691,8 @@ package body Codefix.Merge_Utils is
       Len := 0;
 
       if It.Info_Index > This.Infos'Last then
+         Start := 0;
+         Info := Original_Unit;
          return;
       end if;
 
