1da6671f289982dc09fad30325971c41d3e8afc3
Jerome Lambourg <lambourg@adacore.com>
2007-06-26 13:23:52 +0000

    (Copy): New procedure for copying files or directories. Part of F803-016
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@111197 936e1b1b-40f2-da11-902a-00137254ae57

--- common/src/vfs.ads
+++ common/src/vfs.ads
@@ -1,8 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                     Copyright (C) 2003-2007                       --
---                              AdaCore                              --
+--                 Copyright (C) 2003-2007, AdaCore                  --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -151,6 +150,12 @@ package VFS is
       Full_Name : String;
       Success   : out Boolean);
    --  Rename a file or directory. This does not work for remote files
+
+   procedure Copy
+     (File        : Virtual_File;
+      Target_Name : String;
+      Success     : out Boolean);
+   --  Copy a file or directory. This does not work for remote files
 
    procedure Delete (File : Virtual_File; Success : out Boolean);
    --  Remove file from the disk. This also works for remote files
