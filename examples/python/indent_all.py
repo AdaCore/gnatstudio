22c0ed207ff6968f0c13bc439f7fce4af6353a4f
Emmanuel Briot <briot@adacore.com>
2007-07-20 09:18:02 +0000

    Shows how to indent multiple source files (G719-005)
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@113727 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ examples/python/indent_all.py
@@ -0,0 +1,15 @@
+"""This script shows how to reindent automatically all source files from
+   all projects"""
+
+from GPS import *
+
+def indent_all (menu):
+   for p in Project.root().dependencies(recursive=True):
+      for s in p.sources():
+         ed = EditorBuffer.get (s)
+         ed.indent ()
+         ed.save()
+         ed.close()
+   Console().write ("Done indenting")
+
+Menu.create ("/Edit/Indent all files", indent_all)
