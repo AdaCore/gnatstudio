47d3cba0ea2e6eca741a162f4c4250338a88d911
Emmanuel Briot <briot@adacore.com>
2007-07-20 10:55:09 +0000

    Initial implementation for D825-009. This script lists unused entities in a file, project or application
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@113734 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ share/library/unused_entities.py
@@ -0,0 +1,115 @@
+"""This plug-in provides menus to list unusued entities
+
+This menu adds some submenus to /Navigate that will show the list of entities
+that are declared in the whole application, a specific project or a specific
+file, and that are not used anywhere. In some cases, the compiler can warn
+about unused entities local to a subprogram (or even to a package body in
+Ada), but cannot do so for entities declared in specs.
+
+The global entities that are not used anywhere will be listed in the locations
+window, so that you can click on them to jump to their declaration.
+
+For the time being, subprograms that are primitive operations of a tagged
+type (or method of a class in languages other than Ada) are not listed, since
+they might actually be called through dynamic dispatching and should not be
+removed.
+"""
+
+
+#############################################################################
+## No user customization below this line
+#############################################################################
+
+from GPS import *
+
+def EntityIterator (where):
+  """Return all entities from WHERE"""
+  if not where:
+     for s in Project.root().sources (recursive=True):
+       for e in s.entities():
+          yield e
+  elif isinstance (where, Project):
+    for s in where.sources():
+       for e in s.entities():
+          yield e
+  elif isinstance (where, File):
+    for e in where.entities():
+      yield e
+
+
+def GlobalIterator (where):
+   """Return all global entities from WHERE"""
+   for e in EntityIterator (where):
+     if e.attributes()["global"]:
+        yield e
+
+
+def is_unused (entity):
+   refs = entity.references \
+      (include_implicit=True, synchronous=True, show_kind=True)
+   for loc,kind in refs.iteritems():
+     if kind != 'declaration' \
+       and kind != 'body' \
+       and kind != 'label':
+        # Logger ("UNUSED").log (`entity` + " not unused because of " + `loc`)
+        return False
+
+   # If we have a primitive operation, do not report it for now, since it
+   # might actually be called through dispatching. We do not know yet how
+   # to test that
+
+   if entity.primitive_of():
+      return False
+
+   #Logger ("UNUSED").log (`entity` + " is unused: " + `refs`)
+   return True
+
+
+def UnusedIterator (where, globals_only):
+   """Return all unused entities from WHERE, and only global entities if
+      GLOBALS_ONLY is true"""
+   if globals_only:
+      iter = GlobalIterator
+   else:
+      iter = EntityIterator
+
+   for e in iter(where):
+     if is_unused (e):
+        yield e
+
+def show_unused_entities (where, globals_only):
+   """List all unused global entities from WHERE in the locations window"""
+   Editor.register_highlighting ("Unused_Entities", "blue")
+   Locations.remove_category ("Unused entity")
+
+   set_busy()
+   for e in UnusedIterator (where, globals_only=globals_only):
+      Locations.add (category  = "Unused entity",
+                     file      = e.declaration().file(),
+                     line      = e.declaration().line(),
+                     column    = e.declaration().column(),
+                     message   = "unused entity " + e.name(),
+                     highlight = "Unused_Entities",
+                     length    = len (e.name()))
+   unset_busy()
+
+def show_unused_entities_in_file (menu):
+    show_unused_entities (EditorBuffer.get().file(), True)
+def show_unused_entities_in_project (menu):
+    show_unused_entities (EditorBuffer.get().file().project(), True)
+def show_unused_entities_in_projects (menu):
+    show_unused_entities (None, True)
+
+
+Menu.create ("/Navigate/List unused entities/From file",
+             on_activate=show_unused_entities_in_file,
+             ref="Goto Body",
+             add_before=False)
+Menu.create ("/Navigate/List unused entities/From project",
+             on_activate=show_unused_entities_in_project,
+             ref="Goto Body",
+             add_before=False)
+Menu.create ("/Navigate/List unused entities/From all projects",
+             on_activate=show_unused_entities_in_projects,
+             ref="Goto Body",
+             add_before=False)
