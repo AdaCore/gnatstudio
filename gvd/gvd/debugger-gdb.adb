5b0b9ea99f2e0915c33e79f002c421f9bdaf7757
Nicolas Setton <setton@adacore.com>
2007-09-12 14:02:30 +0000

    [complete previous revision log] (Connect_To_Target_If_Needed): New subprogram. (Initialize): If no executable was specified but "--target" options were  passed on the command line, connect at debugger initialization. Fixes G726-020.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@115469 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/debugger-gdb.adb
+++ gvd/gvd/debugger-gdb.adb
@@ -883,7 +883,6 @@ package body Debugger.Gdb is
          --  Connect to the target, if needed. This is normally done by
          --  Set_Executable, but GPS should also connect immediately if
          --  the corresponding options were passed on the command line.
-
          Connect_To_Target_If_Needed (Debugger);
 
          --  Indicate that a new executable is present (even if there is none,
