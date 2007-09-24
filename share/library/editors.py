8f56512113b759f145506fc74c821380a9cd7694
Florian Villoing <villoing@adacore.com>
2007-09-24 12:51:18 +0000

    Add a new plug-ins that provides a menu allowing to close all editors. For G921-015.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@116032 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ share/library/editors.py
@@ -0,0 +1,22 @@
+"""Add a menu to close all editors
+
+This script adds a new menu /File/Close Add Editors that close all open editors
+and only editors upon activation.
+"""
+
+^L
+############################################################################
+## No user customization below this line
+############################################################################
+
+import GPS
+
+
+def close_editors(menu):
+   for ed in GPS.EditorBuffer.list():
+      ed.close()
+
+GPS.Menu.create ("/File/Close All Editors",
+                 on_activate=close_editors,
+                 ref="Close All",
+                 add_before=False);
