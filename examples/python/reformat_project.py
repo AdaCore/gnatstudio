a4f00b142edb19f918506b98d2c27a735fe69a4b
Florian Villoing <villoing@adacore.com>
2007-11-28 13:36:27 +0000

    New python example that demonstrate how to reformat all sources from a project in a single command line. For GB27-021.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@119729 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ examples/python/reformat_project.py
@@ -0,0 +1,31 @@
+# This example demonstrates how to reformat all sources of a project
+# You can do that with the following command line:
+#
+#    $ gps -P<your_project> --load=python:reformat_project.py
+#
+# where <your_project> is your project file.
+
+import GPS
+
+# You can update the following preference values your needs
+GPS.Preference ("Ada-Ident-Casing").set ("Automatic")
+GPS.Preference ("Ada-Casing-Policy").set ("On_The_Fly")
+GPS.Preference ("Ada-Align-On-Colons").set (True)
+GPS.Preference ("Ada-Align-On-Arrows").set (True)
+GPS.Preference ("Ada-Format-Operators").set (True)
+GPS.Preference ("Ada-Format-Operators").set (True)
+GPS.Preference ("Ada-Align-Decl-On-Colon").set (True)
+GPS.Preference ("Ada-Stick-Comments").set (False)
+GPS.Preference ("Ada-Reserved-Casing").set ("Lower")
+
+# Beginning of the processing
+for buf in GPS.EditorBuffer.list():
+   buf.close()
+
+for f in GPS.Project.root().sources (recursive=True):
+   buf = GPS.EditorBuffer.get (f)
+   buf.select()
+   GPS.execute_action ("Format Selection")
+   buf.save (interactive=False)
+
+GPS.exit()
