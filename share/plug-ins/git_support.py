5ffca7240f1e75149897fa295bce0a697a62478e
Pascal Obry <obry@adacore.com>
2007-12-09 19:15:42 +0000

    Initial git support for GPS VCS -- Work for GC06-020.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@119959 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ share/plug-ins/git_support.py
@@ -0,0 +1,12 @@
+
+import os.path
+
+def from_git_root (filename):
+    "Given a filename it returns the pathname relative to the Git root"
+    dir=os.getcwd()
+    full=""
+    while not os.path.exists(dir + '/.git'):
+        full = os.path.basename (dir) + "/" + full
+        dir = os.path.dirname (dir)
+    full = full + filename
+    return full
