2479aee3c39e6a9c8738f2de29167461c519544a
Arnaud Charlet <charlet@adacore.com>
2008-01-03 14:31:58 +0000

    (Interrupt): Remove unmodified pragma, no longer needed.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@120352 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/commands-custom.adb
+++ kernel/src/commands-custom.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                 Copyright (C) 2001-2007, AdaCore                  --
+--                 Copyright (C) 2001-2008, AdaCore                  --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -2386,7 +2386,6 @@ package body Commands.Custom is
    ---------------
 
    procedure Interrupt (Command : in out Custom_Command) is
-      pragma Unmodified (Command);
    begin
       if Command.Execution /= null then
          Command.Execution.Cmd_Index := Command.Components'First;
