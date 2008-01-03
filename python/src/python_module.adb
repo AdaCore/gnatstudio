0d4e86914c1d313ee9835ff6871543bc032ebc8b
Arnaud Charlet <charlet@adacore.com>
2008-01-03 14:24:57 +0000

    Remove pragma Unmodified, now that warning has been tuned in GNAT.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@120349 936e1b1b-40f2-da11-902a-00137254ae57

--- python/src/python_module.adb
+++ python/src/python_module.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                Copyright (C) 2003-2007, AdaCore                   --
+--                Copyright (C) 2003-2008, AdaCore                   --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -727,7 +727,6 @@ package body Python_Module is
    -------------
 
    procedure Destroy (Module : in out Python_Module_Record) is
-      pragma Unmodified (Module);
    begin
       Unregister_Python_Scripting (Get_Scripts (Get_Kernel (Module)));
    end Destroy;
