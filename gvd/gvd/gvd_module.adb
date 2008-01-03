59b8558885b1315ed47f53d987558fc4baa07d0e
Arnaud Charlet <charlet@adacore.com>
2008-01-03 14:26:21 +0000

    Remove pragma Unmodified now that warning has been tuned in GNAT.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@120350 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/gvd_module.adb
+++ gvd/gvd/gvd_module.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                              G P S                                --
 --                                                                   --
---                Copyright (C) 2001-2007, AdaCore                   --
+--                Copyright (C) 2001-2008, AdaCore                   --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -2732,7 +2732,6 @@ package body GVD_Module is
    -------------
 
    procedure Destroy (Id : in out GVD_Module_Record) is
-      pragma Unmodified (Id);
    begin
       Debug_Terminate (Get_Kernel (Id));
    end Destroy;
