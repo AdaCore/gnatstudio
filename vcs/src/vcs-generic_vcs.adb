4e90bdd2011cd3d3c19267f59d76236b074f03f8
Pascal Obry <obry@adacore.com>
2008-01-07 18:38:03 +0000

    Minor reformatting
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@120504 936e1b1b-40f2-da11-902a-00137254ae57

--- vcs/src/vcs-generic_vcs.adb
+++ vcs/src/vcs-generic_vcs.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                 Copyright (C) 2001-2007, AdaCore                  --
+--                 Copyright (C) 2001-2008, AdaCore                  --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -2000,7 +2000,6 @@ package body VCS.Generic_VCS is
          exit when Matches (0) = No_Match;
 
          declare
-            Command : Custom_Command_Access;
             Rev     : constant String :=
                         S (Matches (Parser.Repository_Rev_Index).First
                            .. Matches (Parser.Repository_Rev_Index).Last);
@@ -2013,6 +2012,7 @@ package body VCS.Generic_VCS is
             Log     : constant String :=
                         S (Matches (Parser.Log_Index).First
                            .. Matches (Parser.Log_Index).Last);
+            Command : Custom_Command_Access;
          begin
             Create
               (Command, -"add log", Kernel,
@@ -2083,13 +2083,13 @@ package body VCS.Generic_VCS is
          exit when Matches (0) = No_Match;
 
          declare
-            Command : Custom_Command_Access;
             Rev     : constant String :=
                         S (Matches (Parser.Repository_Rev_Index).First
                            .. Matches (Parser.Repository_Rev_Index).Last);
             Sym     : constant String :=
                         S (Matches (Parser.Sym_Index).First
                            .. Matches (Parser.Sym_Index).Last);
+            Command : Custom_Command_Access;
          begin
             Create
               (Command, -"add revision", Kernel,
