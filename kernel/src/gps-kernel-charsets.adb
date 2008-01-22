7529a8b2a9d8e92a09852d615192152326b918ec
Arnaud Charlet <charlet@adacore.com>
2008-01-22 09:54:42 +0000

    (Charsets): Add iso-8859-15. Part of H122-001
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@120983 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/gps-kernel-charsets.adb
+++ kernel/src/gps-kernel-charsets.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                      Copyright (C) 2005-2007, AdaCore             --
+--                      Copyright (C) 2005-2008, AdaCore             --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -61,7 +61,9 @@ package body GPS.Kernel.Charsets is
 
    Charsets : constant Charset_Description_Array :=
      ((Name        => new String'("ISO-8859-1"),
-       Description => new String'("Western European (ISO-8859-1)")),
+       Description => new String'("Western/Latin-1 (ISO-8859-1)")),
+      (Name        => new String'("ISO-8859-15"),
+       Description => new String'("Western/Latin-9 (ISO-8859-15)")),
       (Name        => new String'("ISO-8859-2"),
        Description => new String'("Central European (ISO-8859-2)")),
       (Name        => new String'("ISO-8859-5"),
