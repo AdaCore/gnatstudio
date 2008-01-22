52e31320c8033b886987242186dd983ff0793ce5
Nicolas Setton <setton@adacore.com>
2008-01-22 14:35:19 +0000

    (Gtk_Log): Add an exception handler, since this is a callback.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@121003 936e1b1b-40f2-da11-902a-00137254ae57

--- gps/src/gps-callbacks.adb
+++ gps/src/gps-callbacks.adb
@@ -1,8 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                     Copyright (C) 2001-2007                       --
---                             AdaCore                               --
+--                 Copyright (C) 2001-2008, AdaCore                  --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -57,6 +56,9 @@ package body GPS.Callbacks is
       else
          Trace (Gtk_Trace, Log_Domain & "-MISC: " & Message);
       end if;
+   exception
+      when E : others =>
+         Trace (Exception_Handle, E);
    end Gtk_Log;
 
    --------------------
