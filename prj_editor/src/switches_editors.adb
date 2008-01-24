910b4e271dfe388ceba379e4a570f1fd84e28b15
Florian Villoing <villoing@adacore.com>
2008-01-24 10:57:47 +0000

    (Generate_Project): Fix a uninitialized value. Probably fixes H122-026.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@121109 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/src/switches_editors.adb
+++ prj_editor/src/switches_editors.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                      Copyright (C) 2001-2007, AdaCore             --
+--                      Copyright (C) 2001-2008, AdaCore             --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -376,7 +376,7 @@ package body Switches_Editors is
                     Get_Switches
                       (Rename_Prj,
                        Tool => Tool,
-                       Use_Initial_Value => Is_Default_Value);
+                       Use_Initial_Value => True);
                begin
                   Is_Default_Value := Page = Default_Args;
                   if not Is_Default_Value and then Active (Me) then
