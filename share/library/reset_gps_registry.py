fa0d4361b0675f5cc0ce5e41fa62f98a8bbb0784
Pascal Obry <obry@adacore.com>
2008-01-30 15:37:44 +0000

    New plug-in for reseting the Win32 registry.
    Those settings are to associate some specific extensions like .ads,
    .adb, .gpr to GPS. It can be the case that installing another application
    breaks those default settings. By running this new plug-in (Help/Reset
    Registry) all the values are restored to their GPS default values.
    --
    Work for GC19-021.
    
    
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@121336 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ share/library/reset_gps_registry.py
@@ -0,0 +1,37 @@
+"""Provides an action to reset the Win32 registry settings for GPS.
+
+This script specifically reset all the file associations that are normally
+created when installing GPS. It can be the case that installing another
+application breaks the initial associations.
+
+This is only to be used on Windows plate-forms.
+"""
+
+import GPS
+from _winreg import *
+
+def on_gps_started (hook_name):
+   "Initializes this module."
+
+   GPS.parse_xml ("""<action name='Reset Registry' category='Editor'>
+     <description>Reset the Win32 registry setting for GPS.</description>
+      <shell lang="python" output="none">reset_gps_registry.reset_registry()</shell>
+   </action>
+
+   <menu action='Reset Registry' before="About">
+      <title>/Help/Reset Registry</title>
+   </menu>""")
+
+def write_gnat_reg (ext, val):
+    key = CreateKey (HKEY_CLASSES_ROOT, ext)
+    SetValue (key, None, REG_SZ, val)
+
+def reset_registry():
+    write_gnat_reg (".ads", "GNAT.ads_file")
+    write_gnat_reg (".adb", "GNAT.adb_file")
+    write_gnat_reg (".ada", "GNAT.ada_file")
+    write_gnat_reg (".ali", "GNAT.ali_file")
+    write_gnat_reg (".gpr", "GNAT.project_file")
+    write_gnat_reg (".o", "GNAT.object_file")
+
+GPS.Hook ("gps_started").add (on_gps_started)
