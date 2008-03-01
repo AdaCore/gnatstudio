5c25c01c9116b8dbec0110b5d87ad124b02ab27d
Arnaud Charlet <charlet@adacore.com>
2008-03-01 10:21:05 +0000

    Fix casing.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@122554 936e1b1b-40f2-da11-902a-00137254ae57

--- cpp_module/src/cpp_parser.adb
+++ cpp_module/src/cpp_parser.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                 Copyright (C) 2003-2007, AdaCore                  --
+--                 Copyright (C) 2003-2008, AdaCore                  --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -2762,7 +2762,7 @@ package body CPP_Parser is
          Source  : Source_File;
       begin
          Source := Get_Or_Create
-           (DB      => Handler.Db,
+           (Db      => Handler.Db,
             File    => File,
             Handler => LI_Handler (Handler),
             LI      => null);
