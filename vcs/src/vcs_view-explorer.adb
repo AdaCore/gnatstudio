622f5bab2c095d053eae82e984080c55af27f1f4
Pascal Obry <obry@adacore.com>
2008-03-07 20:34:24 +0000

    Minor reformatting
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@122813 936e1b1b-40f2-da11-902a-00137254ae57

--- vcs/src/vcs_view-explorer.adb
+++ vcs/src/vcs_view-explorer.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                 Copyright (C) 2001-2007, AdaCore                  --
+--                 Copyright (C) 2001-2008, AdaCore                  --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -134,7 +134,7 @@ package body VCS_View.Explorer is
    function Match_Filter
      (Explorer : VCS_Explorer_View_Record;
       Label    : String) return Boolean;
-   --  Returns True if Status can be displayed in the explorer.
+   --  Returns True if Status can be displayed in the explorer
 
    procedure Display_File_Status
      (Kernel         : Kernel_Handle;
@@ -852,7 +852,7 @@ package body VCS_View.Explorer is
 
                Set_File_Information
                  (Context,
-                  File => First_File,
+                  File    => First_File,
                   Project => Project);
             end;
 
