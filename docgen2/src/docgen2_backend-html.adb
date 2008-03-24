21b4a492d2203184b1b0c1b5a9e6bb724c8011b8
Simon Descarpentries <descarpe@adacore.com>
2008-03-24 15:58:21 +0000

    (Gen_Tag): Minor reformating.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@123184 936e1b1b-40f2-da11-902a-00137254ae57

--- docgen2/src/docgen2_backend-html.adb
+++ docgen2/src/docgen2_backend-html.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                   Copyright (C) 2007, AdaCore                     --
+--                  Copyright (C) 2007-2008, AdaCore                 --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -144,9 +144,9 @@ package body Docgen2_Backend.HTML is
    -------------
 
    function Gen_Tag
-     (Backend : access HTML_Backend_Record;
-      Tag     : Language_Entity;
-      Value   : String;
+     (Backend  : access HTML_Backend_Record;
+      Tag      : Language_Entity;
+      Value    : String;
       Emphasis : Boolean := False)
       return String
    is
