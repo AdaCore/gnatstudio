e82d39eef3bdf4249a523392ab5fcf709414d70a
Emmanuel Briot <briot@adacore.com>
2008-05-01 19:39:18 +0000

    vfs.ads was moved to gnatcoll-vfs.ads as part of H114-016  Change not tested, since I am running on linux
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@124428 936e1b1b-40f2-da11-902a-00137254ae57

--- gps/src/dde_win32.adb
+++ gps/src/dde_win32.adb
@@ -1,8 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                        Copyright (C) 2003                         --
---                            ACT-Europe                             --
+--                        Copyright (C) 2003-2008, AdaCore           --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -27,7 +26,7 @@ with Ada.Exceptions;    use Ada.Exceptions;
 
 with GPS.Kernel.Standard_Hooks;
 with Traces;            use Traces;
-with VFS;               use VFS;
+with GNATCOLL.VFS;      use GNATCOLL.VFS;
 
 package body DDE is
 
