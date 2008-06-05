fe4c1c8bd873a095106cd80924b88228b0bb5e33
Arnaud Charlet <charlet@adacore.com>
2008-06-05 15:37:43 +0000

    (Register_Module): Fix menu order.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@125365 936e1b1b-40f2-da11-902a-00137254ae57

--- docgen2/src/docgen2_module.adb
+++ docgen2/src/docgen2_module.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                   Copyright (C) 2007, AdaCore                     --
+--                   Copyright (C) 2007-2008, AdaCore                --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -522,7 +522,7 @@ package body Docgen2_Module is
 
       Register_Menu
         (Kernel, Tools, "_Documentation", Callback => null,
-         Ref_Item => -"Compare", Add_Before => False);
+         Ref_Item => -"Consoles", Add_Before => False);
 
       Register_Menu
         (Kernel,
