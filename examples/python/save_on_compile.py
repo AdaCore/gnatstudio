55d72b6f512c2f909d3a8e268dc604b19296a819
Arnaud Charlet <charlet@adacore.com>
2008-06-13 07:47:19 +0000

    Improve plug-in to save contents in project's object dir. Part of H103-005
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@125620 936e1b1b-40f2-da11-902a-00137254ae57

--- examples/python/save_on_compile.py
+++ examples/python/save_on_compile.py
@@ -1,14 +1,14 @@
 """Automatically saves the contents of the Messages window when a
    compilation has finished.
-   The output is saved in a file called "output" in the current directory.
-   Such a script should be enhanced to save the file in a specific directory,
-   for instance the root project's object dir, to make sure we do not ovewrite
-   an existing file,..."""
+   The output is saved in a file called "messages.txt" in the root project's
+   object directory.
+"""
 
+
 from GPS import *
 
 def on_compilation_finished (hook, category):
-   f = file ("output", "w")
+   f = file (Project.root().object_dirs (False)[0] + "/messages.txt", "w")
    f.write (Console().get_text())
    f.close()
 
