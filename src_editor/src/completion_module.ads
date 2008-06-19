acd29539b31609df80bd4e695417083020eac794
Nicolas Setton <setton@adacore.com>
2008-06-19 10:37:42 +0000

    (In_Smart_Completion): New subprogram.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@125820 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/completion_module.ads
+++ src_editor/src/completion_module.ads
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                  Copyright (C) 2005-2007, AdaCore                 --
+--                  Copyright (C) 2005-2008, AdaCore                 --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -34,5 +34,8 @@ package Completion_Module is
 
    procedure Remove_Completion;
    --  Remove the completion window
+
+   function In_Smart_Completion return Boolean;
+   --  Return True if we are currently showing a smart completion window.
 
 end Completion_Module;
