6c2f91e4ba2f5f6e3e9622217cdb3c26dc7c532a
Pascal Obry <obry@adacore.com>
2008-08-28 09:47:02 +0000

    Minor reformatting.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@128134 936e1b1b-40f2-da11-902a-00137254ae57

--- src_editor/src/src_editor_buffer-text_handling.adb
+++ src_editor/src/src_editor_buffer-text_handling.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                              G P S                                --
 --                                                                   --
---                     Copyright (C) 2003-2008, AdaCore              --
+--                 Copyright (C) 2003-2008, AdaCore                  --
 --                                                                   --
 -- GPS is free  software; you can  redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -55,7 +55,7 @@ package body Src_Editor_Buffer.Text_Handling is
       Column_Begin : out Character_Offset_Type;
       Line_End     : out Editable_Line_Type;
       Column_End   : out Character_Offset_Type);
-   --  Get the positions around the given position.
+   --  Get the positions around the given position
 
    ------------------
    -- Get_Location --
