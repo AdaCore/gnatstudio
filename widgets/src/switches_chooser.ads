724e7c2c778df1bb7abee930ac3bb60480b15a9a
Nicolas Setton <setton@adacore.com>
2008-09-08 10:39:00 +0000

    Minor style fix.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@128453 936e1b1b-40f2-da11-902a-00137254ae57

--- widgets/src/switches_chooser.ads
+++ widgets/src/switches_chooser.ads
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                      Copyright (C) 2007-2008, AdaCore              --
+--                      Copyright (C) 2007-2008, AdaCore             --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
