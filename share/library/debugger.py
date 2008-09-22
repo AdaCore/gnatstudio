c74829b2cebcba90d0459d6a1e08e25a323d84fd
Emmanuel Briot <briot@adacore.com>
2008-09-22 20:46:04 +0000

    Add support for ignore exceptions on specific slocs (H918-015)
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@128859 936e1b1b-40f2-da11-902a-00137254ae57

--- share/library/debugger.py
+++ share/library/debugger.py
@@ -11,24 +11,26 @@
    displays all local variables in the data window, one box per variable.
    This is different from the /Debug/Data/Display Local Variables menu
    which does not pretty-print the variables.
+
+   It adds a way to ignore exceptions raised at specific source locations,
+   while still stopping on all other exceptions. The source locations are
+   set through the contextual menu "Ignore exception breakpoints" (and removed
+   likewise), and are preserved between GPS sessions.
 """
 
 
 from GPS import *
-import text_utils
-
-def print_as_dec_label (context):
-   return "Debug/Print <b> " + context.entity().name() + "</b> as decimal"
-
-def print_as_dec_run (context):
-   Debugger.get().send ("print/d " + context.entity().name(),
-                        show_in_console=True)
+import text_utils, re, os.path
 
 def in_debugger (context):
-   try:
-     return Debugger.get() != None and context.entity() != None
-   except:
-     return False
+   return Debugger.get() != None
+
+def print_in_console (debug, txt):
+   Console ("Debugger Console").write (txt)
+
+######################################
+# Display all local vars in graph    #
+######################################
 
 def display_local_vars (menu):
    buffer = EditorBuffer.get()
@@ -40,11 +42,89 @@ def display_local_vars (menu):
    for v in vars:
       debug.send ("graph display " + v.name(), False)
 
-Contextual ("debug print as decimal").create (
-   label       = print_as_dec_label,
-   on_activate = print_as_dec_run,
-   filter      = in_debugger)
-
 Menu.create (
    path = "/Debug/Data/Graph display local variables",
    on_activate = display_local_vars)
+ 
+######################################
+# Printing as decimal                #
+######################################
+
+def print_as_dec_label (context):
+   return "Debug/Print <b> " + context.entity().name() + "</b> as decimal"
+
+def print_as_dec_run (context):
+   Debugger.get().send ("print/d " + context.entity().name(),
+                        show_in_console=True)
+
+def in_debugger_on_entity (context):
+   try:
+     return in_debugger (context) and context.entity() != None 
+   except:
+     return False
+
+Contextual ("debug print as decimal").create (
+   label       = print_as_dec_label,
+   on_activate = print_as_dec_run,
+   filter      = in_debugger_on_entity)
+
+####################################
+# Breakpoint exceptions            #
+####################################
+
+def debug_add_br_exception_label (context):
+   f = os.path.basename (context.file().name())\
+       + ":" + `context.location().line()`
+   if f in autocont_br:
+     return "Debug/Do not ignore exception breakpoints on line <b>" \
+        + `context.location().line ()` + "</b>"
+   else:
+     return "Debug/Ignore exception breakpoints on line <b>" \
+        + `context.location().line ()` + "</b>"
+
+autocont_br = set ()
+
+def on_debugger_stopped (h, debug):
+   frame = debug.send ("frame", False)
+
+   # Process terminated ?
+   if frame.find ("No stack.") != -1:
+      return
+
+   # The only case where we don't stop at frame 0 is for exceptions. So
+   # if we are on frame 0, we are in an exception
+   if re.search ("^#0\s", frame): return
+
+   m = re.search ("at (.+):(\d+)", frame)
+   file = os.path.basename (m.group(1)) + ":" + m.group(2)
+   if file in autocont_br:
+      debug.non_blocking_send ("cont")
+
+def add_breakpoint_exception (context):
+   global autocont_br
+   # Only consider base names of files, since the debugger does not always
+   # show the full name in the "frame" command
+   f = os.path.basename (context.file().name())\
+       + ":" + `context.location().line()`
+   if f in autocont_br:
+      autocont_br.remove (f)
+   else:
+      autocont_br.add (f)
+   Project.root().set_property ("autocont_br", "--".join (autocont_br), True)
+
+def on_project_view_changed (h):
+   global autocont_br
+   try:
+     autocont_br = set (Project.root().get_property ("autocont_br").split ("--"))
+     Console().write ("The debugger will not stop when an exception is raised at " + "\n".join (autocont_br))
+   except:
+     autocont_br = set ()
+
+Contextual ("debug add breakpoint exception").create (
+   label       = debug_add_br_exception_label,
+   on_activate = add_breakpoint_exception,
+   filter      = in_debugger)
+
+Hook ("debugger_process_stopped").add (on_debugger_stopped)
+Hook ("project_view_changed").add (on_project_view_changed)
+
