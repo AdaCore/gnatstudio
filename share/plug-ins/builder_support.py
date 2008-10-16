ae87b51880baffa6ec58747bac7aec0e616efccf
Arnaud Charlet <charlet@adacore.com>
2008-10-16 10:41:09 +0000

    (Clean Root): Set mode to Manually, less dangerous than clean all, and  also needed for e.g. H129-003 test.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@129771 936e1b1b-40f2-da11-902a-00137254ae57

--- share/plug-ins/builder_support.py
+++ share/plug-ins/builder_support.py
@@ -312,7 +312,7 @@ Clean_Targets = """
 <target model="gprclean" category="C_lean" name="Clean _Root">
     <in-toolbar>FALSE</in-toolbar>
     <icon>gps-clean</icon>
-    <launch-mode>MANUALLY_WITH_DIALOG</launch-mode>
+    <launch-mode>MANUALLY</launch-mode>
     <read-only>TRUE</read-only>
     <command-line>
        <arg>%gprclean</arg>
