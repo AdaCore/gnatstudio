93332c21d73c7bfba8daea16b5be1461db348637
Nicolas Setton <setton@adacore.com>
2008-11-14 10:25:23 +0000

    (Reduce): Revert previous change, which only hides the problem and introduces  regressions. For HB13-012.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@132350 936e1b1b-40f2-da11-902a-00137254ae57

--- common/src/string_utils.adb
+++ common/src/string_utils.adb
@@ -744,66 +744,60 @@ package body String_Utils is
    function Reduce
      (S            : String;
       Max_Length   : Positive := Positive'Last;
-      Continuation : String := "...") return String is
+      Continuation : String := "...") return String
+   is
+      Result : String (S'Range);
+      Len    : Positive := Result'First;
+      Blank  : Boolean  := False;
+
+      Max    : Natural;
+      --  Max if the position of the last character to be returned
+      Cut    : Boolean := False;
+      --  Cut set to true if string was cut before the end at Max characters
+      Char   : Natural := S'First;
+      Next   : Natural := Char;
    begin
-      if S'Length <= Max_Length then
-         return S;
+
+      if Max_Length = Positive'Last then
+         Max := Positive'Last;
+      else
+         Max := S'First + Max_Length - Continuation'Length - 1;
       end if;
 
-      declare
-         Result : String (S'Range);
-         Len    : Positive := Result'First;
-         Blank  : Boolean  := False;
-
-         Max    : Natural;
-         --  Max if the position of the last character to be returned
-         Cut    : Boolean := False;
-         --  Cut set to true if string was cut before the end at Max characters
-         Char   : Natural := S'First;
-         Next   : Natural := Char;
-      begin
-
-         if Max_Length = Positive'Last then
-            Max := Positive'Last;
-         else
-            Max := S'First + Max_Length - Continuation'Length - 1;
+      while Next <= S'Last loop
+         Char := Next;
+         Next := UTF8_Next_Char (S, Char);
+         if Next > S'Last then
+            Next := S'Last + 1;
          end if;
 
-         while Next <= S'Last loop
-            Char := Next;
-            Next := UTF8_Next_Char (S, Char);
-            if Next > S'Last then
-               Next := S'Last + 1;
+         if S (Char) = ASCII.LF or else S (Char) = ASCII.CR
+           or else S (Char) = ASCII.HT or else S (Char) = ' '
+         then
+            if not Blank then
+               Result (Len) := ' ';
+               Len := Len + 1;
+               Blank := True;
             end if;
 
-            if S (Char) = ASCII.LF or else S (Char) = ASCII.CR
-              or else S (Char) = ASCII.HT or else S (Char) = ' '
-            then
-               if not Blank then
-                  Result (Len) := ' ';
-                  Len := Len + 1;
-                  Blank := True;
-               end if;
-
-            else
-               Blank := False;
-
-               Result (Len .. Len + Next - Char - 1) := S (Char .. Next - 1);
-               Len := Len + Next - Char;
-            end if;
-
-            if Len >= Max then
-               Cut := True;
-               exit;
-            end if;
-         end loop;
-
-         if Cut then
-            return Result (Result'First .. Len - 1) & Continuation;
          else
-            return Result (Result'First .. Len - 1);
+            Blank := False;
+
+            Result (Len .. Len + Next - Char - 1) := S (Char .. Next - 1);
+            Len := Len + Next - Char;
          end if;
-      end;
+
+         if Len >= Max then
+            Cut := True;
+            exit;
+         end if;
+      end loop;
+
+      if Cut then
+         return Result (Result'First .. Len - 1) & Continuation;
+      else
+         return Result (Result'First .. Len - 1);
+      end if;
    end Reduce;
 
    ------------
