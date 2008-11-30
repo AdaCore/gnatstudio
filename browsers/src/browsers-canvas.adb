d6fa09f311dbb83d5830bb12cfd4359bf5f3c894
Arnaud Charlet <charlet@adacore.com>
2008-11-30 12:16:12 +0000

    Add warnings off on Dump, now flagged by gnat as unused.
    git-svn-id: svn+ssh://svn.eu/Dev/branches/global/gps-4_3-branch/gps@132887 936e1b1b-40f2-da11-902a-00137254ae57

--- browsers/src/browsers-canvas.adb
+++ browsers/src/browsers-canvas.adb
@@ -156,6 +156,7 @@ package body Browsers.Canvas is
 
    procedure Dump
      (Me : Debug_Handle; Tree : Active_Area_Tree; Indent : Natural := 0);
+   pragma Warnings (Off, Dump);
    --  For debugging purposes, dump the tree to Me.
 
    procedure Destroyed (Browser : access Gtk_Widget_Record'Class);
