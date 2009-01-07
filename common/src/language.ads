79c1b3a4d23c154a5022ea871ddd2b7a836ff604
Arnaud Charlet <charlet@adacore.com>
2009-01-07 14:38:30 +0000

    Fix typos. Add __gnat_separate_run_path_options for compatibility with existing GNAT compilers.
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@135271 936e1b1b-40f2-da11-902a-00137254ae57

--- common/src/language.ads
+++ common/src/language.ads
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                  Copyright (C) 2000-2008, AdaCore                 --
+--                  Copyright (C) 2000-2009, AdaCore                 --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -821,7 +821,7 @@ package Language is
    --  This function looks backwards from the offset given in parameter and
    --  parses the relevant completion expression.
    --  Start_Offset is the offset (in byte) of where we have to look.
-   --  The buffer given in parameter much as a liftime superior or equal to
+   --  The buffer given in parameter must have a lifetime superior or equal to
    --  the resulting parser expression, as it gets referenced by this
    --  expression.
    --  An example, if we have the following Ada code:
@@ -831,11 +831,11 @@ package Language is
    --      Tok_Identifier + Tok_Dot + Tok_Identifier + Tok_Open_Parenthesis
    --      + Tok_Identifier + Tok_Close_Parenthesis + Tok_Dot + Tok_Identifdier
    --  Note that Start_Offset must point on the d, or the last identifier
-   --  returned will only contain a part of the name
+   --  returned will only contain a part of the name.
    --
-   --  This parser may be able to retreive complex expression, e.g. A + B,
+   --  This parser may be able to retrieve complex expression, e.g. A + B,
    --  or A => B in ada. If the flat Simple_Expression is true, then it will
-   --  avoid returning this kind of complex result involving more that on
+   --  avoid returning this kind of complex result involving more that one
    --  top-level object.
    --
    --  The default implementation for any language is to return the current
