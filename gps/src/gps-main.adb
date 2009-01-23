044247e7dafac970a6a61952078ccb8fe7543933
Nicolas Setton <setton@adacore.com>
2009-01-23 09:01:57 +0000

    (Init_Settings): Reset DYLD_LIBRARY_PATH to the contents of GPS_STARTUP_DYLD_LIBRARY_PATH even when this is null: this might mean that the user had an empty DYLD_LIBRARY_PATH, which we need to restore. Merge change 135869.
    git-svn-id: svn+ssh://svn.eu/Dev/branches/global/gps-4_3-branch/gps@136058 936e1b1b-40f2-da11-902a-00137254ae57

--- gps/src/gps-main.adb
+++ gps/src/gps-main.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                 Copyright (C) 2001-2008, AdaCore                  --
+--                 Copyright (C) 2001-2009, AdaCore                  --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -408,9 +408,7 @@ procedure GPS.Main is
       --  is launched through the startup script. In this case it contains the
       --  value that should be set as DYLD_LIBRARY_PATH.
       Tmp := Getenv ("GPS_STARTUP_DYLD_LIBRARY_PATH");
-      if Tmp.all /= "" then
-         Setenv ("DYLD_LIBRARY_PATH", Tmp.all);
-      end if;
+      Setenv ("DYLD_LIBRARY_PATH", Tmp.all);
       Free (Tmp);
 
       Charset := Getenv ("CHARSET");
