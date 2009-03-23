f3580c0d01de043ad924bda13828028451862a7b
Emmanuel Briot <briot@adacore.com>
2009-03-23 17:10:08 +0000

    (first_subtype.py): new plugin, for I323-007
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@141770 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ share/library/first_subtype.py
@@ -0,0 +1,50 @@
+"""
+This plugin adds a new contextual menu entry which points to the first
+subtype of a type. For instance, if you have the following Ada code:
+
+    type T is new Integer;
+    type T1 is T;
+    type T2 is T;
+
+and you click on T2, that contextual menu would jump to the declaration
+of T.
+"""
+
+
+
+from GPS import *
+
+def is_predefined (entity):
+   f = entity.declaration ().file ().name ()
+   return f.find ("predefined") != -1
+
+def get_first_subtype (entity):
+   while True:
+      parent = entity.type ()
+      if is_predefined (parent):
+          return entity
+      entity = parent
+   return None
+
+def goto_first_subtype (context):
+   if context.first_subtype:
+      decl    = context.first_subtype.declaration ()
+      buffer  = EditorBuffer.get (decl.file())
+      buffer.current_view ().goto (
+         EditorLocation (buffer, decl.line (), decl.column ()))
+
+def has_first_subtype (context):
+   if isinstance (context, EntityContext):
+      context.first_subtype = get_first_subtype (context.entity ())
+      return context.first_subtype != None
+   return False
+
+def label (context):
+   return "Goto first subtype of <b>" + context.first_subtype.name() + "</b>"
+
+Contextual ("Goto first subtype").create (
+   on_activate=goto_first_subtype,
+   label=label,
+   filter=has_first_subtype,
+   ref="Goto body of entity",
+   add_before=False)
