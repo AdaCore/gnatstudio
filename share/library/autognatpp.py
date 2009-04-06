5316f4043dae5289fea8797fb6bab55248f46e92
Arnaud Charlet <charlet@adacore.com>
2009-04-06 16:10:16 +0000

    Add comments. Part of I405-005
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@142326 936e1b1b-40f2-da11-902a-00137254ae57

--- share/library/autognatpp.py
+++ share/library/autognatpp.py
@@ -1,6 +1,8 @@
 """This plug-in will automatically pretty-print a source file each time it is
 saved on disk by calling gnatpp. See also autoformat.py for a lighter version
-using the built-in editor reformatter instead.
+using the built-in editor reformatter instead. Warning: note that if you save
+files often, this will generate many calls to gnatpp with no protection/lock,
+so you may end up with multiple gnatpp processes running at the same time.
 """
 
 
