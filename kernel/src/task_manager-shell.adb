d872f637e132d1b1795a8561a117cab5589b2c05
Nicolas Setton <setton@adacore.com>
2009-06-09 17:04:27 +0000

    Initial revision.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@145478 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ kernel/src/task_manager-shell.adb
@@ -0,0 +1,86 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                 Copyright (C) 2009, AdaCore                       --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with GNATCOLL.Scripts; use GNATCOLL.Scripts;
+with GPS.Kernel.Scripts; use GPS.Kernel.Scripts;
+with GPS.Kernel; use GPS.Kernel;
+with GPS.Kernel.Task_Manager; use GPS.Kernel.Task_Manager;
+with Task_Manager.GUI; use Task_Manager.GUI;
+
+package body Task_Manager.Shell is
+
+   Class : constant String := "Task";
+   Task_Class : Class_Type;
+
+   procedure Task_Command_Handler
+     (Data : in out Callback_Data'Class; Command : String);
+   --  Handler for the Task commands
+
+   --------------------------
+   -- Task_Command_Handler --
+   --------------------------
+
+   procedure Task_Command_Handler
+     (Data : in out Callback_Data'Class; Command : String)
+   is
+      Kernel      : constant Kernel_Handle := Get_Kernel (Data);
+      Task_Inst : Class_Instance;
+      Manager   : constant Task_Manager_Access := Get_Task_Manager (Kernel);
+   begin
+      if Manager = null then
+         return;
+      end if;
+
+      if Command = "list" then
+         Set_Return_Value_As_List (Data);
+
+         if Manager.Queues = null then
+            return;
+         end if;
+
+         for J in Manager.Queues.all'Range loop
+            Task_Inst := New_Instance (Get_Script (Data), Task_Class);
+            Set_Data (Task_Inst, Task_Class, J);
+            Set_Return_Value (Data, Task_Inst);
+         end loop;
+
+      elsif Command = "interrupt" then
+         Task_Inst := Nth_Arg (Data, 1, Task_Class);
+
+         Interrupt_Command (Manager, Get_Data (Task_Inst, Task_Class));
+      end if;
+   end Task_Command_Handler;
+
+   -----------------------
+   -- Register_Commands --
+   -----------------------
+
+   procedure Register_Commands
+     (Kernel : access GPS.Kernel.Kernel_Handle_Record'Class)
+   is
+   begin
+      Task_Class := New_Class (Kernel, Class);
+
+      Register_Command
+        (Kernel, "list", 0, 0, Task_Command_Handler'Access, Task_Class, True);
+      Register_Command
+        (Kernel, "interrupt", 0, 0, Task_Command_Handler'Access, Task_Class);
+   end Register_Commands;
+
+end Task_Manager.Shell;
