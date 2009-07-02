f614d2e9cac3fca68f781399a9096ed76b1b8fdf
Quentin Ochem <ochem@adacore.com>
2009-07-02 09:26:42 +0000

    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@146615 936e1b1b-40f2-da11-902a-00137254ae57

--- common/src/language-tree-database.ads
+++ common/src/language-tree-database.ads
@@ -166,7 +166,11 @@ package Language.Tree.Database is
    procedure Update_Contents (File : Structured_File_Access);
    --  This function will re-analyze the full contents of the file
 
-   type Update_Lock is limited new Limited_Controlled with private;
+   --  type Update_Lock is limited new Limited_Controlled with private;
+   --  ??? The above declaration is the actual expected one, but we can't use
+   --  it until the Java code generator is fixed (I702-011)
+
+   type Update_Lock is new Controlled with private;
    --  This type is used to avoid updates on a given file for a limited amount
    --  of time. The lock can be release either explicitely, by doing calls to
    --  Unlock, or implicitely, at object finalization. If two or more locks are
@@ -550,7 +554,7 @@ private
       Update_Locked : Boolean := False;
    end record;
 
-   type Update_Lock is limited new Limited_Controlled with record
+   type Update_Lock is new Controlled with record
       File_Locked : Structured_File_Access;
    end record;
 
