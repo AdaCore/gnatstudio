0ed4bc43fa178535cca4e85d905dda63b4ee98a5
Nicolas Setton <setton@adacore.com>
2009-07-17 16:10:29 +0000

    Remove "font_size" span attributes, apparently not supported under Windows. Integrate tips text. Make the window smaller.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@147346 936e1b1b-40f2-da11-902a-00137254ae57

--- share/plug-ins/tip_of_the_day.py
+++ share/plug-ins/tip_of_the_day.py
@@ -13,8 +13,150 @@ import gtk.gdk
 import os.path
 import re
 
-def parse_tips_file (filename):
-    """ Parse filename and return a list of the form
+tips = """
+
+Tip 1
+
+You'd like the TAB key to insert spaces? Go to Edit->Key shortcuts,
+then select the Editor->Insert Tab With Spaces action, then click on
+'Grab' and hit the 'TAB' key.
+
+Tip 2
+
+Need to view multiple parts of the same file? Use the menu File->New View,
+or hold the shift key, click on the Editor's title bar and drag it to a
+place where you'd like the new view to be created
+
+Tip 3
+
+In order to gain room in your workspace, you can go to Edit->Preferences,
+then Windows and disable the 'Show title bars' option: this will remove
+the title bars on each window.
+
+Tip 4
+
+Did you know that you can move the position of the notebook tabs?
+To do so, you can right click on any tab and select the Tabs location menu.
+Alternatively, you can also go to Edit->Preferences and then Windows.
+
+Tip 5
+
+Edit->Key shortcuts Editor->Subprogram Box then glick 'Grab' and
+type a key short cut (e.g. "control-b control-b")
+
+Tip 6
+
+To temporarily disable auto casing, you can use the alt-q key
+
+Tip 7
+
+To disable casing and indentation on the next key (e.g. Enter key), you
+can use the control-q key and then press e.g. Enter.
+
+Tip 8
+
+To unfloat a window, you can simply click on the close button and it will
+automatically go back to its original location in the GPS main window.
+Alternatively, you can also select the window, and then use the Window->Float
+menu in the main window.
+
+Tip 9
+
+Need to have a quick access to an OS shell? Go to Tools->Consoles->OS Shell
+
+Tip 10
+
+You would like to configure the Build menu items (either modify the commands
+launched, or add/remove menu items)? Go to Build->Settings->Targets
+
+Tip 11
+
+If you have a Makefile in the same directory where you project file is located,
+GPS will automatically parse it and create a Build->Makefile menu with
+all the targets defined in your Makefile
+
+Tip 12
+
+You always perform the same sequence of keys? You can record it as a macro
+and replay it automatically using the Tools->Macro menu
+
+Tip 13
+
+Need to find an entity in your project and its associated
+profile/documentation? Use Tools->Views->Entities
+
+Tip 14
+
+Did you know that you can drag-n-drop the Entities View
+(Tools->Views->Entities) to e.g. the left part of the main window, and it will
+automatically switch to a vertical display to fit the space available?
+
+Tip 15
+
+The Outline View (Tools->Views->Outline) by default displays entities in
+alphabetical order. You can instead get entities sorted by order of appearance
+in the file by right clicking in the Outline View and unselect
+Outline->Sort alphabetically
+
+Tip 16
+
+To rebuild your project with different switches, you can use the Build mode
+combo selector in the main toolbar: by default it provides predefined
+modes (debug, optimize, gprof, ...). You can also create your own build
+modes via simple xml files.
+
+Tip 17
+
+Need to temporarily and quickly add a switch for your build? Use the
+Build->Project menu and you'll get access to the command line launched by GPS
+to perform the build action, with the ability to modify it.
+
+Tip 18
+
+If you need GPS to automatically complete the name of an entity, or fill
+a subprogram profile, simply pressing ctrl-space in a source editor will
+display a completion list with possible completion for the current word.
+Automatic completion is also displayed automatically when you type special
+characters such as '.' or '('
+
+Tip 19
+
+You'd like GPS to make more suggestions automatically to complete identifiers
+while you're typing without having to press control-space? Go to
+Edit->Preferences, Editor and then set the 'Smart competion' preference to
+'Dynamic'. You might also want to tune the default timeout.
+
+Tip 20
+
+Did you know that GPS can highlight all dispatching calls in your source?
+To do so, go to Tools->Plug-ins and enable the 'dispatching.py' plug-in.
+
+Tip 21
+
+Did you know that when opening a contextual menu on a dispatching call,
+GPS is able to list all the possible targets for this call? By default, GPS
+will take into account the cross reference information it has already
+computed in memory, and you can get more accurate and complete results
+by going to Edit->Preferences and change the
+Editor->Submenu for dispatching calls preference to 'Accurate'.
+
+Tip 22
+
+Need to have GPS insert automatically text of Ada constructs? Go to
+Tools->Plug-ins and enable the predef_ada_entity_insertions.py plug-in which
+will add contextual menus in the source editor.
+
+Tip 23
+
+You'd like to highlight all occurrences of the current word in the editor?
+Enable the 'occurrences.py' plug-in in Tools->Plug-ins and then set a key
+short cut for the 'Mark Occurrences' action via the Edit->Shortcuts editor.
+
+
+"""
+
+def parse_tips ():
+    """ Parse the tips string and return a list of the form
         [ (tip_title_1, tip_text_1), (tip_title_2, tip_text_2) ... ]
     """
 
@@ -23,11 +165,9 @@ def parse_tips_file (filename):
     current_tip_title = ""
     current_tip_text  = ""
 
-    f=open(filename)
-
     title_re = re.compile ("Tip [0-9]+:?( .*)?")
 
-    for l in f:
+    for l in tips.split('\n'):
        m = title_re.match(l)
 
        if m:
@@ -38,9 +178,7 @@ def parse_tips_file (filename):
 
        else:
            if len (l) > 1:
-              current_tip_text += l
-
-    f.close()
+              current_tip_text += l + '\n'
 
     return result
 
@@ -54,7 +192,7 @@ def display_tip (title, doc):
 
     title_label = gtk.Label()
     title_label.set_use_markup(True)
-    title_label.set_markup ("""<span font_size="x-large">%s</span>""" % title)
+    title_label.set_markup ("""<big>%s</big>""" % title)
 
     hbox.pack_start (title_label, False, False, 10)
 
@@ -127,7 +265,7 @@ class Tip:
         window = gtk.Window()
         self.window = window
 
-        window.set_default_size (600, 400)
+        window.set_default_size (550, 350)
         window.set_decorated (False)
 
         if parent:
@@ -149,7 +287,7 @@ class Tip:
         title_label = gtk.Label()
         title_label.set_use_markup(True)
         title_label.set_markup (
-          """<span font_size="xx-large">Tip of the Day</span>""")
+          """<big>Tip of the Day</big>""")
 
         img=gtk.Image()
         img.set_from_stock (gtk.STOCK_INFO, gtk.ICON_SIZE_LARGE_TOOLBAR)
@@ -248,8 +386,7 @@ def on_gps_started (hook):
      # If we reach this point, display the tip of the day
 
      # Parse the tips file
-     results = parse_tips_file (os.path.join
-         (os.path.dirname(__file__), "tips.txt"))
+     results = parse_tips ()
 
      # Get the main window
      messages = GPS.MDI.get ("Messages").pywidget()
