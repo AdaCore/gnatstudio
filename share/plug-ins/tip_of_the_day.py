7c724248c5fbab34c5dfe554617cacfb3c73fa81
Arnaud Charlet <charlet@adacore.com>
2009-07-18 09:44:31 +0000

    Take advantage of pango mark ups. Add 7 new tips. Part of I717-007
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@147366 936e1b1b-40f2-da11-902a-00137254ae57

--- share/plug-ins/tip_of_the_day.py
+++ share/plug-ins/tip_of_the_day.py
@@ -18,8 +18,10 @@ tips = """
 Tip 1: Insert spaces with TAB key
 
 You'd like the TAB key to insert spaces? Go to Edit->Key shortcuts,
-then select the Editor->Insert Tab With Spaces action, then click on
-'Grab' and hit the 'TAB' key.
+Editor page, select the <i>Insert Tab With Spaces</i> action, then click on
+<i>Grab</i> and hit the <i>TAB</i> key.
+The number of spaces inserted will depend on the indentation level set
+in the preferences dialog (Edit->Preferences).
 
 Tip 2: Multiple views of the same file
 
@@ -27,25 +29,27 @@ Need to view multiple parts of the same file? Use the menu File->New View,
 or hold the shift key, click on the Editor's title bar and drag it to a
 place where you'd like the new view to be created
 
-Tip 3: How to gain space on your workspace
-
-In order to gain room in your workspace, you can go to Edit->Preferences,
-then Windows and disable the 'Show title bars' option: this will remove
-the title bars on each window.
-
-Tip 4: Move the position of notebook tabs
+Tip 3: Move the position of notebook tabs
 
 Did you know that you can move the position of the notebook tabs?
-To do so, you can right click on any tab and select the Tabs location menu.
-Alternatively, you can also go to Edit->Preferences and then Windows.
+To do so, you can right click on any tab and select the <i>Tabs location</i>
+menu. Alternatively, you can also go to Edit->Preferences and select the
+Windows page.
 
-Tip 5: Add subprogram box
+Tip 4: Multi language builder
 
-Ever wanted to ask GPS to automatically add a box containing the current
-subprogram name just before this subprogram?
-Go to Edit->Key shortcuts, select Editor->Subprogram Box then glick 'Grab'
-and type a key short cut (e.g. "control-b control-b"). You can then use this
-key shortcut in the middle of a subprogram.
+GPS will automatically try to detect whether your project should be built
+using <i>gnatmake</i> (Ada language used only) or <i>gprbuild</i> (other
+languages used).
+If you want to force a specific builder, go to Edit->Preferences, then
+in the General section, set the <i>Multi language builder</i> preference.
+
+Tip 5: Jump to first location
+
+By default GPS will jump to the first location loaded in the <i>Locations</i>
+window (e.g. first build message, or first search result). If you do not
+like this behavior, you can go to Edit->Preferences, select the General
+page and then disable the <i>Jump to first location</i> preference.
 
 Tip 6: Highlight all occurrences of a word
 
@@ -72,24 +76,24 @@ Go to Tools->Consoles->OS Shell
 
 Tip 10: Build menu configuration
 
-You would like to configure the Build menu items (either modify the commands
-launched, or add/remove menu items)? Go to Build->Settings->Targets
+You would like to configure the Build menu items (e.g. modify the commands
+launched, or add/remove menu items)? Go to Build->Settings->Targets.
 
 Tip 11: Makefile support
 
 If you have a Makefile in the same directory where you project file is located,
 GPS will automatically parse it and create a Build->Makefile menu with
-all the targets defined in your Makefile
+all the targets defined in your Makefile.
 
 Tip 12: Keyboard macro
 
 You always perform the same sequence of keys? You can record it as a macro
-and replay it automatically using the Tools->Macro menu
+and replay it automatically using the Tools->Macro menu.
 
 Tip 13: Entities View
 
 Need to find an entity in your project and its associated
-profile/documentation? Use Tools->Views->Entities
+profile/documentation? Use Tools->Views->Entities.
 
 Tip 14: Entities View and drag'n'drop
 
@@ -102,7 +106,7 @@ Tip 15: Sorting in Outline View
 The Outline View (Tools->Views->Outline) by default displays entities in
 alphabetical order. You can instead get entities sorted by order of appearance
 in the file by right clicking in the Outline View and unselect
-Outline->Sort alphabetically
+<i>Outline->Sort alphabetically</i>.
 
 Tip 16: Build modes
 
@@ -123,19 +127,19 @@ If you need GPS to automatically complete the name of an entity, or fill
 a subprogram profile, simply pressing ctrl-space in a source editor will
 display a completion list with possible completion for the current word.
 Automatic completion is also displayed automatically when you type special
-characters such as '.' or '('
+characters such as '<b>.</b>' or '<b>(</b>'.
 
 Tip 19: Dynamic smart completion
 
 You'd like GPS to make more suggestions automatically to complete identifiers
 while you're typing without having to press control-space? Go to
-Edit->Preferences, Editor and then set the 'Smart completion' preference to
-'Dynamic'. You might also want to tune the default timeout.
+Edit->Preferences, Editor and then set the <i>Smart completion</i> preference to
+<i>Dynamic</i>. You might also want to tune the default timeout.
 
 Tip 20: Highlight dispatching calls
 
 Did you know that GPS can highlight all dispatching calls in your source?
-To do so, go to Tools->Plug-ins and enable the 'dispatching.py' plug-in.
+To do so, go to Tools->Plug-ins and enable the <i>dispatching.py</i> plug-in.
 
 Tip 21: Source navigation through dispatching calls
 
@@ -143,26 +147,81 @@ Did you know that when opening a contextual menu on a dispatching call,
 GPS is able to list all the possible targets for this call? By default, GPS
 will take into account the cross reference information it has already
 computed in memory, and you can get more accurate and complete results
-by going to Edit->Preferences and change the
-Editor->Submenu for dispatching calls preference to 'Accurate'.
+by going to Edit->Preferences, select the Editor page and set the
+<i>Submenu for dispatching calls</i> preference to <i>Accurate</i>.
 
 Tip 22: Ada constructs menu
 
 Need to have GPS insert automatically text of Ada constructs? Go to
-Tools->Plug-ins and enable the predef_ada_entity_insertions.py plug-in which
-will add contextual menus in the source editor.
+Tools->Plug-ins and enable the <i>predef_ada_entity_insertions.py</i> plug-in
+which will add contextual menus in the source editor.
 
 Tip 23: On the fly auto casing 
 
 To enable on the fly automatic casing of your identifiers, go to
-Edit->Preferences, then select Editor->Ada and set 'Casing policy' to
-'On_The_Fly', or if you find that too intrusive, 'End_Of_Line'.
+Edit->Preferences, then select Editor->Ada and set <i>Casing policy</i> to
+<i>On_The_Fly</i> or if you find that too intrusive, <i>End_Of_Line</i>.
 
 Tip 24: Disable temporarily auto casing
 
-To temporarily disable auto casing, you can use the alt-q key.
+To temporarily disable auto casing, you can use the <i>alt-q</i> key.
 This can be particularly useful when you've set the casing policy to
-'On The Fly' (see previous tip). You can re-enable it using the same key.
+<i>On The Fly</i> (see previous tip). You can re-enable it using the same key.
+
+Tip 25: How to gain space on your workspace
+
+In order to gain room in your workspace, you can go to Edit->Preferences,
+then Windows and disable the <i>Show title bars</i> option: this will remove
+the title bars on each window.
+
+Tip 26: How to hide the toolbar
+
+Did you know that the GPS tool bar (below the menu bar) can be hidden?
+To do so, go to Edit->Preferences, select the General page and set
+<i>Tool bar style</i> to <i>Hide_Toolbar</i>.
+
+Tip 27: Automatic code fixing
+
+In many cases, the warning and error messages generated by GNAT are so
+precise, that it is possible for GPS to automatically fix the code for you.
+
+When this is possible, GPS will add a small <i>wrench</i> icon on the left
+of the message in the Locations view which you can click on.
+
+If multiple fixes are possible, GPS will display a menu with the possible
+choices.
+
+Tip 28: Splash screen
+
+Tired of getting the splash screen at start up?
+Go to Edit->Preferences, select the General page and disable the
+<i>Display splash screen</i> preference.
+
+Tip 29: Welcome dialog
+
+When you start GPS in a directory which contains a single project file,
+GPS will automatically use this project. Otherwise, GPS will open the
+<i>Welcome Dialog</i> where you can select which project to load, or work
+with the default project.
+
+If you prefer to always start with the default project instead of getting
+the <i>Welcome Dialog</i>, go to Edit->Preferences, select the General
+page and disable the <i>Display welcome window</i> preference.
+
+Tip 30: Multiple key shortcut
+
+Did you know that you can associate a multiple key shortcut to any action in
+GPS? To do so, go to Edit->Key shortcuts, select the action or menu you'd
+like to associate or change a key binding, click <i>Grab</i> and then
+type the two keys (e.g. <i>control-x a</i>) for your key binding.
+
+Tip 31: Add subprogram box
+
+Ever wanted to ask GPS to automatically add a box containing the current
+subprogram name just before this subprogram?
+Go to Edit->Key shortcuts, select Editor->Subprogram Box then glick <i>Grab</i>
+and type a key short cut (e.g. <i>control-b control-b</i>). You can then use
+this key shortcut in the middle of a subprogram.
 
 """
 
