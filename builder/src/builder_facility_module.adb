0d76fce448d8ab66904cf291b9f8d792e851d983
Vadim Godunko <godunko@adacore.com>
2009-08-10 11:53:40 +0000

    * builder_facility_module.adb: (On_Compute_Build_Targets): Add handling of "executable" target type.
    For I326-025.
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@148189 936e1b1b-40f2-da11-902a-00137254ae57

--- builder/src/builder_facility_module.adb
+++ builder/src/builder_facility_module.adb
@@ -931,6 +931,45 @@ package body Builder_Facility_Module is
             Free (Mains);
             return Result;
          end;
+
+      elsif Kind = "executable" then
+         declare
+            Mains  : Argument_List := Get_Mains (Kernel_Handle (Kernel));
+            Prj    : constant Project_Type :=
+              Get_Project (Kernel_Handle (Kernel));
+
+            Result : Any_Type (List_Type, Mains'Length);
+            pragma Warnings (Off, Result);
+
+         begin
+            for J in Mains'Range loop
+               declare
+                  Exec : constant Virtual_File :=
+                    Create_From_Dir
+                      (Executables_Directory (Prj),
+                       Get_Executable_Name (Prj, +Mains (J).all));
+                  Base : constant String :=
+                    String (Exec.Base_Name);
+                  Full : constant String :=
+                    String (Exec.Full_Name.all);
+                  Display_Name : constant Any_Type :=
+                    (String_Type, Base'Length, Base);
+                  Full_Name    : constant Any_Type :=
+                    (String_Type, Full'Length, Full);
+
+               begin
+                  Result.List (1 + J - Mains'First) := new Any_Type'
+                    ((Tuple_Type, 2,
+                     Tuple => (1 => new Any_Type'(Display_Name),
+                               2 => new Any_Type'(Full_Name))));
+               end;
+            end loop;
+
+            Free (Mains);
+
+            return Result;
+         end;
+
       else
          return Empty_Any_Type;
       end if;
