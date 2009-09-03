b9ecea448c3d5f43a10c660b7ef5914f5f719cf4
Nicolas Setton <setton@adacore.com>
2009-09-03 13:57:49 +0000

    (Preferences_Changed): Do not call show_all on the statusbar, since that has  the side effect of showing the task manager progress bar. Simply call Show,  this is enough since there is no other widget in the status bar. Fixes I902-015.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@148954 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/gps-main_window.adb
+++ kernel/src/gps-main_window.adb
@@ -476,7 +476,7 @@ package body GPS.Main_Window is
       end case;
 
       if Pref_Show_Statusbar.Get_Pref then
-         Show_All (Win.Statusbar);
+         Show (Win.Statusbar);
          Set_Child_Visible (Win.Statusbar, True);
          Set_Size_Request (Win.Statusbar, 0, -1);
       else
