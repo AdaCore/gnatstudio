d703e6bfed062a82256f77db03440b365faaec63
Arnaud Charlet <charlet@adacore.com>
2009-10-14 06:25:45 +0000

    Add example on how to set preferences automatically. Part of IA13-037
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@150768 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ examples/python/prefs.py
@@ -0,0 +1,12 @@
+## This is an example showing how to force setting of GPS preferences
+## at start up.
+
+from GPS import *
+
+def on_gps_started (hook):
+  # Set the preferences. You can adjust them at your convenience.
+  Preference ("Ada-Format-Operators").set (True)
+  Preference ("Ada-Ident-Casing").set ("Smart_Mixed")
+  Preference ("Warnings-Src-Highlight-Color").set ("#FFFF6D6D6D6D")
+
+Hook ("gps_started").add (on_gps_started)
