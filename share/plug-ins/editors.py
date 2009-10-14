46c89adfafb66280260d55e1dccc9d85715b1d96
Emmanuel Briot <briot@adacore.com>
2009-10-14 09:47:49 +0000

    (/File/Close All Editors Except Current): new menu  Suggested in IA13-016
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@150777 936e1b1b-40f2-da11-902a-00137254ae57

--- share/plug-ins/editors.py
+++ share/plug-ins/editors.py
@@ -11,12 +11,23 @@ and only editors upon activation.
 
 import GPS
 
-def close_editors(menu):
+def close_editors (menu):
    GPS.execute_action ("/File/Save More/All")
    for ed in GPS.EditorBuffer.list():
       ed.close (True)
 
+def close_editors_except_current (menu):
+   buffer = GPS.EditorBuffer.get (open=False)
+   GPS.execute_action ("/File/Save More/All")
+   for ed in GPS.EditorBuffer.list():
+      if ed != buffer:
+         ed.close (True)
+
 GPS.Menu.create ("/File/Close All Editors",
                  on_activate=close_editors,
                  ref="Close All",
+                 add_before=False);
+GPS.Menu.create ("/File/Close All Editors Except Current",
+                 on_activate=close_editors_except_current,
+                 ref="Close All Editors",
                  add_before=False);
