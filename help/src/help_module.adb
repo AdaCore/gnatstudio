294ec7d1e3f606f5a9985971598e2d17152130dd
Arnaud Charlet <charlet@adacore.com>
2010-01-06 09:33:20 +0000

    Bump copyright notice
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/branches/global/gps-4.4/gps@154745 936e1b1b-40f2-da11-902a-00137254ae57

--- help/src/help_module.adb
+++ help/src/help_module.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                 Copyright (C) 2001-2009, AdaCore                  --
+--                 Copyright (C) 2001-2010, AdaCore                  --
 --                                                                   --
 -- GPS is free  software; you can  redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -947,7 +947,7 @@ package body Help_Module is
            (-") hosted on ") & Config.Target & LF &
          (-"GNAT ") & GNAT_Version (Kernel) & LF & LF &
          (-"the GNAT Programming Studio") & LF & Contents.all & LF &
-         "(c) 2001-2009 AdaCore",
+         "(c) 2001-2010 AdaCore",
          Buttons => Button_OK,
          Title   => -"About...",
          Parent  => Get_Current_Window (Kernel));
