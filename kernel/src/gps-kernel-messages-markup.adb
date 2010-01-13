dd7c25445bee4eebf97950c6bbf4322a5df661f4
Vadim Godunko <godunko@adacore.com>
2010-01-13 11:29:40 +0000

    * gps-kernel-messages-markup.ads: New file.
    * gps-kernel-messages-markup.adb: New file.
    
    Add new kind of messages, this kind supports use of Pango markup in the
    message's text.
    
    Part of I324-006.
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@155148 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ kernel/src/gps-kernel-messages-markup.adb
@@ -0,0 +1,187 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                    Copyright (C) 2010, AdaCore                    --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+package body GPS.Kernel.Messages.Markup is
+
+   use Ada.Strings.Unbounded;
+   use XML_Utils;
+
+   procedure Save
+     (Message_Node : not null Message_Access;
+      XML_Node     : not null Node_Ptr);
+
+   function Load
+     (XML_Node      : not null Node_Ptr;
+      Container     : not null Messages_Container_Access;
+      Category      : String;
+      File          : GNATCOLL.VFS.Virtual_File;
+      Line          : Natural;
+      Column        : Basic_Types.Visible_Column_Type;
+      Actual_Line   : Integer;
+      Actual_Column : Integer)
+      return not null Message_Access;
+
+   function Create_Markup_Message
+     (Container     : not null Messages_Container_Access;
+      Category      : String;
+      File          : GNATCOLL.VFS.Virtual_File;
+      Line          : Natural;
+      Column        : Basic_Types.Visible_Column_Type;
+      Text          : String;
+      Actual_Line   : Integer;
+      Actual_Column : Integer)
+      return not null Markup_Message_Access;
+
+   ---------------------------
+   -- Create_Markup_Message --
+   ---------------------------
+
+   function Create_Markup_Message
+     (Container : not null Messages_Container_Access;
+      Category  : String;
+      File      : GNATCOLL.VFS.Virtual_File;
+      Line      : Natural;
+      Column    : Basic_Types.Visible_Column_Type;
+      Text      : String)
+      return not null Markup_Message_Access is
+   begin
+      return
+        Create_Markup_Message
+          (Container,
+           Category,
+           File,
+           Line,
+           Column,
+           Text,
+           Line,
+           Integer (Column));
+   end Create_Markup_Message;
+
+   ---------------------------
+   -- Create_Markup_Message --
+   ---------------------------
+
+   function Create_Markup_Message
+     (Container     : not null Messages_Container_Access;
+      Category      : String;
+      File          : GNATCOLL.VFS.Virtual_File;
+      Line          : Natural;
+      Column        : Basic_Types.Visible_Column_Type;
+      Text          : String;
+      Actual_Line   : Integer;
+      Actual_Column : Integer)
+      return not null Markup_Message_Access
+   is
+      Result : constant not null Markup_Message_Access := new Markup_Message;
+
+   begin
+      Initialize
+        (Result,
+         Container,
+         Category,
+         File,
+         Line,
+         Column,
+         Actual_Line,
+         Actual_Column);
+      Result.Text := To_Unbounded_String (Text);
+
+      return Result;
+   end Create_Markup_Message;
+
+   ----------------
+   -- Get_Markup --
+   ----------------
+
+   overriding function Get_Markup
+     (Self : not null access constant Markup_Message)
+      return Ada.Strings.Unbounded.Unbounded_String
+   is
+   begin
+      return Self.Text;
+   end Get_Markup;
+
+   --------------
+   -- Get_Text --
+   --------------
+
+   overriding function Get_Text
+     (Self : not null access constant Markup_Message)
+      return Ada.Strings.Unbounded.Unbounded_String is
+   begin
+      return Self.Text;
+   end Get_Text;
+
+   ----------
+   -- Load --
+   ----------
+
+   function Load
+     (XML_Node      : not null Node_Ptr;
+      Container     : not null Messages_Container_Access;
+      Category      : String;
+      File          : GNATCOLL.VFS.Virtual_File;
+      Line          : Natural;
+      Column        : Basic_Types.Visible_Column_Type;
+      Actual_Line   : Integer;
+      Actual_Column : Integer)
+      return not null Message_Access
+   is
+      Text : constant String := Get_Attribute (XML_Node, "text", "");
+
+   begin
+      return
+        Message_Access
+          (Create_Markup_Message
+               (Container,
+                Category,
+                File,
+                Line,
+                Column,
+                Text,
+                Actual_Line,
+                Actual_Column));
+   end Load;
+
+   --------------
+   -- Register --
+   --------------
+
+   procedure Register (Container : not null access Messages_Container'Class) is
+   begin
+      Container.Register_Message_Class
+        (Markup_Message'Tag, Save'Access, Load'Access, null);
+   end Register;
+
+   ----------
+   -- Save --
+   ----------
+
+   procedure Save
+     (Message_Node : not null Message_Access;
+      XML_Node     : not null Node_Ptr)
+   is
+      Self : constant Markup_Message_Access :=
+               Markup_Message_Access (Message_Node);
+
+   begin
+      Set_Attribute (XML_Node, "text", To_String (Self.Text));
+   end Save;
+
+end GPS.Kernel.Messages.Markup;
---
+++ kernel/src/gps-kernel-messages-markup.ads
@@ -0,0 +1,56 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                    Copyright (C) 2010, AdaCore                    --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+--  This package provides implementation of Abstract_Message which is
+--  suitable for use when message text includes Pango markup. Support
+--  for secondary messages are not provided.
+
+package GPS.Kernel.Messages.Markup is
+
+   type Markup_Message is new Abstract_Message (Primary) with private;
+
+   type Markup_Message_Access is access all Markup_Message'Class;
+
+   function Create_Markup_Message
+     (Container : not null Messages_Container_Access;
+      Category  : String;
+      File      : GNATCOLL.VFS.Virtual_File;
+      Line      : Natural;
+      Column    : Basic_Types.Visible_Column_Type;
+      Text      : String)
+      return not null Markup_Message_Access;
+   --  Creates new instance of Markup_Message.
+
+   procedure Register (Container : not null access Messages_Container'Class);
+   --  Registers load/save procedures for the markup message
+
+private
+
+   type Markup_Message is new Abstract_Message (Primary) with record
+      Text : Ada.Strings.Unbounded.Unbounded_String;
+   end record;
+
+   overriding function Get_Text
+     (Self : not null access constant Markup_Message)
+      return Ada.Strings.Unbounded.Unbounded_String;
+
+   overriding function Get_Markup
+     (Self : not null access constant Markup_Message)
+      return Ada.Strings.Unbounded.Unbounded_String;
+
+end GPS.Kernel.Messages.Markup;
