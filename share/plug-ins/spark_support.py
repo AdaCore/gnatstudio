342348c392c8f6ac7754777342ae2bb14363f232
Arnaud Charlet <charlet@adacore.com>
2010-02-23 19:42:05 +0000

    Fix typo.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@156981 936e1b1b-40f2-da11-902a-00137254ae57

--- share/plug-ins/spark_support.py
+++ share/plug-ins/spark_support.py
@@ -8,7 +8,7 @@ import GPS, os_utils
 spark_exe = os_utils.locate_exec_on_path ("spark")
 if spark_exe != "":
   spark_plugins = os.path.dirname(spark_exe)+"/../share/gps/plug-ins"
-  if os.path.isfile(spark_plugins+'/spark.py')
+  if os.path.isfile(spark_plugins+"/spark.py"):
     sys.path=[spark_plugins]+sys.path
     import spark
   else:
