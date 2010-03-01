6bd9b07458491099dd420e8bcb34a327b4557225
Nicolas Setton <setton@adacore.com>
2010-03-01 11:37:40 +0000

    Add incomplete body now, to help Quentin work on J301-006.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@157165 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ builder/src/extending_environments.adb
@@ -0,0 +1,117 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                     Copyright (C) 2010, AdaCore                   --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Projects;          use Projects;
+with Projects.Registry; use Projects.Registry;
+
+with GPS.Kernel.Projects; use GPS.Kernel.Projects;
+
+package body Extending_Environments is
+
+   --------------
+   -- Get_File --
+   --------------
+
+   function Get_File (Env : Extending_Environment) return Virtual_File is
+   begin
+      return Env.File;
+   end Get_File;
+
+   -----------------
+   -- Get_Project --
+   -----------------
+
+   function Get_Project (Env : Extending_Environment) return Virtual_File is
+   begin
+      return Env.Project_File;
+   end Get_Project;
+
+   ----------------------------------
+   -- Create_Extending_Environment --
+   ----------------------------------
+
+   function Create_Extending_Environment
+     (Kernel : Kernel_Handle;
+      Source : Virtual_File;
+      Server : Server_Type) return Extending_Environment
+   is
+      Env : Extending_Environment;
+      P : Project_Type;
+      F : Virtual_File;
+      W : Writable_File;
+
+   begin
+      --  Create the temporary directory
+      Env.Temporary_Dir := Create_From_Dir
+        (Get_Tmp_Directory (Get_Nickname (Server)),
+         Base_Name => Source.Base_Name);
+
+      if not Is_Directory (Env.Temporary_Dir) then
+         Make_Dir (Env.Temporary_Dir);
+      end if;
+
+      --  Create the project file
+      P := Get_Project_From_File (Get_Registry (Kernel).all, Source);
+
+      F := Create_From_Dir
+        (Env.Temporary_Dir, "extends_" & Base_Name (Project_Path (P)));
+
+      W := Write_File (F);
+      Write (W, "project Extends_" & Project_Name (P)
+             & " extends """
+             & (+Project_Path (P).Full_Name.all) & """ is"
+             & ASCII.LF);
+
+      --  If this is a library project, add a "Library_Dir" attribute
+      if Get_Attribute_Value
+        (P, Library_Name_Attribute) /= ""
+      then
+         declare
+            Lib_Directory : Virtual_File;
+         begin
+            Lib_Directory :=
+              Create_From_Dir (Get_Background_Dir, "lib");
+            if not Is_Directory (Lib_Directory) then
+               Make_Dir (Lib_Directory);
+            end if;
+            Write (W, "   for Library_Dir use ""lib"";" & ASCII.LF);
+         end;
+      end if;
+
+      Write (W, "end Extends_" & Project_Name (P) & ";");
+      Close (W);
+
+      --  Create the file
+
+      return Env;
+
+   end Create_Extending_Environment;
+
+   -------------
+   -- Destroy --
+   -------------
+
+   procedure Destroy (Env : Extending_Environment) is
+      Dummy : Boolean;
+      pragma Unreferenced (Dummy);
+   begin
+      Remove_Dir (Env.Temporary_Dir, Recursive => True, Success => Dummy);
+   end Destroy;
+
+end Extending_Environments;
