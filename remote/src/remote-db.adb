06814fe8a2ea7dda2ef1d97f3fec0a273641fc95
Jerome Lambourg <lambourg@adacore.com>
2010-03-25 13:23:14 +0000

    * gnatcoll-io-remote.adb: Handle new FS_Unix_Case_Insensitive FS kind.
    * gnatcoll-path.adb:
    (Internal_Local_FS): Return FS_Unix_Case_Insensitive if the filesystem
     is case insensitive.
    (*): Handle FS_Unix_Case_Insensitive
    
    * gnatcoll-vfs_types.ads:
    Add new FS type FS_Unix_Case_Insensitive to handle OSX filesystem.
    
    * remote-db.adb:
    (Parse_Shell_Node): Handle FS_Unix_Case_Insensitive.
    
    Add support for case insensitive unix filesystems, as can be found with osx.
    For J325-015
    
    
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@158088 936e1b1b-40f2-da11-902a-00137254ae57

--- remote/src/remote-db.adb
+++ remote/src/remote-db.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                     Copyright (C) 2009, AdaCore                   --
+--                 Copyright (C) 2009-2010, AdaCore                  --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -442,6 +442,8 @@ package body Remote.Db is
          FS := FS_Windows;
       elsif Tmp.all = "unix" then
          FS := FS_Unix;
+      elsif Tmp.all = "unix-case-insensitive" then
+         FS := FS_Unix_Case_Insensitive;
       else
          Console.Insert
            (Kernel, "XML Error for shell " & Shell_Name &
