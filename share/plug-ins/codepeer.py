5d76da233fd0dc1c74a79ffc7704c50f141f0095
Yannick Moy <moy@adacore.com>
2010-05-17 07:10:01 +0000

    Part of J409-026. Add example in GPS ones.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@159987 936e1b1b-40f2-da11-902a-00137254ae57

--- share/plug-ins/codepeer.py
+++ share/plug-ins/codepeer.py
@@ -134,6 +134,23 @@ if codepeer != "":
       </menu>
     </submenu>
 
+    <action name="codepeer_example_conductor" category=""
+            show-command="false" output="none">
+      <shell>Project.load """ + '"' + example_root + \
+             """/conductor/conductor.gpr"</shell>
+      <shell>Editor.edit "score.ads"</shell>
+      <shell>Editor.edit "conductor.ads"</shell>
+      <shell>Editor.edit "conductor.adb"</shell>
+      <shell>Editor.edit "README.txt"</shell>
+    </action>
+
+    <submenu before="About">
+      <title>/Help/CodePeer/Examples</title>
+      <menu action="codepeer_example_conductor">
+        <title>Conductor</title>
+      </menu>
+    </submenu>
+
     <target-model name="generate_scil" category="">
        <description>Generate SCIL files for CodePeer</description>
        <command-line>
