fffeecdee47c71064919509ca623b7cd5ed24dfd
Vadim Godunko <godunko@adacore.com>
2010-06-23 10:35:03 +0000

    Add menu item to load stack usage information.
    Part of J219-002.
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@161507 936e1b1b-40f2-da11-902a-00137254ae57

--- code_analysis/src/gnatstack-module.adb
+++ code_analysis/src/gnatstack-module.adb
@@ -16,11 +16,16 @@
 -- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
 -- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
+with Input_Sources.File;
 with Glib.Object;
 with GPS.Intl;
+with GPS.Kernel.Console;
+with GPS.Kernel.Project;
+with GNATCOLL.Projects;
 with GNATCOLL.VFS;
 with Traces;
 
+with GNATStack.Readers;
 with GNATStack.Shell_Commands;
 
 package body GNATStack.Module is
@@ -35,8 +40,12 @@ package body GNATStack.Module is
       Kernel : GPS.Kernel.Kernel_Handle);
    --  Called when "Analyze stack usage" menu item is activated
 
+   procedure On_Load_Data
+     (Widget : access Glib.Object.GObject_Record'Class;
+      Kernel : GPS.Kernel.Kernel_Handle);
+   --  Called when "Load data" menu item is activated
+
    Module : GNATStack_Module_Id;
-   pragma Unreferenced (Module);
 
    ----------------------------
    -- On_Analyze_Stack_Usage --
@@ -60,6 +69,49 @@ package body GNATStack.Module is
          Trace (Exception_Handle, E);
    end On_Analyze_Stack_Usage;
 
+   ------------------
+   -- On_Load_Data --
+   ------------------
+
+   procedure On_Load_Data
+     (Widget : access Glib.Object.GObject_Record'Class;
+      Kernel : GPS.Kernel.Kernel_Handle)
+   is
+      pragma Unreferenced (Widget);
+
+      Name   : constant GNATCOLL.VFS.Virtual_File :=
+                 GNATCOLL.VFS.Create_From_Dir
+                   (GNATCOLL.Projects.Object_Dir
+                      (GPS.Kernel.Project.Get_Project (Kernel)),
+                    "stack_usage.xml");
+      File   : Input_Sources.File.File_Input;
+      Reader : GNATStack.Readers.Reader;
+
+   begin
+      if Name.Is_Regular_File then
+         Input_Sources.File.Open (String (Name.Full_Name.all), File);
+         Reader.Parse (File);
+         Input_Sources.File.Close (File);
+         Module.Loaded := True;
+
+      else
+         GPS.Kernel.Console.Insert
+           (Kernel,
+            "stack_usage.xml not found. Run GNATStack first.",
+            True,
+            GPS.Kernel.Console.Error);
+      end if;
+
+   exception
+      when E : others =>
+         GPS.Kernel.Console.Insert
+           (Kernel,
+            "Unable to load stack usage information.",
+            True,
+            GPS.Kernel.Console.Error);
+         Trace (Exception_Handle, E);
+   end On_Load_Data;
+
    ---------------------
    -- Register_Module --
    ---------------------
@@ -81,12 +133,19 @@ package body GNATStack.Module is
       end if;
 
       Module := new GNATStack_Module_Id_Record (Kernel);
+
+      Module.Register_Module (Kernel, "GNATStack");
       GPS.Kernel.Modules.Register_Menu
         (Kernel      => Kernel,
          Parent_Path => -"/Tools/GNATStack",
          Text        => -"_Analyze stack usage",
          Ref_Item    => -"Macro",
          Callback    => On_Analyze_Stack_Usage'Access);
+      GPS.Kernel.Modules.Register_Menu
+        (Kernel      => Kernel,
+         Parent_Path => -"/Tools/GNATStack",
+         Text        => -"_Load data",
+         Callback    => On_Load_Data'Access);
    end Register_Module;
 
 end GNATStack.Module;
