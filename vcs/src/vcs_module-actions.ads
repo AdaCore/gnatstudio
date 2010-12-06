62e39a792fb8d35b8037cb464852fcd8ff513705
Nicolas Setton <setton@adacore.com>
2010-12-06 17:26:31 +0000

    Initial revision.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@167999 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ vcs/src/vcs_module-actions.ads
@@ -0,0 +1,28 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                 Copyright (C) 2001-2010, AdaCore                  --
+--                                                                   --
+-- GPS is free  software; you can  redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+--  This package defines and registers VCS actions
+
+package VCS_Module.Actions is
+
+   procedure Register_Actions
+     (Kernel : access Kernel_Handle_Record'Class);
+   --  Register the VCS actions.
+
+end VCS_Module.Actions;
