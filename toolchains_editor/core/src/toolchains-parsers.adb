e8cebeef7431d1634fc55612e86f55357d0060c6
Quentin Ochem <ochem@adacore.com>
2011-01-04 17:57:09 +0000

    (Adjust_Target_Variable): Fixed generation of the targets list, using names,  not labels.
    Fixes JB18-002.
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@168753 936e1b1b-40f2-da11-902a-00137254ae57

--- toolchains_editor/core/src/toolchains-parsers.adb
+++ toolchains_editor/core/src/toolchains-parsers.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                    Copyright (C) 2010, AdaCore                    --
+--                  Copyright (C) 2010-2011, AdaCore                 --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -842,7 +842,7 @@ package body Toolchains.Parsers is
                end if;
 
                Toolchain_Node := Create_Literal_String
-                 (Get_Name_Id (Get_Label (Toolchains (J))),
+                 (Get_Name_Id (Get_Name (Toolchains (J))),
                   This.Node_Data);
 
                if Prev_Node = Empty_Node then
