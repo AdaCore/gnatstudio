de63e9069a62a5758ddd35ed57313dcda0faa842
Pascal Obry <obry@adacore.com>
2011-01-05 13:47:23 +0000

    Minor change to support more BSD based platforms.
    Contributed by Thomas Løcke.
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@168793 936e1b1b-40f2-da11-902a-00137254ae57

--- common/tty/terminals.c
+++ common/tty/terminals.c
@@ -31,7 +31,10 @@
 #ifndef WIN32
 
 /* First defined some macro to identify easily some systems */
-#if defined (__FreeBSD__) || defined (__NetBSD__)
+#if defined (__FreeBSD__) \
+ || defined (__OpenBSD__) \
+ || defined (__NetBSD__)  \
+ || defined (__DragonFly__)
 #   define FREEBSD
 #endif
 #if defined (__alpha__) && defined (__osf__)
