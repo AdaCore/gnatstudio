7755f5b7db8ca244a18de38129aa15ffb05e4b4d
Jerome Lambourg <lambourg@adacore.com>
2011-02-09 16:58:06 +0000

    (Set_Graphical_Widget): Add an exception handler to make sure that GPS won't  crash upon invalid parameter conversion. Part of K208-017
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@170265 936e1b1b-40f2-da11-902a-00137254ae57

--- widgets/src/switches_chooser-gtkada.adb
+++ widgets/src/switches_chooser-gtkada.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                 Copyright (C) 2007-2010, AdaCore                  --
+--                 Copyright (C) 2007-2011, AdaCore                  --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -347,6 +347,10 @@ package body Switches_Chooser.Gtkada is
          when Switch_Popup =>
             null;
       end case;
+
+   exception
+      when others =>
+         null;
    end Set_Graphical_Widget;
 
    ----------------------
