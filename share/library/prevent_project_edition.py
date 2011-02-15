af3bb899baf117182e8806a3c061343bcb15c550
Nicolas Setton <setton@adacore.com>
2011-02-15 15:48:01 +0000

    Initial revision. For K209-048.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@170418 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ share/library/prevent_project_edition.py
@@ -0,0 +1,28 @@
+"""This plug-in removes the menus that allow graphical edition
+of project files.
+
+This is useful when working with project files that have been
+formatted manually, and graphical edition should be avoided,
+so as to preserve the formatting.
+"""
+
+
+#############################################################################
+## No user customization below this line
+#############################################################################
+
+from GPS import *
+
+def on_gps_started(hook):
+    # Remove the contextual menus that may cause modifications in the
+    # projects
+    GPS.Contextual("Edit project properties").hide()
+    GPS.Contextual("Project dependencies").hide()
+    GPS.Contextual("Add configuration variable").hide()
+    GPS.Contextual("Save project").hide()
+
+    # Remove the global menus
+    GPS.Menu.get("/Project/Edit project properties").hide()
+    GPS.Menu.get("/Project/Edit file switches").hide()
+
+GPS.Hook ("gps_started").add (on_gps_started)
