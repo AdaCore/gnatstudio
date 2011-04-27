f12ed6f2099e28a6c3f0024cf9c6c860e6f79e56
Nicolas Setton <setton@adacore.com>
2011-04-27 15:55:11 +0000

    (Initialize): Call Set_Args with Mode set to Internal, so that Set_Args waits  for prompt. This is needed so that further initialization commands may execute  properly. Also fixes K427-016.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/branches/global/ide-2010/gps@172827 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/gvd/debugger-gdb.adb
+++ gvd/gvd/debugger-gdb.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                 Copyright (C) 2000-2010, AdaCore                  --
+--                 Copyright (C) 2000-2011, AdaCore                  --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -1025,7 +1025,7 @@ package body Debugger.Gdb is
       end if;
 
       if Debugger.Executable_Args /= null then
-         Set_Args (Debugger, Debugger.Executable_Args.all, Mode => Visible);
+         Set_Args (Debugger, Debugger.Executable_Args.all, Mode => Internal);
       end if;
 
       if Debugger.Executable = GNATCOLL.VFS.No_File then
