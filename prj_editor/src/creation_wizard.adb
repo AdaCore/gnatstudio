ebe866c92252034adcdc7365ae430c102a7af774
Nicolas Setton <setton@adacore.com>
2011-05-18 09:56:27 +0000

    Call Present to raise the Creation Wizard to the front and give it the keyboard focus. Part of K517-001.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@173827 936e1b1b-40f2-da11-902a-00137254ae57

--- prj_editor/src/creation_wizard.adb
+++ prj_editor/src/creation_wizard.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                    Copyright (C) 2004-2010, AdaCore               --
+--                    Copyright (C) 2004-2011, AdaCore               --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -431,6 +431,7 @@ package body Creation_Wizard is
       Name : GNATCOLL.VFS.Virtual_File;
    begin
       Show_All (Wiz);
+      Present (Wiz);
 
       if Run (Wiz) = Gtk_Response_Apply then
          if Wiz.Project = No_Project then
