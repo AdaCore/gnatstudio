39a28fa5e2c4efbd71f3dd0e76cdd038ab0abdf1
Vadim Godunko <godunko@adacore.com>
2011-09-20 20:09:39 +0000

    Follow up for previous commit.
    For J106-016.
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@178955 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ code_analysis/src/code_peer-race_details_models.adb
@@ -0,0 +1,317 @@
+-----------------------------------------------------------------------
+--                               G P S                               --
+--                                                                   --
+--                     Copyright (C) 2011, AdaCore                   --
+--                                                                   --
+-- GPS is Free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Ada.Unchecked_Conversion;
+with System;
+
+with Gtk.Tree_Model.Utils;
+
+package body Code_Peer.Race_Details_Models is
+
+   use type Gtk.Tree_Model.Gtk_Tree_Iter;
+
+   function To_Iter (Index : Natural) return Gtk.Tree_Model.Gtk_Tree_Iter;
+
+   function From_Iter (Iter : Gtk.Tree_Model.Gtk_Tree_Iter) return Natural;
+
+   ---------------
+   -- From_Iter --
+   ---------------
+
+   function From_Iter (Iter : Gtk.Tree_Model.Gtk_Tree_Iter) return Natural is
+      pragma Warnings (Off);
+      function To_Integer is
+        new Ada.Unchecked_Conversion (System.Address, Integer);
+      pragma Warnings (On);
+
+   begin
+      if Iter = Gtk.Tree_Model.Null_Iter then
+         return 0;
+
+      else
+         return To_Integer (Gtk.Tree_Model.Utils.Get_User_Data_1 (Iter));
+      end if;
+   end From_Iter;
+
+   ---------------------
+   -- Get_Column_Type --
+   ---------------------
+
+   overriding function Get_Column_Type
+     (Self  : access Race_Details_Model_Record;
+      Index : Glib.Gint) return Glib.GType
+   is
+      pragma Unreferenced (Self);
+
+   begin
+      case Index is
+         when Entry_Point_Name_Column =>
+            return Glib.GType_String;
+
+         when Access_Kind_Column =>
+            return Glib.GType_String;
+
+         when others =>
+            return Glib.GType_Invalid;
+      end case;
+   end Get_Column_Type;
+
+   --------------
+   -- Get_Iter --
+   --------------
+
+   overriding function Get_Iter
+     (Self : access Race_Details_Model_Record;
+      Path : Gtk.Tree_Model.Gtk_Tree_Path)
+      return Gtk.Tree_Model.Gtk_Tree_Iter
+   is
+      Indices : constant Glib.Gint_Array := Gtk.Tree_Model.Get_Indices (Path);
+      Index   : Natural;
+
+   begin
+      if Indices'Length = 1 then
+         Index := Natural (Indices (Indices'First)) + 1;
+
+         if Index in Self.Data.First_Index .. Self.Data.Last_Index then
+            return To_Iter (Index);
+         end if;
+      end if;
+
+      return Gtk.Tree_Model.Null_Iter;
+   end Get_Iter;
+
+   -------------------
+   -- Get_N_Columns --
+   -------------------
+
+   overriding function Get_N_Columns
+     (Self : access Race_Details_Model_Record) return Glib.Gint
+   is
+      pragma Unreferenced (Self);
+
+   begin
+      return Number_Of_Columns;
+   end Get_N_Columns;
+
+   --------------
+   -- Get_Path --
+   --------------
+
+   overriding function Get_Path
+     (Self : access Race_Details_Model_Record;
+      Iter : Gtk.Tree_Model.Gtk_Tree_Iter)
+      return Gtk.Tree_Model.Gtk_Tree_Path
+   is
+      Index : constant Natural := From_Iter (Iter);
+      Path  : Gtk.Tree_Model.Gtk_Tree_Path;
+
+   begin
+      if Iter /= Gtk.Tree_Model.Null_Iter
+        and then Index in Self.Data.First_Index .. Self.Data.Last_Index
+      then
+         Path := Gtk.Tree_Model.Gtk_New;
+         Gtk.Tree_Model.Append_Index (Path, Glib.Gint (Index - 1));
+
+      else
+         Path := null;
+      end if;
+
+      return Path;
+   end Get_Path;
+
+   ---------------
+   -- Get_Value --
+   ---------------
+
+   overriding procedure Get_Value
+     (Self   : access Race_Details_Model_Record;
+      Iter   : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Column : Glib.Gint;
+      Value  : out Glib.Values.GValue)
+   is
+      Index : constant Natural := From_Iter (Iter);
+
+   begin
+      if Iter /= Gtk.Tree_Model.Null_Iter
+        and then Index in Self.Data.First_Index .. Self.Data.Last_Index
+      then
+         case Column is
+            when Entry_Point_Name_Column =>
+               Glib.Values.Init (Value, Glib.GType_String);
+               Glib.Values.Set_String
+                 (Value, Self.Data.Element (Index).Entry_Point.Name.all);
+
+            when Access_Kind_Column =>
+               Glib.Values.Init (Value, Glib.GType_String);
+
+               case Self.Data.Element (Index).Kind is
+                  when Read =>
+                     Glib.Values.Set_String (Value, "READ");
+
+                  when Update =>
+                     Glib.Values.Set_String (Value, "UPDATE");
+               end case;
+
+            when others =>
+               null;
+         end case;
+      end if;
+   end Get_Value;
+
+   -------------
+   -- Gtk_New --
+   -------------
+
+   procedure Gtk_New (Model : out Race_Details_Model) is
+   begin
+      Model := new Race_Details_Model_Record;
+      Initialize (Model);
+   end Gtk_New;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize
+     (Self : not null access Race_Details_Model_Record'Class) is
+   begin
+      Gtkada.Abstract_List_Model.Initialize (Self);
+   end Initialize;
+
+   ----------------
+   -- N_Children --
+   ----------------
+
+   overriding function N_Children
+     (Self : access Race_Details_Model_Record;
+      Iter : Gtk.Tree_Model.Gtk_Tree_Iter)
+      return Glib.Gint is
+   begin
+      if Iter = Gtk.Tree_Model.Null_Iter then
+         return Glib.Gint (Self.Data.Length);
+
+      else
+         return 0;
+      end if;
+   end N_Children;
+
+   ----------
+   -- Next --
+   ----------
+
+   overriding procedure Next
+     (Self : access Race_Details_Model_Record;
+      Iter : in out Gtk.Tree_Model.Gtk_Tree_Iter)
+   is
+      Index : constant Natural := From_Iter (Iter) + 1;
+
+   begin
+      if Iter /= Gtk.Tree_Model.Null_Iter
+        and then Index in Self.Data.First_Index .. Self.Data.Last_Index
+      then
+         Iter := To_Iter (Index);
+
+      else
+         Iter := Gtk.Tree_Model.Null_Iter;
+      end if;
+   end Next;
+
+   ---------------
+   -- Nth_Child --
+   ---------------
+
+   overriding function Nth_Child
+     (Self   : access Race_Details_Model_Record;
+      Parent : Gtk.Tree_Model.Gtk_Tree_Iter;
+      N      : Glib.Gint) return Gtk.Tree_Model.Gtk_Tree_Iter
+   is
+      Index : constant Natural := Natural (N) + 1;
+
+   begin
+      if Parent = Gtk.Tree_Model.Null_Iter
+        and then Index in Self.Data.First_Index .. Self.Data.Last_Index
+      then
+         return To_Iter (Index);
+
+      else
+         return Gtk.Tree_Model.Null_Iter;
+      end if;
+   end Nth_Child;
+
+   ---------
+   -- Set --
+   ---------
+
+   procedure Set
+     (Self : not null access Race_Details_Model_Record'Class;
+      Data : Code_Peer.Entry_Point_Object_Access_Vectors.Vector)
+   is
+      Info : Entry_Point_Object_Access_Information;
+      Path : Gtk.Tree_Model.Gtk_Tree_Path;
+      Iter : Gtk.Tree_Model.Gtk_Tree_Iter;
+
+   begin
+      --  Clear current content of the model and notify view.
+
+      while not Self.Data.Is_Empty loop
+         Iter := To_Iter (Self.Data.Last_Index);
+         Path := Self.Get_Path (Iter);
+         Self.Data.Delete_Last;
+         Self.Row_Deleted (Path);
+         Gtk.Tree_Model.Path_Free (Path);
+      end loop;
+
+      --  Transform data into internal representation
+
+      for J in Data.First_Index .. Data.Last_Index loop
+         Info := Data.Element (J);
+
+         for K in
+           Info.Object_Accesses.First_Index .. Info.Object_Accesses.Last_Index
+         loop
+            Self.Data.Append
+              ((Info.Entry_Point, Info.Object_Accesses.Element (K).Kind));
+            Iter := To_Iter (Self.Data.Last_Index);
+            Path := Self.Get_Path (Iter);
+            Self.Row_Inserted (Path, Iter);
+            Gtk.Tree_Model.Path_Free (Path);
+         end loop;
+      end loop;
+   end Set;
+
+   -------------
+   -- To_Iter --
+   -------------
+
+   function To_Iter (Index : Natural) return Gtk.Tree_Model.Gtk_Tree_Iter is
+      pragma Warnings (Off);
+      function To_Address is
+        new Ada.Unchecked_Conversion (Integer, System.Address);
+      pragma Warnings (On);
+
+   begin
+      if Index = 0 then
+         return Gtk.Tree_Model.Null_Iter;
+
+      else
+         return Gtk.Tree_Model.Utils.Init_Tree_Iter (1, To_Address (Index));
+      end if;
+   end To_Iter;
+
+end Code_Peer.Race_Details_Models;
