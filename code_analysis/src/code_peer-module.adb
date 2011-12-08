a8cd1d26fa267fde93f4110ac74abc72e9e97546
Vadim Godunko <godunko@adacore.com>
2011-12-08 09:13:24 +0000

    Merge change for KB29-013 into IDE-2011 branch.
    Add correct initialization of 'in speedbar' attribute of styles, used by
    CodePeer plugin.
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/branches/global/ide-2011/gps@182126 936e1b1b-40f2-da11-902a-00137254ae57

--- code_analysis/src/code_peer-module.adb
+++ code_analysis/src/code_peer-module.adb
@@ -2284,7 +2284,8 @@ package body Code_Peer.Module is
       procedure Initialize_Style
         (Style      : out GPS.Styles.UI.Style_Access;
          Name       : String;
-         Preference : Default_Preferences.Color_Preference);
+         Preference : Default_Preferences.Color_Preference;
+         Speedbar   : Boolean);
       --  Initializes style and sets background color from preference.
 
       ----------------------
@@ -2294,12 +2295,14 @@ package body Code_Peer.Module is
       procedure Initialize_Style
         (Style      : out GPS.Styles.UI.Style_Access;
          Name       : String;
-         Preference : Default_Preferences.Color_Preference) is
+         Preference : Default_Preferences.Color_Preference;
+         Speedbar   : Boolean) is
       begin
          Style :=
            GPS.Kernel.Styles.Get_Or_Create_Style
              (GPS.Kernel.Kernel_Handle (Kernel), Name);
          Style.Set_Background (Preference.Get_Pref);
+         Style.Set_In_Speedbar (Speedbar);
       end Initialize_Style;
 
       Submenu_Factory : GPS.Kernel.Modules.UI.Submenu_Factory;
@@ -2494,27 +2497,33 @@ package body Code_Peer.Module is
       Initialize_Style
         (Module.Annotation_Style,
          Annotation_Style_Name,
-         Module.Annotation_Color);
+         Module.Annotation_Color,
+         False);
       Initialize_Style
         (Module.Message_Styles (Code_Peer.High),
          High_Probability_Style_Name,
-         Module.Message_Colors (Code_Peer.High));
+         Module.Message_Colors (Code_Peer.High),
+         True);
       Initialize_Style
         (Module.Message_Styles (Code_Peer.Medium),
          Medium_Probability_Style_Name,
-         Module.Message_Colors (Code_Peer.Medium));
+         Module.Message_Colors (Code_Peer.Medium),
+         True);
       Initialize_Style
         (Module.Message_Styles (Code_Peer.Low),
          Low_Probability_Style_Name,
-         Module.Message_Colors (Code_Peer.Low));
+         Module.Message_Colors (Code_Peer.Low),
+         True);
       Initialize_Style
         (Module.Message_Styles (Code_Peer.Informational),
          Informational_Probability_Style_Name,
-         Module.Message_Colors (Code_Peer.Informational));
+         Module.Message_Colors (Code_Peer.Informational),
+         True);
       Initialize_Style
         (Module.Message_Styles (Code_Peer.Suppressed),
          Suppressed_Probability_Style_Name,
-         Module.Message_Colors (Code_Peer.Suppressed));
+         Module.Message_Colors (Code_Peer.Suppressed),
+         True);
 
       GPS.Kernel.Hooks.Add_Hook
         (Kernel, GPS.Kernel.Compilation_Finished_Hook,
