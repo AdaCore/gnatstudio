c4d6a2193a39c730f373e848e995facb3b426663
Emmanuel Briot <briot@adacore.com>
2011-12-20 09:58:07 +0000

    Continued transition to GPL v3 KC20-002
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@182689 936e1b1b-40f2-da11-902a-00137254ae57

--- gvd/win32/winps.adb
+++ gvd/win32/winps.adb
@@ -1,22 +1,18 @@
 ------------------------------------------------------------------------------
---                         P S - For Windows NT/2000                        --
+--                        P S - For Windows NT/2000                         --
 --                                                                          --
---                          Copyright (C) 2001-2005                         --
---                                 AdaCore                                  --
+--                     Copyright (C) 2001-2012, AdaCore                     --
 --                                                                          --
---  This program is free software; you can redistribute it and/or modify    --
---  it under the terms of the GNU General Public License as published by    --
---  the Free Software Foundation; either version 2 of the License, or (at   --
---  your option) any later version.                                         --
---                                                                          --
---  This library is distributed in the hope that it will be useful, but     --
---  WITHOUT ANY WARRANTY; without even the implied warranty of              --
---  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
---  General Public License for more details.                                --
---                                                                          --
---  You should have received a copy of the GNU General Public License       --
---  along with this library; if not, write to the Free Software Foundation, --
---  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
 ------------------------------------------------------------------------------
 
 with GNAT.IO; use GNAT.IO;
