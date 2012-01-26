1499d58db2a8d063960e4c6ae5ddf0dbfe3654ec
Pascal Obry <obry@adacore.com>
2012-01-26 19:15:29 +0000

    Code clean-up detected by Codepeer.
    Remove unneeded initializations and a test always true.
    
    For L126-010.
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@184741 936e1b1b-40f2-da11-902a-00137254ae57

--- templates_parser/src/templates_parser.adb
+++ templates_parser/src/templates_parser.adb
@@ -3577,7 +3577,8 @@ package body Templates_Parser is
             --  Then check for named ones
 
             declare
-               Sep, SP, EP : Natural := Parameters'First;
+               SP, EP : Natural;
+               Sep    : Natural := Parameters'First;
             begin
                loop
                   Sep := Strings.Fixed.Index (Parameters, "=>", From => Sep);
@@ -3605,7 +3606,7 @@ package body Templates_Parser is
                      SP := SP - 1;
                   end loop;
 
-                  if EP >= SP and then Parameters (EP) in '0' .. '9' then
+                  if Parameters (EP) in '0' .. '9' then
                      Count := Natural'Max
                        (Count, Natural'Value (Parameters (SP .. EP)));
                   end if;
