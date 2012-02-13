AdaCore/gnatstudio

commit f6631c86f7e9f4334304fbdb615d1e59bbee4bc7

(Diff): Simplify code. Spotted by CodePeer. Part of L119-014
git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@184954 936e1b1b-40f2-da11-902a-00137254ae57

vcs/src/vcs-generic_vcs.adb
@@ -1042,25 +1042,18 @@ package body VCS.Generic_VCS is
       Args : GNAT.Strings.String_List_Access;
 
    begin
-      if Version_1 = ""
-        and then Version_2 = ""
-      then
+      if Version_1 = "" and then Version_2 = "" then
          Generic_Command (Rep, File, null, Diff_Head);
+      elsif Version_1 /= "" then
+         if Version_2 = "" then
+            Args := new GNAT.Strings.String_List (1 .. 1);
+            Args (1) := new String'(Version_1);
+         else
+            Args := new GNAT.Strings.String_List (1 .. 2);
+            Args (1) := new String'(Version_1);
+            Args (2) := new String'(Version_2);
+         end if;
 
-      elsif Version_1 /= ""
-        and then Version_2 = ""
-      then
-         Args := new GNAT.Strings.String_List (1 .. 1);
-         Args (1) := new String'(Version_1);
-         Generic_Command (Rep, File, Args, Diff);
-         GNAT.Strings.Free (Args);
-
-      elsif Version_1 /= ""
-        and then Version_2 /= ""
-      then
-         Args := new GNAT.Strings.String_List (1 .. 2);
-         Args (1) := new String'(Version_1);
-         Args (2) := new String'(Version_2);
          Generic_Command (Rep, File, Args, Diff2);
          GNAT.Strings.Free (Args);
       end if;

commit b71f703f4ac03796989e53d90af2d37f83a4f670

(Diff): Fix regression introduced in revision r184954.
The code refactoring done for the CodePeer report was not correct.

Continued work for L119-014.
Fixes L213-026.

git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@185695 936e1b1b-40f2-da11-902a-00137254ae57

vcs/src/vcs-generic_vcs.adb
@@ -1049,13 +1049,14 @@ package body VCS.Generic_VCS is
          if Version_2 = "" then
             Args := new GNAT.Strings.String_List (1 .. 1);
             Args (1) := new String'(Version_1);
+            Generic_Command (Rep, File, Args, Diff);
          else
             Args := new GNAT.Strings.String_List (1 .. 2);
             Args (1) := new String'(Version_1);
             Args (2) := new String'(Version_2);
+            Generic_Command (Rep, File, Args, Diff2);
          end if;
 
-         Generic_Command (Rep, File, Args, Diff2);
          GNAT.Strings.Free (Args);
       end if;
    end Diff;