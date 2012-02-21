1ccb718dbf99dd37882de9161b8075730111615e
Nicolas Setton <setton@adacore.com>
2012-02-21 15:34:50 +0000

    (Parse_XML): Do not attempt to expand the first iter if it doesn't exist.  Fixes a potential access to unallocated memory. Fixes L221-023.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@186042 936e1b1b-40f2-da11-902a-00137254ae57

--- custom/src/xml_viewer.adb
+++ custom/src/xml_viewer.adb
@@ -497,6 +497,7 @@ package body XML_Viewer is
    is
       Error : GNAT.Strings.String_Access;
       Root  : Node_Ptr;
+      Iter  : Gtk_Tree_Iter;
       Path  : Gtk_Tree_Path;
       Col   : Gint;
       Dummy : Boolean;
@@ -599,10 +600,12 @@ package body XML_Viewer is
       View.XML := Root;
 
       --  Expand the first iter
-
-      Path := Get_Path (View.Tree.Model, Get_Iter_First (View.Tree.Model));
-      Dummy := Expand_Row (View.Tree, Path, True);
-      Path_Free (Path);
+      Iter := Get_Iter_First (View.Tree.Model);
+      if Iter /= Null_Iter then
+         Path := Get_Path (View.Tree.Model, Get_Iter_First (View.Tree.Model));
+         Dummy := Expand_Row (View.Tree, Path, True);
+         Path_Free (Path);
+      end if;
 
       return "";
    end Parse_XML;
