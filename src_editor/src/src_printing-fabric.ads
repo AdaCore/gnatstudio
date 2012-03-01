2f14ee5dd1ab5fa3f4157503c07273534752e9ed
Maxim Reznik <reznik@adacore.com>
2012-03-01 20:12:25 +0000

    * src_printing-fabric.ads Add missing spec file
    IB10-031
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@186412 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ src_editor/src/src_printing-fabric.ads
@@ -0,0 +1,24 @@
+------------------------------------------------------------------------------
+--                                  G P S                                   --
+--                                                                          --
+--                     Copyright (C) 2012, AdaCore                          --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+--  This package creates printing object for Source_Buffers.
+
+package Src_Printing.Fabric is
+
+   function Create return Abstract_Printer'Class;
+
+end Src_Printing.Fabric;
