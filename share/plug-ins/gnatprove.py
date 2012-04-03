d73c3c24d178a1ca35abb3786f53b29cc1c60d05
Yannick Moy <moy@adacore.com>
2012-04-03 11:05:43 +0000

    Update gnatprove plugin
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@188040 936e1b1b-40f2-da11-902a-00137254ae57

--- share/plug-ins/gnatprove.py
+++ share/plug-ins/gnatprove.py
@@ -3,7 +3,7 @@
    GNATprove is a tool to apply automatic program proof to Ada programs.
    This plugin allows the user to perform a proof run and integrate its output
    into GPS.
-   See menu Tools->GNATprove.
+   See menu Prove.
 """
 
 ############################################################################
@@ -22,10 +22,16 @@ xml_gnatprove = """<?xml version="1.0"?>
           <arg>-P%PP</arg>
        </command-line>
        <icon>gps-build-all</icon>
-       <switches command="%(tool_name)s" columns="1" lines="1">
+       <switches command="%(tool_name)s" columns="2" lines="1">
          <title column="1" line="1" >Compilation</title>
          <check label="Force Recompilation" switch="-f" column="1"
                 tip="All actions are redone entirely, including compilation and proof" />
+         <title column="2" line="1" >Proof</title>
+         <check label="Report Proved VCs" switch="--report=all" column="2"
+                tip="Report the status of all VCs, including those proved" />
+         <spin label="Prover Timeout" switch="--timeout=" column="2"
+                default="1" min="1" max="3600"
+                tip="Set the prover timeout (in s) for individual VCs" />
        </switches>
     </target-model>
 
@@ -55,6 +61,32 @@ xml_gnatprove = """<?xml version="1.0"?>
        </command-line>
     </target>
 
+    <target model="gnatprove" name="Prove Subprogram" category="GNATprove">
+       <in-menu>FALSE</in-menu>
+       <icon>gps-build-all</icon>
+       <launch-mode>MANUALLY_WITH_DIALOG</launch-mode>
+       <read-only>TRUE</read-only>
+       <command-line>
+          <arg>gnatprove</arg>
+          <arg>-P%PP</arg>
+          <arg>--mode=prove</arg>
+          <arg>--limit-subp=%f:%l</arg>
+       </command-line>
+    </target>
+
+    <target model="gnatprove" name="Prove Line" category="GNATprove">
+       <in-menu>FALSE</in-menu>
+       <icon>gps-build-all</icon>
+       <launch-mode>MANUALLY_WITH_DIALOG</launch-mode>
+       <read-only>TRUE</read-only>
+       <command-line>
+          <arg>gnatprove</arg>
+          <arg>-P%PP</arg>
+          <arg>--mode=prove</arg>
+          <arg>--limit-line=%f:%l</arg>
+       </command-line>
+    </target>
+
   </GNATPROVE>
 """
 
@@ -69,8 +101,14 @@ prove_subp         = "Prove Subprogram"
 
 gnatprove = os_utils.locate_exec_on_path("gnatprove")
 
-def is_entity_context(self):
-    if isinstance (self, GPS.EntityContext) and self.entity():
+# This is the context of an entity declaration if there is an entity, there is
+# a corresponding declaration, and their file:line locations match.
+def is_entity_decl_context(self):
+    if isinstance (self, GPS.EntityContext) and \
+       self.entity() and \
+       self.entity().declaration() and \
+       self.location().file() == self.entity().declaration().file() and \
+       self.location().line() == self.entity().declaration().line():
         return True
     else:
         return False
@@ -86,14 +124,10 @@ def on_prove_file(self):
     GPS.BuildTarget(prove_file).execute()
 
 def on_prove_line(self):
-    target = GPS.BuildTarget(prove_root_project)
-    loc = self.location()
-    target.execute (extra_args="--limit-line="+mk_loc_string(loc))
+    GPS.BuildTarget(prove_line).execute()
 
 def on_prove_subp(self):
-    target = GPS.BuildTarget(prove_root_project)
-    loc = self.entity().declaration()
-    target.execute (extra_args="--limit-subp="+mk_loc_string (loc))
+    GPS.BuildTarget(prove_subp).execute()
 
 if gnatprove:
   GPS.Menu.create(menu_prefix, ref = "Window", add_before = True)
@@ -102,5 +136,5 @@ if gnatprove:
   GPS.Contextual (prefix + "/" + prove_file).create(on_activate = on_prove_file)
   GPS.Contextual (prefix + "/" + prove_line).create(on_activate = on_prove_line)
   GPS.Contextual (prefix + "/" + prove_subp).create(
-          on_activate = on_prove_subp, filter = is_entity_context)
+          on_activate = on_prove_subp, filter = is_entity_decl_context)
   GPS.parse_xml(xml_gnatprove)
