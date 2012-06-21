bccde15e95a780058543611bbad29ca6b8f2d4b6
Arnaud Charlet <charlet@adacore.com>
2012-06-21 09:26:31 +0000

    Get rid of legacy ClearCase module. Part of L614-009
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@191195 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/gps-kernel-preferences.adb
+++ kernel/src/gps-kernel-preferences.adb
@@ -904,14 +904,6 @@ package body GPS.Kernel.Preferences is
          Label   => -"Implicit status",
          Page    => -"VCS");
 
-      Clearcase_Active := Create
-        (Manager => Kernel.Preferences,
-         Name    => "VCS-Activate-Clearcase",
-         Default => False,
-         Doc     => -("Whether the internal ClearCase module is activated."),
-         Label   => -"ClearCase module",
-         Page    => -"VCS");
-
       -- Diff_Utils --
 
       Diff_Mode := Vdiff_Modes_Prefs.Create
