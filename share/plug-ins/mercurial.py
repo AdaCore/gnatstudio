956a85da6751246e96fb91f5561181ed49d67dd1
Automatic merge <gnatmail@adacore.com>
2012-07-11 16:45:54 +0000

    merging automatically changes 192135 to 192137
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/branches/global/gtk3/gps@192138 936e1b1b-40f2-da11-902a-00137254ae57

--- share/plug-ins/mercurial.py
+++ share/plug-ins/mercurial.py
@@ -1,4 +1,11 @@
 """Mercurial CVS support plug-in
+
+   Provides support for the Mercurial configuration management system.
+
+   It integrates into GPS's VCS support, and uses the same menus
+   as all other VCS systems supported by GPS.
+   You can easily edit this file if you would like to customize
+   the mercurial commands that are sent for each of the menus.
 """
 
 ###########################################################################
@@ -39,7 +46,7 @@ actions = [
  SEPARATOR,
 
  { ACTION: "Diff against head",            LABEL: "Compare against tip revision"  },
-{  ACTION: "Diff against revision",        LABEL: "Compare against specific revision" },
+{  ACTION: "Diff against revision",        LABEL: "Compare against other revision" },
 
  SEPARATOR,
 
@@ -74,14 +81,6 @@ register_vcs_actions ("Mercurial", actions)
 #  Mercurial VCS plugin configuration
 
 MERCURIAL_CONFIG = u'''<?xml version="1.0"?>
-<!--  Provides support for the Mercurial configuration management system.
-
-      It integrates into GPS's VCS support, and uses the same menus
-      as all other VCS systems supported by GPS.
-      You can easily edit this file if you would like to customize
-      the mercurial commands that are sent for each of the menus.
-
--->
 
 <GPS>
 
