a971d2c23e6c58e59391bf100dd9cfdbe1f7594c
Automatic merge <gnatmail@adacore.com>
2012-11-29 14:35:05 +0000

    ------------------------------------------------------------------------ r198884 | reznik | 2012-11-29 15:34:01 +0100 (Thu, 29 Nov 2012) | 2 lines
    Move readonly area protection code into gnatttest.py. For LB22_024
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/branches/global/gtk3/gps@198886 936e1b1b-40f2-da11-902a-00137254ae57

--- share/plug-ins/gnattest.py
+++ share/plug-ins/gnattest.py
@@ -5,6 +5,11 @@
 
 import os.path, GPS
 
+GPS.Preference ("Plugins/gnattest/read_only_color").create (
+  "Highlight color", "color",
+   """Background color for read-only areas""",
+   "#e0e0e0")
+
 last_gnattest_project = None
 
 def run (project, target, extra_args=""):
@@ -64,7 +69,7 @@ def on_compilation_finished(hook, category,
    open_harness_project (last_gnattest_project)
 
 def on_project_view_changed(hook):
-
+   """ Replace run target in harness project. """
    test_run_target=GPS.BuildTarget ("Run a test-driver")
    run_main_target=GPS.BuildTarget ("Run Main")
 
@@ -75,5 +80,46 @@ def on_project_view_changed(hook):
       run_main_target.show()
       test_run_target.hide()
 
+def on_file_edited (hook,file):
+   """ Find read-only areas and apply an overlay on them. """
+   if not is_harness_project():
+      return
+
+   read_only_overlay = None
+   buffer = GPS.EditorBuffer.get (file)
+   loc = buffer.beginning_of_buffer ()
+
+   # Iterate over read-only areas
+   while loc:
+      found = loc.search ("--  begin read only", dialog_on_failure=False)
+
+      if found:
+         from_line,last = found
+         found = last.search ("--  end read only", dialog_on_failure=False)
+
+         if found:
+            to_line,loc = found
+         else:
+            loc = None
+
+      else:
+         loc = None
+
+      # if area found
+      if loc:
+         from_line = from_line.beginning_of_line ()
+         to_line = to_line.end_of_line ()
+
+         # if overlay hasn't exist yet, create one
+         if read_only_overlay == None:
+            read_only_overlay = buffer.create_overlay ()
+            color = GPS.Preference ("Plugins/gnattest/read_only_color").get ()
+            read_only_overlay.set_property ("background", color)
+            read_only_overlay.set_property ("editable", False)
+
+         buffer.apply_overlay (read_only_overlay, from_line, to_line)
+   # No more read-only areas
+
+GPS.Hook("file_edited").add (on_file_edited)
 GPS.Hook("compilation_finished").add(on_compilation_finished)
 GPS.Hook("project_view_changed").add(on_project_view_changed)
