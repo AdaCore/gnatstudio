51573a3260ab03125995b059bc8557d2fa0f48b7
Automatic merge <gnatmail@adacore.com>
2013-02-28 10:46:00 +0000

    ------------------------------------------------------------------------ r203278 | reznik | 2013-02-28 11:44:50 +0100 (Thu, 28 Feb 2013) | 7 lines
    * gps-scripts-files.ad[sb]
    Add package
    * gps-kernel-scripts.ad[sb]
    Move commands for File class to GPS.Scripts.Files
    
    For LB21_009
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/branches/global/gtk3/gps@203279 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ kernel/src_info/gps-scripts-files.ads
@@ -0,0 +1,54 @@
+------------------------------------------------------------------------------
+--                                  G P S                                   --
+--                                                                          --
+--                        Copyright (C) 2013, AdaCore                       --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+--  Implementation of File class
+
+with GPS.Core_Kernels;
+with GNATCOLL.Scripts;       use GNATCOLL.Scripts;
+with GNATCOLL.VFS;
+
+package GPS.Scripts.Files is
+
+   procedure Register_Commands
+     (Kernel : access GPS.Core_Kernels.Core_Kernel_Record'Class);
+   --  Add script commands for file class.
+
+   function Get_File_Class
+     (Kernel : access GPS.Core_Kernels.Core_Kernel_Record'Class)
+      return Class_Type;
+   --  Return the class to use for file types. This encapsulates a File_Info.
+   --  This is more efficient than calling directly
+   --  GPS.Kernel.Scripts.New_Class particularly when a File class has already
+   --  been created.
+
+   function Nth_Arg
+     (Data : Callback_Data'Class; N : Positive)
+      return GNATCOLL.VFS.Virtual_File;
+   procedure Set_Nth_Arg
+     (Data : in out Callback_Data'Class;
+      N    : Positive;
+      File : GNATCOLL.VFS.Virtual_File);
+   function Get_Data
+     (Instance : Class_Instance) return GNATCOLL.VFS.Virtual_File;
+   --  Retrieve the file information from an instance. This returns No_File
+   --  if no instance is passed
+
+   function Create_File
+     (Script : access Scripting_Language_Record'Class;
+      File   : GNATCOLL.VFS.Virtual_File) return Class_Instance;
+   --  Return a new file
+
+end GPS.Scripts.Files;
