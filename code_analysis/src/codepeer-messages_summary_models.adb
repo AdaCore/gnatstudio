24ec274e086c745b9d23c80366d242a7c03677ed
Automatic merge <gnatmail@adacore.com>
2013-03-08 12:48:17 +0000

    ------------------------------------------------------------------------ r203961 | godunko | 2013-03-08 13:47:53 +0100 (Fri, 08 Mar 2013) | 10 lines
    * codepeer-messages_summary_models.adb
    (Clear): Hide messages of all lifeages.
    
    (Initialize): Show messages of all lifeages.
    
    * codepeer-messages_reports.adb
    (Initialize): Set visible lifeages.
    
    For M129_023.
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/branches/global/gtk3/gps@203962 936e1b1b-40f2-da11-902a-00137254ae57

--- code_analysis/src/codepeer-messages_summary_models.adb
+++ code_analysis/src/codepeer-messages_summary_models.adb
@@ -36,6 +36,7 @@ package body CodePeer.Messages_Summary_Models is
    procedure Clear (Self : access Messages_Summary_Model_Record) is
    begin
       Self.Message_Categories.Clear;
+      Self.Message_Lifeages := (others => False);
    end Clear;
 
    ------------
@@ -483,6 +484,7 @@ package body CodePeer.Messages_Summary_Models is
 
       Model.Tree := Tree;
       Model.Message_Categories := Categories;
+      Model.Message_Lifeages := (others => True);
 
       Model.Project_Icon    := Project_Icon;
       Model.File_Icon       := File_Icon;
