9a9f9c7fe76300c3c92eb5462a577c5dc6dafea0
Automatic merge <gnatmail@adacore.com>
2013-03-24 17:06:41 +0000

    ------------------------------------------------------------------------ r204677 | godunko | 2013-03-24 18:06:19 +0100 (Sun, 24 Mar 2013) | 5 lines
    Use object directory of 'codepeer' mode to store requests to add audit
    records instead of object directory of project.
    
    Found till investigating of M314_019.
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/branches/global/gtk3/gps@204678 936e1b1b-40f2-da11-902a-00137254ae57

--- code_analysis/src/codepeer-module-bridge.adb
+++ code_analysis/src/codepeer-module-bridge.adb
@@ -74,22 +74,22 @@ package body CodePeer.Module.Bridge is
    is
       Project           : constant Project_Type :=
                             GPS.Kernel.Project.Get_Project (Module.Kernel);
-      Object_Directory  : constant Virtual_File := Project.Object_Dir;
-      Command_File_Name : constant Virtual_File :=
-                            Create_From_Dir
-                              (Object_Directory, Add_Audit_File_Name);
-
       Mode              : constant String := Module.Kernel.Get_Build_Mode;
+      Object_Directory  : Virtual_File;
+      Command_File_Name : Virtual_File;
       Success           : Boolean;
       CL                : Arg_List;
       Ids               : Natural_Sets.Set;
       pragma Warnings (Off, Success);
 
    begin
-      CL := Create ("gps_codepeer_bridge");
-      Append_Argument (CL, +Command_File_Name.Full_Name.all, One_Arg);
-
       Module.Kernel.Set_Build_Mode ("codepeer");
+
+      --  Compute name of object directory and request file
+
+      Object_Directory  := Project.Object_Dir;
+      Command_File_Name :=
+        Create_From_Dir (Object_Directory, Add_Audit_File_Name);
 
       --  Generate command file
 
@@ -116,6 +116,9 @@ package body CodePeer.Module.Bridge is
       end if;
 
       --  Run gps_codepeer_bridge
+
+      CL := Create ("gps_codepeer_bridge");
+      Append_Argument (CL, +Command_File_Name.Full_Name.all, One_Arg);
 
       GPS.Kernel.Timeout.Launch_Process
         (Kernel        => GPS.Kernel.Kernel_Handle (Module.Kernel),
