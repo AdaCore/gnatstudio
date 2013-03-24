2efe5605918c118b24d712e2aff1a563497e15ea
Automatic merge <gnatmail@adacore.com>
2013-03-24 16:41:16 +0000

    ------------------------------------------------------------------------ r204673 | godunko | 2013-03-24 17:40:06 +0100 (Sun, 24 Mar 2013) | 6 lines
    Use object directory of 'codepeer' mode to comute paths to request and
    responce files for audit information instead of use project's object
    directory.
    
    Found while investigating of M314_019.
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/branches/global/gtk3/gps@204674 936e1b1b-40f2-da11-902a-00137254ae57

--- code_analysis/src/codepeer-module-bridge.adb
+++ code_analysis/src/codepeer-module-bridge.adb
@@ -271,22 +271,25 @@ package body CodePeer.Module.Bridge is
    is
       Project            : constant Project_Type :=
                              GPS.Kernel.Project.Get_Project (Module.Kernel);
-      Object_Directory   : constant Virtual_File := Project.Object_Dir;
-      Command_File_Name  : constant Virtual_File :=
-                             Create_From_Dir
-                               (Object_Directory, Audit_Request_File_Name);
-      Reply_File_Name    : constant Virtual_File :=
-                             Create_From_Dir
-                               (Object_Directory, Audit_Reply_File_Name);
+      Object_Directory   : Virtual_File;
+      Command_File_Name  : Virtual_File;
+      Reply_File_Name    : Virtual_File;
       Mode               : constant String := Module.Kernel.Get_Build_Mode;
       CL                 : Arg_List;
       Success            : Boolean;
       pragma Warnings (Off, Success);
 
    begin
-      CL := Create ("gps_codepeer_bridge");
-      Append_Argument (CL, +Command_File_Name.Full_Name.all, One_Arg);
       Module.Kernel.Set_Build_Mode ("codepeer");
+
+      --  Compute directories' and files' names.
+
+      Object_Directory := Project.Object_Dir;
+      Command_File_Name :=
+        Create_From_Dir
+          (Object_Directory, Audit_Request_File_Name);
+      Reply_File_Name :=
+        Create_From_Dir (Object_Directory, Audit_Reply_File_Name);
 
       --  Generate command file
 
@@ -297,6 +300,9 @@ package body CodePeer.Module.Bridge is
          Message.Id);
 
       --  Run gps_codepeer_bridge
+
+      CL := Create ("gps_codepeer_bridge");
+      Append_Argument (CL, +Command_File_Name.Full_Name.all, One_Arg);
 
       GPS.Kernel.Timeout.Launch_Process
         (Kernel        => GPS.Kernel.Kernel_Handle (Module.Kernel),
