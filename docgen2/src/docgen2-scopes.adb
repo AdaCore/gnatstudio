bd8bc6c6b086beb457940def95eaa7cf1a8cf756
Automatic merge <gnatmail@adacore.com>
2013-03-26 09:11:03 +0000

    ------------------------------------------------------------------------ r204749 | miranda | 2013-03-26 10:10:04 +0100 (Tue, 26 Mar 2013) | 5 lines
    * docgen2.adb
    Moving local package Docgen2.Scopes to a private package.
    
    For M219_046
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/branches/global/gtk3/gps@204750 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ docgen2/src/docgen2-scopes.adb
@@ -0,0 +1,266 @@
+------------------------------------------------------------------------------
+--                                  G P S                                   --
+--                                                                          --
+--                     Copyright (C) 2007-2013, AdaCore                     --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+package body Docgen2.Scopes is
+   Empty_Stack : Context_Stack.Vector renames Context_Stack.Empty_Vector;
+
+   procedure Pop (Context : in out Analysis_Context);
+   --  Remove an element from the stack
+
+   procedure Push
+     (Context : in out Analysis_Context;
+      Elem    : Context_Element);
+   --  Add an element to the stack
+
+   function Top (Context : Analysis_Context) return Context_Element;
+   --  Returns the element in the top of the stack
+
+   function Is_Empty (Context : Analysis_Context) return Boolean;
+   --  Returns True if the stack is empty
+
+   ---------
+   -- Pop --
+   ---------
+
+   procedure Pop (Context : in out Analysis_Context) is
+   begin
+      pragma Assert (not Is_Empty (Context));
+      Context.Stack.Delete_Last (Count => 1);
+   end Pop;
+
+   ----------
+   -- Push --
+   ----------
+
+   procedure Push
+     (Context : in out Analysis_Context;
+      Elem    : Context_Element) is
+   begin
+      Context.Stack.Append (Elem);
+   end Push;
+
+   ---------
+   -- Top --
+   ---------
+
+   function Top
+     (Context : Analysis_Context) return Context_Element is
+   begin
+      pragma Assert (not Is_Empty (Context));
+      return Context.Stack.Last_Element;
+   end Top;
+
+   -------------------
+   -- Current_Scope --
+   -------------------
+
+   function Current_Scope
+     (Context : Analysis_Context) return Context_Element is
+   begin
+      return Top (Context);
+   end Current_Scope;
+
+   -----------------
+   -- Enter_Scope --
+   -----------------
+
+   procedure Enter_Scope
+     (Context : in out Analysis_Context;
+      Parent  : Entity_Info;
+      Entity  : Entity_Info)
+   is
+      New_Scope : constant Context_Element :=
+        (Parent_Entity => Parent,
+         Pkg_Entity    => Entity,
+         Parent_Iter   => Context.Iter);
+   begin
+      Push (Context, New_Scope);
+      Context.Iter :=
+        Next (Context.Tree, Context.Iter, Jump_Into);
+   end Enter_Scope;
+
+   ----------------
+   -- Exit_Scope --
+   ----------------
+
+   procedure Exit_Scope
+     (Context : in out Analysis_Context) is
+   begin
+      Context.Iter :=
+        Next (Context.Tree, Top (Context).Parent_Iter, Jump_Over);
+      Pop (Context);
+   end Exit_Scope;
+
+   ----------
+   -- Free --
+   ----------
+
+   procedure Free (Context : in out Analysis_Context) is
+   begin
+      Free (Context.Tree);
+      Free (Context.File_Buffer);
+      Free (Context.Comments);
+   end Free;
+
+   ------------------
+   -- Get_Comments --
+   ------------------
+
+   function Get_Comments
+     (Context : Analysis_Context) return Comments_List.Vector is
+   begin
+      return Context.Comments;
+   end Get_Comments;
+
+   -------------------
+   -- Get_Construct --
+   -------------------
+
+   function Get_Construct
+     (Context : Analysis_Context) return access Simple_Construct_Information is
+   begin
+      return Get_Construct (Context.Iter);
+   end Get_Construct;
+
+   --------------
+   -- Get_File --
+   --------------
+
+   function Get_File (Context : Analysis_Context) return Virtual_File is
+   begin
+      return Context.File;
+   end Get_File;
+
+   ---------------------
+   -- Get_File_Buffer --
+   ---------------------
+
+   function Get_File_Buffer
+     (Context : Analysis_Context) return GNAT.Strings.String_Access is
+   begin
+      return Context.File_Buffer;
+   end Get_File_Buffer;
+
+   ------------------
+   -- Get_Language --
+   ------------------
+
+   function Get_Language
+     (Context : Analysis_Context) return Language_Handler
+   is
+   begin
+      return Context.Language;
+   end Get_Language;
+
+   -------------------------------
+   -- Get_Parent_Scope_Iterator --
+   -------------------------------
+
+   function Get_Parent_Scope_Iterator
+     (Context : Analysis_Context) return Construct_Tree_Iterator is
+   begin
+      return Get_Parent_Scope (Context.Tree, Context.Iter);
+   end Get_Parent_Scope_Iterator;
+
+   --------------
+   -- Has_Next --
+   --------------
+
+   function Has_Next_Construct
+     (Context : Analysis_Context) return Boolean is
+   begin
+      return Context.Iter /= Null_Construct_Tree_Iterator;
+   end Has_Next_Construct;
+
+   --------------------
+   -- Get_New_Pkg_Id --
+   --------------------
+
+   function Get_New_Pkg_Id
+     (Context : in out Analysis_Context) return Natural is
+   begin
+      Context.Pkg_Nb := Context.Pkg_Nb + 1;
+      return Context.Pkg_Nb;
+   end Get_New_Pkg_Id;
+
+   -------------
+   -- In_Body --
+   -------------
+
+   function In_Body (Context : Analysis_Context) return Boolean is
+   begin
+      return Context.In_Body;
+   end In_Body;
+
+   --------------
+   -- Is_Empty --
+   --------------
+
+   function Is_Empty
+     (Context : Analysis_Context) return Boolean is
+   begin
+      return Context.Stack.Is_Empty;
+   end Is_Empty;
+
+   ----------
+   -- Next --
+   ----------
+
+   procedure Next_Construct
+     (Context : in out Analysis_Context) is
+   begin
+      Context.Iter :=
+        Next (Context.Tree, Context.Iter, Jump_Over);
+   end Next_Construct;
+
+   -----------------
+   -- New_Context --
+   -----------------
+
+   function New_Context
+     (File_EInfo  : Entity_Info;
+      Tree        : Construct_Tree;
+      File_Buffer : GNAT.Strings.String_Access;
+      Language    : Language_Handler;
+      Comments    : Comments_List.Vector) return Analysis_Context
+   is
+      Ctxt_Elem   : Context_Element;
+      New_Context : Analysis_Context;
+
+   begin
+      New_Context :=
+        (Stack          => Empty_Stack,
+         Iter           => First (Tree),
+         Tree           => Tree,
+         File_Buffer    => File_Buffer,
+         Language       => Language,
+         File           => File_EInfo.Location.Spec_Loc.File,
+         In_Body        => File_EInfo.Is_Body,
+         Comments       => Comments,
+         Pkg_Nb         => 0);
+
+      Ctxt_Elem :=
+        (Parent_Entity => File_EInfo,
+         Pkg_Entity    => null,
+         Parent_Iter   => Null_Construct_Tree_Iterator);
+
+      Push (New_Context, Ctxt_Elem);
+
+      return New_Context;
+   end New_Context;
+
+end Docgen2.Scopes;
---
+++ docgen2/src/docgen2-scopes.ads
@@ -0,0 +1,114 @@
+------------------------------------------------------------------------------
+--                                  G P S                                   --
+--                                                                          --
+--                     Copyright (C) 2007-2013, AdaCore                     --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+--  Supplementary package used to perform the analysis of constructs.
+
+with GNAT.Strings;      use GNAT.Strings;
+with GNATCOLL.VFS;      use GNATCOLL.VFS;
+
+with Language;          use Language;
+with Language.Tree;     use Language.Tree;
+with Language_Handlers; use Language_Handlers;
+
+with Docgen2.Entities;  use Docgen2.Entities;
+with Docgen2.Tags;      use Docgen2.Tags;
+
+private package Docgen2.Scopes is
+
+   type Context_Element is record
+      Parent_Entity : Entity_Info;
+      Pkg_Entity    : Entity_Info;
+      Parent_Iter   : Construct_Tree_Iterator;
+   end record;
+
+   type Analysis_Context is private;
+
+   function New_Context
+     (File_EInfo  : Entity_Info;
+      Tree        : Construct_Tree;
+      File_Buffer : GNAT.Strings.String_Access;
+      Language    : Language_Handler;
+      Comments    : Comments_List.Vector) return Analysis_Context;
+   --  Constructor of Analysis_Context. Initializes the implicit cursor to
+   --  start traversing the Tree of constructs.
+
+   procedure Free (Context : in out Analysis_Context);
+
+   procedure Enter_Scope
+     (Context : in out Analysis_Context;
+      Parent  : Entity_Info;
+      Entity  : Entity_Info);
+   --  Enter in the scope of Entity and displace the implicit cursor to start
+   --  processing all the entities defined in the scope of Entity.
+
+   procedure Exit_Scope
+     (Context : in out Analysis_Context);
+   --  Exit the current scope and move the implicit cursor to the next
+   --  construct of the enclosing scope
+
+   function Current_Scope
+     (Context : Analysis_Context)
+         return Context_Element;
+   --  Return the current scope referenced by the implicit cursor.
+
+   function Get_New_Pkg_Id
+     (Context : in out Analysis_Context) return Natural;
+   --  Generate and returns a new package number identifier
+
+   --  Implicit cursor subprograms **************************************
+   function Get_Construct
+     (Context : Analysis_Context)
+         return access Simple_Construct_Information;
+   --  Returns the current construct referenced by the implicit cursor
+   function Has_Next_Construct
+     (Context : Analysis_Context) return Boolean;
+   --  Return True if the current scope has more constructs to traverse
+   procedure Next_Construct
+     (Context : in out Analysis_Context);
+   --  Move the implicit cursor to the next construct of the current scope
+
+   --  Context getters/setters *****************************************
+   function Get_Comments
+     (Context : Analysis_Context) return Comments_List.Vector;
+   function Get_File
+     (Context : Analysis_Context) return Virtual_File;
+   function Get_File_Buffer
+     (Context : Analysis_Context) return GNAT.Strings.String_Access;
+   function Get_Language
+     (Context : Analysis_Context) return Language_Handler;
+   function Get_Parent_Scope_Iterator
+     (Context : Analysis_Context) return Construct_Tree_Iterator;
+   function In_Body
+     (Context : Analysis_Context) return Boolean;
+
+private
+   package Context_Stack is new Ada.Containers.Vectors
+     (Index_Type   => Natural,
+      Element_Type => Context_Element);
+
+   type Analysis_Context is record
+      Stack       : Context_Stack.Vector;
+      Iter        : Construct_Tree_Iterator;
+      Tree        : Construct_Tree;
+      File_Buffer : GNAT.Strings.String_Access;
+      File        : Virtual_File;
+      Language    : Language_Handler;
+      Pkg_Nb      : Natural;
+      Comments    : Comments_List.Vector;
+      In_Body     : Boolean;
+   end record;
+end Docgen2.Scopes;
