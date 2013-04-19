1ae3741aeec16aa5227050b3e64b1ded205c4434
Vadim Godunko <godunko@adacore.com>
2013-04-19 06:35:10 +0000

    Remove use of custom tree model and use GtkTreeStore instead.
    For M409-045.
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/branches/global/gtk3/gps@207187 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/gps-location_view-listener.ads
+++ kernel/src/gps-location_view-listener.ads
@@ -18,25 +18,22 @@
 --  This package provides classic Category/File/Message Gtk+ model to
 --  be used in location views.
 
-with Ada.Containers.Vectors;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
-with Gtkada.Abstract_Tree_Model;
+with Gtk.Tree_Store;
 with GNATCOLL.VFS;
 
-with GPS.Kernel.Messages; use GPS.Kernel.Messages;
+with GPS.Kernel.Messages;   use GPS.Kernel.Messages;
 
 with Glib;
 private with Gdk.RGBA;
 private with Gdk.Pixbuf;
-private with Glib.Values;
 with Gtk.Tree_Model;
 
 package GPS.Location_View.Listener is
 
    type Classic_Tree_Model_Record is
-     new Gtkada.Abstract_Tree_Model.Gtk_Abstract_Tree_Model_Record
-   with private;
+     new Gtk.Tree_Store.Gtk_Tree_Store_Record with private;
 
    type Classic_Tree_Model is access all Classic_Tree_Model_Record'Class;
 
@@ -49,7 +46,7 @@ package GPS.Location_View.Listener is
 
    procedure Unregister
      (Kernel : Kernel_Handle;
-      L      : in out Locations_Listener_Access);
+      Self   : in out Locations_Listener_Access);
    --  Unregister the listener and free memory associated to it
 
    function Get_Model
@@ -105,140 +102,24 @@ package GPS.Location_View.Listener is
    --  Total number of columns.
 
 private
-   type Node_Record is tagged;
-   type Node_Access is access all Node_Record'Class;
-
-   package Node_Vectors is
-     new Ada.Containers.Vectors (Positive, Node_Access);
-
-   type Node_Kinds is (Node_Category, Node_File, Node_Message);
-
-   type Node_Record (Kind : Node_Kinds) is tagged limited record
-      Parent        : Node_Access := null;
-      Children      : Node_Vectors.Vector;
-
-      Message_Count : Natural := 0;
-
-      case Kind is
-         when Node_Category =>
-            Name    : Ada.Strings.Unbounded.Unbounded_String;
-
-         when Node_File =>
-            File    : GNATCOLL.VFS.Virtual_File;
-
-         when Node_Message =>
-            Message : Message_Access;
-      end case;
-   end record;
 
    type Classic_Tree_Model_Record is
-     new Gtkada.Abstract_Tree_Model.Gtk_Abstract_Tree_Model_Record with record
+     new Gtk.Tree_Store.Gtk_Tree_Store_Record with null record;
+
+   procedure Initialize (Self : access Classic_Tree_Model_Record'Class);
+   procedure Gtk_New (Object : out Classic_Tree_Model);
+
+   type Locations_Listener is
+     new GPS.Kernel.Messages.Abstract_Listener with record
       Kernel          : Kernel_Handle;
-
-      Categories      : Node_Vectors.Vector;
-
-      Stamp           : Glib.Gint := 1;
-      --  Integer value to distinguish obsolete values of Gtk_Tree_Iter by
-      --  Gtk+ convention.
-
-      Non_Leaf_Color  : Gdk.RGBA.Gdk_RGBA;
-      --  Foreground color for category and file nodes.
+      Model           : Classic_Tree_Model;
 
       Category_Pixbuf : Gdk.Pixbuf.Gdk_Pixbuf;
       File_Pixbuf     : Gdk.Pixbuf.Gdk_Pixbuf;
       --  Pixbufs to decorate category and file nodes.
-   end record;
 
-   procedure Initialize (Self : access Classic_Tree_Model_Record'Class);
-   procedure Gtk_New
-     (Object    : out Classic_Tree_Model;
-      Kernel    : Kernel_Handle);
-
-   procedure Node_Added
-     (Self : not null access Classic_Tree_Model_Record;
-      Node : not null Node_Access);
-   --  Common implementation of node adding handling, it handles items of all
-   --  levels. Category_Added and File_Added just a renaming of Node_Added;
-   --  Message_Added can't be renamed due to difference in subprogram's
-   --  profile.
-
-   overriding function Get_N_Columns
-     (Self : access Classic_Tree_Model_Record) return Glib.Gint;
-   --  Override this to return the number of columns supported by Tree_Model.
-
-   overriding function Get_Column_Type
-     (Self  : access Classic_Tree_Model_Record;
-      Index : Glib.Gint) return Glib.GType;
-   --  Override this to return the type of the Index-th column in the model.
-
-   overriding function Get_Iter
-     (Self : access Classic_Tree_Model_Record;
-      Path : Gtk.Tree_Model.Gtk_Tree_Path)
-      return Gtk.Tree_Model.Gtk_Tree_Iter;
-   --  Override this return an iterator pointing to Path.
-   --  Null_Iter is returned if Path was invalid or no iterator could be set.
-
-   overriding function Get_Path
-     (Self : access Classic_Tree_Model_Record;
-      Iter : Gtk.Tree_Model.Gtk_Tree_Iter)
-      return Gtk.Tree_Model.Gtk_Tree_Path;
-   --  Override this to return a newly created Gtk_Tree_Path referenced by
-   --  Iter. This path will be freed with Path_Free by the caller.
-
-   overriding procedure Get_Value
-     (Self   : access Classic_Tree_Model_Record;
-      Iter   : Gtk.Tree_Model.Gtk_Tree_Iter;
-      Column : Glib.Gint;
-      Value  : out Glib.Values.GValue);
-   --  Override this get a value from the model, at column Column and line
-   --  Iter. Value must be freed by the caller.
-
-   overriding procedure Next
-     (Self : access Classic_Tree_Model_Record;
-      Iter : in out Gtk.Tree_Model.Gtk_Tree_Iter);
-   --  Override this to set Iter to point to the node following it at the
-   --  current level. If there is none, Iter is set to Null_Iter.
-
-   overriding function Children
-     (Self   : access Classic_Tree_Model_Record;
-      Parent : Gtk.Tree_Model.Gtk_Tree_Iter)
-      return Gtk.Tree_Model.Gtk_Tree_Iter;
-   --  Override this to return the first child of Parent. If Parent has no
-   --  children, return Null_Iter. Parent will remain a valid node after this
-   --  function has been called.
-
-   overriding function Has_Child
-     (Self : access Classic_Tree_Model_Record;
-      Iter : Gtk.Tree_Model.Gtk_Tree_Iter) return Boolean;
-   --  Override this to return True if Iter has children, False otherwise.
-
-   overriding function N_Children
-     (Self : access Classic_Tree_Model_Record;
-      Iter : Gtk.Tree_Model.Gtk_Tree_Iter := Gtk.Tree_Model.Null_Iter)
-      return Glib.Gint;
-   --  Override this to return the number of children that Iter has.
-   --  As a special case, if Iter is Null_Iter, then the number of toplevel
-   --  nodes is returned.
-
-   overriding function Nth_Child
-     (Self   : access Classic_Tree_Model_Record;
-      Parent : Gtk.Tree_Model.Gtk_Tree_Iter;
-      N      : Glib.Gint) return Gtk.Tree_Model.Gtk_Tree_Iter;
-   --  Override this to return the child of Parent, using the given index.
-   --  The First index is 0. If Index is too big, or Parent has no children,
-   --  return Null_Iter. If Parent is Null_Iter, then the nth root node is set.
-
-   overriding function Parent
-     (Self  : access Classic_Tree_Model_Record;
-      Child : Gtk.Tree_Model.Gtk_Tree_Iter)
-      return Gtk.Tree_Model.Gtk_Tree_Iter;
-   --  Override this to return the parent of Child. If Child is at the
-   --  toplevel, and doesn't have a parent, then Null_Iter is returned.
-
-   type Locations_Listener
-     is new Abstract_Listener
-   with record
-      Model  : Classic_Tree_Model;
+      Non_Leaf_Color  : Gdk.RGBA.Gdk_RGBA;
+      --  Foreground color for category and file nodes.
    end record;
 
    overriding procedure Message_Property_Changed
@@ -268,5 +149,9 @@ private
      (Self     : not null access Locations_Listener;
       Category : Ada.Strings.Unbounded.Unbounded_String;
       Allow_Auto_Jump_To_First : Boolean);
+
+   overriding procedure Category_Removed
+     (Self     : not null access Locations_Listener;
+      Category : Ada.Strings.Unbounded.Unbounded_String);
 
 end GPS.Location_View.Listener;
