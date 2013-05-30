596f723f50fb301188457480635337185f3d7691
Vadim Godunko <godunko@adacore.com>
2013-05-30 17:06:45 +0000

    Generate data file to add audit records using corresponding version of format.
    Part of M301-031.
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@210534 936e1b1b-40f2-da11-902a-00137254ae57

--- code_analysis/src/codepeer-bridge-commands.ads
+++ code_analysis/src/codepeer-bridge-commands.ads
@@ -46,6 +46,17 @@ package CodePeer.Bridge.Commands is
       Probability_Changed : Boolean;
       New_Ranking         : CodePeer.Message_Ranking_Level;
       Comment             : Unbounded_String);
-   --  Generates command file for add audit record to the database.
+   --  Generates command file for add audit record to the database using
+   --  version 2 of interchange format.
+
+   procedure Add_Audit_Record_V3
+     (Command_File_Name : Virtual_File;
+      Output_Directory  : Virtual_File;
+      Ids               : Natural_Sets.Set;
+      Status            : CodePeer.Audit_Status_Kinds;
+      Approved_By       : Unbounded_String;
+      Comment           : Unbounded_String);
+   --  Generates command file for add audit record to the database using
+   --  version 3 of interchange format.
 
 end CodePeer.Bridge.Commands;
