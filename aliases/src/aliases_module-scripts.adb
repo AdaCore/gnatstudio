16d7be2a132c9286c8669560a2ede3b8f34c7432
Raphael Amiard <amiard@adacore.com>
2013-06-02 14:14:59 +0000

    M411-022: Added banners and documentation to the script package for aliases. Added an entry in the feature file for aliases expansion in-editor.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/branches/global/gtk3/gps@210650 936e1b1b-40f2-da11-902a-00137254ae57

--- aliases/src/aliases_module-scripts.adb
+++ aliases/src/aliases_module-scripts.adb
@@ -1,3 +1,20 @@
+------------------------------------------------------------------------------
+--                                  G P S                                   --
+--                                                                          --
+--                     Copyright (C) 2003-2013, AdaCore                     --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
 with GNATCOLL.Scripts; use GNATCOLL.Scripts;
 with GPS.Kernel.Scripts;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
@@ -6,10 +23,10 @@ package body Aliases_Module.Scripts is
 
    Alias_Class_Name : constant String := "Alias";
 
-   --  Static alias getter for the Alias class
    procedure Aliases_Static_Get
      (Data    : in out Callback_Data'Class;
       Command : String);
+   --  Static alias getter for the Alias class
 
    ------------------------
    -- Aliases_Static_Get --
--- aliases/src/aliases_module-scripts.ads
+++ aliases/src/aliases_module-scripts.ads
@@ -1,3 +1,26 @@
+------------------------------------------------------------------------------
+--                                  G P S                                   --
+--                                                                          --
+--                     Copyright (C) 2003-2013, AdaCore                     --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+-- This package presents a very bare bones scripting interface to the 
+-- aliases module.
+-- The only functionality provided on the ada side is to be able to retrieve
+-- an alias instance by name. Every instance has two properties, name and
+-- expansion. Everything else is done python-side
+
 with GPS.Kernel;
 
 package Aliases_Module.Scripts is
