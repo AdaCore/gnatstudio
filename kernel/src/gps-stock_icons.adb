d4227e0843ff0503f56a9e4a7fb4c1fe765cbcb7
Emmanuel Briot <briot@adacore.com>
2013-06-20 08:04:51 +0000

    Remove hard-coded paths for some of the icons, and use icons.xml instead.  This will make it easier to have icon themes eventually (for instance to  display on a dark background)
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/branches/global/gtk3/gps@211676 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/gps-stock_icons.adb
+++ kernel/src/gps-stock_icons.adb
@@ -17,14 +17,9 @@
 
 with GNATCOLL.Traces;   use GNATCOLL.Traces;
 with GNATCOLL.VFS;      use GNATCOLL.VFS;
-with Gdk.Pixbuf;        use Gdk.Pixbuf;
-with Glib.Error;        use Glib, Glib.Error;
+with Glib;              use Glib;
 with Gtk.Enums;         use Gtk.Enums;
 with Gtk.Icon_Factory;  use Gtk.Icon_Factory;
-with Gtk.Icon_Set;      use Gtk.Icon_Set;
-with Gtk.Icon_Source;   use Gtk.Icon_Source;
-with GPS.Intl;          use GPS.Intl;
-with Gtk.Stock;         use Gtk.Stock;
 
 package body GPS.Stock_Icons is
    Me : constant Trace_Handle := Create ("STOCK");
@@ -34,71 +29,15 @@ package body GPS.Stock_Icons is
    --------------------------
 
    procedure Register_Stock_Icons (System_Dir : GNATCOLL.VFS.Virtual_File) is
-      Sys : constant Virtual_File :=
-        System_Dir.Create_From_Dir ("share/gps/icons");
+      pragma Unreferenced (System_Dir);
       W, H    : Gint;
       Result  : Boolean;
-      Factory : Gtk_Icon_Factory;
-
-      procedure Icon (Stock, Label : String; Path : Filesystem_String;
-                      Wildcarded : Boolean := True;
-                      Size : Gtk_Icon_Size := Icon_Size_Small_Toolbar);
-      --  Register a single icon.
-      --  If Wildcarded is false, then the icon cannot be used at other sizes,
-      --  although it is set to be best for Size.
-
-      procedure Icon (Stock, Label : String; Path : Filesystem_String;
-                      Wildcarded : Boolean := True;
-                      Size : Gtk_Icon_Size := Icon_Size_Small_Toolbar)
-      is
-         P : constant Virtual_File := Sys.Create_From_Dir (Path);
-         Set     : Gtk_Icon_Set;
-         Source  : Gtk_Icon_Source;
-         Pixbuf  : Gdk_Pixbuf;
-         Error   : GError;
-         Item    : Gtk_Stock_Item;
-      begin
-         if Path /= "" then
-            if not Wildcarded then
-               Gtk_New (Source);
-               Source.Set_Size_Wildcarded (False);
-               Source.Set_Size (Size);
-               Source.Set_Filename (P.Display_Full_Name);
-               Source.Set_Icon_Name (Stock);
-
-               Gtk_New (Set);
-               Set.Add_Source (Source);
-               Factory.Add (Stock, Set);
-
-            else
-               --  The fallback for the icon
-               Gdk_New_From_File (Pixbuf, +P.Full_Name.all, Error);
-
-               if Error /= null then
-                  Trace (Me, "Error loading " & P.Display_Full_Name & ": "
-                         & Get_Message (Error));
-               else
-                  Gtk_New_From_Pixbuf (Set, Pixbuf);
-                  Factory.Add (Stock, Set);
-               end if;
-            end if;
-         end if;
-
-         if Label /= "" then
-            Gtk_New (Item, Stock_Id => Stock, Label => Label,
-                     Modifier => 0, Keyval => 0, Translation_Domain => "");
-            Add (Item);
-         end if;
-      end Icon;
-
    begin
       Icon_Size_Action_Button := Icon_Size_Register ("ICON_SIZE_ACTION", 7, 7);
       Icon_Size_Local_Toolbar :=
          Icon_Size_Register ("ICON_SIZE_LOCAL_TOOLBAR", 12, 12);
 
       if Active (Me) then
-         Trace (Me, "Icons dir=" & Sys.Display_Full_Name);
-
          Icon_Size_Lookup (Icon_Size_Action_Button, W, H, Result);
          Trace (Me, "Icon size Action =>" & W'Img & "x" & H'Img);
 
@@ -117,22 +56,6 @@ package body GPS.Stock_Icons is
          Icon_Size_Lookup (Icon_Size_Button, W, H, Result);
          Trace (Me, "Icon size Button =>" & W'Img & "x" & H'Img);
       end if;
-
-      Gtk_New (Factory);
-      Add_Default (Factory);
-
-      Icon (GPS_Stock_Config_Menu, -"Configuration", "svg/menu.svg");
-      Icon (GPS_Stop_Task, -"Close", "svg/close.svg");
-      Icon (GPS_Expand_All, -"Expand all", "9px/expand_12.png");
-      Icon (GPS_Collapse_All, "-Collapse all", "9px/collapse_12.png");
-      Icon (GPS_Clear_Entry, -"Clear", "16px/clear_entry_16.png");
-      Icon (GPS_Toggle_Links, -"Hide/Show links", "9px/toggle_links_12.png");
-      Icon (GPS_Remove_Unselected, -"Remove unselected",
-            "9px/remove_unselected_12.png");
-      Icon (GPS_Read_Only, -"Read only", "svg/lock.svg");
-      Icon (GPS_Writable, -"Writable", "svg/unlock.svg");
-      Icon (GPS_Regexp, -"Regexp", "16px/regexp.png");
-      Icon (GPS_Negate_Search, -"Invert search", "16px/negate.png");
    end Register_Stock_Icons;
 
 end GPS.Stock_Icons;
