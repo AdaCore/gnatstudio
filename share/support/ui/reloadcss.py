0e6f77c1acbacaea49e4f651320a7fb3fe1150cc
Emmanuel Briot <briot@adacore.com>
2013-08-12 15:18:37 +0000

    Move reloadcss.py to support/ui so that it isn't visible in the  Plug-ins dialog. Added missing documentation Add ??? comments
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@214035 936e1b1b-40f2-da11-902a-00137254ae57

--- share/support/ui/reloadcss.py
+++ share/support/ui/reloadcss.py
@@ -1,23 +1,35 @@
+"""
+This plug in provides a convenient action to force the reload
+of the CSS file.
+This is mostly useful for developers who want to build their
+own CSS files.
+"""
+
+
+
 import GPS
-from gps_utils import *
+from gps_utils import interactive
 from gi.repository import Gtk
 
 providers = {}
 
 
 def get_css_provider(buf):
+    # ??? Why do we need one provider per buffer ? One per
+    # screen who be enough and more efficient
+
     f = buf.file()
     fname = f.name()
-    screen = buf.current_view().pywidget().get_screen()
     p = providers.get(fname, None)
     if not p:
         p = Gtk.CssProvider()
         providers[fname] = p
+        screen = buf.current_view().pywidget().get_screen()
         Gtk.StyleContext.add_provider_for_screen(screen, p, 1000)
     return p
 
 
-@interactive("Editor",  name="Apply editor's css to GPS")
+@interactive("Editor", name="Apply editor's css to GPS")
 def apply_css():
     buf = GPS.EditorBuffer.get()
     text = buf.get_chars()
