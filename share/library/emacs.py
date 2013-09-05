ceda019cb6531ec55ee5470142ccae2d60ae89a1
Emmanuel Briot <briot@adacore.com>
2013-09-05 15:54:31 +0000

    On OSX, alt- shortcuts should be cmd-
    There is no Alt key there, and that was disabling a number of standard
    key shortcuts on that platform.
    M905-047
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@215192 936e1b1b-40f2-da11-902a-00137254ae57

--- share/library/emacs.py
+++ share/library/emacs.py
@@ -20,6 +20,9 @@ import isearch
 import rectangles
 import text_utils
 import navigation_utils
+import sys
+
+alt = 'alt' if sys.platform != 'darwin' else 'cmd'
 
 XML = r"""<?xml version="1.0" ?>
 <GPS>
@@ -35,19 +38,19 @@ XML = r"""<?xml version="1.0" ?>
   <key action="subprogram box">control-c n</key>
   <key action="kill line">control-k</key>
   <key action="kill forward">control-d</key>
-  <key action="Delete word forward">alt-d</key>
+  <key action="Delete word forward">%(alt)s-d</key>
   <key action="transpose chars">control-t</key>
   <key action="Transpose lines">control-x control-t</key>
   <key action="/File/Save">control-x control-s</key>
   <key action="/File/Save As..." >control-x control-w</key>
   <key action="goto beginning of line">control-a</key>
   <key action="goto end of line">control-e</key>
-  <key action="goto beginning of buffer">alt-less</key>
-  <key action="goto end of buffer">alt-shift-greater</key>
+  <key action="goto beginning of buffer">%(alt)s-less</key>
+  <key action="goto end of buffer">%(alt)s-shift-greater</key>
   <key action="Format selection" >tab</key>
   <key action="/File/Open...">control-x control-f</key>
   <key action="/Edit/Undo">control-_</key>
-  <key action="/Edit/Refill">alt-q</key>
+  <key action="/Edit/Refill">%(alt)s-q</key>
   <key action="repeat next">control-u</key>
 
   <key action="goto declaration or body">control-c control-d</key>
@@ -55,23 +58,23 @@ XML = r"""<?xml version="1.0" ?>
 
   <key action="/Navigate/Back">control-c control-s</key>
   <key action="open line">control-o</key>
-  <key action="Join line">alt-j</key>
-  <key action="Upper case word">alt-u</key>
-  <key action="Lower case word">alt-l</key>
-  <key action="Capitalize word">alt-c</key>
-  <key action="/Navigate/Goto Line...">alt-g</key>
-  <key action='zap to char'>alt-z</key>
-  <key action='just one space'>alt-space</key>
+  <key action="Join line">%(alt)s-j</key>
+  <key action="Upper case word">%(alt)s-u</key>
+  <key action="Lower case word">%(alt)s-l</key>
+  <key action="Capitalize word">%(alt)s-c</key>
+  <key action="/Navigate/Goto Line...">%(alt)s-g</key>
+  <key action='zap to char'>%(alt)s-z</key>
+  <key action='just one space'>%(alt)s-space</key>
 
   <key action="set mark command">control-space</key>
   <key action="Cancel mark command">control-g</key>
 
-  <key action="Global Search in context: actions">alt-x</key>
+  <key action="Global Search in context: actions">%(alt)s-x</key>
   <key action="Global Search in context: opened windows">control-x b</key>
 
   <key action="/Edit/Paste">control-y</key>
-  <key action="/Edit/Paste Previous">alt-y</key>
-  <key action="/Edit/Copy">alt-w</key>
+  <key action="/Edit/Paste Previous">%(alt)s-y</key>
+  <key action="/Edit/Copy">%(alt)s-w</key>
   <key action="/Edit/Cut">control-w</key>
 
   <key action="Macro Start Keyboard">control-x parenleft</key>
@@ -86,7 +89,7 @@ XML = r"""<?xml version="1.0" ?>
 
   <key action="/Navigate/Find or Replace...">control-s</key>
 
-  <key action="delete horizontal space">alt-backslash</key>
+  <key action="delete horizontal space">%(alt)s-backslash</key>
 
   <key action="New View Vertical">control-x 2</key>
   <key action="New View Horizontal">control-x 3</key>
@@ -98,11 +101,11 @@ XML = r"""<?xml version="1.0" ?>
 
   <key action="Center cursor on screen">control-l</key>
 
-  <key action="Delete word backward">alt-BackSpace</key>
+  <key action="Delete word backward">%(alt)s-BackSpace</key>
 
-  <key action="Move to previous word">alt-Left</key>
-  <key action="Move to next word">alt-Right</key>
-  <key action="Move to previous page">alt-v</key>
+  <key action="Move to previous word">%(alt)s-Left</key>
+  <key action="Move to next word">%(alt)s-Right</key>
+  <key action="Move to previous page">%(alt)s-v</key>
   <key action="Move to next page">control-v</key>
 
   <key action="/Edit/Rectangle/Cut">control-x r k</key>
@@ -115,6 +118,6 @@ XML = r"""<?xml version="1.0" ?>
   <key action="/Navigate/Find Incremental">control-s</key>
   <key action="/Navigate/Find Previous Incremental">control-r</key>
 </GPS>
-"""
+""" % {"alt": alt}
 
 GPS.parse_xml(XML)
