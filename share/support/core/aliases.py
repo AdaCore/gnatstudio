4a3ed571e6b87ec8862009726d792aafb9eb3aae
Raphael Amiard <amiard@adacore.com>
2013-09-12 13:51:51 +0000

    Some cleaning up in aliases, plus adding is_in_alias_expansion predicate helper
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@215577 936e1b1b-40f2-da11-902a-00137254ae57

--- share/support/core/aliases.py
+++ share/support/core/aliases.py
@@ -52,41 +52,45 @@ def get_comments_colors():
     return c1, c2
 
 
-def reset_overlay(editor):
+def reset_overlay(ed_buffer):
     """
     Reset the aliases overlay completely
     """
-    editor.remove_overlay(
-        editor.aliases_overlay,
-        editor.alias_begin_mark.location(),
-        editor.alias_end_mark.location()
+    ed_buffer.remove_overlay(
+        ed_buffer.aliases_overlay,
+        ed_buffer.alias_begin_mark.location(),
+        ed_buffer.alias_end_mark.location()
     )
 
+def is_in_alias_expansion(ed_buffer):
+    """
+    Returns true if the ed_buffer is in the process of alias expansion
+    """
+    return bool(getattr(ed_buffer, "current_alias_mark_index", None))
 
-def exit_alias_expand(editor):
+def exit_alias_expand(ed_buffer):
     """
     Exit alias expansion.
     """
-    editor.remove_all_multi_cursors()
-    reset_overlay(editor)
-    editor.remove_overlay(
-        editor.aliases_background_overlay,
-        editor.alias_begin_mark.location().beginning_of_line(),
-        editor.alias_end_mark.location()
+    ed_buffer.remove_all_multi_cursors()
+    reset_overlay(ed_buffer)
+    ed_buffer.remove_overlay(
+        ed_buffer.aliases_background_overlay,
+        ed_buffer.alias_begin_mark.location().beginning_of_line(),
+        ed_buffer.alias_end_mark.location()
     )
-    editor.remove_overlay(
-        editor.aliases_background_overlay_1,
-        editor.beginning_of_buffer(),
-        editor.end_of_buffer()
+    ed_buffer.remove_overlay(
+        ed_buffer.aliases_background_overlay_1,
+        ed_buffer.beginning_of_buffer(),
+        ed_buffer.end_of_buffer()
     )
-    editor.current_alias_mark_index = None
-    editor.alias_marks = None
-    editor.current_alias_mark_index = None
-    editor.alias_end_mark = None
-    editor.alias_begin_mark = None
+    ed_buffer.current_alias_mark_index = None
+    ed_buffer.alias_marks = None
+    ed_buffer.alias_end_mark = None
+    ed_buffer.alias_begin_mark = None
     Hook("character_added").remove(on_edit)
     Hook("location_changed").remove(on_move)
-    editor.finish_undo_group()
+    ed_buffer.finish_undo_group()
 
 
 @interactive("Editor", name="Expand alias under cursor")
@@ -95,87 +99,87 @@ def expand_alias_action():
     Action to expand the alias under cursor directly from
     the editor
     """
-    editor = EditorBuffer.get()
-    editor.start_undo_group()
-    cursor_loc = editor.current_view().cursor().forward_char(-1)
+    ed_buffer = EditorBuffer.get()
+    ed_buffer.start_undo_group()
+    cursor_loc = ed_buffer.current_view().cursor().forward_char(-1)
     start_loc = goto_word_start(cursor_loc)
-    alias_name = editor.get_chars(start_loc, cursor_loc)
-    editor.delete(start_loc, cursor_loc)
+    alias_name = ed_buffer.get_chars(start_loc, cursor_loc)
+    ed_buffer.delete(start_loc, cursor_loc)
     alias = Alias.get(alias_name)
     if alias:
-        expand_alias(editor, alias)
+        expand_alias(ed_buffer, alias)
 
 
 @interactive("Editor",  name="Toggle to next alias field")
-def toggle_next_field(editor=None):
+def toggle_next_field(ed_buffer=None):
     """
     When in alias expansion, toggle to next field
     """
 
-    if not editor:
-        editor = EditorBuffer.get()
+    if not ed_buffer:
+        ed_buffer = EditorBuffer.get()
 
     try:
-        reset_overlay(editor)
+        reset_overlay(ed_buffer)
 
-        editor.apply_overlay(
-            editor.aliases_background_overlay_1,
-            editor.beginning_of_buffer(),
-            editor.end_of_buffer()
+        ed_buffer.apply_overlay(
+            ed_buffer.aliases_background_overlay_1,
+            ed_buffer.beginning_of_buffer(),
+            ed_buffer.end_of_buffer()
         )
 
-        editor.apply_overlay(
-            editor.aliases_background_overlay,
-            editor.alias_begin_mark.location().beginning_of_line(),
-            editor.alias_end_mark.location()
+        ed_buffer.apply_overlay(
+            ed_buffer.aliases_background_overlay,
+            ed_buffer.alias_begin_mark.location().beginning_of_line(),
+            ed_buffer.alias_end_mark.location()
         )
 
-        i = editor.current_alias_mark_index
+        i = ed_buffer.current_alias_mark_index
 
         if i is None:
             return
 
-        if i >= len(editor.alias_marks):
-            if editor.last_alias_mark:
-                editor.current_view().goto(editor.last_alias_mark.location())
-                exit_alias_expand(editor)
+        if i >= len(ed_buffer.alias_marks):
+            if ed_buffer.last_alias_mark:
+                ed_buffer.current_view().goto(ed_buffer.last_alias_mark.location())
+                exit_alias_expand(ed_buffer)
                 # ??? Doesn't work every time if executed only one
                 execute_action("/Edit/Format Selection")
                 execute_action("/Edit/Format Selection")
             else:
-                exit_alias_expand(editor)
+                exit_alias_expand(ed_buffer)
             return
 
-        editor.remove_all_multi_cursors()
-        marks = editor.alias_marks[i]
+        ed_buffer.remove_all_multi_cursors()
+        marks = ed_buffer.alias_marks[i]
 
         # Delete the placeholder text
         for mark_start, mark_end in marks:
             lstart = mark_start.location()
             lend = mark_end.location().forward_char(-1)
             if lend >= lstart:
-                editor.delete(lstart, lend)
+                ed_buffer.delete(lstart, lend)
 
-        editor.current_view().goto(marks[0][0].location())
+        ed_buffer.current_view().goto(marks[0][0].location())
         try:
             execute_action("/Edit/Format Selection")
         except:
             pass
 
-        reset_overlay(editor)
+        reset_overlay(ed_buffer)
 
         # Add multi cursors for every other mark
         if len(marks) > 1:
             for mark_begin, mark_end in marks[1:]:
-                editor.add_multi_cursor(mark_begin.location())
+                ed_buffer.add_multi_cursor(mark_begin.location())
 
-        editor.current_alias_mark_index += 1
+        ed_buffer.current_alias_mark_index += 1
 
     except AttributeError:
         return
 
 
-def apply_overlay(editor, mark_start, mark_end, overlay):
+def apply_overlay(ed_buffer, mark_start, mark_end, overlay):
     """
     Apply overlay overlay between mark_start and mark end
     if mark_start - mark_end >= 1 char
@@ -183,7 +187,7 @@ def apply_overlay(editor, mark_start, mark_end, overlay):
     lstart = mark_start.location()
     lend = mark_end.location().forward_char(-1)
     if lend >= lstart:
-        editor.apply_overlay(overlay, lstart, lend)
+        ed_buffer.apply_overlay(overlay, lstart, lend)
 
 
 def on_edit(hook_name, file_name):
@@ -191,12 +195,12 @@ def on_edit(hook_name, file_name):
     Event handler on insert/delete. Mainly ensures that the current field
     in alias expansion is highlighted (via the aliases overlay)
     """
-    editor = EditorBuffer.get(file_name)
-    if editor.current_alias_mark_index > 0:
-        marks_list = editor.alias_marks[editor.current_alias_mark_index - 1]
-        reset_overlay(editor)
+    ed_buffer = EditorBuffer.get(file_name)
+    if ed_buffer.current_alias_mark_index > 0:
+        marks_list = ed_buffer.alias_marks[ed_buffer.current_alias_mark_index - 1]
+        reset_overlay(ed_buffer)
         for mark_start, mark_end in marks_list:
-            apply_overlay(editor, mark_start, mark_end, editor.aliases_overlay)
+            apply_overlay(ed_buffer, mark_start, mark_end, ed_buffer.aliases_overlay)
 
 
 def on_move(hook_name, file_name, line, column):
@@ -204,17 +208,17 @@ def on_move(hook_name, file_name, line, column):
     Event handler on cursor move. Gets out of alias expansion mode
     when the cursor gets out of the zone.
     """
-    editor = EditorBuffer.get(file_name)
-    index = editor.current_alias_mark_index - 1
-    start_mark, end_mark = editor.alias_marks[index][0]
+    ed_buffer = EditorBuffer.get(file_name)
+    index = ed_buffer.current_alias_mark_index - 1
+    start_mark, end_mark = ed_buffer.alias_marks[index][0]
     start_loc = start_mark.location()
     end_loc = end_mark.location()
-    cursor_loc = editor.current_view().cursor()
+    cursor_loc = ed_buffer.current_view().cursor()
     if not (start_loc <= cursor_loc <= end_loc):
-        exit_alias_expand(editor)
+        exit_alias_expand(ed_buffer)
 
 
-def expand_alias(editor, alias):
+def expand_alias(ed_buffer, alias):
     """
     Expand given alias in the given editor buffer at the point where the cursor
     is.
@@ -224,74 +228,74 @@ def expand_alias(editor, alias):
               for s in subst_pattern.findall(alias.expansion)]
     alias_labels = defaultdict(list)
 
-    editor.aliases_overlay = editor.create_overlay("aliases_overlay")
-    editor.aliases_overlay.set_property(
+    ed_buffer.aliases_overlay = ed_buffer.create_overlay("aliases_overlay")
+    ed_buffer.aliases_overlay.set_property(
         "background", Preference(color_pref_name).get()
     )
 
     color, color1 = get_paragraph_color()
-    editor.aliases_background_overlay_1 = editor.create_overlay(
+    ed_buffer.aliases_background_overlay_1 = ed_buffer.create_overlay(
         "aliases_background_overlay_1"
     )
-    editor.aliases_background_overlay_1.set_property(
+    ed_buffer.aliases_background_overlay_1.set_property(
         "paragraph-background", color1
     )
-    editor.aliases_background_overlay = editor.create_overlay(
+    ed_buffer.aliases_background_overlay = ed_buffer.create_overlay(
         "aliases_background_overlay"
     )
-    editor.aliases_background_overlay.set_property(
+    ed_buffer.aliases_background_overlay.set_property(
         "paragraph-background", color
     )
 
-    editor.aliases_overlay_next = editor.create_overlay("aliases_overlay_next")
+    ed_buffer.aliases_overlay_next = ed_buffer.create_overlay("aliases_overlay_next")
     c1, c2 = get_comments_colors()
-    editor.aliases_overlay_next.set_property("foreground", c1)
+    ed_buffer.aliases_overlay_next.set_property("foreground", c1)
     if c2:
-        editor.aliases_overlay_next.set_property("background", "#124")
+        ed_buffer.aliases_overlay_next.set_property("background", "#124")
 
     # Create a mark with right gravity so it will stay at the end of what we
     # have inserted, giving us the current insert point
-    editor.alias_begin_mark = editor.current_view().cursor().create_mark()
-    editor.alias_end_mark = editor.current_view().cursor().create_mark(
+    ed_buffer.alias_begin_mark = ed_buffer.current_view().cursor().create_mark()
+    ed_buffer.alias_end_mark = ed_buffer.current_view().cursor().create_mark(
         left_gravity=False
     )
-    insert_mark = editor.alias_end_mark
+    insert_mark = ed_buffer.alias_end_mark
 
     for text, subst in izip_longest(text_chunks, substs):
-        editor.insert(insert_mark.location(), text)
+        ed_buffer.insert(insert_mark.location(), text)
         if subst:
             alias_labels[subst].append(insert_mark.location().create_mark())
 
-    editor.alias_marks = []
+    ed_buffer.alias_marks = []
     substs_set = set()
     for subst in substs:
         if subst not in substs_set and subst != "%_":
-            editor.alias_marks.append(
+            ed_buffer.alias_marks.append(
                 [(m, m.location().create_mark(left_gravity=False))
                  for m in alias_labels[subst]]
             )
             for m in alias_labels[subst]:
-                editor.insert(m.location(), "<{0}>".format(subst))
+                ed_buffer.insert(m.location(), "<{0}>".format(subst))
             substs_set.add(subst)
 
-    for marks_list in editor.alias_marks:
+    for marks_list in ed_buffer.alias_marks:
         for mark_start, mark_end in marks_list:
             apply_overlay(
-                editor, mark_start, mark_end, editor.aliases_overlay_next
+                ed_buffer, mark_start, mark_end, ed_buffer.aliases_overlay_next
             )
 
     if "%_" in alias_labels:
-        editor.last_alias_mark = alias_labels["%_"][0]
+        ed_buffer.last_alias_mark = alias_labels["%_"][0]
     else:
-        editor.last_alias_mark = None
+        ed_buffer.last_alias_mark = None
 
-    editor.current_alias_mark_index = 0
+    ed_buffer.current_alias_mark_index = 0
     Hook("character_added").add(on_edit)
     Hook("location_changed").add(on_move)
 
-    editor.indent(editor.alias_begin_mark.location(),
-                  editor.alias_end_mark.location())
-    toggle_next_field(editor)
+    ed_buffer.indent(ed_buffer.alias_begin_mark.location(),
+                  ed_buffer.alias_end_mark.location())
+    toggle_next_field(ed_buffer)
 
 EditorBuffer.expand_alias = expand_alias
 parse_xml(xml_conf)
