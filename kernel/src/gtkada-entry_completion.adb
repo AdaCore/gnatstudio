a0eb9fe050867d12910d31479a41b4166f302b45
Nicolas Setton <setton@adacore.com>
2013-09-18 17:27:40 +0000

    Revert style of the omnisearch popup. For M917-061.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@215904 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/gtkada-entry_completion.adb
+++ kernel/src/gtkada-entry_completion.adb
@@ -359,7 +359,7 @@ package body Gtkada.Entry_Completion is
             when Role_Provider =>
                Res := Convert (Get_Address (Child, Child_It, Column_Data));
                Set_String
-                 (Value, "<b>" & Res.Provider.Display_Name & "</b> ("
+                 (Value, Res.Provider.Display_Name & " ("
                   & Image (Res.Provider.Count, Min_Width => 0) & ")");
             when Role_To_Locations =>
                Set_String (Value,
@@ -542,16 +542,14 @@ package body Gtkada.Entry_Completion is
         (Render, "markup", Column_Provider);
 
       --  ??? Should onnect to preferences_changed for the color
-      Get_Style_Context (Self.View).Get_Color (Gtk_State_Flag_Normal, Color);
+      Get_Style_Context (Self.View).Get_Background_Color
+        (Gtk_State_Flag_Normal, Color);
       Set_Property
         (Render, Gtk.Cell_Renderer_Text.Foreground_Rgba_Property,
-         Shade_Or_Lighten (Color, 0.1));
-
-      --  ??? Setting a background on the provider column does not render
-      --  very well when the window is non-resizing, which is the default
---        Set_Property
---          (Render, Gtk.Cell_Renderer_Text.Background_Rgba_Property,
---           Shade_Or_Lighten (Color, 0.05));
+         Shade_Or_Lighten (Color, 0.3));
+      Set_Property
+        (Render, Gtk.Cell_Renderer_Text.Background_Rgba_Property,
+         Shade_Or_Lighten (Color, 0.05));
       Set_Property (Render, Gtk.Cell_Renderer.Xalign_Property, 1.0);
       Set_Property (Render, Gtk.Cell_Renderer.Yalign_Property, 0.0);
 
