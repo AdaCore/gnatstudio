09b61b82308f67152bdf07842f87296930de23e4
Jerome Lambourg <lambourg@adacore.com>
2013-10-31 14:43:39 +0000

    Refine the way OSX Application is created, and provide an installer for GPS.app. Continuing work on MA30-010
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@218367 936e1b1b-40f2-da11-902a-00137254ae57

--- osx_bundle/scripts/dodmg.sh
+++ osx_bundle/scripts/dodmg.sh
@@ -2,6 +2,7 @@
 
 APP=$1
 VERSION=$2
+PKG=${APP}-${VERSION}.pkg
 backgroundPictureName=logo.png
 
 usage() {
@@ -17,7 +18,7 @@ if [ x${VERSION} = x ]; then
   usage
 fi
 
-SIZE=`du -k -d 0 ${APP}.app | sed -e 's/\([0-9]*\).*/\1/'`
+SIZE=`du -k -d 0 ${PKG} | sed -e 's/\([0-9]*\).*/\1/'`
 SIZE_SRCS=`du -k -d 0 srcs | sed -e 's/\([0-9]*\).*/\1/'`
 let SIZE=${SIZE}+${SIZE_SRCS}
 # Add some more space for extra folder metadata, and potential rounding issues
@@ -27,7 +28,7 @@ rm -f pack.temp.dmg
 rm -f ${APP}-${VERSION}.dmg
 
 echo "=== Creating temporary dmg file of ${SIZE} ko"
-hdiutil create -srcfolder "${APP}.app" -volname "${APP} ${VERSION}" -fs HFS+ \
+hdiutil create -srcfolder "${PKG}" -volname "${APP} ${VERSION}" -fs HFS+ \
  -fsargs "-c c=64,a=16,e=16" -format UDRW -size ${SIZE}k pack.temp.dmg
 echo "=== Mount the dmg file"
 device=$(hdiutil attach -readwrite -noverify -noautoopen "pack.temp.dmg" | \
@@ -56,9 +57,9 @@ echo '
        set arrangement of theViewOptions to not arranged
        set icon size of theViewOptions to 72
        set background picture of theViewOptions to file ".bg:'${backgroundPictureName}'"
-       make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
-       set position of item "'${APP}'.app" of container window to {100, 170}
-       set position of item "Applications" of container window to {275, 170}
+       --  make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
+       set position of item "'${PKG}'" of container window to {100, 170}
+       --  set position of item "Applications" of container window to {275, 170}
        update without registering applications
        close
        open
