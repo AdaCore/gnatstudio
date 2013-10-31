215fba3bea46bf868d32d9a986feb6125954660a
Jerome Lambourg <lambourg@adacore.com>
2013-10-31 14:58:28 +0000

    Remove rsh from the list of supported remote access tools. Move it to a separate optional module stating that its use is deprecated. For JC07-006
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@218368 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ share/library/remote_rsh.py
@@ -0,0 +1,37 @@
+"""
+Adds support for rsh when using GPS remote.
+
+The GPS remote facility (/Tools/View/Remote) allows you to easily
+work (eg build and compile) on remote hosts. The communication with
+these remote hosts can be done through a number of protocols. This package
+adds support for the rsh protocol.
+
+Please note that use of rsh is now deprecated on most systems, and may cause
+connection issues or unterminated remote executions with the GPS remote mode.
+
+Consider such use as obsolete and not recommended.
+"""
+
+
+
+import GPS
+
+XML = r"""<?xml version="1.0"?>
+<GPS>
+
+  <!-- RSH -->
+
+  <!-- rlogin ignores the start command. We use rlogin instead of rsh to make
+       sure under Windows that the Microsoft rsh version is not used. The GNU
+       rsh provides directly the rlogin tool that we want to use anyway.
+    -->
+  <remote_connection_config name="rsh">
+    <start_command use_pipes="false">rlogin</start_command>
+    <start_command_common_args>%U %h</start_command_common_args>
+    <start_command_user_args>-l %u</start_command_user_args>
+  </remote_connection_config>
+
+</GPS>
+"""
+
+GPS.parse_xml(XML)
