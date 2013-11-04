831c55b806363ce482b0c58b896b033b950a8ed4
Raphael Amiard <amiard@adacore.com>
2013-11-04 11:31:44 +0000

    Fix for MA18-024 : Color of error messages don't change before you restart GPS.
    * GPS.Kernel.Messages.Tools_Output (Add_Tool_Message): Don't create a
      copy of the style for each error message.
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@218451 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src/gps-kernel-messages-tools_output.adb
+++ kernel/src/gps-kernel-messages-tools_output.adb
@@ -162,11 +162,7 @@ package body GPS.Kernel.Messages.Tools_Output is
             Returned := Primary;
 
             if Highlight_Category /= null then
-               Highlight_Style :=
-                 Get_Or_Create_Style_Copy
-                   (Kernel_Handle (Container.Kernel),
-                    Get_Name (Highlight_Category) & '/' & Category,
-                    Highlight_Category);
+               Highlight_Style := Highlight_Category;
 
                if Length = 0 then
                   Primary.Set_Highlighting (Highlight_Style);
