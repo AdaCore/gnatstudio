ba913b1658cd80e59fa1ba4c1c61f4a809452348
Emmanuel Briot <briot@adacore.com>
2013-11-07 16:30:28 +0000

    Fix shortcuts in emacs mode, so that they reference actions rather than  menus. This works better when user can start modifying menus through  menus.xml This also fixes isearch.py, where pressing ctrl-s while already in  isearch mode would close the command window because the action  'isearch' was not associated with ctrl-s (but the menu was). MB07-015
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@218646 936e1b1b-40f2-da11-902a-00137254ae57

--- share/library/emacs.py
+++ share/library/emacs.py
@@ -28,7 +28,7 @@ XML = r"""<?xml version="1.0" ?>
 <GPS>
   <!--  disable GPS key shortcuts which conflict with emacs shortcuts -->
 
-  <key action="/Edit/Smart Completion" />
+  <key action="complete identifier (advanced)" />
 
   <key action="">control-x</key>
   <key action="">control-c</key>
@@ -38,85 +38,83 @@ XML = r"""<?xml version="1.0" ?>
   <key action="subprogram box">control-c n</key>
   <key action="kill line">control-k</key>
   <key action="kill forward">control-d</key>
-  <key action="Delete word forward">%(alt)s-d</key>
+  <key action="delete word forward">%(alt)s-d</key>
   <key action="transpose chars">control-t</key>
-  <key action="Transpose lines">control-x control-t</key>
-  <key action="/File/Save">control-x control-s</key>
-  <key action="/File/Save As..." >control-x control-w</key>
+  <key action="transpose lines">control-x control-t</key>
+  <key action="save">control-x control-s</key>
+  <key action="save as" >control-x control-w</key>
   <key action="goto beginning of line">control-a</key>
   <key action="goto end of line">control-e</key>
   <key action="goto beginning of buffer">%(alt)s-less</key>
   <key action="goto end of buffer">%(alt)s-shift-greater</key>
-  <key action="Format selection" >tab</key>
-  <key action="/File/Open...">control-x control-f</key>
-  <key action="/Edit/Undo">control-_</key>
-  <key action="/Edit/Refill">%(alt)s-q</key>
+  <key action="format selection" >tab</key>
+  <key action="open file">control-x control-f</key>
+  <key action="refill">%(alt)s-q</key>
   <key action="repeat next">control-u</key>
 
   <key action="goto declaration or body">control-c control-d</key>
   <key action="goto other file">control-c o</key>
 
-  <key action="/Navigate/Back">control-c control-s</key>
+  <key action="backward locations history">control-c control-s</key>
   <key action="open line">control-o</key>
-  <key action="Join line">%(alt)s-j</key>
+  <key action="join line">%(alt)s-j</key>
   <key action="Upper case word">%(alt)s-u</key>
   <key action="Lower case word">%(alt)s-l</key>
   <key action="Capitalize word">%(alt)s-c</key>
-  <key action="/Navigate/Goto Line...">%(alt)s-g</key>
+  <key action="goto line">%(alt)s-g</key>
   <key action='zap to char'>%(alt)s-z</key>
   <key action='just one space'>%(alt)s-space</key>
 
   <key action="set mark command">control-space</key>
-  <key action="Cancel mark command">control-g</key>
+  <key action="cancel mark command">control-g</key>
 
-  <key action="Global Search in context: actions">%(alt)s-x</key>
-  <key action="Global Search in context: opened windows">control-x b</key>
+  <key action="global search in context: actions">%(alt)s-x</key>
+  <key action="global search in context: opened windows">control-x b</key>
 
-  <key action="/Edit/Paste">control-y</key>
-  <key action="/Edit/Paste Previous">%(alt)s-y</key>
-  <key action="/Edit/Copy">%(alt)s-w</key>
-  <key action="/Edit/Cut">control-w</key>
+  <key action="paste from clipboard">control-y</key>
+  <key action="paste previous from clipboard">%(alt)s-y</key>
+  <key action="copy to clipboard">%(alt)s-w</key>
+  <key action="cut to clipboard">control-w</key>
 
-  <key action="Macro Start Keyboard">control-x parenleft</key>
-  <key action="Macro Stop">control-x parenright</key>
-  <key action="Macro Play">control-x e</key>
+  <key action="macro start Keyboard">control-x parenleft</key>
+  <key action="macro stop">control-x parenright</key>
+  <key action="macro play">control-x e</key>
 
-  <key action="/Edit/Undo">shift-control-underscore</key>
+  <key action="undo">control-underscore</key>
+  <key action="redo">shift-control-underscore</key>
 
-  <key action="/File/Close">control-x k</key>
+  <key action="close current window">control-x k</key>
 
-  <key action="/File/Exit">control-x control-c</key>
-
-  <key action="/Navigate/Find or Replace...">control-s</key>
+  <key action="exit">control-x control-c</key>
 
   <key action="delete horizontal space">%(alt)s-backslash</key>
 
-  <key action="New View Vertical">control-x 2</key>
-  <key action="New View Horizontal">control-x 3</key>
+  <key action="new view vertical">control-x 2</key>
+  <key action="new view horizontal">control-x 3</key>
 
-  <key action="Move to next window">control-x control-b</key>
-  <key action="Move to previous window" />
+  <key action="move to next window">control-x control-b</key>
+  <key action="move to previous window" />
 
-  <key action="Select other window">control-x o</key>
+  <key action="select other window">control-x o</key>
 
-  <key action="Center cursor on screen">control-l</key>
+  <key action="center cursor on screen">control-l</key>
 
-  <key action="Delete word backward">%(alt)s-BackSpace</key>
+  <key action="delete word backward">%(alt)s-BackSpace</key>
 
-  <key action="Move to previous word">%(alt)s-Left</key>
-  <key action="Move to next word">%(alt)s-Right</key>
-  <key action="Move to previous page">%(alt)s-v</key>
-  <key action="Move to next page">control-v</key>
+  <key action="move to previous word">%(alt)s-Left</key>
+  <key action="move to next word">%(alt)s-Right</key>
+  <key action="move to previous page">%(alt)s-v</key>
+  <key action="move to next page">control-v</key>
 
-  <key action="/Edit/Rectangle/Cut">control-x r k</key>
-  <key action="/Edit/Rectangle/Paste">control-x r y</key>
-  <key action="/Edit/Rectangle/Delete">control-x r d</key>
-  <key action="/Edit/Rectangle/Clear">control-x r c</key>
-  <key action="/Edit/Rectangle/Open">control-x r o</key>
-  <key action="/Edit/Rectangle/Replace with Text">control-x r t</key>
+  <key action="rectangle_cut">control-x r k</key>
+  <key action="rectangle_paste">control-x r y</key>
+  <key action="rectangle_delete">control-x r d</key>
+  <key action="rectangle_clear">control-x r c</key>
+  <key action="rectangle_open">control-x r o</key>
+  <key action="rectangle_string">control-x r t</key>
 
-  <key action="/Navigate/Find Incremental">control-s</key>
-  <key action="/Navigate/Find Previous Incremental">control-r</key>
+  <key action="isearch">control-s</key>
+  <key action="isearch backward">control-r</key>
 </GPS>
 """ % {"alt": alt}
 
