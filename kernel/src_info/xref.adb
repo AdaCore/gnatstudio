ad55e9e4982421670e609e6a5ece9f6ff8d392a5
Raphael Amiard <amiard@adacore.com>
2013-11-14 15:01:55 +0000

    GPS: Implement MB13-030 - Adding fallback by name using constructs in GPS.Xref, so that Get_Entity will retrieve a valid entity by name if it is unambiguous, even in the case it is not in the lexical closure of the current file.
    * GPS.Xref
        (Node_From_Entity): Add a name parameter. If this parameter is not
        the empty string, then global search by name on the constructs
        database if no result is found via Find_Declaration. If there are
        more than one result, dont return anything
    
        (Find_Declaration_Or_Overloaded): Pass the name parameter to
        Node_From_Entity
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@219431 936e1b1b-40f2-da11-902a-00137254ae57

--- kernel/src_info/xref.adb
+++ kernel/src_info/xref.adb
@@ -65,8 +65,11 @@ package body Xref is
       Handler     : access Abstract_Language_Handler_Record'Class;
       Decl        : General_Location;
       Ent         : out Entity_Access;
-      Tree_Lang   : out Tree_Language_Access);
-   --  Returns the constructs data for a given entity.
+      Tree_Lang   : out Tree_Language_Access;
+      Name        : String := "");
+   --  Returns the constructs data for a given entity. Name is optional. If it
+   --  is given, this will perform a search by name in the construct database.
+   --  If the result is unique, then it will return it.
 
    function Construct_From_Entity
      (Self : access General_Xref_Database_Record'Class;
@@ -637,7 +640,8 @@ package body Xref is
                 Handler   => Self.Lang_Handler,
                 Decl      => Loc,
                 Ent       => Result,
-                Tree_Lang => Tree_Lang);
+                Tree_Lang => Tree_Lang,
+                Name      => Entity_Name);
 
             if Result /= Null_Entity_Access
                and then
@@ -1242,7 +1246,8 @@ package body Xref is
       Handler     : access Abstract_Language_Handler_Record'Class;
       Decl        : General_Location;
       Ent         : out Entity_Access;
-      Tree_Lang   : out Tree_Language_Access)
+      Tree_Lang   : out Tree_Language_Access;
+      Name        : String := "")
    is
       Data_File   : Structured_File_Access;
    begin
@@ -1264,6 +1269,24 @@ package body Xref is
             (Data_File, Decl.Line,
              To_Line_String_Index (Data_File, Decl.Line, Decl.Column));
       end if;
+
+      if Ent = Null_Entity_Access
+        and then Name /= ""
+      then
+         declare
+            It : Construct_Db_Iterator := Self.Constructs.Start (Name, True);
+         begin
+            Ent := Get (It);
+            Next (It);
+
+            --  Return a null result if there is more than one result
+
+            if not At_End (It) then
+               Ent := Null_Entity_Access;
+            end if;
+         end;
+      end if;
+
    end Node_From_Entity;
 
    ---------------------------
