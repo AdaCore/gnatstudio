a16a888e16600ca2a4a06f25512e1ad7a47f2f9e
Emmanuel Briot <briot@adacore.com>
2013-11-19 14:48:35 +0000

    (gnatr.py): rewritten to use a build target rather than XML (MA23-035) Move the menu definition in menus.xml so that we can better control its  location.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@219598 936e1b1b-40f2-da11-902a-00137254ae57

--- share/library/gnatr.py
+++ share/library/gnatr.py
@@ -9,27 +9,67 @@ the file with -gnatR.
 
 
 import GPS
+import gps_utils
+from tool_output import OutputParser
+
+
+class OutputStore(OutputParser):
+    def __init__(self, child):
+        super(OutputStore, self).__init__(child)
+        self.output = ""
+
+    def on_stdout(self, text, command):
+        self.output += text
+        if self.child:
+            self.child.on_stdout(text, command)
+
+    def on_exit(self, status, command):
+        if self.child:
+            self.child.on_exit(status, command)
+
+        buffer = GPS.EditorBuffer.get_new()
+        buffer.insert(buffer.beginning_of_buffer(), self.output)
+        buffer.set_read_only(False)
+
+        # ??? How to get access to the file name ?
+        GPS.MDI.get_by_child(buffer.current_view()).rename('Type layout')
+
 
 XML = r"""<?xml version="1.0" ?>
-<gnatr>
-   <action name="View types layout in file" output="none" category="Builder" >
-      <external>gnatmake -f -gnatc -P%pp -gnatR -u %F</external>
-      <shell>base_name "%F"</shell>
-      <shell>dump_file "%2" "type_layout_%1"</shell>
-      <shell>Editor.edit "%1"</shell>
-      <shell>Editor.set_title "%2" "Type layout in %3"</shell>
-      <shell>Editor.set_writable "%3" FALSE</shell>
-      <shell>delete "%4"</shell>
-      <filter id="Source editor" />
-   </action>
-
-   <Submenu>
-      <Title>Build</Title>
-      <menu action="View types layout in file">
-         <Title>View types layout in file</Title>
-      </menu>
-   </Submenu>
-</gnatr>
+<GPS>
+   <target-model name='gnatR' category='File'>
+      <description>Launches GNAT to generate information on the layout of types (using -gnatR)</description>
+      <icon>gps-custom-build</icon>
+      <server>Tools_Server</server>
+   </target-model>
+   <target name='Compute Types Layout' category='File' model='gnatR'>
+      <launch-mode>MANUALLY_WITH_NO_DIALOG</launch-mode>
+      <in-menu>FALSE</in-menu>
+      <in-toolbar>FALSE</in-toolbar>
+      <command-line>
+         <arg>%gnatmake</arg>
+         <arg>-q</arg>
+         <arg>-f</arg>
+         <arg>-c</arg>
+         <arg>-gnatR</arg>
+         <arg>-u</arg>
+         <arg>-P%PP</arg>
+         <arg>%eL</arg>
+         <arg>%X</arg>
+         <arg>%fp</arg>
+      </command-line>
+      <output-parsers>outputstore</output-parsers>
+   </target>
+</GPS>
 """
-
 GPS.parse_xml(XML)
+
+
+@gps_utils.interactive(
+    name='view types layout in file',
+    filter='Source editor',
+    category='Builder')
+def view_types_layout():
+    context = GPS.current_context()
+    f = context.file()
+    GPS.BuildTarget('Compute Types Layout').execute(file=f)
