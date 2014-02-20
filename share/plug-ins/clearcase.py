b7f611d33a4e85d46243b0c0e1d6497ca4a91747
Nicolas Setton <setton@adacore.com>
2014-02-20 21:56:44 +0000

    Implement "diff_working". For N116-013.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@223659 936e1b1b-40f2-da11-902a-00137254ae57

--- share/plug-ins/clearcase.py
+++ share/plug-ins/clearcase.py
@@ -7,67 +7,67 @@ customize the cleartool commands that are sent for each of the menus.
 """
 
 
-
 import GPS
 
 from vcs import *
 
 actions = [
- SEPARATOR,
+    SEPARATOR,
 
- { ACTION: "Status", LABEL: "Describe"  },
- { ACTION: "Update", LABEL: "Update"  },
- { ACTION: "Commit", LABEL: "Check in"  },
- { ACTION: "Commit (via revision log)", LABEL: "Check in (via revision log)"  },
+    { ACTION: "Status", LABEL: "Describe"  },
+    { ACTION: "Update", LABEL: "Update"  },
+    { ACTION: "Commit", LABEL: "Check in"  },
+    { ACTION: "Commit (via revision log)", LABEL: "Check in (via revision log)"  },
 
- SEPARATOR,
+    SEPARATOR,
 
- { ACTION: "Open",   LABEL: "Check out" },
- { ACTION: "History (as text)",          LABEL: "View _entire revision history (as text)" },
- { ACTION: "History",                    LABEL: "View _entire revision history" },
- { ACTION: "History for revision",       LABEL: "View specific revision _history"  },
+    { ACTION: "Open",   LABEL: "Check out" },
+    { ACTION: "History (as text)",          LABEL: "View _entire revision history (as text)" },
+    { ACTION: "History",                    LABEL: "View _entire revision history" },
+    { ACTION: "History for revision",       LABEL: "View specific revision _history"  },
 
- SEPARATOR,
+    SEPARATOR,
 
- { ACTION: "Diff against head",          LABEL: "Compare against head revision"  },
- { ACTION: "Diff against revision",      LABEL: "Compare against specific revision" },
- { ACTION: "Diff between two revisions", LABEL: "Compare two revisions" },
- { ACTION: "Diff base against head",     LABEL: "Compare base against head" },
+    { ACTION: "Diff against head",          LABEL: "Compare against head revision"  },
+    { ACTION: "Diff against base",          LABEL: "Compare against working revision"  },
+    { ACTION: "Diff against revision",      LABEL: "Compare against specific revision" },
+    { ACTION: "Diff between two revisions", LABEL: "Compare two revisions" },
+    { ACTION: "Diff base against head",     LABEL: "Compare base against head" },
 
- SEPARATOR,
+    SEPARATOR,
 
- { ACTION: "Annotate",                LABEL: "Annotations"  },
- { ACTION: "Remove Annotate",         LABEL: "Remove annotations"  },
- { ACTION: "Edit revision log",       LABEL: "Edit revision log"  },
- { ACTION: "Edit global ChangeLog",   LABEL: "Edit global ChangeLog"  },
- { ACTION: "Remove revision log",     LABEL: "Remove revision log"  },
+    { ACTION: "Annotate",                LABEL: "Annotations"  },
+    { ACTION: "Remove Annotate",         LABEL: "Remove annotations"  },
+    { ACTION: "Edit revision log",       LABEL: "Edit revision log"  },
+    { ACTION: "Edit global ChangeLog",   LABEL: "Edit global ChangeLog"  },
+    { ACTION: "Remove revision log",     LABEL: "Remove revision log"  },
 
- SEPARATOR,
+    SEPARATOR,
 
- { ACTION: "Add",               LABEL: "Add"  },
- { ACTION: "Add (via revision log)",               LABEL: "Add (via revision log)"  },
- { ACTION: "Add no commit",     LABEL: "Add, no commit"  },
- { ACTION: "Remove",            LABEL: "Remove"  },
- { ACTION: "Remove (via revision log)",            LABEL: "Remove (via revision log)"  },
- { ACTION: "Remove no commit",  LABEL: "Remove, no commit"  },
- { ACTION: "Revert",            LABEL: "Cancel checkout"  },
+    { ACTION: "Add",               LABEL: "Add"  },
+    { ACTION: "Add (via revision log)",               LABEL: "Add (via revision log)"  },
+    { ACTION: "Add no commit",     LABEL: "Add, no commit"  },
+    { ACTION: "Remove",            LABEL: "Remove"  },
+    { ACTION: "Remove (via revision log)",            LABEL: "Remove (via revision log)"  },
+    { ACTION: "Remove no commit",  LABEL: "Remove, no commit"  },
+    { ACTION: "Revert",            LABEL: "Cancel checkout"  },
 
- SEPARATOR,
+    SEPARATOR,
 
- { ACTION: "Add directory, no commit",     LABEL: "Directory/Add directory, no commit"  },
- { ACTION: "Remove directory, no commit",  LABEL: "Directory/Remove directory, no commit"  },
- { ACTION: "Commit directory",             LABEL: "Directory/Commit directory"  },
- { ACTION: "Status dir",                   LABEL: "Directory/Query status for directory"  },
- { ACTION: "Update dir",                   LABEL: "Directory/Update directory"  },
- { ACTION: "Status dir (recursively)",     LABEL: "Directory/Query status for directory recursively"  },
- { ACTION: "Update dir (recursively)",     LABEL: "Directory/Update directory recursively"  },
+    { ACTION: "Add directory, no commit",     LABEL: "Directory/Add directory, no commit"  },
+    { ACTION: "Remove directory, no commit",  LABEL: "Directory/Remove directory, no commit"  },
+    { ACTION: "Commit directory",             LABEL: "Directory/Commit directory"  },
+    { ACTION: "Status dir",                   LABEL: "Directory/Query status for directory"  },
+    { ACTION: "Update dir",                   LABEL: "Directory/Update directory"  },
+    { ACTION: "Status dir (recursively)",     LABEL: "Directory/Query status for directory recursively"  },
+    { ACTION: "Update dir (recursively)",     LABEL: "Directory/Update directory recursively"  },
 
- { ACTION: "List project",                 LABEL: "Project/List files in project"  },
- { ACTION: "Status project",               LABEL: "Project/Query status for project"  },
- { ACTION: "Update project",               LABEL: "Project/Update project"  },
- { ACTION: "List project (recursively)",   LABEL: "Project/List files in project (recursively)"  },
- { ACTION: "Status project (recursively)", LABEL: "Project/Query status for project (recursively)"  },
- { ACTION: "Update project (recursively)", LABEL: "Project/Update project (recursively)"  },
+    { ACTION: "List project",                 LABEL: "Project/List files in project"  },
+    { ACTION: "Status project",               LABEL: "Project/Query status for project"  },
+    { ACTION: "Update project",               LABEL: "Project/Update project"  },
+    { ACTION: "List project (recursively)",   LABEL: "Project/List files in project (recursively)"  },
+    { ACTION: "Status project (recursively)", LABEL: "Project/Query status for project (recursively)"  },
+    { ACTION: "Update project (recursively)", LABEL: "Project/Update project (recursively)"  },
 ]
 
 XML = r"""<?xml version="1.0" ?>
@@ -165,6 +165,20 @@ XML = r"""<?xml version="1.0" ?>
         <shell>File $1</shell>
         <shell>Hook "diff_action_hook"</shell>
         <shell>Hook.run %1 "$1" null %2 %3 "%5 [LATEST]"</shell>
+        <shell>delete %5</shell>
+      </on-failure>
+      <shell output="">echo "No differences found."</shell>
+   </action>
+
+   <action name="clearcase_diff_working" show-command="true" output="none" category="">
+      <external>cleartool diff -diff_format -predecessor "$1"</external>
+      <on-failure>
+        <shell>base_name "$1"</shell>
+        <shell>dump "%2" TRUE</shell>
+        <shell>File %1</shell>
+        <shell>File $1</shell>
+        <shell>Hook "diff_action_hook"</shell>
+        <shell>Hook.run %1 "$1" null %2 %3 "%5 [WORKING]"</shell>
         <shell>delete %5</shell>
       </on-failure>
       <shell output="">echo "No differences found."</shell>
@@ -272,6 +286,7 @@ XML = r"""<?xml version="1.0" ?>
       <revert           action="clearcase_uncheckout"       label="Cancel checkout"/>
       <diff_patch       action="clearcase_diff_patch"       label="Compare against head revision for building a patch file"/>
       <diff_head        action="clearcase_diff_head"        label="Compare against latest"/>
+      <diff_working     action="clearcase_diff_working"     label="Compare against working"/>
       <diff             action="clearcase_diff"             label="Compare against other rev."/>
 
       <status label="Not checked out" stock="gps-vcs-up-to-date" />
