3f5a53c37cb6498c9c052fd13a41f3d2621b29b9
Arnaud Charlet <charlet@adacore.com>
2014-03-22 10:32:35 +0000

    Remove, no longer used. Part of N219-023
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@224942 936e1b1b-40f2-da11-902a-00137254ae57

--- tools/project_converter/convert-gpr.ads
+++
@@ -1,9 +0,0 @@
-
---  This package converts from .adp projects to .gpr projects
-
-package Convert.Gpr is
-
-   procedure Convert_From_Gpr_To_Adp (Gpr_Filename : String);
-   --  Convert project is sent to standard output
-
-end Convert.Gpr;
--- tools/project_converter/convert.ads
+++
@@ -1,3 +0,0 @@
-package Convert is
-
-end Convert;
