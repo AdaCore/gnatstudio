e92134aaef3b77b5917e684815cabfbcf217c167
Arnaud Charlet <charlet@adacore.com>
2014-03-27 08:15:41 +0000

    Remove language dir, no longer tested and likely no longer in sync with language.ads
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@225172 936e1b1b-40f2-da11-902a-00137254ae57

--- examples/language/gpr_custom.ads
+++
@@ -1,9 +0,0 @@
-with Interfaces.C.Strings; use Interfaces.C.Strings;
-
-package GPR_Custom is
-
-   function Comment_Line
-     (Line : String; Comment : Boolean; Reserved : Integer) return chars_ptr;
-   pragma Export (Ada, Comment_Line, "gpr_comment_line");
-
-end GPR_Custom;
