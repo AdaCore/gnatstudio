548f536738c7ec75de15f4da09a31fed5337ef5f
Hristian Kirtchev <kirtchev@adacore.com>
2014-04-10 17:50:41 +0000

    * modeling.py Add Matlab and Simulink language definitions. Add project-related definitions. (Setup): Languages and project attributes are now registered on plug-in setup.
    N319-035
    
    [GPS] Simulink code generation from GPS
    
    This patch add support for GMC project attributes and switches.
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@226007 936e1b1b-40f2-da11-902a-00137254ae57

--- share/plug-ins/modeling.py
+++ share/plug-ins/modeling.py
@@ -1,6 +1,6 @@
 """
-This plug-in adds support for the GMC compiler, generating
-code from Simulink models.
+This plug-in adds support for GMC (the GNAT Modeling Compiler) which generates
+Ada (SPARK 2014?) and C code from Simulink models.
 
 =========================================
 THIS IS WORK IN PROGRESS
@@ -13,66 +13,154 @@ you open a .mdl file, for instance from the Project view.
 
 """
 
-import modules
 import GPS
 import GPS.Browsers
+import modules
 import os.path
+import os_utils
+
+# Constants
+
+gmc_name = 'gmc'
+gmc_exec = os_utils.locate_exec_on_path(gmc_name)
+
+# Language definitions
+
+matlab_def = """<?xml version='1.0' ?>
+  <GPS>
+    <Language>
+      <Name>Matlab</Name>
+      <Body_Suffix>.m</Body_Suffix>
+      <Obj_Suffix>-</Obj_Suffix>
+    </Language>
+  </GPS>"""
+
+simulink_def = """<?xml version='1.0' ?>
+  <GPS>
+    <Language>
+      <Name>Simulink</Name>
+      <Body_Suffix>.mdl</Body_Suffix>
+      <Obj_Suffix>-</Obj_Suffix>
+    </Language>
+  </GPS>"""
+
+# Project-related definitions
+
+project_attributes = """<?xml version='1.0' ?>
+  <GPS>
+    <project_attribute
+     package="GMC"
+     name="Root_Models"
+     editor_page="GMC"
+     label="Root models"
+     description="Ask Matteo 1"
+     hide_in="wizard library_wizard">
+       <string type="file" filter="project"/>
+    </project_attribute>
+
+    <project_attribute
+     package="GMC"
+     name="Output_Dir"
+     editor_page="GMC"
+     label="Output directory"
+     description="Ask Matteo 2"
+     hide_in="wizard library_wizard">
+       <string type="directory"/>
+    </project_attribute>
+  </GPS>"""
+
+project_switches = """<?xml version='1.0' ?>
+  <GPS>
+    <tool
+     name="GMC"
+     package="GMC"
+     index="Simulink">
+      <language>Simulink</language>
+      <initial-cmd-line>-c -l ada</initial-cmd-line>
+      <switches lines="3">
+        <title line="1">Output</title>
+        <title line="2">Generation</title>
+        <title line="3">Files</title>
+
+        <radio line="1">
+          <radio-entry
+           label="Delete"
+           switch="-c"
+           tip="Delete contents of output directory between compilations"/>
+          <radio-entry
+           label="Preserve"
+           switch="-i"
+           tip="Preserve contents of output directory between compilations"/>
+        </radio>
+
+        <combo
+         line="2"
+         label="Target language"
+         switch="-l"
+         separator=" "
+         tip="Ask Matteo 3">
+           <combo-entry label="Ada" value="ada"/>
+           <combo-entry label="C" value="c"/>
+        </combo>
+
+        <field
+         line="3"
+         label="Matlab file"
+         switch="-m"
+         separator=" "
+         as-file="true"
+         tip="Ask Matteo 4"/>
+        <field
+         line="3"
+         label="Decoration file"
+         switch="-t"
+         separator=" "
+         as-file="true"
+         tip="Ask Matteo 5"/>
+        <field
+         line="3"
+         label="Reference file"
+         switch="-b"
+         separator=" "
+         as-file="true"
+         tip="Ask Matteo 6"/>
+      </switches>
+    </tool>
+  </GPS>"""
+
+# GMC plug-in module
 
 
 class GMC_Module(modules.Module):
-    def setup(self):
-        GPS.Hook('open_file_action_hook').add(
-            self.__on_open_file_action_hook, last=False)
 
-    def open_file(self, file):
-        """
-        Open a .mdl file as a browser, and puts it in the MDI.
-        :param GPS.File file: the file to open
-        :return: The created GPS.Browsers.View
-        """
-        diagrams = GPS.Browsers.Diagram.load_json(file.name())
-        v = GPS.Browsers.View.create(
-            diagrams[0],
-            title=os.path.basename(file.name()),
-            save_desktop=self._save_desktop)
-        v.file = file
-        v.set_background(
-            GPS.Browsers.View.Background.GRID,
-            GPS.Browsers.Style(stroke="rgba(200,200,200,0.8)"))
-        v.scale_to_fit(max_scale=1.0)
-        return v
+    # Handle an "open file" event
 
     def __on_open_file_action_hook(self, hook, file, *args):
-        """Called for open_file_action_hook"""
-
         if file.language() == 'simulink':
-            v = self.open_file(file)
+            diagrams = GPS.Browsers.Diagram.load_json(file.name())
+            v = GPS.Browsers.View.create(
+                diagrams[0], title=os.path.basename(file.name()))
+            v.set_background(
+                GPS.Browsers.View.Background.GRID,
+                GPS.Browsers.Style(
+                    stroke="rgba(200,200,200,0.8)"))
+            v.scale_to_fit(max_scale=2.0)
+
             return True
+
         return False
 
-    def save_desktop(self, child):
-        # ??? Should save position and scaling factor too
-        view = child.get_child()
-        return view.file.name()
+    # Setup the module only when the GMC executable is available on the path.
+    # This action registeres the Matlab and Simulink languages along with the
+    # various project attributes.
 
-    def load_desktop(self, view, data):
-        v = self.open_file(GPS.File(data))
-        return GPS.MDI.get_by_child(v)
+    def setup(self):
+        if gmc_exec:
+            GPS.parse_xml(matlab_def)
+            GPS.parse_xml(simulink_def)
 
-# Need to load the new language definition right away, since GPS loads
-# the project before setting up the module, so the extensions would not
-# be known otherwise
+            GPS.parse_xml(project_attributes)
+            GPS.parse_xml(project_switches)
 
-GPS.parse_xml(r"""<?xml version='1.0' ?>
-<GPS>
-   <Language>
-       <Name>Simulink</Name>
-       <Body_Suffix>.mdl</Body_Suffix>
-       <Obj_Suffix>-</Obj_Suffix>
-   </Language>
-   <Language>
-       <Name>Matlab</Name>
-       <Body_Suffix>.m</Body_Suffix>
-       <Obj_Suffix>-</Obj_Suffix>
-   </Language>
-</GPS>""")
+            GPS.Hook('open_file_action_hook').add(
+                self.__on_open_file_action_hook, last=False)
