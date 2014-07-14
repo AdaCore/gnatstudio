7fc1b3417d8de548fb669de21ea8460aee7f6ec6
Qinhua Yan <yan@adacore.com>
2014-07-14 15:16:35 +0000

    * promises.py   - add wrapper of debugger: class DebuggerWrapper
    * rename workflow_driver to workflow.py
    
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@230437 936e1b1b-40f2-da11-902a-00137254ae57

--- share/support/core/gps_utils/promises.py
+++ share/support/core/gps_utils/promises.py
@@ -61,15 +61,14 @@ class ProcessWrapper():
             cmd += (i+" ")
         cmd = cmd.rstrip(" ")
 
-        self._final_promise = None
-        self._current_promise = None
-        self._current_pattern = None
-        self._output = ""
+        self.__final_promise = None
+        self.__current_promise = None
+        self.__current_pattern = None
+        self.__output = ""
         self.finished = False
-        self.matched = False
-        self._process = GPS.Process(cmd, ".+",
-                                    on_match=self.__on_match,
-                                    on_exit=self.__on_exit)
+        self.__process = GPS.Process(cmd, ".+",
+                                     on_match=self.__on_match,
+                                     on_exit=self.__on_exit)
 
     def __on_match(self, process, match, unmatch):
         """
@@ -77,32 +76,32 @@ class ProcessWrapper():
         """
         # Update all output returned by the process
         # and store it as a private buffer
-        self._output += match + unmatch
+        self.__output += match + unmatch
 
         # check if user has issued some pattern to match
-        if self._current_pattern is not None:
-            p = re.search(self._current_pattern, self._output)
+        if self.__current_pattern is not None:
+            p = re.search(self.__current_pattern, self.__output)
             # if the pattern is found, update the output to remaining and
             # answer the promise with True->found it
             if p is not None:
-                self.matched = True
-                self._output = self._output[p.span()[1]::]
-                self._current_promise.resolve(self.matched)
+                self.__output = self.__output[p.span()[1]::]
+                self.__current_promise.resolve(True)
 
     def __on_exit(self, process, status, remaining_output):
         """
            Call by GPS when the process is finished.
+           Final_promise will be solved with status
+           Current_promise will be solved with False
         """
         # check if there's unanswered match promises
-        # if there is, that means the pattern is never found,
-        # so answer it with fail
-        if self._current_promise is not None:
-            self._current_promise.resolve(False)
+        # if there is --> the pattern is never found, answer it with False
+        if self.__current_promise is not None:
+            self.__current_promise.resolve(False)
 
         # check if I had made a promise to finish the process
         # if there is, answer that promise with whatever the result is
-        if self._final_promise is not None:
-            self._final_promise.resolve(status)
+        if self.__final_promise is not None:
+            self.__final_promise.resolve(status)
 
         # mark my process as finished
         self.finished = True
@@ -114,14 +113,13 @@ class ProcessWrapper():
         """
 
         # keep the pattern info and return my promise
-        self._current_pattern = pattern
-        self._current_promise = Promise()
-        self.matched = False
+        self.__current_pattern = pattern
+        self.__current_promise = Promise()
 
         # if user set up a timeout, set up: close output check after timeout
         if timeout > 0:
             x = GPS.Timeout(timeout, self.__on_timeout)
-        return self._current_promise
+        return self.__current_promise
 
     def wait_until_terminate(self):
         """
@@ -133,17 +131,66 @@ class ProcessWrapper():
             return None
 
         # process is still running, return my promise
-        self._final_promise = Promise()
-        return self._final_promise
+        self.__final_promise = Promise()
+        return self.__final_promise
 
     def __on_timeout(self, timeout):
         """
-           Called by GPS when time for a match desire is out.
+           Called by GPS when it's timeout for a pattern to appear in output.
         """
-        if self._current_promise is not None:
+        if self.__current_promise is not None:
             # if the pattern is not found, answer the promise with False
-            if not self.matched:
-                self._current_promise.resolve(False)
-        self._current_pattern = None
-        self._current_promise = None
+            self.__current_promise.resolve(False)
+        self.__current_pattern = None
+        self.__current_promise = None
         timeout.remove()
+
+
+class DebuggerWrapper():
+
+    def __init__(self, f):
+        self.__debugger = GPS.Debugger.spawn(f)
+        self.__this_promise = None
+        self.__next_cmd = None
+        self.__output = ""
+
+    def __is_busy(self, timeout):
+        """
+           Called by GPS at each interval.
+        """
+        # if the debugger is not busy
+        if not self.__debugger.is_busy():
+            timeout.remove()
+            # if there's cmd to run, send it
+            if self.__next_cmd is not None:
+                self.__output = self.__debugger.send(self.__next_cmd)
+                print "output will be: " + self.__output
+                self.__next_cmd = None
+            # if there's unanswered promise, answer it
+            if self.__this_promise is not None:
+                self.__this_promise.resolve(self.__output)
+                self.__this_promise = None
+
+    def __on_cmd_timeout(self, timeout):
+        print self.__next_cmd, "is timeout"
+        """
+           Called by GPS at each interval
+        """
+        # check if promise unanswered
+        if self.__this_promise is not None:
+            self.__this_promise.resolve()
+        self.__this_promise = None
+        self.__next_cmd = None
+        timeout.remove()
+
+    def wait_and_send(self, cmd, timeout=0, interval=500):
+        self.__this_promise = Promise()
+        self.__next_cmd = cmd
+        self.__output = None
+
+        GPS.Timeout(interval, self.__is_busy)
+
+        if timeout > 0:
+            GPS.Timeout(timeout, self.__on_cmd_timeout)
+
+        return self.__this_promise
