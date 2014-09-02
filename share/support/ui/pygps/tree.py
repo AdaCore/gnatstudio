3256f01fd14bbe37bf76f2c0cff5e21872fb80ca
Emmanuel Briot <briot@adacore.com>
2014-09-02 18:46:42 +0000

    Some more cleanup
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@231960 936e1b1b-40f2-da11-902a-00137254ae57

--- share/support/ui/pygps/tree.py
+++ share/support/ui/pygps/tree.py
@@ -85,8 +85,8 @@ try:
             event.window = view.get_bin_window()
             event.device = pygps.default_event_device()
             event.button = button
-            event.button.x = float(rect.x + rect.width / 2)
-            event.button.y = float(rect.y + rect.height / 2)
+            event.x = float(rect.x + rect.width / 2)
+            event.y = float(rect.y + rect.height / 2)
 
             if modifier is not None:
                 state = modifier
