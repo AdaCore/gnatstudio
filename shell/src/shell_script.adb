99003368bc987aa7bcadd689cd2bab786da2aea5
Emmanuel Briot <briot@adacore.com>
2014-09-16 10:38:49 +0200

    Remove the GPS shell console
    L621-013
    
    Change-Id: I2e9ded5d2b073c9e32a7c6557ecd27a23e8f489b

--- shell/src/shell_script.adb
+++ shell/src/shell_script.adb
@@ -15,35 +15,16 @@
 -- of the license.                                                          --
 ------------------------------------------------------------------------------
 
---  This package contains the implementation for a specific scripting language,
---  the simple GPS shell.
-
-with System;                    use System;
-
 with GNAT.OS_Lib;               use GNAT.OS_Lib;
 with GNATCOLL.Scripts;          use GNATCOLL.Scripts;
-with GNATCOLL.Scripts.Impl;     use GNATCOLL.Scripts.Impl;
 with GNATCOLL.Scripts.Shell;    use GNATCOLL.Scripts.Shell;
-with GNATCOLL.Scripts.Python;
 with GNATCOLL.VFS;              use GNATCOLL.VFS;
-
-with Glib.Object;               use Glib.Object;
-with Gtk.Enums;                 use Gtk.Enums;
-with Gtk.Widget;                use Gtk.Widget;
-with Gtkada.MDI;
-
-with Generic_Views;
-with GPS.Intl;                  use GPS.Intl;
 with GPS.Kernel.Actions;        use GPS.Kernel.Actions;
-with GPS.Kernel.MDI;            use GPS.Kernel.MDI;
-with GPS.Kernel.Preferences;    use GPS.Kernel.Preferences;
 with GPS.Kernel.Scripts;        use GPS.Kernel.Scripts;
 with GPS.Kernel.Task_Manager;   use GPS.Kernel.Task_Manager;
 with GPS.Kernel;                use GPS.Kernel;
 with Commands;                  use Commands;
 with Commands.Interactive;      use Commands.Interactive;
-with Histories;                 use Histories;
-with Interactive_Consoles;      use Interactive_Consoles;
 with String_Utils;              use String_Utils;
 
 package body Shell_Script is
@@ -95,59 +76,6 @@ package body Shell_Script is
      (Data : Shell_GPS_Callback_Data; N : Positive) return Subprogram_Type;
    --  See doc from inherited subprogram
 
-   procedure Module_Command_Handler
-     (Data : in out Callback_Data'Class; Command : String);
-   --  Internal handler for the shell functions defined in this module
-
-   -------------
-   -- Console --
-   -------------
-
-   type Shell_Console_Record
-      is new Interactive_Console_Record with null record;
-   --  The shell console. This is mostly use to have a unique tag when saving
-   --  the console.
-
-   function Initialize
-     (Console : access Shell_Console_Record'Class) return Gtk_Widget;
-   --  Initialize a new shell console and returns the focus widget.
-
-   package Shell_Views is new Generic_Views.Simple_Views
-     (Module_Name        => "Shell_Console",
-      View_Name          => -"Shell",
-      Formal_View_Record => Shell_Console_Record,
-      Formal_MDI_Child   => GPS_MDI_Child_Record,
-      Reuse_If_Exist     => True,
-      Initialize         => Initialize,
-      Local_Toolbar      => False,
-      Local_Config       => False,
-      Areas              => Gtkada.MDI.Sides_Only,
-      Group              => Group_Consoles);
-
-   ----------------
-   -- Initialize --
-   ----------------
-
-   function Initialize
-     (Console : access Shell_Console_Record'Class) return Gtk_Widget
-   is
-      Script  : constant Shell_Scripting := Shell_Scripting
-        (Lookup_Scripting_Language
-           (Get_Scripts (Console.Kernel), GPS_Shell_Name));
-   begin
-      Initialize
-        (Console      => Console,
-         Kernel       => Console.Kernel,
-         Prompt       => "GPS> ",
-         User_Data    => Console.Kernel.all'Address,
-         History_List => Get_History (Console.Kernel),
-         Key          => "shell",
-         Wrap_Mode    => Wrap_Char);
-      Set_Font_And_Colors (Get_View (Console), Fixed_Font => True);
-      Set_Default_Console (Script, Get_Or_Create_Virtual_Console (Console));
-      return Gtk_Widget (Console.Get_View);
-   end Initialize;
-
    ---------------------
    -- Register_Module --
    ---------------------
@@ -158,63 +86,7 @@ package body Shell_Script is
       Script : constant Shell_Scripting := Global_Shell_Script'Access;
    begin
       Register_Shell_Scripting (Get_Scripts (Kernel), Script);
-      Set_Prompt (Script, "GPS>");
-
-      Shell_Views.Register_Module (Kernel);
-
-      --  Only remember the last 100 commands.
-      Set_Max_Length (Get_History (Kernel).all, 100, "shell");
-      Allow_Duplicates (Get_History (Kernel).all, "shell", True, True);
-
-      --  The following commands are specific to the GPS shell script.
-      Register_Command
-        (Get_Scripts (Kernel), "help",
-         Minimum_Args => 0,
-         Maximum_Args => 1,
-         Handler      => Module_Command_Handler'Access,
-         Language     => Shell_Name);
    end Register_Module;
-
-   ----------------------------
-   -- Module_Command_Handler --
-   ----------------------------
-
-   procedure Module_Command_Handler
-     (Data    : in out Callback_Data'Class;
-      Command : String)
-   is
-      Kernel : constant Kernel_Handle := Get_Kernel (Data);
-      Error  : aliased Boolean;
-   begin
-      if Command = "help" then
-         if Number_Of_Arguments (Data) = 0 then
-            Insert_Text
-              (Get_Script (Data), null,
-               -"The following commands are defined:" & ASCII.LF);
-            List_Commands (Shell_Scripting (Get_Script (Data)), null);
-            Insert_Text
-              (Get_Script (Data), null,
-               -"Type ""help <cmd>"" to get help about a specific command."
-               & ASCII.LF);
-
-         else
-            declare
-               --  Can't call python's help(), since that would display the
-               --  help in the Python console, not the GPS shell console
-               N : constant String := Execute_Command
-                 (Lookup_Scripting_Language
-                    (Get_Scripts (Kernel),
-                     GNATCOLL.Scripts.Python.Python_Name),
-                   "GPS_help.helpdoc('" & Nth_Arg (Data, 1) & "')",
-                   Errors => Error'Access);
-            begin
-               Insert_Text
-                  (Get_Script (Data), null,
-                  N & ASCII.LF);
-            end;
-         end if;
-      end if;
-   end Module_Command_Handler;
 
    ------------
    -- Create --
