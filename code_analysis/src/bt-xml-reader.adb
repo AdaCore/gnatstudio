0035b60f686739be535aa41e18838d79c282856c
Vadim Godunko <godunko@adacore.com>
2014-10-08 17:42:26 +0400

    Copy files from CodePeer's sources.
    Part of N902-004.
    
    Change-Id: Ia2fe82291da77fb1d1addc2f04741375b60db9a5

---
+++ code_analysis/src/bt-xml-reader.adb
@@ -0,0 +1,548 @@
+------------------------------------------------------------------------------
+--                              C O D E P E E R                             --
+--                                                                          --
+--                     Copyright (C) 2008-2014, AdaCore                     --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+--                                                                          --
+-- The CodePeer technology was originally developed by SofCheck, Inc.       --
+------------------------------------------------------------------------------
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+with Sax.Readers;    use Sax.Readers;
+with Sax.Attributes; use Sax.Attributes;
+with Unicode.CES;
+with Input_Sources;
+with Input_Sources.File;
+
+with Ada.Containers.Hashed_Maps;
+with Ada.Containers.Indefinite_Doubly_Linked_Lists;
+with Ada.Strings.Unbounded.Hash_Case_Insensitive;
+with Ada.Strings.Unbounded.Equal_Case_Insensitive;
+
+with GNAT.OS_Lib; use GNAT.OS_Lib;
+
+package body BT.Xml.Reader is
+
+   use Ada.Containers;
+   use Message_Kinds;
+
+   Debug_On : constant Boolean := False;
+
+   function Hash (Key_Type : Natural) return Hash_Type
+     is (Hash_Type (Key_Type));
+   --  Hash function on the VN_Id
+
+   package VN_To_BT_Mappings is new Ada.Containers.Hashed_Maps
+     (Key_Type        => Natural,
+      Element_Type    => BT_Info_Seqs.Vector,
+      Hash            => Hash,
+      Equivalent_Keys => "=",
+      "="             => BT_Info_Seqs."=");
+
+   --  mapping check_id => BE_Subkind
+   package Check_Kind_Mappings is new Ada.Containers.Hashed_Maps
+     (Key_Type        => Natural,
+      Element_Type    => Message_Kinds.BE_Message_Subkind,
+      Hash            => Hash,
+      Equivalent_Keys => "=");
+
+   --  mapping check_id => readable_subkind
+   package Check_Text_Mappings is new Ada.Containers.Hashed_Maps
+     (Key_Type        => Natural,
+      Element_Type    => Unbounded_String,
+      Hash            => Hash,
+      Equivalent_Keys => "=");
+
+   --  mapping event_id => Event_Enum
+   package Event_Kind_Mappings is new Ada.Containers.Hashed_Maps
+     (Key_Type        => Natural,
+      Element_Type    => Event_Enum,
+      Hash            => Hash,
+      Equivalent_Keys => "=");
+
+   package Event_Text_Mappings is new Ada.Containers.Hashed_Maps
+     (Key_Type        => Natural,
+      Element_Type    => Unbounded_String,
+      Hash            => Hash,
+      Equivalent_Keys => "=");
+
+   type Mapping_Ptr is access VN_To_BT_Mappings.Map;
+
+   --  mapping proc => (vn_id => backtraces)
+   package Procs_To_Vns_Mappings is new Ada.Containers.Hashed_Maps
+     (Key_Type        => Unbounded_String,
+      Element_Type    => Mapping_Ptr,
+      Hash            => Ada.Strings.Unbounded.Hash_Case_Insensitive,
+      Equivalent_Keys => Ada.Strings.Unbounded.Equal_Case_Insensitive);
+
+   Proc_Vns : Procs_To_Vns_Mappings.Map;
+
+   package File_Set is new
+     Ada.Containers.Indefinite_Doubly_Linked_Lists (String);
+
+   Files_Read : File_Set.List;
+   --  Keep track of XML files that have been read
+
+   --  extra precondition_event information
+   type Callee_Info_Record is record
+      Bt_Id            : Natural;
+      Callee_File_Name : Unbounded_String;
+      Callee_Name      : Unbounded_String;
+      Callee_Vn        : Natural;
+      Callee_Srcpos    : Source_Position;
+   end record;
+
+   package Callee_Mappings is new Ada.Containers.Hashed_Maps
+     (Key_Type        => Natural,
+      Element_Type    => Callee_Info_Record,
+      Hash            => Hash,
+      Equivalent_Keys => "=");
+
+   Callee_Mapping : Callee_Mappings.Map;
+   Current_BT_Seq : BT_Info_Seqs.Vector;
+   Current_Bt_Id  : Natural := 0;
+
+   function EQ (B1, B2 : BT_Info) return Boolean;
+   --  compare 2 backtrace records
+
+   function LT (E1, E2 : BT_Info) return Boolean;
+   --  compare 2 backtrace records based on line numbers only for now.
+
+   package Sort_Backtraces is new BT_Info_Seqs.Generic_Sorting (LT);
+
+   type Backtrace_Reader is new Sax.Readers.Reader with record
+      File_Name    : Unbounded_String;
+      Current_Proc : Unbounded_String;
+      Current_VN   : Natural;
+      Check_Kind   : Check_Kind_Mappings.Map;
+      Check_Text   : Check_Text_Mappings.Map;
+      Event_Kind   : Event_Kind_Mappings.Map;
+      Event_Text   : Event_Text_Mappings.Map;
+   end record;
+
+   overriding procedure Start_Element
+     (Self          : in out Backtrace_Reader;
+      Namespace_URI : Unicode.CES.Byte_Sequence;
+      Local_Name    : Unicode.CES.Byte_Sequence;
+      Qname         : Unicode.CES.Byte_Sequence;
+      Attrs         : Sax.Attributes.Attributes'Class);
+
+   overriding procedure End_Element
+     (Self          : in out Backtrace_Reader;
+      Namespace_URI : Unicode.CES.Byte_Sequence;
+      Local_Name    : Unicode.CES.Byte_Sequence;
+      Qname         : Unicode.CES.Byte_Sequence);
+
+   --------
+   -- EQ --
+   --------
+
+   function EQ (B1, B2 : BT_Info) return Boolean is
+   begin
+      return B1.Event = B2.Event
+        and then B1.Kind = B2.Kind
+        and then B1.Sloc.Line = B2.Sloc.Line
+        and then B1.Sloc.Column = B2.Sloc.Column;
+   end EQ;
+
+   --------
+   -- LT --
+   --------
+
+   function LT (E1, E2 : BT_Info) return Boolean is
+   begin
+      return E1.Sloc.Line < E2.Sloc.Line;
+   end LT;
+
+   -------------------
+   -- Start_Element --
+   -------------------
+
+   overriding procedure Start_Element
+     (Self          : in out Backtrace_Reader;
+      Namespace_URI : Unicode.CES.Byte_Sequence;
+      Local_Name    : Unicode.CES.Byte_Sequence;
+      Qname         : Unicode.CES.Byte_Sequence;
+      Attrs         : Sax.Attributes.Attributes'Class)
+   is
+      pragma Unreferenced (Namespace_URI, Local_Name);
+   begin
+      if Qname = File_Tag then
+         declare
+            File_Name : constant String := Attrs.Get_Value (Name_Attribute);
+         begin
+            if Debug_On then
+               Put_Line ("reading xml for file " & File_Name);
+            end if;
+
+            Set_Unbounded_String (Self.File_Name, File_Name);
+         end;
+
+      elsif Qname = Bt_Tag then
+         declare
+            BT : BT_Info;
+            type BT_Attribute_Enum is (Line, Col, Check, Event);
+
+         begin
+            if Debug_On then
+               Put_Line ("reading backtrace");
+            end if;
+            Current_Bt_Id := Current_Bt_Id + 1;
+            BT.Bt_Id := Current_Bt_Id;
+            --  default
+            BT.Event := Check_Event;
+            for J in 0 .. Get_Length (Attrs) - 1 loop
+               declare
+                  Attr_Value : constant String := Get_Value (Attrs, J);
+               begin
+                  case BT_Attribute_Enum'Value (Get_Qname (Attrs, J)) is
+                  when Line =>
+                     BT.Sloc.Line := Positive'Value (Attr_Value);
+                  when Col =>
+                     BT.Sloc.Column := Positive'Value (Attr_Value);
+                  when Check =>
+                     BT.Kind := Self.Check_Kind (Natural'Value (Attr_Value));
+                     BT.Text := Self.Check_Text (Natural'Value (Attr_Value));
+                  when Event =>
+                     BT.Event := Self.Event_Kind (Natural'Value (Attr_Value));
+                     BT.Text := Self.Event_Text (Natural'Value (Attr_Value));
+                  end case;
+               end;
+            end loop;
+            BT_Info_Seqs.Append (Current_BT_Seq, BT);
+         end;
+      elsif Qname = Vn_Tag then
+         Current_BT_Seq := BT_Info_Seqs.Empty_Vector;
+         Self.Current_VN :=
+           Natural'Value (Attrs.Get_Value (Id_Attribute));
+
+      elsif Qname = Proc_Tag then
+         declare
+            Proc_Name : constant String := Attrs.Get_Value
+               (Name_Attribute);
+         begin
+            if Debug_On then
+               Put_Line ("reading proc " & Proc_Name);
+            end if;
+            Set_Unbounded_String (Self.Current_Proc, Proc_Name);
+            Procs_To_Vns_Mappings.Insert
+              (Proc_Vns, Self.Current_Proc, new VN_To_BT_Mappings.Map);
+         end;
+
+      elsif Qname = Callee_Tag then
+         declare
+            Info : Callee_Info_Record;
+         begin
+            Info.Bt_Id := Current_Bt_Id;
+            Set_Unbounded_String
+              (Info.Callee_Name, Attrs.Get_Value (Name_Attribute));
+            Set_Unbounded_String
+              (Info.Callee_File_Name, Attrs.Get_Value (File_Name_Attribute));
+            Info.Callee_Vn   := Positive'Value
+               (Attrs.Get_Value (Vn_Attribute));
+            Info.Callee_Srcpos.Line := Positive'Value
+               (Attrs.Get_Value (Line_Attribute));
+            Info.Callee_Srcpos.Column := Positive'Value
+               (Attrs.Get_Value (Col_Attribute));
+            Callee_Mappings.Insert (Callee_Mapping,
+              Current_Bt_Id, Info);
+         end;
+      elsif Qname = Checks_Tag then
+         --  nothing to do
+         null;
+      elsif Qname = Check_Tag then
+         declare
+            Check_Name : constant String := Attrs.Get_Value (Name_Attribute);
+            Check_Text : constant String := Attrs.Get_Value (Text_Attribute);
+            Check_Id   : constant Natural :=
+              Natural'Value (Attrs.Get_Value (Id_Attribute));
+
+         begin
+            Self.Check_Kind.Insert
+              (Check_Id, Message_Kinds.BE_Message_Subkind'Value (Check_Name));
+            Self.Check_Text.Insert
+              (Check_Id, To_Unbounded_String (Check_Text));
+         end;
+
+      elsif Qname = Event_Tag then
+         declare
+            Event_Name : constant String := Attrs.Get_Value (Name_Attribute);
+            Event_Text : constant String := Attrs.Get_Value (Text_Attribute);
+            Event_Id   : constant Natural :=
+              Natural'Value (Attrs.Get_Value (Id_Attribute));
+
+         begin
+            Self.Event_Kind.Insert (Event_Id, Event_Enum'Value (Event_Name));
+            Self.Event_Text.Insert
+              (Event_Id, To_Unbounded_String (Event_Text));
+         end;
+      elsif Qname = Events_Tag then
+         --  nothing to do
+         null;
+      else
+         null;
+      end if;
+   end Start_Element;
+
+   -------------------
+   -- End_Element --
+   -------------------
+
+   overriding procedure End_Element
+     (Self          : in out Backtrace_Reader;
+      Namespace_URI : Unicode.CES.Byte_Sequence;
+      Local_Name    : Unicode.CES.Byte_Sequence;
+      Qname         : Unicode.CES.Byte_Sequence)
+   is
+      pragma Unreferenced (Namespace_URI, Local_Name);
+   begin
+      if Qname = File_Tag then
+         --  we are done
+         null;
+      elsif Qname = Bt_Tag then
+         --  nothing to do
+         null;
+      elsif Qname = Vn_Tag then
+         --  store seq of backtraces into mapping
+         pragma Assert (Self.Current_VN /= 0);
+         Proc_Vns.Element (Self.Current_Proc).Include
+           (Self.Current_VN, Current_BT_Seq);
+         Current_BT_Seq := BT_Info_Seqs.Empty_Vector;
+         Self.Current_VN := 0;
+
+      elsif Qname = Proc_Tag then
+         --  nothing to do
+         null;
+      elsif Qname = Checks_Tag then
+         null;
+      elsif Qname = Check_Tag then
+         null;
+      elsif Qname = Events_Tag then
+         null;
+      elsif Qname = Event_Tag then
+         null;
+      else
+         null;
+      end if;
+   end End_Element;
+
+   -------------------
+   -- Read_File_Xml --
+   -------------------
+
+   procedure Read_File_Xml
+     (Output_Dir  : String;
+      File_Name   : String;
+      File_Exists : out Boolean)
+   is
+      use Input_Sources.File;
+
+      File_To_Read : constant String :=
+        BT.Xml.Xml_File_Name (Output_Dir, File_Name, For_Backtraces => True);
+
+      Input  : Input_Sources.File.File_Input;
+      Reader : Backtrace_Reader;
+
+   begin
+      if not Is_Regular_File (File_To_Read) then
+         File_Exists := False;
+         return;
+      end if;
+
+      File_Exists := True;
+
+      if Files_Read.Contains (File_To_Read) then
+         --  already processed
+         return;
+      end if;
+
+      Input_Sources.Set_Public_Id
+        (Input_Sources.Input_Source (Input), File_To_Read);
+      Input_Sources.Set_System_Id
+        (Input_Sources.Input_Source (Input), File_To_Read);
+      Input_Sources.File.Open (File_To_Read, Input);
+      Set_Feature (Reader, Validation_Feature, False);
+      Parse (Reader, Input);
+      Close (Input);
+      Files_Read.Append (File_To_Read);
+   end Read_File_Xml;
+
+   -----------------------
+   -- Get_Vn_Backtraces --
+   -----------------------
+
+   procedure Get_Vn_Backtraces
+     (Proc_Name  : String;
+      Vn_Id      : Natural;
+      Backtraces : in out BT.BT_Info_Seqs.Vector)
+   is
+      Proc : constant Unbounded_String := To_Unbounded_String (Proc_Name);
+
+   begin
+      if not Proc_Vns.Contains (Proc) or else
+         not Proc_Vns.Element (Proc).Contains (Vn_Id)
+      then
+         if Debug_On then
+            Put_Line ("No backtraces for this vn " & Natural'Image (Vn_Id)
+              & " in " & Proc_Name);
+         end if;
+
+         return;
+      end if;
+
+      --  Collect all backtraces (including the backtraces imported
+      --  at precondition_check events.
+      --  Uniquify and sort (by increasing line numbers) before
+      --  returning the sequence.
+      declare
+         VN_BTs : constant VN_To_BT_Mappings.Cursor :=
+           Proc_Vns.Element (Proc).Find (Vn_Id);
+
+         All_Checks     : Check_Kinds_Array := Check_Kinds_Array_Default;
+         Primary_Checks : Check_Kinds_Array;
+         Precond_Found  : Boolean := False;
+
+         use BT_Info_Seqs;
+
+         procedure Add_One_Backtrace (Info : BT_Info);
+         --  Add backtrace to the seq of relevant backtrace for this
+         --  VN. For precondition events, recursively import the backtraces
+         --  contributing to the corresponding precondition.
+
+         ---------------------
+         --  Add_One_Backtrace
+         ---------------------
+
+         procedure Add_One_Backtrace (Info : BT_Info) is
+         begin
+            if BT.BT_Info_Seqs.Contains (Backtraces, Info) then
+               return;
+            end if;
+            Append (Backtraces, Info);
+
+            if Info.Event = Precondition_Event then
+               declare
+                  Callee_BTs : BT.BT_Info_Seqs.Vector;
+               begin
+                  Get_Vn_Backtraces
+                    (Get_Precondition_Callee_Name (Info.Bt_Id),
+                     Get_Precondition_VN (Info.Bt_Id),
+                     Callee_BTs);
+
+                  --  Import the backtraces contributing to that
+                  --  precondition.
+
+                  for BT of Callee_BTs loop
+                     Add_One_Backtrace (BT);
+                     Precond_Found := True;
+                  end loop;
+               end;
+
+            elsif Info.Event = Check_Event then
+               All_Checks (Info.Kind) := True;
+            end if;
+         end Add_One_Backtrace;
+
+         use VN_To_BT_Mappings;
+
+      begin
+         if VN_BTs /= VN_To_BT_Mappings.No_Element then
+            for BT of VN_To_BT_Mappings.Element (VN_BTs) loop
+               Add_One_Backtrace (BT);
+            end loop;
+         end if;
+
+         Sort_Backtraces.Sort (Backtraces);
+
+         if Precond_Found then
+            --  Cleanup the new sequence by removing duplicates and
+            --  backtraces associated with non-primary checks
+            Primary_Checks := Primary_Original_Checks (All_Checks);
+
+            declare
+               Result_BTs : BT_Info_Seqs.Vector;
+               Prev       : BT_Info := No_BT_Info;
+            begin
+               for Info of Backtraces loop
+                  if not EQ (Info, Prev) then
+                     Prev := Info;
+
+                     if Info.Event /= Check_Event
+                       or else Primary_Checks (Info.Kind)
+                     then
+                        Append (Result_BTs, Info);
+                     end if;
+                  end if;
+               end loop;
+
+               Backtraces := Result_BTs;
+            end;
+         end if;
+      end;
+   end Get_Vn_Backtraces;
+
+   ----------------------------------
+   -- Get_Precondition_Callee_Name --
+   ----------------------------------
+
+   function Get_Precondition_Callee_Name (Bt_Id : Natural) return String is
+      Info : constant Callee_Info_Record := Callee_Mapping.Element (Bt_Id);
+   begin
+      return To_String (Info.Callee_Name);
+   end Get_Precondition_Callee_Name;
+
+   -------------------------
+   -- Get_Precondition_VN --
+   -------------------------
+
+   function Get_Precondition_VN (Bt_Id : Natural) return Natural is
+      Info : constant Callee_Info_Record := Callee_Mapping.Element (Bt_Id);
+   begin
+      return Info.Callee_Vn;
+   end Get_Precondition_VN;
+
+   -----------------------
+   -- Get_Callee_Srcpos --
+   -----------------------
+
+   function Get_Callee_Srcpos (Bt_Id : Natural) return Source_Position is
+      Info : constant Callee_Info_Record := Callee_Mapping.Element (Bt_Id);
+   begin
+      return Info.Callee_Srcpos;
+   end Get_Callee_Srcpos;
+
+   --------------------------
+   -- Get_Callee_File_Name --
+   --------------------------
+
+   function Get_Callee_File_Name (Bt_Id : Natural) return String is
+      Info : constant Callee_Info_Record := Callee_Mapping.Element (Bt_Id);
+   begin
+      return To_String (Info.Callee_File_Name);
+   end Get_Callee_File_Name;
+
+   --------------------------
+   -- Get_Srcpos_Vn_Values --
+   --------------------------
+
+   function Get_Srcpos_Vn_Values
+     (File_Name : String;
+      Srcpos    : Source_Position) return Vn_Values_Seqs.Vector
+   is
+      pragma Unreferenced (File_Name, Srcpos);
+   begin
+      return Vn_Values_Seqs.Empty_Vector;
+   end Get_Srcpos_Vn_Values;
+
+end BT.Xml.Reader;
