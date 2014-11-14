9c529b7d0fdce52e83975d03e42ca47eb5bfe840
Vadim Godunko <godunko@adacore.com>
2014-11-14 13:17:15 +0300

    Load and store names of data files for backtraces and values.
    Part of N902-004.
    
    Change-Id: Id87b574ae75d1a8cc8a3c0fde8cfc1918b0d331d

--- code_analysis/src/codepeer-bridge-inspection_readers.adb
+++ code_analysis/src/codepeer-bridge-inspection_readers.adb
@@ -36,6 +36,7 @@ package body CodePeer.Bridge.Inspection_Readers is
    Entry_Point_Access_Tag  : constant String := "entry_point_access";
    Object_Access_Tag       : constant String := "object_access";
 
+   Backtraces_Attribute     : constant String := "backtraces";
    Category_Attribute       : constant String := "category";
    Checks_Attribute         : constant String := "checks";
    Column_Attribute         : constant String := "column";
@@ -50,6 +51,7 @@ package body CodePeer.Bridge.Inspection_Readers is
    Previous_Attribute       : constant String := "previous";
    Primary_Checks_Attribute : constant String := "primary_checks";
    Rank_Attribute           : constant String := "rank";
+   Values_Attribute         : constant String := "values";
    Vn_Id_Attribute          : constant String := "vn-id";
    Vn_Ids_Attribute         : constant String := "vn-ids";
 
@@ -485,12 +487,19 @@ package body CodePeer.Bridge.Inspection_Readers is
                 (Self.Projects,
                  F_Info.Project);
          end;
+
          Self.File_Node :=
            Code_Analysis.Get_Or_Create (Project_Node, File_Name);
          Self.File_Node.Analysis_Data.CodePeer_Data :=
            new CodePeer.File_Data'
                  (Lifeage      => Lifeage,
-                  Total_Checks => Checks);
+                  Total_Checks => Checks,
+                  Backtraces_File =>
+                    GNATCOLL.VFS.Create_From_UTF8
+                      (Attrs.Get_Value (Backtraces_Attribute)),
+                  Values_File     =>
+                    GNATCOLL.VFS.Create_From_UTF8
+                      (Attrs.Get_Value (Values_Attribute)));
 
       elsif Qname = Subprogram_Tag then
          Self.Subprogram_Node :=
