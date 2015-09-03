72d776ed970bccf0f25913325f00a05f887e6d17
Raphael Amiard <amiard@adacore.com>
2015-09-03 12:54:02 +0200

    O806-012: Fix save_on_compile when there is no root object_dir
    In that case store the log at the root of the project
    
    Change-Id: Ica19a9f18975b9d3b6e53f732b69fde2bbbadeea

--- share/library/save_on_compile.py
+++ share/library/save_on_compile.py
@@ -2,15 +2,26 @@
 Automatically saves the contents of the Messages window when a
 compilation has finished.
 The output is saved in a file called "messages.txt" in the root project's
-object directory.
+object directory. If there is no object directory for the root project, then
+the file will be saved at the root directory of the project itself.
 """
 
-from GPS import *
+from GPS import Preference, Project, Console, Hook
+from os.path import dirname, join
+
+
+file_name_pref = Preference("Plugins/Save on compile/file_name").create(
+    "File Name", "string",
+    "Name of the file you want to save the messages into"
+    "messages.txt"
+)
 
 
 def on_compilation_finished(hook, category, target_name, mode_name, status):
-    f = file(Project.root().object_dirs(False)[0] + "/messages.txt", "w")
-    f.write(Console().get_text())
-    f.close()
+    obj_dirs = Project.root().object_dirs(False)
+    path = obj_dirs[0] if obj_dirs else dirname(Project.root().file().name())
+
+    with file(join(path, file_name_pref.get()), "w") as f:
+        f.write(Console().get_text())
 
 Hook("compilation_finished").add(on_compilation_finished)
