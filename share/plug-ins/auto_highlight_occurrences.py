AdaCore/gnatstudio

commit 7186b6fced9cadb3a80c395570563252130de28e

GPS: Add some extensions to the python API
* GPS.EditorBuffer.entity_under_cursor: Shortcut to return the
  GPS.Entity that is under the cursor.

* GPS.Cursor.location: Shortcut to return the GPS.EditorLocation of the
  Cursor instance

* GPS.EditorLocation.get_word: Returns the word that contains the
  location, if there is one.

* GPS.EditorLocation.entity: Returns the entity corresponding to the
  EditorLocation

* auto_highlight_occurences.py: Factor out the logic of word selection,
  and use EditorLocation.get_word instead.

For libclang testing - MB18-041

Change-Id: Ice55dfedb5faf68b79d96e014846bac30633f0b6

share/plug-ins/auto_highlight_occurrences.py
@@ -31,7 +31,6 @@ like "TODO", or special comments for instance.
 import GPS
 from gps_utils import *
 from gps_utils.highlighter import Location_Highlighter, OverlayStyle
-import traceback
 
 GPS.Preference(
     "Plugins/auto_highlight_occurrences/highlight_entities").create(
@@ -245,18 +244,7 @@ class Current_Entity_Highlighter(Location_Highlighter):
             if location and buffer:
                 location = GPS.EditorLocation(
                     buffer, location.line(), location.column())
-
-                if location.inside_word():
-                    start_loc = location
-                    while not start_loc.starts_word():
-                        start_loc = start_loc.forward_char(-1)
-
-                    end_loc = location
-                    while not end_loc.ends_word():
-                        end_loc = end_loc.forward_char()
-
-                    word = buffer.get_chars(start_loc, end_loc).strip()
-                    word = word.decode("utf8")  # make unicode-string
+                word = location.get_word()
 
         # Exit if we are highlighting the word or the entity that we were
         # already highlighting.

share/support/core/extensions/__init__.py
@@ -1,4 +1,3 @@
-from extensions.private.xml import X
 from GPS import *
 import GPS
 
@@ -59,7 +58,7 @@ class EditorBuffer(object):
 
         .. seealso:: :func:`GPS.EditorBuffer.delete`
         """
-        if isinstance(loc_or_text, EditorLocation):
+        if isinstance(loc_or_text, GPS.EditorLocation):
             assert isinstance(text, str) or isinstance(text, unicode)
             self._insert_at_location(loc_or_text, text)
         else:
@@ -67,6 +66,73 @@ class EditorBuffer(object):
             assert isinstance(text, str) or isinstance(text, unicode)
             self._insert_at_location(self.current_view().cursor(),
                                      loc_or_text)
+
+    def entity_under_cursor(self):
+        """
+        Shortcut to return a :class:`GPS.Entity` instance corresponding to the
+        entity under cursor
+
+        :rtype: :class:`GPS.Entity`
+        """
+        return self.main_cursor().location().entity()
+
+
+@extend_gps
+class Cursor(object):
+    def location(self):
+        """
+        Returns the cursor's location
+        :rtype: :class:`GPS.EditorLocation`
+        """
+        return self.mark().location()
+
+
+@extend_gps
+class EditorLocation(object):
+    def get_word(self):
+        """
+        This will return the word that contains this location, if there
+        is one, the empty string otherwise. This is a shortcut method that uses
+        the inside_word, starts_word and ends_word methods of
+        `GPS.EditorLocation`.
+
+        :returns: A tuple (word, start location, end location)
+        :rtype: (unicode,
+                 :class:`GPS.EditorLocation`, :class:`GPS.EditorLocation`)
+        """
+
+        word = ""
+        start_loc = None
+        end_loc = None
+
+        if self.inside_word():
+            start_loc = self
+            while not start_loc.starts_word():
+                start_loc = start_loc.forward_char(-1)
+
+            end_loc = self
+            while not end_loc.ends_word():
+                end_loc = end_loc.forward_char()
+
+            word = self.buffer().get_chars(start_loc, end_loc).strip()
+            word = word.decode("utf8")  # make unicode-string
+
+        return word, start_loc, end_loc
+
+    def entity(self):
+        """
+        Returns a :class:`GPS.Entity` instance at the given location.
+
+        If there is no entity that can be resolved at this location, returns None
+
+        :rtype: :class:`GPS.Entity`
+        """
+        try:
+            word, start_loc, end_loc = self.get_word()
+            return GPS.Entity(word, self.buffer().file(),
+                              start_loc.line(), start_loc.column())
+        except Exception:
+            return None
 
 
 @extend_gps

commit 5de6c6719c49f348f7415d4702f2de42f935314f

Fix regressions introduced by 7186b6f
Commit message was: GPS: Add some extensions to the python API

For libclang testing - MB18-041

Change-Id: Ic9052bf0472640da59870924b9dd72e8516ed6e6

share/plug-ins/auto_highlight_occurrences.py
@@ -244,7 +244,7 @@ class Current_Entity_Highlighter(Location_Highlighter):
             if location and buffer:
                 location = GPS.EditorLocation(
                     buffer, location.line(), location.column())
-                word = location.get_word()
+                word, _, _ = location.get_word()
 
         # Exit if we are highlighting the word or the entity that we were
         # already highlighting.