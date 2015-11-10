d7d795dfbf61c51985868115eb9427387e8b24f4
Emmanuel Briot <briot@adacore.com>
2015-11-10 15:31:37 +0100

    Remove the gbuilder directory
    Not supported, never worked, obsolete code base
    
    Change-Id: I4e594a338c3848e8882e9f47559148f9cebff3b3

--- gbuilder/src/rad-debug.ads
+++
@@ -1,9 +0,0 @@
-package RAD.Debug is
-
-   procedure Message (Str : String; Level : Positive := 1);
-   --  Output Str if level is lower than the current level of traces
-
-   procedure Set_Level (Level : Positive);
-   --  Set the current level of debugging
-
-end RAD.Debug;
