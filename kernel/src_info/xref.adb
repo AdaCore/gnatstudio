AdaCore/gnatstudio

commit 147f8d6e99994268d2b93381f10c526966ca8863

Fix memory leak when connecting to the xref database
Change-Id: I29dc42605e57129032093ff598cb70cf9542f613

kernel/src_info/xref.adb
@@ -1988,7 +1988,7 @@ package body Xref is
      (Self   : General_Xref_Database; Tree   : Project_Tree_Access)
    is
       Working_Xref_File : Virtual_File;
-
+      Descr : GNATCOLL.SQL.Exec.Database_Description;
       Error : GNAT.Strings.String_Access;
    begin
       Self.Working_Xref_Db := GNATCOLL.VFS.No_File;
@@ -2001,19 +2001,21 @@ package body Xref is
 
       if Self.Disable_SQL_Queries then
          --  Just to avoid errors because we are accessing a non-existing db
+         Descr := GNATCOLL.SQL.Sqlite.Setup
+            (Database => ":memory:", Errors   => Self.Errors);
          Self.Xref.Setup_DB
-           (DB    => GNATCOLL.SQL.Sqlite.Setup
-              (Database => ":memory:",
-               Errors   => Self.Errors),
+           (DB    => Descr,
             Tree  => Tree,
             Error => Error);
+
       else
          declare
          begin
+            Descr :=  GNATCOLL.SQL.Sqlite.Setup
+              (Database => +Working_Xref_File.Full_Name.all,
+               Errors   => Self.Errors);
             Self.Xref.Setup_DB
-              (DB    => GNATCOLL.SQL.Sqlite.Setup
-                 (Database => +Working_Xref_File.Full_Name.all,
-                  Errors   => Self.Errors),
+              (DB    => Descr,
                Tree  => Tree,
                Error => Error);
          exception
@@ -2025,6 +2027,8 @@ package body Xref is
                       & Exception_Information (E));
          end;
       end if;
+
+      GNATCOLL.SQL.Exec.Free (Descr);
 
       --  Not interested in schema version errors, gnatinspect already
       --  displays them on the console
@@ -2697,6 +2701,7 @@ package body Xref is
 
    procedure Project_Changed (Self : General_Xref_Database) is
       Error : GNAT.Strings.String_Access;
+      Descr : GNATCOLL.SQL.Exec.Database_Description;
    begin
       --  Create an initial empty database. It will never be filled, and
       --  will be shortly replaced in Project_View_Changed, but it ensures
@@ -2714,12 +2719,13 @@ package body Xref is
       Trace (Me, "Set up xref database: :memory:");
       Self.Working_Xref_Db := GNATCOLL.VFS.No_File;
       Self.Xref_Db_Is_Temporary := True;
+      Descr := GNATCOLL.SQL.Sqlite.Setup
+         (Database => ":memory:", Errors   => Self.Errors);
       Self.Xref.Setup_DB
-        (DB    => GNATCOLL.SQL.Sqlite.Setup
-           (Database => ":memory:",
-            Errors   => Self.Errors),
+        (DB    => Descr,
          Tree  => Self.Registry.Tree,
          Error => Error);
+      GNATCOLL.SQL.Exec.Free (Descr);
 
       --  not interested in schema version errors, gnatinspect will
       --  already display those for the user.

commit 4ec197348734919d687bdf54f47253092fea6979

Fix memory corruption
Introduced in 147f8d6e99994268d2b93381f10c526966ca8863
Made GB crash

Change-Id: I506e56cd90b5a44eeb180bb6847be2af97c84029

kernel/src_info/xref.adb
@@ -1988,7 +1988,6 @@ package body Xref is
      (Self   : General_Xref_Database; Tree   : Project_Tree_Access)
    is
       Working_Xref_File : Virtual_File;
-      Descr : GNATCOLL.SQL.Exec.Database_Description;
       Error : GNAT.Strings.String_Access;
    begin
       Self.Working_Xref_Db := GNATCOLL.VFS.No_File;
@@ -2001,21 +2000,20 @@ package body Xref is
 
       if Self.Disable_SQL_Queries then
          --  Just to avoid errors because we are accessing a non-existing db
-         Descr := GNATCOLL.SQL.Sqlite.Setup
+         Self.DB := GNATCOLL.SQL.Sqlite.Setup
             (Database => ":memory:", Errors   => Self.Errors);
          Self.Xref.Setup_DB
-           (DB    => Descr,
+           (DB    => Self.DB,
             Tree  => Tree,
             Error => Error);
 
       else
-         declare
          begin
-            Descr :=  GNATCOLL.SQL.Sqlite.Setup
+            Self.DB :=  GNATCOLL.SQL.Sqlite.Setup
               (Database => +Working_Xref_File.Full_Name.all,
                Errors   => Self.Errors);
             Self.Xref.Setup_DB
-              (DB    => Descr,
+              (DB    => Self.DB,
                Tree  => Tree,
                Error => Error);
          exception
@@ -2027,8 +2025,6 @@ package body Xref is
                       & Exception_Information (E));
          end;
       end if;
-
-      GNATCOLL.SQL.Exec.Free (Descr);
 
       --  Not interested in schema version errors, gnatinspect already
       --  displays them on the console
@@ -2062,6 +2058,8 @@ package body Xref is
             end if;
          end if;
       end if;
+
+      GNATCOLL.SQL.Exec.Free (Self.DB);
    end Close_Database;
 
    -------------
@@ -2701,7 +2699,6 @@ package body Xref is
 
    procedure Project_Changed (Self : General_Xref_Database) is
       Error : GNAT.Strings.String_Access;
-      Descr : GNATCOLL.SQL.Exec.Database_Description;
    begin
       --  Create an initial empty database. It will never be filled, and
       --  will be shortly replaced in Project_View_Changed, but it ensures
@@ -2719,13 +2716,12 @@ package body Xref is
       Trace (Me, "Set up xref database: :memory:");
       Self.Working_Xref_Db := GNATCOLL.VFS.No_File;
       Self.Xref_Db_Is_Temporary := True;
-      Descr := GNATCOLL.SQL.Sqlite.Setup
+      Self.DB := GNATCOLL.SQL.Sqlite.Setup
          (Database => ":memory:", Errors   => Self.Errors);
       Self.Xref.Setup_DB
-        (DB    => Descr,
+        (DB    => Self.DB,
          Tree  => Self.Registry.Tree,
          Error => Error);
-      GNATCOLL.SQL.Exec.Free (Descr);
 
       --  not interested in schema version errors, gnatinspect will
       --  already display those for the user.

kernel/src_info/xref.ads
@@ -138,6 +138,11 @@ package Xref is
       Freeze_Count : Integer := 0;
       --  Used to implement freeze of the DB
 
+      DB : GNATCOLL.SQL.Exec.Database_Description;
+      --  The description of the database we are currently connected to.
+      --  This must not be freed while where exists connections to this
+      --  database, since the connections have a pointer to this descr.
+
       Working_Xref_Db : GNATCOLL.VFS.Virtual_File := GNATCOLL.VFS.No_File;
       --  Location of the sqlite database on which GPS is currently working
       --  Set to No_File if GPS is not working on a database at the moment.