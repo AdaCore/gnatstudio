df6e03b8d5a5d4322f3445699e2b03bcd2243589
Emmanuel Briot <briot@adacore.com>
2015-12-09 12:55:21 +0100

    Fix incorrect refresh of non-actionable contextual menus
    When a contextual menu is not associated with an action (such as a
    parent menu) and has a dynamic label using macros, the label used is
    never refreshed once it has been computed.
    Fixes label for "Goto declarations of %el"
    
    OC07-052
    
    Change-Id: I79b28ee001a9df28bec2747ef98c11d365621b2e

--- kernel/src/gps-kernel-modules-ui.adb
+++ kernel/src/gps-kernel-modules-ui.adb
@@ -981,7 +981,9 @@ package body GPS.Kernel.Modules.UI is
          if C.Label = null then
             return C.Name.all;
          else
-            --  Cache the expensive call to Get_Label in C.Label_For_Context
+            --  Cache the expensive call to Get_Label in C.Label_For_Context.
+            --  This name is used while building the menu to find parent
+            --  items.
             if C.Label_For_Context = Null_Unbounded_String then
                C.Label_For_Context := To_Unbounded_String
                  (Get_Label (C.Label, Context));
@@ -1034,10 +1036,10 @@ package body GPS.Kernel.Modules.UI is
       while C /= null loop
          if C.Menu_Type = Type_Action then
             C.Filter_Matched := Menu_Is_Visible (C, Context);
-
-            --  Reset the cache set in the previous contextual menu
-            C.Label_For_Context := Null_Unbounded_String;
          end if;
+
+         --  Reset the cache set in the previous contextual menu
+         C.Label_For_Context := Null_Unbounded_String;
          C := C.Next;
       end loop;
 
