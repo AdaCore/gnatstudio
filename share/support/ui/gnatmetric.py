8c89c884b60bc880df4f1704cee95aaa7b4db133
Vasiliy Fofanov <fofanov@adacore.com>
2016-02-22 16:11:11 +0100

    Change text and grouping of gnatmetric options (minor).
    Change-Id: Ic19d2da807d4fe3fcc05b47eb18dac4de2afc867
    TN: P219-034

--- share/support/ui/gnatmetric.py
+++ share/support/ui/gnatmetric.py
@@ -2,7 +2,6 @@
 Provides support for gnatmetric.
 """
 
-import re
 import os
 import GPS
 from gps_utils import interactive, hook
@@ -23,147 +22,150 @@ xml_base = ("""
    </command-line>
    <iconname>gps-semantic-check-symbolic</iconname>
    <switches command="%(tool_name)s" columns="2" lines="2">
-      <title column="1" line="1">Complexity metrics</title>
-      <title column="1" line="2">Line metrics</title>
-      <title column="2" line="1">Syntax element metrics</title>
-      <title column="2" line="2">Coupling metrics</title>
-<check label="all complexity metrics"
-                   line="1"  column="1"
+      <title line="1" column="1">Line metrics</title>
+      <title line="1" column="2">Syntax element metrics</title>
+      <title line="2" column="1">Complexity metrics</title>
+      <title line="2" column="2">Coupling metrics</title>
+<check label="All complexity metrics"
+                   line="2"  column="1"
                    switch="--complexity-all"
-                   tip="all complexity metrics" />
+                   tip="All complexity metrics" />
 <check label="McCabe Cyclomatic Complexity"
-                   line="1"  column="1"
+                   line="2"  column="1"
                    switch="--complexity-cyclomatic"
                    tip="McCabe Cyclomatic Complexity" />
-<check label="Essential Complexity"
-                   line="1"  column="1"
+<check label="McCabe Essential Complexity"
+                   line="2"  column="1"
                    switch="--complexity-essential"
-                   tip="Essential Complexity" />
-<check label="average McCabe Cyclomatic Complexity of a body"
-                   line="1"  column="1"
+                   tip="McCabe Essential Complexity" />
+<check label="Average McCabe CC of a body"
+                   line="2"  column="1"
                    switch="--complexity-average"
-                   tip="average McCabe Cyclomatic Complexity of a body" />
-<check label="maximal loop nesting level"
-                   line="1"  column="1"
+                   tip="Average McCabe Cyclomatic Complexity of a body" />
+<check label="Maximal loop nesting level"
+                   line="2"  column="1"
                    switch="--loop-nesting"
-                   tip="maximal loop nesting level" />
-<check label="do not count static loops for cyclomatic complexity"
-                   line="1"  column="1"
+                   tip="Maximal loop nesting level" />
+<check label="Do not count static loops for CC"
+                   line="2"  column="1"
                    switch="--no-static-loop"
-                   tip="do not count static loops for cyclomatic complexity" />
-<check label="do not consider exit statements as gotos"
-                   line="1"  column="1"
+                   tip="Do not count static loops for cyclomatic complexity" />
+<check label="Do not consider exit statements as gotos"
+                   line="2"  column="1"
                    switch="-ne"
-                   tip="do not consider exit statements as gotos when """
+                   tip="Do not consider exit statements as gotos when """
             """computing Essential Complexity" />
-<check label="extra exit points in subprograms"
-                   line="1"  column="1"
+<check label="Extra exit points in subprograms"
+                   line="2"  column="1"
                    switch="--extra-exit-points"
-                   tip="extra exit points in subprograms" />
-<check label="all line metrics"
-                   line="1"  column="2"
+                   tip="Extra exit points in subprograms" />
+<check label="All line metrics"
+                   line="1"  column="1"
                    switch="--lines-all"
-                   tip="all line metrics" />
-<check label="number of all lines"
-                   line="1"  column="2"
+                   tip="All line metrics" />
+<check label="Total number of lines"
+                   line="1"  column="1"
                    switch="--lines"
-                   tip="number of all lines" />
-<check label="number of code lines"
-                   line="1"  column="2"
+                   tip="Total number of lines" />
+<check label="Number of code lines"
+                   line="1"  column="1"
                    switch="--lines-code"
-                   tip="number of code lines" />
-<check label="number of comment lines"
-                   line="1"  column="2"
+                   tip="Number of code lines" />
+<check label="Number of comment lines"
+                   line="1"  column="1"
                    switch="--lines-comment"
-                   tip="number of comment lines" />
-<check label="number of code lines also containing comments"
-                   line="1"  column="2"
+                   tip="Number of comment lines" />
+<check label="Number of code lines with comments"
+                   line="1"  column="1"
                    switch="--lines-eol-comment"
-                   tip="number of code lines also containing comments" />
-<check label="comment/code lines percentage"
-                   line="1"  column="2"
+                   tip="Number of code lines with end-of-line comments" />
+<check label="Comment lines percentage"
+                   line="1"  column="1"
                    switch="--lines-ratio"
-                   tip="comment/code lines percentage" />
-<check label="number of blank lines"
-                   line="1"  column="2"
+                   tip="Ratio between the number of lines that contain """
+            """comments and the total number of non-blank lines" />
+<check label="Number of blank lines"
+                   line="1"  column="1"
                    switch="--lines-blank"
-                   tip="number of blank lines" />
-<check label="average number of code lines in a body"
-                   line="1"  column="2"
+                   tip="Number of blank lines" />
+<check label="Average number of code lines in bodies"
+                   line="1"  column="1"
                    switch="--lines-average"
-                   tip="average number of code lines in a body" />
-<check label="all syntax element metrics"
-                   line="2"  column="1"
+                   tip="Average number of code lines in subprogram bodies, """
+            """task bodies, entry bodies and statement sequences in """
+            """package bodies" />
+<check label="All syntax element metrics"
+                   line="1"  column="2"
                    switch="--syntax-all"
-                   tip="all syntax element metrics" />
-<check label="total number of declarations"
-                   line="2"  column="1"
+                   tip="All syntax element metrics" />
+<check label="Number of declarations"
+                   line="1"  column="2"
                    switch="--declarations"
-                   tip="total number of declarations" />
-<check label="total number of statements"
-                   line="2"  column="1"
+                   tip="Total number of declarations" />
+<check label="Number of statements"
+                   line="1"  column="2"
                    switch="--statements"
-                   tip="total number of statements" />
-<check label="number of public subprograms in a compilation unit"
-                   line="2"  column="1"
+                   tip="Total number of statements" />
+<check label="Number of public subprograms in a unit"
+                   line="1"  column="2"
                    switch="--public-subprograms"
-                   tip="number of public subprograms in a compilation unit" />
-<check label="number of subprograms in a compilation unit"
-                   line="2"  column="1"
+                   tip="Number of public subprograms in a compilation unit" />
+<check label="Number of subprograms in a unit"
+                   line="1"  column="2"
                    switch="--all-subprograms"
-                   tip="number of subprograms in a compilation unit" />
-<check label="number of public types in a compilation unit"
-                   line="2"  column="1"
+                   tip="Number of subprograms in a compilation unit" />
+<check label="Number of public types in a unit"
+                   line="1"  column="2"
                    switch="--public-types"
-                   tip="number of public types in a compilation unit" />
-<check label="number of types in a compilation unit"
-                   line="2"  column="1"
+                   tip="Number of public types in a compilation unit" />
+<check label="Number of types in a unit"
+                   line="1"  column="2"
                    switch="--all-types"
-                   tip="number of types in a compilation unit" />
-<check label="maximal unit nesting level"
-                   line="2"  column="1"
+                   tip="Number of types in a compilation unit" />
+<check label="Maximal unit nesting level"
+                   line="1"  column="2"
                    switch="--unit-nesting"
-                   tip="maximal unit nesting level" />
-<check label="maximal construct nesting level"
-                   line="2"  column="1"
+                   tip="Maximal unit nesting level" />
+<check label="Maximal construct nesting level"
+                   line="1"  column="2"
                    switch="--construct-nesting"
-                   tip="maximal construct nesting level" />
-<check label="all coupling metrics"
+                   tip="Maximal construct nesting level" />
+<check label="All coupling metrics"
                    line="2"  column="2"
                    switch="--coupling-all"
-                   tip="all coupling metrics" />
-<check label="tagged (class) fan-out coupling"
+                   tip="All coupling metrics" />
+<check label="Tagged (class) fan-out coupling"
                    line="2"  column="2"
                    switch="--tagged-coupling-out"
-                   tip="tagged (class) fan-out coupling" />
-<check label="tagged (class) fan-in coupling"
+                   tip="Tagged (class) fan-out coupling" />
+<check label="Tagged (class) fan-in coupling"
                    line="2"  column="2"
                    switch="--tagged-coupling-in"
-                   tip="tagged (class) fan-in coupling" />
-<check label="hierarchy (category) fan-out coupling"
+                   tip="Tagged (class) fan-in coupling" />
+<check label="Hierarchy (category) fan-out coupling"
                    line="2"  column="2"
                    switch="--hierarchy-coupling-out"
-                   tip="hierarchy (category) fan-out coupling" />
-<check label="hierarchy (category) fan-in coupling"
+                   tip="Hierarchy (category) fan-out coupling" />
+<check label="Hierarchy (category) fan-in coupling"
                    line="2"  column="2"
                    switch="--hierarchy-coupling-in"
-                   tip="hierarchy (category) fan-in coupling" />
-<check label="unit fan-out coupling"
+                   tip="Hierarchy (category) fan-in coupling" />
+<check label="Unit fan-out coupling"
                    line="2"  column="2"
                    switch="--unit-coupling-out"
-                   tip="unit fan-out coupling" />
-<check label="unit fan-in coupling"
+                   tip="Unit fan-out coupling" />
+<check label="Unit fan-in coupling"
                    line="2"  column="2"
                    switch="--unit-coupling-in"
-                   tip="unit fan-in coupling" />
-<check label="control fan-out coupling"
+                   tip="Unit fan-in coupling" />
+<check label="Control fan-out coupling"
                    line="2"  column="2"
                    switch="--control-coupling-out"
-                   tip="control fan-out coupling" />
-<check label="control fan-in coupling"
+                   tip="Control fan-out coupling" />
+<check label="Control fan-in coupling"
                    line="2"  column="2"
                    switch="--control-coupling-in"
-                   tip="control fan-in coupling" />
+                   tip="Control fan-in coupling" />
    </switches>
 </target-model>
 
