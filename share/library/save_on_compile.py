bd0bd968066e6456bda4cd8cb10f5d7d4a424251
Emmanuel Briot <briot@adacore.com>
2016-02-23 18:12:39 +0100

    Better error message when the file name preference is not set
    P223-004
    
    Change-Id: Ia6c68da7267534312317f48c07370edc8f109fd5

--- share/library/save_on_compile.py
+++ share/library/save_on_compile.py
@@ -1,27 +1,42 @@
 """
 Automatically saves the contents of the Messages window when a
 compilation has finished.
-The output is saved in a file called "messages.txt" in the root project's
-object directory. If there is no object directory for the root project, then
-the file will be saved at the root directory of the project itself.
+
+The output is saved in a file called "messages.txt" (or some other name set in
+the preferences) in the root project's object directory. If there is no object
+directory for the root project, then the file is saved in the directory of the
+project file itself.
+
 """
 
-from GPS import Preference, Project, Console, Hook
+from GPS import Preference, Project, Console
 from os.path import dirname, join
+from gps_utils import hook
 
 
 file_name_pref = Preference("Plugins/save_on_compile/file_name").create(
     "File Name", "string",
     "Name of the file you want to save the messages into",
-    "messages.txt"
-)
+    "messages.txt")
 
 
-def on_compilation_finished(hook, category, target_name, mode_name, status):
+@hook('compilation_finished')
+def on_compilation_finished(category, target_name, mode_name, status):
     obj_dirs = Project.root().object_dirs(False)
     path = obj_dirs[0] if obj_dirs else dirname(Project.root().file().name())
+    base = file_name_pref.get()
 
-    with file(join(path, file_name_pref.get()), "w") as f:
-        f.write(Console().get_text())
-
-Hook("compilation_finished").add(on_compilation_finished)
+    if not base:
+        Console().write(
+            "plugin save_on_compile.py: no file name is specified in the"
+            + " preferences\n")
+    else:
+        try:
+            full = join(path, base)
+            with open(full, "w") as f:
+                f.write(Console().get_text())
+            Console().write("Output saved in %s\n" % (full, ))
+        except:
+            Console().write(
+                "plugin save_on_compile.py: error saving in '%s'\n" % (
+                    full, ))
