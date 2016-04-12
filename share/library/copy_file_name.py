9acd04e266845aa673311b697cd2c4185f5aa9c5
Emmanuel Briot <briot@adacore.com>
2016-04-12 09:36:33 +0200

    New plug-in copy_file_name.py
    P408-036
    
    
    Change-Id: Ic69b52738c7e8ecb9f0dc9d85f7a87c904cb782f

---
+++ share/library/copy_file_name.py
@@ -0,0 +1,35 @@
+"""
+This plug-in creates a new action in GPS (which you can bind to a key
+shortcut or a menu), which copies the name of the current file into
+the system clipboard. You can then paste it in any application.
+
+The current file is either the select file (if for instance the Project view
+has the focus), or the name of the last editor that had the focus otherwise.
+"""
+
+import GPS
+import gps_utils
+from gi.repository import Gtk
+
+
+@gps_utils.interactive()
+def copy_file_name():
+    """
+    Copy the name of the current file in the clipboard, so that it can be
+    pasted elsewhere.
+    """
+
+    ctxt = GPS.current_context()
+    if ctxt.file():
+        f = ctxt.file().name()
+    elif ctxt.directory():
+        f = ctxt.directory()
+    else:
+        b = GPS.EditorBuffer.get(open=False)
+        if b:
+            f = b.file().name()
+
+    if not f:
+        GPS.Console().write("No file found\n")
+    else:
+        GPS.Clipboard.copy(f)
