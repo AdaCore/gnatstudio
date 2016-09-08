AdaCore/gnatstudio

commit 84a20bbb2b1a04fdb5394e4e740dfd2de4d6196f

In Emacs mode, move to previous/next page must preserve mark location
P902-013

Change-Id: I15c701a0e2830b756260b6a4964570004d32b842

src_editor/src/src_editor_view-commands.adb
@@ -133,19 +133,10 @@ package body Src_Editor_View.Commands is
       if Command.Kind = Page then
          Scrolled := Gtk_Scrolled_Window (Get_Parent (View));
          Adj      := Get_Vadjustment (Scrolled);
-         if Command.Step > 0 then
-            Set_Value
-              (Adj,
-               Get_Value (Adj)
-               + Gdouble (Command.Step) * Get_Page_Increment (Adj));
-         else
-            Set_Value
-              (Adj,
-               Get_Value (Adj)
-               + Gdouble (Command.Step) * Get_Page_Increment (Adj));
-         end if;
-         Moved := Place_Cursor_Onscreen (View);
-         Moved := Move_Mark_Onscreen (View, Mark);
+         Adj.Set_Value
+           (Adj.Get_Value + Gdouble (Command.Step) * Adj.Get_Page_Increment);
+         Moved := Move_Mark_Onscreen (View, Buffer.Get_Insert);
+
       else
 
          for Cursor of Get_Cursors (Buffer) loop

distrib/known-problems-62
@@ -24,6 +24,20 @@ If no tag is specified, the entry is relevant to all IDEs.
 
 Known problems fixed in GPS 17.0 / GNATbench 17.0
 -------------------------------------------------
+
+KP-62-P902-013 GPS: emacs-mode page up/down loses selection
+
+  Problem:    If you have activated the Emacs mode in the preferences Key
+              Shortcuts, you can use ctrl-space to set a mark. Keyboard
+              navigation then always extends the selection. But using key
+              shortcuts like ctrl-v (move to next page) or alt-v (move to
+              previous page) resets the location of the mark and thus
+              the selection.
+
+  Status:     This was fixed in 17.0 on 2016-09-07
+
+  Workaround: Use the PageUp and PageDown keys on your keyboard.
+
 KP-62-P822-023 GPS: Command line parameter "debug" may hung GPS
 
   Problem:    GPS may hung when a project contains a case statement

commit 74cab430fcfeb43f691abd753ae0a87d814cae3b

Better support for Emacs-like selection
Reimplement the patch 9231849c4beefb24cdc3348900d7b3b5be2b0419

Move the "Extend_Selection" flag from the EditorView to the
 EditorBuffer, since this is where we handle the selection. Provide
 backward compatibility via a python extension (even though this flag
 is likely never used outside of GPS).
Clarify the semantics of the flag: we do not want to create a new
 selection when none exists yet. But after the user has created a
 mark, we want to always extend the selection.

Fixes issue introduced in 84a20bbb2b1a04fdb5394e4e740dfd2de4d6196f,
 where pressing Ctrl-V in Emacs mode would always create a selection
 even if none existed.

Fix handling of selection when moving to next or previous subprogram,
 as reported in
P902-013 (1/2)

Change-Id: Ibb9370742fa865095a262510d85f5ad99846f173

docs/users_guide/GPS/__init__.py
@@ -3355,6 +3355,19 @@ class EditorBuffer(object):
     manipulated through this class.
     """
 
+    extend_existing_selection = False
+    """
+    When set to True, this flag puts the editor in a special mode where all
+    cursor moves will create and extend the selection.
+    This is used to emulate the behavior of some editors, like Emacs, or
+    vi's "v" mode".
+
+    The default behavior is that cursor moves will cancel any existing
+    selection, unless they are associated with the :kbd:`shift` key. In
+    this case, a new selection is created if none exists, and the selection
+    is extended to include the new cursor location.
+    """
+
     def __init__(self):
         """
         Prevents the direct creation of instances of
@@ -4758,15 +4771,6 @@ class EditorView(GUI):
         """
         pass  # implemented in Ada
 
-    def get_extend_selection(self):
-        """
-
-        :rtype: bool
-        :return: A boolean, whether the user is currently performing a
-           selection. This should impact cursor movements (since moving
-           the cursor should extend the selection).
-        """
-
     def goto(self, location, extend_selection=False):
         """
         Moves the cursor to the given location. Each view of a particular
@@ -4790,17 +4794,6 @@ class EditorView(GUI):
         .. seealso:: :func:`GPS.EditorBuffer.is_read_only`
         """
         pass  # implemented in Ada
-
-    def set_extend_selection(self, extend):
-        """
-        Sets the mode for cursor movement. When the parameter is true,
-        moving the cursor extends the selection (for instance shift+cursor
-        keys, or the Emacs mode of setting the mark with ctrl-space and
-        then moving the cursor). When the parameter is false, cursor
-        movement cancels the selection.
-
-        :param bool extend: A boolean
-        """
 
     def set_read_only(self, read_only=True):
         """

kernel/src_info/gps-editors.ads
@@ -401,18 +401,6 @@ package GPS.Editors is
    --  Get_User_Data needs to be used to convert to a MDI_Child -- this is
    --  to prevent the need for a No_Strict_Aliasing pragma on MDI_Child).
 
-   function Get_Extend_Selection
-     (This : Editor_View) return Boolean is abstract;
-   --  Get the value of the Extend_Selection property. This property determines
-   --  if any movement command will extend the selection without any modifiers
-   --  or not. Note that for user-defined actions, this depends on the user
-   --  actually reading this flag and implementing the correct behavior
-
-   procedure Set_Extend_Selection
-     (This : Editor_View; Extend_Selection : Boolean) is abstract;
-   --  Set the value of the Extend_Selection property. Default value is False,
-   --  which means, editor commands don't extend the user's selection.
-
    package View_Lists is new Ada.Containers.Indefinite_Doubly_Linked_Lists
      (Editor_View'Class);
 
@@ -711,6 +699,30 @@ package GPS.Editors is
        Lang   : Language.Language_Access) is null;
    --  Set the language of the given buffer. The syntax highlighting is
    --  recomputed using the new language.
+
+   procedure Set_Extend_Existing_Selection
+     (This : Editor_Buffer; Extend : Boolean) is abstract;
+   function Extend_Existing_Selection
+     (This : Editor_Buffer) return Boolean is abstract;
+   --  This mode controls the handling of selection when the cursor is
+   --  moved:
+   --  * for subprograms that receive an Extend_Selection parameter set to
+   --    True, GPS will always create a new selection if none exists, and
+   --    then extend it to include the new location of the cursor. This is
+   --    the standard handling of shortcuts like shift-arrow keys
+   --  * for subprograms that receive an Extend_Selection parameter set to
+   --    False, the behavior depends on Always_Extend_Selection:
+   --       - if False, then GPS always resets and cancels the current
+   --         selection when the cursor moves. This is the standard behavior
+   --         when using arrow keys for instance.
+   --       - if True, GPS creates a new selection, and extend it.
+   --         This is the standard Emacs mode where the mark is set
+   --         independently (or vi's "v" mode).
+   --         ??? This behavior requires that the emacs mode sets and unsets
+   --         this flag when the mark is set or cancelled. Unfortunately, we
+   --         can't simply look at whether there is a selection already,
+   --         because in gtk having no selection or an empty selection is the
+   --         same.
 
    package Buffer_Lists is new Ada.Containers.Indefinite_Doubly_Linked_Lists
      (Editor_Buffer'Class);
@@ -1169,6 +1181,11 @@ private
    overriding function "="
      (This : Dummy_Editor_Buffer; Buffer : Dummy_Editor_Buffer) return Boolean;
 
+   overriding procedure Set_Extend_Existing_Selection
+     (This : Dummy_Editor_Buffer; Extend : Boolean) is null;
+   overriding function Extend_Existing_Selection
+     (This : Dummy_Editor_Buffer) return Boolean is (False);
+
    Nil_Editor_Buffer : constant Editor_Buffer'Class :=
      Dummy_Editor_Buffer'(Controlled with null record);
 
@@ -1205,12 +1222,6 @@ private
 
    overriding function Buffer
      (This : Dummy_Editor_View) return Editor_Buffer'Class;
-
-   overriding function Get_Extend_Selection
-     (This : Dummy_Editor_View) return Boolean is (False);
-
-   overriding procedure Set_Extend_Selection
-     (This : Dummy_Editor_View; Extend_Selection : Boolean) is null;
 
    Nil_Editor_View : constant Editor_View'Class :=
      Dummy_Editor_View'(Controlled with null record);

share/support/core/extensions/__init__.py
@@ -109,6 +109,22 @@ GPS.MessageContext = GPS.Context
 ######################
 
 @extend_gps
+class EditorView(object):
+
+    def set_extend_selection(self, extend):
+        """
+        See :func:`GPS.EditorBuffer.extend_existing_selection`
+        """
+        self.buffer().extend_existing_selection = extend
+
+    def get_extend_selection(self):
+        """
+        See :func:`GPS.EditorBuffer.extend_existing_selection`
+        """
+        return self.buffer().extend_existing_selection
+
+
+@extend_gps
 class EditorBuffer(object):
 
     def insert(self, loc_or_text, text=None):

share/support/core/text_utils.py
@@ -540,8 +540,8 @@ def serialize(increment=1):
             eol = repl.end_of_line()
             if repl + frm_col > eol:
                 buffer.insert(eol,
-                              " " * ((eol - repl) - frm_col + 2)
-                              + format % value)
+                              " " * ((eol - repl) - frm_col + 2) +
+                              format % value)
             else:
                 replace(repl + frm_col, min(repl + end_col, eol),
                         format % value)
@@ -613,9 +613,9 @@ def kill_line(location=None, count=1):
             end = bol.end_of_line()
             str = buffer.get_chars(start, end)
             strip_str = str.rstrip()
-            if (count == 1
-                    and len(str) > 0
-                    and str[len(str) - 1] == '\n' and strip_str != ""):
+            if (count == 1 and
+               len(str) > 0 and
+               str[len(str) - 1] == '\n' and strip_str != ""):
                 end = end.forward_char(-1)
             bol = end + 1
 
@@ -718,8 +718,7 @@ def _goto_line_bound(beginning, extend_selection):
                 else:
                     d = d.end_of_line()
 
-                c.move(d, extend_selection or
-                       ed.current_view().get_extend_selection())
+                c.move(d, extend_selection or ed.extend_existing_selection)
 
 
 @interactive("Editor", filter_text_actions,
@@ -1373,7 +1372,9 @@ def set_mark_command(location=None):
     """
     if not location:
         location = GPS.EditorBuffer.get().current_view().cursor()
-    location.buffer().current_view().set_extend_selection(True)
+
+    location.buffer().extend_existing_selection = True
+
     if has_pygtk:
         location.create_mark("selection_bound")
         override_key_bindings(select=True)
@@ -1390,7 +1391,9 @@ def cancel_mark_command(buffer=None):
     """
     if not buffer:
         buffer = GPS.EditorBuffer.get()
-    buffer.current_view().set_extend_selection(False)
+
+    buffer.extend_existing_selection = False
+
     try:
         buffer.unselect()
         if has_pygtk:

src_editor/src/src_editor_buffer.adb
@@ -4560,6 +4560,39 @@ package body Src_Editor_Buffer is
       return Buffer.Lang;
    end Get_Language;
 
+   -----------------------------
+   -- Should_Extend_Selection --
+   -----------------------------
+
+   function Should_Extend_Selection
+     (Buffer           : not null access Source_Buffer_Record;
+      Extend_Selection : Boolean) return Boolean is
+   begin
+      return Extend_Selection
+        or else Buffer.Extend_Existing_Selection;
+   end Should_Extend_Selection;
+
+   -----------------------------------
+   -- Set_Extend_Existing_Selection --
+   -----------------------------------
+
+   procedure Set_Extend_Existing_Selection
+     (Buffer : not null access Source_Buffer_Record;
+      Extend : Boolean) is
+   begin
+      Buffer.Extend_Existing_Selection := Extend;
+   end Set_Extend_Existing_Selection;
+
+   -------------------------------
+   -- Extend_Existing_Selection --
+   -------------------------------
+
+   function Extend_Existing_Selection
+     (Buffer : not null access Source_Buffer_Record) return Boolean is
+   begin
+      return Buffer.Extend_Existing_Selection;
+   end Extend_Existing_Selection;
+
    -------------------------
    -- Set_Cursor_Position --
    -------------------------
@@ -4593,7 +4626,7 @@ package body Src_Editor_Buffer is
 
       Get_Iter_At_Line_Offset (Buffer, Iter, Line, Column);
 
-      if Extend_Selection then
+      if Buffer.Should_Extend_Selection (Extend_Selection) then
          Move_Mark (Buffer, Buffer.Insert_Mark, Iter);
       else
          Place_Cursor (Buffer, Iter);

src_editor/src/src_editor_buffer.ads
@@ -364,6 +364,25 @@ package Src_Editor_Buffer is
       Column : out Gint);
    --  Same as above, for the cursor position
 
+   procedure Set_Extend_Existing_Selection
+     (Buffer : not null access Source_Buffer_Record;
+      Extend : Boolean);
+   function Extend_Existing_Selection
+     (Buffer        : not null access Source_Buffer_Record) return Boolean;
+   --  See GPS.Editors.Set_Extend_Existing_Selection
+
+   function Should_Extend_Selection
+     (Buffer           : not null access Source_Buffer_Record;
+      Extend_Selection : Boolean) return Boolean;
+   --  Computes whether the selection should be extended as part of moving a
+   --  cursor.
+   --  The behavior depends on the Extend_Selection parameter (which basically
+   --  indicates whether the user has used the modifier, typically 'shift, to
+   --  force this behavior, and on whether Extend_Existing_Selection is true,
+   --  to emulate Emacs-like selection.
+   --  When this function returns True, a new selection should be created, if
+   --  none exists, and then extended to include the new cursor location.
+
    procedure Get_Selection_Bounds
      (Buffer       : access Source_Buffer_Record;
       Start_Line   : out Editable_Line_Type;
@@ -1508,6 +1527,9 @@ private
 
       Non_Editable_Tag : Gtk.Text_Tag.Gtk_Text_Tag;
       --  A tag for text that cannot be interactively deleted
+
+      Extend_Existing_Selection : Boolean := False;
+      --  See Set_Extend_Existing_Selection
 
       Insert_Mark      : Gtk.Text_Mark.Gtk_Text_Mark;
       --  This is a copy of the "insert" mark.

src_editor/src/src_editor_module-editors.adb
@@ -490,6 +490,10 @@ package body Src_Editor_Module.Editors is
    overriding procedure Finalize (This : in out Src_Editor_Buffer);
    overriding function "="
      (This : Src_Editor_Buffer; Buffer : Src_Editor_Buffer) return Boolean;
+   overriding procedure Set_Extend_Existing_Selection
+     (This : Src_Editor_Buffer; Extend : Boolean);
+   overriding function Extend_Existing_Selection
+     (This : Src_Editor_Buffer) return Boolean;
 
    function Convert is new Ada.Unchecked_Conversion
      (Buffer_Reference_Access, System.Address);
@@ -545,12 +549,6 @@ package body Src_Editor_Module.Editors is
      (Contents : System.Address; View : System.Address);
    pragma Convention (C, On_View_Destroyed);
    --  Called when the gtk+ object is destroyed, to reset internal fields
-
-   overriding function Get_Extend_Selection
-     (This : Src_Editor_View) return Boolean;
-
-   overriding procedure Set_Extend_Selection
-     (This : Src_Editor_View; Extend_Selection : Boolean);
 
    ------------------------
    -- Src_Editor_Overlay --
@@ -2432,25 +2430,25 @@ package body Src_Editor_Module.Editors is
       end if;
    end Delete;
 
-   --------------------------
-   -- Get_Extend_Selection --
-   --------------------------
+   -------------------------------
+   -- Extend_Existing_Selection --
+   -------------------------------
 
-   overriding function Get_Extend_Selection
-     (This : Src_Editor_View) return Boolean is
+   overriding function Extend_Existing_Selection
+     (This : Src_Editor_Buffer) return Boolean is
    begin
-      return This.Contents.Box.Get_View.Get_Extend_Selection;
-   end Get_Extend_Selection;
+      return This.Contents.Buffer.Extend_Existing_Selection;
+   end Extend_Existing_Selection;
 
-   --------------------------
-   -- Set_Extend_Selection --
-   --------------------------
+   -----------------------------------
+   -- Set_Extend_Existing_Selection --
+   -----------------------------------
 
-   overriding procedure Set_Extend_Selection
-     (This : Src_Editor_View; Extend_Selection : Boolean) is
+   overriding procedure Set_Extend_Existing_Selection
+     (This : Src_Editor_Buffer; Extend : Boolean) is
    begin
-      This.Contents.Box.Get_View.Set_Extend_Selection (Extend_Selection);
-   end Set_Extend_Selection;
+      This.Contents.Buffer.Set_Extend_Existing_Selection (Extend);
+   end Set_Extend_Existing_Selection;
 
    -------------------
    -- Set_Read_Only --

src_editor/src/src_editor_module-shell.adb
@@ -2056,6 +2056,15 @@ package body Src_Editor_Module.Shell is
            (Create_Language_Info
               (Get_Script (Data), Get_Buffer (Data, 1).Get_Language));
 
+      elsif Command = "extend_existing_selection" then
+         if Data.Number_Of_Arguments = 2 then
+            Get_Buffer (Data, 1).Set_Extend_Existing_Selection
+              (Data.Nth_Arg (2));
+         else
+            Data.Set_Return_Value
+              (Get_Buffer (Data, 1).Extend_Existing_Selection);
+         end if;
+
       else
          Set_Error_Msg (Data, -"Command not implemented: " & Command);
       end if;
@@ -2425,13 +2434,6 @@ package body Src_Editor_Module.Shell is
          Set_Return_Value
            (Data, Create_Editor_Location
               (Get_Script (Data), Get_View (Data, 1).Cursor));
-
-      elsif Command = "get_extend_selection" then
-         Data.Set_Return_Value (Get_View (Data, 1).Get_Extend_Selection);
-
-      elsif Command = "set_extend_selection" then
-         Get_View (Data, 1).Set_Extend_Selection (Nth_Arg (Data, 2));
-
       end if;
    end View_Cmds;
 
@@ -2789,7 +2791,6 @@ package body Src_Editor_Module.Shell is
         (Kernel, "apply_overlay",  1, 3, Buffer_Cmds'Access, EditorBuffer);
       Register_Command
         (Kernel, "remove_overlay",  1, 3, Buffer_Cmds'Access, EditorBuffer);
-
       Register_Command
         (Kernel, "add_cursor",  1, 1, Buffer_Cmds'Access, EditorBuffer);
       Register_Command
@@ -2809,7 +2810,6 @@ package body Src_Editor_Module.Shell is
         (Kernel, "main_cursor",  0, 0, Buffer_Cmds'Access, EditorBuffer);
       Register_Command
         (Kernel, "has_slave_cursors",  0, 0, Buffer_Cmds'Access, EditorBuffer);
-
       Register_Command
         (Kernel, "file", 0, 0, Buffer_Cmds'Access, EditorBuffer);
       Register_Command
@@ -2898,6 +2898,11 @@ package body Src_Editor_Module.Shell is
       Register_Command
         (Kernel,
          "at", 2, 3, Buffer_Cmds'Access, EditorBuffer);
+      Kernel.Scripts.Register_Property
+        ("extend_existing_selection",
+         Class  => EditorBuffer,
+         Setter => Buffer_Cmds'Access,
+         Getter => Buffer_Cmds'Access);
 
       --  EditorView
 
@@ -2909,10 +2914,6 @@ package body Src_Editor_Module.Shell is
         (Kernel, "set_read_only", 0, 1, View_Cmds'Access, EditorView);
       Register_Command
         (Kernel, "is_read_only", 0, 0, View_Cmds'Access, EditorView);
-      Register_Command
-        (Kernel, "set_extend_selection", 0, 1, View_Cmds'Access, EditorView);
-      Register_Command
-        (Kernel, "get_extend_selection", 0, 0, View_Cmds'Access, EditorView);
       Register_Command (Kernel, "center", 0, 1, View_Cmds'Access, EditorView);
       Register_Command (Kernel, "goto", 1, 2, View_Cmds'Access, EditorView);
       Register_Command (Kernel, "cursor", 0, 0, View_Cmds'Access, EditorView);

src_editor/src/src_editor_view-commands.adb
@@ -122,8 +122,9 @@ package body Src_Editor_View.Commands is
         := Get_Main_Cursor (Buffer);
       Column       : constant Gint :=
         Get_Column_Memory (Get_Main_Cursor (Buffer));
+
       Extend_Selection : constant Boolean :=
-        View.Get_Extend_Selection or Command.Extend_Selection;
+        Buffer.Should_Extend_Selection (Command.Extend_Selection);
 
       pragma Unreferenced (Context, Moved);
 
@@ -135,7 +136,13 @@ package body Src_Editor_View.Commands is
          Adj      := Get_Vadjustment (Scrolled);
          Adj.Set_Value
            (Adj.Get_Value + Gdouble (Command.Step) * Adj.Get_Page_Increment);
-         Moved := Move_Mark_Onscreen (View, Buffer.Get_Insert);
+
+         if Extend_Selection then
+            Moved := Move_Mark_Onscreen (View, Buffer.Get_Insert);
+         else
+            Moved := Place_Cursor_Onscreen (View)
+              or Move_Mark_Onscreen (View, Mark);
+         end if;
 
       else
 

src_editor/src/src_editor_view.adb
@@ -2734,28 +2734,6 @@ package body Src_Editor_View is
       end if;
    end Set_Position_Set_Explicitely;
 
-   --------------------------
-   -- Get_Extend_Selection --
-   --------------------------
-
-   function Get_Extend_Selection
-     (Self : access Source_View_Record) return Boolean
-   is
-   begin
-      return Self.Extend_Selection;
-   end Get_Extend_Selection;
-
-   --------------------------
-   -- Set_Extend_Selection --
-   --------------------------
-
-   procedure Set_Extend_Selection
-     (Self : access Source_View_Record; Extend_Selection : Boolean)
-   is
-   begin
-      Self.Extend_Selection := Extend_Selection;
-   end Set_Extend_Selection;
-
    -----------------
    -- Get_Project --
    -----------------

src_editor/src/src_editor_view.ads
@@ -210,18 +210,6 @@ package Src_Editor_View is
    --  a location. This flag will not do anything on editors that are already
    --  open and scrolled.
 
-   function Get_Extend_Selection
-     (Self : access Source_View_Record) return Boolean;
-   --  Get the value of the Extend_Selection property. This property determines
-   --  if any movement command will extend the selection without any modifiers
-   --  or not. Note that for user-defined actions, this depends on the user
-   --  actually reading this flag and implementing the correct behavior
-
-   procedure Set_Extend_Selection
-     (Self : access Source_View_Record; Extend_Selection : Boolean);
-   --  Set the value of the Extend_Selection property. Default value is False,
-   --  which means, editor commands don't extend the user's selection.
-
    type Location_Type is (Location_Mouse, Location_Cursor, Location_Event);
    function Build_Editor_Context
      (View     : access Source_View_Record;
@@ -382,8 +370,6 @@ private
       Cursor_Needs_Change          : Boolean := False;
       --  Whether we just entered hyper mode, and the cursor aspect needs
       --  to be changed.
-
-      Extend_Selection             : Boolean := False;
 
       Source_Buffer_Handlers       : Handlers_Array (1 .. 6);
    end record;