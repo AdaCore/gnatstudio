5cb563a400d3de797ff3d21c72fc7d320485f001
Andry Ogorodnik <ogorodnik@adacore.com>
2016-10-05 18:11:53 +0300

    PA05-018 Remove single "message" tab from GNATHub report
    Change-Id: I820bf5840b669daaa250420478dd08dc023eab53

--- code_analysis/src/gnathub-reports-collector.adb
+++ code_analysis/src/gnathub-reports-collector.adb
@@ -17,7 +17,6 @@
 
 with Ada.Unchecked_Deallocation;
 
-with Gtk.Notebook;
 with Gtk.Widget;
 with Gtkada.Handlers;
 with GNAThub.Messages;
@@ -52,25 +51,15 @@ package body GNAThub.Reports.Collector is
      (Self       : not null access GNAThub_Report_Collector'Class;
       Kernel     : GPS.Kernel.Kernel_Handle;
       Tree       : Code_Analysis.Code_Analysis_Tree;
-      Severities : GNAThub.Severities_Ordered_Sets.Set)
-   is
-      Notebook : Gtk.Notebook.Gtk_Notebook;
+      Severities : GNAThub.Severities_Ordered_Sets.Set) is
    begin
       Gtk.Box.Initialize_Vbox (Self);
       Self.Kernel := Kernel;
 
-      --  Notebook
-
-      Gtk.Notebook.Gtk_New (Notebook);
-      Self.Pack_Start (Notebook);
-
-      --  Messages report tab
-
       GNAThub.Reports.Messages.Gtk_New
         (Self.Messages_Report, Kernel, Tree, Severities);
 
-      Notebook.Append_Page (Self.Messages_Report);
-      Notebook.Set_Tab_Label_Text (Self.Messages_Report, "Messages");
+      Self.Pack_Start (Self.Messages_Report);
 
       Self.Listener := new Message_Listener (Gtk.Box.Gtk_Vbox (Self));
 
