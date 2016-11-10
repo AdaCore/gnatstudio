AdaCore/gnatstudio

commit d60997b0a0463aa9a2d7cfc02a09b190b7ccd0ce

PA17-045: Fix the 'Edit->Paste Previous' capability
Pastes on Gtk_Text_Buffer widgets can be asynchronous: in this
case, trying to retrieve the text buffer cursor's position right
after a call to Gtk.Text_Buffer.Paste_Clipboard will not return the
cursor's position after the paste.

To avoid this problem, we now connect to the 'paste-done' signal
to retrieve the cursor's position after a paste, since this signal
is emitted every time a paste has been done.

This signal was disabled in Src_Editor_Buffer due to an old bug
in Gtk but this bug does not appear anymore.

Change-Id: I51ebfab1f33c6d0bf25b80bd7385b1ef7164e993

kernel/src/gps-kernel-clipboard.adb
@@ -26,6 +26,7 @@ with Glib.Types;                 use Glib.Types;
 with Glib.Object;                use Glib.Object;
 with Gtk.Clipboard;              use Gtk.Clipboard;
 with Gtk.Editable;               use Gtk.Editable;
+with Gtk.Handlers;               use Gtk.Handlers;
 with Gtk.Text_View;              use Gtk.Text_View;
 with Gtk.Text_Buffer;            use Gtk.Text_Buffer;
 with Gtk.Text_Iter;              use Gtk.Text_Iter;
@@ -82,6 +83,15 @@ package body GPS.Kernel.Clipboard is
      (Data : in out Callback_Data'Class; Command : String);
    --  Handles shell commands associated with the clipboard
 
+   package On_Paste_Done_Callback is new Gtk.Handlers.User_Callback
+      (Widget_Type => Gtk_Text_Buffer_Record,
+       User_Type   => Clipboard_Access);
+   procedure On_Paste_Done
+      (Self      : access Gtk_Text_Buffer_Record'Class;
+       Clipboard : Clipboard_Access);
+   --  Called when a paste has been done. Used to retrieve the text buffer
+   --  cursor's position.
+
    -------------
    -- Execute --
    -------------
@@ -105,6 +115,20 @@ package body GPS.Kernel.Clipboard is
          Clipboard_Changed_Hook.Run (Kernel);
       end if;
    end Execute;
+
+   -------------------
+   -- On_Paste_Done --
+   -------------------
+
+   procedure On_Paste_Done
+      (Self      : access Gtk_Text_Buffer_Record'Class;
+       Clipboard : Clipboard_Access)
+   is
+      Iter : Gtk_Text_Iter;
+   begin
+      Get_Iter_At_Mark (Self, Iter, Get_Insert (Self));
+      Clipboard.Last_Position := Get_Offset (Iter);
+   end On_Paste_Done;
 
    ----------------------
    -- Create_Clipboard --
@@ -454,15 +478,22 @@ package body GPS.Kernel.Clipboard is
                Result := Delete_Selection (Buffer, False, False);
             end if;
 
+            --  Connect to the 'paste-done' signal to retrieve the cursor's
+            --  position after a paste.
+            On_Paste_Done_Callback.Object_Connect
+               (Buffer,
+                Name        => Gtk.Text_Buffer.Signal_Paste_Done,
+                Cb          => On_Paste_Done'Access,
+                Slot_Object => Buffer,
+                User_Data   => Clipboard_Access (Clipboard),
+                After       => True);
+
             Get_Iter_At_Mark (Buffer, Iter, Get_Insert (Buffer));
             Clipboard.First_Position := Get_Offset (Iter);
 
             Paste_Clipboard
-              (Buffer, Gtk.Clipboard.Get,
-               Default_Editable => Default_Editable);
-
-            Get_Iter_At_Mark (Buffer, Iter, Get_Insert (Buffer));
-            Clipboard.Last_Position := Get_Offset (Iter);
+               (Buffer, Gtk.Clipboard.Get,
+                Default_Editable => Default_Editable);
          end if;
       end if;
    end Paste_Clipboard;

src_editor/src/src_editor_buffer.adb
@@ -489,10 +489,6 @@ package body Src_Editor_Buffer is
    --  Return the location category corresponding to errors when converting
    --  to Charset.
 
-   procedure On_Paste_Done (Buffer : access Source_Buffer_Record'Class);
-   --  Disable the "paste-done" signal introduced in gtk+ 2.18, which breaks
-   --  the handling of multiple views
-
    procedure Update_Logical_Timestamp
      (Buffer : access Source_Buffer_Record'Class);
    pragma Inline (Update_Logical_Timestamp);
@@ -3224,15 +3220,6 @@ package body Src_Editor_Buffer is
       Recalculate_Side_Column_Width (Buffer);
    end Initialize_Hook;
 
-   -------------------
-   -- On_Paste_Done --
-   -------------------
-
-   procedure On_Paste_Done (Buffer : access Source_Buffer_Record'Class) is
-   begin
-      Emit_Stop_By_Name (Buffer, "paste-done");
-   end On_Paste_Done;
-
    ----------------
    -- Initialize --
    ----------------
@@ -3332,24 +3319,6 @@ package body Src_Editor_Buffer is
       --  Initialize the queue for editor commands
 
       Buffer.Queue := New_Queue;
-
-      --  Workaround a bug in gtk+ 2.18: when a paste was completed, the
-      --  paste-done signal is emitted, and results in scrolling _all_ views
-      --  to the cursor location (which breaks the handling of multiple views).
-      --  The simplest workaround is simply to not emit that signal at all
-      --  (GPS takes care of the scrolling for the active view anyway).
-      --  The RH for this signal in gtk+ is:
-      --      Add a "paste-done" signal and use it to properly scroll the
-      --      view at the end of the pasted text in the case of an async paste
-
-      if Glib.Object.Lookup
-        (Get_Type (Buffer), "paste-done") /= Null_Signal_Id
-      then
-         Buffer_Callback.Connect
-           (Buffer, "paste-done",
-            Marsh => Buffer_Callback.To_Marshaller (On_Paste_Done'Access),
-            After => False);
-      end if;
 
       --  And finally, connect ourselves to the interesting signals
 

commit 29178c3dd0da013b576636a49b692e5e3c676743

Fix scrolling when pasting with multiple views
Change d60997b0a0463aa9a2d7cfc02a09b190b7ccd0ce introduced a
regression. If we have multiple views of the same buffer, then
pasting in any of the view will scroll all of them (the default
behavior of gtk+, which we were overriding).
That commit was also adding one callback on paste-done every time
we did a paste on a buffer, resulting in extra work

PA17-045

Change-Id: I4558d687fbfaed4b38ebea7e43a5a588dfca4060

kernel/src/gps-kernel-clipboard.adb
@@ -26,9 +26,7 @@ with Glib.Types;                 use Glib.Types;
 with Glib.Object;                use Glib.Object;
 with Gtk.Clipboard;              use Gtk.Clipboard;
 with Gtk.Editable;               use Gtk.Editable;
-with Gtk.Handlers;               use Gtk.Handlers;
 with Gtk.Text_View;              use Gtk.Text_View;
-with Gtk.Text_Buffer;            use Gtk.Text_Buffer;
 with Gtk.Text_Iter;              use Gtk.Text_Iter;
 with Gtk.Tree_View;              use Gtk.Tree_View;
 with Gtk.Widget;                 use Gtk.Widget;
@@ -83,15 +81,6 @@ package body GPS.Kernel.Clipboard is
      (Data : in out Callback_Data'Class; Command : String);
    --  Handles shell commands associated with the clipboard
 
-   package On_Paste_Done_Callback is new Gtk.Handlers.User_Callback
-      (Widget_Type => Gtk_Text_Buffer_Record,
-       User_Type   => Clipboard_Access);
-   procedure On_Paste_Done
-      (Self      : access Gtk_Text_Buffer_Record'Class;
-       Clipboard : Clipboard_Access);
-   --  Called when a paste has been done. Used to retrieve the text buffer
-   --  cursor's position.
-
    -------------
    -- Execute --
    -------------
@@ -121,12 +110,12 @@ package body GPS.Kernel.Clipboard is
    -------------------
 
    procedure On_Paste_Done
-      (Self      : access Gtk_Text_Buffer_Record'Class;
-       Clipboard : Clipboard_Access)
+     (Clipboard : not null access Clipboard_Record;
+      Buffer    : not null access Gtk_Text_Buffer_Record'Class)
    is
       Iter : Gtk_Text_Iter;
    begin
-      Get_Iter_At_Mark (Self, Iter, Get_Insert (Self));
+      Get_Iter_At_Mark (Buffer, Iter, Get_Insert (Buffer));
       Clipboard.Last_Position := Get_Offset (Iter);
    end On_Paste_Done;
 
@@ -477,16 +466,6 @@ package body GPS.Kernel.Clipboard is
             if Selection_Exists (Buffer) then
                Result := Delete_Selection (Buffer, False, False);
             end if;
-
-            --  Connect to the 'paste-done' signal to retrieve the cursor's
-            --  position after a paste.
-            On_Paste_Done_Callback.Object_Connect
-               (Buffer,
-                Name        => Gtk.Text_Buffer.Signal_Paste_Done,
-                Cb          => On_Paste_Done'Access,
-                Slot_Object => Buffer,
-                User_Data   => Clipboard_Access (Clipboard),
-                After       => True);
 
             Get_Iter_At_Mark (Buffer, Iter, Get_Insert (Buffer));
             Clipboard.First_Position := Get_Offset (Iter);

kernel/src/gps-kernel-clipboard.ads
@@ -16,6 +16,7 @@
 ------------------------------------------------------------------------------
 
 with Glib.Object;
+with Gtk.Text_Buffer;     use Gtk.Text_Buffer;
 with Gtk.Widget;
 with GNAT.Strings;
 
@@ -98,6 +99,12 @@ package GPS.Kernel.Clipboard is
 
    procedure Register_Commands (Kernel : access Kernel_Handle_Record'Class);
    --  Register shell commands associated with the clipboard
+
+   procedure On_Paste_Done
+     (Clipboard : not null access Clipboard_Record;
+      Buffer    : not null access Gtk_Text_Buffer_Record'Class);
+   --  Should be called when a paste has finished
+   --  Used to do setup the paste-previous
 
 private
    type Selection_List_Access is access Selection_List;

src_editor/src/src_editor_buffer.adb
@@ -73,6 +73,7 @@ with GPS.Default_Styles;                  use GPS.Default_Styles;
 with GPS.Intl;                            use GPS.Intl;
 with GPS.Kernel;                          use GPS.Kernel;
 with GPS.Kernel.Charsets;                 use GPS.Kernel.Charsets;
+with GPS.Kernel.Clipboard;                use GPS.Kernel.Clipboard;
 with GPS.Kernel.Contexts;                 use GPS.Kernel.Contexts;
 with GPS.Kernel.Hooks;                    use GPS.Kernel.Hooks;
 with GPS.Kernel.MDI;                      use GPS.Kernel.MDI;
@@ -488,6 +489,10 @@ package body Src_Editor_Buffer is
      (Charset : String) return Basic_Types.UTF8_String;
    --  Return the location category corresponding to errors when converting
    --  to Charset.
+
+   procedure On_Paste_Done (Buffer : access Source_Buffer_Record'Class);
+   --  Disable the "paste-done" signal introduced in gtk+ 2.18, which breaks
+   --  the handling of multiple views
 
    procedure Update_Logical_Timestamp
      (Buffer : access Source_Buffer_Record'Class);
@@ -3215,6 +3220,26 @@ package body Src_Editor_Buffer is
       Recalculate_Side_Column_Width (Buffer);
    end Initialize_Hook;
 
+   -------------------
+   -- On_Paste_Done --
+   -------------------
+
+   procedure On_Paste_Done (Buffer : access Source_Buffer_Record'Class) is
+   begin
+      --  Workaround a bug in gtk+ 2.18: when a paste was completed, the
+      --  paste-done signal is emitted, and results in scrolling _all_ views
+      --  to the cursor location (which breaks the handling of multiple views).
+      --  The simplest workaround is simply to not emit that signal at all
+      --  (GPS takes care of the scrolling for the active view anyway).
+      --  The RH for this signal in gtk+ is:
+      --      Add a "paste-done" signal and use it to properly scroll the
+      --      view at the end of the pasted text in the case of an async paste
+
+      Emit_Stop_By_Name (Buffer, "paste-done");
+
+      On_Paste_Done (Get_Clipboard (Buffer.Kernel), Buffer);
+   end On_Paste_Done;
+
    ----------------
    -- Initialize --
    ----------------
@@ -3314,6 +3339,20 @@ package body Src_Editor_Buffer is
       --  Initialize the queue for editor commands
 
       Buffer.Queue := New_Queue;
+
+      --  Workaround a bug in gtk+ 2.18: when a paste was completed, the
+      --  paste-done signal is emitted, and results in scrolling _all_ views
+      --  to the cursor location (which breaks the handling of multiple views).
+      --  The simplest workaround is simply to not emit that signal at all
+      --  (GPS takes care of the scrolling for the active view anyway).
+      --  The RH for this signal in gtk+ is:
+      --      Add a "paste-done" signal and use it to properly scroll the
+      --      view at the end of the pasted text in the case of an async paste
+
+      Buffer_Callback.Connect
+        (Buffer, "paste-done",
+         Marsh => Buffer_Callback.To_Marshaller (On_Paste_Done'Access),
+         After => False);
 
       --  And finally, connect ourselves to the interesting signals
 