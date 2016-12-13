d92c4c63a108eb4923bd8ff0bc565264bbe60720
Vadim Godunko <godunko@adacore.com>
2016-12-13 12:11:26 +0300

    Add powerpc64-generic-linux-gnu
    For PB08-046
    
    Change-Id: Id881f08c65abe73db9f79db8c3df2317d0bbef6d

--- share/support/core/toolchains.py
+++ share/support/core/toolchains.py
@@ -67,6 +67,12 @@ XML = r"""<?xml version="1.0" ?>
     <debugger/>
   </toolchain>
 
+  <toolchain name="powerpc64-wrs-vxworks7">
+    <debugger/>
+  </toolchain>
+
+  <toolchain name="powerpc64-generic-linux-gnu"/>
+
   <toolchain name="powerpc-wrs-linux"/>
 
   <toolchain name="powerpc-sysgo-pikeos"/>
