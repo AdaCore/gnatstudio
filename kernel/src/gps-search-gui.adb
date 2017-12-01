0e60f5dc2fd744166a78186fff6622d48d7e8864
Vadim Godunko <godunko@adacore.com>
2017-12-01 18:26:59 +0300

    QB28-049 Fix build error with current GNAT.
    Change-Id: I0c4368feb0b986daf7f6d94fc505987ac7e0fd15

--- kernel/src/gps-search-gui.adb
+++ kernel/src/gps-search-gui.adb
@@ -740,7 +740,8 @@ package body GPS.Search.GUI is
       B.Pack_Start (Label, Expand => False);
 
       Gtk_New (Spin, Min => 2.0, Max => 20.0, Step => 1.0);
-      Spin.Set_Value (Gdouble (Pref_Proposals_Per_Provider.Get_Pref));
+      Spin.Set_Value
+        (Gdouble (Integer'(Pref_Proposals_Per_Provider.Get_Pref)));
       B.Pack_Start (Spin, Expand => False);
 
       Kernel_Callback.Connect
