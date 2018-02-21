63eaf61cf9b286479212d9501932e43e779732ef
Zhu Qun-Ying <zhu.qunying@gmail.com>
2018-02-20 16:01:38 -0800

    Update python pep8 to new names

--- share/plug-ins/pep8_integration.py
+++ share/plug-ins/pep8_integration.py
@@ -1,12 +1,12 @@
 """
 This is the python format checker plugin for GPS.
-Its aim is to check identatoin, style when:
+Its aim is to check indentation, style when:
  1 user stop editing
  2 before python script is saved
 """
 
 import sys
-import pep8
+import pycodestyle
 import GPS
 import colorschemes
 from cStringIO import StringIO
@@ -29,7 +29,7 @@ class Pep8_Module(Module):
 
     def __format_check(self, file):
         """
-           Check format using pep8 for python source codes
+           Check format using pycodestyle for python source codes
         """
         # only check python file
         if file.language() == "python":
@@ -51,7 +51,7 @@ class Pep8_Module(Module):
                 source = [i + "\n" for i in s.splitlines()]
 
             with Catch_Stdout() as output:
-                m = pep8.Checker(filename=None, lines=source, report=False)
+                m = pycodestyle.Checker(filename=None, lines=source, report=False)
                 m.check_all()
 
             for i in output:
