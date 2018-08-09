bf5276beb9c2e2539d7e810c6dbd3f112657ea5d
boulanger <boulanger@adacore.com>
2018-08-09 17:13:40 +0200

    R803-001: Correct the test for windows
    Missing ".exe" in the name of the executable.
    
    Change-Id: I864a36ebdc1455c1f8e89d7076cf1baec73cfbd5

--- testsuite/tests/R803-001.console.non_utf_8/test.py
+++ testsuite/tests/R803-001.console.non_utf_8/test.py
@@ -2,12 +2,14 @@
 """Test that a string encoded in Latin-1 is correctly displaid in a Console"""
 from GPS import *
 from gps_utils.internal.utils import *
-
+import sys
 
 @run_test_driver
 def run_test():
     yield execute_action("Build & Run Number 1")
     yield wait_tasks(other_than=known_tasks)
-    gps_assert("Printing àéè" in GPS.Console("Run: accented").get_text(),
+    yield timeout(500)
+    name = "Run: accented" + (".exe" if sys.platform == "win32" else "")
+    gps_assert("Printing àéè" in GPS.Console(name).get_text(),
                True,
                "The Latin-1 characters should have been displayed")
