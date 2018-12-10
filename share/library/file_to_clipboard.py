95357b701cc9f7fc22054627e4c3acfc533e7d88
Adrien Boulanger <boulanger@adacore.com>
2018-12-10 16:00:33 +0100

    RC10-001: Create file_to_clipboard plug-in
    Add actions to copy file path data into the clipboard.
    
    Change-Id: I72f117d99522fb2e83ddaa5bcb3c6db7a4fb13e8

---
+++ share/library/file_to_clipboard.py
@@ -0,0 +1,42 @@
+"""
+This plugin adds 3 actions:
+    - Copy the full path of the current file to the clipboard
+    - Copy the full path of the current dir to the clipboard
+    - Copy the name of the current file to the clipboard
+"""
+
+import GPS
+import os
+from gps_utils import make_interactive
+
+MENU_PATH = "Edit/Clipboard/"
+CONTEXTUAL_PATH = "Clipboard/"
+
+
+def file_path_to_clipboard():
+    "Copy the full path of the current path to the clipboard"
+    GPS.Clipboard.copy(GPS.current_context().file().name())
+
+
+def dir_to_clipboard():
+    "Copy the full path of the current directory to the clipboard"
+    GPS.Clipboard.copy(GPS.current_context().directory())
+
+
+def name_to_clipboard():
+    "Copy the name of the current path to the clipboard"
+    GPS.Clipboard.copy(os.path.basename(GPS.current_context().file().name()))
+
+
+def custom_make_interactive(name, action):
+    make_interactive(callback=action,
+                     name=name,
+                     category="Clipboard",
+                     filter="File",
+                     menu=MENU_PATH + name,
+                     contextual=CONTEXTUAL_PATH + name)
+
+
+custom_make_interactive("Copy file path", file_path_to_clipboard)
+custom_make_interactive("Copy directory path", dir_to_clipboard)
+custom_make_interactive("Copy file name", name_to_clipboard)
