f8738d0f21e21e16eb0137348f7b6aee398b766f
Nicolas Setton <setton@adacore.com>
2018-12-12 15:44:45 -0500

    Add test for O119-027
    Change-Id: I72af3a391965d7f7446ddccbeb747c3016a29c1e

---
+++ testsuite/tests/O119-027.editor.align_colons/align.adb
@@ -0,0 +1,6 @@
+procedure Align is
+   X   : Integer;
+   Yyyyy : Integer;
+begin
+   null;
+end Align;
---
+++ testsuite/tests/O119-027.editor.align_colons/test.py
@@ -0,0 +1,19 @@
+from gps_utils.internal.utils import run_test_driver, gps_assert
+
+expected = """procedure Align is
+   X     : Integer;
+   Yyyyy : Integer;
+begin
+   null;
+end Align;
+"""
+
+
+@run_test_driver
+def driver():
+    b = GPS.EditorBuffer.get(GPS.File("align.adb"))
+    b.select()
+    GPS.execute_action("Align colons")
+    gps_assert(b.get_chars(),
+               expected,
+               "Align colons failed")
