21080c109303c0dc30221e7b5a39ebe8134f7ab6
Nicolas Setton <setton@adacore.com>
2018-12-14 09:35:45 -0500

    Add test for P626-003
    Change-Id: If911a76a22916568ab95285f077c816a97152b20

---
+++ testsuite/tests/P626-003.xrefs.c_crash/cairo.c
@@ -0,0 +1,12 @@
+#include "cairoint.h"
+
+/**
+  * cairo_set_line_join:
+  * @cr: a cairo context
+  * @line_join: a line join style
+  *
+  **/
+void
+cairo_set_line_join (cairo_t *cr, cairo_line_join_t line_join)
+{
+}
---
+++ testsuite/tests/P626-003.xrefs.c_crash/test.py
@@ -0,0 +1,9 @@
+from gps_utils.internal.utils import run_test_driver, wait_tasks, known_tasks
+
+
+@run_test_driver
+def driver():
+    b = GPS.EditorBuffer.get(GPS.File("cairo.c"))
+    b.insert(b.at(1, 1), "\n")
+    # Give time for libclang to reparse things
+    yield wait_tasks(other_than=known_tasks)
