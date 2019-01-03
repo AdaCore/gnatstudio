22cf269538c8b6e16f519385ff2ad37a079b5b92
Anthony Leonardo Gracio <leonardo@adacore.com>
2019-01-03 12:05:14 +0100

    Update copyright year
    (no-tn-check)
    
    Change-Id: I27c5f49b41225d056dbc326411dd77b9516a766b

--- gnatcoll_extras/src/python_support_gtk.c
+++ gnatcoll_extras/src/python_support_gtk.c
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------
 --                                  G N A T C O L L                         --
 --                                                                          --
---                     Copyright (C) 2008-2018, AdaCore                     --
+--                     Copyright (C) 2008-2019, AdaCore                     --
 --                                                                          --
 -- This is free software;  you can redistribute it  and/or modify it  under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
