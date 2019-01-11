e30d6e48ab3e7d0c00daee3fa6a94bf24c88ae7e
Maxim Reznik <reznik@adacore.com>
2019-01-11 19:06:33 +0200

    S111-039 Drop lal.py module
    Change-Id: I85d8a6acf675079c9bdf02dddec0681369339b15

--- share/support/core/lal_utils/__init__.py
+++ share/support/core/lal_utils/__init__.py
@@ -21,3 +21,22 @@ def node(kind_name, line, column):
 def _wrap_analysis_unit(value):
     c_type = ctypes.cast(value, libadalang.AnalysisUnit._c_type)
     return libadalang.AnalysisUnit._wrap(c_type)
+
+
+def _location(self, open=True):
+    """
+    Return GPS.EditorLocation corresponding to start of given AdaNode.
+    If open=False and there is no open editor with the unit, return None.
+    """
+    file = GPS.File(self.unit.filename)
+    buffer = GPS.EditorBuffer.get(file, open=open)
+
+    if buffer:
+        start = self.token_start.sloc_range.start
+        return buffer.at(int(start.line), int(start.column))
+    else:
+        return None
+
+
+# Register new method in AdaNode
+libadalang.AdaNode.gps_location = _location
