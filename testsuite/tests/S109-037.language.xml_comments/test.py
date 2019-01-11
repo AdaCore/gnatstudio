a17211495c8a7c5c2e783e4ef8083786c2359624
Anthony Leonardo Gracio <leonardo@adacore.com>
2019-01-11 17:36:04 +0100

    S109-037: Fix detection of multiline comments
    Add an automatic test for that too.
    
    Change-Id: Ief55e6de5ef66f099f9a35ba82c390139369aafd

---
+++ testsuite/tests/S109-037.language.xml_comments/test.py
@@ -0,0 +1,27 @@
+"""
+This test checks that we correctly highlight XML multiline
+comments.
+"""
+
+from GPS import *
+from gps_utils.internal.utils import *
+
+
+expected_tags = ["string 1:15 1:19",
+                 "string 1:30 1:41",
+                 "keyword 1:43 1:43",
+                 "comment 3:1 11:1",
+                 "keyword 12:1 12:8",
+                 "keyword 13:1 13:9",
+                 ""]
+
+@run_test_driver
+def run_test():
+    buffer = EditorBuffer.get(File("a.xml"))
+    tags = get_all_tags(buffer)
+    tags_list = tags.split('\n')
+
+    gps_assert(
+        tags_list,
+        expected_tags,
+        "The highlighting is not correct")
