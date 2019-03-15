b86b244d60301a602b5e475cba212ab6a5fa9d43
Anthony Leonardo Gracio <leonardo@adacore.com>
2019-03-15 11:54:04 +0100

    S315-014: Fix the 'Replace all' action
    An automatic test has also been added.
    
    Change-Id: Ib529597c5e4d69fc048a66f28fe97cd8c24f6e5c

---
+++ testsuite/tests/S315-014.search.replace_all_action/test.py
@@ -0,0 +1,24 @@
+"""
+This test checks the good behavior of the 'Replace all'
+action.
+"""
+
+import GPS
+from gps_utils.internal.utils import *
+
+
+@run_test_driver
+def test_driver():
+    GPS.Preference("Ask-Confirmation-For-Replace-All").set(False)
+    buf = GPS.EditorBuffer.get(GPS.File("main.adb"))
+
+    s = dialogs.Search()
+    yield s.open_and_yield()
+    s.set_scope(dialogs.Search.Context.CURRENT_FILE)
+    s.pattern.set_text('Hello')
+    s.replace_text.set_text('Hi')
+
+    GPS.execute_action("replace all")
+    yield wait_tasks(other_than=known_tasks)
+    gps_assert(buf.get_chars(), "Hi Hi Hi\n",
+               "'Replace all' action failed")
