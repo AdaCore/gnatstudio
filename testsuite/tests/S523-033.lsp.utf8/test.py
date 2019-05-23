8539c5461c29d22f4b5826fd4664ecf8737f328a
Nicolas Setton <setton@adacore.com>
2019-05-23 15:53:32 -0400

    S523-033 add test for UTF-8 encoded files using the ALS
    Change-Id: If27856d5d349514de989086641eef416610db919

---
+++ testsuite/tests/S523-033.lsp.utf8/test.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+""" Test for "find all references" in a codebase encoded in utf-8"""
+import os
+import GPS
+import json
+from gps_utils.internal.utils import run_test_driver, gps_assert, \
+                                     wait_tasks
+from workflows.promises import timeout
+
+
+def to_str(m):
+    """Make a string out of a message"""
+    return "{}:{}:{}".format(os.path.basename(m.get_file().name()),
+                             m.get_line(), m.get_column())
+
+
+@run_test_driver
+def driver():
+    # This is an UTF-8 test
+    GPS.Preference("General-Charset").set("UTF-8")
+    main = GPS.EditorBuffer.get(GPS.File("main.adb"))
+
+    #
+    # Goodmorning
+    #
+
+    GPS.Console().clear()
+
+    main.current_view().goto(main.at(4, 4))
+    GPS.execute_action("find all references")
+
+    yield wait_tasks()
+    yield timeout(100)
+
+    # Verify the references
+    m = [to_str(m) for m in GPS.Message.list("References for Goodmorning")]
+    m.sort()
+
+    gps_assert(m,
+               ["main.adb:4:4",
+                "p.ads:2:37"],
+               "references for goodmorning are off")
+
+    gps_assert(GPS.Console().get_text(), "", "the console should be empty")
+
+    #
+    # доброеутро
+    #
+
+    GPS.Console().clear()
+
+    main.current_view().goto(main.at(5, 4))
+    GPS.execute_action("find all references")
+
+    yield wait_tasks()
+    yield timeout(100)
+
+    # Verify the references
+    m = [to_str(m) for m in GPS.Message.list(u"References for доброеутро")]
+    m.sort()
+
+    gps_assert(m,
+               ['main.adb:5:4',
+                'main.adb:6:4',
+                'p.ads:2:14'],
+               u"references for доброеутро are off")
+
+    gps_assert(GPS.Console().get_text(), "", "the console should be empty")
+
+    #
+    # доброеутро*56
+    #
+
+    GPS.Console().clear()
+
+    main.current_view().goto(main.at(7, 4))
+    GPS.execute_action("find all references")
+
+    yield wait_tasks()
+    yield timeout(100)
+
+    # Verify the references
+    m = [to_str(m) for m in GPS.Message.list
+         ("References for " + u"доброеутро" * 56)]
+    m.sort()
+
+    gps_assert(m,
+               ['main.adb:7:4',
+                'p.ads:3:14'],
+               u"references for the long identifier are off")
+
+    gps_assert(GPS.Console().get_text(), "", "the console should be empty")
