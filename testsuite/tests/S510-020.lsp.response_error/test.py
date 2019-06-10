4ee2c1bb9cbd0373068aa36fe9ce1fa0ae8002f6
Vadim Godunko <godunko@adacore.com>
2019-06-10 09:38:12 +0300

    S510-020 Automated test.
    Change-Id: Ia5879fcbca4a4ce4c1a863fcf457e534a19efd2b

---
+++ testsuite/tests/S510-020.lsp.response_error/test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+""" Test for reporting of error for unknown request """
+import GPS
+from gps_utils.internal.utils import run_test_driver, simple_error
+from workflows.promises import timeout
+
+@run_test_driver
+def driver():
+    als = GPS.LanguageServer.get_by_language_name("Ada")
+
+    yield ('language_server_started')
+
+    params = {}
+    result = yield als.request_promise("textDocument/unknown_request", None)
+
+    if not result.is_error:
+        simple_error("we were expecting an error response")
+    msg = GPS.Console("Messages").get_text()
+
+    if "The language server has reported the following error" not in msg:
+        simple_error("error response must be displayed in Messages view")
