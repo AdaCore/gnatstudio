c76e226cc329252d262fc48921ab093022422b59
Fedor Rybin <frybin@adacore.com>
2019-06-26 16:42:49 +0300

    add test for Q220-003
    Change-Id: Id6fb8f078ba753691b31f3f3aebba05027a15865

---
+++ testsuite/tests/Q220-003.project.target_reference/test.py
@@ -0,0 +1,18 @@
+"""
+This test verifies that target reference is taken
+into account for project directories
+"""
+import GPS
+import os.path
+from gps_utils.internal.utils import *
+
+
+@run_test_driver
+def run_test():
+    p = GPS.Project("default")
+    gps_assert(os.path.basename(os.path.normpath((p.exec_dir()))),
+               "obj_",
+               "Target reference ignored in gpr1")
+    #Deactivate this until we have moved to GPR2
+    #Once moved to gpr2 this test should start failing
+    #and be modified accordingly.
