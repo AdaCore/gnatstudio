d0001125275970752e86c66b6166a4e7e3d7c549
Vadim Godunko <godunko@adacore.com>
2019-07-04 10:34:23 +0300

    S702-042 Minor reorganization...
    ... in hope to understad nightly failures better
    
    Change-Id: I5929ea9d52c6593081b77d41e55268165514e6ab

--- testsuite/tests/S613-017.lsp.scenario_variables_changed/test.py
+++ testsuite/tests/S613-017.lsp.scenario_variables_changed/test.py
@@ -13,7 +13,6 @@ def test_driver():
     b = GPS.EditorBuffer.get(f)
 
     b.find_all_refs(b.at(1, 17), True)
-
     yield hook("language_server_response_processed")
     yield wait_tasks()
 
@@ -30,14 +29,13 @@ def test_driver():
     yield wait_tasks()
 
     b.find_all_refs(b.at(1, 17), True)
-
     yield hook("language_server_response_processed")
     yield wait_tasks()
 
-    gps_assert(GPS.Locations.list_locations("References for To_Be_Called (to_be_called.adb:1)", sf.path),
-               [],
-               "wrong list of locations (2/1)")
     gps_assert(GPS.Locations.list_locations("References for To_Be_Called (to_be_called.adb:1)", tf.path),
                [GPS.FileLocation(tf, 1, 6), 'To_Be_Called',
                 GPS.FileLocation(tf, 4, 4), 'To_Be_Called'],
+               "wrong list of locations (2/1)")
+    gps_assert(GPS.Locations.list_locations("References for To_Be_Called (to_be_called.adb:1)", sf.path),
+               [],
                "wrong list of locations (2/2)")
