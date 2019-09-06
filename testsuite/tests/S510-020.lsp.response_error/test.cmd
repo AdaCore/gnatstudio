b4fc8932a008c27a89e78513d5430721a21466e7
Nicolas Setton <setton@adacore.com>
2019-09-06 08:51:27 -0400

    S726-039 adjust test to new mechanism for LSP error reporting
    Change-Id: Ic0693371201383f1531da65c78806d491d591d33

--- testsuite/tests/S510-020.lsp.response_error/test.cmd
+++ testsuite/tests/S510-020.lsp.response_error/test.cmd
@@ -1,1 +1,4 @@
-$GPS --traceon=GPS.LSP.ADA_SUPPORT --load=python:test.py
+# We pass --traceoff=GPS.LSP_CLIENT.ERRORS because we *do* expect
+# an error as part of this test, and we verify in test.py that
+# this is reported.
+$GPS --traceon=GPS.LSP.ADA_SUPPORT --traceoff=GPS.LSP_CLIENT.ERRORS --load=python:test.py
