c8c800f38bba4387dd5b1001c575513b4973f3e0
Adrien Boulanger <boulanger@adacore.com>
2019-09-10 18:08:31 +0200

    S905-018: Stabilize test
    Change-Id: I697fe6c1ad736411a673a2b5b7ff00fa88f60f89

--- testsuite/tests/S905-018.editors.autocasing/test.py
+++ testsuite/tests/S905-018.editors.autocasing/test.py
@@ -9,14 +9,16 @@ EXPECTED = "Hello_World is Natural A and then B"
 @run_test_driver
 def driver():
     GPS.Preference("Ada-Casing-Policy").set("On_The_Fly")
+    # Disable the completion thus wait_idle() will not freeze GPS
+    GPS.Preference("Smart-Completion-Mode").set("Disabled")
     buf = GPS.EditorBuffer.get(GPS.File("main.adb"))
     for ch in EXPECTED.lower():
         send_key_event(ord(ch))
-        yield timeout(200)
+        yield wait_idle()
     gps_assert(buf.get_chars().rstrip(), EXPECTED, "Wrong autocasing")
     buf.current_view().goto(buf.at(1, len(EXPECTED) - 1))
     send_key_event(GDK_RETURN)
-    yield wait_tasks(other_than=known_tasks)
+    yield wait_idle()
     gps_assert(buf.get_chars().rstrip(),
                EXPECTED[:-2] + "\n    " + EXPECTED[-2:],
                "Issue when inserting new line")
