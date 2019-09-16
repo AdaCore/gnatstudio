2f6b342af046ef283082e610831e9cd4aaefbe79
Nicolas Setton <setton@adacore.com>
2019-09-16 14:42:23 -0400

    Add test for S916-074
    Change-Id: I500014eafa13a6c102b75ae3bb7e310f93daa9ff

---
+++ testsuite/tests/S916-074.debug.storage_error/main.adb
@@ -0,0 +1,8 @@
+with Ada.Text_IO; use Ada.Text_IO;
+procedure Main is
+begin
+   for J in 1 .. 4 loop
+      Put_Line ("Put_Line");
+      delay 0.2;
+   end loop;
+end Main;
---
+++ testsuite/tests/S916-074.debug.storage_error/test.py
@@ -0,0 +1,23 @@
+from gps_utils.internal.utils import run_test_driver, gps_assert, wait_tasks
+from pygps import get_widgets_by_type
+from workflows.promises import timeout
+
+
+@run_test_driver
+def driver():
+    # This test launches a debug session, and runs the program in the
+    # debugger. The goal is to check against a memory corruption that
+    # occurs after the process has terminated - the test itself might
+    # not report an issue, but valgrind might.
+    GPS.execute_action("Build & Debug Number 1")
+    yield wait_tasks()
+    GPS.Debugger.get().non_blocking_send("run")
+    yield timeout(2000)
+
+    # sanity check that the test ran
+    view = get_widgets_by_type(Gtk.TextView,
+                               GPS.MDI.get("Debugger Execution").pywidget())[0]
+    buf = view.get_buffer()
+    gps_assert(buf.get_text(buf.get_start_iter(), buf.get_end_iter(), True),
+               "Put_Line\n" * 4,
+               "debug output not visible in console")
