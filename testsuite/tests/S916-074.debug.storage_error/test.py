568d9dd98f2ea6a0ffed63b846ee21d292ebdfde
Anthony Leonardo Gracio <leonardo@adacore.com>
2019-09-27 15:01:52 +0200

    S919-030: Tentative fix for Windows failure
    Change-Id: I320cd3540d31109e274152ef8395b40a6f4c7a9f

--- testsuite/tests/S916-074.debug.storage_error/test.py
+++ testsuite/tests/S916-074.debug.storage_error/test.py
@@ -1,10 +1,13 @@
-from gps_utils.internal.utils import run_test_driver, gps_assert, wait_tasks
+from gps_utils.internal.utils import run_test_driver, gps_assert, \
+    wait_tasks, wait_for_mdi_child
 from pygps import get_widgets_by_type
 from workflows.promises import timeout
 
 
 @run_test_driver
 def driver():
+    GPS.Preference("Debugger-Execution-Window").set(True)
+
     # This test launches a debug session, and runs the program in the
     # debugger. The goal is to check against a memory corruption that
     # occurs after the process has terminated - the test itself might
@@ -13,6 +16,8 @@ def driver():
     yield wait_tasks()
     GPS.Debugger.get().non_blocking_send("run")
     yield timeout(2000)
+
+    yield wait_for_mdi_child("Debugger Execution")
 
     # sanity check that the test ran
     view = get_widgets_by_type(Gtk.TextView,
