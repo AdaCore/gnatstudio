7ae554d70529aa37026ed2ee598c817a6c96f79b
Anthony Leonardo Gracio <leonardo@adacore.com>
2020-01-20 11:04:38 +0100

    T116-019: Fix GNATcheck Project Properties casing
    Change-Id: I93b76660221913fb18bf2c4c8cac908302d336f4

--- share/support/core/gnatcheck.py
+++ share/support/core/gnatcheck.py
@@ -435,7 +435,7 @@ def edit_gnatcheck_rules():
 @hook('gps_started')
 def __on_gps_started():
     GPS.parse_xml("""
-    <tool name="GnatCheck" package="Check" index="Ada" override="false"
+    <tool name="GNATcheck" package="Check" index="Ada" override="false"
     attribute="Default_Switches">
         <language>Ada</language>
         <switches sections="-rules">
