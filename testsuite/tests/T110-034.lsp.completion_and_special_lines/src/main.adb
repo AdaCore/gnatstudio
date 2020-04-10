f9bfb72d896d99fa2f7f4d27c8d161abe99e089d
Anthony Leonardo Gracio <leonardo@adacore.com>
2020-04-10 15:03:57 +0200

    T110-034: Add a test for LSP completion and special lines
    Change-Id: I4158ced0d40cefdb2b1ada6fc49a08627a4dfe65

---
+++ testsuite/tests/T110-034.lsp.completion_and_special_lines/src/main.adb
@@ -0,0 +1,9 @@
+with Bar; use Bar;
+with Ada.Text_IO;
+
+procedure Main is
+   Obj : My_Int := (A => 10);
+begin
+
+   Obj.Do_
+end Main;
---
+++ testsuite/tests/T110-034.lsp.completion_and_special_lines/test.py
@@ -0,0 +1,52 @@
+"""
+This test checks that ALS completion snippets are handled properly
+by GNAT Studio.
+"""
+
+import GPS
+from gs_utils.internal.utils import *
+
+
+EXPECTED_SNIPPET = "  Obj.Do_Nothing (<A : Integer>, <B : Integer>)"
+EXPECTED_RESULT = "  Obj.Do_Nothing (1, 2)"
+
+
+@run_test_driver
+def run_test():
+    buf = GPS.EditorBuffer.get(GPS.File("main.adb"))
+    view = buf.current_view()
+
+    buf.add_special_line(7, "This is a special line")
+
+    view.goto(buf.at(8, 1).end_of_line())
+    yield wait_idle()
+
+    # Insert a completion snippet received from clangd
+    for ch in "Not":
+        send_key_event(ord(ch))
+        yield timeout(100)
+
+    pop_tree = get_widget_by_name("completion-view")
+    click_in_tree(pop_tree, path="0", events=double_click_events)
+
+    # Verify that it has been correctly parsed by the aliases plugin
+    line = buf.get_chars(buf.at(8, 1), buf.at(8, 1).end_of_line())
+    gps_assert(line.strip(), EXPECTED_SNIPPET.strip(),
+               "The completion snippet has not been correctly inserted")
+
+    # Iterate over the snippet params using TAB and give a value to
+    # each of them
+    for ch in "12":
+        send_key_event(ord(ch))
+        yield timeout(50)
+        send_key_event(GDK_TAB)
+        yield timeout(50)
+
+    # Verify that the snippet parameters have been inserted properly
+    line = buf.get_chars(buf.at(8, 1), buf.at(8, 1).end_of_line())
+    gps_assert(line.strip(), EXPECTED_RESULT.strip(),
+               "The snippet paramter values have not been inserted properly")
+
+    # Verify that we jumped to the final tab stop
+    gps_assert(view.cursor(), buf.at(8, 1).end_of_line(),
+               "last TAB did not jump to the snippet final tab stop")
