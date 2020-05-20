30411c0aaba165d4702517774b3d9bff9446d5ff
Romain Beguet <romain-be@hotmail.com>
2020-05-20 10:36:40 +0200

    T330-007: Highlight Ada2020 tokens.
    Change-Id: Id0050e6e201f2a4baa0528ef930eecb7be9b30f2

--- lal/core/lal-highlighters.adb
+++ lal/core/lal-highlighters.adb
@@ -366,6 +366,8 @@ package body LAL.Highlighters is
            Ada_Amp |
            Ada_Arrow |
            Ada_Assign |
+           Ada_Brack_Close |
+           Ada_Brack_Open |
            Ada_Colon |
            Ada_Comma |
            Ada_Diamond |
