d19627286b958e9812c414e875f7036e05cdedb5
Andry Ogorodnik <ogorodnik@adacore.com>
2020-06-01 19:27:03 +0300

    T519-008 Test has been added
    Change-Id: I1d394d03108f1b8d3b7df4783e35ae0c90f79f70

---
+++ testsuite/tests/T519-008.subprogram_parameters_formatting/main.adb
@@ -0,0 +1,4 @@
+procedure Main is
+begin
+   Set_Num_Vector (D, Cps_Result_Name, Strat_Name, Name & "[" & Und & "]", Get_Num_Vector (Cps_Pricing_Dict, Book_Pricing_Result, Sub_Book_Name, Name & "_" & Und & "[" & Und & "]"));
+end Main;
