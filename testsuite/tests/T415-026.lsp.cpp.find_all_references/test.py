fd5f99d2e5278ba3fdfead44d4964441102322b1
Andry Ogorodnik <ogorodnik@adacore.com>
2020-06-09 21:07:00 +0300

    T415-026.lsp.cpp.find_all_references test
    Change-Id: Ie26b14069821557012478b1f410095e9e69559d3

---
+++ testsuite/tests/T415-026.lsp.cpp.find_all_references/test.py
@@ -0,0 +1,29 @@
+"""
+This test checks that find all references works fine through LSP and clangd.
+"""
+
+import GPS
+from gs_utils.internal.utils import *
+
+expected = [
+ 'References for ch (main.cpp:11) (3 items in 2 files)',
+  ['main.cpp (1 item)',
+  ['<b>11:16</b>     auto a = obj.<b>ch</b>;'],
+  'my_class.hh (2 items)',
+  ['<b>7:16</b>      int num;char <b>ch</b>;My_Record_Of_Record rec;',
+  '<b>12:24</b>     My_Class(){num = 100;<b>ch</b> = &apos;A&apos;;}']]]
+
+
+@run_test_driver
+def run_test():
+    buf = GPS.EditorBuffer.get(GPS.File("main.cpp"))
+    buf.current_view().goto(buf.at(11, 17))
+    yield wait_idle()
+
+    GPS.execute_action("find all references")
+    yield hook("language_server_response_processed")
+    yield wait_idle()
+
+
+    gps_assert(dump_locations_tree(), expected)
+
