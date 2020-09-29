ef52f33503f10b4a7d85274b5dce9e23ad2d8536
Andry Ogorodnik <ogorodnik@adacore.com>
2020-09-29 13:53:09 +0300

    T924-005 Comment lines in a project file possibility
    Change-Id: Ie6cd3f786d005ccde03890a7931c480db23792c3

---
+++ testsuite/tests/T924-005.comment_project_lines/test.py
@@ -0,0 +1,35 @@
+"""
+This test checks that comment/uncomment work
+in a project file
+"""
+
+import GPS
+from gs_utils.internal.utils import *
+
+expect = '''project Test is
+   for Main use ("main.adb");
+
+   --  package Builder is
+   --     for Switches ("ada") use ("-g");
+   --  end Builder;
+
+end Test;
+'''
+
+
+@run_test_driver
+def test_driver():
+    buf = GPS.EditorBuffer.get(GPS.File("test.gpr"))
+    orig = buf.get_chars()
+
+    buf.current_view().goto(buf.at(4, 1))
+    GPS.execute_action("comment lines")
+    GPS.execute_action("comment lines")
+    GPS.execute_action("comment lines")
+    gps_assert(buf.get_chars(), expect, "Wrong content after commenting")
+
+    buf.current_view().goto(buf.at(4, 1))
+    GPS.execute_action("uncomment lines")
+    GPS.execute_action("uncomment lines")
+    GPS.execute_action("uncomment lines")
+    gps_assert(buf.get_chars(), orig, "Wrong content after uncommenting")
