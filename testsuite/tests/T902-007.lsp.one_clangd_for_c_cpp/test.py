b90b8e5ad9b5c437671c991019f912fd45b4bf98
Nicolas Setton <setton@adacore.com>
2020-09-30 20:01:05 +0000

    T902-007 fix counting of clangd processes
    Counting the processes is not compatible with parallelism. Instead,
    we need to count the clangd processes that are children of GNAT Studio.
    
    Tentatively do this using the psutil module that comes with e3, in
    theory this is portable.
    
    Change-Id: I05f4043f66bf8af741ba4930bb2fa9260ca9f230

--- testsuite/tests/T902-007.lsp.one_clangd_for_c_cpp/test.py
+++ testsuite/tests/T902-007.lsp.one_clangd_for_c_cpp/test.py
@@ -4,11 +4,11 @@ loading a mixed C/C++ project.
 """
 
 import GPS
+import os
 from gs_utils.internal.utils import *
 
 
 @run_test_driver
 def run_test():
-    out = GPS.Process("pidof clangd").get_result()
-    pids = out.split(' ')
-    gps_assert(len(pids), 1, "There should be only one instance of clangd")
+    out = GPS.Process("python count_clangds.py {}".format(os.getpid())).get_result()
+    gps_assert(out, "1", "There should be only one instance of clangd")
