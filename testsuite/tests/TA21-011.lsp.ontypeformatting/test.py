630061f9529d7b15c05264c5a20cb2883feba11c
Adrien Boulanger <boulanger@adacore.com>
2020-10-21 12:28:02 +0200

    TA21-011: Wrong location for OnTypeFormatting
    We were looking for the trigger character from the end of the
    selected range however it can be found multiple times inside
    resulting into a buffer corruption => look for it from the start.
    (The issue was hidden because of a missing -1 in the line
    computation).
    
    Furthermore, the LSP action was using an unitialized value
    for Set which was introducing garbage.
    
    Add a test.
    
    Change-Id: I99689bd29fc805ceb4c91fda4d4c16dc3368d247
    (cherry picked from commit 8e28e43d408a7ff25564e1511b3eaf9d84259404)

---
+++ testsuite/tests/TA21-011.lsp.ontypeformatting/test.py
@@ -0,0 +1,19 @@
+"""
+Test the OnTypeFormatting request at the start of a CPP file.
+"""
+import GPS
+from gs_utils.internal.utils import *
+
+
+@run_test_driver
+def test_driver():
+
+    buf = GPS.EditorBuffer.get(GPS.File("main.cpp"))
+    start = buf.get_chars(include_hidden_chars=False)
+    buf.insert(buf.at(1, 1), "     ")
+    pygps.send_key_event(GDK_RETURN)
+    yield wait_language_server('textDocument/onTypeFormatting', 'C++')
+    # The whitespaces from the insert will be removed
+    gps_assert(buf.get_chars(include_hidden_chars=False),
+               "\n" + start,
+               "Formatting does not work properly")
