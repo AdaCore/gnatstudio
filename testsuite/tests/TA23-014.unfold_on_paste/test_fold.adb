157f842a2d150458389400e6fc3b6c3d0a60fffa
Nicolas Setton <setton@adacore.com>
2020-10-23 15:11:38 +0100

    TA23-014 Properly unfold a block when editing a line
    When editing a line, we need to unfold the block underneath
    it before making the edition.
    
    Add a test.
    
    Change-Id: I9d5851b99ba44ab90bfa2c1be6659015b22422cb

---
+++ testsuite/tests/TA23-014.unfold_on_paste/test_fold.adb
@@ -0,0 +1,19 @@
+procedure Test_Fold is
+
+   procedure P1;
+
+   procedure P2;
+
+   procedure P1 is
+   begin
+      null;
+   end P1;
+
+   procedure P2 is
+   begin
+      null;
+   end P2;
+
+begin
+   null;
+end Test_Fold;
