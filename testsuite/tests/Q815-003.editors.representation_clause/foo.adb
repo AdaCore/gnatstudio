af0158b5bbaab200116a940de2aec6e839387f45
Piotr Trojanek <trojanek@adacore.com>
2020-12-02 23:53:06 +0100

    TC02-056 pretty-print conditional expressions in representation
    The show-representation plugin now pretty-prints conditional expressions, so
    that instead of:
    
       for Foo.Baz'Value_Size use
         ((#1 != 0) ?<> ((#1 != 1) ?<> ((#1 == 2)
         ?<> 160 ?<> 40) ?<> 128) ?<> 96);
    
    we are now showing:
    
       for Baz'Value_Size use
         (if (#1 != 0) then (if (#1 != 1) then (if (#1 == 2)
         then 160 else 40 end) else 128 end) else 96 end);
    
    exactly like in the textual output with -gnatR3 switch.
    
    no-precommit-check) still using python2 (reference to 'long')
    
    Change-Id: I943cb62d3b7a5874d41070904c2eb0bf122d8550

--- testsuite/tests/Q815-003.editors.representation_clause/foo.adb
+++ testsuite/tests/Q815-003.editors.representation_clause/foo.adb
@@ -2,6 +2,19 @@ with A;
 
 procedure Foo is
    type Bar is new Integer range 1 .. 100;
+   type Baz (D : Integer) is record
+      B : Boolean;
+      case D is
+         when 0 =>
+            C0A : Integer;
+         when 1 =>
+            C1A, C1B : Integer;
+         when 2 =>
+            C2A, C2B, C3C : Integer;
+         when others =>
+            null;
+      end case;
+   end record;
 begin
    A.E := 42;
    A.C;
