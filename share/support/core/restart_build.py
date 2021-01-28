b091519308ff703b847bb9485d0cc05f2125aaad
Andry Ogorodnik <ogorodnik@adacore.com>
2021-01-28 13:22:18 +0200

    SB29-024 'Restart build all' action has been added
    Interrupts the current building if any started and starts
     new 'Build All'.
    
    Change-Id: I35349598ca1537532e3ff08347a2a60ef2971c4e

---
+++ share/support/core/restart_build.py
@@ -0,0 +1,16 @@
+"""
+This file provides a 'Restart build all' action which interrupts
+the current build, if any, and starts a new build via the 
+'Build all' action.
+"""
+
+from GPS import Action, Task, Console
+from gs_utils import interactive
+
+
+@interactive('Build', name='Restart build all')
+def on_activate():
+    for task in Task.list():
+        if 'Build' in task.name():
+            task.interrupt()
+    Action("Build All").execute_if_possible()
