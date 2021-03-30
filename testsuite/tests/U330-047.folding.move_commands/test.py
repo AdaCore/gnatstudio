af5200b6938f7c37bb164ea4d72c8a5690ef778c
Anthony Leonardo Gracio <leonardo@adacore.com>
2021-03-30 17:57:05 +0200

    U330-047: move around visible lines for basic move commands
    An automatic test has been added.
    
    Change-Id: I719172624e77889b282b686b79cafb77e98851f2

---
+++ testsuite/tests/U330-047.folding.move_commands/test.py
@@ -0,0 +1,41 @@
+"""
+Verify that basic move commands (i.e: move to next line) work fine with
+folded blocks.
+"""
+
+from gs_utils.internal.utils import *
+from workflows.promises import known_tasks
+
+
+@run_test_driver
+def driver():
+    b = GPS.EditorBuffer.get(GPS.File("hello.adb"))
+    v = b.current_view()
+
+    # wait LSP responses has been processed
+    if GPS.LanguageServer.is_enabled_for_language_name("Ada"):
+        yield wait_tasks(other_than=known_tasks)
+
+    yield timeout(1000)
+    b.blocks_fold()
+
+    v.goto(b.at(2, 1))
+    GPS.execute_action("move to next line")
+    gps_assert(
+        v.cursor().line(), 6,
+        "'move to next line' does not work with folded blocks")
+
+    GPS.execute_action("move to previous line")
+    gps_assert(
+        v.cursor().line(), 2,
+        "'move to previous line' does not work with folded blocks")
+
+    GPS.execute_action("move to next paragraph")
+    gps_assert(
+        v.cursor().line(), 10,
+        "'move to next paragraph' does not work with folded blocks")
+
+    GPS.execute_action("move to previous paragraph")
+    gps_assert(
+        v.cursor().line(), 1,
+        "'move to previous paragraph' does not work with folded blocks")
