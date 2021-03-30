6cd8b2544ba4a979dcaebbb0bf0d2cf291ea63c1
Anthony Leonardo Gracio <leonardo@adacore.com>
2021-03-30 11:14:54 +0200

    U328-002: avoid redundant -rules option for GNATcheck
    We should avoid to specify the -rules option when the coding
    standard file is already specified in the .gpr file.
    
    Add an automatic test.
    
    Change-Id: I4ef46ba4c0db83f9f8c6bc36f86ace3921591ad7

---
+++ testsuite/tests/U328-002.gnatcheck.redundant_rules/test.py
@@ -0,0 +1,17 @@
+"""
+This test checks that we correctly invoke gnatcheck on non-native
+projects.
+"""
+import GPS
+from gs_utils.internal.utils import *
+
+
+@run_test_driver
+def test_driver():
+    GPS.EditorBuffer.get(GPS.File("main.adb"))
+    GPS.execute_action("gnatcheck root project")
+    yield wait_tasks(other_than=known_tasks)
+
+    gps_assert ("-rules" in GPS.Console().get_text(), False,
+                "The -rules option should not be speicified again, since " +
+                "it's already specified in the .gpr file")
