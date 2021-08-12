d9f6168eb98f299074713dd4030d659189d0599c
Adrien Boulanger <boulanger@adacore.com>
2021-08-12 15:51:45 +0200

    U712-029: Fix files being opened when creating codefixes
    Add a test.
    
    Change-Id: I4130cb54b35d216e8e3085376896c9cd9b47403a

---
+++ testsuite/tests/U712-029.codefix.casing/test.py
@@ -0,0 +1,27 @@
+"""
+Test the creation of codefix for bad casing
+"""
+
+from gs_utils.internal.utils import *
+from GPS import *
+
+
+@run_test_driver
+def run():
+    # Disable auto jump to first => it will prevent opening the first file
+    # with a build message (Thus no file should be opened)
+    GPS.Preference("locations-auto-jump-to-first").set(False)
+    GPS.execute_action("Build Main Number 1")
+    yield wait_tasks()
+    gps_assert(len(GPS.Message.list()),
+               6,
+               "Missing messages for bad casing")
+    gps_assert(GPS.MDI.get("foo.adb"),
+               None,
+               "foo.adb should not be opened")
+    gps_assert(GPS.MDI.get("bar.adb"),
+               None,
+               "bar.adb should bot be opened")
+    gps_assert(GPS.MDI.get("bar.ads"),
+               None,
+               "bar.ads should bot be opened")
