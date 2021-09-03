395d87428417616f823412fe02816716d565377e
Anthony Leonardo Gracio <leonardo@adacore.com>
2021-09-03 11:41:40 +0200

    T727-021: Don't propose aliases within dotted names
    Adapt test to cover this case.
    
    Change-Id: I6cbf2d67a0de496361ea742cbbf061ec88e2d940

--- testsuite/tests/T727-021.lsp.aliases_completion/test.py
+++ testsuite/tests/T727-021.lsp.aliases_completion/test.py
@@ -39,6 +39,8 @@ def run_test():
         "Wrong order for aliases completion items")
 
     GPS.execute_action("undo")
+
+    # Verify that aliases are not listed on '.'
     for ch in "Ada.":
         send_key_event(ord(ch))
         yield timeout(100)
@@ -50,5 +52,23 @@ def run_test():
 
     gps_assert(
         'for (alias)' in dump_tree_model(pop_tree.get_model(), LABEL_COLUMN),
+        False,
+        "Aliases should not be listed on dot completion")
+
+    send_key_event(GDK_ESCAPE)
+    yield wait_idle()
+
+    # Verify that aliases are not listed on dotted names (i.e: when completion
+    # is trigerred manually or dinamically within a dotted name)
+    send_key_event(ord('t'))
+    yield timeout(100)
+
+    pop_tree = get_widget_by_name("completion-view")
+    model = pop_tree.get_model()
+    yield wait_until_true(
+        lambda: model.get_value(model.get_iter_first(), 0) != "Computing...")
+
+    gps_assert(
+        'task (alias)' in dump_tree_model(pop_tree.get_model(), LABEL_COLUMN),
         False,
         "Aliases should not be listed after dotted names")
