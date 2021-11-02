5b1e264a3e0d59d09dfb8366eb9af9252ed10d67
Anthony Leonardo Gracio <leonardo@adacore.com>
2021-11-02 11:52:54 +0100

    UA11-032: Tentative to stabilize tests on Windows
    Change-Id: I847d3208e32041f867f5dcf8efa4ec4ba878b576

--- testsuite/tests/RC11-022.editor.status_bar_location/test.py
+++ testsuite/tests/RC11-022.editor.status_bar_location/test.py
@@ -26,38 +26,38 @@ def run_test():
                "Wrong label after moving the cursor")
 
     send_key_event(GDK_RIGHT)
-    yield wait_tasks(other_than=known_tasks)
+    yield wait_idle()
     gps_assert(button.get_label(),
                "1:3",
                "Wrong label after Key right")
 
     send_key_event(GDK_DOWN)
-    yield wait_tasks(other_than=known_tasks)
+    yield wait_idle()
     gps_assert(button.get_label(),
                "2:3",
                "Wrong label after Key down")
 
     # Movement with selection
     buf.select(buf.at(1, 1), buf.at(1, 2))
-    yield wait_tasks(other_than=known_tasks)
+    yield wait_idle()
     gps_assert(button.get_label(),
                "(1 line, 1 char) 1:2",
                "Wrong label after selecting 1 character")
 
     buf.select(buf.at(1, 1), buf.at(1, 5))
-    yield wait_tasks(other_than=known_tasks)
+    yield wait_idle()
     gps_assert(button.get_label(),
                "(1 line, 4 chars) 1:5",
                "Wrong label after selecting multiple characters")
 
     buf.current_view().goto(buf.at(1, 2))
-    yield wait_tasks(other_than=known_tasks)
+    yield wait_idle()
     gps_assert(button.get_label(),
                "1:2",
                "Wrong label after moving the cursor")
 
     send_key_event(GDK_DOWN, shift=True)
-    yield wait_tasks(other_than=known_tasks)
+    yield wait_idle()
     gps_assert(button.get_label(),
                "(2 lines, 17 chars) 2:2",
                "Wrong label after selecting Key down + Shift")
