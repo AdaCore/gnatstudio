edfdc05d666f33a44c38f9a569300671722127fe
Adrien Boulanger <boulanger@adacore.com>
2021-11-22 10:27:35 +0100

    UB17-028: Remove spurious message about LSP request errors
    Some errors are expected like ContentModified and RequestCanceled.
    Use a specific trace GPS.LSP_CLIENT.ERRORS for such error codes
    and GPS.LSP_CLIENT.EXCEPTIONS for others issues.
    
    Adapt test: the error is not shown anymore in the Messages view
    
    Change-Id: I738f7fb936e161926a0ffac5af52b6e4a207a6e4

--- testsuite/tests/S510-020.lsp.response_error/test.py
+++ testsuite/tests/S510-020.lsp.response_error/test.py
@@ -5,6 +5,7 @@ import GPS
 from gs_utils.internal.utils import run_test_driver, simple_error
 from workflows.promises import timeout
 
+
 @run_test_driver
 def driver():
     als = GPS.LanguageServer.get_by_language_name("Ada")
@@ -14,7 +15,3 @@ def driver():
 
     if not result.is_error:
         simple_error("we were expecting an error response")
-    msg = GPS.Console("Messages").get_text()
-
-    if "The language server has reported the following error" not in msg:
-        simple_error("error response must be displayed in Messages view")
