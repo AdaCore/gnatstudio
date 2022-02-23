eb3d2caf179ffaaf913d7c97edd19b1bae77bd67
Anthony Leonardo Gracio <leonardo@adacore.com>
2022-02-23 18:07:30 +0100

    V222-026: Place locked editors in a separate MDI group
    In order to keep them always visible when placed in a separate
    notebook. When the user will ctrl-click on an entity that requires
    to open a new editor, it will be opened in the 'default' group,
    letting the locked editor always visible.
    
    An existing test has been improved to cover this case.
    
    Change-Id: I398546f7a6af371e08da587690bb3712db785cd4

--- testsuite/tests/SA11-032.editors.locked.advanced/src/main.adb
+++ testsuite/tests/SA11-032.editors.locked.advanced/src/main.adb
@@ -1,6 +1,8 @@
 with Test;
+with Test_2;
 
 procedure Main is
 begin
    Test.Do_Nothing;
+   Test_2.Do_Nothing_2;
 end Main;
---
+++ testsuite/tests/SA11-032.editors.locked.advanced/src/test_2.ads
@@ -0,0 +1,5 @@
+package Test_2 is
+
+   procedure Do_Nothing_2 is null;
+
+end Test_2;
