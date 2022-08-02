19f5dc680e4ec76043dca26a104ba8cf5acaaedb
Anthony Leonardo Gracio <leonardo@adacore.com>
2022-08-02 15:29:49 +0200

    Fix tests on edge
    (no-tn-check)
    
    Change-Id: Ib33f2cfc767c36fd3716971372f721a99db2d3ae

--- testsuite/tests/U922-018.buffer_holders.project_load/test.cmd
+++ testsuite/tests/U922-018.buffer_holders.project_load/test.cmd
@@ -1,5 +1,5 @@
 ($GPS -P test.gpr --load=python:test.py --traceon=GPS.SOURCE_EDITOR.BUFFER  --traceon=GPS.MAIN.GPS 2>&1 ) | tr -d '\r' > traces.out
 # The last line should not be about destroying a buffer
 tail -1 traces.out > last_line.txt
-echo "   [GPS.MAIN.GPS] Shutdown" > expected.txt
+echo "[GPS.MAIN.GPS] Shutdown" > expected.txt
 diff last_line.txt expected.txt
