7b8ec16707cb668a09efa836ded2298a178818a0
Adrien Boulanger <boulanger@adacore.com>
2022-09-12 18:13:29 +0200

    V803-020: Tentative to stabilize test
    Change-Id: I05aa5b94f2d497c857959321c2a815699ef249d3

--- testsuite/tests/V803-020.lsp.snippet_pretty_print2/test.py
+++ testsuite/tests/V803-020.lsp.snippet_pretty_print2/test.py
@@ -29,19 +29,20 @@ def run_test():
         lambda: get_widget_by_name("completion-view") is not None)
     pop_tree = get_widget_by_name("completion-view")
     model = pop_tree.get_model()
-    yield wait_idle()
+    yield timeout(500)
     click_in_tree(pop_tree, path="0", events=double_click_events)
     yield wait_idle()
 
     gps_assert(buf.get_chars(frm=buf.at(7, 1)),
                EXPECTED,
-               "Wrong completion when for ',' without newline")
+               "Wrong completion for ',' without newline")
 
     # Remove completion and ","
     buf.undo()
     buf.undo()
     buf.undo()
     yield wait_idle()
+    yield timeout(500)
 
     # It should still work even with a new line
     buf.insert(buf.at(7, 1).end_of_line(), "\n")
@@ -50,10 +51,10 @@ def run_test():
         lambda: get_widget_by_name("completion-view") is not None)
     pop_tree = get_widget_by_name("completion-view")
     model = pop_tree.get_model()
-    yield wait_idle()
+    yield timeout(500)
     click_in_tree(pop_tree, path="0", events=double_click_events)
     yield wait_idle()
 
     gps_assert(buf.get_chars(frm=buf.at(7, 1)),
                EXPECTED,
-               "Wrong completion when for ',' without newline")
+               "Wrong completion for ',' with newline")
