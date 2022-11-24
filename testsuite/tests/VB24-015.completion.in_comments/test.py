ab3050ad737ef179f29d0e58a256bc45609c0cf8
Anthony Leonardo Gracio <leonardo@adacore.com>
2022-11-24 15:23:25 +0100

    VB24-015: Avoid aliases completion within comments
    Add an automatic test for this.
    
    Change-Id: Ia6bf806b8366cf0338c308d12682e2cd2629cb16

---
+++ testsuite/tests/VB24-015.completion.in_comments/test.py
@@ -0,0 +1,29 @@
+"""
+This test checks that we don't have any completion when
+typing within a comment.
+"""
+
+import GPS
+from gs_utils.internal.utils import *
+
+
+@run_test_driver
+def run_test():
+    GPS.Preference("Smart-Completion-Mode").set("3")
+    buf = GPS.EditorBuffer.get(GPS.File("main.adb"))
+    view = buf.current_view()
+    view.goto(buf.at(5, 1).end_of_line())
+    yield wait_idle()
+
+    # Type a character wthin an Ada comment: we should not have any results,
+    # in particular no alias completion (no 'main (alias)' result for instance)
+    for ch in "  m":
+        send_key_event(ord(ch))
+        yield timeout(100)
+
+    yield timeout(1000)
+    gps_assert(
+        get_widget_by_name("completion-view"),
+        None,
+        "We should not have any completion window",
+    )
