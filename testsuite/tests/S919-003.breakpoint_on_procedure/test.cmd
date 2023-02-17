5f71a184ec4b0a77beedcd2a1d778de51c7225d3
Andry Ogorodnik <ogorodnik@adacore.com>
2023-02-17 17:52:44 +0200

    V329-034 Fixing tests scripts for DAP

--- testsuite/tests/S919-003.breakpoint_on_procedure/test.cmd
+++ testsuite/tests/S919-003.breakpoint_on_procedure/test.cmd
@@ -3,7 +3,7 @@ rm -rf $GNATSTUDIO_HOME/.gnatstudio/properties.db
 rm -rf $GNATSTUDIO_HOME/.gnatstudio/properties.json
 $GPS -Pdefault --load=test.py --traceoff=GPS.DEBUGGING.Gdb_MI --traceon=MODULE.Debugger_Gdb_MI
 
-if [[ -v DAP_GDB ]]; then 
+if [ -v DAP_GDB ]; then 
   rm -rf $GNATSTUDIO_HOME/.gnatstudio/properties.db
   rm -rf $GNATSTUDIO_HOME/.gnatstudio/properties.json
   $GPS -Pdefault --load=test.py --traceon=GPS.DEBUGGING.DAP_MODULE --traceon=MODULE.Debugger_DAP
--- testsuite/tests/T701-032.breakpoints_in_assembly/test.cmd
+++ testsuite/tests/T701-032.breakpoints_in_assembly/test.cmd
@@ -1,6 +1,6 @@
 $GPS -Pdefault --load=test.py --traceoff=GPS.DEBUGGING.Gdb_MI --traceon=MODULE.Debugger_Gdb
 $GPS -Pdefault --load=test.py --traceoff=GPS.DEBUGGING.Gdb_MI --traceon=MODULE.Debugger_Gdb_MI
-if [[ -v DAP_GDB ]]; then
+if [ -v DAP_GDB ]; then
   $GPS -Pdefault --load=test.py --traceon=GPS.DEBUGGING.DAP_MODULE --traceon=MODULE.Debugger_DAP
 fi
 
