ca88d98ec88b2cada7dc3084db4eb53c5bb0c8f9
Anthony Leonardo Gracio <leonardo@adacore.com>
2023-03-13 09:50:59 +0000

    VB23-029: Adapt test to new LSP renaming diagnostics

--- testsuite/tests/V706-026.rename.error_in_location/test.py
+++ testsuite/tests/V706-026.rename.error_in_location/test.py
@@ -6,20 +6,22 @@ import GPS
 from gs_utils.internal.utils import *
 
 
-EXPECT_FIRST = [
-    'Renaming (1 item in 1 file)',
+EXPECT_FIRST =[
+    'Diagnostics: Ada (1 item in 1 file)',
     ['foo.adb (1 item)',
-     ['<b>3:13</b>      Renaming Bar to Foo_Bar creates a name collision']]]
+     ['<b>2:4</b>       Can&apos;t rename identifier &apos;Bar&apos;',
+      ['          Renaming Bar to Foo_Bar creates a name collision with &lt;Id &quot;Foo_Bar&quot; foo.adb:3:13-3:20&gt;']]]]
 
-EXPECT_SECOND = [
-    'Renaming (2 items in 1 file)',
-    ['foo.adb (2 items)',
-     ['<b>2:4</b>       Renaming Foo_Bar to Bar creates a name collision',
-      '<b>3:13</b>      Renaming Bar to Foo_Bar creates a name collision']]]
-
+    
+EXPECT_SECOND =  [
+    'Diagnostics: Ada (1 item in 1 file)',
+    ['foo.adb (1 item)',
+     ['<b>3:13</b>      Can&apos;t rename identifier &apos;Foo_Bar&apos;',
+      ['          Renaming Foo_Bar to Bar creates a name collision with &lt;Id &quot;Bar&quot; foo.adb:2:4-2:7&gt;']]]]
 
 @run_test_driver
 def driver():
+    GPS.Preference("LSP-Diagnostics-Display").set("Editor_And_Locations")
     buf = GPS.EditorBuffer.get(GPS.File("foo.adb"))
 
     def rename(loc, new_name):
