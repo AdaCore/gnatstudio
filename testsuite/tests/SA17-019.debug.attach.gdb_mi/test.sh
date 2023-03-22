71367fbbb1e390aa280d0f76a3afa2e114beaa09
Anthony Leonardo Gracio <leonardo@adacore.com>
2023-03-22 12:30:26 +0100

    SA17-019: Stabilize test
    Silence the kill command in case the program already stopped.

--- testsuite/tests/SA17-019.debug.attach.gdb_mi/test.sh
+++ testsuite/tests/SA17-019.debug.attach.gdb_mi/test.sh
@@ -3,4 +3,4 @@ gprbuild -p -q
 TESTPID=`pidof -s main_sa17_019`
 export TESTPID
 $GPS -Ptest.gpr --load=test.py --traceoff=GPS.DEBUGGING.Gdb_MI --traceon=MODULE.Debugger_Gdb_MI
-kill $TESTPID
+kill $TESTPID &> /dev/null
