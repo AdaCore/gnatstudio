cbe2eb961a28b7019ed6598beddba82ae170824d
Andry Ogorodnik <ogorodnik@adacore.com>
2023-07-11 17:50:03 +0300

    22 Add OpCodes to the assembly view

--- testsuite/tests/T701-029.gdb.opcodes/test.cmd
+++ testsuite/tests/T701-029.gdb.opcodes/test.cmd
@@ -1,7 +1,7 @@
 $GPS -Pdefault --load=test.py --traceoff=GPS.DEBUGGING.Gdb_MI --traceon=MODULE.Debugger_Gdb
 $GPS -Pdefault --load=test.py --traceoff=GPS.DEBUGGING.Gdb_MI --traceon=MODULE.Debugger_Gdb_MI
 
-# OpCodes do not implemenetd on the gdb side yet
+# Opcodes implemented in gdb 13.1.90
 #if [ "`type qgenc 2>/dev/null`" = "" ]; then
   # Do not run test with qgen
 #  v="$(gdb -v | head -n 1 | cut -c 14-16)"
