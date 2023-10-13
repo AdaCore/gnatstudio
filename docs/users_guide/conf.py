58916d24a0789df5b8ffc8144cc59e112cf8ce3b
bortolussi <bortolussi@adacore.com>
2023-10-13 17:34:30 +0200

    Update the logo in the docs
    Update the logo and background color in the top left corner of the doc.
    
    eng/ide/gnatstudio#103

--- docs/users_guide/conf.py
+++ docs/users_guide/conf.py
@@ -124,7 +124,9 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "style_nav_header_background": "#12284c",
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -138,7 +140,7 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = 'adacore_transparent.png'
+html_logo = 'adacore-logo-white.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
