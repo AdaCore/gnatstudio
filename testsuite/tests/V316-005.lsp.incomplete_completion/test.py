4189a8b99e3c0200348747fcaa8c7285009d0945
Anthony Leonardo Gracio <leonardo@adacore.com>
2025-05-14 10:10:20 +0000

    Adapt tests after bump to LLVM 19
    For eng/toolchain/llvm-project#17

--- testsuite/tests/V316-005.lsp.incomplete_completion/test.py
+++ testsuite/tests/V316-005.lsp.incomplete_completion/test.py
@@ -13,7 +13,7 @@ def run_test():
 
     for ch in "std::cou":
         send_key_event(ord(ch))
-        yield timeout(100)
+        yield timeout(300)
 
     pop_tree = get_widget_by_name("completion-view")
     gps_assert(
@@ -25,11 +25,11 @@ def run_test():
 
     for ch in "std::fa":
         send_key_event(ord(ch))
-        yield timeout(100)
-
+        yield timeout(300)
     pop_tree = get_widget_by_name("completion-view")
+    first_proposal = dump_tree_model(pop_tree.get_model(), 6)[0]
     gps_assert(
-        dump_tree_model(pop_tree.get_model(), 6)[0],
-        "•false_type",
+        first_proposal,
+        "•fabs(…)",
         "Wrong first completion proposal",
     )
