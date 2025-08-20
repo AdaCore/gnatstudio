6ce673653b96b1bee28649b378dd7da9a72b3beb
Anthony Leonardo Gracio <leonardo@adacore.com>
2025-08-20 13:08:20 +0000

    Stabilize test
    (no-issue-check)

--- testsuite/tests/als.1593.source_info_diagnostics/test.py
+++ testsuite/tests/als.1593.source_info_diagnostics/test.py
@@ -10,33 +10,30 @@ from gs_utils.internal.utils import *
 
 SOURCE_INFO_DIAGS_CATEGORY = "Diagnostics: ada.sourceInformation"
 
+
 @run_test_driver
 def run_test():
     # Make sure to enable source info diagnostics first
-    GPS.Preference('LSP-Ada-Source-Info-Diagnostics').set(True)
+    GPS.Preference("LSP-Ada-Source-Info-Diagnostics").set(True)
 
     # Open a file that does not belong to the project
-    buf = GPS.EditorBuffer.get(GPS.File("foo.adb"))
-    yield timeout(1000)
-
-    # Check that we get the expected source info diagnostic
-    msgs = GPS.Message.list(category=SOURCE_INFO_DIAGS_CATEGORY)
-    gps_assert(
-        len(msgs),
-        1,
-        "Wrong number of messages when source info diagnostics are enabled.",
-    )
-    diag_msg = msgs[0]
-    gps_assert(
-        diag_msg.get_category(),
-        SOURCE_INFO_DIAGS_CATEGORY,
-        "Wrong message category for expected diagnostic",
+    GPS.EditorBuffer.get(GPS.File("foo.adb"))
+    yield wait_until_true(
+        lambda: len(GPS.Message.list(category=SOURCE_INFO_DIAGS_CATEGORY)) == 1,
+        timeout=3000,
+        error_msg=(
+            "Timeout while waiting for source information diagnostic "
+            "to appear when opening a file that does not belong to the project."
+        ),
     )
 
     # Disable source info diagnostics
-    GPS.Preference('LSP-Ada-Source-Info-Diagnostics').set(False)
-    yield timeout(1000)
-    msgs = GPS.Message.list(category=SOURCE_INFO_DIAGS_CATEGORY)
-    gps_assert(
-        msgs, [], "The source info diagnostic should have disappeared"
+    GPS.Preference("LSP-Ada-Source-Info-Diagnostics").set(False)
+    yield wait_until_true(
+        lambda: len(GPS.Message.list(category=SOURCE_INFO_DIAGS_CATEGORY)) == 0,
+        timeout=3000,
+        error_msg=(
+            "Timeout while waiting for source information diagnostics to disappear "
+            "after disabling the 'LSP-Ada-Source-Info-Diagnostics' preference"
+        ),
     )
