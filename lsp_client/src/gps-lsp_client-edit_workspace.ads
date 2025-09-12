5ebe939579e76488b3ac762ff7fd0ca7178075bb
Vadim Godunko <godunko@adacore.com>
2025-09-12 10:58:12 +0400

    Fix indentation of the comments.

--- lsp_client/src/gps-lsp_client-edit_workspace.ads
+++ lsp_client/src/gps-lsp_client-edit_workspace.ads
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                               GNAT Studio                                --
 --                                                                          --
---                       Copyright (C) 2019-2025, AdaCore                   --
+--                     Copyright (C) 2019-2025, AdaCore                     --
 --                                                                          --
 -- This is free software;  you can redistribute it  and/or modify it  under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -34,24 +34,24 @@ package GPS.LSP_Client.Edit_Workspace is
       Limit_Span               : LSP.Messages.Span := LSP.Messages.Empty_Span;
       Compute_Minimal_Edits    : Boolean := False;
       Avoid_Cursor_Move        : Boolean := True);
-     --  Apply edit changes.
-     --  Title is used for information/error dialogs and for the messages
-     --  category when Show_Messages is True.
-     --  Make_Writable controls whether changing read-only files.
-     --  Allow_File_Renaming controls whether filenames should potentially be
-     --  renamed when appropriate (i.e: when renaming a package).
-     --  Locations_Message_Markup indicates what to display in the Locations
-     --  View: if it is the empty string, do not display messages in the
-     --  Locations view; if it is not empty, it needs to be valid pango markup.
-     --  Limit_Span is used to filter out Workspace_Edit and only allow Edits
-     --  within it, do nothing when empty.
-     --  Compute_Minimal_Edits controls whether we'll try to split the given
-     --  Edits into smaller ones, allowing to preserve the current cursor's
-     --  position: thus, this should only be used in particular contexts
-     --  (e.g: formatting). This uses an implementation of the Myers diff
-     --  algorithm.
-     --  If Avoid_Curson_Move is True, the cursor won't be moved to the
-     --  location of the last change being applied: it will be kept at the
-     --  current location.
+   --  Apply edit changes.
+   --  Title is used for information/error dialogs and for the messages
+   --  category when Show_Messages is True.
+   --  Make_Writable controls whether changing read-only files.
+   --  Allow_File_Renaming controls whether filenames should potentially be
+   --  renamed when appropriate (i.e: when renaming a package).
+   --  Locations_Message_Markup indicates what to display in the Locations
+   --  View: if it is the empty string, do not display messages in the
+   --  Locations view; if it is not empty, it needs to be valid pango markup.
+   --  Limit_Span is used to filter out Workspace_Edit and only allow Edits
+   --  within it, do nothing when empty.
+   --  Compute_Minimal_Edits controls whether we'll try to split the given
+   --  Edits into smaller ones, allowing to preserve the current cursor's
+   --  position: thus, this should only be used in particular contexts
+   --  (e.g: formatting). This uses an implementation of the Myers diff
+   --  algorithm.
+   --  If Avoid_Curson_Move is True, the cursor won't be moved to the
+   --  location of the last change being applied: it will be kept at the
+   --  current location.
 
 end GPS.LSP_Client.Edit_Workspace;
