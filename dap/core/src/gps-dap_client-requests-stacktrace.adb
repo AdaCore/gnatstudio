70cafd93ddcce857b0ce57798962fd581321fd61
Rebecca Wise <rebecca@berkeleynerd.com>
2025-11-06 15:38:13 +0000

    dap: add stacktrace request base in headless core

--- dap/core/src/gps-dap_client-requests-stacktrace.adb
+++ dap/core/src/gps-dap_client-requests-stacktrace.adb
@@ -4,14 +4,14 @@ with DAP.Tools.Outputs;
 package body GPS.DAP_Client.Requests.Stacktrace is
 
    overriding procedure Write
-     (Self   : StackTrace_Request;
+     (Self   : Stacktrace_Request;
       Stream : in out VSS.JSON.Content_Handlers.JSON_Content_Handler'Class) is
    begin
       DAP.Tools.Outputs.Output_StackTraceRequest (Stream, Self.Parameters);
    end Write;
 
    overriding procedure On_Result_Message
-     (Self        : in out StackTrace_Request;
+     (Self        : in out Stacktrace_Request;
       Stream      : in out VSS.JSON.Pull_Readers.JSON_Pull_Reader'Class;
       Success     : in out Boolean;
       New_Request : in out Request_Access)
@@ -21,13 +21,13 @@ package body GPS.DAP_Client.Requests.Stacktrace is
       DAP.Tools.Inputs.Input_StackTraceResponse (Stream, Response, Success);
 
       if Success then
-         StackTrace_Request'Class
+         Stacktrace_Request'Class
            (Self).On_Result_Message (Response, New_Request);
       end if;
    end On_Result_Message;
 
    overriding procedure Set_Seq
-     (Self : in out StackTrace_Request;
+     (Self : in out Stacktrace_Request;
       Id   : Integer) is
    begin
       Self.Parameters.seq := Id;
