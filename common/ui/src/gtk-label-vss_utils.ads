8fe3bbbc2a2b4956d552afeb76b00698e160eee3
Vadim Godunko <godunko@adacore.com>
2026-03-10 10:34:38 +0400

    Gtk.Label.VSS_Utils

---
+++ common/ui/src/gtk-label-vss_utils.ads
@@ -0,0 +1,32 @@
+------------------------------------------------------------------------------
+--                               GNAT Studio                                --
+--                                                                          --
+--                       Copyright (C) 2026, AdaCore                        --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+--  Various GtkLabel operations using `Virtual_String` type
+
+with VSS.Strings;
+
+package Gtk.Label.VSS_Utils is
+
+   procedure Gtk_New
+     (Label : out Gtk_Label;
+      Text  : VSS.Strings.Virtual_String);
+
+   procedure Set_Markup
+      (Label : not null access Gtk_Label_Record'Class;
+       Text  : VSS.Strings.Virtual_String);
+
+end Gtk.Label.VSS_Utils;
